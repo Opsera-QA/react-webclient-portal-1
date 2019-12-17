ce388b98fca2cbb4487b1a3c4822575722bfbd58
Todd Barczak <todd_barczak@symantec.com>
2019-12-17 15:07:02 -0500

    OC-21 Improving responsive design left nav

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,12 @@ class App extends Component {
         <Router>
           <Security {...config.oidc}>
             <Navbar />          
-            <div className="container-fluid row sidebar">
-              <div className="col-xl-2 col-md-2 col-12 bg-dark pt-4">
+            <div className="container-fluid" style={{ padding: 0}}>
+            <div className="flex-xl-nowrap row sidebar">
+              <div className="col-xl-2 col-md-3 col-12 d-flex flex-column bg-dark pt-4">
                 <Sidebar /> 
               </div>
-              <div className="col-xl-10 col-md-10 col-12 pt-4">
+              <div className="col-xl-8 col-md-9 col-12 pt-4">
                 <Container text>
                   <Route path="/" exact component={Home} />
                   <Route path="/signup" exact component={Signup} />
@@ -46,6 +47,7 @@ class App extends Component {
                   <SecureRoute path="/api_connector" component={ApiConnector} />
                 </Container>
               </div>
+            </div>
             </div>
           </Security>
         </Router>
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -25,15 +25,15 @@ export default withAuth(class Sidebar extends Component {
     return (
       <Nav defaultActiveKey="/" className="d-flex flex-row flex-md-column sidebar">
         <Nav.Link className="nav-link-light" href="/"><FontAwesomeIcon icon={faHome} fixedWidth /> Home</Nav.Link>
-        {this.state.authenticated && <Nav.Link className="nav-link-light" href="/home"><FontAwesomeIcon icon={faPlus} fixedWidth /> New Platform</Nav.Link>}
-        {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-1"><FontAwesomeIcon icon={faWrench} fixedWidth /> CI/CD Pipeline</Nav.Link>}
-        {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-2"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</Nav.Link>}
+        {this.state.authenticated && <Nav.Link className="nav-link-light" href="/home"><FontAwesomeIcon icon={faPlus} fixedWidth /> Platform</Nav.Link>}
+        {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-1"><FontAwesomeIcon icon={faWrench} fixedWidth /> Pipeline</Nav.Link>}
+        {/* {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-2"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</Nav.Link>} */}
         
-        {this.state.authenticated && <Nav.Link className="nav-link-light mt-3" href="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</Nav.Link>}
+        {this.state.authenticated && <Nav.Link className="nav-link-light" href="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</Nav.Link>}
         {this.state.authenticated && <Nav.Link className="nav-link-light" href="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</Nav.Link>}
         {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-2"><FontAwesomeIcon icon={faEnvelope} fixedWidth /> Upgrades</Nav.Link>}
         {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-2"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Reporting</Nav.Link>}
-        {this.state.authenticated && <Nav.Link className="nav-link-light" eventKey="link-2"><FontAwesomeIcon icon={faTimes} fixedWidth /> Delete Tools</Nav.Link>}
+        {this.state.authenticated && <Nav.Link className="nav-link-light" disabled eventKey="link-2"><FontAwesomeIcon icon={faTimes} fixedWidth /> Delete Tools</Nav.Link>}
 
         {!this.state.authenticated && <Nav.Link className="nav-link-light" href="/about">Customers</Nav.Link>}      
         {!this.state.authenticated && <Nav.Link className="nav-link-light" href="/about/solutions">Solutions</Nav.Link>}
