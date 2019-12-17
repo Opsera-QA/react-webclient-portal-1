da5a45ded2083c5cba3786f53c6cb9d535e2f5e9
Todd Barczak <todd_barczak@symantec.com>
2019-12-17 10:03:21 -0500

    OC-21: Add responsivle layout for inline display

--- src/Home.jsx
+++ src/Home.jsx
@@ -35,14 +35,14 @@ export default withAuth(class Home extends Component {
           <div style={{ marginTop: 15 }}>      
             {this.state.authenticated &&
               <div>
-                <h3>Welcome back, {this.state.userinfo.name}!</h3>
+                <h2>Welcome back, {this.state.userinfo.name}!</h2>
                 <p>
-                  You have successfully authenticated against your Okta OpsERA Organization.  You now have an ID token and access token in local storage.
-                Visit the <a href="/profile">My Profile</a> page to take a look inside the ID token.
-              </p>
-                <br/><br/><br/>
-                <h3>Jira Ticket OC-16 Work: Build out Dashboard as Inventory in this system</h3>
-                <p>Link to inventory view:  <a href="/inventory">Inventory</a></p>
+                  You have successfully logged in!  You now have an ID token and access token in local storage.
+                  Visit the <a href="/profile">My Profile</a> page to take a look inside the ID token.
+                </p>
+                
+                <h3 style={{ marginTop: 25 }}>Features:</h3>
+                <div><a href="/inventory">My Inventory</a></div>
     
               </div>
             }
@@ -50,16 +50,16 @@ export default withAuth(class Home extends Component {
               <div style={{ marginTop: 25 }}>
                 
                 <div className="row">
-                  <div className="col-5 mx-auto col-md-5 order-md-2">
+                  <div className="col-5 mx-auto col-md-5 order-md-2 d-none d-lg-inline">
                     <img src="/img/opsera_logo_large.png" width="375" alt="" />
                   </div>
-                  <div className="col-md-7 order-md-1 text-center text-md-left pr-md-5">
+                  <div className="col-md-12 col-lg-7 order-md-1 text-center text-md-left pr-md-5">
                     <h1 class="mb-3 bd-text-purple-bright">OpsERA</h1>
                     <p class="lead">
                       Make DevOps a streamlined, managed experience, allowing developers to focus on what they enjoy doing most: writing code!
                     </p>
                     <p class="lead mb-4">
-                    OpsERA is an end to end DevOps Workflow Solution that can manage all of the tasks and resources for a team’s CI/CD Pipeline automatically, allowing for a single pane of glass view on the entire DevOps workflow including easy to use interfaces and advanced, consolidated error reporting and usage and optimization reporting.
+                      OpsERA is an end to end DevOps Workflow Solution that can manage all of the tasks and resources for a team’s CI/CD Pipeline automatically, allowing for a single pane of glass view on the entire DevOps workflow including easy to use interfaces and advanced, consolidated error reporting and usage and optimization reporting.
                     </p>
                     <div class="row mx-n2">
                       <div class="col-md px-2">
