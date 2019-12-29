b696a6adbac2930e8d232f86000fdec109c4349e
Todd Barczak <todd_barczak@symantec.com>
2019-12-29 13:17:59 -0500

    Enable active link on side navbar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import {AuthContext} from './contexts/AuthContext';
+import { withRouter } from "react-router";
 import { Nav } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faWrench, faChartLine, faClipboardList, faLink, faEnvelope, faDownload, faHome, faTools } from '@fortawesome/free-solid-svg-icons'
@@ -11,9 +12,10 @@ class Sidebar extends Component {
   static contextType = AuthContext;
 
   render() {
+    const { location } = this.props;
     const { authenticated, } = this.context;
     return (
-      <Nav defaultActiveKey="/" className="d-flex flex-row flex-md-column sidebar">
+      <Nav defaultActiveKey="/" activeKey={location.pathname} className="d-flex flex-row flex-md-column sidebar">
         <Nav.Link className="nav-link-light" href="/"><FontAwesomeIcon icon={faHome} fixedWidth /> Home</Nav.Link>
         { authenticated && <Nav.Link className="nav-link-light" href="/platform"><FontAwesomeIcon icon={faPlus} fixedWidth /> Platform</Nav.Link>}
         { authenticated && <Nav.Link className="nav-link-light" href="/pipeline"><FontAwesomeIcon icon={faWrench} fixedWidth /> Pipeline</Nav.Link>}
@@ -23,11 +25,11 @@ class Sidebar extends Component {
         { authenticated && <Nav.Link className="nav-link-light" href="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</Nav.Link>}
         { authenticated && <Nav.Link className="nav-link-light" href="/update"><FontAwesomeIcon icon={faDownload} fixedWidth /> Updates</Nav.Link>}
         
-        { authenticated && <Nav.Link className="nav-link-light" href="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</Nav.Link>}
+        { authenticated && <Nav.Link className="nav-link-light + (this.active ? 'nav-link-active' : '')" href="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</Nav.Link>}
         {/* { authenticated && <Nav.Link className="nav-link-light" disabled href="/removal"><FontAwesomeIcon icon={faTimes} fixedWidth /> Delete Tools</Nav.Link>} */}
       </Nav>
     );
   }
 };
 
-export default Sidebar;
+export default withRouter(Sidebar);
