fc516fb25d36183986c3975e5836ff8b63ea37ba
Todd Barczak <todd_barczak@symantec.com>
2020-01-09 14:33:46 -0500

    Add ESLint to project, tweak based on testing

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,24 +1,24 @@
-const axios = require('axios');
-const config = require('../config');
+const axios = require("axios");
+const config = require("../config");
 
 const axiosInstance = axios.create({
   baseURL: config.apiServerUrl,
   timeout: 10000,
-})
+});
 
 const setInterceptorToken = (authToken) => {
   axiosInstance.interceptors.request.use(function (config) {
-    const token = 'Bearer ' + authToken;
+    const token = "Bearer " + authToken;
     config.headers.Authorization =  token;
     return config;
   },authToken);
-}
+};
 
 export class ApiService {
   constructor( url, params, token, data ) {
-    this.url = url
-    this.params = params
-    this.data = data //POST BODY Data JSON format
+    this.url = url;
+    this.params = params;
+    this.data = data; //POST BODY Data JSON format
     if (token) {setInterceptorToken(token);}
   }
 
@@ -26,22 +26,22 @@ export class ApiService {
     var self = this;   
     console.log(self.params);
     return axiosInstance({
-      method: 'get',
+      method: "get",
       url: self.url,
       params : self.params,
-      responseType: 'stream'
-    })
+      responseType: "stream"
+    });
   }
 
   post() {
     var self = this;   
     return axiosInstance({
-      method: 'post',
+      method: "post",
       url: self.url,
       params : self.params,
       data : self.data,
-      responseType: 'stream'
-    })
+      responseType: "stream"
+    });
   }
 
   //TODO: Add Update Method
@@ -51,11 +51,11 @@ export class ApiService {
   delete() {
     var self = this;   
     return axiosInstance({
-      method: 'delete',
+      method: "delete",
       url: self.url,
       params: self.params,
       // responseType: 'stream'
-    })
+    });
   }
 }
 
--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import PropTypes from "prop-types";
 import LoadingDialog from "../common/loading";
 import ErrorDialog from "../common/error";
 import { handleError } from "../../helpers";
-import { AuthContext } from '../../contexts/AuthContext'; //New AuthContext State 
-import { ApiService } from '../../api/apiService';
+import { AuthContext } from "../../contexts/AuthContext"; //New AuthContext State 
+import { ApiService } from "../../api/apiService";
 
 class Update extends Component {
   static contextType = AuthContext;  //Import AuthContext values into Component
@@ -29,14 +30,14 @@ class Update extends Component {
     const accessToken = await getAccessToken();
     const userInfo = await getUserInfo();
     const urlParams = { userid: userInfo.sub };
-    const apiCall = new ApiService('/tools/upgradable', urlParams, accessToken);
+    const apiCall = new ApiService("/tools/upgradable", urlParams, accessToken);
     let currentComponent = this;
     apiCall.get()
       .then(function (response) {
         currentComponent.setState({
           apps: response.apps,
           error: false,
-          messages: 'API call was successful!'
+          messages: "API call was successful!"
         });
       })
       .catch(function (error) {
@@ -44,7 +45,7 @@ class Update extends Component {
 
         currentComponent.setState({
           error: true,
-          messages: message ? message : 'Error reported accessing API.'
+          messages: message ? message : "Error reported accessing API."
         });
       })
       .finally(function () {
@@ -57,21 +58,22 @@ class Update extends Component {
     let currentComponent = this;
     this.setState(ps => ({
       disabledIds: [...ps.disabledIds, `${applicationId}${toolId}`],
-    }))
+    }));
 
     //TODO: The token MAY not be required here because of the interceptor set in apiService, so test that
-    const apiCall = new ApiService('/tools/upgrade', null, null, { applicationId, toolId });
+    const apiCall = new ApiService("/tools/upgrade", null, null, { applicationId, toolId });
     apiCall.post()
       .then(function (response) {
+        console.log(response);
         currentComponent.setState({
           error: false,
-          messages: 'Tool Submitted for upgrade, you will be notified completeness.'
+          messages: "Tool Submitted for upgrade, you will be notified completeness."
         });
       })
       .catch(function (error) {
         currentComponent.setState({
           error: true,
-          messages: 'Error reported updating the tool.'
+          messages: "Error reported updating the tool."
         });
         console.log(`Error Reported: ${error}`);
       })
@@ -82,19 +84,19 @@ class Update extends Component {
   }
 
   loader = () => {
-    const { fetching } = this.state
+    const { fetching } = this.state;
     return fetching ? (
       <Button loading style={{ backgroundColor: "transparent" }} />
-    ) : null
+    ) : null;
   }
 
   messages = () => {
-    const { fetching, apps } = this.state
-    return !fetching && apps.length === 0 ? <p>No Tools To Upgrade</p> : <LoadingDialog />
+    const { fetching, apps } = this.state;
+    return !fetching && apps.length === 0 ? <p>No Tools To Upgrade</p> : <LoadingDialog />;
   }
 
   toolList = () => {
-    const { apps, disabledIds } = this.state
+    const { apps, disabledIds } = this.state;
     return (
       <Row>
         {apps ? apps.map(app => {
@@ -107,16 +109,16 @@ class Update extends Component {
               key={i}
               i={i}
             />
-          ))
+          ));
         }) : <div className="col mx-auto">
-              <div class="text-center m-4">No Updates Required</div>
-            </div>}
+          <div className="text-center m-4">No Updates Required</div>
+        </div>}
       </Row>
-    )
+    );
   }
 
   render() {
-    const { error, messages, fetching } = this.state
+    const { error, messages, fetching } = this.state;
     return (
       <div>
         <h3>Updates</h3>
@@ -131,45 +133,56 @@ class Update extends Component {
           {this.toolList()}
         </div>
       </div>
-    )
+    );
   }
 }
 
-function ToolView({
-  handleButtonClick = () => { },
-  disabledIds = [],
-  app: { _id: applicationId, name: appname },
-  tool: { _id: toolId, name: toolName, versionNumber },
-}) {
+
+Update.propTypes = {
+  handleButtonClick: PropTypes.func,
+  disabledIds: PropTypes.array,
+  app: PropTypes.shape({
+    _id: PropTypes.string,
+    name: PropTypes.string
+  }),
+  tool:{
+    _id: PropTypes.string,
+    name: PropTypes.string,
+    versionNumber: PropTypes.string
+  }
+};
+
+const ToolView = () => {
+  const {handleButtonClick, disabledIds, app, tool } = this.props;
   return (
     <Row>
       <Col>
-        <h2 style={{ margin: 0 }}>{appname}</h2>
-        <h3 style={{ margin: 0 }}>{toolName}</h3>
+        <h2 style={{ margin: 0 }}>{app.name}</h2>
+        <h3 style={{ margin: 0 }}>{tool.toolName}</h3>
       </Col>
       <Col>
-        <div>version number: {versionNumber || "unknown"}</div>
-        {toolName === "Jenkins" && (
+        <div>version number: {tool.versionNumber || "unknown"}</div>
+        {tool.name === "Jenkins" && (
           <div style={{ fontWeight: "bold", color: "green" }}>
             New version available for upgrade: 2.152
-              </div>
+          </div>
         )}
-        {toolName === "Artifactory" && (
+        {tool.name === "Artifactory" && (
           <div style={{ fontWeight: "bold", color: "green" }}>
             New version available for upgrade: 6.5
-              </div>
+          </div>
         )}
       </Col>
       <Col>
         <Button
           primary
-          disabled={disabledIds.includes(`${applicationId}${toolId}`)}
-          onClick={() => handleButtonClick({ applicationId, toolId })}
+          disabled={disabledIds.includes(`${app._id}${tool._id}`)}
+          onClick={() => handleButtonClick()}
         >
           Update
-            </Button>
+        </Button>
       </Col>
     </Row>
-  )
-}
+  );
+};
 export default Update;
