2e3fad5e44dc629c70f2ceed9bcb76a8df4b3abd
Todd Barczak <todd_barczak@symantec.com>
2020-01-13 14:14:52 -0500

    Flush out Signup form to work with new Okta settings and ssoUsers Model

--- src/helpers.js
+++ src/helpers.js
@@ -3,18 +3,18 @@ export function isLocalHost(hostname) {
     hostname === "localhost" ||
     hostname === "[::1]" ||
     hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
-  )
+  );
 }
 
 // check if char/string is alpha number or has a dash.
 export function isAlphaNumeric(str) {
-  return /^[a-z0-9-]+$/i.test(str)
+  return /^[a-z0-9-]+$/i.test(str);
 }
 
 export function validateEmail(email) {
   // eslint-disable-next-line
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(email).toLowerCase())
+  return re.test(String(email).toLowerCase());
 }
 
 export function handleError(error) {
