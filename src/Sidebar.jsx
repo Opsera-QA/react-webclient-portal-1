afb1c6959e03409476fc6e048cc19e7bb2dced8f
Todd Barczak <todd_barczak@symantec.com>
2020-01-15 16:14:48 -0500

    refactoring Sidebar as a Functional Compontent w/ Hooks & Context

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,34 +1,42 @@
 
-import React, { Component } from "react";
+import React, { useContext, useReducer, useEffect } from "react";
 import { AuthContext } from "./contexts/AuthContext";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStream, faChartLine, faClipboardList, faLink, faToolbox, faDownload, faHome, faTools } from "@fortawesome/free-solid-svg-icons";
 import "./sidebar.css";
 
+function Sidebar() {
+  const contextType = useContext(AuthContext);
+  const [state, setState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {authenticated: false, administrator: false}
+  );
 
-class Sidebar extends Component {
-  static contextType = AuthContext;
+  useEffect(() => {
+    const { authenticated, userInfo } = contextType;
+    setState({ authenticated: authenticated});
+    if (userInfo) {
+      setState({ administrator: userInfo.email.includes("opsera.io")});
+    }
 
-  render() {
-    const { authenticated, } = this.context;
-    return (
-      <div className="d-flex flex-row flex-md-column sidebar">
-        <div className="navbar-list">
-          <NavLink className="nav-link" activeClassName="chosen" exact to="/"><FontAwesomeIcon icon={faHome} fixedWidth /> Home</NavLink>
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</NavLink>}
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/platform"><FontAwesomeIcon icon={faToolbox} fixedWidth /> Platform</NavLink>}
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/pipeline"><FontAwesomeIcon icon={faStream} fixedWidth /> Pipeline</NavLink>}
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/analytics"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</NavLink>}
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</NavLink>}
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/update"><FontAwesomeIcon icon={faDownload} fixedWidth /> Updates</NavLink>}
+  }, [contextType]); 
 
-          {authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</NavLink>}
-          {/* { authenticated && <Nav.Link className="nav-link-light" disabled href="/removal"><FontAwesomeIcon icon={faTimes} fixedWidth /> Delete Tools</Nav.Link>} */}
-        </div>
+  return (
+    <div className="d-flex flex-row flex-md-column sidebar">
+      <div className="navbar-list">
+        <NavLink className="nav-link" activeClassName="chosen" exact to="/"><FontAwesomeIcon icon={faHome} fixedWidth /> Home</NavLink>
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</NavLink>}
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/platform"><FontAwesomeIcon icon={faToolbox} fixedWidth /> Platform</NavLink>}
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/pipeline"><FontAwesomeIcon icon={faStream} fixedWidth /> Pipeline</NavLink>}
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/analytics"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</NavLink>}
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</NavLink>}
+        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/update"><FontAwesomeIcon icon={faDownload} fixedWidth /> Updates</NavLink>}
+
+        {state.administrator && <NavLink className="nav-link" activeClassName="chosen" to="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</NavLink>}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Sidebar;
