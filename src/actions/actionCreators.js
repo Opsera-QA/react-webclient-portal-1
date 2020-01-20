0efdc4de0d127e601f87d89c00eaed23f6798620
Todd Barczak <todd_barczak@symantec.com>
2020-01-20 10:43:21 -0500

    Remove additional legacy code

--- src/actions/actionCreators.js
+++
@@ -1,6 +0,0 @@
-import * as at from "./actionTypes"
-
-export const getApps = payload => ({
-  type: at.GET_APPS,
-  payload,
-})
--- src/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const GET_APPS = "GET_APPS"
-export const SET_OKTA_USER = "SET_OKTA_USER"
