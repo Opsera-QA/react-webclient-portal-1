61e85d48794e2dd0e589916dae77600964f9b670
Todd Barczak <todd_barczak@symantec.com>
2020-01-21 09:50:42 -0500

    Tweak to hide Admin Tools behind an "Admin" group.  Refactored how authenticated Links are rendered

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -9,15 +9,15 @@ import "./sidebar.css";
 function Sidebar() {
   const contextType = useContext(AuthContext);
   const [state, setState] = useReducer(
-    (state, newState) => ({...state, ...newState}),
-    {authenticated: false, administrator: false}
+    (state, newState) => ({ ...state, ...newState }),
+    { authenticated: false, administrator: false }
   );
 
   useEffect(() => {
     const { authenticated, userInfo } = contextType;
-    setState({ authenticated: authenticated});
+    setState({ authenticated: authenticated });
     if (userInfo) {
-      setState({ administrator: userInfo.email.includes("opsera.io")});
+      setState({ administrator: userInfo.Groups.includes("Admin") });
     }
 
   }, [contextType]); 
@@ -26,17 +26,20 @@ function Sidebar() {
     <div className="d-flex flex-row flex-md-column sidebar">
       <div className="navbar-list">
         <NavLink className="nav-link" activeClassName="chosen" exact to="/"><FontAwesomeIcon icon={faHome} fixedWidth /> Home</NavLink>
-
-        <div className="mt-3 side-nav-header-text">Products</div>
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</NavLink>}
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/platform"><FontAwesomeIcon icon={faPlus} fixedWidth /> New Platform</NavLink>}
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/pipeline"><FontAwesomeIcon icon={faStream} fixedWidth /> Pipelines</NavLink>}
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/analytics"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</NavLink>}
+        {state.authenticated &&
+        <>
+          <div className="mt-3 side-nav-header-text">Products</div>
+          <NavLink className="nav-link" activeClassName="chosen" to="/inventory"><FontAwesomeIcon icon={faClipboardList} fixedWidth /> Inventory</NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/platform"><FontAwesomeIcon icon={faPlus} fixedWidth /> New Platform</NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/pipeline"><FontAwesomeIcon icon={faStream} fixedWidth /> Pipelines</NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/analytics"><FontAwesomeIcon icon={faChartLine} fixedWidth /> Analytics</NavLink>
         
-        <div className="mt-3 side-nav-header-text">Operations</div>
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</NavLink>}
-        {state.authenticated && <NavLink className="nav-link" activeClassName="chosen" to="/update"><FontAwesomeIcon icon={faDownload} fixedWidth /> Updates</NavLink>}
-        {state.administrator && <NavLink className="nav-link" activeClassName="chosen" to="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</NavLink>}
+          <div className="mt-3 side-nav-header-text">Operations</div>
+          <NavLink className="nav-link" activeClassName="chosen" to="/api_connector"><FontAwesomeIcon icon={faLink} fixedWidth /> Connectors</NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/update"><FontAwesomeIcon icon={faDownload} fixedWidth /> Updates</NavLink>
+          {state.administrator && <NavLink className="nav-link" activeClassName="chosen" to="/admin"><FontAwesomeIcon icon={faTools} fixedWidth /> Admin Tools</NavLink>}
+        </>
+        }
       </div>
     </div>
   );
