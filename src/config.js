2c6af06ecb7a3b13dbb91450e7931c4d0a5501ae
Todd Barczak <todd_barczak@symantec.com>
2020-01-23 08:09:08 -0500

    OC-84 Move APIConnectors URL into .env config files

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ const CLIENT_ID = process.env.REACT_APP_OKTA_CLIENT_ID
   , OKTA_TESTING_DISABLEHTTPSCHECK = process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false
   , oktaRedirectUri = process.env.REACT_APP_OPSERA_OKTA_REDIRECTURI;
 
-export const apiServerUrl = `https://${process.env.REACT_APP_OPSERA_API_HOST}`;
+export const apiServerUrl = process.env.REACT_APP_OPSERA_API_SERVER_URL;
+export const apiConnectorURL = process.env.REACT_APP_OPSERA_API_CONNECTORS_URL;
 
 export default {
   oidc: {
@@ -20,8 +21,8 @@ export default {
   },
 };
 
-// backend urls: TODO: Refactor this
+// backend urls: TODO: After OC-83 is complete, move into .env files (with proper DNS)
 export const BACKENDURIJava = "https://serveropsera.com:9091/api";
 export const baseURL = "http://ec2-3-17-157-88.us-east-2.compute.amazonaws.com:3000";
 export const oldURL = "http://ec2-18-220-143-66.us-east-2.compute.amazonaws.com:3000";
-export const apiConnectorURL = "http://52.15.121.104:3040/api/";
+
