e97579085242f14e1d0e6db9db74094e542d072d
Todd Barczak <todd_barczak@symantec.com>
2020-02-03 13:39:05 -0500

    OC-95 Layout changes to the report

--- src/components/admin/RegisteredUsers.jsx
+++ src/components/admin/RegisteredUsers.jsx
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react'
-import { Table } from "react-bootstrap"
+import React, { PureComponent } from "react";
+import { Table, Row, Col } from "react-bootstrap";
 import ErrorDialog from "../common/error";
 import LoadingDialog from "../common/loading";
 import Moment from "react-moment";
@@ -9,108 +9,98 @@ import { ApiService } from "../../api/apiService";
 export default class RegisteredUsers extends PureComponent {
     static contextType = AuthContext;
     constructor(props, context) {
-        super(props, context);
-        this.state = {
-            data: [],
-            fetching: true,
-            error: null,
-            messages: null
-        };
+      super(props, context);
+      this.state = {
+        data: [],
+        fetching: true,
+        error: null,
+        messages: null
+      };
     }
 
     componentDidMount() {
-        this.getApiData();
+      this.getApiData();
     }
 
     async getApiData() {
-        const { getAccessToken } = this.context;
-        const accessToken = await getAccessToken();
-        const apiCall = new ApiService("/users/get-users", {}, accessToken);
-        let currentComponent = this;
-        apiCall.get()
-            .then(function (response) {
-                // console.log(response.data)
-                currentComponent.setState({
-                    data: response.data,
-                    error: null,
-                    fetching: false
-                });
-            })
-            .catch(function (error) {
-                currentComponent.setState({
-                    error: error,
-                    fetching: false
-                });
-            });
+      const { getAccessToken } = this.context;
+      const accessToken = await getAccessToken();
+      const apiCall = new ApiService("/users/get-users", {}, accessToken);
+      let currentComponent = this;
+      apiCall.get()
+        .then(function (response) {
+          // console.log(response.data)
+          currentComponent.setState({
+            data: response.data,
+            error: null,
+            fetching: false
+          });
+        })
+        .catch(function (error) {
+          currentComponent.setState({
+            error: error,
+            fetching: false
+          });
+        });
     }
 
     render() {
-        const { data, error, fetching } = this.state;
-        console.log(data)
-        return (
-            <>
-                <h3 style={{ padding: "20px" }}>Registered Users</h3>
+      const { data, error, fetching } = this.state;
+      console.log(data);
+      return (
+        <>
+          <h3 style={{ padding: "20px" }}>Registered Users</h3>
 
-                {error ? <ErrorDialog error={error} /> : null}
-                {fetching && <LoadingDialog />}
+          {error ? <ErrorDialog error={error} /> : null}
+          {fetching && <LoadingDialog />}
 
-                <Table responsive>
-                    {/* <thead>
-                        <tr>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email</th>
-                            <th>Organization</th>
-                            <th>Division</th>
-                            <th>Domain</th>
-                            <th>Created</th>
-                        </tr>
-                    </thead> */}
-                    <tbody>
-                        {data.map(val => (
-                            <>
-                                <br></br>
-                                <tr>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Email</th>
-                                    <th>Organization</th>
-                                    <th>Division</th>
-                                    <th>Domain</th>
-                                    <th>Created</th>
-                                </tr>
-                                <tr>
-                                    <td>{val.firstName}</td>
-                                    <td>{val.lastName}</td>
-                                    <td>{val.email}</td>
-                                    <td>{val.organizationName}</td>
-                                    <td>{val.division}</td>
-                                    <td>{val.domain}</td>
-                                    <td><Moment format="MM/DD/YYYY" date={val.createdAt} /></td>
-                                </tr>
-                                {val.tools.length > 0 && (
-                                    <>
-                                        <tr>
-                                            <th>Tool Name</th>
-                                            <th>Status</th>
-                                            <th>Tool ID</th>
-
-                                        </tr>
-                                        {val.tools.map(tool => (
-                                            <tr>
-                                                <td>{tool.name}</td>
-                                                <td>{tool.toolStatus}</td>
-                                                <td>{tool._id}</td>
-                                            </tr>
-                                        ))}
-                                    </>
-                                )}
-                                <br></br>
-                            </>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Organization</th>
+                <th>Division</th>
+                <th>Domain</th>
+                <th>Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map(val => (
+                <>
+                  <tr>
+                    <td>{val.firstName}</td>
+                    <td>{val.lastName}</td>
+                    <td>{val.email}</td>
+                    <td>{val.organizationName}</td>
+                    <td>{val.division}</td>
+                    <td>{val.domain}</td>
+                    <td>111<Moment format="MM/DD/YYYY" date={val.createdAt} /></td>
+                  </tr>
+                  {val.tools.length > 0 ? (
+                    <tr>
+                      <td colSpan="7" style={{ borderTop:0, paddingTop:0, marginTop:0, paddingBottom:"25px" }}>
+                        {val.tools.map((tool, index) => (
+                          <Row key={index} style={{ marginLeft:"10px", fontSize:".9em" }}>
+                            <Col>{tool.name}</Col>
+                            <Col>{tool.toolStatus}</Col>
+                            <Col>{tool._id}</Col>
+                          </Row>
                         ))}
-                    </tbody>
-                </Table>
-            </>
-        )
+                      </td>
+                    </tr>
+                  ) : 
+                    <tr>
+                      <td colSpan="7" className="text-muted text-center" style={{ borderTop:0, paddingBottom:"25px" }}>No tools are associated with this user account!</td>
+                    </tr>
+                  }
+                  
+                </>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      );
     }
 }
