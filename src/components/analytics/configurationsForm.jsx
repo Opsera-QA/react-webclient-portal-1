6ac3f3c59a92715cde405657cb90f506f08f57ae
Todd Barczak <toddbarczak@Todds-MacBook-Pro-2.local>
2020-02-07 08:23:49 -0500

    Enhance UI for Activating Analytics

--- src/components/analytics/configurationsForm.jsx
+++ src/components/analytics/configurationsForm.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import { Card, Form, Button } from "react-bootstrap";
 import LoadingDialog from "../common/loading";
 import ErrorDialog from "../common/error";
-import InfoDialog from "../common/info";
+//import InfoDialog from "../common/info";
 import Modal from "../common/modal";
 import { ApiService } from "../../api/apiService";
 import Moment from "react-moment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave, faTools } from "@fortawesome/free-solid-svg-icons";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 const INITIAL_SETTINGS = {
   dataUsage: "500",
@@ -62,7 +62,7 @@ function ConfigurationsForm( { settings, token }) {
   }
 
   function postData() {
-    setState({ loaded: false });
+    setState({ loaded: false, error: null });
     const body = state.data;
     const apiCall = new ApiService("/analytics/update", {}, token, body);  
     apiCall.post()
@@ -100,14 +100,13 @@ function ConfigurationsForm( { settings, token }) {
   }
 
 
-  const { data, loaded, messages, showModal, editEnabled, error } = state;
+  const { data, loaded, messages, showModal, error } = state;
   const { enabledTools, disabledToolsOn, active, enabledToolsOn } = data;
   return (
     <div>
       {!loaded && <LoadingDialog />}
       { error && <ErrorDialog error={error} /> }
-      {/* {!active && <InfoDialog message="Analytics are currently NOT enabled for this account." />} */}
-
+      
       {showModal ? <Modal header="Confirm Deactivation" 
         message={messages} 
         button="Confirm"  
@@ -115,83 +114,88 @@ function ConfigurationsForm( { settings, token }) {
 
       <Card>
         <Card.Body>
-          <Card.Title>Configuration Profile</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Analytics configuration details are shown below for available tools.  We offer analytics for Infrastructure and Pipeline workflows as well as many popular tools.</Card.Subtitle>
+          <Card.Title>{ !active ? "Getting Started" : "Configuration Profile" }</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">
+            { !active ? "Welcome to the OpsERA Analytics Portal!  Here you can enable analytics for our supported tools and dashboards and metrics around your system's activities.  Simply click the \
+              Activate Analytics button below and then begin configuring your dashboards!" : 
+              "Analytics configuration details are shown below for available tools.  We offer analytics for Infrastructure and Pipeline workflows as well as many popular tools."}</Card.Subtitle>
           
           <Form>
-            <fieldset>
-              <div className="mt-3 text-muted">Workflow:</div>
-              <div key="inline-checkbox-workflow" className="mb-1 mt-2 p-2">
-                {
-                  ANALYTICS_TYPES.map(item => (
-                    <Form.Check inline 
-                      type="checkbox" 
-                      label={item}
-                      id={`TYPE-${item}`}
-                      checked={data.workflowType ? data.workflowType[item] : false}
-                      onChange={handleCheckBoxChange.bind(this, item)}
-                      key={item} />
-                  ))
-                }
-              </div>
+            { !active ? 
+              <>
+                <div className="mt-4">
+                  <Button variant="primary" onClick={() => { updateProfile(); }}>Activate Analytics</Button>
+                </div>
+              </> :
+              <fieldset>
+                <div className="mt-3 text-muted">Workflow:</div>
+                <div key="inline-checkbox-workflow" className="mb-1 mt-2 p-2">
+                  {
+                    ANALYTICS_TYPES.map(item => (
+                      <Form.Check inline 
+                        type="checkbox" 
+                        label={item}
+                        id={`TYPE-${item}`}
+                        checked={data.workflowType ? data.workflowType[item] : false}
+                        onChange={handleCheckBoxChange.bind(this, item)}
+                        key={item} />
+                    ))
+                  }
+                </div>
 
-              <div className="mt-3 text-muted">Daily Data Limit:</div>
-              <div key="radio-data-limit" className="mt-1 p-2">
-                {
-                  DATA_LIMITS.map(item => (
-                    <Form.Check inline label={item.Label} 
-                      checked={state.data.dataUsage.toString() === item.Value.toString()} 
-                      onChange={handleOptionChange}
-                      value={item.Value.toString()} 
-                      type="radio" 
-                      id={`STORAGE-${item.Value}`}
-                      key={item.Value} />
-                  ))
-                }
-              </div>
+                <div className="mt-3 text-muted">Daily Data Limit:</div>
+                <div key="radio-data-limit" className="mt-1 p-2">
+                  {
+                    DATA_LIMITS.map(item => (
+                      <Form.Check inline label={item.Label} 
+                        checked={state.data.dataUsage.toString() === item.Value.toString()} 
+                        onChange={handleOptionChange}
+                        value={item.Value.toString()} 
+                        type="radio" 
+                        id={`STORAGE-${item.Value}`}
+                        key={item.Value} />
+                    ))
+                  }
+                </div>
 
-              <div className="mt-3 text-muted">Tools:
-                { (enabledToolsOn && !disabledToolsOn) && 
+                <div className="mt-3 text-muted">Tools:
+                  { (enabledToolsOn && !disabledToolsOn) && 
                 <>
                   <span className="italic" style={{ fontSize: "smaller", paddingLeft:"10px" }}>(Enabled on&nbsp; 
                     <Moment format="MM/DD/YYYY" date={enabledToolsOn} />)
                   </span> 
                 </>
-                }
-              </div>
-              <div key="checkbox-tools" className="mb-1 mt-2 p-2 d-none">
-                {
-                  TOOLS.map(item => (
-                    <Form.Check inline 
-                      type="checkbox" 
-                      label={item}
-                      id={`TOOL-${item}`}
-                      checked={enabledTools ? enabledTools[item] : false}
-                      onChange={handleCheckBoxChange.bind(this, item)}
-                      key={item} />
-                  ))
-                }
-              </div>
+                  }
+                </div>
+                <div key="checkbox-tools" className="mb-1 mt-2 p-2 d-none">
+                  {
+                    TOOLS.map(item => (
+                      <Form.Check inline 
+                        type="checkbox" 
+                        label={item}
+                        id={`TOOL-${item}`}
+                        checked={enabledTools ? enabledTools[item] : false}
+                        onChange={handleCheckBoxChange.bind(this, item)}
+                        key={item} />
+                    ))
+                  }
+                </div>
 
-              <div className="mb-3 mt-1 text-muted">
-                <div>The following tools are currently supported with analytics:</div>
+                <div className="mb-3 mt-1 text-muted">
+                  <div>The following tools are currently supported with analytics:</div>
                 
-                {
-                  TOOLS.map(item => (
-                    <span key={item} style={{ marginRight:"5px" }}>&nbsp;{item}&nbsp;</span>
-                  ))}
-              </div>
+                  {
+                    TOOLS.map(item => (
+                      <span key={item} style={{ marginRight:"5px" }}>&nbsp;{item}&nbsp;</span>
+                    ))}
+                </div>
 
-              <div className="text-right">
-                { !active ? 
-                  <Button variant="primary" onClick={() => { updateProfile(); }}>Activate Analytics</Button> : 
-                  <>
-                    <Button variant="primary" className="mr-3" onClick={() => { updateProfile(); }}><FontAwesomeIcon icon={faSave} fixedWidth /> Save Settings</Button>
-                    <Button variant="outline-danger" onClick={() => { confirmDeactivation(); }}>Deactivate Analytics</Button>
-                  </> }
-              </div> 
+                <div className="text-right">
+                  <Button variant="primary" className="mr-3" onClick={() => { updateProfile(); }}><FontAwesomeIcon icon={faSave} fixedWidth /> Save Settings</Button>
+                  <Button variant="outline-danger" onClick={() => { confirmDeactivation(); }}>Deactivate Analytics</Button>
+                </div> 
                 
-            </fieldset>
+              </fieldset>}
 
           </Form>          
         </Card.Body>
