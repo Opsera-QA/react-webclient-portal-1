e4f41c5027dcbff217d8a1cf79ab773e3cf740a3
Todd Barczak <toddbarczak@Todds-MacBook-Pro-2.local>
2020-02-14 12:24:16 -0500

    Wire up API call for build counts (Functional Approach)

--- src/components/dashboard/buildCounts.jsx
+++ src/components/dashboard/buildCounts.jsx
@@ -1,92 +1,97 @@
 /* eslint-disable no-unused-vars */
-import React, { useReducer, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 //import { CardGroup, Card, Button } from "react-bootstrap";
 import { AuthContext } from "../../contexts/AuthContext";
 import { ApiService } from "../../api/apiService";
-//import LoadingDialog from "../common/loading";
+import LoadingDialog from "../common/loading";
 //import ErrorDialog from "../common/error";
 
-function BuildCounts({ settings }) {
+function BuildCounts() {
   const contextType = useContext(AuthContext);
-  const [state, setState] = useReducer(
-    (state, newState) => ({ ...state, ...newState }),
-    { message: null, detail: null, statusCode: null }
-  );
   
+  const [hasError, setErrors] = useState(false);
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState({});
 
   const getApiData = async () => {
+    setLoading(true);
     const { getAccessToken } = contextType;
     const accessToken = await getAccessToken();
-    const apiCall = new ApiService("/users/get-users", {}, accessToken);
-    let currentComponent = this;
+    const apiCall = new ApiService("/analytics/dashboard", {}, accessToken);
+    
     apiCall.get()
-      .then(function (response) {
-        // console.log(response.data)
-        currentComponent.setState({
-          data: response.data,
-          error: null,
-          fetching: false
-        });
+      .then(res => {
+        setData(res.data.data);
+        setLoading(false);
       })
-      .catch(function (error) {
-        currentComponent.setState({
-          error: error,
-          fetching: false
-        });
+      .catch(err => {
+        setErrors(err);
+        setLoading(false);
       });
   };
 
   useEffect( () => {
-    if (settings) {
-      if ("active" in settings) {
-        setState({ data: settings });  
-      } 
-    }
-  }, [settings]);
+    getApiData();
+  }, []);
 
-  return (
-    <div>
-      <div className="element-box p-3 text-center">
-        <blockquote className="blockquote mb-0 ">
-          <div className="box-metric">125</div>
-          <footer className="blockquote">
-            <small className="text-muted">
+  if(loading) {
+    return (<LoadingDialog size="sm" />);
+  } 
+  else {
+    return (
+      <div>
+        <div className="element-box p-3 text-center">
+          <blockquote className="blockquote mb-0 ">
+            <div className="box-metric">{data.successfulBuilds.data[0].key}</div>
+            <footer className="blockquote">
+              <small className="text-muted">
               Successful Builds
-            </small>
-          </footer>
-        </blockquote>
-      </div>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
       
-      <div className="element-box p-3 mt-3 text-center">
-        <blockquote className="blockquote mb-0 ">
-          <div className="box-metric">12</div>
-          <footer className="blockquote">
-            <small className="text-muted">
+        <div className="element-box p-3 mt-3 text-center">
+          <blockquote className="blockquote mb-0 ">
+            <div className="box-metric">{data.failedBuilds.data[0].key}</div>
+            <footer className="blockquote">
+              <small className="text-muted">
             Failed Builds
-            </small>
-          </footer>
-        </blockquote>
-      </div>
+              </small>
+            </footer>
+          </blockquote>
+        </div>
 
-      <div className="element-box p-3 mt-3 text-center">
-        <blockquote className="blockquote mb-0 ">
-          <div className="box-metric">4</div>
-          <footer className="blockquote">
-            <small className="text-muted">
-            Active Pipelines
-            </small>
-          </footer>
-        </blockquote>
-      </div>
-    </div>
-  );
+        <div className="element-box p-3 mt-3 text-center">
+          <blockquote className="blockquote mb-0 ">
+            <div className="box-metric">{data.successfulDeployments.data[0].key}</div>
+            <footer className="blockquote">
+              <small className="text-muted">
+            Successful Deployments
+              </small>
+            </footer>
+          </blockquote>
+        </div>
 
+        <div className="element-box p-3 mt-3 text-center">
+          <blockquote className="blockquote mb-0 ">
+            <div className="box-metric">{data.failedDeployments.data[0].key}</div>
+            <footer className="blockquote">
+              <small className="text-muted">
+            Failed Deployments
+              </small>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    );
+  }
 }
 
-BuildCounts.propTypes = {
+/* BuildCounts.propTypes = {
   settings: PropTypes.object
 };
-
+ */
 
 export default BuildCounts;
