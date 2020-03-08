d3daf5ced1f8a1f5fa3dbeb23fc5ef6292421c27
“Todd <todd@opsera.io>
2020-03-08 17:10:32 -0400

    Fix spacing on detail grid

--- src/components/workflow/pipelineDetail.jsx
+++ src/components/workflow/pipelineDetail.jsx
@@ -88,25 +88,25 @@ const ItemSummaryDetail = (props) => {
               {data.description}
               
             </Card.Text>
-            <Row className="mt-3">
-              <Col lg><span className="text-muted mr-1">ID:</span> {data._id}</Col>
-              <Col lg><span className="text-muted mr-1">Owner:</span> {data.owner}</Col>                
-            </Row>
             <Row className="mt-2">
-              <Col lg><span className="text-muted mr-1">Organization:</span> {data.organizationName}</Col>
-              <Col lg><span className="text-muted mr-1">Created On:</span>  <Moment format="MMM Do YYYY, h:mm:ss a" date={data.createdAt} /></Col>
+              <Col lg className="py-1"><span className="text-muted mr-1">ID:</span> {data._id}</Col>
+              <Col lg className="py-1"><span className="text-muted mr-1">Owner:</span> {data.owner}</Col>                
             </Row>
-            <Row className="mt-2">
-              <Col><span className="text-muted mr-1">Tags:</span> 
+            <Row>
+              <Col lg className="py-1"><span className="text-muted mr-1">Organization:</span> {data.organizationName}</Col>
+              <Col lg className="py-1"><span className="text-muted mr-1">Created On:</span>  <Moment format="MMM Do YYYY, h:mm:ss a" date={data.createdAt} /></Col>
+            </Row>
+            <Row>
+              <Col className="py-1"><span className="text-muted mr-1">Tags:</span> 
                 {data.tags.map((item, idx) => (<span key={idx}>{item}, </span>))}</Col>
             </Row>
-            <Row className="mt-3">
-              <Col md><span className="text-muted mr-1">Source:</span> <span className="upper-case-first">{data.workflow.source.name}</span></Col>
-              <Col md><span className="text-muted mr-1">Repository:</span> {data.workflow.source.repository}</Col>
-              <Col md><span className="text-muted mr-1">Branch:</span> {data.workflow.source.branch}</Col>
+            <Row>
+              <Col md className="py-1"><span className="text-muted mr-1">Source:</span> <span className="upper-case-first">{data.workflow.source.name}</span></Col>
+              <Col md className="py-1"><span className="text-muted mr-1">Repository:</span> {data.workflow.source.repository}</Col>
+              <Col md className="py-1"><span className="text-muted mr-1">Branch:</span> {data.workflow.source.branch}</Col>
             </Row>
-            <Row className="mt-3">
-              <Col><span className="text-muted mr-1">Tools:</span> 
+            <Row>
+              <Col className="py-1"><span className="text-muted mr-1">Tools:</span> 
                 {data.workflow.plan.map((item, idx) => (<span key={idx} className="upper-case-first mr-1">{item.tool.name}, </span>))}</Col>
             </Row>
           </Card.Body>
