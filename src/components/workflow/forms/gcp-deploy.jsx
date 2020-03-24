b501f389f37f3a08d250ad79d54bf735b57f4cab
faseeh <faseeh@collaaj.com>
2020-03-24 16:34:22 +0530

    OC-149 : Added forms for jmeter selenium twistlock and cloud deploy

---
+++ src/components/workflow/forms/gcp-deploy.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Form, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+
+
+//This must match the form below and the data object expected.  Each tools' data object is different
+const INITIAL_DATA = {
+  buildScript: ""
+};
+
+
+//data is JUST the tool object passed from parent component, that's returned through parent Callback
+// ONLY allow changing of the configuration and threshold properties of "tool"!
+function GcpDeployStepConfiguration( { data, parentCallback }) {
+  const [thresholdVal, setThresholdValue] = useState("");
+  const [thresholdType, setThresholdType] = useState("");
+  const [formData, setFormData] = useState(INITIAL_DATA);
+  const [formMessage, setFormMessage] = useState("");
+
+  useEffect(() => {
+    if (typeof(data) !== "undefined") {
+      let { configuration, threshold } = data;
+      if (typeof(configuration) !== "undefined") {
+        setFormData(configuration);
+      }
+      if (typeof(threshold) !== "undefined") {
+        setThresholdType(threshold.type);
+        setThresholdValue(threshold.value);
+      }
+    } else {
+      setFormData(INITIAL_DATA);
+    }
+  }, [data]);
+
+
+  const callbackFunction = () => {
+    if (validateRequiredFields()) {
+      const item = {
+        configuration: formData,
+        threshold: {
+          type: thresholdType,
+          value: thresholdVal
+        }
+      };
+      parentCallback(item);
+    }
+  };
+
+
+  const validateRequiredFields = () => {
+    let { buildScript } = formData;
+    if (buildScript.length === 0) {
+      setFormMessage("Required Fields Missing!");
+      return false;
+    } else {
+      setFormMessage("");
+      return true;
+    }
+  };
+  
+  return (
+    <Form>
+      { formMessage.length > 0 ? <p className="text-danger">{formMessage}</p> : null}
+
+      <Form.Group controlId="repoField">
+        <Form.Label>Enter build script content*</Form.Label>
+        <Form.Control as="textarea" type="text" placeholder="" value={formData.buildScript || ""} onChange={e => setFormData({ ...formData, buildScript: e.target.value })} />
+      </Form.Group>
+     
+      <small className="form-text text-muted mt-2 text-left pb-2">A platform-specific script, which will be executed as .cmd file on Windows or as a shellscript in Unix-like environments</small>
+     
+      {/* Leave the threshold form group as is for now, just read only for all forms */}
+      <Form.Group controlId="threshold">
+        <Form.Label>Step Success Threshold</Form.Label>
+        <Form.Control type="text" placeholder="" value={thresholdVal} onChange={e => setThresholdValue(e.target.value)} disabled={true} />
+      </Form.Group>
+      
+      <Button variant="primary" type="button" 
+        onClick={() => { callbackFunction(); }}> 
+        <FontAwesomeIcon icon={faSave} className="mr-1"/> Save
+      </Button>
+      <small className="form-text text-muted mt-2 text-right">* Required Fields</small>
+    </Form>
+  );
+}
+
+GcpDeployStepConfiguration.propTypes = {
+  data: PropTypes.object,
+  parentCallback: PropTypes.func
+};
+
+export default GcpDeployStepConfiguration;
--- src/components/workflow/forms/stepToolConfiguration.jsx
+++ src/components/workflow/forms/stepToolConfiguration.jsx
@@ -8,6 +8,11 @@ import SonarStepConfiguration from "./sonar";
 import NPMStepConfiguration from "./npm";
 import CommandLineStepConfiguration from "./commandLine";
 import TeamCityStepConfiguration from "./teamCity";
+import GcpDeployStepConfiguration from "./gcp-deploy";
+import AwsDeployStepConfiguration from "./aws-deploy";
+import JmeterStepConfiguration from "./jmeter";
+import SeleniumStepConfiguration from "./selenium";
+import TwistlockStepConfiguration from "./twistlock";
 
 
 
@@ -49,6 +54,11 @@ function StepToolConfiguration( { data, editItem, parentCallback }) {
           {editItem.tool_name.toLowerCase() === "command line" ? <CommandLineStepConfiguration data={stepTool} parentCallback={callbackFunction} /> : null }
           {editItem.tool_name.toLowerCase() === "npm" ? <NPMStepConfiguration data={stepTool} parentCallback={callbackFunction} /> : null }
           {editItem.tool_name.toLowerCase() === "teamcity" ? <TeamCityStepConfiguration data={stepTool} parentCallback={callbackFunction} /> : null }
+          {editItem.tool_name.toLowerCase() === "jmeter" ?  <JmeterStepConfiguration data={stepTool} parentCallback={callbackFunction} />: null }
+          {editItem.tool_name.toLowerCase() === "selenium" ? <SeleniumStepConfiguration data={stepTool} parentCallback={callbackFunction} /> : null }
+          {editItem.tool_name.toLowerCase() === "twistlock" ? <TwistlockStepConfiguration data={stepTool} parentCallback={callbackFunction} />: null }
+          {editItem.tool_name.toLowerCase() === "aws-deploy" ? <AwsDeployStepConfiguration  data={stepTool} parentCallback={callbackFunction} /> : null }
+          {editItem.tool_name.toLowerCase() === "gcp-deploy" ? <GcpDeployStepConfiguration data={stepTool} parentCallback={callbackFunction} /> : null }
         </div>
         : null }
 
