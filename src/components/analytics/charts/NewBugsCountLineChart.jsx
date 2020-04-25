3413919b0022938aec74b413f34ae864611c8981
Syed Faseeh Uddin <faseeh@opsera.io>
2020-04-25 17:04:39 +0000

    changed analytics page development cahrt layouts and minor fixes for reliability charts error handling-also added comments for charts

--- src/components/analytics/charts/NewBugsCountLineChart.jsx
+++ src/components/analytics/charts/NewBugsCountLineChart.jsx
@@ -1,66 +1,123 @@
-import React from "react";
+// Ticket Number - AN 47 Reliability
+// Worked on By - Syed Faseeh Uddin
+// Sprint - Analytics Mt. Rainier
+// Persona - Developer
+
 import PropTypes from "prop-types";
 import { ResponsiveLine } from "@nivo/line";
 import ErrorDialog from "../../common/error";
 import config from "./NewBugsCountLineChartConfigs";
 import "./charts.css";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { axiosApiService } from "../../../api/apiService";
+import LoadingDialog from "../../common/loading";
 
+function NewBugsCountLineChart( { persona } ) {
+  const contextType = useContext(AuthContext);
+  const [error, setErrors] = useState(false);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-function NewBugsCountLineChart( { data, persona } ) {
+  useEffect(() => {    
+    const controller = new AbortController();
+    const runEffect = async () => {
+      try {
+        console.log("FETCHING DATA");
+        await getApiData();        
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("Request was canceled via controller.abort");
+          return;
+        }        
+      }
+    };
+    runEffect();
 
-  if (typeof data !== "object" || Object.keys(data).length == 0) {
-    return (<ErrorDialog  error="No Data Present in the ES!" />);
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  const getApiData = async () => {
+    
+    setLoading(true);
+    const { getAccessToken } = contextType;
+    const accessToken = await getAccessToken();
+    const apiUrl = "/analytics/data";   
+    const postBody = {
+      data: [
+        { 
+          request: "newBugs",
+          metric: "line" 
+        }
+      ]
+    };
+
+    try {
+      const res = await axiosApiService(accessToken).post(apiUrl, postBody);
+      let dataObject = res && res.data ? res.data.data[0].newBugs : [];
+      setData(dataObject);
+      setLoading(false);
+    }
+    catch (err) {
+      console.log(err.message);
+      setLoading(false);
+      setErrors(err.message);
+    }
+  };
+
+  if(loading) {
+    return (<LoadingDialog size="sm" />);
+  } else if (error) {
+    return (<ErrorDialog  error={error} />);
+  } else if (typeof data !== "object" || Object.keys(data).length == 0 || data.status !== 200) {
+    return (<ErrorDialog error="No Data is available for this chart at this time." />);
   } else {
-    const { new_bugs }  =  data;
     return (
       <>
-        <div className="chart mb-3" style={{ height: "300px" }}>
-          <div className="chart-label-text">Sonar: New Bugs Count</div>
-          {/* additional check */}
-          {new_bugs.status != "200" ? <><br></br><ErrorDialog  error="Something went wrong!" /></> :
-            <ResponsiveLine
-              data={new_bugs && typeof(new_bugs.data) === "object" ? new_bugs.data : []}
-              margin={{ top: 40, right: 110, bottom: 70, left: 100 }}
-              xScale={{ type: "point" }}
-              yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
-              axisTop={null}
-              axisRight={null}
-              axisBottom={config.axisBottom}
-              axisLeft={config.axisLeft}
-              pointSize={10}
-              pointBorderWidth={8}
-              pointLabel="y"
-              pointLabelYOffset={-12}
-              useMesh={true}
-              lineWidth={3.5}
-              colors={{ scheme: "category10" }}
-              tooltip={({ point, color }) => (
-                <div style={{
-                  background: "white",
-                  padding: "9px 12px",
-                  border: "1px solid #ccc",
-                }}>
-                  <strong style={{ color }}>
+        <div className="chart-label-text">Sonar: New Bugs Count</div>
+        <ResponsiveLine
+          data={data ? data.data : []}
+          margin={{ top: 40, right: 110, bottom: 70, left: 100 }}
+          xScale={{ type: "point" }}
+          yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={config.axisBottom}
+          axisLeft={config.axisLeft}
+          pointSize={10}
+          pointBorderWidth={8}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          useMesh={true}
+          lineWidth={3.5}
+          colors={{ scheme: "category10" }}
+          tooltip={({ point, color }) => (
+            <div style={{
+              background: "white",
+              padding: "9px 12px",
+              border: "1px solid #ccc",
+            }}>
+              <strong style={{ color }}>
             Build Number: </strong> {point.data.x}<br></br>
-                  <strong style={{ color }}>  Bugs: </strong> {point.data.y}<br></br>
-                  <strong style={{ color }}>  Revision: </strong> {point.data.revision}
-                </div>
-              )}
-              theme={{
-                tooltip: {
-                  container: {
-                    fontSize: "16px",
-                  },
-                },
-              }}
-            />
-          }</div>
+              <strong style={{ color }}>  Bugs: </strong> {point.data.y}<br></br>
+              <strong style={{ color }}>  Revision: </strong> {point.data.revision}
+            </div>
+          )}
+          theme={{
+            tooltip: {
+              container: {
+                fontSize: "16px",
+              },
+            },
+          }}
+        />
       </>
     );
   }
 }
 NewBugsCountLineChart.propTypes = {
-  data: PropTypes.object,
   persona: PropTypes.string
 };
 
--- src/components/analytics/charts/ReliabilityRatingLineChart.jsx
+++ src/components/analytics/charts/ReliabilityRatingLineChart.jsx
@@ -1,66 +1,122 @@
-import React from "react";
+// Ticket Number - AN 47 Reliability
+// Worked on By - Syed Faseeh Uddin
+// Sprint - Analytics Mt. Rainier
+// Persona - All
+
 import PropTypes from "prop-types";
 import { ResponsiveLine } from "@nivo/line";
 import ErrorDialog from "../../common/error";
 import config from "./ReliabilityRatingLineChartConfigs";
 import "./charts.css";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { axiosApiService } from "../../../api/apiService";
+import LoadingDialog from "../../common/loading";
 
+function ReliabilityRatingLineChart( { persona } ) {
+  const contextType = useContext(AuthContext);
+  const [error, setErrors] = useState(false);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-function ReliabilityRatingLineChart( { data, persona } ) {
+  useEffect(() => {    
+    const controller = new AbortController();
+    const runEffect = async () => {
+      try {
+        console.log("FETCHING DATA");
+        await getApiData();        
+      } catch (err) {
+        if (err.name === "AbortError") {
+          console.log("Request was canceled via controller.abort");
+          return;
+        }        
+      }
+    };
+    runEffect();
 
-  if (typeof data !== "object" || Object.keys(data).length == 0) {
-    return (<ErrorDialog  error="No Data Present in the ES!" />);
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  const getApiData = async () => {
+    setLoading(true);
+    const { getAccessToken } = contextType;
+    const accessToken = await getAccessToken();
+    const apiUrl = "/analytics/data";   
+    const postBody = {
+      data: [
+        { 
+          request: "reliabilityRating",
+          metric: "line" 
+        }
+      ]
+    };
+
+    try {
+      const res = await axiosApiService(accessToken).post(apiUrl, postBody);
+      let dataObject = res && res.data ? res.data.data[0].reliabilityRating : [];
+      setData(dataObject);
+      setLoading(false);
+    }
+    catch (err) {
+      console.log(err.message);
+      setLoading(false);
+      setErrors(err.message);
+    }
+  };
+  
+  if(loading) {
+    return (<LoadingDialog size="sm" />);
+  } else if (error) {
+    return (<ErrorDialog  error={error} />);
+  } else if (typeof data !== "object" || Object.keys(data).length == 0 || data.status !== 200) {
+    return (<ErrorDialog error="No Data is available for this chart at this time." />);
   } else {
-    const { reliability_rating }  =  data;
     return (
       <>
-        <div className="chart mb-3" style={{ height: "300px" }}>
-          <div className="chart-label-text">Sonar: Reliability Rating</div>
-          {/* additional check */}
-          {reliability_rating.status != "200" ? <><br></br><ErrorDialog  error="Something went wrong!" /></> :
-            <ResponsiveLine
-              data={reliability_rating && typeof(reliability_rating.data) === "object" ? reliability_rating.data : []}
-              margin={{ top: 40, right: 110, bottom: 70, left: 100 }}
-              xScale={{ type: "point" }}
-              yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
-              axisTop={null}
-              axisRight={null}
-              axisBottom={config.axisBottom}
-              axisLeft={config.axisLeft}
-              pointSize={10}
-              pointBorderWidth={8}
-              pointLabel="y"
-              pointLabelYOffset={-12}
-              useMesh={true}
-              lineWidth={3.5}
-              colors={{ scheme: "category10" }}
-              tooltip={({ point, color }) => (
-                <div style={{
-                  background: "white",
-                  padding: "9px 12px",
-                  border: "1px solid #ccc",
-                }}>
-                  <strong style={{ color }}>
+        <div className="chart-label-text">Sonar: Reliability Rating</div>
+        <ResponsiveLine
+          data={data ? data.data : []}
+          margin={{ top: 40, right: 110, bottom: 70, left: 100 }}
+          xScale={{ type: "point" }}
+          yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={config.axisBottom}
+          axisLeft={config.axisLeft}
+          pointSize={10}
+          pointBorderWidth={8}
+          pointLabel="y"
+          pointLabelYOffset={-12}
+          useMesh={true}
+          lineWidth={3.5}
+          colors={{ scheme: "category10" }}
+          tooltip={({ point, color }) => (
+            <div style={{
+              background: "white",
+              padding: "9px 12px",
+              border: "1px solid #ccc",
+            }}>
+              <strong style={{ color }}>
               Date: </strong> {new Date(point.data.x).toLocaleString()}<br></br>
-                  <strong style={{ color }}>  Rating: </strong> {point.data.y === 1 && <>A</>} {point.data.y === 2 && <>B</>} {point.data.y === 3 && <>C</>}  {point.data.y === 4 && <>D</>}  {point.data.y === 5 && <>E</>} <br></br>
-                  <strong style={{ color }}>  Build Number: </strong> {point.data.buildNumber}
-                </div>
-              )}
-              theme={{
-                tooltip: {
-                  container: {
-                    fontSize: "16px",
-                  },
-                },
-              }}
-            />
-          }</div>
+              <strong style={{ color }}>  Rating: </strong> {point.data.y === 1 && <>A</>} {point.data.y === 2 && <>B</>} {point.data.y === 3 && <>C</>}  {point.data.y === 4 && <>D</>}  {point.data.y === 5 && <>E</>} <br></br>
+              <strong style={{ color }}>  Build Number: </strong> {point.data.buildNumber}
+            </div>
+          )}
+          theme={{
+            tooltip: {
+              container: {
+                fontSize: "16px",
+              },
+            },
+          }}
+        />
       </>
     );
   }
 }
 ReliabilityRatingLineChart.propTypes = {
-  data: PropTypes.object,
   persona: PropTypes.string
 };
 
