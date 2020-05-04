de97a89c8efa0375a4cd5001a155e2b337360982
Mahantha <mahanta@opsera.io>
2020-05-05 00:52:46 +0530

    AN-48 sonar graphs

--- src/components/analytics/charts/sonarSecurityLineChart.jsx
+++ src/components/analytics/charts/sonarSecurityLineChart.jsx
@@ -1,16 +1,21 @@
 // Analytics Software Development Tab, Developer, Node Ticket AN-153
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-import {AuthContext} from "../../../contexts/AuthContext";
-import {ResponsiveLine} from "@nivo/line";
-import {axiosApiService} from "../../../api/apiService";
+import { AuthContext } from "../../../contexts/AuthContext";
+import { ResponsiveLine } from "@nivo/line";
+import { axiosApiService } from "../../../api/apiService";
 import LoadingDialog from "../../common/loading";
 import ErrorDialog from "../../common/error";
 import config from "./deploymentFrequencyLineChartConfigs";
 import "./charts.css";
 
-
-function SonarSecurityLineChart({persona}) {
+/**
+ *
+ * @param {sonarMeasure} param0
+ * allowed sonarMeasure values
+ * bugs, classes, code_smells, cognitive_complexity, comment_lines, complexity, confirmed_issues, coverage, duplicated_blocks
+ */
+function SonarSecurityLineChart({ persona, sonarMeasure }) {
   const contextType = useContext(AuthContext);
   const [error, setErrors] = useState(false);
   const [data, setData] = useState([]);
@@ -35,10 +40,9 @@ function SonarSecurityLineChart({persona}) {
     };
   }, []);
 
-
   const fetchData = async () => {
     setLoading(true);
-    const {getAccessToken} = contextType;
+    const { getAccessToken } = contextType;
     const accessToken = await getAccessToken();
     const apiUrl = "/analytics/data";
     const postBody = {
@@ -46,7 +50,7 @@ function SonarSecurityLineChart({persona}) {
         {
           request: "sonarMeasures",
           metric: "line",
-          measure: "vulnerabilities"
+          measure: sonarMeasure
 
         }
       ]
@@ -54,7 +58,7 @@ function SonarSecurityLineChart({persona}) {
 
     try {
       const res = await axiosApiService(accessToken).post(apiUrl, postBody);
-      let dataObject = res && res.data ? res.data.data[0].sonarVulnerabilities : [];
+      let dataObject = res && res.data ? res.data.data[0].sonarMeasures : [];
       setData(dataObject);
       setLoading(false);
     }
@@ -71,16 +75,17 @@ function SonarSecurityLineChart({persona}) {
   } else if (error) {
     return (<ErrorDialog error={error} />);
   } else if (typeof data !== "object" || Object.keys(data).length == 0 || data.status !== 200) {
+    console.log(data);
     return (<ErrorDialog error="No Data is available for this chart at this time." />);
   } else {
     console.log(data.data);
     return (
       <>
-        <div className="chart mb-3" style={{height: "300px"}}>
+        <div className="chart mb-3" style={{ height: "300px" }}>
           <div className="chart-label-text">Sonar: Security</div>
           <ResponsiveLine
             data={data ? data.data : []}
-            margin={{top: 40, right: 110, bottom: 70, left: 100}}
+            margin={{ top: 40, right: 110, bottom: 70, left: 100 }}
 
 
             axisLeft={{
@@ -110,9 +115,9 @@ function SonarSecurityLineChart({persona}) {
                 border: "1px solid #ccc",
               }}>
                 <div>
-                  <strong> revision: </strong>  {node.point.data.info._source.revision} <br />
-                  <strong> qualityGate: </strong>  {node.point.data.info._source.qualityGate.name} <br />
-                  <strong> qualityGate: </strong> {node.point.data.info._source.qualityGate.status} <br />
+                  <strong> Revision: </strong>  {node.point.data.info._source.revision} <br />
+                  <strong> Quality Gate: </strong>  {node.point.data.info._source.qualityGate.name} <br />
+                  <strong> Quality Gate: </strong> {node.point.data.info._source.qualityGate.status} <br />
                   <strong> qualifier: </strong>  {node.point.data.info._source.sonarqube_measures.component.measures.qualifier} <br />
                   <strong> Date: </strong> {node.point.data.xFormatted} <br></br>
                   <strong>  {node.point.serieId}: {node.point.data.yFormatted}  </strong>
@@ -134,7 +139,8 @@ function SonarSecurityLineChart({persona}) {
   }
 }
 SonarSecurityLineChart.propTypes = {
-  persona: PropTypes.string
+  persona: PropTypes.string,
+  sonarMeasure: PropTypes.string
 };
 
 export default SonarSecurityLineChart;
