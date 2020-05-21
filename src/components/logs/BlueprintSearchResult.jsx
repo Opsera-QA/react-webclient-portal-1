4e050f417b43df30f6d9455d48196c196805455b
Nobal <contact@nobal.in>
2020-05-21 15:57:01 +0530

    AN-184: Added pagination support for search logs and added pagination window: Any time show only +/- 4 pages to support large number of pages

---
+++ src/components/logs/BlueprintSearchResult.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Table } from "react-bootstrap";
+import ModalLogs from "components/common/modalLogs";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
+import { format } from "date-fns";
+
+function BlueprintSearchResult({ searchResults }) {
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState({});
+
+  const handleClick = (param) => {
+    setModalMessage(param);
+    setShowModal(true);
+  };
+
+  return (   
+    <>
+      <Table striped bordered hover className="mt-4 table-sm" style={{ fontSize:"small" }}>
+        <thead>
+          <tr>
+            <th style={{ width: "5%" }}>Build</th>
+            <th style={{ width: "5%" }}>Environment</th>
+            <th style={{ width: "20%" }}>Date</th>
+            <th style={{ width: "10%" }}>Tool</th>
+            <th style={{ width: "30%" }}>Message</th>
+            <th style={{ width: "25%" }}>Git Commit ID</th>
+            <th style={{ width: "5%" }}>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchResults.map((item, idx) => (
+            <tr key={idx} >
+              <td>{item["build_number"]}
+                <FontAwesomeIcon icon={faSearchPlus}
+                  className="ml-1"
+                  size="xs"
+                  style={{ cursor: "pointer" }}
+                  onClick= {() => { handleClick(item); }} /></td>
+              <td className="force-text-wrap upper-case-all">{item["Release Environment"]}</td> 
+              <td>{format(new Date(item["time"]), "yyyy-MM-dd', 'hh:mm a")}</td>                
+              <td className="upper-case-first">{item["tool"]}</td>
+              <td className="force-text-wrap">{item["message"]}</td>
+              <td className="force-text-wrap">{item["Git Commit ID"]}</td>
+              <td className="upper-case-all">{item["status"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <ModalLogs header={"Build Number: " + modalMessage.build_number} size="lg" jsonMessage={modalMessage} dataType="bar" show={showModal} setParentVisibility={setShowModal} />
+    </>
+  );
+}
+
+BlueprintSearchResult.propTypes = {
+  searchResults: PropTypes.array
+};
+
+
+export default BlueprintSearchResult;
