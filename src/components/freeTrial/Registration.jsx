366be3c96e64b5fdd00381554cdb035c32189a9e
“Todd <todd@opsera.io>
2020-05-21 10:52:27 -0400

    Create placeholder for free trial registration form

---
+++ src/components/freeTrial/Registration.jsx
@@ -0,0 +1,24 @@
+/* Top level Registration component.  Details on this are still getting flushed out per OC-166 */
+import React, { useEffect, useState } from "react";
+/* import { Link, useParams, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome, faPlus, faList } from "@fortawesome/free-solid-svg-icons";
+ */
+
+function TrialRegistration() {
+  
+
+  return (
+    <>
+      <div className="mt-3 max-content-width">
+       Free Trial Registration Form Coming Soon...
+
+      </div>
+    </>
+  );
+}
+
+
+export default TrialRegistration;
+
+
