60b5713251f54c7805b2b3036515bd537909986f
“Todd <todd@opsera.io>
2020-05-21 12:28:55 -0400

    Minor UI tweaks

--- src/components/tools/Registry.jsx
+++ src/components/tools/Registry.jsx
@@ -24,17 +24,15 @@ function ToolRegistry () {
   };
 
   return (
-    <div className="mt-3 max-content-width">
+    <div className="mt-1 max-content-width">
     
-      <div className="mt-3 max-content-module-width-50">
-        Developing PP-55's Tool configurations interface here as well as an open ended UI form to register tools outside of the OpsERA Platform managed tools. 
-        This will provide one place to track and configure any tools users will want to leverage for Pipeline or Analytics or simply have awareness of for future use.
-      </div>
-      <div className="my-2 max-content-module-width-50 text-right">
-        <Button variant="primary" size="sm" type="button"  
-          onClick={() => { handleActionClick("add"); }}> 
-          <FontAwesomeIcon icon={faPlus} className="mr-1"/> New Entry
-        </Button> 
+      
+      <div className="mt-2 mb-2 max-content-module-width-50 text-right">
+        { selection !== "add" ? 
+          <Button variant="primary" size="sm" type="button"  
+            onClick={() => { handleActionClick("add"); }}> 
+            <FontAwesomeIcon icon={faPlus} className="mr-1"/> New Entry
+          </Button>  : null }
 
         { selection === "add" ? 
           <Button variant="secondary" size="sm" type="button" className="ml-2"
@@ -52,6 +50,12 @@ function ToolRegistry () {
         <div className="mt-1">
           <NewTool />
         </div> : null }
+
+
+      <div className="mt-5 max-content-module-width-50">
+        Developing PP-55's Tool configurations interface here as well as an open ended UI form to register tools outside of the OpsERA Platform managed tools. 
+        This will provide one place to track and configure any tools users will want to leverage for Pipeline or Analytics or simply have awareness of for future use.
+      </div>
 
 
     </div >
