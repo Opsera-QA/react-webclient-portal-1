6666857e16cbc1863bac8d548f10c44f65e8b1cc
Shrey Malhotra <shrey@opsera.io>
2020-06-17 13:07:10 -0700

    Cypess logs search support enhanced

--- src/components/logs/LogSearchResult.jsx
+++ src/components/logs/LogSearchResult.jsx
@@ -29,8 +29,8 @@ function LogSearchResult({ searchResults }) {
                 style={{ cursor: "pointer", alignItems: "flex-end" }}
                 onClick= {() => { handleClick(item); }} />
               <strong className="ml-2">{format(new Date(typeof(item._source["@timestamp"]) !== "undefined" ? item._source["@timestamp"] : null), "yyyy-MM-dd', 'hh:mm a")}</strong>
-              { (item._source.data) ? <strong className="ml-4">Job Name: {typeof(item._source.data.buildVariables) !== "undefined" ? item._source.data.buildVariables.JOB_NAME : "N/A"}</strong> : ""}
-              { (item._source.data) ? <strong className="ml-4">Build Number: {typeof(item._source.data.buildNum) !== "undefined" ? item._source.data.buildNum : "N/A"}</strong> : ""}
+              { (item._source.data) ? <strong className="ml-4">{typeof(item._source.data.buildVariables) !== "undefined" ? "Job Name: " + item._source.data.buildVariables.JOB_NAME : ""}</strong> : ""}
+              { (item._source.data) ? <strong className="ml-4">{typeof(item._source.data.buildNum) !== "undefined" ? "Build Number: " + item._source.data.buildNum : ""}</strong> : ""}
             </div>
             <div className="row ml-2" style={{ lineHeight: 2 }}>
               <Highlight matchClass="react-highlighter-lightgray" search={/".*?":/}>{JSON.stringify(item, null, 2).substring(0, 1000)}</Highlight>
