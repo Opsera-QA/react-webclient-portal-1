ce95ae75f91e8f029dba38e3986297946e2ae2d9
Todd Barczak <todd_barczak@symantec.com>
2020-07-21 06:21:21 -0400

    Add some text

--- src/components/inventory/tools/toolDetails/toolJobs.jsx
+++ src/components/inventory/tools/toolDetails/toolJobs.jsx
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 function ToolJobs(props) {
   return (
     <>
+      <div className="text-muted mx-4 mt-3">Save settings for custom jobs here.  These settings can then be re-used in pipeline steps to create jobs.  In OpsERA, 
+    managed jobs are created at the step level in pipeline.</div>
       {props.toolData.tool_identifier.toLowerCase() === "jenkins" ? <JenkinJobs {...props} /> : null }
       {props.toolData.tool_identifier.toLowerCase() !== "jenkins" ? <div className="text-center p-5 text-muted mt-5">Tool jobs: create / manage coming soon</div>  : null }
     </>
