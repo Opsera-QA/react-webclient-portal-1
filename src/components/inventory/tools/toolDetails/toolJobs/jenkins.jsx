a898dd417dd694c04f8fe0e86ff032d938ebf59f
“Todd <todd@opsera.io>
2020-07-27 16:01:19 -0400

    Update layout, wording of tool job&account UI.

--- src/components/inventory/tools/toolDetails/toolJobs/jenkins.jsx
+++ src/components/inventory/tools/toolDetails/toolJobs/jenkins.jsx
@@ -28,26 +28,8 @@ function JenkinJobs({ toolData }) {
   }, [jobAction]);
 
   return (
-    <div className="pr-4 pl-4">
-      <br />
-      {/* {jobAction === "" && */}
-      {/* <Form className="newToolFormContainer">
-        <Form.Group  controlId="formPlaintextEmail" className="mt-2 vertical-center-cols-in-row">
-          <Form.Label column sm="3">
-            Job Actions
-          </Form.Label>
-          <Col sm="9" className="text-right">
-            <Form.Control as="select" disabled={false} value={jobAction} onChange={e => setJobAction( e.target.value)} placeholder="Select One">
-              <option name="Select One" value="" disabled={true}>Select One</option>
-              <option name="Create Account" value="CREATE_ACCOUNT" >Create Account</option>
-              <option name="Create Job" value="CREATE_JOB">Create Job </option>
-            </Form.Control>
-          </Col>
-        </Form.Group>
-      </Form> */}
-      {/* } */}
-      <br />
-      
+    <div className="px-4 mt-2">
+
       {(jobAction === "CREATE_ACCOUNT") && <> 
         <JenkinsCreateAccount toolId={toolData._id} toolData={toolData} jobAction={jobAction} setJobAction={(action) => setJobAction(action)} />
       </>}
