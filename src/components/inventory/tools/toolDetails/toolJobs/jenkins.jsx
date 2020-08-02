7d7c4a387ec582caf64ebfd6b5b64e9a21afee7e
“Todd <todd@opsera.io>
2020-08-02 13:41:03 -0400

    Fix to react error if data passed into table is undefined

--- src/components/inventory/tools/toolDetails/toolJobs/jenkins.jsx
+++ src/components/inventory/tools/toolDetails/toolJobs/jenkins.jsx
@@ -11,9 +11,9 @@ import PropTypes from "prop-types";
 import "components/inventory/tools/tools.css";
 
 function JenkinJobs({ toolData }) {
-  
-  const [ jobAction, setJobAction ] = useState("JOB_LIST");
-  const [ jobData, setJobData ] = useState({});
+
+  const [jobAction, setJobAction] = useState("JOB_LIST");
+  const [jobData, setJobData] = useState({});
 
   const selectedJobRow = (rowData) => {
     setJobAction("CREATE_JOB");
@@ -21,52 +21,59 @@ function JenkinJobs({ toolData }) {
   };
 
   useEffect(() => {
-    if(jobAction == "") { 
+    if (jobAction == "") {
       setJobAction("JOB_LIST");
-      setJobData({}); 
+      setJobData({});
     }
   }, [jobAction]);
 
   return (
     <div className="px-4 mt-2">
 
-      {(jobAction === "CREATE_ACCOUNT") && <> 
-        <JenkinsCreateAccount toolId={toolData._id} toolData={toolData} jobAction={jobAction} setJobAction={(action) => setJobAction(action)} />
+      {(jobAction === "CREATE_ACCOUNT") && <>
+        <JenkinsCreateAccount toolId={toolData._id} toolData={toolData} jobAction={jobAction}
+                              setJobAction={(action) => setJobAction(action)}/>
       </>}
 
-      {(jobAction === "CREATE_JOB" ) && <> 
-        <JenkinsCreateJob toolId={toolData._id} toolData={toolData} jobAction={jobAction} setJobAction={(action) => setJobAction(action)} jobData={jobData} />
-      </>}      
+      {(jobAction === "CREATE_JOB") && <>
+        <JenkinsCreateJob toolId={toolData._id} toolData={toolData} jobAction={jobAction}
+                          setJobAction={(action) => setJobAction(action)} jobData={jobData}/>
+      </>}
 
-      {(jobAction === "JOB_LIST" || jobAction === "ACCOUNT_LIST" ) && <> <ButtonToolbar className="justify-content-between">
+      {(jobAction === "JOB_LIST" || jobAction === "ACCOUNT_LIST") && <> <ButtonToolbar
+        className="justify-content-between">
         <ButtonGroup>
-          <Button size="sm" className="mr-2" variant={jobAction === "JOB_LIST" ? "primary" : "secondary"} onClick={() => setJobAction("JOB_LIST")}>Jobs</Button>
-          <Button size="sm" className="mr-2" variant={jobAction === "ACCOUNT_LIST" ? "primary" : "secondary"} onClick={() => setJobAction("ACCOUNT_LIST")}>Accounts</Button>
+          <Button size="sm" className="mr-2" variant={jobAction === "JOB_LIST" ? "primary" : "secondary"}
+                  onClick={() => setJobAction("JOB_LIST")}>Jobs</Button>
+          <Button size="sm" className="mr-2" variant={jobAction === "ACCOUNT_LIST" ? "primary" : "secondary"}
+                  onClick={() => setJobAction("ACCOUNT_LIST")}>Accounts</Button>
         </ButtonGroup>
       </ButtonToolbar>
-      </>}  
+      </>}
 
-      {(jobAction === "JOB_LIST" ) && <>
+      {(jobAction === "JOB_LIST") && <>
         <div className="my-1 text-right">
-          <Button variant="primary" size="sm"  
-            onClick={() => setJobAction("CREATE_JOB")}> 
+          <Button variant="primary" size="sm"
+                  onClick={() => setJobAction("CREATE_JOB")}>
             <FontAwesomeIcon icon={faPlus} className="mr-1"/> Create Job
           </Button>
-          <br />
+          <br/>
         </div>
-        <JenkinsJobsTable data={toolData.jobs} selectedRow={rowData => selectedJobRow(rowData)} />
+        {toolData.jobs !== undefined &&
+        <JenkinsJobsTable data={toolData.jobs} selectedRow={rowData => selectedJobRow(rowData)}/>}
       </>}
 
       {(jobAction === "ACCOUNT_LIST") && <>
         <div className="my-1 text-right">
-          <Button variant="primary" size="sm"  
-            onClick={() => setJobAction("CREATE_ACCOUNT")}> 
+          <Button variant="primary" size="sm"
+                  onClick={() => setJobAction("CREATE_ACCOUNT")}>
             <FontAwesomeIcon icon={faPlus} className="mr-1"/> Create Account
           </Button>
-          <br />
+          <br/>
         </div>
-        <JenkinsAccountsTable data={toolData.accounts} />
-      </>}    
+        {toolData.accounts !== undefined &&
+        <JenkinsAccountsTable data={toolData.accounts}/>}
+      </>}
 
     </div>
   );
@@ -74,6 +81,6 @@ function JenkinJobs({ toolData }) {
 
 
 JenkinJobs.propTypes = {
-  toolData: PropTypes.object
+  toolData: PropTypes.object,
 };
 export default JenkinJobs;
