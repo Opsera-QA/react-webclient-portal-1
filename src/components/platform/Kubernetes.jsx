53ef4e9493e10663454f996ab891751eb9e26e19
nobal_opsera <nobal@opsera.io>
2020-08-25 19:50:05 +0530

    added missing services

--- src/components/platform/Kubernetes.jsx
+++ src/components/platform/Kubernetes.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Card } from "react-bootstrap";
 import { NewAppContext } from "./context";
 
+
 function Kubernetes(props) {
 
   const { data, setState } = useContext(NewAppContext);
@@ -11,6 +12,11 @@ function Kubernetes(props) {
     Anchore: tools.includes("Anchore") ? true : false,
     Argocd: tools.includes("Argocd") ? true : false,
     Spinnaker: tools.includes("Spinnaker") ? true : false,
+    MongoDB: tools.includes("MongoDB") ? true : false,
+    Kibana: tools.includes("Kibana") ? true : false,
+    Sonatype: tools.includes("Sonatype") ? true : false,
+    Prometheus: tools.includes("Prometheus") ? true : false,
+    FluxCD: tools.includes("FluxCD") ? true : false,    
   });
 
   useEffect(() => {
@@ -18,6 +24,11 @@ function Kubernetes(props) {
       Anchore: false,
       Argocd: false,
       Spinnaker: false,
+      MongoDB: false,
+      Kibana: false,
+      Sonatype: false,
+      Prometheus: false,
+      FluxCD: false,       
     });
     if(tools.length > 0) {
       tools.map((tool) => {
@@ -62,6 +73,7 @@ function Kubernetes(props) {
         <Card.Title>Kubernetes</Card.Title>
           
         <div>
+
           <div
             className={`newApp__service-logo ${tools.includes("Anchore") ? "newApp__service-logo--alredy-installed" : ""}`}
             onClick={() =>
@@ -83,8 +95,6 @@ function Kubernetes(props) {
             <span className="newApp__service-title">Anchore</span>
           </div>
 
-
-
           <div
             className={`newApp__service-logo ${tools.includes("Argocd") ? "newApp__service-logo--alredy-installed" : ""}`}
             onClick={() =>
@@ -105,8 +115,6 @@ function Kubernetes(props) {
             <img src={require("./imgs/argocd.png")} />
             <span className="newApp__service-title">Argocd</span>
           </div>
-
-
 
           <div
             className={`newApp__service-logo ${tools.includes("Spinnaker") ? "newApp__service-logo--alredy-installed" : ""}`}
@@ -130,6 +138,111 @@ function Kubernetes(props) {
           </div>
         </div>
 
+
+          <div
+            className={`newApp__service-logo ${tools.includes("MongoDB") ? "newApp__service-logo--alredy-installed" : ""}`}
+            onClick={() =>
+              setState({
+                open: !tools.includes("MongoDB"),
+                category: "Monitoring",
+                service: "MongoDB",
+              })
+            }
+          >
+            <input type="checkbox"
+              inline
+              disabled={tools.includes("MongoDB") ? true : false}
+              checked={isChecked.MongoDB}
+              className="newApp__checkbox"
+              onClick={() => selectCard("MongoDB")}
+            />
+            <img src={require("./imgs/mongo.png")} />
+            <span className="newApp__service-title">MongoDB</span>
+          </div>
+
+          <div
+            className={`newApp__service-logo ${tools.includes("Kibana") ? "newApp__service-logo--alredy-installed" : ""}`}
+            onClick={() =>
+              setState({
+                open: !tools.includes("Kibana"),
+                category: "Monitoring",
+                service: "Kibana",
+              })
+            }
+          >
+            <input type="checkbox"
+              inline
+              disabled={tools.includes("Kibana") ? true : false}
+              checked={isChecked.Kibana}
+              className="newApp__checkbox"
+              onClick={() => selectCard("Kibana")}
+            />
+            <img src={require("./imgs/kibana.png")} />
+            <span className="newApp__service-title">Kibana</span>
+          </div>
+
+          <div
+            className={`newApp__service-logo ${tools.includes("Sonatype") ? "newApp__service-logo--alredy-installed" : ""}`}
+            onClick={() =>
+              setState({
+                open: !tools.includes("Sonatype"),
+                category: "Monitoring",
+                service: "Sonatype",
+              })
+            }
+          >
+            <input type="checkbox"
+              inline
+              disabled={tools.includes("Sonatype") ? true : false}
+              checked={isChecked.Anchore}
+              className="newApp__checkbox"
+              onClick={() => selectCard("Sonatype")}
+            />
+            <img src={require("./imgs/sonatype.png")} />
+            <span className="newApp__service-title">Sonatype</span>
+          </div>
+
+          <div
+            className={`newApp__service-logo ${tools.includes("Prometheus") ? "newApp__service-logo--alredy-installed" : ""}`}
+            onClick={() =>
+              setState({
+                open: !tools.includes("Prometheus"),
+                category: "Monitoring",
+                service: "Prometheus",
+              })
+            }
+          >
+            <input type="checkbox"
+              inline
+              disabled={tools.includes("Prometheus") ? true : false}
+              checked={isChecked.Prometheus}
+              className="newApp__checkbox"
+              onClick={() => selectCard("Prometheus")}
+            />
+            <img src={require("./imgs/prometheus.png")} />
+            <span className="newApp__service-title">Prometheus</span>
+          </div>
+
+          <div
+            className={`newApp__service-logo ${tools.includes("FluxCD") ? "newApp__service-logo--alredy-installed" : ""}`}
+            onClick={() =>
+              setState({
+                open: !tools.includes("FluxCD"),
+                category: "Monitoring",
+                service: "FluxCD",
+              })
+            }
+          >
+            <input type="checkbox"
+              inline
+              disabled={tools.includes("FluxCD") ? true : false}
+              checked={isChecked.FluxCD}
+              className="newApp__checkbox"
+              onClick={() => selectCard("FluxCD")}
+            />
+            <img src={require("./imgs/flux.png")} />
+            <span className="newApp__service-title">FluxCD</span>
+          </div>                                                
 
       </Card.Body>
     </Card>
