3992a618db6baf2bc316ee491cfc7bc39ebd13d9
faseeh <faseeh@collaaj.com>
2020-09-02 10:01:42 +0530

    added domain in free trial reg form

--- src/components/free_trial/Registration.jsx
+++ src/components/free_trial/Registration.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card, Form } from "react-bootstrap";
 import { ApiService } from "api/apiService";
 import { useHistory } from "react-router-dom";
@@ -6,7 +6,7 @@ import registrationMetadata from "./freetrial-metadata";
 import DtoTextInput from "../common/input/dto_input/dto-text-input";
 import Model from "../../core/data_model/model";
 import LoadingDialog from "../common/status_notifications/loading";
-import {getErrorDialog, getFormValidationErrorDialog, getUpdateFailureResultDialog} from "../common/toasts/toasts";
+import { getErrorDialog, getFormValidationErrorDialog, getUpdateFailureResultDialog } from "../common/toasts/toasts";
 
 const INITIAL_DATA = {
   firstName: "",
@@ -14,6 +14,7 @@ const INITIAL_DATA = {
   email: "",
   password: "",
   confirmPassword: "",
+  domain: "",
   street: "",
   city: "",
   state: "",
@@ -44,12 +45,12 @@ function FreeTrialSignup(props) {
   //Check if the email is already registered in the system
   const isEmailAvailable = async () => {
     const apiCall = new ApiService("/users/check-email", {}, null, { email: registrationDataDto.getData("email") });
-    return await apiCall.post()
+    return await apiCall
+      .post()
       .then(function (response) {
         if (response.data) {
           return false;
-        }
-        else {
+        } else {
           return true;
         }
       })
@@ -81,21 +82,21 @@ function FreeTrialSignup(props) {
       let toast = getErrorDialog("Email address already exists.", setShowToast);
       setToast(toast);
       setShowToast(true);
-    }
-    else if (registrationDataDto.isModelValid()) {
+    } else if (registrationDataDto.isModelValid()) {
       let finalObject = registrationDataDto.getPersistData();
-      let attributes = { title: registrationDataDto.getData("title"), company: registrationDataDto.getData("company")};
-      let configuration = { cloudProvider: "EKS", cloudProviderRegion: "us-east-2"};
+      let attributes = { title: registrationDataDto.getData("title"), company: registrationDataDto.getData("company") };
+      let configuration = { cloudProvider: "EKS", cloudProviderRegion: "us-east-2" };
       delete finalObject["title"];
       delete finalObject["company"];
       finalObject["attributes"] = attributes;
       finalObject["configuration"] = configuration;
-      finalObject["domain"] = "freetrial";
+      finalObject["domain"] = registrationDataDto.getData("domain");
       finalObject["organizationName"] = "freeTrial";
 
       setIsLoading(true);
       const apiCall = new ApiService("/users/create", {}, null, finalObject);
-      await apiCall.post()
+      await apiCall
+        .post()
         .then(function (response) {
           console.debug(response);
           setIsLoading(false);
@@ -108,8 +109,7 @@ function FreeTrialSignup(props) {
           console.error(error.message);
           setIsLoading(false);
         });
-    }
-    else {
+    } else {
       let toast = getFormValidationErrorDialog(setShowToast, "detailPanelTop");
       setToast(toast);
       setShowToast(true);
@@ -117,44 +117,91 @@ function FreeTrialSignup(props) {
   };
 
   if (isLoading || registrationDataDto == null) {
-    return (<LoadingDialog size="sm"/>);
+    return <LoadingDialog size="sm" />;
   } else {
     return (
       <div className="new-user-signup-form">
         <Form className="m-auto" noValidate onSubmit={signupSubmit}>
           <Card>
-            <Card.Header as="h5" className="new-user-header">Sign Up For Opsera</Card.Header>
+            <Card.Header as="h5" className="new-user-header">
+              Sign Up For Opsera
+            </Card.Header>
             <Card.Body className="new-user-body">
               {showToast && toast}
               <div className="signupForm p-2">
                 <div className="pr-3 pb-3">
-                  <DtoTextInput fieldName={"firstName"} setDataObject={setRegistrationDataDto}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"lastName"} setDataObject={setRegistrationDataDto}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"email"} setDataObject={setRegistrationDataDto}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"company"} setDataObject={setRegistrationDataDto}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"title"} setDataObject={setRegistrationDataDto}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"password"} setDataObject={setRegistrationDataDto} type={"password"}
-                                dataObject={registrationDataDto}/>
-                  <DtoTextInput fieldName={"confirmPassword"} setDataObject={setRegistrationDataDto} type={"password"}
-                                dataObject={registrationDataDto}/>
+                  <DtoTextInput
+                    fieldName={"firstName"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"lastName"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"email"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"company"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"domain"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"title"}
+                    setDataObject={setRegistrationDataDto}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"password"}
+                    setDataObject={setRegistrationDataDto}
+                    type={"password"}
+                    dataObject={registrationDataDto}
+                  />
+                  <DtoTextInput
+                    fieldName={"confirmPassword"}
+                    setDataObject={setRegistrationDataDto}
+                    type={"password"}
+                    dataObject={registrationDataDto}
+                  />
                 </div>
                 <div className="px-2">
-                  {isLoading ?
-                    <Button id="login-button" disabled={true} variant="outline-success" className="mr-2 px-4"
-                            type="button"><span>Working...</span></Button> :
-                    <Button size="md" className="register-button mx-auto" id="login-button" type="submit"
-                            variant="success"><span>Register Account</span></Button>
-                  }
+                  {isLoading ? (
+                    <Button
+                      id="login-button"
+                      disabled={true}
+                      variant="outline-success"
+                      className="mr-2 px-4"
+                      type="button"
+                    >
+                      <span>Working...</span>
+                    </Button>
+                  ) : (
+                    <Button
+                      size="md"
+                      className="register-button mx-auto"
+                      id="login-button"
+                      type="submit"
+                      variant="success"
+                    >
+                      <span>Register Account</span>
+                    </Button>
+                  )}
                 </div>
               </div>
             </Card.Body>
             <Card.Footer className="new-user-footer">
-              <div className="text-muted text-right pr-2"><span className="danger-red">*</span> Required Fields</div>
+              <div className="text-muted text-right pr-2">
+                <span className="danger-red">*</span> Required Fields
+              </div>
             </Card.Footer>
           </Card>
         </Form>
