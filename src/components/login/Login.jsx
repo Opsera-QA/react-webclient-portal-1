cfa73b5d9ee547e6e35fc77e3b2382a1ad8eaf72
Todd Barczak <todd@opsera.io>
2020-09-02 15:32:05 -0400

    Remove unnecessary code

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,16 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LoginForm from "./LoginForm";
-import { useOktaAuth } from "@okta/okta-react";
 
 const Login = ({ issuer }) => {
-  const { authState } = useOktaAuth();
-
-  if (authState.isPending) {
-    return <div>Loading...</div>;
-  }
-
-  console.log(authState)
 
   return <LoginForm issuer={issuer} />;
 };
