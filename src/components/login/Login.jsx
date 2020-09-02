db8842b6c9b92f570a92b4c89939f589a440c655
Todd Barczak <todd@opsera.io>
2020-09-02 18:24:42 -0400

    New route of Okta/login tests (part 2b)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,17 +2,17 @@ import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import LoginForm from "./LoginForm";
 import { useOktaAuth } from "@okta/okta-react";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 
 const Login = ({ issuer, redirectFromLogin }) => {
-  const history = useHistory();
+  //const history = useHistory();
   const { authState } = useOktaAuth();
 
   useEffect(() => {
-    console.log(authState);
+    //console.log(authState);
     if (!authState.isPending && authState.isAuthenticated) {
       console.log("Login.jsx detected an authenticated state, so pushing to /");
-      console.debug("warning, this could be a problem with 401 errors IF this state says it's authenticated but token expired, so may now want to do this");
+      //console.debug("warning, this could be a problem with 401 errors IF this state says it's authenticated but token expired, so may now want to do this");
       //history.push("/");
       redirectFromLogin();
     }
