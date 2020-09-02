1ed67803941a3fa22f3c38509574d34fa0f31470
Todd Barczak <todd@opsera.io>
2020-09-02 15:58:24 -0400

    Login issue fixes (part 5)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import LoginForm from "./LoginForm";
+import { useOktaAuth } from "@okta/okta-react";
 
 const Login = ({ issuer }) => {
+  const { authState } = useOktaAuth();
 
-  return <LoginForm issuer={issuer} />;
+  useEffect(() => {
+
+  }, [authState]);
+
+  if (authState.isAuthenticated) {
+    window.location = "/";
+    return;
+  }
+
+  if (authState.isPending) {
+    return <div>Loading...</div>;
+  }
+
+  return <LoginForm issuer={issuer}/>;
 };
 
 Login.propTypes = {
-  issuer: PropTypes.string
+  issuer: PropTypes.string,
 };
 export default Login;
 
