174104ba8c2966924e23a38293a91421c9792c09
Syed Faseeh Uddin <faseeh@opsera.io>
2020-09-11 16:51:40 +0000

    Sfdc tool url

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ArgoCDStepConfiguration.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ArgoCDStepConfiguration.jsx
@@ -199,7 +199,7 @@ function ArgoCDStepConfiguration( { stepTool, pipelineId, plan, stepId, parentCa
 
   const handleArgoChange = (selectedOption) => {
     setFormData({ ...formData, toolConfigId: selectedOption.id ? selectedOption.id : "", userName: selectedOption.configuration && selectedOption.configuration.userName  ? selectedOption.configuration.userName : "",
-      toolUrl: selectedOption.configuration && selectedOption.configuration.toolUrl  ? selectedOption.configuration.toolUrl : "",
+      toolUrl: selectedOption.configuration && selectedOption.configuration.toolURL  ? selectedOption.configuration.toolURL : "",
       password: selectedOption.configuration && selectedOption.configuration.password ? selectedOption.configuration.password : "",
       vaultSecretKey: selectedOption.configuration && selectedOption.configuration.password ? selectedOption.configuration.password.vaultKey : "",
       applicationName: ""
