d936433de17c659e9ff93a6e678494e85be01ff9
Noah Champoux <noah@opsera.io>
2020-09-17 23:13:16 +0000

    Added Argo Base pages

--- src/components/inventory/tools/tool_details/tool_jobs/argo/applications/ArgoApplications.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/argo/applications/ArgoApplications.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import JenkinsJobEditorPanel from "./details/JenkinsJobEditorPanel";
-import JenkinsJobsTable from "./ArgoApplicationsTable";
+import ArgoApplicationEditorPanel from "./details/ArgoApplicationEditorPanel";
+import ArgoApplicationsTable from "./ArgoApplicationsTable";
 
 import PropTypes from "prop-types";
 import "components/inventory/tools/tools.css";
 
 function ArgoApplications({ toolData, loadData, isLoading }) {
-  const [jobData, setJobData] = useState(undefined);
+  const [argoApplicationData, setArgoApplicationData] = useState(undefined);
 
   const selectedJobRow = (rowData) => {
-    setJobData(rowData.original);
+    setArgoApplicationData(rowData.original);
   };
 
   return (
     <div>
-      {jobData != null
-        ? <JenkinsJobEditorPanel toolData={toolData.getPersistData()} jobData={jobData} loadData={loadData}/>
-        : <JenkinsJobsTable isLoading={isLoading} toolData={toolData} loadData={loadData} selectedRow={rowData => selectedJobRow(rowData)}/>}
+      {argoApplicationData != null
+        ? <ArgoApplicationEditorPanel toolData={toolData.getPersistData()} argoApplicationData={argoApplicationData} loadData={loadData}/>
+        : <ArgoApplicationsTable isLoading={isLoading} toolData={toolData} loadData={loadData} selectedRow={rowData => selectedJobRow(rowData)}/>}
     </div>
   );
 }
