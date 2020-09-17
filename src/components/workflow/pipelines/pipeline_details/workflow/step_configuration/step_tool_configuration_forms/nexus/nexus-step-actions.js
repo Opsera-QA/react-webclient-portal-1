97ae315f952e28fc9dfb60ae37ef71d8d2acd242
Noah Champoux <noah@opsera.io>
2020-09-17 18:14:27 -0400

    Nexus step configuration cleanup

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/nexus/nexus-step-actions.js
@@ -0,0 +1,21 @@
+import {axiosApiService} from "../../../../../../../../api/apiService";
+
+const nexusStepActions = {};
+
+// TODO: This might make more sense to put into a master stepToolActions form. I'll do that, if most of the other configurations only have one unique API call
+nexusStepActions.getNexusRepositoriesList = async (toolID, getAccessToken) => {
+  let postBody = {
+    params: {
+      tool: "nexus",
+      toolId: toolID,
+    }
+  }
+  const accessToken = await getAccessToken();
+  const apiUrl = `/tools/nexus/repositories`;
+  const response = await axiosApiService(accessToken).get(apiUrl, postBody)
+    .then((result) =>  {return result;})
+    .catch(error => {throw error;});
+  return response;
+};
+
+export default nexusStepActions;
