25695c29398dbaa44322e9c142feb78b778bcf78
Noah Champoux <noah@opsera.io>
2020-09-19 14:42:41 -0400

    Created Detail Panel Loading Dialog component

---
+++ src/components/common/loading/DetailPanelLoadingDialog.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons";
+
+function DetailPanelLoadingDialog({ type }) {
+  return (
+    <div className="p-5 text-lg-center">
+      <span className="m-auto"><FontAwesomeIcon icon={faSpinner} spin className="mr-2"/>Loading {type}</span>
+    </div>
+  );
+}
+
+DetailPanelLoadingDialog.propTypes = {
+  type: PropTypes.string
+};
+
+
+export default DetailPanelLoadingDialog;
