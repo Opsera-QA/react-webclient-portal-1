084560fa4239116ad069784196354c249b41d34d
Noah Champoux <noah@opsera.io>
2020-10-08 17:08:01 +0000

    Delete button pipeline

--- src/components/common/actions/buttons/ActionBarDeleteButton.jsx
+++ src/components/common/actions/buttons/ActionBarDeleteButton.jsx
@@ -3,16 +3,21 @@ import PropTypes from "prop-types";
 import ActionBarButton from "./ActionBarButton";
 import {faTrash} from "@fortawesome/pro-light-svg-icons";
 
-function ActionBarDeleteButton({handleDeleteClick, itemName}) {
+function ActionBarDeleteButton({handleDeleteClick, itemName, itemId}) {
+
+  const deleteObject = () => {
+    handleDeleteClick(itemId);
+  }
 
   return (
-    <ActionBarButton action={handleDeleteClick} icon={faTrash} iconClasses={"danger-red"} popoverText={`Delete this ${itemName}`} />
+    <ActionBarButton action={deleteObject} icon={faTrash} iconClasses={"danger-red"} popoverText={`Delete this ${itemName}`} />
   );
 }
 
 ActionBarDeleteButton.propTypes = {
   handleDeleteClick: PropTypes.func,
-  itemName: PropTypes.string
+  itemName: PropTypes.string,
+  itemId: PropTypes.string
 };
 
 export default ActionBarDeleteButton;
