ce2891be6fbcaca654583d139a69a94f8873f1a9
Noah Champoux <noah@opsera.io>
2020-10-16 12:41:20 -0400

    Limiting page numbers from showing too many values with large amounts of data.

--- src/components/common/pagination/pagination-helpers.js
+++ src/components/common/pagination/pagination-helpers.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export const getResultSummary = (paginationDto) => {
+
+  if (paginationDto.getData("totalCount") === 0) {
+    return (
+      <div><span>No Results Found</span></div>
+    );
+  }
+
   const lowerResultsViewLimit = ((paginationDto.getData("currentPage") - 1) * paginationDto.getData("pageSize")) + 1;
   const upperResultsViewLimit = (lowerResultsViewLimit + paginationDto.getData("pageSize")) - 1;
   let countOffsetUpper = (upperResultsViewLimit < paginationDto.getData("totalCount")) ? upperResultsViewLimit : paginationDto.getData("totalCount");
