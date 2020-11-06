8f87dc142577f0946261f4e55ddd9dd70b037dd2
Todd Barczak <todd@opsera.io>
2020-11-06 13:30:55 -0500

    Fixes to build scripts for free-trial

--- dev-scripts/01_push-image.sh
+++ dev-scripts/01_push-image.sh
@@ -2,9 +2,9 @@
 set -xe
 docker stop opsera-reactapp || true
 docker rm opsera-reactapp || true
-docker build -t opsera-reactapp:freetrial --build-arg DOCKER_ENV=freetrial  ..
+docker build -t opsera-reactapp:freetrial --build-arg build_env=freetrial  ..
 
 #Push to AWS.
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 440953937617.dkr.ecr.us-east-2.amazonaws.com
-docker tag opsera-reactapp:latest 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
+docker tag opsera-reactapp:freetrial 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
 docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
