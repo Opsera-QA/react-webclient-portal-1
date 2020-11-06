ac3729ae587e7bb0cd1886e30b1ef8e3fa7f017b
Todd Barczak <todd@opsera.io>
2020-11-06 13:47:01 -0500

    Fix run command

--- dev-scripts/remote-script.sh
+++ dev-scripts/remote-script.sh
@@ -6,9 +6,10 @@ docker rm opsera-reactapp || true
 docker image prune -a -f
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 440953937617.dkr.ecr.us-east-2.amazonaws.com
 docker pull 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
-docker run --network=host \
-	--name opsera-reactapp \
-	--restart unless-stopped \
-	-d 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
+docker run \
+    --name opsera-reactapp \
+    --restart unless-stopped \
+    -p 8000:80 \
+    -d 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:freetrial
 docker ps -a
 echo "Successfully deployed reactapp"
