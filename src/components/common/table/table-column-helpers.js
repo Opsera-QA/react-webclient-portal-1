77257ec2abdfb4570643294cb8c1934180821188
Todd Barczak <todd@opsera.io>
2020-11-23 12:53:11 -0500

    Update table status display to match latest changes

--- src/components/common/table/table-column-helpers.js
+++ src/components/common/table/table-column-helpers.js
@@ -1,7 +1,6 @@
-// TODO: Create field helper class
 import { format } from "date-fns";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCheckCircle, faPause, faSpinner, faStop, faTimesCircle} from "@fortawesome/free-solid-svg-icons";
+import {faCheckCircle, faPause, faSpinner, faStop, faTimesCircle} from "@fortawesome/pro-light-svg-icons";
 import React from "react";
 import PipelineHelpers from "../../workflow/pipelineHelpers";
 import PipelineStatus from "../../workflow/pipelines/PipelineStatus";
@@ -47,36 +46,43 @@ export const getTablePipelineStatusColumn = (field) => {
       let pipelineStatus = PipelineHelpers.getPipelineStatus(workflow.row.original);
 
       switch (pipelineStatus) {
-        case "failed":
-          return (
-            <div className="red">
-              <PipelineStatus className="red"
-                              innerText={"An error has occurred in this pipeline.  See activity logs for details."}
-                              icon={faTimesCircle} statusText={"Failed"} tableColumn={true}/>
-            </div>
-          );
-        case "running":
-          return (
-            <div className="green">
-              <PipelineStatus innerText={"A pipeline operation is currently in progress."} icon={faSpinner}
-                              statusText={"Running"} tableColumn={true}/>
-            </div>
-          );
-        case "paused":
-          return (
-            <div className="yellow">
-              <PipelineStatus innerText={"The pipeline operation is currently paused."} icon={faPause}
-                              statusText={"Paused"} tableColumn={true}/>
-            </div>
-          );
-        default:
-          return (
-            <PipelineStatus innerText={"This pipeline is not currently running."} icon={faStop}
-                            statusText={"Stopped"} tableColumn={true}/>
-          );
+      case "failed":
+        return (
+          <div className="red">
+            <PipelineStatus className="red"
+                            innerText={"An error has occurred in this pipeline.  See activity logs for details."}
+                            icon={faTimesCircle} statusText={"Failed"} tableColumn={true}/>
+          </div>
+        );
+      case "running":
+        return (
+          <div className="green">
+            <PipelineStatus innerText={"A pipeline operation is currently in progress."} icon={faSpinner}
+                            statusText={"Running"} tableColumn={true}/>
+          </div>
+        );
+      case "paused":
+        return (
+          <div className="yellow">
+            <PipelineStatus innerText={"The pipeline operation is currently paused."} icon={faPause}
+                            statusText={"Paused"} tableColumn={true}/>
+          </div>
+        );
+      case "success":
+        return (
+          <div className="green">
+            <PipelineStatus innerText={"The most recent run of this pipeline was successful."} icon={faCheckCircle}
+                            statusText={"Successful"} tableColumn={true}/>
+          </div>
+        );
+      default:
+        return (
+          <PipelineStatus innerText={"This pipeline is not currently running."} icon={faStop}
+                          statusText={"Stopped"} tableColumn={true}/>
+        );
       }
     },
-    class: "text-center"
+    class: "text-left"
   };
 };
 
