a2c565b3c49523aa8d4b8f71657b2ab384a393bd
Todd Barczak <todd@opsera.io>
2020-12-07 09:57:13 -0500

    Rework Left Nav for OpseraAdmin only. Added new Reports and updated Insights links (with Feature Flag)

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -21,7 +21,7 @@ import {
   faCogs,
   faChartNetwork,
   faCaretSquareDown,
-  faCaretSquareUp
+  faCaretSquareUp, faAnalytics,
 } from "@fortawesome/pro-light-svg-icons";
 
 import "./sidebar.css";
@@ -32,7 +32,7 @@ const hiddenNav = () => {
 
 function Sidebar({ userData, hideSideBar }) {
   const contextType = useContext(AuthContext);
-  const { setAccessRoles } = contextType;
+  const { setAccessRoles, featureFlagHideItemInProd } = contextType;
   const [renderOutput, setRenderOutput] = useState(hiddenNav);
 
   /*const [insights, setInsights] = useState(false);
@@ -59,7 +59,8 @@ function Sidebar({ userData, hideSideBar }) {
     }
 
     if (accessRole.OpseraAdministrator) {
-      return funcOpseraAdminNav;
+      //return funcOpseraAdminNav;
+      return (<OpseraAdminUserNav accessRole={accessRole} featureFlagHideItemInProd={featureFlagHideItemInProd}/>);
     } else if (accessRole.Type === "sass-user") {
       return funcSassNav;
     } else { //drop into LDAP Org roles
@@ -77,15 +78,8 @@ function Sidebar({ userData, hideSideBar }) {
 }
 
 
-const funcOpseraAdminNav = () => {
+/*const funcOpseraAdminNav = () => {
   return <>
-    {/*<div className="d-block d-md-none pt-1 mr-2">
-      <Button variant="outline-primary" onClick={handleToggleMenuClick}>
-              <span className="dark-blue-text"><i
-                className="fas fa-bars fa-1x"></i></span>
-      </Button>
-    </div>*/}
-
     <div className={"w-20 pt-1 d-block pl-2"}>
       <div className="sidebar-container sticky-top pb-5 pt-1 pl-1">
         <div className="sidebar-menu pt-3">
@@ -118,9 +112,9 @@ const funcOpseraAdminNav = () => {
             <FontAwesomeIcon size="lg" icon={faLayerGroup} fixedWidth/> <span
             className="menu-text">Blueprints</span></NavLink>
 
-          {/*<NavLink className="nav-link" activeClassName="chosen" to="/tools">
+          {/!*<NavLink className="nav-link" activeClassName="chosen" to="/tools">
             <FontAwesomeIcon size="lg" icon={faLink} fixedWidth/> <span
-            className="menu-text">API Tools</span></NavLink>*/}
+            className="menu-text">API Tools</span></NavLink>*!/}
           <NavLink className="nav-link" activeClassName="chosen" to="/update">
             <FontAwesomeIcon size="lg" icon={faDownload} fixedWidth/> <span
             className="menu-text">Updates</span></NavLink>
@@ -135,7 +129,7 @@ const funcOpseraAdminNav = () => {
       </div>
     </div>
   </>;
-};
+};*/
 
 const funcSassNav = () => {
   return (<>
@@ -440,7 +434,7 @@ function FreeTrialNav({ accessRole }) {
             <div className="text-center pointer flex-grow-1 pr-2 pt-2"
                  onClick={() => setInsights(insights => !insights)}>
               {insights ?
-              <FontAwesomeIcon size="sm" icon={faCaretSquareUp} fixedWidth/> :
+                <FontAwesomeIcon size="sm" icon={faCaretSquareUp} fixedWidth/> :
                 <FontAwesomeIcon size="sm" icon={faCaretSquareDown} fixedWidth/>}
             </div>
           </div>
@@ -496,8 +490,104 @@ function FreeTrialNav({ accessRole }) {
 }
 
 
+function OpseraAdminUserNav({ accessRole, featureFlagHideItemInProd }) {
+  const [insights, setInsights] = useState(false);
+
+  //get FeatureFlag
+
+  return (<>
+    <div className="w-20 pt-1 d-block">
+      <div className="sidebar-container sticky-top pb-5 pt-1 pl-1">
+        <div className="sidebar-menu pt-3">
+          <NavLink className="nav-link" activeClassName="chosen" exact to="/">
+            <FontAwesomeIcon size="lg" icon={faHome} fixedWidth/> <span
+            className="menu-text">Overview</span></NavLink>
+
+
+          <div className="mt-4 mb-2 sub-header">Products</div>
+          <NavLink className="nav-link" activeClassName="chosen" to="/platform">
+            <FontAwesomeIcon size="lg" icon={faBox} fixedWidth/> <span
+            className="menu-text">Toolchain</span></NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/workflow">
+            <FontAwesomeIcon size="lg" icon={faDraftingCompass} fixedWidth/> <span
+            className="menu-text">Pipelines</span></NavLink>
+
+          {!featureFlagHideItemInProd ?
+            <NavLink className="nav-link" activeClassName="chosen" to="/analytics">
+              <FontAwesomeIcon size="lg" icon={faChartNetwork} fixedWidth/> <span
+              className="menu-text">Insights</span></NavLink>
+            :
+            <>
+              <div className="d-flex flex-row">
+                <div>
+                  <NavLink className="nav-link" activeClassName="chosen" to="/insights/dashboards"
+                           onClick={() => setInsights(true)}>
+                    <FontAwesomeIcon size="lg" icon={faChartNetwork} fixedWidth/> <span
+                    className="menu-text">Insights</span>
+                  </NavLink>
+                </div>
+                <div className="text-center pointer flex-grow-1 pr-2 pt-2"
+                     onClick={() => setInsights(insights => !insights)}>
+                  {insights ?
+                    <FontAwesomeIcon size="sm" icon={faCaretSquareUp} fixedWidth/> :
+                    <FontAwesomeIcon size="sm" icon={faCaretSquareDown} fixedWidth/>}
+                </div>
+              </div>
+            </>
+          }
+
+          {insights && <>
+            <NavLink className="nav-link" activeClassName="chosen" exact to="/insights/marketplace">
+              <span className="menu-text-sub">Marketplace</span>
+            </NavLink>
+
+            <NavLink className="nav-link" activeClassName="chosen" to="/analytics">
+              <span className="menu-text-sub">Legacy Analytics</span></NavLink>
+            <NavLink className="nav-link" activeClassName="chosen" exact to="/dashboard">
+              <span className="menu-text-sub">Legacy Dashboards</span>
+            </NavLink>
+          </>}
+
+
+          <div className="mt-3 mb-2 sub-header">Operations</div>
+          <NavLink className="nav-link" activeClassName="chosen" to="/inventory/tools">
+            <FontAwesomeIcon size="lg" icon={faClipboardList} fixedWidth/> <span
+            className="menu-text">Tool Registry</span></NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/logs">
+            <FontAwesomeIcon size="lg" icon={faArchive} fixedWidth/> <span className="menu-text">Logs</span></NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/blueprint">
+            <FontAwesomeIcon size="lg" icon={faLayerGroup} fixedWidth/> <span
+            className="menu-text">Blueprints</span></NavLink>
+
+          {featureFlagHideItemInProd &&
+            <NavLink className="nav-link" activeClassName="chosen" to="/reports">
+              <FontAwesomeIcon size="lg" icon={faAnalytics} fixedWidth/> <span
+              className="menu-text">Reports</span></NavLink>
+          }
+
+          <NavLink className="nav-link" activeClassName="chosen" to="/update">
+            <FontAwesomeIcon size="lg" icon={faDownload} fixedWidth/> <span
+            className="menu-text">Updates</span></NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/settings">
+            <FontAwesomeIcon size="lg" icon={faCogs} fixedWidth/> <span
+            className="menu-text">Settings</span></NavLink>
+          <NavLink className="nav-link" activeClassName="chosen" to="/admin">
+            <FontAwesomeIcon size="lg" icon={faTools} fixedWidth/> <span
+            className="menu-text">Admin Tools</span></NavLink>
+
+        </div>
+      </div>
+    </div>
+  </>);
+}
+
 FreeTrialNav.propTypes = {
   accessRole: PropTypes.object,
+};
+
+OpseraAdminUserNav.propTypes = {
+  accessRole: PropTypes.object,
+  featureFlagHideItemInProd: PropTypes.func,
 };
 
 Sidebar.propTypes = {
