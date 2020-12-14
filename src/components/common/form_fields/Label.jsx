3f1038b52f04ce84ac3b8b5bda9eea11650e1552
Todd Barczak <todd@opsera.io>
2020-12-14 15:41:34 -0500

    Fix undefined error

--- src/components/common/form_fields/Label.jsx
+++ src/components/common/form_fields/Label.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Label({ field }) {
-  if (field.label) {
+  if (field && field.label) {
     return (<label className="mr-2 text-muted"><span>{field.label}:</span></label>);
   }
   return (<></>);
