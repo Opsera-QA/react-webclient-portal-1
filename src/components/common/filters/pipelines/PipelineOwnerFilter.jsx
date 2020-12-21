73cdcf1b2c793a1a9b2dd1c9081a2d7e239b25b1
Noah Champoux <noah@opsera.io>
2020-12-21 14:12:12 +0000

    TP-273 Don't show Pipeline Owner filter on Sass users.

--- src/components/common/filters/pipelines/PipelineOwnerFilter.jsx
+++ src/components/common/filters/pipelines/PipelineOwnerFilter.jsx
@@ -6,8 +6,10 @@ import {DialogToastContext} from "../../../../contexts/DialogToastContext";
 import {getUsersByDomain} from "../../../settings/ldap_users/user-functions";
 
 function PipelineOwnerFilter({ filterDto, setFilterDto }) {
-  const { getAccessToken, getUserRecord } = useContext(AuthContext);
+  const { getAccessToken, getUserRecord, setAccessRoles } = useContext(AuthContext);
   const toastContext  = useContext(DialogToastContext);
+  const [accessRoleData, setAccessRoleData] = useState(undefined);
+  const [user, setUser] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
   const [userOptions, setUserOptions] = useState([]);
 
@@ -18,7 +20,16 @@ function PipelineOwnerFilter({ filterDto, setFilterDto }) {
   const loadData = async () => {
     try {
       setIsLoading(true);
-      await getUsers();
+      const user = await getUserRecord();
+      const {ldap} = user;
+      setUser(user);
+      const userRoleAccess = await setAccessRoles(user);
+      setAccessRoleData(userRoleAccess)
+
+      if (userRoleAccess.Type !== "sass-user" && ldap.domain != null)
+      {
+        await getUsers(ldap);
+      }
     }
     catch (error) {
       toastContext.showErrorDialog(error,"Could not load users.");
@@ -28,9 +39,7 @@ function PipelineOwnerFilter({ filterDto, setFilterDto }) {
     }
   }
 
-  const getUsers = async () => {
-    const user = await getUserRecord();
-    const {ldap} = user;
+  const getUsers = async (ldap) => {
     let users = await getUsersByDomain(ldap.domain, getAccessToken);
     let userOptions = [];
 
@@ -42,6 +51,11 @@ function PipelineOwnerFilter({ filterDto, setFilterDto }) {
 
     setUserOptions(userOptions);
   };
+
+  if (user == null || user.ldap?.domain == null || accessRoleData == null || accessRoleData?.Type === "sass-user") {
+    return <></>
+  }
+
 
   return (
     <div><DtoFilterSelectInput fieldName={"owner"} busy={isLoading} placeholderText={"Filter by Pipeline Owner"} setDataObject={setFilterDto} dataObject={filterDto} selectOptions={userOptions} /></div>
