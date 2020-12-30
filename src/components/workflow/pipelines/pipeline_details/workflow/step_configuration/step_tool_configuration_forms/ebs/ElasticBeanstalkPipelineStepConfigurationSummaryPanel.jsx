28b4294beb148109d715b8656ef9eceee36e8aa4
Noah Champoux <noah@opsera.io>
2020-12-30 23:57:45 +0000

    Elastic beanstalk pipeline step summary panel (Merge 3rd)

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ebs/ElasticBeanstalkPipelineStepConfigurationSummaryPanel.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import PropTypes from "prop-types";
+import PipelineStepSummaryPanelContainer from "../../PipelineStepSummaryPanelContainer";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import JsonField from "components/common/fields/json/JsonField";
+
+function ElasticBeanstalkPipelineStepConfigurationSummaryPanel({ elasticBeanstalkPipelineStepData, pipelineData, setActiveTab }) {
+  return (
+    <PipelineStepSummaryPanelContainer setActiveTab={setActiveTab} pipelineData={pipelineData}>
+      <Row>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"awsToolConfigId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"bucketName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"ec2KeyName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"port"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"platform"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"applicationName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"applicationVersionLabel"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"s3StepId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"hostedZoneId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"bucketName"} />
+        </Col>
+        <Col lg={12}>
+          <TextFieldBase dataObject={elasticBeanstalkPipelineStepData} fieldName={"description"} />
+        </Col>
+        <Col lg={12}>
+          <JsonField dataObject={elasticBeanstalkPipelineStepData} fieldName={"dockerVolumePath"} />
+        </Col>
+        <Col lg={12}>
+          <JsonField dataObject={elasticBeanstalkPipelineStepData} fieldName={"environments"} />
+        </Col>
+      </Row>
+    </PipelineStepSummaryPanelContainer>
+  );
+}
+
+ElasticBeanstalkPipelineStepConfigurationSummaryPanel.propTypes = {
+  elasticBeanstalkPipelineStepData: PropTypes.object,
+  pipelineData: PropTypes.object,
+  setActiveTab: PropTypes.func
+};
+
+export default ElasticBeanstalkPipelineStepConfigurationSummaryPanel;
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/elastic_beanstalk_deploy/elasticBeanstalkDeployPipelineStepConfigurationMetadata.js
@@ -0,0 +1,74 @@
+const elasticBeanstalkDeployPipelineStepConfigurationMetadata = {
+  type: "Elastic Beanstalk Deploy Pipeline Step Configuration",
+  fields: [
+    {
+      label: "AWS Credentials",
+      id: "awsToolConfigId",
+      isRequired: true
+    },
+    {
+      label: "S3 Bucket Name",
+      id: "bucketName",
+      isRequired: true,
+      maxLength: 150
+    },
+    {
+      label: "EC2 Key Name",
+      id: "ec2KeyName",
+      isRequired: true,
+      maxLength: 50,
+      formText: "Key-pair file name used to access the EC2 instance."
+    },
+    {
+      label: "Application Port",
+      id: "port",
+      isRequired: true,
+      maxLength: 10,
+      formText: "Port that the application needs in order to run."
+    },
+    {
+      label: "Platform",
+      id: "platform",
+      isRequired: true,
+    },
+    {
+      label: "Application Name",
+      id: "applicationName",
+      maxLength: 250,
+    },
+    {
+      label: "Description",
+      id: "description",
+      maxLength: 250,
+    },
+    {
+      label: "Application Version",
+      id: "applicationVersionLabel",
+      maxLength: 50,
+    },
+    {
+      label: "S3 Step Info",
+      id: "s3StepId",
+    },
+    {
+      label: "AWS Regions",
+      id: "regions",
+    },
+  ],
+  newModelBase: {
+    awsToolConfigId: "",
+    accessKey: "",
+    secretKey: "",
+    bucketName: "",
+    regions: "",
+    applicationName: "",
+    applicationVersionLabel: "",
+    s3StepId: "",
+    description: "",
+    port: "",
+    ec2KeyName: "",
+    platform: ""
+  }
+};
+
+export default elasticBeanstalkDeployPipelineStepConfigurationMetadata
