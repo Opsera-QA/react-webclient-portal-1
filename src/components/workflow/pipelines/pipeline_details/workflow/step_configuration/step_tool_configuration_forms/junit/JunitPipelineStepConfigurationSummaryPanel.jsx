baf66745a39182a3d059aa35c7ae07ebffb6dfad
Noah Champoux <noah@opsera.io>
2020-12-31 00:01:59 +0000

    JUnit Step Summary Panel (Merge 6th)

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/junit/JunitPipelineStepConfigurationSummaryPanel.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import PropTypes from "prop-types";
+import PipelineStepSummaryPanelContainer from "../../PipelineStepSummaryPanelContainer";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import JsonField from "components/common/fields/json/JsonField";
+
+function JunitPipelineStepConfigurationSummaryPanel({ junitPipelineStepData, pipelineData, setActiveTab }) {
+  return (
+    <PipelineStepSummaryPanelContainer setActiveTab={setActiveTab} pipelineData={pipelineData}>
+      <Row>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"toolConfigId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"jobType"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"jobName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"toolJobId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"gitCredential"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"workspace"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"repository"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"branch"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"rollbackBranchName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"gitBranch"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"stepIdXML"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"dockerName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"dockerTagName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={junitPipelineStepData} fieldName={"dockerPath"} />
+        </Col>
+        <Col lg={6}>
+          <JsonField dataObject={junitPipelineStepData} fieldName={"buildArgs"} />
+        </Col>
+      </Row>
+    </PipelineStepSummaryPanelContainer>
+  );
+}
+
+JunitPipelineStepConfigurationSummaryPanel.propTypes = {
+  junitPipelineStepData: PropTypes.object,
+  pipelineData: PropTypes.object,
+  setActiveTab: PropTypes.func
+};
+
+export default JunitPipelineStepConfigurationSummaryPanel;
