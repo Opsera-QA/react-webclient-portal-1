dfe58054af4e10c66f032124297f25520fc624be
Noah Champoux <noah@opsera.io>
2021-01-04 16:46:28 +0000

    (Merge 2nd) Bitbucket tool configuration refactor

---
+++ src/components/inventory/tools/tool_details/tool_jobs/bitbucket/BitbucketTwoFactorToggle.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import bitbucketConnectionMetadata from "./bitbucket-connection-metadata";
+import {Form} from "react-bootstrap";
+import InputContainer from "components/common/inputs/InputContainer";
+
+function BitbucketTwoFactorToggle({dataObject, setDataObject, fieldName, disabled}) {
+  const [field, setField] = useState(dataObject.getFieldById(fieldName));
+
+  const triggerAuthenticationChange = () => {
+    let newDataObject = dataObject;
+    let twoFactorAuthentication = !dataObject.getData("twoFactorAuthentication");
+    newDataObject.setData("twoFactorAuthentication", twoFactorAuthentication);
+    newDataObject.setMetaDataFields(twoFactorAuthentication === true ? bitbucketConnectionMetadata.fieldsAlt : bitbucketConnectionMetadata.fields);
+    newDataObject.setData("secretPrivateKey", "");
+    newDataObject.setData("secretAccessTokenKey", "");
+    newDataObject.setData("accountPassword", "");
+    setDataObject({...newDataObject});
+  };
+
+  return (
+    <InputContainer>
+      <Form.Check
+        type="switch"
+        id={field.id}
+        checked={!!dataObject.getData(fieldName)}
+        disabled={disabled}
+        label={field.label}
+        onChange={() => triggerAuthenticationChange()}
+      />
+    </InputContainer>
+  );
+}
+
+BitbucketTwoFactorToggle.propTypes = {
+  dataObject: PropTypes.object,
+  fieldName: PropTypes.string,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool
+};
+
+export default BitbucketTwoFactorToggle;
