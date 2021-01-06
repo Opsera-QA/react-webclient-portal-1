f5d1c9fa68c942dd9005a41c06abd25817ef6feb
Mahantha <mahanta@opsera.io>
2021-01-06 19:59:31 +0530

    update script

--- scripts/build/prod-docker.sh
+++ scripts/build/prod-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -xe
-docker build -t opsera-reactapp:kube-prod --build-arg build_env=kube-prod  ../../
+nodeenv=${1:-kube-prod}
+docker build -t opsera-reactapp:$nodeenv --build-arg build_env=$nodeenv  ../../
 docker run --rm \
         440953937617.dkr.ecr.us-east-2.amazonaws.com/kubectl \
         aws ecr get-login-password \
@@ -9,10 +10,10 @@ docker run --rm \
         --password-stdin 440953937617.dkr.ecr.us-east-2.amazonaws.com
 
 date_tag=`date +%-d-%m-%Y-%T | sed 's/:/-/g'`
-docker tag opsera-reactapp:kube-prod 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:kube-prod
-docker tag opsera-reactapp:kube-prod 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:kube-prod-${date_tag}
+docker tag opsera-reactapp:$nodeenv 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:$nodeenv
+docker tag opsera-reactapp:$nodeenv 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:$nodeenv-${date_tag}
 
-docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:kube-prod
-docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:kube-prod-${date_tag}
+docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:$nodeenv
+docker push 440953937617.dkr.ecr.us-east-2.amazonaws.com/opsera-reactapp:$nodeenv-${date_tag}
 
 
