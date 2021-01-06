c7f17c20fa5f273ef954ac9d7dc096c1d1c11ef7
Noah Champoux <noah@opsera.io>
2021-01-06 10:30:52 -0500

    Added FieldLabel for future enhancements

---
+++ src/components/common/fields/FieldLabel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function FieldLabel({ field, fieldName }) {
+  if (field == null) {
+    return <label className="mr-2 text-muted"><span>No Metadata For This Field [${fieldName}]:</span></label>;
+  }
+
+  if (field.label == null) {
+    return <></>;
+  }
+
+  return (<label className="mr-2 text-muted"><span>{field.label}:</span></label>);
+}
+
+FieldLabel.propTypes = {
+  field: PropTypes.object,
+  fieldName: PropTypes.string
+};
+
+export default FieldLabel;
