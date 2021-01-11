aa227a81bbb14d7b65b7d9e6a4db34152b897b25
Noah Champoux <noah@opsera.io>
2021-01-11 18:15:21 -0500

    Updated Save button on template editor panel

--- src/components/admin/template_editor/template_detail_view/TemplateEditorPanel.jsx
+++ src/components/admin/template_editor/template_detail_view/TemplateEditorPanel.jsx
@@ -1,24 +1,21 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Col, Row } from "react-bootstrap";
 import { AuthContext } from "contexts/AuthContext";
-import templateActions from "../template-actions";
 import PropTypes from "prop-types";
-import DtoTextInput from "../../../common/input/dto_input/dto-text-input";
-import DtoToggleInput from "../../../common/input/dto_input/dto-toggle-input";
-import DtoJsonInput from "../../../common/input/dto_input/dto-json-input";
-import DtoTagManagerInput from "../../../common/input/dto_input/dto-tag-manager-input";
-import LoadingDialog from "../../../common/status_notifications/loading";
-import SaveButton from "../../../common/buttons/SaveButton";
-import pipelineHelpers from "../../../workflow/pipelineHelpers";
-import {DialogToastContext} from "../../../../contexts/DialogToastContext";
-import DetailPanelContainer from "../../../common/panels/detail_panel_container/DetailPanelContainer";
-import BooleanToggleInput from "../../../common/inputs/BooleanToggleInput";
-import EditorPanelContainer from "../../../common/panels/detail_panel_container/EditorPanelContainer";
+import templateActions from "components/admin/template_editor/template-actions";
+import LoadingDialog from "components/common/status_notifications/loading";
+import EditorPanelContainer from "components/common/panels/detail_panel_container/EditorPanelContainer";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
 import PipelineTypeMultiselectInput
-  from "../../../common/list_of_values_input/admin/pipeline_templates/PipelineTypeMultiselectInput";
+  from "components/common/list_of_values_input/admin/pipeline_templates/PipelineTypeMultiselectInput";
+import DtoTagManagerInput from "components/common/input/dto_input/dto-tag-manager-input";
+import ActivityToggleInput from "components/common/inputs/boolean/ActivityToggleInput";
+import AccountRoleMultiSelectInput from "components/common/list_of_values_input/roles/AccountRoleMultiSelectInput";
 import LdapOrganizationAccountSelectInput
-  from "../../../common/list_of_values_input/admin/accounts/ldap_accounts/LdapOrganizationAccountSelectInput";
-import AccountRoleMultiSelectInput from "../../../common/list_of_values_input/roles/AccountRoleMultiSelectInput";
+  from "components/common/list_of_values_input/admin/accounts/ldap_accounts/LdapOrganizationAccountSelectInput";
+import BooleanToggleInput from "components/common/inputs/BooleanToggleInput";
+import JsonInput from "components/common/inputs/object/JsonInput";
+import PersistButtonContainer from "components/common/buttons/saving/containers/PersistButtonContainer";
 
 function TemplateEditorPanel({ templateData, setTemplateData, handleClose }) {
   const { getAccessToken } = useContext(AuthContext);
@@ -51,13 +48,13 @@ function TemplateEditorPanel({ templateData, setTemplateData, handleClose }) {
       <EditorPanelContainer>
           <Row>
             <Col lg={6}>
-              <DtoTextInput fieldName={"name"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
+              <TextInputBase fieldName={"name"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
             </Col>
             <Col lg={6}>
-              <DtoToggleInput fieldName={"active"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
+              <ActivityToggleInput fieldName={"active"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput fieldName={"description"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
+              <TextInputBase fieldName={"description"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
             </Col>
             <Col lg={6}>
               <PipelineTypeMultiselectInput dataObject={templateDataDto} setDataObject={setTemplateDataDto} />
@@ -78,14 +75,16 @@ function TemplateEditorPanel({ templateData, setTemplateData, handleClose }) {
               <BooleanToggleInput fieldName={"singleUse"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoJsonInput fieldName={"plan"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
+              <JsonInput fieldName={"plan"} dataObject={templateDataDto} setDataObject={setTemplateDataDto}/>
             </Col>
           </Row>
-          <Row>
-            <div className="ml-auto m-3 px-3">
-              <SaveButton updateRecord={updateTemplate} setData={setTemplateData} setRecordDto={setTemplateDataDto} handleClose={handleClose} createRecord={createTemplate} recordDto={templateDataDto} />
-            </div>
-          </Row>
+        <PersistButtonContainer
+          createRecord={createTemplate}
+          updateRecord={updateTemplate}
+          recordDto={templateDataDto}
+          handleClose={handleClose}
+          setRecordDto={setTemplateDataDto}
+        />
       </EditorPanelContainer>
     );
 }
