1f476d78d1a76822fa850191d7280dd30473f7d1
Noah Champoux <noah@opsera.io>
2021-01-11 18:11:39 -0500

    Wired up new save button, new text input to organization editor panel

--- src/components/admin/accounts/ldap/organizations/organizations_detail_view/LdapOrganizationEditorPanel.jsx
+++ src/components/admin/accounts/ldap/organizations/organizations_detail_view/LdapOrganizationEditorPanel.jsx
@@ -3,15 +3,15 @@ import PropTypes from "prop-types";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import DropdownList from "react-widgets/lib/DropdownList";
-import accountsActions from "../../../accounts-actions";
-import { AuthContext } from "../../../../../../contexts/AuthContext";
-import {capitalizeFirstLetter} from "../../../../../common/helpers/string-helpers";
-import DtoTextInput from "../../../../../common/input/dto_input/dto-text-input";
-import LoadingDialog from "../../../../../common/status_notifications/loading";
-import SaveButton from "../../../../../common/buttons/SaveButton";
-import {DialogToastContext} from "../../../../../../contexts/DialogToastContext";
-import WarningDialog from "../../../../../common/status_notifications/WarningDialog";
-import EditorPanelContainer from "../../../../../common/panels/detail_panel_container/EditorPanelContainer";
+import {AuthContext} from "contexts/AuthContext";
+import {DialogToastContext} from "contexts/DialogToastContext";
+import accountsActions from "components/admin/accounts/accounts-actions";
+import LoadingDialog from "components/common/status_notifications/loading";
+import WarningDialog from "components/common/status_notifications/WarningDialog";
+import EditorPanelContainer from "components/common/panels/detail_panel_container/EditorPanelContainer";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
+import PersistButtonContainer from "components/common/buttons/saving/containers/PersistButtonContainer";
+import {capitalizeFirstLetter} from "components/common/helpers/string-helpers";
 
 function LdapOrganizationEditorPanel({ ldapOrganizationData, setLdapOrganizationData, authorizedActions, handleClose }) {
   const { getAccessToken } = useContext(AuthContext);
@@ -95,38 +95,41 @@ function LdapOrganizationEditorPanel({ ldapOrganizationData, setLdapOrganization
               </div>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={!ldapOrganizationDataDto.isNew()} fieldName={"name"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={!ldapOrganizationDataDto.isNew()} fieldName={"name"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput fieldName={"description"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase fieldName={"description"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput fieldName={"orgName"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase fieldName={"orgName"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"orgOwner"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"orgOwner"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"orgOwnerEmail"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"orgOwnerEmail"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"envCount"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"envCount"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"numberOfLicenses"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"numberOfLicenses"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"objectCount"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"objectCount"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
             <Col lg={12}>
-              <DtoTextInput disabled={true} fieldName={"subscription"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
+              <TextInputBase disabled={true} fieldName={"subscription"} dataObject={ldapOrganizationDataDto} setDataObject={setLdapOrganizationDataDto}/>
             </Col>
           </Row>
-          <Row>
-            <div className="ml-auto mt-3 px-3">
-              <SaveButton recordDto={ldapOrganizationDataDto} setRecordDto={setLdapOrganizationDataDto} setData={setLdapOrganizationData} createRecord={createOrganization} updateRecord={updateLdapOrganization} handleClose={handleClose}/>
-            </div>
-          </Row>
+        <PersistButtonContainer
+          handleClose={handleClose}
+          recordDto={ldapOrganizationDataDto}
+          setRecordDto={setLdapOrganizationDataDto}
+          updateRecord={updateLdapOrganization}
+          createRecord={createOrganization}
+          addAnotherOption={false}
+        />
       </EditorPanelContainer>
     );
 }
