4ec88d03194968ac8110a3e31363cbe281dbfa1e
Noah Champoux <noah@opsera.io>
2021-01-12 11:14:51 -0500

    Added ability to use lenient save button with persist button container.

--- src/components/common/buttons/saving/containers/PersistButtonContainer.jsx
+++ src/components/common/buttons/saving/containers/PersistButtonContainer.jsx
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 import SaveButtonContainer from "./SaveButtonContainer";
 import StrictSaveButton from "../StrictSaveButton";
 import CreateButton from "../CreateButton";
+import LenientSaveButton from "components/common/buttons/saving/LenientSaveButton";
 
-function PersistButtonContainer({ recordDto, setRecordDto, updateRecord, createRecord, handleClose, addAnotherOption, disable }) {
+function PersistButtonContainer({ recordDto, setRecordDto, updateRecord, createRecord, handleClose, addAnotherOption, disable, lenient }) {
   const getSaveButton = () => {
     if (recordDto.isNew()){
       return (
@@ -17,6 +18,10 @@ function PersistButtonContainer({ recordDto, setRecordDto, updateRecord, createR
           disable={disable}
         />
       );
+    }
+
+    if (lenient) {
+      return (<LenientSaveButton recordDto={recordDto} updateRecord={updateRecord} disable={disable} />)
     }
 
     return (<StrictSaveButton recordDto={recordDto} updateRecord={updateRecord} disable={disable} />);
@@ -36,7 +41,8 @@ PersistButtonContainer.propTypes = {
   setRecordDto: PropTypes.func,
   handleClose: PropTypes.func,
   disable: PropTypes.bool,
-  addAnotherOption: PropTypes.bool
+  addAnotherOption: PropTypes.bool,
+  lenient: PropTypes.bool
 };
 
 PersistButtonContainer.defaultProps = {
