550ef8bb2d5d36c127ef37c6613316d423409dba
Noah Champoux <noah@opsera.io>
2021-01-13 07:54:28 -0500

    Cleanup on Tool Identifier Summary Panel

--- src/components/admin/tools/tool_identifier/tool_identifier_detail_view/ToolIdentifierSummaryPanel.jsx
+++ src/components/admin/tools/tool_identifier/tool_identifier_detail_view/ToolIdentifierSummaryPanel.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 
-import "components/inventory/tools/tools.css";
 import TextFieldBase from "components/common/fields/text/TextFieldBase";
 import ActivityField from "components/common/fields/boolean/ActivityField";
 import BooleanField from "components/common/fields/boolean/BooleanField";
 import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
 import LoadingDialog from "components/common/status_notifications/loading";
-import DtoItemField from "components/common/form_fields/dto_form_fields/dto-item-field";
 import DtoPropertiesField from "components/common/form_fields/dto_form_fields/dto-properties-field";
-import DtoDateField from "components/common/form_fields/dto_form_fields/dto-date-field";
+import DateFieldBase from "components/common/fields/date/DateFieldBase";
+import GenericItemField from "components/common/fields/multiple_items/GenericItemField";
 
 function ToolIdentifierSummaryPanel({ toolIdentifierData, setActiveTab }) {
 
@@ -34,7 +33,7 @@ function ToolIdentifierSummaryPanel({ toolIdentifierData, setActiveTab }) {
           <TextFieldBase dataObject={toolIdentifierData} fieldName={"tool_type_identifier"}/>
         </Col>
         <Col lg={6}>
-          <DtoDateField dataObject={toolIdentifierData} fieldName={"createdAt"}/>
+          <DateFieldBase dataObject={toolIdentifierData} fieldName={"createdAt"}/>
         </Col>
         <Col lg={6}>
           <TextFieldBase dataObject={toolIdentifierData} fieldName={"_id"}/>
@@ -46,7 +45,7 @@ function ToolIdentifierSummaryPanel({ toolIdentifierData, setActiveTab }) {
           <TextFieldBase dataObject={toolIdentifierData} fieldName={"usageType"}/>
         </Col>
         <Col lg={12}>
-          <DtoItemField dataObject={toolIdentifierData} fieldName={"tags"}/>
+          <GenericItemField dataObject={toolIdentifierData} fieldName={"tags"}/>
         </Col>
         <Col lg={6}>
           <BooleanField dataObject={toolIdentifierData} fieldName={"enabledInRegistry"}/>
