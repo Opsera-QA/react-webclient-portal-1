e4503bd9d48ebd6b3655c2c329948e0d6fbecf36
Noah Champoux <noah@opsera.io>
2021-01-18 20:14:46 +0000

    OC-737 - Pipeline Workflow Catalog Filters

---
+++ src/components/workflow/catalog/catalog-filter-metadata.js
@@ -0,0 +1,77 @@
+const catalogFilterMetadata = {
+  idProperty: "_id",
+  type: "Pipeline Catalog",
+  fields: [
+    {
+      label: "Status",
+      id: "status",
+    },
+    {
+      label: "Tag",
+      id: "tag",
+    },
+    {
+      label: "Page Size",
+      id: "pageSize",
+    },
+    {
+      label: "Total Count",
+      id: "totalCount",
+    },
+    {
+      label: "Sort Option",
+      id: "sortOption",
+    },
+    {
+      label: "Pipeline Owner",
+      id: "owner"
+    },
+    {
+      label: "Search",
+      id: "search",
+    },
+    {
+      label: "Active Filters",
+      id: "activeFilters",
+    },
+    {
+      label: "Type",
+      id: "type",
+    },
+  ],
+  getActiveFilters(filterDto) {
+    let activeFilters = [];
+
+    if (filterDto.getData("tag") !== "") {
+      activeFilters.push({filterId: "tag", ...filterDto.getData("tag")});
+    }
+
+    if (filterDto.getData("type") !== "") {
+      activeFilters.push({filterId: "type", ...filterDto.getData("type")});
+    }
+
+    if (filterDto.getData("search") !== "") {
+      activeFilters.push({filterId: "search", text: `Keyword: ${filterDto.getData("search")}`});
+    }
+
+    return activeFilters;
+  },
+  newObjectFields: {
+    pageSize: 25,
+    currentPage: 1,
+    sortOption: { option: "name", text: "Template Name" },
+    search: "",
+    type: "",
+    tag: "",
+    activeFilters: []
+  },
+  sortOptions: [
+    { option: "newest", text: "Newest Templates"},
+    { option: "oldest", text: "Oldest Templates"},
+    { option: "name", text: "Template Name" },
+    { option: "lastupdated", text: "Last Updated"},
+    { option: "description", text: "Description"},
+  ]
+};
+
+export default catalogFilterMetadata;
