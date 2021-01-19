a107a81693ba2d9243e5daa4320d1425d07b1435
Noah Champoux <noah@opsera.io>
2021-01-19 13:49:11 -0500

    Created Temp Text Input for future enhancements to TextInputBase

---
+++ src/components/common/inputs/text/TempTextInput.jsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import PropTypes from "prop-types";
+import InputContainer from "components/common/inputs/InputContainer";
+import InputLabel from "components/common/inputs/info_text/InputLabel";
+import InfoText from "components/common/inputs/info_text/InfoText";
+
+// TODO: This is temporary for this sprint. It's a test of more dynamic validation for password/email fields, but should probably have the useEffect in the main textInputBase
+function TempTextInput({ fieldName, dataObject, setDataObject, disabled, type, inputPopover, inputClasses }) {
+  const [field, setField] = useState(dataObject.getFieldById(fieldName));
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (dataObject.getData(fieldName) !== "") {
+      setErrorMessage(dataObject.getFieldError(fieldName))
+    }
+  }, [dataObject]);
+
+  const validateAndSetData = (value) => {
+    let newDataObject = dataObject;
+    newDataObject.setTextData(fieldName, value);
+    setErrorMessage(newDataObject.getFieldError(fieldName));
+    setDataObject({...newDataObject});
+  };
+
+  const getInputClasses = () => {
+    let classes = `form-control`;
+
+    if (errorMessage !== "") {
+      classes += ` border border-danger error-text`;
+    }
+
+    if (inputClasses) {
+      classes += ` ${inputClasses}`;
+    }
+
+    return classes;
+  };
+
+  return (
+    <InputContainer>
+      <InputLabel field={field} inputPopover={inputPopover} />
+      <input
+        type={type}
+        disabled={disabled}
+        value={dataObject.getData(fieldName)}
+        onChange={(event) => validateAndSetData(event.target.value)}
+        className={getInputClasses()}
+      />
+      <InfoText field={field} errorMessage={errorMessage}/>
+    </InputContainer>
+  );
+}
+
+TempTextInput.propTypes = {
+  type: PropTypes.string,
+  fieldName: PropTypes.string,
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  inputPopover: PropTypes.object,
+  inputClasses: PropTypes.string,
+  disabled: PropTypes.bool
+};
+
+export default TempTextInput;
