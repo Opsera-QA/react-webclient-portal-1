83219487ec07c9efd160ce8dc7cf72318f843b2f
Noah Champoux <noah@opsera.io>
2021-01-20 14:52:00 -0500

    Wired up notification logs to actions file, cleanup

--- src/components/notifications/notification_details/activity_logs/NotificationActivityLogsTable.jsx
+++ src/components/notifications/notification_details/activity_logs/NotificationActivityLogsTable.jsx
@@ -12,6 +12,7 @@ import notificationActivityLogMetadata
   from "components/notifications/notification_details/activity_logs/notification-activity-log-metadata";
 import ModalActivityLogsDialog from "components/common/modal/modalActivityLogs";
 import DetailPanelContainer from "components/common/panels/detail_panel_container/DetailPanelContainer";
+import notificationsActions from "components/notifications/notifications-actions";
 
 function NotificationActivityLogsTable({ notificationData }) {
   let fields = notificationActivityLogMetadata.fields;
@@ -28,10 +29,9 @@ function NotificationActivityLogsTable({ notificationData }) {
     pageIndex: 0,
   };
 
-  const selectRow = (rowInfo) => {
-
-    console.log("rowINfo: " + JSON.stringify(rowInfo));
-    return getRowInfo(rowInfo);
+  const showActivityLog = (row) => {
+    setModalData(row);
+    setShowModal(true);
   };
 
   const columns = useMemo(
@@ -41,35 +41,31 @@ function NotificationActivityLogsTable({ notificationData }) {
       getTableTextColumn(fields.find(field => { return field.id === "message"})),
       getTableBooleanIconColumn(fields.find(field => { return field.id === "status"})),
       getTableDateColumn(fields.find(field => { return field.id === "createdAt"})),
-      getTableInfoIconColumn(selectRow),
+      getTableInfoIconColumn(showActivityLog),
     ],
     [],
   );
 
   // Executed every time page number or page size changes
   useEffect(() => {
-    getToolLog();
+    getNotificationLogs();
   }, [currentPage, pageSize]);
 
-  const getToolLog = async () => {
+  const getNotificationLogs = async () => {
     try {
       setIsLoading(true)
-      const accessToken = await getAccessToken();
-      const apiUrl = `/notifications/logs/${notificationData.getData("_id")}?page=${currentPage}&size=${pageSize}`;
-      const tool_logs = await axiosApiService(accessToken).get(apiUrl, {});
-      setLogCount(tool_logs.data.count);
-      setLogData(tool_logs.data.data);
-    } catch (err) {
-      console.log(err.message);
+      const notificationLogResponse = await notificationsActions.getNotificationActivityLogs(notificationData, currentPage, pageSize, getAccessToken);
+
+      if (notificationLogResponse?.data) {
+        setLogCount(notificationLogResponse.data.count);
+        setLogData(notificationLogResponse.data.data);
+      }
+    } catch (error) {
+      console.log(error.message);
     }
     finally {
       setIsLoading(false)
     }
-  };
-
-  const getRowInfo = (row) => {
-    setModalData(row);
-    setShowModal(true);
   };
 
   // TODO: Wire up new pagination
