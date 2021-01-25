61c05e17ac040a135138cc2f245df4633b9577ce
Noah Champoux <noah@opsera.io>
2021-01-25 16:42:56 -0500

    Updated form fields on unused SiteNotificationSummaryPanel

--- src/components/admin/site_notifications/site_notification_detail_view/SiteNotificationSummaryPanel.jsx
+++ src/components/admin/site_notifications/site_notification_detail_view/SiteNotificationSummaryPanel.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
-import "components/inventory/tools/tools.css";
-import DtoTextField from "../../../common/form_fields/dto_form_fields/dto-text-field";
-import DtoToggleField from "../../../common/form_fields/dto_form_fields/dto-toggle-field";
-import LoadingDialog from "../../../common/status_notifications/loading";
-import SummaryPanelContainer from "../../../common/panels/detail_view/SummaryPanelContainer";
-import DtoDateField from "../../../common/form_fields/dto_form_fields/dto-date-field";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import DateTimeField from "components/common/fields/date/DateTimeField";
+import LoadingDialog from "components/common/status_notifications/loading";
+import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
 
 function SiteNotificationSummaryPanel({ siteNotificationData, setActiveTab }) {
   if (siteNotificationData == null) {
@@ -18,26 +16,28 @@ function SiteNotificationSummaryPanel({ siteNotificationData, setActiveTab }) {
       <Row>
         <Col lg={6}>
           {/*TODO: Make type field that pretty prints it*/}
-          <DtoTextField dataObject={siteNotificationData} fieldName={"type"}/>
+          <TextFieldBase dataObject={siteNotificationData} fieldName={"type"}/>
         </Col>
         <Col lg={6}>
-          <DtoTextField dataObject={siteNotificationData} fieldName={"header"}/>
+          <TextFieldBase dataObject={siteNotificationData} fieldName={"header"}/>
         </Col>
         <Col lg={6}>
           {/*TODO: Make view field that pretty prints it*/}
-          <DtoTextField dataObject={siteNotificationData} fieldName={"view"}/>
+          <TextFieldBase dataObject={siteNotificationData} fieldName={"view"}/>
         </Col>
         <Col lg={6}>
           {/*TODO: Make FQDN Link Displayer*/}
-          <DtoTextField dataObject={siteNotificationData} fieldName={"link"}/>
+          <TextFieldBase dataObject={siteNotificationData} fieldName={"link"}/>
         </Col>
         <Col lg={6}>
-          {/*TODO: Make DateTime field*/}
-          <DtoDateField dataObject={siteNotificationData} fieldName={"expiration"}/>
+          <DateTimeField dataObject={siteNotificationData} fieldName={"displayOnDate"}/>
+        </Col>
+        <Col lg={6}>
+          <DateTimeField dataObject={siteNotificationData} fieldName={"expiration"}/>
         </Col>
         <Col lg={12}>
           {/*TODO: Make field to display what it will look like*/}
-          <DtoTextField dataObject={siteNotificationData} fieldName={"message"}/>
+          <TextFieldBase dataObject={siteNotificationData} fieldName={"message"}/>
         </Col>
       </Row>
     </SummaryPanelContainer>
