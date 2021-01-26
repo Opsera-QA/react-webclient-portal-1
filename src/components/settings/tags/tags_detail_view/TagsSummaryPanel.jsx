1ae45dae092a7620d12ab0ba5791f95ae234a752
Noah Champoux <noah@opsera.io>
2021-01-26 13:57:35 -0500

    Wired up ConfigurationField on TagsSummaryPanel, styled configuration input to match container colors

--- src/components/settings/tags/tags_detail_view/TagsSummaryPanel.jsx
+++ src/components/settings/tags/tags_detail_view/TagsSummaryPanel.jsx
@@ -7,23 +7,8 @@ import DateFieldBase from "components/common/fields/date/DateFieldBase";
 import LoadingDialog from "components/common/status_notifications/loading";
 import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
 import ConfigurationField from "components/common/fields/multiple_items/ConfigurationField";
-import NameValueTable from "components/common/table/nameValueTable";
 
 function TagsSummaryPanel({ tagData, setActiveTab }) {
-  const parseNameValueArray = (nameValueArray) => {
-    let parsedValues = [];
-
-    if (nameValueArray != null) {
-      for (const key of Object.keys(nameValueArray)) {
-        if (key != null) {
-          parsedValues.push({ name: key, value: nameValueArray[key] });
-        }
-      }
-    }
-
-    return parsedValues;
-  };
-
   if (tagData == null) {
     return <LoadingDialog size="sm" />;
   }
@@ -49,17 +34,8 @@ function TagsSummaryPanel({ tagData, setActiveTab }) {
         <Col lg={6}>
           <ActivityField dataObject={tagData} fieldName={"active"}/>
         </Col>
-        {/*<Col lg={12}>*/}
-        {/*  <ConfigurationField dataObject={tagData} fieldName={"configuration"} />*/}
-        {/*</Col>*/}
-      </Row>
-      <Row>
-        <Col>
-          <div className="mt-3">
-            <NameValueTable label="Configuration"
-                            data={parseNameValueArray(tagData.configuration)}
-                            noDataMessage="No configurations are assigned to this tag."/>
-          </div>
+        <Col lg={12}>
+          <ConfigurationField dataObject={tagData} fieldName={"configuration"} />
         </Col>
       </Row>
     </SummaryPanelContainer>
