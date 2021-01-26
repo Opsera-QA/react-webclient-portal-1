5431e255b687f31c2dd8ab1b73cc71edc2352c3f
Noah Champoux <noah@opsera.io>
2021-01-26 13:42:30 -0500

    Changed color of active default-custom-tab to match header, cleanup

--- src/components/common/tabs/CustomTab.jsx
+++ src/components/common/tabs/CustomTab.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from "react";
+import React  from "react";
 import PropTypes from "prop-types";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-function CustomTab({ activeTab, tabName, tabText, handleTabClick, icon }) {
+function CustomTab({activeTab, tabName, tabText, handleTabClick, icon}) {
   return (
-    <>
-      <li className="nav-item mr-1">
-        <a className={"nav-link " + (activeTab === tabName ? "active" : "")} onClick={handleTabClick(tabName)}
-           href="#"><FontAwesomeIcon icon={icon} fixedWidth/><span className="ml-2 d-none d-lg-inline">{tabText}</span></a>
-      </li>
-    </>
+    <li className="nav-item mr-1">
+      <a className={"nav-link " + (activeTab === tabName ? "active" : "")} onClick={handleTabClick(tabName)} href="#">
+        <FontAwesomeIcon icon={icon} fixedWidth/><span className="ml-2 d-none d-lg-inline">{tabText}</span>
+      </a>
+    </li>
   );
 }
 
