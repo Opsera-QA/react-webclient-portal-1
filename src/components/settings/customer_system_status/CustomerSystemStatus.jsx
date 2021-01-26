996a540addd83b772667e75ffaa1f20a5f66d402
Noah Champoux <noah@opsera.io>
2021-01-26 12:42:33 -0500

    Wired up ScreenContainer on Customer System Status screen

--- src/components/settings/customer_system_status/CustomerSystemStatus.jsx
+++ src/components/settings/customer_system_status/CustomerSystemStatus.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useContext, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { AuthContext } from "contexts/AuthContext";
 import LoadingDialog from "components/common/status_notifications/loading";
-import {DialogToastContext} from "../../../contexts/DialogToastContext";
-import customerSystemStatusActions from "./customer-system-status-actions";
-import AccessDeniedDialog from "../../common/status_notifications/accessDeniedInfo";
-import BreadcrumbTrail from "../../common/navigation/breadcrumbTrail";
-import SystemStatusCard from "./SystemStatusCard";
-import StatusLegend from "../../common/status/StatusLegend";
+import ScreenContainer from "components/common/panels/general/ScreenContainer";
+import {DialogToastContext} from "contexts/DialogToastContext";
+import customerSystemStatusActions from "components/settings/customer_system_status/customer-system-status-actions";
+import StatusLegend from "components/common/status/StatusLegend";
+import SystemStatusCard from "components/admin/status/SystemStatusCard";
 
 function CustomerSystemStatus() {
   const toastContext = useContext(DialogToastContext);
@@ -40,31 +39,47 @@ function CustomerSystemStatus() {
 
     if (userRoleAccess) {
       setAccessRoleData(userRoleAccess);
-    }
 
-    await getSystemStatuses();
+      if (userRoleAccess?.PowerUser || userRoleAccess?.Administrator || userRoleAccess?.OpseraAdministrator) {
+        await getSystemStatuses();
+      }
+    }
   };
 
   const getSystemStatuses = async () => {
     const response = await customerSystemStatusActions.getSystemStatuses(getAccessToken);
-    let data = response.data ? response.data : [];
+    let data = response?.data ? response.data : [];
     setCustomerSystemStatusData(data);
   }
 
-  if (isLoading) {
+  const getBody = () => {
+    // TODO: When wiring up getBody, style
+    if (customerSystemStatusData == null || customerSystemStatusData.length === 0) {
+      return (<div className="p-5">Could not pull system statuses</div>);
+    }
+
+    return (
+      customerSystemStatusData.map((customerSystemStatus, key) => (
+          <Col key={key} md={3} sm={6}>
+            <SystemStatusCard systemStatus={customerSystemStatus}/>
+          </Col>
+        )
+      )
+    );
+  };
+
+  if (!accessRoleData) {
     return (<LoadingDialog />);
   }
 
-  if (!accessRoleData.PowerUser && !accessRoleData.Administrator && !accessRoleData.OpseraAdministrator) {
-    return (<AccessDeniedDialog roleData={accessRoleData}/>);
-  }
-
   return (
-    <div className="mt-3 max-content-width">
-      <BreadcrumbTrail destination="customerSystemStatus" />
-      <h5>Customer System Status</h5>
-      <div>Listed below are Customer tools for Opsera.</div>
-      <Row>
+    <ScreenContainer
+      accessDenied={!accessRoleData.PowerUser && !accessRoleData.Administrator && !accessRoleData.OpseraAdministrator}
+      breadcrumbDestination={"customerSystemStatus"}
+      pageDescription={`View the current system status for Customer Tools`}
+      isLoading={isLoading}
+    >
+      <Row className="p-3">
         <Col sm={12} className="p-0">
           <Row>
             <Col sm={6}/>
@@ -74,6 +89,7 @@ function CustomerSystemStatus() {
           </Row>
         </Col>
         <Col sm={12} className="status-table p-3">
+          {/*TODO: Wire up getBody when query is properly working*/}
           <Row>
             {customerSystemStatusData && customerSystemStatusData.map((customerSystemStatus, key) => (
               <Col key={key} md={3} sm={6}>
@@ -84,7 +100,7 @@ function CustomerSystemStatus() {
           </Row>
         </Col>
       </Row>
-    </div>
+    </ScreenContainer>
   );
 }
 
