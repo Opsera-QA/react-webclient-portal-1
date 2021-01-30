d3e3c873d4090d56abd3ff1d17ab16ade01a2a6f
Todd Barczak <todd@opsera.io>
2021-01-30 08:47:51 -0500

    ensure comparison of emails is always case insensitive

--- src/components/workflow/pipelines/pipeline_details/workflow/workflow-authorized-actions.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/workflow-authorized-actions.js
@@ -123,7 +123,7 @@ const calculateUserObjectRole = (userEmail, userGroups, objectRoles) => {
 
   //filter out only user records (groups null)
   const userRoles = objectRoles.filter(function(item) {
-    return item.user && item.user === userEmail;
+    return item.user && item.user.toLowerCase() === userEmail.toLowerCase();
   });
 
   if (userRoles.length > 0) {
