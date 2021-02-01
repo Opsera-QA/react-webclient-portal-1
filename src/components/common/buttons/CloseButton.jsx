d78d38bad9dcaca5cf57f92c88dc48c4afeaee2c
Noah Champoux <noah@opsera.io>
2021-02-01 18:34:39 -0500

    Added Unsaved Changes message to close button

--- src/components/common/buttons/CloseButton.jsx
+++ src/components/common/buttons/CloseButton.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button } from "react-bootstrap";
+import {Button} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import TooltipWrapper from "components/common/tooltip/tooltipWrapper";
+import {unsavedChanges} from "components/common/tooltip/popover-text";
 
 function CloseButton({ isLoading, closeEditorCallback }) {
-
-  // TODO: Add unsaved changes popover
   return (
     <div className="mx-1">
-      <Button size="md" variant="secondary" disabled={isLoading} onClick={() => closeEditorCallback()}>
-        <span><FontAwesomeIcon icon={faTimes} className="mr-1" fixedWidth/>Close</span>
-      </Button>
+      <TooltipWrapper innerText={unsavedChanges}>
+        <Button size="md" variant="secondary" disabled={isLoading} onClick={() => closeEditorCallback()}>
+          <span><FontAwesomeIcon icon={faTimes} className="mr-1" fixedWidth/>Close</span>
+        </Button>
+      </TooltipWrapper>
     </div>
   );
 }
--- src/components/common/tooltip/popover-text.js
+++ src/components/common/tooltip/popover-text.js
@@ -1,2 +1,3 @@
 export const unsavedChanges = "All unsaved changes will be lost";
 export const cannotBeUndone = "This action can not be undone";
+export const tabAccessRestricted = "This tab is disabled due to access limitations";
