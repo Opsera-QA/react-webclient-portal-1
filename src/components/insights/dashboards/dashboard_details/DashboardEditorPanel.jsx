44f036e8a9ceb40bd257008aa960ea39345ca77a
Noah Champoux <noah@opsera.io>
2021-02-02 07:42:05 -0500

    Cleaned up DashboardEditorPanel, wired up new components

--- src/components/insights/dashboards/dashboard_details/DashboardEditorPanel.jsx
+++ src/components/insights/dashboards/dashboard_details/DashboardEditorPanel.jsx
@@ -2,14 +2,15 @@ import React, {useState, useContext, useEffect} from "react";
 import PropTypes from "prop-types";
 import { AuthContext } from "contexts/AuthContext";
 import Row from "react-bootstrap/Row";
-import dashboardsActions from "../dashboards-actions";
-import DtoTextInput from "../../../common/input/dto_input/dto-text-input";
-import DtoToggleInput from "../../../common/input/dto_input/dto-toggle-input";
 import Col from "react-bootstrap/Col";
-import EditorPanelContainer from "../../../common/panels/detail_panel_container/EditorPanelContainer";
-import DashboardTypeSelectInput from "../../../common/list_of_values_input/insights/dashboards/DashboardTypeSelectInput";
-import DashboardAccessSelectInput from "../../../common/list_of_values_input/insights/dashboards/DashboardAccessSelectInput";
-import PersistButtonContainer from "components/common/buttons/saving/containers/PersistButtonContainer";
+import dashboardsActions from "components/insights/dashboards/dashboards-actions";
+import EditorPanelContainer from "components/common/panels/detail_panel_container/EditorPanelContainer";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
+import DashboardTypeSelectInput
+  from "components/common/list_of_values_input/insights/dashboards/DashboardTypeSelectInput";
+import DashboardAccessSelectInput
+  from "components/common/list_of_values_input/insights/dashboards/DashboardAccessSelectInput";
+import ActivityToggleInput from "components/common/inputs/boolean/ActivityToggleInput";
 
 function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) {
   const { getAccessToken } = useContext(AuthContext);
@@ -35,33 +36,33 @@ function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) 
   };
 
   return (
-    <EditorPanelContainer isLoading={isLoading}>
+    <EditorPanelContainer
+      isLoading={isLoading}
+      recordDto={dashboardDataDto}
+      handleClose={handleClose}
+      setRecordDto={setDashboardDataDto}
+      createRecord={createDashboard}
+      updateRecord={updateDashboard}
+      addAnotherOption={false}
+    >
       <div className="mx-2">
         <Row>
           <Col md={6}>
-            <DtoTextInput fieldName={"name"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+            <TextInputBase fieldName={"name"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
           </Col>
           <Col md={6}>
             <DashboardTypeSelectInput dataObject={dashboardDataDto} setDataObject={setDashboardDataDto}/>
           </Col>
           <Col md={12}>
-            <DtoTextInput fieldName={"description"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+            <TextInputBase fieldName={"description"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
           </Col>
           <Col md={6}>
             <DashboardAccessSelectInput dataObject={dashboardDataDto} setDataObject={setDashboardDataDto} disabled={["public"]}/>
           </Col>
           <Col md={3}>
-            <DtoToggleInput fieldName={"active"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+            <ActivityToggleInput fieldName={"active"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
           </Col>
         </Row>
-        <PersistButtonContainer
-          recordDto={dashboardDataDto}
-          handleClose={handleClose}
-          setRecordDto={setDashboardDataDto}
-          createRecord={createDashboard}
-          updateRecord={updateDashboard}
-          addAnotherOption={false}
-        />
       </div>
     </EditorPanelContainer>
   );
