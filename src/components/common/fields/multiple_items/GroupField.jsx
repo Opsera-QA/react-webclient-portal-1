a83b1cf01ea4dd5bff546284e4fd960409add039
Noah Champoux <noah@opsera.io>
2021-02-03 10:25:00 -0500

    Added Group Field for displaying groups

---
+++ src/components/common/fields/multiple_items/GroupField.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import FieldContainer from "components/common/fields/FieldContainer";
+import FieldLabel from "components/common/fields/FieldLabel";
+import {faUserFriends} from "@fortawesome/pro-light-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function GroupField({dataObject, fieldName}) {
+  const [field] = useState(dataObject.getFieldById(fieldName));
+
+  const getGroups = () => {
+    const groups = dataObject.getData(fieldName);
+
+    if (!groups || !Array.isArray(groups) || groups.length === 0) {
+      return <span>User is not a member of any groups.</span>
+    }
+
+    return (
+      groups.map((group, i) => {
+        return (
+          <span key={i} className="mx-1 mb-1 badge badge-light group-badge">
+            <FontAwesomeIcon icon={faUserFriends} fixedWidth className="mr-1"/>{`${group}`}
+          </span>
+        );
+      })
+    );
+  };
+
+  return (
+    <FieldContainer>
+      <FieldLabel fieldName={fieldName} field={field}/>
+      <span className="item-field">
+        {getGroups()}
+      </span>
+    </FieldContainer>
+  );
+}
+
+GroupField.propTypes = {
+  fieldName: PropTypes.string,
+  dataObject: PropTypes.object,
+};
+
+export default GroupField;
