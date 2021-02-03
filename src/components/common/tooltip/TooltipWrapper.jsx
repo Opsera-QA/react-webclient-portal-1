6435152bb64689cdfaa28b94c4f4ec10fe0e96ec
Noah Champoux <noah@opsera.io>
2021-02-03 12:57:48 -0500

    Removed duplicate file

--- src/components/common/tooltip/TooltipWrapper.jsx
+++ src/components/common/tooltip/TooltipWrapper.jsx
@@ -2,30 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import {OverlayTrigger, Popover} from "react-bootstrap";
 
-function TooltipWrapper({ innerText, children }) {
-  
+function TooltipWrapper({ innerText, placement, children }) {
+
   const getPopover = (innerText) => {
     return (
-    <Popover id="popover-basic">
-      <Popover.Content>
-        {innerText}
-      </Popover.Content>
-    </Popover>
+      <Popover id="popover-basic">
+        <Popover.Content>
+          {innerText}
+        </Popover.Content>
+      </Popover>
     );
   };
 
   return (
-    <>
-      <OverlayTrigger trigger={["hover", "hover"]} placement="top" overlay={getPopover(innerText)}>
-        {children}
-      </OverlayTrigger>
-    </>
+    <OverlayTrigger trigger={["hover", "hover"]} placement={placement} overlay={getPopover(innerText)}>
+      {children}
+    </OverlayTrigger>
   );
 }
 
 TooltipWrapper.propTypes = {
   innerText: PropTypes.string,
-  children: PropTypes.any
+  children: PropTypes.any,
+  placement: PropTypes.string
+};
+
+TooltipWrapper.defaultProps = {
+  placement: "top"
 };
 
 export default TooltipWrapper;
