22721df9dab8ad671bdad4ace623d98b7685e295
faseeh <faseeh@collaaj.com>
2021-02-05 09:53:30 +0530

    updated the jenkins agent values

--- src/components/common/list_of_values_input/workflow/pipelines/AgentLabelsMultiSelectInput.jsx
+++ src/components/common/list_of_values_input/workflow/pipelines/AgentLabelsMultiSelectInput.jsx
@@ -7,16 +7,21 @@ import SelectInputBase from "components/common/inputs/select/SelectInputBase";
 // TODO:  Check with mahantha to get the actual agent label values and update it here
 // TODO : un-comment when this feature is pushed - dependency ticket -KI-150 
 export const jenkinsAgentArray = [
-  // {
-  //   "name": "Windows Agent",
-  //   "env" : "windows",
-  //   "agentLabel": "exampleWindowsAgentLabel",
-  // },
-  // {
-  //   "name": "Linux Agen",
-  //   "env" : "linux",
-  //   "agentLabel": "exampleLinuxAgentLabel",
-  // }
+  {
+    "name": "Opsera Agent",
+    "env" : "linux",
+    "agentLabel": "opsera",
+  },
+  {
+    "name": "Node Agent",
+    "env" : "linux",
+    "agentLabel": "nodejs",
+  },
+  {
+    "name": "Maven Agent",
+    "env" : "linux",
+    "agentLabel": "maven",
+  }
 ];
 
 function AgentLabelsMultiSelectInput({ fieldName, dataObject, setDataObject, setDataFunction, disabled }) {
