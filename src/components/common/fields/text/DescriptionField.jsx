196e39bb78965b035d48ac072c060c55a0732181
Noah Champoux <noah@opsera.io>
2021-02-05 07:55:18 -0500

    Added Character Limit to Description Field

--- src/components/common/fields/text/DescriptionField.jsx
+++ src/components/common/fields/text/DescriptionField.jsx
@@ -6,11 +6,15 @@ function DescriptionField({dataObject, fieldName, characterLimit}) {
   const getParsedText = () => {
     let description = dataObject.getData(fieldName);
 
+    if (description != null && description.length > characterLimit) {
+      description = `${description.substring(0, characterLimit)}...`;
+    }
+
     return description;
   };
 
   return (
-    <FieldContainer>
+    <FieldContainer className="mt-0">
       <span>{getParsedText()}</span>
     </FieldContainer>
   );
@@ -20,6 +24,10 @@ DescriptionField.propTypes = {
   fieldName: PropTypes.string,
   dataObject: PropTypes.object,
   characterLimit: PropTypes.number
+};
+
+DescriptionField.defaultProps = {
+  characterLimit: 150
 };
 
 export default DescriptionField;
