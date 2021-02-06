4ad8bae5fccac5dd790a773aa5aa5cab468dbea7
faseeh <faseeh@collaaj.com>
2021-02-06 23:20:04 +0530

    commented out the test values for jenkins agent array

--- src/components/common/list_of_values_input/workflow/pipelines/AgentLabelsMultiSelectInput.jsx
+++ src/components/common/list_of_values_input/workflow/pipelines/AgentLabelsMultiSelectInput.jsx
@@ -6,7 +6,7 @@ import SelectInputBase from "components/common/inputs/select/SelectInputBase";
 
 // TODO:  Check with mahantha to get the actual agent label values and update it here
 // TODO : un-comment when this feature is pushed - dependency ticket -KI-150 
-export const jenkinsAgentArray = [
+export const jenkinsAgentArray = [];
   // {
   //   "name": "Opsera Agent",
   //   "env" : "linux",
@@ -22,7 +22,7 @@ export const jenkinsAgentArray = [
   //   "env" : "linux",
   //   "agentLabel": "maven",
   // }
-];
+
 
 function AgentLabelsMultiSelectInput({ fieldName, dataObject, setDataObject, setDataFunction, disabled }) {
   const toastContext = useContext(DialogToastContext);
