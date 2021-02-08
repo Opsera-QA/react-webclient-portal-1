5c65e32ec58d678c3de0c95dc7cc57a7760f613c
Noah Champoux <noah@opsera.io>
2021-02-08 08:18:38 -0500

    Cleaned up StatusFilter

--- src/components/common/filters/status/StatusFilter.jsx
+++ src/components/common/filters/status/StatusFilter.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
-import DtoFilterSelectInput from "../input/DtoFilterSelectInput";
+import FilterSelectInputBase from "components/common/filters/input/FilterSelectInputBase";
 
-function StatusFilter({ filterDto, setFilterDto}) {
-  const statusOptions = [{text: "Status: All", value: undefined}, {text: "Status: Active", value: "active"}, {text: "Status: Inactive", value: "inactive"} ];
+const statusOptions = [
+  {text: "Status: All", value: undefined},
+  {text: "Status: Active", value: "active"},
+  {text: "Status: Inactive", value: "inactive"}
+];
+
+function StatusFilter({ filterDto, setFilterDto, className}) {
+  if (filterDto == null) {
+    return null;
+  }
 
   return (
-    <div><DtoFilterSelectInput fieldName={"status"} setDataObject={setFilterDto} dataObject={filterDto} selectOptions={statusOptions} /></div>
+    <div className={className}>
+      <FilterSelectInputBase
+        fieldName={"status"}
+        setDataObject={setFilterDto}
+        dataObject={filterDto}
+        selectOptions={statusOptions}
+      />
+    </div>
   );
 }
-
 
 StatusFilter.propTypes = {
   filterDto: PropTypes.object,
   setFilterDto: PropTypes.func,
+  className: PropTypes.string
 };
 
 export default StatusFilter;
