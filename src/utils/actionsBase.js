7036b5955d3271f0aa3bb86cc9d214b80f0b178e
Noah Champoux <noah@opsera.io>
2021-02-11 08:52:09 -0500

    Implemented apiServiceV2

--- src/utils/actionsBase.js
+++ src/utils/actionsBase.js
@@ -1,4 +1,11 @@
 import {axiosApiService} from "api/apiService";
+import {
+  axiosApiDeleteCall,
+  axiosApiGetCall,
+  axiosApiPatchCall,
+  axiosApiPostCall,
+  axiosApiPutCall
+} from "api/apiServiceV2";
 
 const baseActions = {};
 
@@ -7,6 +14,10 @@ baseActions.apiGetCall = async (getAccessToken, apiUrl, urlParams) => {
   return axiosApiService(accessToken).get(apiUrl, urlParams)
     .then((result) =>  {return result;})
     .catch(error => {throw { error };});
+};
+
+baseActions.apiGetCallV2 = async (getAccessToken, sourceToken, apiUrl, urlParams) => {
+  return await axiosApiGetCall(getAccessToken, sourceToken, apiUrl, urlParams);
 };
 
 baseActions.customTokenApiGetCall = async (customToken, apiUrl, urlParams) => {
@@ -28,11 +39,25 @@ baseActions.apiPostCall = async (getAccessToken, apiUrl, postBody = {}) => {
     .catch(error => {throw { error };});
 };
 
+baseActions.apiPostCallV2 = async (getAccessToken, sourceToken, apiUrl, postBody = {}) => {
+  return await axiosApiPostCall(getAccessToken, sourceToken, apiUrl, postBody);
+};
+
 baseActions.apiPutCall = async (getAccessToken, apiUrl, postBody = {}) => {
   const accessToken = await getAccessToken();
   return axiosApiService(accessToken).put(apiUrl, postBody)
     .then((result) =>  {return result;})
     .catch(error => {throw { error };});
+};
+
+// TODO: Wire up and test
+baseActions.apiPutCallV2 = async (getAccessToken, sourceToken, apiUrl, postBody = {}) => {
+  return await axiosApiPutCall(getAccessToken, sourceToken, apiUrl, postBody);
+};
+
+// TODO: Wire up and test
+baseActions.apiPatchCallV2 = async (getAccessToken, sourceToken, apiUrl, postBody = {}) => {
+  return await axiosApiPatchCall(getAccessToken, sourceToken, apiUrl, postBody);
 };
 
 baseActions.apiDeleteCall = async (getAccessToken, apiUrl) => {
@@ -41,6 +66,12 @@ baseActions.apiDeleteCall = async (getAccessToken, apiUrl) => {
     .then((result) =>  {return result;})
     .catch(error => {throw { error };});
 };
+
+// TODO: Wire up and test
+baseActions.apiDeleteCallV2 = async (getAccessToken, sourceToken, apiUrl) => {
+  return await axiosApiDeleteCall(getAccessToken, sourceToken, apiUrl);
+};
+
 
 
 export default baseActions;
--- src/utils/regexHelpers.js
+++ src/utils/regexHelpers.js
@@ -4,6 +4,7 @@ regexHelpers.regexTypes = {
   // TODO: #1 will be standard across most of the text fields, so once it's wired up it will not need to be included in metadata
   generalText: /^[A-Za-z0-9-_.:]*$/, // Letters, numbers, dashes, colons, underscores, and periods allowed
   generalTextWithSpaces: /^[A-Za-z0-9'-_.: ]*$/, // Letters, numbers, spaces, dashes, apostrophes, colons, underscores, and periods allowed
+  email: /^[A-Za-z0-9'-_.@]*$/, // Letters, numbers, dashes, apostrophes, colons, underscores, @, and periods allowed
   alphanumeric: /^[A-Za-z0-9]*$/, // Letters, numbers
   alphanumericPlusSpaces: /^[A-Za-z0-9 ]*$/, // Letters, numbers and spaces allowed
 };
