da91b1821e1869d1ec34db2e542555cb6c5d65c5
Noah Champoux <noah@opsera.io>
2021-02-12 07:26:39 -0500

    Added ability to set size on create button

--- src/components/common/buttons/saving/CreateButton.jsx
+++ src/components/common/buttons/saving/CreateButton.jsx
@@ -12,7 +12,7 @@ import {
 import {useHistory} from "react-router-dom";
 import {DialogToastContext} from "contexts/DialogToastContext";
 
-function CreateButton({recordDto, createRecord, disable, showSuccessToasts, lenient, setRecordDto, addAnotherOption, handleClose}) {
+function CreateButton({recordDto, createRecord, disable, showSuccessToasts, lenient, setRecordDto, addAnotherOption, handleClose, size}) {
   const [isSaving, setIsSaving] = useState(false);
   const [addAnother, setAddAnother] = useState(false);
   const history = useHistory();
@@ -68,7 +68,7 @@ function CreateButton({recordDto, createRecord, disable, showSuccessToasts, leni
     <div className="px-2 d-flex">
       {getAddAnotherCheckbox()}
       {/*TODO: Make sure button is not clickable until form is valid*/}
-      <Button size="sm" variant="primary" disabled={isSaving || disable} onClick={() => persistRecord()}>
+      <Button size={size} variant="primary" disabled={isSaving || disable} onClick={() => persistRecord()}>
         {getLabel()}
       </Button>
     </div>
@@ -83,12 +83,14 @@ CreateButton.propTypes = {
   showSuccessToasts: PropTypes.bool,
   handleClose: PropTypes.func,
   lenient: PropTypes.bool,
-  addAnotherOption: PropTypes.bool
+  addAnotherOption: PropTypes.bool,
+  size: PropTypes.string
 };
 
 CreateButton.defaultProps = {
   showSuccessToasts: true,
-  addAnotherOption: true
+  addAnotherOption: true,
+  size: "sm"
 }
 
 export default CreateButton;
