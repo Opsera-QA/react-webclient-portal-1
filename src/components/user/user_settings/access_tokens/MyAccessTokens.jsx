f9d98a0854f9c69355e3c6d55e49a043e738447a
Noah Champoux <noah@opsera.io>
2021-02-14 13:51:39 -0500

    Wired up Token Activity Log on AccessTokens screen

--- src/components/user/user_settings/access_tokens/MyAccessTokens.jsx
+++ src/components/user/user_settings/access_tokens/MyAccessTokens.jsx
@@ -5,6 +5,13 @@ import axios from "axios";
 import tokenActions from "components/user/user_settings/access_tokens/token-actions";
 import AccessTokenTable from "components/user/user_settings/access_tokens/AccessTokenTable";
 import AccessTokenEditorPanel from "components/user/user_settings/access_tokens/details/AccessTokenEditorPanel";
+import TabPanelContainer from "components/common/panels/general/TabPanelContainer";
+import CustomTabContainer from "components/common/tabs/CustomTabContainer";
+import CustomTab from "components/common/tabs/CustomTab";
+import {faTable} from "@fortawesome/pro-light-svg-icons";
+import SummaryTab from "components/common/tabs/detail_view/SummaryTab";
+import AccessTokenLogPanel from "components/user/user_settings/access_tokens/details/logs/AccessTokenLogPanel";
+import DetailPanelContainer from "components/common/panels/detail_panel_container/DetailPanelContainer";
 
 function MyAccessTokens() {
   const toastContext = useContext(DialogToastContext);
@@ -14,6 +21,12 @@ function MyAccessTokens() {
   const [accessTokens, setAccessTokens] = useState([]);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [activeTab, setActiveTab] = useState("summary");
+
+  const handleTabClick = (tabSelection) => e => {
+    e.preventDefault();
+    setActiveTab(tabSelection);
+  };
 
   useEffect(() => {
     if (cancelTokenSource) {
@@ -76,12 +89,34 @@ function MyAccessTokens() {
     }
   };
 
-  return (
-    <div>
-      <div className="mb-3 mx-2"><AccessTokenEditorPanel cancelTokenSource={cancelTokenSource} /></div>
-      <AccessTokenTable loadData={loadData} isLoading={isLoading} data={accessTokens} isMounted={isMounted} cancelTokenSource={cancelTokenSource} />
-    </div>
-  );
+  const getTabContainer = () => {
+    return (
+      <CustomTabContainer>
+        <SummaryTab activeTab={activeTab} handleTabClick={handleTabClick} />
+        <CustomTab activeTab={activeTab} tabText={"Token Activity Log"} handleTabClick={handleTabClick} tabName={"logs"} icon={faTable}/>
+      </CustomTabContainer>
+    );
+  };
+
+  const getCurrentView = () => {
+    switch (activeTab) {
+      case "summary":
+        return (
+          <>
+            <div className="mb-3 mx-2">
+              <AccessTokenEditorPanel cancelTokenSource={cancelTokenSource} />
+            </div>
+            <AccessTokenTable loadData={loadData} isLoading={isLoading} data={accessTokens} isMounted={isMounted} cancelTokenSource={cancelTokenSource} />
+          </>
+        );
+      case "logs":
+        return <AccessTokenLogPanel />;
+      default:
+        return null;
+    }
+  };
+
+  return (<div className="px-3"><TabPanelContainer currentView={getCurrentView()} tabContainer={getTabContainer()} /></div>);
 }
 
 export default MyAccessTokens;
