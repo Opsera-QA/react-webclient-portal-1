f38b1ef9e85e1c6e86430e257b632d9897b26cd6
Noah Champoux <noah@opsera.io>
2021-02-17 14:45:16 -0500

    Increased minimum width of Table/Card View Results summary

--- src/components/common/pagination/pagination-helpers.js
+++ src/components/common/pagination/pagination-helpers.js
@@ -9,5 +9,5 @@ export const getResultSummary = (paginationDto, isLoading) => {
   const lowerResultsViewLimit = ((paginationDto.getData("currentPage") - 1) * paginationDto.getData("pageSize")) + 1;
   const upperResultsViewLimit = (lowerResultsViewLimit + paginationDto.getData("pageSize")) - 1;
   let countOffsetUpper = (upperResultsViewLimit < paginationDto.getData("totalCount")) ? upperResultsViewLimit : paginationDto.getData("totalCount");
-  return (<div><span>Results {lowerResultsViewLimit} - {countOffsetUpper} of {paginationDto.getData("totalCount")}</span></div>);
+  return (<div className="text-nowrap"><span>{`Results ${lowerResultsViewLimit} - ${countOffsetUpper} of ${paginationDto?.getData("totalCount")}`}</span></div>);
 };
