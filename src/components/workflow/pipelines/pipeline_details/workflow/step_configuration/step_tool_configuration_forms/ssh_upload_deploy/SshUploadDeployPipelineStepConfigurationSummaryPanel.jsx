8ff5919090ecba34130445c80e38f3127013e2d7
Noah Champoux <noah@opsera.io>
2021-02-18 13:05:04 -0500

    Created SshUploadDeployPipelineStepConfigurationSummaryPanel (to be wired up after 2/20 release)

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ssh_upload_deploy/SshUploadDeployPipelineStepConfigurationSummaryPanel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import PropTypes from "prop-types";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import PipelineStepSummaryPanelContainer
+  from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepSummaryPanelContainer";
+
+function SshUploadDeployPipelineStepConfigurationSummaryPanel({ sshUploadDeployDataObject, pipelineData, setActiveTab }) {
+  return (
+    <PipelineStepSummaryPanelContainer setActiveTab={setActiveTab} pipelineData={pipelineData}>
+      <Row>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"toolConfigId"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"userName"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"serverIp"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"serverPath"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"sshAction"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={sshUploadDeployDataObject} fieldName={"commands"} />
+        </Col>
+      </Row>
+    </PipelineStepSummaryPanelContainer>
+  );
+}
+
+SshUploadDeployPipelineStepConfigurationSummaryPanel.propTypes = {
+  sshUploadDeployDataObject: PropTypes.object,
+  pipelineData: PropTypes.object,
+  setActiveTab: PropTypes.func
+};
+
+export default SshUploadDeployPipelineStepConfigurationSummaryPanel;
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ssh_upload_deploy/sshUploadDeployPipelineStepConfigurationMetadata.js
@@ -0,0 +1,62 @@
+const sshUploadDeployPipelineStepConfigurationMetadata = {
+  type: "SFDC Pipeline Step Configuration",
+  fields: [
+    {
+      label: "Security Key",
+      id: "fileName",
+      isRequired: true,
+      formText: "Attach the PEM/CER key file needed for accessing the EC2 instance.  This data will be secured in our Vault."
+    },
+    {
+      label: "User Name",
+      id: "userName",
+      maxLength: 150,
+      formText: "Username needed to access the EC2 instance."
+    },
+    {
+      label: "Server Address",
+      id: "serverIp",
+      maxLength: 75,
+      formText: "DNS or IP Address for the server."
+    },
+    {
+      label: "Server Path",
+      id: "serverPath",
+      maxLength: 250,
+      isRequired: true
+    },
+    {
+      label: "SSH Action",
+      id: "sshAction",
+      isRequired: true
+    },
+    {
+      label: "SSH Commands",
+      id: "commands",
+    },
+    {
+      label: "Tool",
+      id: "toolConfigId",
+      isRequired: true
+    },
+  ],
+  newModelBase: {
+    // accessKey: "",
+    // secretKey: "",
+    sshKey: {}, //file stream to value
+    userName: "",
+    serverIp: "",
+    serverPath: "",
+    commands: "",
+    sshAction: "SSH Execution", // default it's ssh execution not empty anymore
+    // jenkins details if SSHaction is upload
+    toolConfigId: "",
+    jenkinsUrl: "",
+    jenkinsPort: "",
+    jUserId: "",
+    jAuthToken: "",
+    jobName: ""
+  }
+};
+
+export default sshUploadDeployPipelineStepConfigurationMetadata
