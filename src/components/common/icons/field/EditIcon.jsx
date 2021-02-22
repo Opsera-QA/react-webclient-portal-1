2f699bf15e22d41c7c9c5bc08023c3dca39c5435
Noah Champoux <noah@opsera.io>
2021-02-22 08:44:01 -0500

    Created Edit Icon (for use with inline editor components)

---
+++ src/components/common/icons/field/EditIcon.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPencilAlt} from "@fortawesome/pro-light-svg-icons";
+import ButtonTooltip from "components/common/tooltip/ButtonTooltip";
+
+function EditIcon({ editFunction, className, tooltipBody, disabled }) {
+
+  if (disabled === true) {
+    return null;
+  }
+
+  return (
+    <div className={className}>
+      <ButtonTooltip innerText={tooltipBody}>
+        <FontAwesomeIcon
+          onClick={() => {editFunction()}}
+          icon={faPencilAlt}
+          fixedWidth
+          className={"pointer"}
+        />
+      </ButtonTooltip>
+    </div>
+  );
+}
+
+EditIcon.propTypes = {
+  editFunction: PropTypes.func,
+  className: PropTypes.string,
+  tooltipBody: PropTypes.any,
+  disabled: PropTypes.bool
+};
+
+export default EditIcon;
