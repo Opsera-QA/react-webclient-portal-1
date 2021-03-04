0f842b0f44e00c218460e4a3e68f9f6246806d02
Shrey Malhotra <shrey@opsera.io>
2021-03-04 14:46:58 +0000

    OC802 & OC801 - NUnit AND .NET Pipeline Support

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepConfigurationSummary.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepConfigurationSummary.jsx
@@ -125,6 +125,12 @@ import SfdcPipelineStepConfigurationSummaryPanel
   from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/sfdc/SfdcPipelineStepConfigurationSummaryPanel";
 import sfdcPipelineStepConfigurationMetadata
   from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/sfdc/sfdcPipelineStepConfigurationMetadata";
+import DotNetPipelineStepConfigurationSummaryPanel
+  from "./step_tool_configuration_forms/dotnet/DotNetPipelineStepConfigurationSummaryPanel";
+import dotnetStepFormMetadata from "./step_tool_configuration_forms/dotnet/dotnet-stepForm-metadata";
+import NUnitPipelineStepConfigurationSummaryPanel
+  from "./step_tool_configuration_forms/nunit/NUnitPipelineStepConfigurationSummaryPanel";
+import nunitStepFormMetadata from "./step_tool_configuration_forms/nunit/nunit-stepForm-metadata";
 
 function PipelineStepConfigurationSummary({
   pipelineData,
@@ -295,6 +301,20 @@ function PipelineStepConfigurationSummary({
           <PowershellPipelineStepConfigurationSummaryPanel
             pipelineData={pipelineData}
             powershellPipelineDataObject={getModelWrappedObject(powershellStepFormMetadata)}
+          />
+        );
+      case "dotnet":
+        return (
+          <DotNetPipelineStepConfigurationSummaryPanel
+            pipelineData={pipelineData}
+            dotNetPipelineDataObject={getModelWrappedObject(dotnetStepFormMetadata)}
+          />
+        );
+      case "nunit":
+        return (
+          <NUnitPipelineStepConfigurationSummaryPanel
+            pipelineData={pipelineData}
+            nunitPipelineDataObject={getModelWrappedObject(nunitStepFormMetadata)}
           />
         );
       case "sonar":
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/nunit/nunit-stepForm-metadata.js
@@ -0,0 +1,160 @@
+const nunitStepFormMetadata = {
+  type: "NUnit Tool Configuration",
+  fields: [
+    {
+      label: "Jenkins Tool",
+      id: "toolConfigId",
+      isRequired: true
+    },
+    {
+      label: "Jenkins Tool Name",
+      id: "toolName",
+      isRequired: true
+    },
+    {
+      label: "Jenkins Job",
+      id: "toolJobName",
+      isRequired: true
+    },
+    {
+      label: "Jenkins Job Id",
+      id: "toolJobId",
+      isRequired: true
+    },
+    {
+      label: "Jenkins Job Type",
+      id: "toolJobType",
+      // isRequired: true
+    },
+    {
+      label: "Step Job Type",
+      id: "jobType",
+      isRequired: true
+    },
+    {
+      label: "Select SCM Type",
+      id: "type",
+      isRequired: true
+    },
+    
+    {
+      label: "SCM Account",
+      id: "gitCredential",
+      isRequired: true
+    },
+    {
+      label: "SCM Tool",
+      id: "gitToolId",
+      isRequired: true
+    },
+    {
+      label: "Repo ID",
+      id: "repoId",
+    },
+    {
+      label: "SCM Service Type",
+      id: "service",
+      isRequired: true
+    },
+    
+    {
+      id: "projectId",
+    },
+    {
+      id: "gitUrl",
+      isRequired: true
+    },
+    
+    {
+      id: "sshUrl",
+      isRequired: true
+    },
+    
+    {
+      label: "Repository",
+      id: "repository",
+      isRequired: true
+    },
+    
+    {
+      label: "Workspace",
+      id: "workspace",
+      // isRequired: true
+    },
+    {
+      label: "Workspace/Project",
+      id: "workspaceName",
+      // isRequired: true
+    },
+    {
+      label: "Branch",
+      id: "gitBranch",
+      isRequired: true
+    },
+    {
+      label: "Jenkins Agent",
+      id: "agentLabels",
+      // isRequired: true
+    },
+    {
+      label: "Job Name",
+      id: "jobName"
+    },
+    {
+      label: "Solution File Path",
+      id: "solutionFilePath"
+    },
+    {
+      label: "Solution File Name",
+      id: "solutionFileName"
+    },
+    {
+      label: "DLL File Path",
+      id: "dllFilePath"
+    },
+    {
+      label: "DLL File Name",
+      id: "dllFileName"
+    },
+    {
+      label: "Delete workspace before building",
+      id: "workspaceDeleteFlag",
+    },
+  ],
+  newModelBase: {
+
+    type: "",
+
+    jobType: "",
+    toolConfigId: "",
+    toolName: "",
+    jobName: "",
+
+    toolJobId: "",
+     
+    projectId: "",
+
+    gitToolId: "",
+    repoId: "",
+    gitUrl: "",
+    sshUrl: "",
+    service: "",
+
+    gitCredential: "",
+
+    workspace: "",
+    workspaceName: "",
+    repository: "",
+    gitBranch: "",
+
+    agentLabels: "",
+
+    solutionFilePath :"",
+    solutionFileName :"",
+    dllFilePath :"",
+    dllFileName :"",
+    workspaceDeleteFlag: ""
+  }
+};
+
+export default nunitStepFormMetadata;
