52d7f15f9e9b2a6d9ad21b2de688c92914f0d6c2
Noah Champoux <noah@opsera.io>
2021-03-04 09:12:55 -0500

    Removed deprecated createFilterOptions

--- src/components/common/filters/filterHelpers.js
+++ src/components/common/filters/filterHelpers.js
@@ -1,16 +1,3 @@
-// TODO: Add groupBy option
-export function createFilterOptions(data, prependText, textField, valueField) {
-  let filterOptions = [];
-
-  if (data && data.length > 0) {
-    data.map((record, index) => {
-      filterOptions.push({text: `${prependText}: ${record[textField]}`, value: record[valueField]});
-    });
-  }
-
-  return filterOptions;
-}
-
 export function createPageSortOptions(data, prependText, textField = "text", valueField = "option") {
   let filterOptions = [];
 
