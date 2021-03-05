d128f86bc14b2f7db2909eb38324eb0f6c785b19
Noah Champoux <noah@opsera.io>
2021-03-05 08:46:35 -0500

    Created Add Record Icon for Title Bar Usage

---
+++ src/components/common/icons/create/AddNewRecordIcon.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/pro-light-svg-icons";
+
+function AddNewRecordIcon({ addRecordFunction, className, type }) {
+  if (addRecordFunction == null) {
+    return null;
+  }
+
+  return (
+    <div className={className}>
+      <div className={"pointer"}>
+        <FontAwesomeIcon onClick={() => {addRecordFunction()}} icon={faPlus} fixedWidth className={"mr-2"} />
+        <span>Add New {type}</span>
+      </div>
+    </div>
+  );
+}
+
+AddNewRecordIcon.propTypes = {
+  addRecordFunction: PropTypes.func,
+  type: PropTypes.string,
+  className: PropTypes.string
+};
+
+export default AddNewRecordIcon;
