95551dc44e660aba354a2105a327a9d6360ee36f
Noah Champoux <noah@opsera.io>
2021-03-08 08:13:05 -0500

    Styling Enhancements

--- src/components/user/user_settings/subscriptions/TagSubscriptionManager.jsx
+++ src/components/user/user_settings/subscriptions/TagSubscriptionManager.jsx
@@ -20,13 +20,13 @@ function TagSubscriptionManager({loadData, isMounted}) {
 
   return (
     <CenterOverlayContainer titleIcon={faTag}  closePanel={closePanel} titleText={"Tag Subscription Manager"} showPanel={true}>
-      <div className={"p-3 shaded-panel"}>
+      <div className={"p-3"}>
         <div className="mb-4">Click a Tag to manage subscription status.</div>
         <AllTagsCloud />
-        <SaveButtonContainer>
-          <CloseButton className={"mr-2"} size={"sm"} closeEditorCallback={closePanel} />
-        </SaveButtonContainer>
       </div>
+      <SaveButtonContainer>
+        <CloseButton className={"p-3"} size={"sm"} closeEditorCallback={closePanel} />
+      </SaveButtonContainer>
     </CenterOverlayContainer>
   );
 }
