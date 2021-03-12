5449296a8c3f5ad3b6bb8407e4e4f924d4741fc1
Noah Champoux <noah@opsera.io>
2021-03-12 07:17:19 -0500

    Created CustomBadge and CustomBadgeContainers

---
+++ src/components/common/badges/CustomBadge.jsx
@@ -0,0 +1,29 @@
+import React  from "react";
+import PropTypes from "prop-types";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+function CustomBadge({icon, badgeText, className}) {
+  const getIcon = () => {
+    if (icon) {
+      return (<FontAwesomeIcon icon={icon} fixedWidth className="mr-1"/>);
+    }
+  };
+
+  if (badgeText == null || badgeText === "") {
+    return null;
+  }
+
+  return (
+    <span className={`custom-badge ${className}`}>
+      <span>{getIcon()}{badgeText}</span>
+    </span>
+  );
+}
+
+CustomBadge.propTypes = {
+  icon: PropTypes.object,
+  badgeText: PropTypes.string,
+  className: PropTypes.string
+};
+
+export default React.memo(CustomBadge);
