c650e3742c0e8ea827a28ffb1d55371e330695ea
Todd Barczak <todd@opsera.io>
2021-03-15 12:36:03 -0400

    Update wording on registration confirmation page

--- src/components/landing/Registration.jsx
+++ src/components/landing/Registration.jsx
@@ -6,49 +6,58 @@ import { Row, Col } from "react-bootstrap";
 import "./landing.css";
 
 function RegistrationLanding() {
-  
-  
+
+
   return (
-    
+
     <>
       <div className="mt-3 ml-5 w-75">
         <Row>
           <Col xl="12">
-            <div style={{ maxWidth: "1025px" }}>
-              <div className="h4 mb-3">Welcome to Opsera!  Your DevSecOps Solution.</div>
-              <div className="h5 text-muted mb-5">Your registration has been processed successfully.  You will be contacted via email shortly with details on how to proceed.</div>
-              
-              <div className="row mx-n2 mt-3" style={{ minWidth:"1020px" }}>
+            <div style={{ maxWidth: "1025px" }} className={"mt-3"}>
+              <div className="h4 mb-3">Welcome to Opsera! Your DevSecOps Solution.</div>
+              <div className="text-muted mb-5 mt-1" style={{ fontSize: "larger" }}>Your registration request has been
+                processed successfully. You will
+                receive an email from our Okta service in order to complete your account registration. After that is
+                complete,
+                an Opsera representative will contact you in order to finalize your new solution. If you have any
+                questions,
+                please do not hesitate to email us at support@opsera.io
+              </div>
+
+              <div className="row mx-n2 mt-3" style={{ minWidth: "1020px" }}>
                 <div className="col-md px-2 landing-content-module">
                   <img alt="OpsERA"
-                    src="/img/platform.png"
-                    width="195"
-                    height="225"
-                    className="d-inline-block align-top"
+                       src="/img/platform.png"
+                       width="195"
+                       height="225"
+                       className="d-inline-block align-top"
                   />
                 </div>
                 <div className="col-md px-2 landing-content-module">
                   <img alt="OpsERA"
-                    src="/img/pipeline.png"
-                    width="195"
-                    height="225"
-                    className="d-inline-block align-top"
+                       src="/img/pipeline.png"
+                       width="195"
+                       height="225"
+                       className="d-inline-block align-top"
                   />
                 </div>
                 <div className="col-md px-2 landing-content-module2">
                   <img alt="OpsERA"
-                    src="/img/analytics.png"
-                    width="195"
-                    height="225"
-                    className="d-inline-block align-top"
+                       src="/img/analytics.png"
+                       width="195"
+                       height="225"
+                       className="d-inline-block align-top"
                   />
                 </div>
-                
+
               </div>
-              <div className="row mx-n2 mt-4" style={{ minWidth:"1020px" }}>
+              <div className="row mx-n2 mt-4" style={{ minWidth: "1020px" }}>
                 <div className="col-md px-2 landing-content-module">
                   <div className="h5">Toolchain Automation</div>
-                  <div className="text-muted">Get started in your DevOps journey or experiment with many of our tool offerings to figure out your next steps.</div>
+                  <div className="text-muted">Get started in your DevOps journey or experiment with many of our tool
+                    offerings to figure out your next steps.
+                  </div>
                 </div>
                 <div className="col-md px-2 landing-content-module">
                   <div className="h5">Declarative Pipelines</div>
@@ -57,33 +66,29 @@ function RegistrationLanding() {
                 <div className="col-md px-2 landing-content-module">
                   <div className="h5">Analytics and Intelligence</div>
                   <div className="text-muted">Get real time observability across your various pipelines.</div>
-                </div>                
+                </div>
               </div>
 
 
             </div>
-          </Col>          
+          </Col>
         </Row>
         <Row>
-          <Col xl="12" className="pt-4"><hr style={{ width:"1020px", textAlign: "left", marginLeft: "0" }} /></Col>
-        </Row>  
-        <Row>
-          <Col xl="12">
-              At this time Opsera's solution is available for users through invitation only.
-              You will be contacted by Opsera in order to complete your onboarding process.
+          <Col xl="12" className="pt-4">
+            <hr style={{ width: "1020px", textAlign: "left", marginLeft: "0" }}/>
           </Col>
-        </Row>  
+        </Row>
 
         <Row>
           <Col xl="12" className="pt-3">
             <div className="h5">Need help?</div>
-            <div className="text-color h6 mt-1">Send an email to support@opsera.io</div>
+            <div className="text-muted h6 mt-1">Send an email to support@opsera.io</div>
           </Col>
         </Row>
       </div>
-          
+
     </>
-    
+
   );
   // } 
 
