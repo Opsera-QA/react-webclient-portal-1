3409d9b167af34bdadc314688fa1fe958ee58ba5
Noah Champoux <noah@opsera.io>
2021-03-16 14:01:09 -0400

    Do not wrap errors in extra object before throwing

--- src/api/apiServiceV2.js
+++ src/api/apiServiceV2.js
@@ -20,34 +20,34 @@ export async function axiosApiGetCall(getAccessToken, cancelTokenSource, apiUrl,
   const accessToken = await getAccessToken();
   return await getAxiosInstance(accessToken, cancelTokenSource?.token).get(apiUrl, urlParams)
     .then((result) =>  {return result;})
-    .catch(error => { if (!axios.isCancel(error)) {throw { error };}});
+    .catch(error => { if (!axios.isCancel(error)) {throw error;}});
 }
 
 export async function axiosApiPostCall(getAccessToken, cancelTokenSource, apiUrl, postBody) {
   const accessToken = await getAccessToken();
   return await getAxiosInstance(accessToken, cancelTokenSource?.token).post(apiUrl, postBody)
     .then((result) =>  {return result;})
-    .catch(error => { if (!axios.isCancel(error)) {throw { error };}});
+    .catch(error => { if (!axios.isCancel(error)) {throw error;}});
 }
 
 export async function axiosApiPutCall(getAccessToken, cancelTokenSource, apiUrl, postBody) {
   const accessToken = await getAccessToken();
   return await getAxiosInstance(accessToken, cancelTokenSource?.token).put(apiUrl, postBody)
     .then((result) =>  {return result;})
-    .catch(error => { if (!axios.isCancel(error)) {throw { error };}});
+    .catch(error => { if (!axios.isCancel(error)) {throw error;}});
 }
 
 export async function axiosApiPatchCall(getAccessToken, cancelTokenSource, apiUrl, postBody) {
   const accessToken = await getAccessToken();
   return await getAxiosInstance(accessToken, cancelTokenSource?.token).patch(apiUrl, postBody)
     .then((result) =>  {return result;})
-    .catch(error => { if (!axios.isCancel(error)) {throw { error };}});
+    .catch(error => { if (!axios.isCancel(error)) {throw error;}});
 }
 
 export async function axiosApiDeleteCall(getAccessToken, cancelTokenSource, apiUrl) {
   const accessToken = await getAccessToken();
   return await getAxiosInstance(accessToken, cancelTokenSource?.token).delete(apiUrl)
     .then((result) =>  {return result;})
-    .catch(error => { if (!axios.isCancel(error)) {throw { error };}});
+    .catch(error => { if (!axios.isCancel(error)) {throw error;}});
 }
 
