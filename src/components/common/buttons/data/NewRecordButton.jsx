c0cd46f3c200dcbc26634d45a25966ebcdceba7d
Noah Champoux <noah@opsera.io>
2021-03-17 13:37:16 -0400

    Allow disabling of NewRecordButton

--- src/components/common/buttons/data/NewRecordButton.jsx
+++ src/components/common/buttons/data/NewRecordButton.jsx
@@ -4,14 +4,14 @@ import {Button} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faPlus} from "@fortawesome/pro-light-svg-icons";
 
-function NewRecordButton({ isLoading, variant, addRecordFunction, type, size, className }) {
+function NewRecordButton({ isLoading, variant, addRecordFunction, type, size, className, disabled }) {
   if (!addRecordFunction) {
     return <></>;
   }
 
   return (
     <div className={className}>
-      <Button variant={variant} size={size} disabled={isLoading} onClick={() => {addRecordFunction();}}>
+      <Button variant={variant} size={size} disabled={isLoading || disabled === true} onClick={() => {addRecordFunction();}}>
         <span className={"d-xl-none"}><FontAwesomeIcon icon={faPlus} fixedWidth/></span>
         <span className={"d-none d-xl-inline"}><FontAwesomeIcon icon={faPlus} className="mr-1" fixedWidth/> New {type}</span>
       </Button>
@@ -25,7 +25,8 @@ NewRecordButton.propTypes = {
   type: PropTypes.string,
   variant: PropTypes.string,
   size: PropTypes.string,
-  className: PropTypes.string
+  className: PropTypes.string,
+  disabled: PropTypes.bool
 };
 
 NewRecordButton.defaultProps = {
