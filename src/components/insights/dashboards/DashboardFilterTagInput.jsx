bbd4a239e1b5887f04d5abb3a1d4e6e95ceba0ad
Noah Champoux <noah@opsera.io>
2021-03-19 12:32:01 -0400

    Fixed Formatting

--- src/components/insights/dashboards/DashboardFilterTagInput.jsx
+++ src/components/insights/dashboards/DashboardFilterTagInput.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import TagMultiSelectInputBase from "components/common/list_of_values_input/settings/tags/TagMultiSelectInputBase";
 import LenientSaveButton from "components/common/buttons/saving/LenientSaveButton";
 import modelHelpers from "components/common/model/modelHelpers";
+
 function DashboardFilterTagInput({ dataObject, setDataObject, dashboardData, loadData, saveData, className, fieldName}) {
   const validateAndSetData = (fieldName, value) => {
     let newDataObject = dataObject;
@@ -14,17 +15,17 @@ function DashboardFilterTagInput({ dataObject, setDataObject, dashboardData, loa
 
   return (
     <div className="d-flex my-auto">
-    <TagMultiSelectInputBase
-      className={className}
-      showLabel={false}
-      fieldName={fieldName}
-      dataObject={dataObject}
-      setDataObject={setDataObject}
-      setDataFunction={validateAndSetData}
-    />
-    <div className={"mx-2"} >
-    <LenientSaveButton size={"sm"} recordDto={dashboardData} updateRecord={saveData}/>
-    </div>
+      <TagMultiSelectInputBase
+        className={className}
+        showLabel={false}
+        fieldName={fieldName}
+        dataObject={dataObject}
+        setDataObject={setDataObject}
+        setDataFunction={validateAndSetData}
+      />
+      <div className={"mx-2"}>
+        <LenientSaveButton size={"sm"} recordDto={dashboardData} updateRecord={saveData}/>
+      </div>
     </div>
   );
 }
