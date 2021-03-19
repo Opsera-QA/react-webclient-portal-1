13c3e58268321e866635fde5ac7b18512f0b73c8
Tejas Bharadwaj <tejas@opsera.io>
2021-03-19 17:27:49 +0000

    Dashboard filter save button fixes

--- src/components/insights/dashboards/DashboardFilterTagInput.jsx
+++ src/components/insights/dashboards/DashboardFilterTagInput.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import TagMultiSelectInputBase from "components/common/list_of_values_input/settings/tags/TagMultiSelectInputBase";
 import LenientSaveButton from "components/common/buttons/saving/LenientSaveButton";
+import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
 import modelHelpers from "components/common/model/modelHelpers";
 
 function DashboardFilterTagInput({ dataObject, setDataObject, dashboardData, loadData, saveData, className, fieldName}) {
@@ -23,9 +24,11 @@ function DashboardFilterTagInput({ dataObject, setDataObject, dashboardData, loa
         setDataObject={setDataObject}
         setDataFunction={validateAndSetData}
       />
+      <TooltipWrapper innerText={"Saves dashboard view with current settings applied"}>
       <div className={"mx-2"}>
-        <LenientSaveButton size={"sm"} recordDto={dashboardData} updateRecord={saveData}/>
+        <LenientSaveButton saveButtonText={"Save View"} size={"sm"} recordDto={dashboardData} updateRecord={saveData}/>
       </div>
+      </TooltipWrapper>
     </div>
   );
 }
