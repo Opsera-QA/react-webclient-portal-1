8aa26ce3ee2a478036b6bd6ca6df886b5f9befd5
Noah Champoux <noah@opsera.io>
2021-03-25 08:04:11 -0400

    Icon Size enhancements

--- src/components/common/icons/LoadingIcon.jsx
+++ src/components/common/icons/LoadingIcon.jsx
@@ -3,21 +3,22 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSpinner} from "@fortawesome/pro-light-svg-icons";
 import PropTypes from "prop-types";
 
-function LoadingIcon({ isLoading, className }) {
+function LoadingIcon({ isLoading, className, iconSize }) {
   if (!isLoading) {
     return null;
   }
 
   return (
     <span className={className}>
-      <FontAwesomeIcon icon={faSpinner} spin fixedWidth />
+      <FontAwesomeIcon size={iconSize} icon={faSpinner} spin fixedWidth />
     </span>
   );
 }
 
 LoadingIcon.propTypes = {
   isLoading: PropTypes.bool,
-  className: PropTypes.string
+  className: PropTypes.string,
+  iconSize: PropTypes.string
 };
 
 export default React.memo(LoadingIcon);
