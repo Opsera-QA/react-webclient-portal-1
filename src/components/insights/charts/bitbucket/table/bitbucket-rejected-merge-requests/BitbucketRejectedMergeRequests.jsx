2f8a138d5c1290dae95576423d4650b7477c8e6b
Harsha Pullabhatlapogada <harsha@opsera.io>
2021-03-30 14:05:18 +0000

    OC-957: KPI for bitbucket rejected PRs

---
+++ src/components/insights/charts/bitbucket/table/bitbucket-rejected-merge-requests/BitbucketRejectedMergeRequests.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useContext, useState, useMemo, useRef } from "react";
+import { AuthContext } from "contexts/AuthContext";
+import CustomTable from "components/common/table/CustomTable";
+import "components/analytics/charts/charts.css";
+import axios from "axios";
+import chartsActions from "components/insights/charts/charts-actions";
+import PropTypes from "prop-types";
+import ChartContainer from "components/common/panels/insights/charts/ChartContainer";
+import { getTableTextColumn } from "components/common/table/table-column-helpers";
+import bitbucketRejectedMergeRequestsMetadata from "components/insights/charts/bitbucket/table/bitbucket-rejected-merge-requests/bitbucket-rejected-merge-requests-metadata";
+import { getField } from "components/common/metadata/metadata-helpers";
+import { format } from "date-fns";
+import Model from "core/data_model/model";
+import genericChartFilterMetadata from "components/insights/charts/generic_filters/genericChartFilterMetadata";
+
+function BitbucketRejectedMergeRequestsTable({ kpiConfiguration, setKpiConfiguration, dashboardData, index, setKpis }) {
+  const fields = bitbucketRejectedMergeRequestsMetadata.fields;
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(false);
+  const [metrics, setMetrics] = useState([]);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [tableFilterDto, setTableFilterDto] = useState(
+    new Model({ ...genericChartFilterMetadata.newObjectFields }, genericChartFilterMetadata, false)
+  );
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [JSON.stringify(dashboardData)]);
+
+  const loadData = async (cancelSource = cancelTokenSource, filterDto = tableFilterDto) => {
+    try {
+      setIsLoading(true);
+      let dashboardTags =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "bitbucketRejectedPullRequests",
+        kpiConfiguration,
+        dashboardTags,
+        tableFilterDto
+      );
+      let dataObject = response?.data?.data[0]?.bitbucketRejectedPullRequests?.data;
+
+      if (isMounted?.current === true && dataObject) {
+        setMetrics(dataObject);
+        let newFilterDto = filterDto;
+        newFilterDto.setData("totalCount", response?.data?.data[0]?.bitbucketRejectedPullRequests?.count);
+        setTableFilterDto({ ...newFilterDto });
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const noDataMessage = "No Data is available for this chart at this time";
+  const columns = useMemo(
+    () => [
+      getTableTextColumn(getField(fields, "AuthorName")),
+      getTableTextColumn(getField(fields, "AssigneeName")),
+      getTableTextColumn(getField(fields, "MergeRequestTitle")),
+      getTableTextColumn(getField(fields, "BranchName")),
+      getTableTextColumn(getField(fields, "ProjectName")),
+      getTableTextColumn(getField(fields, "RejectedReason")),
+      {
+        Header: "Time",
+        accessor: "mrCompletionTimeTimeStamp",
+        Cell: (row) => {
+          return format(new Date(row.value), "yyyy-MM-dd', 'hh:mm a");
+        },
+      },
+    ],
+    []
+  );
+
+  const getChartTable = () => {
+    return (
+      <CustomTable
+        columns={columns}
+        data={metrics}
+        noDataMessage={noDataMessage}
+        paginationDto={tableFilterDto}
+        setPaginationDto={setTableFilterDto}
+        loadData={loadData}
+        scrollOnLoad={false}
+      />
+    );
+  };
+
+  return (
+    <div>
+      <ChartContainer
+        kpiConfiguration={kpiConfiguration}
+        setKpiConfiguration={setKpiConfiguration}
+        chart={getChartTable()}
+        loadChart={loadData}
+        dashboardData={dashboardData}
+        index={index}
+        error={error}
+        setKpis={setKpis}
+        isLoading={isLoading}
+      />
+    </div>
+  );
+}
+
+BitbucketRejectedMergeRequestsTable.propTypes = {
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
+  index: PropTypes.number,
+  setKpiConfiguration: PropTypes.func,
+  setKpis: PropTypes.func,
+};
+
+export default BitbucketRejectedMergeRequestsTable;
