c82d04f1acb52f5a69689c68e83a898b1d3b1a9b
Noah Champoux <noah@opsera.io>
2021-03-30 11:32:51 -0400

    Wired up pagination on GitlabPendingMergeRequests

--- src/components/insights/charts/gitlab/table/pending_merge_requests/GitlabPendingMergeRequests.jsx
+++ src/components/insights/charts/gitlab/table/pending_merge_requests/GitlabPendingMergeRequests.jsx
@@ -12,6 +12,8 @@ import {
 import gitlabPendingMergeRequestsMetadata
   from "components/insights/charts/gitlab/table/pending_merge_requests/gitlab-pending-merge-requests-metadata.js";
 import {getField} from "components/common/metadata/metadata-helpers";
+import Model from "core/data_model/model";
+import genericChartFilterMetadata from "components/insights/charts/generic_filters/genericChartFilterMetadata";
 
 function GitlabPendingMergeRequests({ kpiConfiguration, setKpiConfiguration, dashboardData, index, setKpis}) {
   const fields = gitlabPendingMergeRequestsMetadata.fields;
@@ -21,6 +23,7 @@ function GitlabPendingMergeRequests({ kpiConfiguration, setKpiConfiguration, das
   const [metrics, setMetrics] = useState([]);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [tableFilterDto, setTableFilterDto] = useState(new Model({...genericChartFilterMetadata.newObjectFields}, genericChartFilterMetadata, false));
 
   const noDataMessage = "No Data is available for this chart at this time";
 
@@ -57,15 +60,25 @@ function GitlabPendingMergeRequests({ kpiConfiguration, setKpiConfiguration, das
     };
   }, [JSON.stringify(dashboardData)]);
 
-  const loadData = async (cancelSource = cancelTokenSource) => {
+  const loadData = async (cancelSource = cancelTokenSource, filterDto = tableFilterDto) => {
     try {
       setIsLoading(true);
       let dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
-      const response = await chartsActions.parseConfigurationAndGetChartMetrics(getAccessToken, cancelSource, "gitlabPendingMergeRequests", kpiConfiguration, dashboardTags);
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "gitlabPendingMergeRequests",
+        kpiConfiguration,
+        dashboardTags,
+        filterDto
+      );
       let dataObject = response?.data?.data[0]?.gitlabPendingMergeRequests?.data;
 
       if (isMounted?.current === true && dataObject) {
         setMetrics(dataObject);
+        let newFilterDto = filterDto;
+        newFilterDto.setData("totalCount", response?.data?.data[0]?.gitlabPendingMergeRequests?.count);
+        setTableFilterDto({...newFilterDto});
       }
     }
     catch (error) {
@@ -81,12 +94,27 @@ function GitlabPendingMergeRequests({ kpiConfiguration, setKpiConfiguration, das
     }
   };
 
+  const getChartTable = () => {
+    return (
+      <CustomTable
+        columns={columns}
+        data={metrics}
+        noDataMessage={noDataMessage}
+        paginationDto={tableFilterDto}
+        setPaginationDto={setTableFilterDto}
+        loadData={loadData}
+        scrollOnLoad={false}
+        noFooter={true}
+      />
+    );
+  };
+
   return (
     <div>
       <ChartContainer
         kpiConfiguration={kpiConfiguration}
         setKpiConfiguration={setKpiConfiguration}
-        chart={<CustomTable columns={columns} data={metrics} noDataMessage={noDataMessage} noFooter={true}/>}
+        chart={getChartTable()}
         loadChart={loadData}
         dashboardData={dashboardData}
         index={index}
