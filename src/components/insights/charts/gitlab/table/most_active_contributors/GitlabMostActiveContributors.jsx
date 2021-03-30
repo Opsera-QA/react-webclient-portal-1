22d1ea7a3c3cc1e69958d196f155ab3e24b6798a
Noah Champoux <noah@opsera.io>
2021-03-30 15:30:52 -0400

    added pagination support for GitlabMostActiveContributors

--- src/components/insights/charts/gitlab/table/most_active_contributors/GitlabMostActiveContributors.jsx
+++ src/components/insights/charts/gitlab/table/most_active_contributors/GitlabMostActiveContributors.jsx
@@ -6,8 +6,6 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import chartsActions from "components/insights/charts/charts-actions";
 import {
-  getChartPipelineStatusColumn,
-  getTableDateTimeColumn,
   getTableTextColumn
 } from "components/common/table/table-column-helpers";
 import gitlabMostActiveContributorsMetadata
@@ -15,6 +13,9 @@ import gitlabMostActiveContributorsMetadata
 import {getField} from "components/common/metadata/metadata-helpers";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Model from "core/data_model/model";
+import genericChartFilterMetadata from "components/insights/charts/generic_filters/genericChartFilterMetadata";
+
 function GitlabMostActiveContributors({ kpiConfiguration, setKpiConfiguration, dashboardData, index, setKpis}) {
   const fields = gitlabMostActiveContributorsMetadata.fields;
   const {getAccessToken} = useContext(AuthContext);
@@ -23,6 +24,7 @@ function GitlabMostActiveContributors({ kpiConfiguration, setKpiConfiguration, d
   const [metrics, setMetrics] = useState([]);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [tableFilterDto, setTableFilterDto] = useState(new Model({...genericChartFilterMetadata.newObjectFields}, genericChartFilterMetadata, false));
 
   const noDataMessage = "No Data is available for this chart at this time";
 
@@ -69,15 +71,25 @@ function GitlabMostActiveContributors({ kpiConfiguration, setKpiConfiguration, d
     };
   }, [JSON.stringify(dashboardData)]);
 
-  const loadData = async (cancelSource = cancelTokenSource) => {
+  const loadData = async (cancelSource = cancelTokenSource, filterDto = tableFilterDto) => {
     try {
       setIsLoading(true);
       let dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
-      const response = await chartsActions.parseConfigurationAndGetChartMetrics(getAccessToken, cancelSource, "gitlabMostActiveUsers", kpiConfiguration, dashboardTags);
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "gitlabMostActiveUsers",
+        kpiConfiguration,
+        dashboardTags,
+        filterDto
+      );
       let dataObject = response?.data?.data[0]?.gitlabMostActiveUsers?.data;
 
       if (isMounted?.current === true && dataObject) {
         setMetrics(dataObject);
+        let newFilterDto = filterDto;
+        newFilterDto.setData("totalCount", response?.data?.data[0]?.gitlabMostActiveUsers?.count);
+        setTableFilterDto({...newFilterDto});
       }
     }
     catch (error) {
@@ -93,12 +105,26 @@ function GitlabMostActiveContributors({ kpiConfiguration, setKpiConfiguration, d
     }
   };
 
+  const getChartTable = () => {
+    return (
+      <CustomTable
+        columns={columns}
+        data={metrics}
+        noDataMessage={noDataMessage}
+        paginationDto={tableFilterDto}
+        setPaginationDto={setTableFilterDto}
+        loadData={loadData}
+        scrollOnLoad={false}
+      />
+    );
+  };
+
   return (
     <div>
       <ChartContainer
         kpiConfiguration={kpiConfiguration}
         setKpiConfiguration={setKpiConfiguration}
-        chart={<CustomTable columns={columns} data={metrics} noDataMessage={noDataMessage} noFooter={true}/>}
+        chart={getChartTable()}
         loadChart={loadData}
         dashboardData={dashboardData}
         index={index}
