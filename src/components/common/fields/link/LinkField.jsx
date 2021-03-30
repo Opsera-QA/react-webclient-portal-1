62c2d6a8e551dfe51174b858c6218a7f7d598842
Noah Champoux <noah@opsera.io>
2021-03-30 09:06:19 -0400

    Link cleanup

--- src/components/common/fields/link/LinkField.jsx
+++ src/components/common/fields/link/LinkField.jsx
@@ -1,38 +1,18 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import FieldContainer from "components/common/fields/FieldContainer";
-import FieldLabel from "components/common/fields/FieldLabel";
-import CopyToClipboardIcon from "components/common/icons/CopyToClipboardIcon";
-import {useHistory} from "react-router-dom";
+import StandaloneLinkField from "components/common/fields/link/standalone/StandaloneLinkField";
 
 function LinkField({dataObject, fieldName, className, showClipboardButton, openInNewWindow }) {
   const [field] = useState(dataObject.getFieldById(fieldName));
-  let history = useHistory();
-
-  const openLink = () => {
-    if (openInNewWindow) {
-      window.open(dataObject.getData(fieldName));
-    }
-    else {
-      history.push(dataObject.getData(fieldName));
-    }
-  };
-
-
-  const getClipboardButton = () => {
-    if (showClipboardButton === true) {
-      return (<CopyToClipboardIcon className={"my-auto ml-auto"} copyString={dataObject?.getData(fieldName)} />);
-    }
-  };
 
   return (
-    <FieldContainer className={className}>
-      <div className="w-100 d-flex">
-        <FieldLabel field={field}/>
-        <span className={"pointer link-text"} onClick={() => openLink()}>{dataObject.getData(fieldName)}</span>
-        {getClipboardButton()}
-      </div>
-    </FieldContainer>
+    <StandaloneLinkField
+      className={className}
+      openInNewWindow={openInNewWindow}
+      link={dataObject?.getData(fieldName)}
+      label={field?.label}
+      showClipboardButton={showClipboardButton}
+    />
   );
 }
 
