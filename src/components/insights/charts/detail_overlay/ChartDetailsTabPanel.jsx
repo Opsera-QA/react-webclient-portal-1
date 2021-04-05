84802f4a75d8abb93083f4357b0ebba5ae7fc451
Noah Champoux <noah@opsera.io>
2021-04-05 08:17:33 -0400

    Created ChartDetailsOverlay (in progress, not wired up)

---
+++ src/components/insights/charts/detail_overlay/ChartDetailsTabPanel.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import SummaryTab from "components/common/tabs/detail_view/SummaryTab";
+import CustomTabContainer from "components/common/tabs/CustomTabContainer";
+import JsonTab from "components/common/tabs/detail_view/JsonTab";
+import ModalTabPanelContainer from "components/common/panels/detail_view/ModalTabPanelContainer";
+import ChartSummaryPanelWrapper from "components/insights/charts/detail_overlay/ChartSummaryPanelWrapper";
+import ChartJsonPanel from "components/insights/charts/detail_overlay/ChartJsonPanel";
+
+function ChartDetailsTabPanel({ chartModel }) {
+  const [activeTab, setActiveTab] = useState("summary");
+
+  const handleTabClick = (activeTab) => e => {
+    e.preventDefault();
+    setActiveTab(activeTab);
+  };
+
+  const getActionSpecificTab = () => {
+    // TODO: Make switch statement if a handful are added
+    // if (chartModel.action === "console output") {
+    //   return (<ConsoleLogTab activeTab={activeTab} handleTabClick={handleTabClick} />);
+    // }
+  };
+
+  const getTabContainer = () => {
+    return (
+      <CustomTabContainer>
+        <SummaryTab handleTabClick={handleTabClick} activeTab={activeTab} />
+        {/*{getActionSpecificTab()}*/}
+        <JsonTab handleTabClick={handleTabClick} activeTab={activeTab} />
+      </CustomTabContainer>
+    );
+  };
+
+  const getCurrentView = () => {
+    switch (activeTab) {
+      case "summary":
+        return <ChartSummaryPanelWrapper chartModel={chartModel} />;
+      case "json":
+        return <ChartJsonPanel chartModel={chartModel} />;
+      default:
+        return null;
+    }
+  };
+
+  return (<ModalTabPanelContainer detailView={getCurrentView()} tabContainer={getTabContainer()} />);
+}
+
+ChartDetailsTabPanel.propTypes = {
+  chartModel: PropTypes.object,
+};
+
+export default ChartDetailsTabPanel;
+
+
---
+++ src/components/insights/charts/detail_overlay/ChartSummaryPanelWrapper.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import ReactJson from "react-json-view";
+import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
+
+function ChartSummaryPanelWrapper({ chartModel }) {
+  const getStepConfigurationSummary = () => {
+    switch (chartModel.getData("kpi_identifier")) {
+      // case "anchore-integrator":
+      //   return (
+      //     // <AnchoreIntegratorStepConfigurationSummaryPanel
+      //     //   pipelineData={pipelineData}
+      //     //   anchoreDataObject={getModelWrappedObject(anchoreIntegratorStepConfigurationMetadata)}
+      //     // />
+      //   );
+      default:
+        return (
+          <SummaryPanelContainer>
+            <ReactJson src={chartModel} enableClipboard={false} displayDataTypes={false} collapsed={false}/>
+          </SummaryPanelContainer>
+        );
+    }
+  };
+
+  return (
+    <div>
+      {getStepConfigurationSummary()}
+    </div>
+  );
+}
+
+ChartSummaryPanelWrapper.propTypes = {
+  chartModel: PropTypes.object,
+};
+
+export default ChartSummaryPanelWrapper;
