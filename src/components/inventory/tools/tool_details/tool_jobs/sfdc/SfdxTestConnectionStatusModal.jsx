6038b56915f4e397db65f4ee6bc335fdae65af6f
Vignesh Subramanian <vignesh@opsera.io>
2021-04-06 10:07:44 +0530

    sfdx test connection modal and polling changes

--- src/components/inventory/tools/tool_details/tool_jobs/sfdc/SfdxTestConnectionStatusModal.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/sfdc/SfdxTestConnectionStatusModal.jsx
@@ -4,12 +4,19 @@ import PropTypes from "prop-types";
 import DetailPanelLoadingDialog from "components/common/loading/DetailPanelLoadingDialog";
 import { axiosApiService } from "api/apiService";
 import {AuthContext} from "contexts/AuthContext";
+<<<<<<< HEAD
 import { DialogToastContext } from "contexts/DialogToastContext";
 
 function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenkinsBuildNumber, setJenkinsBuildNumber}) {
 
   const { getAccessToken } = useContext(AuthContext);
   const toastContext = useContext(DialogToastContext);
+=======
+
+function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData}) {
+
+  const { getAccessToken } = useContext(AuthContext);
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
   const [testResponse, setTestResponse] = useState([]);
   const [modalLoading, setModalLoading] = useState(false);
 
@@ -23,12 +30,18 @@ function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenki
   });
 
   useEffect(() => {
+<<<<<<< HEAD
     if(showModal && jenkinsBuildNumber){      
       toolLogPolling(jenkinsBuildNumber);      
+=======
+    if(showModal){      
+      toolLogPolling();      
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
     }else {
       console.log(timerIds);
       timerIds.forEach(timerId => clearTimeout(timerId));
     }
+<<<<<<< HEAD
   }, [showModal,jenkinsBuildNumber]);
 
   const handleModalClose = () => {
@@ -38,25 +51,46 @@ function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenki
 
   const toolLogPolling = async (jenkinsBuildNumber, count = 1) => { 
     // console.log(jenkinsBuildNumber);   
+=======
+  }, [showModal]);
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const toolLogPolling = async (count = 1) => {    
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
     setModalLoading(true);
     try {
       const accessToken = await getAccessToken();
       const apiUrl = `/registry/log/${toolData.getData("_id")}?page=1&size=50`;
       const tool_logs = await axiosApiService(accessToken).get(apiUrl, {});
 
+<<<<<<< HEAD
       const testRes = tool_logs.data.data.filter(rec => rec.action === 'test_configuration' && rec.run_count == jenkinsBuildNumber);
+=======
+      const testRes = tool_logs.data.data.filter(rec => rec.action === 'test_configuration' && rec.run_count == 59);
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
 
       if(testRes.length !== 1 && count < 5 ){
         await new Promise(resolve => timerIds.push(setTimeout(resolve, 15000)));
         count++;
+<<<<<<< HEAD
         return await toolLogPolling(jenkinsBuildNumber, count);
+=======
+        return await toolLogPolling(count);
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
       }else{        
         setTestResponse(testRes);
         setModalLoading(false);
       }      
     } catch (err) {
       setModalLoading(false);
+<<<<<<< HEAD
       toastContext.showErrorDialog(err.message);
+=======
+      console.log(err.message);
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
     }    
   };
 
@@ -64,12 +98,20 @@ function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenki
     <>
       <Modal show={showModal} onHide={handleModalClose} backdrop="static">
         <Modal.Header closeButton>
+<<<<<<< HEAD
           <Modal.Title>SFDX Connection Check</Modal.Title>
+=======
+          <Modal.Title>Test SFDX Connection</Modal.Title>
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
         </Modal.Header>
         <Modal.Body>
           <div className="content-block m-3 full-height">
             <div className="p-3">
+<<<<<<< HEAD
                 {modalLoading ? <DetailPanelLoadingDialog type={"Connection Results"} /> : testResponse.length > 0 ? (                    
+=======
+                {modalLoading ? <DetailPanelLoadingDialog type={"Test Connection Results"} /> : testResponse.length > 0 ? (                    
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
                     <Row>
                       <Col lg={12}>
                         <label className="mb-0 mr-2 text-muted"><span>Job Name:</span></label>
@@ -95,7 +137,11 @@ function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenki
                         <span>We are unable to fetch the status of the test connection. Please try again.</span>
                       </Col>
                       <Col lg={12}>
+<<<<<<< HEAD
                         <Button variant="secondary" onClick={() => toolLogPolling(jenkinsBuildNumber)}>
+=======
+                        <Button variant="secondary" onClick={() => toolLogPolling()}>
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
                             Refresh
                         </Button>
                       </Col>                      
@@ -103,7 +149,10 @@ function SfdxTestConnectionStatusModal({setShowModal, showModal, toolData, jenki
                   </>
                 ) }
             </div>
+<<<<<<< HEAD
             <div className="text-muted small m-2">Note: The connectivity check may take some time to get the results, you can also check results on logs tab.</div>
+=======
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
           </div>
         </Modal.Body>
         <Modal.Footer>
@@ -120,8 +169,11 @@ SfdxTestConnectionStatusModal.propTypes = {
   showModal: PropTypes.bool,
   setShowModal: PropTypes.func,
   toolData: PropTypes.object,
+<<<<<<< HEAD
   jenkinsBuildNumber: PropTypes.string,
   setJenkinsBuildNumber: PropTypes.func,
+=======
+>>>>>>> e1f68f88 (sfdx test connection modal and polling changes)
 };
 
 export default SfdxTestConnectionStatusModal;
