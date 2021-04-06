aa9f8670e26c3bd4439531fb296b9dc29c75476e
Jim Provenza <jim@Jims-MacBook-Pro.local>
2021-04-06 14:36:18 -0700

    update button component name

--- src/components/reports/tags/tools/TagsUsedInToolsTable.jsx
+++ src/components/reports/tags/tools/TagsUsedInToolsTable.jsx
@@ -13,7 +13,7 @@ import {
 import {getField} from "components/common/metadata/metadata-helpers";
 import FilterContainer from "components/common/table/FilterContainer";
 import {faTags} from "@fortawesome/pro-light-svg-icons";
-import ExportReportButton from 'components/common/buttons/export/reports/ExportReportButton';
+import ExportTagReportButton from 'components/common/buttons/export/reports/ExportTagReportButton';
 
 function TagsUsedInToolsTable({ data, loadData, isLoading}) {
   const history = useHistory();
@@ -63,7 +63,7 @@ function TagsUsedInToolsTable({ data, loadData, isLoading}) {
       titleIcon={faTags}
       title={"Tags"}
       className={"px-2 pb-2"}
-      exportButton={<ExportReportButton className={"ml-2"} isLoading={isLoading} tagData={data} />}
+      exportButton={<ExportTagReportButton className={"ml-2"} isLoading={isLoading} tagData={data} />}
     />
   );
 }
