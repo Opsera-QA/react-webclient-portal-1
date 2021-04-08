5001f79e02ecfc79a4e77facf09d64a3dfce83bf
Noah Champoux <noah@opsera.io>
2021-04-08 15:37:31 -0400

    Git RBAC UI Enhancements (Part two)

--- src/components/git/git_task_details/GitTaskSummaryPanelBase.jsx
+++ src/components/git/git_task_details/GitTaskSummaryPanelBase.jsx
@@ -11,9 +11,11 @@ import TagsInlineInputBase from "components/common/inline_inputs/tags/TagsInline
 import gitTasksActions from "components/git/git-task-actions";
 import axios from "axios";
 import {AuthContext} from "contexts/AuthContext";
+import workflowAuthorizedActions
+  from "components/workflow/pipelines/pipeline_details/workflow/workflow-authorized-actions";
 
 function GitTaskSummaryPanelBase({ gitTasksData, setGitTasksData, setActiveTab, gitTaskTypeSummaryCard, loadData }) {
-  const { getAccessToken } = useContext(AuthContext);
+  const { getAccessToken, getAccessRoleData } = useContext(AuthContext);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
 
@@ -44,8 +46,12 @@ function GitTaskSummaryPanelBase({ gitTasksData, setGitTasksData, setActiveTab, 
     return response;
   };
 
+  const actionAllowed = (action) => {
+    return workflowAuthorizedActions.gitItems(getAccessRoleData(), action, gitTasksData?.getData("owner"), gitTasksData?.getData("roles"));
+  };
+
   return (
-    <SummaryPanelContainer setActiveTab={setActiveTab}>
+    <SummaryPanelContainer setActiveTab={setActiveTab} editingAllowed={actionAllowed("edit-settings")}>
       <Row>
         <Col md={6}>
           <TextFieldBase dataObject={gitTasksData} fieldName={"name"}/>
@@ -63,15 +69,15 @@ function GitTaskSummaryPanelBase({ gitTasksData, setGitTasksData, setActiveTab, 
           <TextFieldBase dataObject={gitTasksData} fieldName={"description"}/>
         </Col>
         <Col md={12} className={"pt-1"}>
-          <TagsInlineInputBase type={"task"} dataObject={gitTasksData} fieldName={"tags"} saveData={updateRecord}/>
+          <TagsInlineInputBase type={"task"} dataObject={gitTasksData} fieldName={"tags"} saveData={updateRecord} disabled={!actionAllowed("edit-settings")}/>
         </Col>
         <Col lg={12}>
-          <GitTaskRoleAccessInput dataObject={gitTasksData} setDataObject={setGitTasksData} />
+          <GitTaskRoleAccessInput dataObject={gitTasksData} setDataObject={setGitTasksData} disabled={!actionAllowed("edit_access_roles")} />
         </Col>
       </Row>
       <Row className={"mx-0 w-100 my-2"}>
         <div className={"mx-auto"}>
-          <div className={"mx-auto"}><GitTaskRunButton gitTasksData={gitTasksData} loadData={loadData} /></div>
+          <div className={"mx-auto"}><GitTaskRunButton gitTasksData={gitTasksData} loadData={loadData} disable={!actionAllowed("run_task")} /></div>
         </div>
       </Row>
       <div className="px-3 mt-3">{gitTaskTypeSummaryCard}</div>
