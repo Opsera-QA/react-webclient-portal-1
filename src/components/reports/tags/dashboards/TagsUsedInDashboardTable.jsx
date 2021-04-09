dee7d3647a2c1078c4de506633fea84970ae1796
Noah Champoux <noah@opsera.io>
2021-04-09 09:19:11 -0400

    Various small cleanups

--- src/components/reports/tags/dashboards/TagsUsedInDashboardTable.jsx
+++ src/components/reports/tags/dashboards/TagsUsedInDashboardTable.jsx
@@ -2,8 +2,6 @@ import React, {useMemo} from "react";
 import PropTypes from "prop-types";
 import CustomTable from "components/common/table/CustomTable";
 import { useHistory } from "react-router-dom";
-import pipelineSummaryMetadata
-  from "components/workflow/pipelines/pipeline_details/pipeline_activity/pipeline-summary-metadata";
 import {
   getTableBooleanIconColumn,
   getTableDateColumn,
@@ -12,12 +10,13 @@ import {
 } from "components/common/table/table-column-helpers";
 import {getField} from "components/common/metadata/metadata-helpers";
 import FilterContainer from "components/common/table/FilterContainer";
-import {faTags} from "@fortawesome/pro-light-svg-icons";
+import {faChartNetwork} from "@fortawesome/pro-light-svg-icons";
 import ExportTagReportButton from 'components/common/buttons/export/reports/ExportTagReportButton';
+import dashboardMetadata from "components/insights/dashboards/dashboard-metadata";
 
 function TagsUsedInDashboardTable({ data, loadData, isLoading}) {
   const history = useHistory();
-  let fields = pipelineSummaryMetadata.fields;
+  let fields = dashboardMetadata.fields;
 
   const columns = useMemo(
     () => [
@@ -58,10 +57,9 @@ function TagsUsedInDashboardTable({ data, loadData, isLoading}) {
       supportSearch={false}
       isLoading={isLoading}
       body={getTagsTable()}
-      metadata={pipelineSummaryMetadata}
-      titleIcon={faTags}
-      title={"Tags"}
-      className={"px-2 pb-2"}
+      metadata={dashboardMetadata}
+      titleIcon={faChartNetwork}
+      title={"Dashboards"}
       exportButton={<ExportTagReportButton className={"ml-2"} isLoading={isLoading} tagData={data} />}
     />
   );
