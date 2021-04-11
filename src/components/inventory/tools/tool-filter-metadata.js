a3def64679cbc22542273372753cec83662d8afd
Noah Champoux <noah@opsera.io>
2021-04-11 11:33:18 -0400

    Fixed viewType on Tool Filter Metadata

--- src/components/inventory/tools/tool-filter-metadata.js
+++ src/components/inventory/tools/tool-filter-metadata.js
@@ -73,7 +73,7 @@ const toolFilterMetadata = {
     sortOption: {text: "Sort: Name", value: "name"},
     search: "",
     activeFilters: [],
-    viewType: "cards",
+    viewType: "card",
   },
   // TODO: If these are the same options everywhere, move to PageSort
   sortOptions: [
