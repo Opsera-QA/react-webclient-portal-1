5a69fe9e9d114de5a6dcb5b7192fe14d22786bc4
Faseeh <faseeh@opsera.io>
2021-04-12 23:18:58 +0530

    added new task type config

---
+++ src/components/git/git_task_details/configuration_forms/sfdc-branch-structure/SFDCBranchStructuringTaskTypeSummaryCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "components/inventory/tools/tools.css";
+import TextFieldBase from "components/common/form_fields/TextFieldBase";
+import GitTasksSummaryCardContainer from "components/git/git_task_details/configuration_forms/GitTasksSummaryCardContainer";
+
+function SFDCBranchStructuringTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurationData, isLoading }) {
+  if (isLoading) {
+    return <GitTasksSummaryCardContainer isLoading={isLoading} />;
+  }
+
+  return (
+    <GitTasksSummaryCardContainer gitTasksDataDto={gitTasksData} isLoading={isLoading}>
+      <div className="mb-2">
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"toolName"} />
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"service"} />
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"gitCredential"} />
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"repository"} />
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"gitBranch"} />
+      </div>
+    </GitTasksSummaryCardContainer>
+  );
+}
+
+SFDCBranchStructuringTaskTypeSummaryCard.propTypes = {
+  gitTasksData: PropTypes.object,
+  gitTaskConfigurationData: PropTypes.object,
+  isLoading: PropTypes.bool,
+};
+
+export default SFDCBranchStructuringTaskTypeSummaryCard;
