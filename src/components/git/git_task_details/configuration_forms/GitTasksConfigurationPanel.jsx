37ee9d957e0deb65a9b7aca57b6a2f4c28059c41
Faseeh <faseeh@opsera.io>
2021-04-12 12:12:57 +0530

    added new git task type

--- src/components/git/git_task_details/configuration_forms/GitTasksConfigurationPanel.jsx
+++ src/components/git/git_task_details/configuration_forms/GitTasksConfigurationPanel.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import GitTaskTypeSelectInput from "components/common/list_of_values_input/git_tasks/GitTaskTypeSelectInput";
 import SFDCGitTaskEditorPanel from "./sfdc/SFDCGitTaskConfigurationPanel";
+import SFDCBranchStructuringTaskTypeConfigurationPanel from "./sfdc-branch-structure/SFDCBranchStructuringTaskTypeConfigurationPanel";
 
 function GitTasksConfigurationPanel({ gitTasksDataDto, setGitTasksDataDto, gitTasksConfigurationData, setGitTasksConfigurationData }) {
   const getConfigurationPanel = () => {
@@ -9,6 +10,14 @@ function GitTasksConfigurationPanel({ gitTasksDataDto, setGitTasksDataDto, gitTa
       case "sync-sfdc-repo":
         return (
           <SFDCGitTaskEditorPanel 
+            gitTasksDataDto={gitTasksDataDto}
+            setGitTasksConfigurationData={setGitTasksConfigurationData}
+            gitTasksConfigurationData={gitTasksConfigurationData}
+          />
+        );
+      case "sync-branch-structure":
+        return (
+          <SFDCBranchStructuringTaskTypeConfigurationPanel 
             gitTasksDataDto={gitTasksDataDto}
             setGitTasksConfigurationData={setGitTasksConfigurationData}
             gitTasksConfigurationData={gitTasksConfigurationData}
