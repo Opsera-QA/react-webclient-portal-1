b5579cac50f46b0e6538fbdb0cf4b04e26704d82
Noah Champoux <noah@opsera.io>
2021-04-13 14:28:57 -0400

    Minor bugfixes

--- src/components/common/help/input/role_access_editor/RoleAccessEditorHelpOverlay.jsx
+++ src/components/common/help/input/role_access_editor/RoleAccessEditorHelpOverlay.jsx
@@ -3,22 +3,34 @@ import PropTypes from "prop-types";
 import HelpOverlayBase from "components/common/overlays/center/help/HelpOverlayBase";
 import {DialogToastContext} from "contexts/DialogToastContext";
 
-function RoleAccessEditorHelpOverlay({ isLoading}) {
+function RoleAccessEditorHelpOverlay({ isLoading, helpComponent}) {
   const toastContext = useContext(DialogToastContext);
 
   const closePanel = () => {
     toastContext.clearOverlayPanel();
   };
 
+  if (helpComponent == null) {
+    return null;
+  }
+
   return (
-    <HelpOverlayBase handleClose={closePanel} showPanel={true} isLoading={isLoading} closePanel={closePanel}>
+    <HelpOverlayBase
+      handleClose={closePanel}
+      showPanel={true}
+      isLoading={isLoading}
+      closePanel={closePanel}
+      titleText={"Rule Base Access"}
+    >
       <div>This is a help document</div>
+      {helpComponent}
     </HelpOverlayBase>
   );
 }
 
 RoleAccessEditorHelpOverlay.propTypes = {
   isLoading: PropTypes.bool,
+  helpComponent: PropTypes.node
 };
 
 export default RoleAccessEditorHelpOverlay;
