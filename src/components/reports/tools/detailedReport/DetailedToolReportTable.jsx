3e04abbb6775b4fd4678ae4f544ceb00a55b1750
Jim Provenza <jim@Jims-MacBook-Pro.local>
2021-04-13 17:02:45 -0700

    update detailed tool report screen

--- src/components/reports/tools/detailedReport/DetailedToolReportTable.jsx
+++ src/components/reports/tools/detailedReport/DetailedToolReportTable.jsx
@@ -1,60 +1,90 @@
 import React, {useMemo} from "react";
 import PropTypes from "prop-types";
 import CustomTable from "components/common/table/CustomTable";
+import { useHistory } from "react-router-dom";
 import {
-  getContactArrayColumn,
-  getNameValueArrayColumn, getRoleArrayColumn,
-  getStringifiedArrayColumn,
-  getTableTextColumn, getTagArrayColumn
+  getLimitedContactArrayColumn,
+  getNameValueArrayColumn,
+  getLimitedStringifiedArrayColumn,
+  getTableTextColumn, 
+  getLimitedTagArrayColumn,
+  getTableBooleanIconColumn,
+  getLimitedTableTextColumn
 } from "components/common/table/table-column-helpers";
 import {getField} from "components/common/metadata/metadata-helpers";
 import toolMetadata from "components/inventory/tools/tool-metadata";
+import FilterContainer from "components/common/table/FilterContainer";
+import {faTools} from "@fortawesome/pro-light-svg-icons";
+import ExportDetailedToolReportButton from 'components/common/buttons/export/reports/ExportDetailedToolReportButton';
 
-function DetailedToolReportTable({ data, isLoading }) {
+
+function DetailedToolReportTable({ data, loadData, isLoading }) {
+  const history = useHistory();
   let fields = toolMetadata.fields;
 
   const columns = useMemo(
     () => [
       getTableTextColumn(getField(fields, "name")),
-      getTableTextColumn(getField(fields, "description"),"force-text-wrap"),
-      getTableTextColumn(getField(fields, "owner_name")),
-      getContactArrayColumn(getField(fields, "contacts"), "force-text-wrap"),
+      getTableTextColumn(getField(fields, "owner_name"),"no-wrap-inline pr-4"),
+      getLimitedTableTextColumn(getField(fields, "description"),60,"no-wrap-inline"),
+      getLimitedContactArrayColumn(getField(fields, "contacts"),25, "no-wrap-inline"),
       getNameValueArrayColumn(getField(fields, "applications"), "force-text-wrap"),
-      getNameValueArrayColumn(getField(fields, "location"),"force-text-wrap"),
-      getNameValueArrayColumn(getField(fields, "organization"),"force-text-wrap"),
-      getStringifiedArrayColumn(getField(fields, "external_reference"),"force-text-wrap"),
-      getTagArrayColumn(getField(fields, "tags"), "force-text-wrap"),
-      getNameValueArrayColumn(getField(fields, "licensing"),"force-text-wrap"),
+      getNameValueArrayColumn(getField(fields, "location"),"no-wrap-inline"),
+      getNameValueArrayColumn(getField(fields, "organization"),"no-wrap-inline"),
+      getLimitedTagArrayColumn(getField(fields, "tags"), 60, "force-text-wrap"),
+      getLimitedStringifiedArrayColumn(getField(fields, "external_reference"), 50),
+      getNameValueArrayColumn(getField(fields, "licensing"),"no-wrap-inline"),
       getNameValueArrayColumn(getField(fields, "compliance"), "force-text-wrap"),
       getTableTextColumn(getField(fields, "costCenter")),
-      getTableTextColumn(getField(fields, "active")),
-      getTableTextColumn(getField(fields, "status")),
-
+      getTableBooleanIconColumn(getField(fields, "active")),
     ],
     []
   );
 
-  // const rowStyling = (row) => {
-  //   return row["count"] === 0 ? " inactive-row" : "";
-  // };
+  console.log(data);
 
+  const onRowSelect = (rowData) => {
+    history.push("/inventory/tools/details/" + rowData.original._id);
+  };
+
+  const rowStyling = (row) => {
+    return row["count"] === 0 ? " inactive-row" : "";
+  };
+
+const getDetailedToolReportTable = () => {
   return (
-    <div className="px-2 pb-2">
       <CustomTable
-        columns={columns}
+        className="table-no-border"
+        onRowSelect={onRowSelect}
         data={data}
-        // rowStyling={rowStyling}
+        rowStyling={rowStyling}
+        columns={columns}
         isLoading={isLoading}
-        tableTitle={"Detailed Tool Report"}
-        type={"Detailed Tool Report"}
+        loadData={loadData}
       />
-    </div>
   );
+};
+
+return (
+  <FilterContainer
+    loadData={loadData}
+    supportSearch={false}
+    isLoading={isLoading}
+    body={getDetailedToolReportTable()}
+    metadata={toolMetadata}
+    titleIcon={faTools}
+    title={"Tools"}
+    className={"px-2 pb-2"}
+    exportButton={<ExportDetailedToolReportButton className={"ml-2"} isLoading={isLoading} toolData={data} />}
+  />
+);
+ 
 }
 
 DetailedToolReportTable.propTypes = {
   data: PropTypes.array,
-  isLoading: PropTypes.bool
+  isLoading: PropTypes.bool,
+  loadData: PropTypes.func,
 };
 
 export default DetailedToolReportTable;
