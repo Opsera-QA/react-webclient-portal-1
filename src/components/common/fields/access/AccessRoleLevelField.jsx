698ee936a6df525a777926be1de56f7e0454d111
Noah Champoux <noah@opsera.io>
2021-04-15 11:33:34 -0400

    Git Task Enhancements/Cleanup

---
+++ src/components/common/fields/access/AccessRoleLevelField.jsx
@@ -0,0 +1,28 @@
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
+import {getUserRoleLevel} from "components/common/helpers/role-helpers";
+import {AuthContext} from "contexts/AuthContext";
+
+function AccessRoleLevelField({accessRoleData, objectRoles, dataObject, className}) {
+  const { featureFlagHideItemInProd } = useContext(AuthContext);
+
+  if (accessRoleData == null || featureFlagHideItemInProd()) {
+    return null;
+  }
+
+  return (
+    <span className={className}>
+      <label className="mr-2 text-muted">Role Access:</label>
+      {getUserRoleLevel(accessRoleData, objectRoles, dataObject)}
+    </span>
+  );
+}
+
+AccessRoleLevelField.propTypes = {
+  objectRoles: PropTypes.array,
+  accessRoleData: PropTypes.object,
+  dataObject: PropTypes.object,
+  className: PropTypes.string
+};
+
+export default AccessRoleLevelField;
