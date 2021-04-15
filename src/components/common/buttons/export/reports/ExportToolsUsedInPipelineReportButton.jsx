13bf8f24e564efa048648f86f858083cea33f715
Jim Provenza <jim@Jims-MacBook-Pro.local>
2021-04-15 13:40:24 -0700

    Add ExportToolsUsedInPipelineReportButton and ToolsUsed in Pipeline Table. Update ToolUsedInPipelineField and ToolsUsedInPipelineReport.

---
+++ src/components/common/buttons/export/reports/ExportToolsUsedInPipelineReportButton.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import PropTypes from "prop-types";
+import "jspdf-autotable";
+import Button from "react-bootstrap/Button";
+import {faFileDownload} from "@fortawesome/pro-light-svg-icons";
+import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import ExportReportsDataModal from "components/common/modal/export_data/ExportReportsDataModal";
+
+function ExportToolsUsedInPipelineReportButton({isLoading, pipelineData, className}) {
+  const [showExportModal, setShowExportModal] = useState(false);
+
+  const closeModal = () => {
+    setShowExportModal(false);
+  };
+
+  const rawDataResults = () =>{
+    return pipelineData ? pipelineData.map(item => JSON.stringify(item)) : "export failure";
+   };
+
+  const formatPipelineData = () => {
+    let formattedData = pipelineData;
+
+    //any data formatting goes here
+
+    return formattedData;
+  };
+
+  // TODO: Refine when more is complete
+  return (
+    <>
+      <TooltipWrapper innerText={"Export"}>
+        <div className={className}>
+          <Button
+            variant={"outline-primary"}
+            size={"sm"}
+            disabled={isLoading}
+            onClick={() => setShowExportModal(true)}>
+            <span><FontAwesomeIcon fixedWidth icon={faFileDownload}/></span>
+          </Button>
+        </div>
+      </TooltipWrapper>
+      <ExportReportsDataModal
+        showModal={showExportModal}
+        closeModal={closeModal}
+        setParentVisibility={setShowExportModal}
+        isLoading={isLoading}
+        formattedData={formatPipelineData()}
+        rawData={rawDataResults()}
+       />
+    </>
+  );
+}
+
+ExportToolsUsedInPipelineReportButton.propTypes = {
+  pipelineData: PropTypes.array,
+  isLoading: PropTypes.bool,
+  className: PropTypes.string
+};
+
+export default ExportToolsUsedInPipelineReportButton;
