2e42a1c92df9916cac053fa2c7492a2da49deaee
Faseeh <faseeh@opsera.io>
2021-04-26 20:02:50 +0530

    refactoring the docker push step WIP

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/docker_push/inputs/DockerPushJenkinsJobInput.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import JenkinsJobInput from "components/common/list_of_values_input/tools/jenkins/JenkinsJobInput";
+
+function DockerPushJenkinsJobInput({dataObject, setDataObject, disabled}) {
+  const setJenkinsJob = (fieldName, selectedOption) => {    
+    let newDataObject = {...dataObject};
+    newDataObject.setData("toolJobName", selectedOption.name);
+    newDataObject.setData("toolJobId", selectedOption._id);
+    newDataObject.setData("jobType", selectedOption.type[0]);
+    newDataObject.setData("jobName", "");
+    newDataObject.setData("agentLabels", selectedOption.configuration?.agentLabels || "");    
+    setDataObject({...newDataObject});
+  };
+
+  return (
+     <JenkinsJobInput
+       fieldName={"toolJobName"}
+       jenkinsId={dataObject?.getData("toolConfigId")}
+       typeFilter={"DOCKER PUSH"}
+       configurationRequired={true}
+       dataObject={dataObject}
+       setDataObject={setDataObject}
+       setDataFunction={setJenkinsJob}
+       disabled={disabled}
+     />
+  );
+}
+
+DockerPushJenkinsJobInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default DockerPushJenkinsJobInput;
