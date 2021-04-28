b92a8cf31a0587ddc03134a65c2ef72a42b536e4
Vignesh Subramanian <vignesh@opsera.io>
2021-04-28 22:26:33 +0530

    octopus iis deploy changes

--- src/components/inventory/tools/tool_details/tool_jobs/octopus/octopus-target-metadata.js
+++ src/components/inventory/tools/tool_details/tool_jobs/octopus/octopus-target-metadata.js
@@ -67,7 +67,110 @@ const OctopusTargetMetadata = {
     {
       label: "ID",
       id: "id"
-    }
+    },
+    {
+      label: "Thumbprint",
+      id: "thumbprint"
+    },
+    {
+      label: "Host Name",
+      id: "hostName"
+    },
+    {
+      label: "Port",
+      id: "port"
+    },
+    {
+      label: "Health Status",
+      id: "healthStatus"
+    },
+],
+fieldsIis: [
+  {
+    label: "type",
+    id: "type",
+    isRequired: true
+  },
+  {
+    label: "AWS Credentials",
+    id: "awsToolConfigId"
+  },
+  {
+    label: "Octopus Configured Account",
+    id: "accountId"    
+  },
+  {
+    label: "Cluster Name",
+    id: "clusterName"
+  },
+  {
+    label: "Environments",
+    id: "environmentIds",
+    isRequired: true
+  },
+  {
+    label: "Name",
+    id: "name",
+    isRequired: true
+  },
+  {
+    label: "Space ID",
+    id: "spaceId",
+    isRequired: true
+  },
+  {
+    label: "Space Name",
+    id: "spaceName",
+    isRequired: true
+  },
+  {
+    label: "Communication Style",
+    id: "communicationStyle"
+  },
+  {
+    label: "Resource Group Name",
+    id: "resourceGroupName"
+  },
+  {
+    label: "Web App Name",
+    id: "webAppName"
+  },
+  {
+    label: "Web App Slot Name",
+    id: "webAppSlotName"
+  },
+  {
+    label: "Cloud Type",
+    id: "cloudType",
+    isRequired: true
+  },
+  {
+    label: "Active",
+    id: "active",
+  },
+  {
+    label: "ID",
+    id: "id"
+  },
+  {
+    label: "Thumbprint",
+    id: "thumbprint"
+  },
+  {
+    label: "Host Name",
+    id: "hostName",
+    isRequired: true
+  },
+  {
+    label: "Port",
+    id: "port",
+    isRequired: true
+  },
+  {
+    label: "Health Status",
+    id: "healthStatus",
+    isRequired: true
+  },
 ],
   newModelBase: {
     _id: "",
@@ -87,6 +190,10 @@ const OctopusTargetMetadata = {
     cloudType: "",
     id: "",
     active: true,
+    thumbprint: "",
+    hostName: "",
+    port: "10933",
+    healthStatus: ""
   }
 };
 
