5abbe26340fe95557f68f254acc2b7c9588df777
Vignesh Subramanian <vignesh@opsera.io>
2021-05-03 19:41:00 +0530

    git task summary card bug fix

--- src/components/git/git_task_details/configuration_forms/branch-to-branch/BranchToBranchGitTaskTypeSummaryCard.jsx
+++ src/components/git/git_task_details/configuration_forms/branch-to-branch/BranchToBranchGitTaskTypeSummaryCard.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import "components/inventory/tools/tools.css";
 import TextFieldBase from "components/common/form_fields/TextFieldBase";
 import GitTasksSummaryCardContainer from "components/git/git_task_details/configuration_forms/GitTasksSummaryCardContainer";
-
+import FieldContainer from "components/common/fields/FieldContainer";
+import FieldLabel from "components/common/fields/FieldLabel";
 function BranchToBranchGitTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurationData, isLoading }) {
 
   console.log('BranchToBranchGitTaskTypeSummaryCard');
@@ -22,8 +23,13 @@ function BranchToBranchGitTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurati
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"repository"} />
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"sourceBranch"} />        
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"gitBranch"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"deleteSourceBranch"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"reviewers"} />
+        <FieldContainer className="mb-2">
+          <div className="w-100 d-flex">
+            <FieldLabel field={gitTaskConfigurationData.getFieldById("deleteSourceBranch")}/>
+            {gitTaskConfigurationData.getData("deleteSourceBranch") ? <span>True</span> : <span>False</span>}
+          </div>
+        </FieldContainer>
+        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"reviewerNames"} />
       </div>
     </GitTasksSummaryCardContainer>
   );
