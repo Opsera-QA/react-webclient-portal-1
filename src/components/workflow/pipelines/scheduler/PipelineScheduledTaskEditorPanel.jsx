c0899749bb04ee8ace0b05d77d43fa37fe562cc7
Noah Champoux <noah@opsera.io>
2021-05-05 09:06:22 -0400

    Created NewPipelineScheduledTaskOverlay, PipelineScheduledTaskEditorPanel

---
+++ src/components/workflow/pipelines/scheduler/PipelineScheduledTaskEditorPanel.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import { AuthContext } from "contexts/AuthContext";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import EditorPanelContainer from "components/common/panels/detail_panel_container/EditorPanelContainer";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
+import axios from "axios";
+import pipelineSchedulerActions from "components/workflow/pipelines/scheduler/pipeline-scheduler-actions";
+
+function PipelineScheduledTaskEditorPanel({ scheduledTaskData, handleClose }) {
+  const { getAccessToken } = useContext(AuthContext);
+  const [schedulerTaskModel, setSchedulerTaskModel] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    loadData().catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async () => {
+    setIsLoading(true);
+    setSchedulerTaskModel(scheduledTaskData);
+    setIsLoading(false);
+  };
+
+  const createScheduledTask = async () => {
+    return await pipelineSchedulerActions.createSchedule(getAccessToken, cancelTokenSource, schedulerTaskModel);
+  };
+
+  const updateScheduledTask = async () => {
+    return await pipelineSchedulerActions.updateSchedule(getAccessToken, cancelTokenSource, schedulerTaskModel);
+  };
+
+  return (
+    <EditorPanelContainer
+      recordDto={schedulerTaskModel}
+      createRecord={createScheduledTask}
+      updateRecord={updateScheduledTask}
+      setRecordDto={setSchedulerTaskModel}
+      isLoading={isLoading}
+      handleClose={handleClose}
+    >
+      <Row>
+        <Col lg={6}>
+          <TextInputBase setDataObject={setSchedulerTaskModel} dataObject={schedulerTaskModel} fieldName={"name"}/>
+        </Col>
+        <Col lg={6}>
+          <TextInputBase setDataObject={setSchedulerTaskModel} dataObject={schedulerTaskModel} fieldName={"notes"}/>
+        </Col>
+        <Col lg={12}>
+          <TextInputBase setDataObject={setSchedulerTaskModel} dataObject={schedulerTaskModel} fieldName={"description"}/>
+        </Col>
+      </Row>
+    </EditorPanelContainer>
+  );
+}
+
+PipelineScheduledTaskEditorPanel.propTypes = {
+  scheduledTaskData: PropTypes.object,
+  setSchedulerTaskModel: PropTypes.func,
+  handleClose: PropTypes.func
+};
+
+export default PipelineScheduledTaskEditorPanel;
+
+
