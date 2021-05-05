2424dec291e03b0223ac9d320c1706ca835933d0
Noah Champoux <noah@opsera.io>
2021-05-05 08:24:25 -0400

    Added PipelineSchedulerActions (for future use)

---
+++ src/components/workflow/pipelines/scheduler/pipeline-scheduler-actions.js
@@ -0,0 +1,40 @@
+import baseActions from "utils/actionsBase";
+import {axiosApiService} from "api/apiService";
+
+const pipelineSchedulerActions = {};
+
+pipelineSchedulerActions.getScheduledTasks = async (getAccessToken, cancelTokenSource, pipelineId) => {
+  const urlParams = {
+    params: {
+      pipelineId: pipelineId,
+    }
+  };
+  const apiUrl = `/scheduler/`;
+
+  return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl, urlParams);
+};
+
+pipelineSchedulerActions.createSchedule = async (getAccessToken, cancelTokenSource, scheduledTaskModel) => {
+  const postBody = {
+    ...scheduledTaskModel.getPersistData()
+  };
+  const apiUrl = `/scheduler/`;
+
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
+
+pipelineSchedulerActions.updateSchedule = async (getAccessToken, cancelTokenSource, scheduledTaskModel) => {
+  const postBody = {
+    ...scheduledTaskModel.getPersistData()
+  };
+  const apiUrl = `/scheduler/${scheduledTaskModel.getData("_id")}/update`;
+
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
+
+pipelineSchedulerActions.deleteScheduledTask = async (getAccessToken, cancelTokenSource, scheduledTaskId) => {
+  let apiUrl = `/scheduler/${scheduledTaskId}`;
+  return await baseActions.apiDeleteCallV2(getAccessToken, cancelTokenSource, apiUrl);
+};
+
+export default pipelineSchedulerActions;
