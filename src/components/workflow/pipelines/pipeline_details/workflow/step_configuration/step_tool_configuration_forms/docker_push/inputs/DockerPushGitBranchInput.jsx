e32431bc2b2c27e0a0692424cddf5385a7649b6f
Faseeh <faseeh@opsera.io>
2021-05-06 22:59:16 +0530

    re written docker push step and added support for ecr repositories

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/docker_push/inputs/DockerPushGitBranchInput.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import GitBranchInput from "components/common/list_of_values_input/tools/git/GitBranchInput";
+
+function DockerPushGitBranchInput({dataObject, setDataObject, disabled}) {
+  const setBranch = (fieldName, selectedOption) => {
+    let newDataObject = {...dataObject};
+    // newDataObject.setData("branch", selectedOption);
+    newDataObject.setData("gitBranch", selectedOption);
+    setDataObject({...newDataObject});
+  };
+  return (
+     <GitBranchInput
+       fieldName={"gitBranch"}
+       service={dataObject.getData("service")}
+       gitToolId={dataObject.getData("gitToolId")}
+       workspace={dataObject.getData("workspace")}
+       repoId={dataObject.getData("repoId")}
+       dataObject={dataObject}
+       setDataFunction={setBranch}
+       setDataObject={setDataObject}
+       disabled={disabled}
+     />
+  );
+}
+
+DockerPushGitBranchInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default DockerPushGitBranchInput;
