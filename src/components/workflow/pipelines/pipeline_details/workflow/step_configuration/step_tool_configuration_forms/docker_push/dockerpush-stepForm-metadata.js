25bc0bb8de2443e1a35dda17d4f07fcc90d36f66
Faseeh <faseeh@opsera.io>
2021-05-06 19:00:42 +0530

    cleaned up the step config form

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/docker_push/dockerpush-stepForm-metadata.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/docker_push/dockerpush-stepForm-metadata.js
@@ -1,8 +1,8 @@
 const dockerPushStepFormMetadata = {
-    type: "Command Line Tool Configuration",
+    type: "Docker Push Tool Configuration",
     fields: [
       {
-        label: "Select Jenkins Tool",
+        label: "Jenkins Tool",
         id: "toolConfigId",
         isRequired: true
       },
@@ -18,126 +18,28 @@ const dockerPushStepFormMetadata = {
       },
       
       {
-        // label: "Jenkins Job Name",
+        label: "Job Name",
         id: "jobName",
       },
       {
-        // label: "Jenkins Job Id",
+        label: "Job",
         id: "toolJobId",
         isRequired: true
       },
       {
-        // label: "Jenkins Job Type",
+        label: "Job Type",
         id: "toolJobType",
         // isRequired: true
       },
       {
-        // label: "Step Job Type",
+        label: "Job Type",
         id: "jobType",
         isRequired: true
-      },
-      {
-        label: "Select SCM Type",
-        id: "type",
-        isRequired: true
-      },
-      
-      {
-        label: "Select Account",
-        id: "gitCredential",
-        isRequired: true
-      },
-      {
-        // label: "Select Account",
-        id: "gitToolId",
-        isRequired: true
-      },
-      {
-        id: "repoId",
-      },
-      
-      {
-        // label: "Type of GIT Service",
-        id: "service",
-        isRequired: true
-      },
-      
-      {
-        id: "projectId",
-      },
-      {
-        id: "gitUrl",
-        // isRequired: true
-      },
-      
-      {
-        id: "sshUrl",
-        // isRequired: true
-      },
-      
-      {
-        label: "Repository",
-        id: "repository",
-        isRequired: true
-      },
-      
-      {
-        label: "Workspace",
-        id: "workspace",
-        // isRequired: true
-      },
-      {
-        label: "Workspace/Project",
-        id: "workspaceName",
-        // isRequired: true
-      },
-  
-      {
-        label: "Branch",
-        id: "gitBranch",
-        isRequired: true
-      },
-      
-      {
-        label: "Want to specifiy the path for a script file?",
-        id: "sourceScript",
-      },
-      {
-        label: "Commands",
-        id: "commands",
-        isRequired: true,
-        formText: "A platform-specific script, which will be executed as .cmd file on Windows or as a shellscript in Unix-like environments. Multiple commands are supported (each line indicates a new command)"
       },
       {
         label: "Dependency",
         id: "dependencyType",
         // isRequired: true
-      },
-      {
-        label: "Script File Path",
-        id: "scriptFilePath",
-        // isRequired: true
-      },
-      {
-        label: "Script File Name",
-        id: "scriptFileName",
-        formText: "File name with extension is expected."
-        // isRequired: true
-      },
-      {
-        label: "Output Path",
-        id: "outputPath",
-        // isRequired: true
-      },
-      {
-        label: "Output File Name",
-        id: "outputFileName",
-        formText: "File name with extension is expected."
-        // isRequired: true
-      },
-      {
-        label: "Delete workspace before building",
-        id: "workspaceDeleteFlag",
       },
       {
         id: "dependencies",
@@ -153,8 +55,9 @@ const dockerPushStepFormMetadata = {
         id: "autoScaleEnable"
       },
       {
-        label: "AWS Tool",
-        id: "awsToolConfigId"
+        label: "AWS Credentials",
+        id: "awsToolConfigId",
+        isRequired: true
       },
       {
         label: "Build Step Info",
@@ -166,55 +69,28 @@ const dockerPushStepFormMetadata = {
         id: "newRepo",
       },
       {
-        label: "Select ECR Repository",
+        label: "ECR Repository",
         id: "ecrRepoName",
+        isRequired: true
       }
         
     ],
     newModelBase: {
-  
       type: "",
-  
       jobType: "", //hardcoded, every step wil have a hardcoded jobType is what i know needs to check with Todd.
       toolConfigId: "",
       toolName: "",
       jobName: "",
-  
       toolJobId: "",
-       
-      projectId: "",
-    
       buildType: "", //hardcoded now but needs to get it from a dropdown
-      gitToolId: "",
-      repoId: "",
-      gitUrl: "",
-      sshUrl: "",
-      service: "",
-  
-      gitCredential: "",  // name given on jenkins
-  
-      workspace: "",
-      workspaceName: "",
-      repository: "",
-      gitBranch: "",
-
       awsToolConfigId: "",
-      
       agentLabels: "",
       autoScaleEnable: false,
-  
       newRepo: false,
       ecrRepoName: "",
-
-      outputPath: "",
-      outputFileName: "",
       dependencies: {},
       dependencyType:"",
       buildStepId: "",
-      sourceScript: false,
-      inputPath: "",
-      inputFileName: "",
-      commands: "",
     }
   };
   
