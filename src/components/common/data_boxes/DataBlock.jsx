4efbe261dd49875e09f9e8af0921bfe614d776fc
harsha-opsera <harsha@opsera.io>
2021-05-13 11:06:38 -0700

    Reverting changes made to datablock file

--- src/components/common/data_boxes/DataBlock.jsx
+++ src/components/common/data_boxes/DataBlock.jsx
@@ -3,34 +3,27 @@ import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
 import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
 
-function DataBlock({
-  title,
-  subTitle,
-  toolTipText,
-  clickAction,
-  statusColor,
-  ellipsesContent,
-  footerText,
-  modal,
-  view,
-}) {
+function DataBlock({ title, subTitle, toolTipText, clickAction, statusColor, 
+                   ellipsesContent, footerText, modal, view }) {
   const statusColors = {
     success: "#00897b",
     danger: "#E57373",
-    warning: "#F1AD0F",
+    warning: "#F1AD0F"
   };
 
   return (
     <>
       <TooltipWrapper innerText={toolTipText}>
-        <Card
-          className="box-metric pointer"
-          style={{ height: view !== "small" ? "100px" : "auto", maxWidth: "250px" }}
-          onClick={clickAction}
-        >
+        <Card className="box-metric pointer"
+              style={{ height: view !== "small" ? "100px" : "auto", maxWidth: "250px"}}
+              onClick={clickAction}>
           <Card.Body className="data-blocks-body">
-            <div className="data-blocks-status" style={{ backgroundColor: statusColors[statusColor] }}></div>
-            <Card.Title className="data-blocks-title">{title}</Card.Title>
+            <div className="data-blocks-status"
+                style={{backgroundColor: statusColors[statusColor]}}>      
+            </div>
+            <Card.Title className="data-blocks-title">
+              {title}
+            </Card.Title>
             <Card.Subtitle className="data-blocks-subtitle">
               {subTitle && <div>{subTitle}</div>}
               {footerText && <div>{footerText}</div>}
@@ -39,7 +32,7 @@ function DataBlock({
           </Card.Body>
         </Card>
       </TooltipWrapper>
-      {modal}
+      {modal} 
     </>
   );
 }
@@ -53,7 +46,7 @@ DataBlock.propTypes = {
   ellipsesContent: PropTypes.node,
   footerText: PropTypes.node,
   modal: PropTypes.node,
-  view: PropTypes.string,
+  view: PropTypes.string
 };
 
 export default DataBlock;
