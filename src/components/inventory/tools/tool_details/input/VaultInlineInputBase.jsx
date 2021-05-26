64435093fa6384c2e269009e7ca57b4d63b65b9f
Shrey Malhotra <shrey@opsera.io>
2021-05-26 14:01:27 -0700

    updates based on feedback

--- src/components/inventory/tools/tool_details/input/VaultInlineInputBase.jsx
+++ src/components/inventory/tools/tool_details/input/VaultInlineInputBase.jsx
@@ -1,37 +1,62 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTools } from "@fortawesome/free-solid-svg-icons";
 import ButtonTooltip from "../../../../common/tooltip/ButtonTooltip";
 import FieldContainer from "../../../../common/fields/FieldContainer";
 import FieldLabel from "../../../../common/fields/FieldLabel";
 import PipelineActions from "../../../../workflow/pipeline-actions";
 import { AuthContext } from "../../../../../contexts/AuthContext";
 import { DialogToastContext } from "../../../../../contexts/DialogToastContext";
-import { faSpinner } from "@fortawesome/pro-light-svg-icons";
+import { faSpinner, faTools } from "@fortawesome/pro-light-svg-icons";
+import axios from "axios";
 
-function VaultInlineInputBase({ dataObject, fieldName, visible }) {
-  const history = useHistory({ forceRefresh: true });
+function VaultInlineInputBase({ dataObject, fieldName, visible, disabled }) {
   const [field] = useState(dataObject.getFieldById(fieldName));
   const [vaultList, setVaultList] = useState([]);
   const [isVaultSearching, setIsVaultSearching] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const { getAccessToken } = useContext(AuthContext);
   const toastContext = useContext(DialogToastContext);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
 
   useEffect(() => {
-    loadData();
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    if (!disabled) {
+      loadData(source).catch((error) => {
+        if (isMounted?.current === true) {
+          throw error;
+        }
+      });
+    }
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
   }, []);
 
-  const loadData = async () => {
+  const loadData = async (cancelSource = cancelTokenSource) => {
     try {
       setIsLoading(true);
-      await fetchVaultDetails();
+      await fetchVaultDetails(cancelSource);
     } catch (error) {
-      toastContext.showLoadingErrorDialog(error);
+      if (isMounted?.current === true) {
+        console.error(error);
+        toastContext.showLoadingErrorDialog(error);
+      }
     } finally {
-      setIsLoading(false);
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -39,29 +64,24 @@ function VaultInlineInputBase({ dataObject, fieldName, visible }) {
     return null;
   }
 
-  const getVaultAccessConfigurationItems = () => {
-    if (dataObject?.getData(fieldName) == null || dataObject?.getData(fieldName)?.length === 0) {
-      return <span>This tool is using the Opsera provided Hashicorp Vault Instance</span>;
-    }
-
-    return isVaultSearching ? (
-      <span>
-        <FontAwesomeIcon icon={faSpinner} spin className="text-muted mr-1" fixedWidth /> Loading Vault Information{" "}
-      </span>
-    ) : vaultList &&
-      vaultList.length > 1 &&
-      vaultList[vaultList.findIndex((obj) => obj.id === dataObject?.getData(fieldName))] ? (
-      vaultList[vaultList.findIndex((obj) => obj.id === dataObject?.getData(fieldName))].name
-    ) : (
-      "User Specified Vault Instance In Use"
-    );
-  };
-
-  const fetchVaultDetails = async () => {
+  const fetchVaultDetails = async (cancelSource) => {
     setIsVaultSearching(true);
     try {
-      let results = await PipelineActions.getToolsList("hashicorp_vault", getAccessToken);
-
+      let results = await PipelineActions.getToolsListV2(getAccessToken, cancelSource, "hashicorp_vault");
+      if (results.data) {
+        let respObj = [];
+        let arrOfObj = results.data;
+        arrOfObj.map((item) => {
+          respObj.push({
+            name: item.name,
+            id: item._id,
+            configuration: item.configuration,
+            accounts: item.accounts,
+            jobs: item.jobs,
+          });
+        });
+        results = respObj;
+      }
       const filteredList = results.filter((el) => el.configuration !== undefined);
       filteredList.unshift({ id: "", name: "Opsera Default Hashicorp Vault" });
       if (filteredList) {
@@ -75,6 +95,50 @@ function VaultInlineInputBase({ dataObject, fieldName, visible }) {
     }
   };
 
+
+  const getVaultAccessConfigurationItems = () => {
+    if (dataObject?.getData(fieldName) == null || dataObject?.getData(fieldName)?.length === 0) {
+      return <span>This tool is using the Opsera provided Hashicorp Vault Instance</span>;
+    }
+
+    if (isVaultSearching) {
+      return (
+        <span>
+          <FontAwesomeIcon icon={faSpinner} spin className="text-muted mr-1" fixedWidth /> Loading Vault Information
+        </span>
+      );
+    }
+
+    if (Array.isArray(vaultList) && vaultList.length > 1) {
+      const currentValue = dataObject?.getData(fieldName);
+      const foundObject = vaultList.findIndex((obj) => obj.id === currentValue);
+
+      if (foundObject) {
+        return foundObject.name;
+      }
+    }
+    return "User Specified Vault Instance In Use";
+  };
+
+  const getToolDetailsButton = () => {
+    if (dataObject?.getData(fieldName)) {
+      return (
+        <div className={"ml-2 mt-2 text-muted"}>
+          <ButtonTooltip innerText={"View Vault Configuration"}>
+            <Link
+              to={`/inventory/tools/details/${dataObject.getData(fieldName)}`}
+              onClick={() => window.location.reload()}
+            >
+              <span>
+                <FontAwesomeIcon icon={faTools} className="pr-1" />
+              </span>
+            </Link>
+          </ButtonTooltip>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="role-access">
       <div className="d-flex">
@@ -85,17 +149,7 @@ function VaultInlineInputBase({ dataObject, fieldName, visible }) {
           </FieldContainer>
         </div>
         <div className="edit-button d-flex">
-          {dataObject?.getData(fieldName) && (
-            <div className={"ml-2 mt-2 text-muted"}>
-              <ButtonTooltip innerText={"View Vault Configuration"}>
-                <Link to={`/inventory/tools/details/${dataObject.getData(fieldName)}`} onClick={() => window.location.reload()}>
-                  <span>
-                    <FontAwesomeIcon icon={faTools} className="pr-1" />
-                  </span>
-                </Link>
-              </ButtonTooltip>
-            </div>
-          )}
+          {getToolDetailsButton()}
         </div>
       </div>
     </div>
@@ -109,11 +163,12 @@ VaultInlineInputBase.propTypes = {
   visible: PropTypes.bool,
   saveData: PropTypes.func,
   type: PropTypes.string,
-  helpComponent: PropTypes.object,
+  helpComponent: PropTypes.object
 };
 
 VaultInlineInputBase.defaultProps = {
   visible: true,
+  disabled: false
 };
 
 export default VaultInlineInputBase;
--- src/components/inventory/tools/tool_details/input/VaultSelectInput.jsx
+++ src/components/inventory/tools/tool_details/input/VaultSelectInput.jsx
@@ -1,35 +1,60 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
-import {DialogToastContext} from "contexts/DialogToastContext";
+import { DialogToastContext } from "contexts/DialogToastContext";
 import SelectInputBase from "components/common/inputs/select/SelectInputBase";
 import { AuthContext } from "contexts/AuthContext";
 import PipelineActions from "components/workflow/pipeline-actions";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExclamationCircle, faTools } from "@fortawesome/free-solid-svg-icons";
+import { faSpinner, faTools } from "@fortawesome/pro-light-svg-icons";
+import axios from "axios";
 
-function VaultToolSelectInput({ fieldName, dataObject, setDataObject, disabled, textField, valueField, tool_prop}) {
+function VaultToolSelectInput({ fieldName, dataObject, setDataObject, disabled, textField, valueField, tool_prop }) {
   const toastContext = useContext(DialogToastContext);
   const { getAccessToken } = useContext(AuthContext);
   const [vaultList, setVaultList] = useState([]);
   const [isVaultSearching, setIsVaultSearching] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [placeholder, setPlaceholder] = useState("Select a Vault Instance");
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
 
   useEffect(() => {
-    loadData();
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    if (!disabled) {
+      loadData(source).catch((error) => {
+        if (isMounted?.current === true) {
+          throw error;
+        }
+      });
+    }
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
   }, []);
 
-  const loadData = async () => {
+  const loadData = async (cancelSource = cancelTokenSource) => {
     try {
       setIsLoading(true);
-      await fetchVaultDetails();
-    }
-    catch (error) {
-      toastContext.showLoadingErrorDialog(error);
-    }
-    finally {
-      setIsLoading(false);
+      await fetchVaultDetails(cancelSource);
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        toastContext.showLoadingErrorDialog(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
     }
   };
 
@@ -37,24 +62,39 @@ function VaultToolSelectInput({ fieldName, dataObject, setDataObject, disabled, 
     if (dataObject.getData(fieldName) !== "") {
       return (
         <Link to={`/inventory/tools/details/${dataObject.getData(fieldName)}`}>
-          <span><FontAwesomeIcon icon={faTools} className="pr-1" />View Or Edit this Tool&apos;s Registry settings</span>
+          <span>
+            <FontAwesomeIcon icon={faTools} className="pr-1" />
+            View Or Edit this Tool&apos;s Registry settings
+          </span>
         </Link>
       );
     }
   };
 
-
-  const fetchVaultDetails = async () => {
+  const fetchVaultDetails = async (cancelSource) => {
     setIsVaultSearching(true);
     try {
-      let results = await PipelineActions.getToolsList("hashicorp_vault", getAccessToken);
-
+      let results = await PipelineActions.getToolsListV2(getAccessToken, cancelSource, "hashicorp_vault");
+      if (results.data) {
+        let respObj = [];
+        let arrOfObj = results.data;
+        arrOfObj.map((item) => {
+          respObj.push({
+            name: item.name,
+            id: item._id,
+            configuration: item.configuration,
+            accounts: item.accounts,
+            jobs: item.jobs,
+          });
+        });
+        results = respObj;
+      }
       const filteredList = results.filter((el) => el.configuration !== undefined);
-      filteredList.unshift({ id: "", name: "Opsera Default Hashicorp Vault"});
+      filteredList.unshift({ id: "", name: "Opsera Default Hashicorp Vault" });
       if (filteredList) {
         setVaultList(filteredList);
       }
-    } catch(error) {
+    } catch (error) {
       setPlaceholder("No User Vault Found");
       console.error(error);
       toastContext.showServiceUnavailableDialog();
@@ -77,9 +117,7 @@ function VaultToolSelectInput({ fieldName, dataObject, setDataObject, disabled, 
         placeholderText={placeholder}
         disabled={disabled || isLoading || (!isLoading && (vaultList == null || vaultList.length === 1))}
       />
-      <small className="text-muted ml-3">
-        {getInfoText()}
-      </small>
+      <small className="text-muted ml-3">{getInfoText()}</small>
     </div>
   );
 }
@@ -91,13 +129,13 @@ VaultToolSelectInput.propTypes = {
   disabled: PropTypes.bool,
   textField: PropTypes.string,
   valueField: PropTypes.string,
-  tool_prop: PropTypes.string
+  tool_prop: PropTypes.string,
 };
 
 VaultToolSelectInput.defaultProps = {
   valueField: "id",
   textField: "name",
-  fieldName: "vault"
+  fieldName: "vault",
 };
 
 export default VaultToolSelectInput;
