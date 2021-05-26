7b4a551b256afe64b4c9b344efccac7a7eef9ba9
Shrey Malhotra <shrey@opsera.io>
2021-05-25 22:17:14 -0700

    resolving file change

--- src/components/inventory/tools/tool_details/tool_jobs/jenkins/JenkinsToolConfiguration.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/jenkins/JenkinsToolConfiguration.jsx
@@ -55,7 +55,7 @@ function JenkinsToolConfiguration({ toolData }) {
       <VaultTextInput dataObject={jenkinsConfigurationDto} setDataObject={setJenkinsConfigurationDto} fieldName={"jAuthToken"} />
     );
   };
-  
+
   if (jenkinsConfigurationDto == null) {
     return <></>;
   }
