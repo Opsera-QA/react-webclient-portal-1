4497213f07e7eb40de902792c28c630c66293a24
Faseeh <faseeh@opsera.io>
2021-06-03 16:50:41 +0530

    minor fixes

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ebs/inputs/EBSKeyPairInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ebs/inputs/EBSKeyPairInput.jsx
@@ -3,12 +3,24 @@ import PropTypes from "prop-types";
 import AWSKeyPairSelectionInput from "components/common/list_of_values_input/tools/aws/AWSKeyPairSelectionInput";
 
 function EBSKeyPairInput({dataObject, setDataObject, disabled}) {
+  const setKeyPair = (fieldName, selectedOption) => {
+    let newDataObject = {...dataObject};
+    newDataObject.setData("ec2KeyName", selectedOption);
+    setDataObject({...newDataObject});
+  };
+  const clearDataFunction = () => {
+    let newDataObject = {...dataObject};
+    newDataObject.setData("ec2KeyName", "");
+    setDataObject({...newDataObject});
+  };
   return (
      <AWSKeyPairSelectionInput
        fieldName={"ec2KeyName"}
        awsToolId={dataObject.getData("awsToolConfigId")}
        dataObject={dataObject}
+       setDataFunction={setKeyPair}
        setDataObject={setDataObject}
+       clearDataFunction={clearDataFunction}
        disabled={disabled}
      />
   );
