3e2ccf60571271c8b283a50b38c26e0b68ad2167
Noah Champoux <noah@opsera.io>
2021-06-07 17:14:41 -0400

    Added PositiveIntegerNumberInput and added support for passing in minimum and maximum number for NumberInputBase

--- src/components/common/inputs/number/PositiveIntegerNumberInput.jsx
+++ src/components/common/inputs/number/PositiveIntegerNumberInput.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
+import regexHelpers from "utils/regexHelpers";
+import {matchesRegex} from "utils/helpers";
+import simpleNumberLocalizer from "react-widgets-simple-number";
 import InputContainer from "components/common/inputs/InputContainer";
 import InputLabel from "components/common/inputs/info_text/InputLabel";
-import InfoText from "components/common/inputs/info_text/InfoText";
 import NumberPicker from "react-widgets/lib/NumberPicker";
-import simpleNumberLocalizer from "react-widgets-simple-number";
+import InfoText from "components/common/inputs/info_text/InfoText";
 
-export const formatTypes = {
-  percent: '%'
-};
-
-// TODO: Move to /common/inputs/number
-function NumberInputBase({ fieldName, dataObject, setDataObject, disabled, placeholderText, formatType, setDataFunction, showLabel, minimum, maximum, className, precision }) {
+function PositiveIntegerNumberInput({ fieldName, className, dataObject, setDataObject, disabled, placeholderText, showLabel, minimum, maximum }) {
   const [field, setField] = useState(dataObject?.getFieldById(fieldName));
   const [errorMessage, setErrorMessage] = useState("");
   simpleNumberLocalizer();
 
   const validateAndSetData = (newValue) => {
+    const parsedValue = Math.trunc(newValue);
+
+    console.log("parsedValue: " + JSON.stringify(parsedValue));
+
+    if (typeof parsedValue !== "number") {
+      setErrorMessage("Not a Number");
+      return;
+    }
+
+    if (!matchesRegex(regexHelpers.regexTypes.numericalField, parsedValue)) {
+      setErrorMessage("Only Integers or 0 Allowed");
+      return;
+    }
+
     let newDataObject = dataObject;
-    newDataObject.setTextData(fieldName, newValue);
+    newDataObject.setTextData(fieldName, parsedValue);
     setErrorMessage(newDataObject.getFieldError(fieldName));
     setDataObject({...newDataObject});
   };
@@ -35,29 +46,30 @@ function NumberInputBase({ fieldName, dataObject, setDataObject, disabled, place
         disabled={disabled}
         value={dataObject.getData(fieldName)}
         className="max-content-width"
-        onChange={setDataFunction ? (newValue) => setDataFunction(field?.id, newValue) : (newValue) => validateAndSetData(newValue)}
+        onChange={(newValue) => validateAndSetData(newValue)}
         min={typeof minimum === "number" ? minimum : field?.minNumber}
         max={typeof minimum === "number" ? maximum : field?.maxNumber}
-        format={ formatType && formatTypes[formatType] != null ? formatTypes[formatType] : undefined}
       />
       <InfoText field={field} errorMessage={errorMessage}/>
     </InputContainer>
   );
 }
 
-NumberInputBase.propTypes = {
+PositiveIntegerNumberInput.propTypes = {
   placeholderText: PropTypes.string,
   fieldName: PropTypes.string,
   dataObject: PropTypes.object,
   setDataObject: PropTypes.func,
   disabled: PropTypes.bool,
   formatType: PropTypes.string,
-  setDataFunction: PropTypes.func,
   showLabel: PropTypes.bool,
   minimum: PropTypes.number,
   maximum: PropTypes.number,
-  className: PropTypes.string,
-  precision: PropTypes.number
+  className: PropTypes.string
 };
 
-export default NumberInputBase;
+PositiveIntegerNumberInput.defaultProps = {
+  minimum: 0
+};
+
+export default PositiveIntegerNumberInput;
