c30b509e7bb8cebc34f18d698fff9de1d4b74303
Noah Champoux <noah@opsera.io>
2021-06-10 09:24:33 -0400

    PositiveIntegerNumberInput enhancements

--- src/components/common/inputs/number/PositiveIntegerNumberInput.jsx
+++ src/components/common/inputs/number/PositiveIntegerNumberInput.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import regexHelpers from "utils/regexHelpers";
 import {matchesRegex} from "utils/helpers";
@@ -13,25 +13,41 @@ function PositiveIntegerNumberInput({ fieldName, className, dataObject, setDataO
   const [errorMessage, setErrorMessage] = useState("");
   simpleNumberLocalizer();
 
-  const validateAndSetData = (newValue) => {
-    const parsedValue = Math.trunc(newValue);
+  useEffect(() => {
+  }, [dataObject]);
 
-    console.log("parsedValue: " + JSON.stringify(parsedValue));
+  const validateAndSetData = (newValue) => {
+    let parsedValue = Math.trunc(newValue);
 
     if (typeof parsedValue !== "number") {
       setErrorMessage("Not a Number");
-      return;
+      parsedValue = dataObject.getData(fieldName);
     }
 
     if (!matchesRegex(regexHelpers.regexTypes.numericalField, parsedValue)) {
       setErrorMessage("Only Integers or 0 Allowed");
-      return;
+      parsedValue = dataObject.getData(fieldName);
+    }
+
+    if (parsedValue < 0) {
+      setErrorMessage("Only Positive Integers and 0 Allowed");
+      parsedValue = dataObject.getData(fieldName);
     }
 
     let newDataObject = dataObject;
     newDataObject.setTextData(fieldName, parsedValue);
     setErrorMessage(newDataObject.getFieldError(fieldName));
     setDataObject({...newDataObject});
+  };
+
+  const handleKeyPress = (event) => {
+    const allowedKeyCodes = [37, 38, 39, 40, 8, 9, 13, 46];
+    const breaksRules = /^[0-9]$/i.test(event.key) !== true && !allowedKeyCodes.includes(event.keyCode);
+
+    if (breaksRules === true) {
+      setErrorMessage("Only Positive Integers and 0 Allowed");
+      event.preventDefault();
+    }
   };
 
   if (field == null) {
@@ -47,10 +63,11 @@ function PositiveIntegerNumberInput({ fieldName, className, dataObject, setDataO
         value={dataObject.getData(fieldName)}
         className="max-content-width"
         onChange={(newValue) => validateAndSetData(newValue)}
+        onKeyDown={(event) => handleKeyPress(event)}
         min={typeof minimum === "number" ? minimum : field?.minNumber}
         max={typeof minimum === "number" ? maximum : field?.maxNumber}
       />
-      <InfoText field={field} errorMessage={errorMessage}/>
+      <InfoText field={field} errorMessage={errorMessage} customMessage={`${field?.label} must be 0 or a positive whole number`}/>
     </InputContainer>
   );
 }
