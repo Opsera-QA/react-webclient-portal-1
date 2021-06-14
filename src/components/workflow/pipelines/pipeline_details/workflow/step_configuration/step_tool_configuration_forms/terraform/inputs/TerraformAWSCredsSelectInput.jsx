7f109748a65a9fbe054f8c0321d5ccf880281c48
Jim Provenza <jim@opsera.io>
2021-06-14 12:44:40 -0700

    terraform changes

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/terraform/inputs/TerraformAWSCredsSelectInput.jsx
@@ -0,0 +1,55 @@
+import React,{useContext, useState, useEffect} from "react";
+import PropTypes from "prop-types";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+import { AuthContext } from "contexts/AuthContext";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import terraformStepActions from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/terraform/terraform-step-actions";
+
+function TerraformAWSCredsSelectInput({dataObject, setDataObject, disabled}) {
+  const { getAccessToken } = useContext(AuthContext);
+  const toastContext = useContext(DialogToastContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [credsList, setCredsList] = useState([]);
+
+  useEffect(() => {
+    getCredsList();
+  }, [dataObject.data.defaultBranch]);
+
+  const getCredsList = async () => {
+    setIsLoading(true);
+    if(dataObject?.data?.defaultBranch?.length){
+      try {
+        let filteredCredsList = await terraformStepActions?.fetchAWSDetails(getAccessToken);
+        setCredsList(filteredCredsList);
+      } catch (error) {
+        toastContext.showErrorDialog(error);
+      }
+    }
+    setIsLoading(false);
+    return;
+  };
+
+  return (
+    
+     <SelectInputBase
+       fieldName={"awsToolConfigId"}
+       dataObject={dataObject}
+       setDataObject={setDataObject}
+       selectOptions={credsList ? credsList : []}
+       valueField={"id"}
+       textField={"name"}
+       placeholderText={"Select Credentials"}
+       disabled={disabled || isLoading || credsList.length === 0}
+       busy={isLoading}
+     />
+  );
+}
+
+TerraformAWSCredsSelectInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+  className: PropTypes.string
+};
+
+export default TerraformAWSCredsSelectInput;
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/terraform/inputs/TerraformBitbucketWorkspaceInput.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import BitbucketWorkspaceInput from "components/common/list_of_values_input/tools/bitbucket/BitbucketWorkspaceInput";
+
+function TerraformBitbucketWorkspaceInput({dataObject, setDataObject, disabled}) {
+  const setWorkspace = (fieldName, selectedOption) => {
+    let newDataObject = {...dataObject};
+    newDataObject.setData("bitbucketWorkspace", selectedOption.key);
+    newDataObject.setData("bitbucketWorkspaceName", selectedOption.name);
+    setDataObject({...newDataObject});
+  };
+
+  if (dataObject.getData("type") !== "bitbucket") {
+    return <></>;
+  }
+
+  return (
+     <BitbucketWorkspaceInput
+       fieldName={"bitbucketWorkspaceName"}
+       gitToolId={dataObject.getData("gitToolId")}
+       dataObject={dataObject}
+       setDataObject={setDataObject}
+       setDataFunction={setWorkspace}
+       disabled={disabled}
+     />
+  );
+}
+
+TerraformBitbucketWorkspaceInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default TerraformBitbucketWorkspaceInput;
