0d87ec8797ae3ba9425e4640fabc7a241c6e6f58
harsha-opsera <harsha@opsera.io>
2021-06-23 01:11:31 -0700

    Adding actionable insights to Sonar Remediation Agg over time KPI

--- src/components/insights/charts/sonar/line_chart/reliability_remediation_effort_aggregation_by_time/SonarReliabilityRemediationEffortAggBytimeLineChart.jsx
+++ src/components/insights/charts/sonar/line_chart/reliability_remediation_effort_aggregation_by_time/SonarReliabilityRemediationEffortAggBytimeLineChart.jsx
@@ -9,6 +9,13 @@ import { AuthContext } from "contexts/AuthContext";
 import ChartContainer from "components/common/panels/insights/charts/ChartContainer";
 import { defaultConfig, getColor, assignStandardColors, shortenLegend } from "../../../charts-views";
 import ChartTooltip from "../../../ChartTooltip";
+import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
+import BlueprintLogOverlay from "components/blueprint/BlueprintLogOverlay";
+import VanityTable from "components/common/table/VanityTable";
+import LoadingDialog from "components/common/status_notifications/loading";
+import { getTableTextColumn } from "components/common/table/table-column-helpers-v2";
+import { getField } from "components/common/metadata/metadata-helpers";
+import { DialogToastContext } from "contexts/DialogToastContext";
 
 function SonarReliabilityRemediationEffortAggBytimeLineChart({
   kpiConfiguration,
@@ -20,10 +27,24 @@ function SonarReliabilityRemediationEffortAggBytimeLineChart({
   const { getAccessToken } = useContext(AuthContext);
   const [error, setError] = useState(undefined);
   const [metrics, setMetrics] = useState([]);
+  const [insights, setInsights] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  let toastContext = useContext(DialogToastContext);
+  const fields = [
+    { id: "pipelineName", label: "Pipeline Name" },
+    { id: "run_count", label: "Run Count" },
+    { id: "project", label: "Project" },
+    { id: "date", label: "Date" },
+  ];
+  const columns = [
+    getTableTextColumn(getField(fields, "pipelineName")),
+    getTableTextColumn(getField(fields, "run_count")),
+    getTableTextColumn(getField(fields, "project")),
+    getTableTextColumn(getField(fields, "date")),
+  ];
 
   useEffect(() => {
     if (cancelTokenSource) {
@@ -59,8 +80,22 @@ function SonarReliabilityRemediationEffortAggBytimeLineChart({
         dashboardTags
       );
       const dataObject =
-        response?.data && response?.data?.data[0]?.sonarReliabilityRemediationEffortAggregattionByTime.status === 200
+        response?.data && response?.data?.data[0]?.sonarReliabilityRemediationEffortAggregattionByTime?.status === 200
           ? response?.data?.data[0]?.sonarReliabilityRemediationEffortAggregattionByTime?.data
+          : [];
+
+      // API for Actionable insights
+      const insightsResponse = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "sonarReliabilityRemediationEffortAggByTimeActIns",
+        kpiConfiguration,
+        dashboardTags
+      );
+      const insightsDataObject =
+        insightsResponse?.data &&
+        insightsResponse?.data?.data[0]?.sonarReliabilityRemediationEffortAggByTimeActIns?.status === 200
+          ? insightsResponse?.data?.data[0]?.sonarReliabilityRemediationEffortAggByTimeActIns?.data
           : [];
 
       assignStandardColors(dataObject);
@@ -68,6 +103,9 @@ function SonarReliabilityRemediationEffortAggBytimeLineChart({
 
       if (isMounted?.current === true && dataObject) {
         setMetrics(dataObject);
+        if (insightsDataObject) {
+          setInsights(insightsDataObject);
+        }
       }
     } catch (error) {
       if (isMounted?.current === true) {
@@ -81,6 +119,52 @@ function SonarReliabilityRemediationEffortAggBytimeLineChart({
     }
   };
 
+  const getProjectsFromDate = (date) => {
+    let projectsData = insights[0]?.data;
+    if (projectsData.length) {
+      for (let thisDateObject of projectsData) {
+        if (thisDateObject.id === date) {
+          if (thisDateObject?.data) {
+            return thisDateObject.data;
+          }
+        }
+      }
+    }
+  };
+
+  const closePanel = () => {
+    toastContext.removeInlineMessage();
+    toastContext.clearOverlayPanel();
+  };
+
+  const onRowSelect = (grid, row) => {
+    toastContext.showOverlayPanel(<BlueprintLogOverlay pipelineId={row?.pipelineId} runCount={row?.run_count} />);
+  };
+
+  const getBody = (projects) => {
+    if (isLoading) {
+      return <LoadingDialog size={"sm"} message={"Loading Data"} />;
+    }
+    return <VanityTable className={"no-table-border"} data={projects} columns={columns} onRowSelect={onRowSelect} />;
+  };
+
+  const onClickHandler = (thisDataPoint) => {
+    let date = thisDataPoint?.data?.xFormatted;
+    let projects = getProjectsFromDate(date);
+    toastContext.showOverlayPanel(
+      <FullScreenCenterOverlayContainer
+        closePanel={closePanel}
+        showPanel={true}
+        titleText={date}
+        showToasts={true}
+        isLoading={false}
+        linkTooltipText={"View Full Blueprint"}
+      >
+        <div className={"p-3"}>{getBody(projects)}</div>
+      </FullScreenCenterOverlayContainer>
+    );
+  };
+
   const getChartBody = () => {
     if (!Array.isArray(metrics) || metrics.length === 0) {
       return null;
@@ -92,7 +176,7 @@ function SonarReliabilityRemediationEffortAggBytimeLineChart({
           data={metrics}
           {...defaultConfig("Remediation Effort Required (min)", "Date", false, true, "wholeNumbers", "monthDate")}
           {...config(getColor)}
-          onClick={() => setShowModal(true)}
+          onClick={(thisDataPoint) => onClickHandler(thisDataPoint)}
           tooltip={(node) => (
             <ChartTooltip
               titles={["Date", "Remediation Effort Required"]}
