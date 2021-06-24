b71463968163add6643c23f9d1594bb26bd34564
“Divyesha <divyesha@opsera.io>
2021-06-24 14:01:20 -0700

    cahge

--- src/components/insights/charts/sonar/sonar_ratings/SonarRatingsDebtRatioSummaryPanel.jsx
+++ src/components/insights/charts/sonar/sonar_ratings/SonarRatingsDebtRatioSummaryPanel.jsx
@@ -56,8 +56,7 @@ function SonarRatingsDebtRatioSummaryPanel({ dashboardData, kpiConfiguration, se
   }, [JSON.stringify(dashboardData)]);
   
   const onRowSelect = (rowData) => {
-    toastContext.clearOverlayPanel();
-    history.push(`/blueprint/${rowData.original.pipelineId}/${rowData.original.run_count}`);
+    toastContext.showOverlayPanel(<BlueprintLogOverlay pipelineId={rowData?.original?.pipelineId} runCount={rowData?.original?.run_count} />);
   };
 
   const loadData = async (cancelSource = cancelTokenSource, filterDto = tableFilterDto) => {
