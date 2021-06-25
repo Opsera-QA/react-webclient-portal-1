2c6358c6abe6f32cee000cb6785e9163ed0a58b8
Shrey Malhotra <shrey@opsera.io>
2021-06-25 12:42:25 -0700

    updating the summary page

--- src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/ECSCreationTaskTypeSummaryCard.jsx
+++ src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/ECSCreationTaskTypeSummaryCard.jsx
@@ -6,12 +6,62 @@ import GitTasksSummaryCardContainer from "components/git/git_task_details/config
 import FieldContainer from "components/common/fields/FieldContainer";
 import FieldLabel from "components/common/fields/FieldLabel";
 import BooleanField from "../../../../common/fields/boolean/BooleanField";
+import TextInputBase from "../../../../common/inputs/text/TextInputBase";
+import VPCSelectInput from "./inputs/VPCSelectInput";
+import SubnetSelectInput from "./inputs/SubnetSelectInput";
+import CodeFieldBase from "../../../../common/fields/code/CodeFieldBase";
 
 function EC2ClusterCreationTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurationData, isLoading }) {
 
   if (isLoading) {
     return <GitTasksSummaryCardContainer isLoading={isLoading} />;
   }
+
+  const getEC2VPCFields = () => {
+    if (gitTaskConfigurationData.getData("clusterTemplate") === "ec2") {
+      if (gitTaskConfigurationData.getData("createVpc")) {
+        return (
+          <>
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"vpcCidrBlock"} />
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_cidr_1"} />
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_cidr_2"} />
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"private_subnet_cidr_1"} />
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"private_subnet_cidr_2"} />
+          </>
+        );
+      } else {
+        return (
+          <>
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"vpcId"} />
+            <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"privateSubnets"} />
+          </>
+        );
+      }
+    }
+  };
+
+  const getEC2GenFields = () => {
+    if (gitTaskConfigurationData.getData("clusterTemplate") === "ec2") {
+      return (
+        <>
+          <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"keyPair"} />
+          <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"instanceType"} />
+          <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"securityGroup"} />
+        </>
+      );
+    }
+  };
+
+  const getFargateFields = () => {
+    if (gitTaskConfigurationData.getData("clusterTemplate") === "fargate") {
+      return (
+        <>
+          <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_1"} />
+          <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_2"} />
+        </>
+      );
+    }
+    };
 
   return (
     <GitTasksSummaryCardContainer gitTasksDataDto={gitTasksData} isLoading={isLoading}>
@@ -21,10 +71,9 @@ function EC2ClusterCreationTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurat
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"clusterName"} />
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"awsToolId"} />
         <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"clusterTemplate"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_1"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"public_subnet_2"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"private_subnet_cidr_1"} />
-        <TextFieldBase dataObject={gitTaskConfigurationData} fieldName={"private_subnet_cidr_2"} />
+        {getEC2VPCFields()}
+        {getEC2GenFields()}
+        {getFargateFields()}
       </div>
     </GitTasksSummaryCardContainer>
   );
