f4893b0dda127a731cc287322210eb740f7895d5
Shrey Malhotra <shrey@opsera.io>
2021-06-28 12:32:44 -0700

    updating certain into text fields based on talk with ravi

--- src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/ecs-creation-git-task-configuration.js
+++ src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/ecs-creation-git-task-configuration.js
@@ -5,7 +5,8 @@ const ec2ClusterCreationTaskConfigurationMetadata = {
   fields: [
     {
       label: "CIDR Block",
-      id: "vpcCidrBlock"
+      id: "vpcCidrBlock",
+      formText: "Sample Format - 10.0.0.0/16"
     },
     {
       label: "Cluster Template",
@@ -32,26 +33,32 @@ const ec2ClusterCreationTaskConfigurationMetadata = {
     {
       label: "Public Subnet 1",
       id: "public_subnet_1",
+      formText : "Sample Format - 10.0.0.0/24"
     },
     {
       label: "Public Subnet 2",
       id: "public_subnet_2",
+      formText : "Sample Format - 10.0.0.0/24"
     },
     {
       label: "Private Subnet CIDR 1",
       id: "private_subnet_cidr_1",
+      formText : "Sample Format - 10.194.20.0/24",
     },
     {
       label: "Private Subnet CIDR 2",
       id: "private_subnet_cidr_2",
+      formText : "Sample Format - 10.194.21.0/24",
     },
     {
       label: "Public Subnet CIDR 1",
       id: "public_subnet_cidr_1",
+      formText : "Sample Format - 10.194.10.0/24",
     },
     {
       label: "Public Subnet CIDR 2",
       id: "public_subnet_cidr_2",
+      formText : "Sample Format - 10.194.11.0/24",
     },
     {
       label: "AWS Tool",
@@ -82,18 +89,18 @@ const ec2ClusterCreationTaskConfigurationMetadata = {
   ],
   newObjectFields:
     {
-      vpcCidrBlock: "10.0.0.0/16",
+      vpcCidrBlock: "",
       createVpc: false,
       clusterName: "",
       publicSubnets: [],
       imageType: "",
       clusterTemplate: "",
-      public_subnet_1: "10.0.0.0/24",
-      public_subnet_2: "10.0.1.0/24",
-      private_subnet_cidr_1: "10.194.20.0/24",
-      private_subnet_cidr_2: "10.194.21.0/24",
-      public_subnet_cidr_1 : "10.194.10.0/24",
-      public_subnet_cidr_2 : "10.194.11.0/24",
+      public_subnet_1: "",
+      public_subnet_2: "",
+      private_subnet_cidr_1: "",
+      private_subnet_cidr_2: "",
+      public_subnet_cidr_1 : "",
+      public_subnet_cidr_2 : "",
       privateSubnets: [],
       awsToolId: "",
       securityGroup : "",
--- src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/inputs/SubnetSelectInput.jsx
+++ src/components/git/git_task_details/configuration_forms/ecs-cluster-creation/inputs/SubnetSelectInput.jsx
@@ -114,7 +114,7 @@ SubnetSelectInput.propTypes = {
 SubnetSelectInput.defaultProps = {
   fieldName: "privateSubnets",
   textField: "cidrBlock",
-  valueField: "cidrBlock",
+  valueField: "subnetId",
 };
 
 export default SubnetSelectInput;
