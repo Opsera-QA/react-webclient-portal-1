3a0f85bbf8337aa6cfb02553e526c072eb4cc774
carlos roman <carlos@opsera.io>
2021-07-01 12:27:15 -0700

    added the new category with the data block Lead Time, and the event to display a chart when clicking the new data block

---
+++ src/components/insights/summary/pipeline_details/JiraLeadTimeDataBlock.jsx
@@ -0,0 +1,126 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import { AuthContext } from "contexts/AuthContext";
+import axios from "axios";
+import chartsActions from "components/insights/charts/charts-actions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/pro-light-svg-icons";
+import InsightsSynopsisDataBlock from "components/common/data_boxes/InsightsSynopsisDataBlock";
+import Model from "core/data_model/model";
+import genericChartFilterMetadata from "components/insights/charts/generic_filters/genericChartFilterMetadata";
+
+function PipelineFailedSecurity({
+  dashboardData,
+  toggleDynamicPanel,
+  selectedDataBlock,
+  style,
+}) {
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metrics, setMetrics] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+//   const [tableFilterDto, setTableFilterDto] = useState(
+//     new Model(
+//       { ...genericChartFilterMetadata.newObjectFields },
+//       genericChartFilterMetadata,
+//       false
+//     )
+//   );
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [JSON.stringify(dashboardData)]);
+
+  const loadData = async ( cancelSource = cancelTokenSource ) => {
+    try {
+      setIsLoading(true);
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "jiraLeadTime"
+        // kpiConfiguration,
+        // dashboardTags
+      );
+      let dataObject =
+        response?.data && response?.data?.data[0]?.jiraLeadTime.status === 200
+          ? response?.data?.data[0]?.jiraLeadTime?.data
+          : [];
+
+      if (isMounted?.current === true && dataObject) {
+        setMetrics(dataObject);
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const onDataBlockSelect = () => {
+    toggleDynamicPanel("jiraLeadTime", metrics[0]?.data);
+  };
+
+  const getChartBody = () => {
+    return (
+      <div
+        className={
+          selectedDataBlock === "jiraLeadTime" ? "selected-data-block" : undefined }
+        style={style}
+      >
+        <InsightsSynopsisDataBlock
+          title={
+            !isLoading && metrics[0]? (
+                metrics[0].data[0].mean
+            ) : (
+              <FontAwesomeIcon
+                icon={faSpinner}
+                spin
+                fixedWidth
+                className="mr-1"
+              />
+            )
+          }
+          subTitle="Mean Lead Time (Days)"
+          toolTipText="Mean Lead Time (Days)"
+          clickAction={() => onDataBlockSelect()}
+        //   statusColor="danger"
+        />
+      </div>
+    );
+  };
+
+  return getChartBody();
+}
+
+PipelineFailedSecurity.propTypes = {
+  dashboardData: PropTypes.object,
+  toggleDynamicPanel: PropTypes.func,
+  selectedDataBlock: PropTypes.string,
+  style: PropTypes.object,
+};
+
+export default PipelineFailedSecurity;
