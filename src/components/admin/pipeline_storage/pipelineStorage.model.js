f36769663e371b001f353c5020e6da3242e47fad
Noah Champoux <noah@opsera.io>
2021-07-05 08:33:18 -0400

    Further Pipeline Storage Management work (in progress)

---
+++ src/components/admin/pipeline_storage/pipelineStorage.model.js
@@ -0,0 +1,36 @@
+import ModelBase, {DataState} from "core/data_model/model.base";
+import PipelineStorageActions from "components/admin/pipeline_storage/pipeline-storage-actions";
+
+export class PipelineStorageModel extends ModelBase {
+  constructor(data, metaData, newModel, getAccessToken, cancelTokenSource, loadData, canUpdate = false, canDelete = false, setStateFunction) {
+    super(data, metaData, newModel);
+    this.getAccessToken = getAccessToken;
+    this.cancelTokenSource = cancelTokenSource;
+    this.loadData = loadData;
+    this.updateAllowed = canUpdate;
+    this.deleteAllowed = canDelete;
+    this.setStateFunction = setStateFunction;
+  }
+
+  deleteModel = async () => {
+    const response = await PipelineStorageActions.deletePipelineStorageDataV2(this.getAccessToken, this.cancelTokenSource, this);
+    this.dataState = DataState.DELETED;
+    return response;
+  };
+
+  getDetailViewTitle = () => {
+    return `Pipeline Storage Details [${this.getData("_id")}]`;
+  };
+
+  getManagementScreenLink = () => {
+    return `/admin/pipeline-storage`;
+  };
+
+  getNewInstance = (newData = this.getNewObjectFields()) => {
+    return new PipelineStorageModel({...newData}, this.metaData, this.newModel, this.getAccessToken, this.cancelTokenSource, this.loadData, this.updateAllowed, this.deleteAllowed, this.setStateFunction);
+  };
+}
+
+export default PipelineStorageModel;
+
+
