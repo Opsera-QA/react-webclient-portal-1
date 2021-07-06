ccd6ecc774b9df4d197c3f8e0ced3bc457140847
Noah Champoux <noah@opsera.io>
2021-07-06 11:04:17 -0400

    Added new components/styling for Pipeline Details Overlay (not wired up). Added missing newObjectProperty to serviceNowStepNotificationMetadata

---
+++ src/components/workflow/pipelines/pipeline_details/PipelineStepDetailsOverview.jsx
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import PipelineStepConfigurationSummary
+  from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepConfigurationSummary";
+import PipelineStepDetailsContainer from "components/workflow/pipelines/pipeline_details/PipelineStepDetailsContainer";
+import PipelineStepJsonPanel
+  from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepJsonPanel";
+import CustomTabContainer from "components/common/tabs/CustomTabContainer";
+import SummaryTab from "components/common/tabs/detail_view/SummaryTab";
+import JsonTab from "components/common/tabs/detail_view/JsonTab";
+import GeneralTabPanelContainer from "components/common/panels/general/GeneralTabPanelContainer";
+import CustomTab from "components/common/tabs/CustomTab";
+import PipelineStepNotificationConfigurationSummaryPanel
+  from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_notification_configuration/PipelineStepNotificationConfigurationSummaryPanel";
+
+function PipelineStepDetailsOverview({ pipelineStep, index }) {
+  const [activeTab, setActiveTab] = useState("summary");
+
+  const handleTabClick = (activeTab) => e => {
+    e.preventDefault();
+    setActiveTab(activeTab);
+  };
+
+  const getCurrentView = () => {
+    switch (activeTab) {
+      case "summary":
+        return <PipelineStepConfigurationSummary pipelineData={pipelineStep} />;
+      case "notifications":
+        return <PipelineStepNotificationConfigurationSummaryPanel pipelineStepData={pipelineStep} />;
+      case "json":
+        return <PipelineStepJsonPanel pipelineStepData={pipelineStep} />;
+      default:
+        return null;
+    }
+  };
+
+  const getTabContainer = () => {
+    return (
+      <CustomTabContainer>
+        <SummaryTab handleTabClick={handleTabClick} activeTab={activeTab} />
+        <CustomTab tabText={"Notification Configuration"} handleTabClick={handleTabClick} activeTab={activeTab} tabName={"notifications"} />
+        {/*{getActionSpecificTab()}*/}
+        <JsonTab handleTabClick={handleTabClick} activeTab={activeTab} />
+      </CustomTabContainer>
+    );
+  };
+
+  if (pipelineStep == null) {
+    return null;
+  }
+
+  return (
+    <PipelineStepDetailsContainer title={`Step ${index + 1}: [${pipelineStep?.name}]`}>
+      <GeneralTabPanelContainer currentView={getCurrentView()} tabContainer={getTabContainer()} />
+    </PipelineStepDetailsContainer>
+  );
+}
+
+PipelineStepDetailsOverview.propTypes = {
+  pipelineStep: PropTypes.object,
+  index: PropTypes.number
+};
+
+export default PipelineStepDetailsOverview;
