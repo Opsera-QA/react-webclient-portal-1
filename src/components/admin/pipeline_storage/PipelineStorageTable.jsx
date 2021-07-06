af5571da3c3e820ff0e74a79e88394a23066766b
Noah Champoux <noah@opsera.io>
2021-07-06 13:57:15 -0400

    Don't show search bar on Pipeline Storage table

--- src/components/admin/pipeline_storage/PipelineStorageTable.jsx
+++ src/components/admin/pipeline_storage/PipelineStorageTable.jsx
@@ -61,7 +61,6 @@ function PipelineStorageTable({ data, loadData, isLoading, pipelineStoragePagina
       loadData={loadData}
       isLoading={isLoading}
       body={getPipelineStorageTable()}
-      supportSearch={true}
       titleIcon={faFileArchive}
       title={"Pipeline Storage Records"}
       className={"px-2 pb-2"}
