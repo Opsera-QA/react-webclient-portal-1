d93e6a64e68bef46055ac355edfc33998ba58b5c
“Divyesha <divyesha@opsera.io>
2021-07-06 21:20:44 -0700

    changes

--- src/components/insights/charts/opsera/bar_chart/mean_time_to_restore/OpseraMeanTimeToRestoreBarChart.jsx
+++ src/components/insights/charts/opsera/bar_chart/mean_time_to_restore/OpseraMeanTimeToRestoreBarChart.jsx
@@ -135,13 +135,13 @@ function OpseraMeanTimeToRestoreBarChart({ kpiConfiguration, setKpiConfiguration
         </div>
         <ResponsiveBar
           data={metrics}
-          {...defaultConfig("Mean Time to Restore (in minutes)", "Date", 
+          {...defaultConfig("Mean Time to Deploy (in minutes)", "Date", 
                     false, false, "wholeNumbers", "monthDate2")}
           {...config(getColorByData, getMaxValue(metrics))}
           {...adjustBarWidth(metrics)}
           onClick={(data) => onRowSelect(data)}
           tooltip={({ indexValue, value, data, color }) => <ChartTooltip 
-                    titles={["Date", "Mean Time to Restore", "Number of Deployments"]}
+                    titles={["Date", "Mean Time to Deploy", "Number of Deployments"]}
                     values={[new Date(indexValue).toDateString(), `${value} minutes`, data.count ]}
                     style={false}
                     color={color} />}
