abeb33812a02cbd367266247e4949ff1124db067
Vignesh Subramanian <vignesh@opsera.io>
2021-07-14 16:02:47 +0530

    DRE chart view base

--- src/components/insights/charts/ChartView.jsx
+++ src/components/insights/charts/ChartView.jsx
@@ -124,6 +124,7 @@ import AutomationPercentagePieChart from "./qa_metrics/AutomationPercentagePieCh
 import AdoptionPercentagePieChart from "./qa_metrics/AdoptionPercentagePieChart";
 import AutomatedTestResultsPieChart from "./qa_metrics/AutomatedTestResultsPieChart";
 import SFDCManualTestResultsPieChart from "./qa_metrics/SFDCManualTestResultsPieChart";
+import DefectRemovalEfficiencyPieChart from "./qa_metrics/DefectRemovalEfficiencyPieChart";
 
 // SFDC KPIs
 import SFDCBackups from "components/insights/charts/sfdc/SFDCBackups";
@@ -1325,6 +1326,18 @@ function ChartView({ kpiConfiguration, dashboardData, index, loadChart, setKpis 
         return (
           <Col xl={6} md={12} className="p-2">
             <SFDCUnitTestingPieChart
+              kpiConfiguration={kpiConfig}
+              setKpiConfiguration={setKpiConfig}
+              dashboardData={dashboardData}
+              setKpis={setKpis}
+              index={index}
+            />
+          </Col>
+        );
+      case "defect-removal-efficiency":
+        return (
+          <Col md={12} className="p-2">
+            <DefectRemovalEfficiencyPieChart
               kpiConfiguration={kpiConfig}
               setKpiConfiguration={setKpiConfig}
               dashboardData={dashboardData}
