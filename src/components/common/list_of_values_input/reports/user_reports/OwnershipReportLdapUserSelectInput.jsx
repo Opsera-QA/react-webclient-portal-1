7678039de30c186ead6284d654cdd15d04fc0cd2
Noah Champoux <noah@opsera.io>
2021-07-22 15:24:38 -0400

    UserToolOwnershipReport enhancements

---
+++ src/components/common/list_of_values_input/reports/user_reports/OwnershipReportLdapUserSelectInput.jsx
@@ -0,0 +1,34 @@
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
+import {AuthContext} from "contexts/AuthContext";
+import LdapOwnerFilter from "components/common/filters/ldap/owner/LdapOwnerFilter";
+
+function OwnershipReportLdapUserSelectInput({ model, loadData }) {
+  const { isSassUser } = useContext(AuthContext);
+
+  const setDataFunction = (fieldName, value) => {
+    model.setData("owner", value);
+    loadData(model);
+  };
+
+  if (isSassUser() === true) {
+    return null;
+  }
+
+  return (
+    <LdapOwnerFilter
+      filterDto={model}
+      fieldName={"owner"}
+      setDataFunction={setDataFunction}
+    />
+  );
+}
+
+OwnershipReportLdapUserSelectInput.propTypes = {
+  model: PropTypes.object,
+  loadData: PropTypes.func,
+};
+
+export default OwnershipReportLdapUserSelectInput;
+
+
