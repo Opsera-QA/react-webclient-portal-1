a651d0519a991895e955861a8e2e202f596aefce
Faseeh <faseeh@opsera.io>
2021-08-02 12:49:49 +0530

    added check for null on info text component

--- src/components/workflow/wizards/sfdc_pipeline_wizard/rules/SfdcPipelineWizardRulesInputContainerBase.jsx
+++ src/components/workflow/wizards/sfdc_pipeline_wizard/rules/SfdcPipelineWizardRulesInputContainerBase.jsx
@@ -172,7 +172,7 @@ function SfdcPipelineWizardRulesInputContainerBase({pipelineWizardModel, setPipe
     <>
       <PropertyInputContainer
         titleIcon={faFilter}
-        field={""}
+        field={null}
         isLoading={!filePullCompleted || isLoading}
         titleText={"File Selection Rule Filter"}
         errorMessage={errorMessage}
