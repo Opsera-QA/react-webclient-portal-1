3c27fafc4951dff5e7c8ea3a5b51d4015c186375
Noah Champoux <noah@opsera.io>
2021-08-03 09:06:12 -0400

    Separated out Insights SubNavigation into its own component for cleanup

--- src/components/insights/summary/InsightsSummary.jsx
+++ src/components/insights/summary/InsightsSummary.jsx
@@ -1,13 +1,10 @@
 import React, {useEffect, useState, useContext, useRef} from "react";
 import { AuthContext } from "contexts/AuthContext";
-import {useHistory} from "react-router-dom";
 import LoadingDialog from "components/common/status_notifications/loading";
 import Model from "core/data_model/model";
 import axios from "axios";
 import {DialogToastContext} from "contexts/DialogToastContext";
 import ScreenContainer from "components/common/panels/general/ScreenContainer";
-import NavigationTabContainer from "components/common/tabs/navigation/NavigationTabContainer";
-import NavigationTab from "components/common/tabs/navigation/NavigationTab";
 import ActionBarContainer from "components/common/actions/ActionBarContainer";
 import PipelineDetails from "components/insights/summary/pipeline_details/PipelineDetails";
 import DashboardFiltersInput from "components/insights/dashboards/DashboardFiltersInput";
@@ -15,22 +12,19 @@ import DashboardFilterOrganizationInput from "components/insights/dashboards/Das
 import dashboardMetadata from "components/insights/dashboards/dashboard-metadata";
 import {dashboardFiltersMetadata} from "components/insights/dashboards/dashboard-metadata";
 import modelHelpers from "components/common/model/modelHelpers";
-import {faAnalytics, faChartNetwork, faChartArea, faRadar} from "@fortawesome/pro-light-svg-icons";  
-import DateRangeInput from "components/common/inputs/date/DateRangeInput";
 import { Button, Popover, Overlay } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar } from "@fortawesome/pro-light-svg-icons";
 import { format, addDays } from "date-fns";
 import { DateRangePicker } from "react-date-range";
+import InsightsSubNavigationBar from "components/insights/InsightsSubNavigationBar";
 
 function InsightsSummary() {
   const {getUserRecord, setAccessRoles} = useContext(AuthContext);
-  const history = useHistory();
   const [accessRoleData, setAccessRoleData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(true);
   const toastContext = useContext(DialogToastContext);
   const isMounted = useRef(false);
-  const [activeTab, setActiveTab] = useState("synopsis");
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
   const [dashboardData, setDashboardData] = useState(undefined);
   const [dashboardFilterTagsModel, setDashboardFilterTagsModel] = useState(modelHelpers.getDashboardFilterModel(dashboardData, "tags", dashboardFiltersMetadata));
@@ -255,50 +249,13 @@ function InsightsSummary() {
       );
   };
 
-  const handleNavTabClick = (tabSelection) => async e => {
-    e.preventDefault();
-
-    if (tabSelection === "analytics") {
-      history.push(`/insights/analytics`);
-      return;
-    }
-
-    if (tabSelection === "marketplace") {
-      history.push(`/insights/marketplace`);
-      return;
-    }
-
-    if (tabSelection === "dashboards") {
-      history.push(`/insights`);
-      return;
-    }
-
-    if (tabSelection === "synopsis") {
-      history.push(`/insights/synopsis`);
-      return;
-    }
-
-    setActiveTab(tabSelection);
-  };
-
-  const getNavigationTabContainer = () => {
-    return (
-      <NavigationTabContainer>
-        <NavigationTab icon={faChartNetwork} tabName={"dashboards"} handleTabClick={handleNavTabClick} activeTab={activeTab} tabText={"Dashboards"} />
-        <NavigationTab icon={faChartArea} tabName={"marketplace"} handleTabClick={handleNavTabClick} activeTab={activeTab} tabText={"Marketplace"} />
-        <NavigationTab icon={faAnalytics} tabName={"analytics"} handleTabClick={handleNavTabClick} activeTab={activeTab} tabText={"Analytics"} />
-        <NavigationTab icon={faRadar} tabName={"synopsis"} handleTabClick={handleNavTabClick} activeTab={activeTab} tabText={"Synopsis"} />
-      </NavigationTabContainer>
-    ); 
-  };
-
   if (!accessRoleData) {
     return (<LoadingDialog size="sm" message="Loading Insights"/>);
   }
 
   return (
     <ScreenContainer
-      navigationTabContainer={getNavigationTabContainer()}
+      navigationTabContainer={<InsightsSubNavigationBar currentTab={"synopsis"} />}
       pageDescription={`
         Opsera provides users with access to a vast repository of logging and analytics. Access all available
         logging, reports and configurations around the Opsera Analytics Platform or search your currently
