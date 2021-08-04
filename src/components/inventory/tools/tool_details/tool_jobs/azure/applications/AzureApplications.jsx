bd44752c1546fd009a9a3da89fd62ecd5267f1d1
Shrey Malhotra <shrey@opsera.io>
2021-08-04 13:45:27 -0700

    Merge branch 'OPL937Azure' into 'test-master'
    OPL937/Test-Master - Azure Account Applications Editor for credentials
    
    See merge request opsera-repo/web-portal/reactjs-client-portal!1396

---
+++ src/components/inventory/tools/tool_details/tool_jobs/azure/applications/AzureApplications.jsx
@@ -0,0 +1,61 @@
+import React, {useContext, useEffect, useRef, useState} from "react";
+import AzureApplicationsTable from "./AzureApplicationsTable";
+import PropTypes from "prop-types";
+import "components/inventory/tools/tools.css";
+import AzureApplicationOverlay
+  from "components/inventory/tools/tool_details/tool_jobs/azure/applications/AzureApplicationOverlay";
+import {DialogToastContext} from "contexts/DialogToastContext";
+
+function AzureApplications({ toolData, loadData, isLoading, toolApplications }) {
+  const toastContext = useContext(DialogToastContext);
+  const [azureApplications, setAzureApplications] = useState([]);
+
+  useEffect(() => {
+    unpackApplications(toolApplications);
+  }, [toolApplications]);
+
+  const unpackApplications = (toolApplications) => {
+    const newApplicationList = [];
+
+    if (Array.isArray(toolApplications)) {
+      toolApplications.forEach((toolAction, index) => {
+        let application = toolAction?.configuration;
+        application = {...application, applicationId: toolAction?._id};
+        application = {...application, index: index};
+        newApplicationList?.push(application);
+      });
+    }
+
+    setAzureApplications(newApplicationList);
+  };
+
+  const onRowSelect = (grid, row) => {
+    let selectedRow = toolData?.getArrayData("applications")[row?.index];
+    toastContext.showOverlayPanel(
+      <AzureApplicationOverlay
+        azureDataObject={selectedRow?.configuration}
+        applicationId={selectedRow?._id}
+        toolData={toolData}
+        loadData={loadData}
+      />
+    );
+  };
+
+  return (
+    <AzureApplicationsTable
+      isLoading={isLoading}
+      toolData={toolData}
+      loadData={loadData}
+      onRowSelect={onRowSelect}
+      azureApplications={azureApplications}
+    />
+  );
+}
+
+AzureApplications.propTypes = {
+  toolData: PropTypes.object,
+  loadData: PropTypes.func,
+  isLoading: PropTypes.bool,
+  toolApplications: PropTypes.array
+};
+export default AzureApplications;
