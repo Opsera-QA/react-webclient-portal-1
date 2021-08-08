c915dfe27e66967a1c157bdfabc23f944d75c2b3
Noah Champoux <noah@opsera.io>
2021-08-08 12:21:13 -0400

    Past Run Xml work (in progress, not wired up)

--- src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/past_run_xml/SfdcPipelineWizardPastRunComponent.jsx
+++ src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/past_run_xml/SfdcPipelineWizardPastRunComponent.jsx
@@ -1,22 +1,15 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Button} from 'react-bootstrap';
 import SaveButtonContainer from "components/common/buttons/saving/containers/SaveButtonContainer";
 import SfdcPipelineWizardSubmitFileTypeButton
   from "components/workflow/wizards/sfdc_pipeline_wizard/csv_file_upload/SfdcPipelineWizardSubmitFileTypeButton";
 import CancelButton from "components/common/buttons/CancelButton";
-import XmlFieldBase from "components/common/fields/code/XmlFieldBase";
-import InlinePipelineRunFilter
-  from "components/common/filters/pipelines/activity_logs/pipeline_run/InlinePipelineRunFilter";
-import blueprintsActions from "components/blueprint/blueprints-actions";
-import {AuthContext} from "contexts/AuthContext";
-import {DialogToastContext} from "contexts/DialogToastContext";
 import axios from "axios";
+import StandalonePackageXmlField from "components/common/fields/pipelines/sfdc/StandalonePackageXmlField";
+import PipelineRunNumberInput from "components/common/inputs/number/pipelines/PipelineRunNumberInput";
 
 function SfdcPipelineWizardPastRunComponent({ pipelineWizardModel, setPipelineWizardModel, setPipelineWizardScreen, handleClose }) {
-  const { getAccessToken } = useContext(AuthContext);
-  const toastContext = useContext(DialogToastContext);
-  const [isLoading, setIsLoading] = useState(false);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
 
@@ -51,14 +44,16 @@ function SfdcPipelineWizardPastRunComponent({ pipelineWizardModel, setPipelineWi
   };
 
   const getXMLView = () => {
-    if (pipelineWizardModel.getData("xmlFileContent") && pipelineWizardModel.getData("xmlFileContent").length > 0) {
-      return (
-        <>
-          <XmlFieldBase fieldName={"xmlFileContent"} model={pipelineWizardModel}/>
-          {buttonContainer()}
-        </>
-      );
-    }
+    return (
+      <>
+        <StandalonePackageXmlField
+          runNumber={pipelineWizardModel?.getData("selectedRunNumber")}
+          pipelineId={pipelineWizardModel?.getData("pipelineId")}
+          setXmlFunction={validateXml}
+        />
+        {buttonContainer()}
+      </>
+    );
   };
   
   const buttonContainer = () => {
@@ -74,34 +69,18 @@ function SfdcPipelineWizardPastRunComponent({ pipelineWizardModel, setPipelineWi
     );
   };
 
-  const pullXmlFromRun = async (newPipelineWizardModel) => {
-    const response = await blueprintsActions.getBlueprintSearchResultsManual(getAccessToken, cancelTokenSource, newPipelineWizardModel.getData("pipelineId"), newPipelineWizardModel.getData("selectedRunNumber"));
-    console.log("response: " + JSON.stringify(response));
-  };
-
-  const getValidateButton = () => {
-    if (pipelineWizardModel.getData("xmlFileContent").length === 0 && pipelineWizardModel.getData("csvFileContent").length === 0) {
-      return (
-        <Button variant="primary" className={"mt-3"} onClick={() => validateXml()}>Process File</Button>
-      );
-    }
-  };
-
   const getBody = () => {
     if (pipelineWizardModel.getData("recordId") && pipelineWizardModel.getData("recordId").length > 0) {
       return (
         <div>
           <div className="my-4 w-100">
             <div className="my-3">
-              <InlinePipelineRunFilter
+              <PipelineRunNumberInput
                 model={pipelineWizardModel}
                 setModel={setPipelineWizardModel}
                 maximumRunCount={pipelineWizardModel?.getData("run_count") > 0 ? pipelineWizardModel?.getData("run_count") : undefined}
-                showLabel={true}
-                loadData={pullXmlFromRun}
               />
             </div>
-            {getValidateButton()}
           </div>
           {getXMLView()}
         </div>
