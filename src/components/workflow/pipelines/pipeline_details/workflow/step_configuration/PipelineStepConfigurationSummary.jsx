6e54117cdbd57d94a2d62cfdfd8dd25dc331cdb9
Shrey Malhotra <shrey@opsera.io>
2021-08-11 13:27:58 -0700

    Merge branch 'OPL-1145' into AzureAcrOctopus

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepConfigurationSummary.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepConfigurationSummary.jsx
@@ -139,6 +139,9 @@ import terrascanStepFormMetadata from "./step_tool_configuration_forms/terrascan
 import AzureDevopsPipelineStepConfigurationSummary
   from "./step_tool_configuration_forms/azure_devops/AzureDevopsPipelineStepConfigurationSummary";
 import azureDevopsStepFormMetadata from "./step_tool_configuration_forms/azure_devops/azureDevops-stepForm-metadata";
+import AzureAcrPushPipelineStepConfigurationSummary
+  from "./step_tool_configuration_forms/azure_acr_push/AzureAcrPushPipelineStepConfigurationSummary";
+import azureAcrPushStepFormMetadata from "./step_tool_configuration_forms/azure_acr_push/azureAcrPush-stepForm-metadata";
 import KafkaConnectPipelineStepConfigurationSummaryPanel
   from "./step_tool_configuration_forms/kafka_connect/KafkaConnectPipelineStepConfigurationSummaryPanel";
 import kafkaConnectStepFormMetadata from "./step_tool_configuration_forms/kafka_connect/kafkaConnect-stepForm-metadata";
@@ -148,6 +151,7 @@ import awsECSDeployStepFormMetadata
   from "./step_tool_configuration_forms/aws_ecs_deploy/awsECSDeploy-stepForm-metadata";
 import coverityStepFormMetadata from "./step_tool_configuration_forms/coverity/coverity-stepForm-metadata";
 import CoverityPipelineStepConfigurationSummaryPanel from "./step_tool_configuration_forms/coverity/CoverityPipelineStepConfigurationSummaryPanel";
+
 
 function PipelineStepConfigurationSummary({
   pipelineData,
@@ -416,6 +420,13 @@ function PipelineStepConfigurationSummary({
           <AzureDevopsPipelineStepConfigurationSummary
             pipelineData={pipelineData}
             azureDevopsPipelineDataObject={getModelWrappedObject(azureDevopsStepFormMetadata)}
+          />
+        );
+      case "azure_acr_push":
+        return (
+          <AzureAcrPushPipelineStepConfigurationSummary
+            pipelineData={pipelineData}
+            azureAcrPushPipelineDataObject={getModelWrappedObject(azureAcrPushStepFormMetadata)}
           />
         );
       case "kafka_connect":
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/azure_acr_push/inputs/AzureAcrPushAzureToolConfigIdSelectInput.jsx
@@ -0,0 +1,173 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+import { AuthContext } from "contexts/AuthContext";
+import PipelineActions from "components/workflow/pipeline-actions";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faTools } from "@fortawesome/pro-light-svg-icons";
+import axios from "axios";
+
+function AzureAcrPushAzureToolConfigIdSelectInput({ fieldName, dataObject, setDataObject, disabled, textField, valueField , setAzureConfig}) {
+  const toastContext = useContext(DialogToastContext);
+  const { getAccessToken } = useContext(AuthContext);
+  const [list, setList] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState("Select a Azure Acr Push Instance");
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+   
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [dataObject.getData('')]);
+
+  const loadData = async (cancelSource = cancelTokenSource) => {
+    try {
+      setIsLoading(true);
+      await fetchAzureDetails(cancelSource);
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        toastContext.showLoadingErrorDialog(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const getInfoText = () => {
+    if (dataObject.getData(fieldName) !== "") {
+      return (
+        <Link to={`/inventory/tools/details/${dataObject.getData(fieldName)}`}>
+          <span>
+            <FontAwesomeIcon icon={faTools} className="pr-1" />
+            View Or Edit this Tool&apos;s Registry settings
+          </span>
+        </Link>
+      );
+    }
+    return "Select a tool to continue";
+  };
+
+  const fetchAzureDetails = async (cancelSource) => {
+    setIsSearching(true);
+    try {
+      let results = await PipelineActions.getToolsListV2(getAccessToken, cancelSource, "azure_account");
+      if (results?.data) {
+        let respObj = [];
+        let arrOfObj = results.data;
+        arrOfObj.map((item) => {
+          respObj.push({
+            name: item.name,
+            id: item._id,
+            configuration: item.configuration,
+            owner: item.owner,
+            applications: item.applications
+          });
+        });
+        results = respObj;
+      }
+      const filteredList = results ? results.filter((el) => el.configuration !== undefined) : [];
+      if (filteredList) {
+        setList(filteredList);
+        if(!dataObject.isNew() && dataObject.getData("azureToolConfigId")){
+          setAzureConfig(filteredList.find(el=>el.id == dataObject.getData("azureToolConfigId")));
+        }
+      }
+    } catch (error) {
+      setPlaceholder("No Azure Acr Push Tool Found");
+      console.error(error);
+      toastContext.showServiceUnavailableDialog();
+    } finally {
+        setIsSearching(false);
+    }
+  };
+  
+  const handleChange=(fieldName,selectedOption)=>{
+    setAzureConfig(list.find(el=>el.id == selectedOption.id));
+    let newDataObject = {...dataObject};    
+    newDataObject.setData(fieldName, selectedOption.id);
+    newDataObject.setData('azureRegistryName', "");
+    newDataObject.setData('azureRepoName', "");
+    newDataObject.setData('acrLoginUrl', "");
+    newDataObject.setData('newRepo', "");
+    newDataObject.setData('useRunCount', "");
+    setDataObject({...newDataObject});
+  };
+
+  const clearDataFunction=()=>{
+    let newDataObject = {...dataObject};
+    newDataObject.setData(fieldName, "");
+    newDataObject.setData('azureRegistryName', "");
+    newDataObject.setData('azureRepoName', "");
+    newDataObject.setData('acrLoginUrl', "");
+    newDataObject.setData('newRepo', "");
+    newDataObject.setData('useRunCount', "");
+    setAzureConfig(null);  
+    setDataObject({...newDataObject});
+  };
+
+  
+
+  return (
+    <div>
+      <SelectInputBase
+        fieldName={fieldName}
+        dataObject={dataObject}
+        setDataObject={setDataObject}
+        setDataFunction={handleChange}
+        clearDataFunction={clearDataFunction}
+        selectOptions={list ? list : []}
+        busy={isSearching ||  isLoading}
+        valueField={valueField}
+        textField={textField}
+        placeholderText={placeholder}
+        disabled={disabled || isLoading ||  isSearching || (list == null || list.length === 0)}
+      />
+      <small className="text-muted ml-3">{getInfoText()}</small>
+    </div>
+  );
+}
+
+AzureAcrPushAzureToolConfigIdSelectInput.propTypes = {
+  fieldName: PropTypes.string,
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+  textField: PropTypes.string,
+  valueField: PropTypes.string,
+  setAzureToolConfigObj: PropTypes.func,
+  setAzureConfig:PropTypes.func,
+};
+
+AzureAcrPushAzureToolConfigIdSelectInput.defaultProps = {
+  valueField: "id",
+  textField: "name",
+  fieldName: "azureToolConfigId",
+  
+};
+
+export default AzureAcrPushAzureToolConfigIdSelectInput;
