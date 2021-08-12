39b42fb0e6538756f3d0df542d888ddb2aa7f4ed
Noah Champoux <noah@opsera.io>
2021-08-12 16:47:31 -0400

    Styling Enhancements and New Components (Not Wired Up Yet)

--- src/components/common/list_of_values_input/tools/RegistryToolInfoOverlay.jsx
+++ src/components/common/list_of_values_input/tools/RegistryToolInfoOverlay.jsx
@@ -2,44 +2,49 @@ import React from "react";
 import PropTypes from "prop-types";
 import InfoOverlayContainer from "components/common/inputs/info_text/InfoOverlayContainer";
 import {Link} from "react-router-dom";
+import ToolInfoContainer from "components/common/fields/inventory/tools/ToolInfoContainer";
+import ToolsTable from "components/inventory/tools/ToolsTable";
+import toolMetadata from "components/inventory/tools/tool-metadata";
+import {faTools} from "@fortawesome/pro-light-svg-icons";
+import FilterContainer from "components/common/table/FilterContainer";
 
 // TODO: Rework to use Summary panel for both the main tool view and also the configuration based off tool identifier like in the registry
-function RegistryToolInfoOverlay({toolData}) {
+function RegistryToolInfoOverlay({tools, toolData, loadData, isLoading}) {
   const getBody = () => {
     if (toolData) {
       return (
         <>
           <div className="text-muted mb-2">
             Configuration details for this item are listed below. Tool and account specific settings are stored in the
-            <span> <Link to="/inventory/tools">Tool Registry</Link></span>. To add a new entry to a dropdown or update settings,
-            make those changes there.
+            <span> <Link to="/inventory/tools">Tool Registry</Link></span>.
+            <div>To add a new entry to a dropdown or update settings, make those changes there.</div>
           </div>
-          {toolData?.configuration && (
-            <>
-              {Object.entries(toolData?.configuration).map(function(a) {
-                return (
-                  <div key={a}>
-                    {a[1].length > 0 && (
-                      <>
-                        <span className="text-muted pr-1">{a[0]}: </span> {a[1]}
-                      </>
-                    )}
-                  </div>
-                );
-              })}
-            </>
-          )}
+          <ToolInfoContainer toolData={toolData} />
         </>
       );
     }
 
     return (
-      <div className="text-muted mb-2">Please select any tool/account to get the details.</div>
+      <div>
+        <div className={"text-muted mb-2"}>
+          Please select any Tool to get the details. Tool specific settings are stored in the
+          <span> <Link to="/inventory/tools">Tool Registry</Link></span>.
+          <div>To add a new entry to a dropdown or update settings, make those changes there.</div>
+        </div>
+        <FilterContainer
+          isLoading={isLoading}
+          metadata={toolMetadata}
+          loadData={loadData}
+          titleIcon={faTools}
+          body={<ToolsTable data={tools} />}
+          title={"Tools"}
+        />
+      </div>
     );
   };
 
   return (
-    <InfoOverlayContainer title={"Tool and Account Details"}>
+    <InfoOverlayContainer title={"Tool Details"}>
       {getBody()}
     </InfoOverlayContainer>
   );
@@ -47,6 +52,9 @@ function RegistryToolInfoOverlay({toolData}) {
 
 RegistryToolInfoOverlay.propTypes = {
   toolData: PropTypes.object,
+  tools: PropTypes.array,
+  loadData: PropTypes.func,
+  isLoading: PropTypes.bool,
 };
 
 export default RegistryToolInfoOverlay;
--- src/components/inventory/tools/tool_details/tool_jobs/argo/applications/details/ArgoApplicationSummaryPanel.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/argo/applications/details/ArgoApplicationSummaryPanel.jsx
@@ -3,20 +3,26 @@ import { Row, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 import TextFieldBase from "components/common/fields/text/TextFieldBase";
 
-
-// TODO: Implement
 function ArgoApplicationSummaryPanel({ argoApplicationData } ) {
-
   if (argoApplicationData == null) {
-    return <LoadingDialog size="sm" />;
+    return null;
   }
 
   return (
     <div className="scroll-y pt-3 px-3">
       <div className="mb-3 flat-top-content-block p-3 detail-view-summary">
         <Row>
+          <Col lg={12}>
+            <TextFieldBase dataObject={argoApplicationData} fieldName={"Project Name"} />
+          </Col>
           <Col lg={6}>
-            <TextFieldBase dataObject={argoApplicationData} fieldName={""} />
+            <TextFieldBase dataObject={argoApplicationData} fieldName={"Project Name"} />
+          </Col>
+          <Col lg={6}>
+            <TextFieldBase dataObject={argoApplicationData} fieldName={"Project Name"} />
+          </Col>
+          <Col lg={6}>
+            <TextFieldBase dataObject={argoApplicationData} fieldName={"Project Name"} />
           </Col>
         </Row>
       </div>
