9c34c1eb5a8ddf8a9bdba4a1529bc289f7ebdfd9
Faseeh <faseeh@opsera.io>
2021-08-13 19:48:52 +0530

    renamed components

--- src/components/git/git_task_details/GitRunTaskModal.jsx
+++ src/components/git/git_task_details/GitRunTaskModal.jsx
@@ -10,7 +10,7 @@ import CloseButton from "components/common/buttons/CloseButton";
 import RunGitTaskButton from "components/common/buttons/git/RunGitTaskButton";
 import SFDCGitBranchInput from "components/git/git_task_details/configuration_forms/sfdc-org-sync/inputs/SFDCGitBranchInput";
 import SFDCNewBranchBoolInput from "components/git/git_task_details/configuration_forms/sfdc-org-sync/inputs/SFDCNewBranchBoolInput";
-import SFDCGitUpstreamBranchInput from "components/git/git_task_details/configuration_forms/sfdc-org-sync/inputs/SFDCGitUpstreamBranchInput";
+import SfdcGitUpstreamBranchInput from "components/git/git_task_details/configuration_forms/sfdc-org-sync/inputs/SfdcGitUpstreamBranchInput";
 import sfdcGitTaskConfigurationMetadata from "components/git/git_task_details/configuration_forms/sfdc-org-sync/sfdc-git-task-configuration-metadata";
 import ec2ClusterCreationTaskConfigurationMetadata from "components/git/git_task_details/configuration_forms/ecs-cluster-creation/ecs-creation-git-task-configuration";
 import sfdxCertGenTaskConfigurationMetadata from "components/git/git_task_details/configuration_forms/sfdx-cert-gen/sfdx-cert-gen-task-configuration-metadata";
@@ -97,7 +97,7 @@ function GitRunTaskModal({ showModal, handleClose, gitTasksData, setGitTasksData
                     dataObject={dataObj} setDataObject={setDataObj} visible={dataObj?.getData("isNewBranch")} />
                   </Col>
                   <Col lg={12}>
-                    <SFDCGitUpstreamBranchInput dataObject={dataObj} setDataObject={setDataObj} />
+                    <SfdcGitUpstreamBranchInput dataObject={dataObj} setDataObject={setDataObj} />
                   </Col>
                 </>
               }
