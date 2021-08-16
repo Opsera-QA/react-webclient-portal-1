73be1062c1205522185e67f1fc15af872ebcb762
Faseeh <faseeh@opsera.io>
2021-08-16 17:28:59 +0530

    added support for commandline and custom params on gradle and maven

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/jenkins/JenkinsStepConfiguration.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/jenkins/JenkinsStepConfiguration.jsx
@@ -156,6 +156,7 @@ function JenkinsStepConfiguration({
   const [deleteDockerSecrets, setDeleteDockerSecrets] = useState(false);  
   const [dataObject, setDataObject] = useState(undefined);
   const [pythonScriptData, setPythonScriptData] = useState(undefined);
+  const [buildScriptData, setBuildScriptData] = useState(undefined);
 
   let step = {};
 
@@ -465,7 +466,7 @@ function JenkinsStepConfiguration({
       //   return;
       // }
 
-      if(formData.buildType === "python") {
+      if(formData.buildType === "python" || formData.buildType === "gradle" || formData.buildType === "maven") {
         handlePythonScriptDetails();
       }
 
@@ -1636,7 +1637,7 @@ function JenkinsStepConfiguration({
                   )}                   */}
                   </>
                 )}                
-                {(formData.buildType === "python") && (
+                {(formData.buildType === "python" || formData.buildType === "gradle" || formData.buildType === "maven") && (
                   <>
                     <BooleanToggleInput 
                       dataObject={pythonScriptData} 
@@ -1645,8 +1646,13 @@ function JenkinsStepConfiguration({
                     />
                     { pythonScriptData.getData("customScript") ? (
                       <>
-                        <StepConfigUseTerraformOutput dataObject={pythonScriptData} setDataObject={setPythonScriptData} fieldName={"useTerraformOutput"} plan={plan} stepId={stepId}/>
-                        {getTerraformSelect()}
+                        {formData.buildType === "python" && 
+                          <>
+                            <StepConfigUseTerraformOutput dataObject={pythonScriptData} setDataObject={setPythonScriptData} fieldName={"useTerraformOutput"} plan={plan} stepId={stepId}/>       
+                            {getTerraformSelect()}              
+                          </> 
+                        }
+                        
                         <ParameterSelectListInputBase
                           titleIcon={faHandshake}
                           dataObject={pythonScriptData}
@@ -1657,8 +1663,8 @@ function JenkinsStepConfiguration({
                           regexValidationRequired={false}
                           titleText={"Parameter Selection"}
                           plan={plan}
-                          tool_prop={pythonScriptData?.getData("terraformStepId") && pythonScriptData?.getData("terraformStepId").length > 0 ?
-                            pythonScriptData?.getData("terraformStepId") : ""}
+                          // tool_prop={pythonScriptData?.getData("terraformStepId") && pythonScriptData?.getData("terraformStepId").length > 0 ?
+                          //   pythonScriptData?.getData("terraformStepId") : ""}
                         />
                         <TextAreaInput 
                           dataObject={pythonScriptData}                         
@@ -1667,11 +1673,15 @@ function JenkinsStepConfiguration({
                         />
                       </>
                     ) : (
-                      <PythonFilesInput 
-                        setDataObject={setPythonScriptData} 
-                        dataObject={pythonScriptData}
-                        fieldName={"inputDetails"}
-                      />
+                      <>
+                        {formData.buildType === "python" && 
+                          <PythonFilesInput 
+                            setDataObject={setPythonScriptData} 
+                            dataObject={pythonScriptData}
+                            fieldName={"inputDetails"}
+                          />
+                        }
+                      </>
                     ) }                    
                   </>                  
                 )}
