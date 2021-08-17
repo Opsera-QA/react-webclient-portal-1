f2ab66f62ad9a477265e5a7ae90ce18c5c9931ef
Noah Champoux <noah@opsera.io>
2021-08-17 13:56:06 -0400

    Show Warning Message if Email on Account Registration screen does not match Organization Domain

--- src/components/user/account_registration/AccountRegistration.jsx
+++ src/components/user/account_registration/AccountRegistration.jsx
@@ -12,6 +12,7 @@ import RequiredFieldsMessage from "components/common/fields/editor/RequiredField
 import accountRegistrationMetadata from "components/user/account_registration/account-registration-metadata";
 import { AuthContext } from "contexts/AuthContext";
 import TempTextInput from "components/common/inputs/text/TempTextInput";
+import {validateEmail} from "utils/helpers";
 
 function AccountRegistration() {
   const { domain } = useParams();
@@ -119,6 +120,19 @@ function AccountRegistration() {
     );
   }
 
+  const getWarning = () => {
+    const email = registrationDataDto?.getData("email");
+    const validEmail = validateEmail(email);
+
+    if (validEmail === true && registrationDataDto?.getData("orgDomain") !== email.substring(email.lastIndexOf("@") + 1)) {
+      return (
+        <div className="warning-text pl-4 mt-1">
+          Warning, you are about to register an account with an email outside of your organization’s domain.
+        </div>
+      );
+    }
+  };
+
   if (isLoading || registrationDataDto == null) {
     return <LoadingDialog />;
   }
@@ -152,6 +166,7 @@ function AccountRegistration() {
                                setDataObject={setRegistrationDataDto} />
               </Col>
             </Row>
+            {getWarning()}
             <Row>
               <div className="ml-auto m-3 px-3">
                 <RegisterButton createAccount={createAccount} recordDto={registrationDataDto} disable={invalidHost} />
