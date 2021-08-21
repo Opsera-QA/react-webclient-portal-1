b36f8041c5d73bb94973e6ac2d3efa160cf6350d
Noah Champoux <noah@opsera.io>
2021-08-21 08:07:18 -0400

    Fixed Null Pointer Exception and Minor cleanup for unused deprecated api calls

---
+++ src/components/inventory/tools/tool_details/ToolRepositoriesPanel.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LoadingDialog from "components/common/status_notifications/loading";
+import DetailPanelContainer from "components/common/panels/detail_panel_container/DetailPanelContainer";
+import JFrogMavenRepositoriesPanel from "components/inventory/tools/tool_details/tool_jobs/jfrog_artifactory/repositories/JFrogMavenRepositoriesPanel";
+import MessageField from "components/common/fields/text/MessageField";
+
+function ToolRepositoriesPanel({ toolData, loadData, isLoading }) {
+  const getToolRepositoriesPanel = () => {
+    switch (toolData?.getData("tool_identifier")) {
+    case "jfrog_artifactory_maven":
+      return <JFrogMavenRepositoriesPanel isLoading={isLoading} toolData={toolData} />;
+    default:
+      return <LoadingDialog message={"Loading Tool Projects"} size={"sm"} />;
+    }
+  };
+
+  return (
+    <DetailPanelContainer>
+      <div className="h6">Managed Repositories Creation</div>
+      <MessageField message={`Add, Modify or Delete JFrog Maven Repositories. These repositories can be entered once and reused across the Opsera platform.`}/>
+      {getToolRepositoriesPanel()}
+    </DetailPanelContainer>
+  );
+}
+
+ToolRepositoriesPanel.propTypes = {
+  toolData: PropTypes.object,
+  loadData: PropTypes.func,
+  isLoading: PropTypes.bool
+};
+
+
+export default ToolRepositoriesPanel;
--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/helpers/git-actions-helper.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/helpers/git-actions-helper.js
@@ -1,80 +1,42 @@
-import { axiosApiService } from "api/apiService";
 import baseActions from "utils/actionsBase";
 
 const GitActionsHelper = {};
 
-  GitActionsHelper.searchRepositories = async (service, gitAccountId, workspaces, getAccessToken) => {
-    const accessToken = await getAccessToken();
-    const apiUrl = "/tools/properties";
-    const postBody = {
-      tool: service,
-      metric: "getRepositories",
-      gitAccountId: gitAccountId,
-      workspaces: workspaces
-    };
-    const res = await axiosApiService(accessToken)
-      .post(apiUrl, postBody)
-      .then((result) => {
-        return result;
-      })
-      .catch((error) => {
-        throw error;
-      });
-    return res;
+GitActionsHelper.searchRepositoriesV2 = async (service, gitAccountId, workspaces, getAccessToken, cancelTokenSource) => {
+  let postBody = {
+    tool: service,
+    metric: "getRepositories",
+    gitAccountId: gitAccountId,
+    workspaces: workspaces
+  };
+  const apiUrl = "/tools/properties";
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
+
+GitActionsHelper.getBranchesV2 = async (getAccessToken, cancelTokenSource, service, gitAccountId, repoId, workspaces) => {
+  const apiUrl = "/tools/properties";
+  const postBody = {
+    tool: service,
+    metric: "getBranches",
+    gitAccountId: gitAccountId,
+    repoId: repoId,
+    workspaces: workspaces
   };
 
-  GitActionsHelper.searchRepositoriesV2 = async (service, gitAccountId, workspaces, getAccessToken, cancelTokenSource) => {
-    let postBody = {
-      tool: service,
-      metric: "getRepositories",
-      gitAccountId: gitAccountId,
-      workspaces: workspaces
-    };
-    const apiUrl = "/tools/properties";
-    return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
+
+GitActionsHelper.getReviewersV2 = async (getAccessToken, cancelTokenSource, service, gitAccountId, repoId, workspaces) => {
+  const apiUrl = "/tools/properties";
+  const postBody = {
+    tool: service,
+    metric: "getReviewers",
+    gitAccountId: gitAccountId,
+    repoId: repoId,
+    workspaces: workspaces
   };
 
-  GitActionsHelper.searchBranches = async (service, gitAccountId, repoId,workspaces, getAccessToken) => {
-    const accessToken = await getAccessToken();
-    const apiUrl = "/tools/properties";
-    const postBody = {
-      tool: service,
-      metric: "getBranches",
-      gitAccountId: gitAccountId,
-      repoId: repoId,
-      workspaces: workspaces
-
-    };
-    const res = await axiosApiService(accessToken)
-      .post(apiUrl, postBody)
-      .then((result) => {
-        return result;
-      })
-      .catch((error) => {
-        throw error;
-      });
-    return res;
-  };
-
-  GitActionsHelper.getReviewers = async (service, gitAccountId, repoId, workspaces, getAccessToken) => {
-    const accessToken = await getAccessToken();
-    const apiUrl = "/tools/properties";
-    const postBody = {
-      tool: service,
-      metric: "getReviewers",
-      gitAccountId: gitAccountId,      
-      repoId: repoId,
-      workspaces: workspaces
-    };
-    const res = await axiosApiService(accessToken)
-      .post(apiUrl, postBody)
-      .then((result) => {
-        return result;
-      })
-      .catch((error) => {
-        throw error;
-      });
-    return res;
-  };
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
 
 export default GitActionsHelper;
