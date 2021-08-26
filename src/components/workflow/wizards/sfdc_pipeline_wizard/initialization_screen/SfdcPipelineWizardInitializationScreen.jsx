dbba0433833cb100a4e140a02a5f4bb202952d8c
Noah Champoux <noah@opsera.io>
2021-08-26 09:13:10 -0400

    Removed Feature Flag from Pipeline Wizard Past Run feature

--- src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/SfdcPipelineWizardInitializationScreen.jsx
+++ src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/SfdcPipelineWizardInitializationScreen.jsx
@@ -320,7 +320,7 @@ const SfdcPipelineWizardInitializationScreen = ({ pipelineWizardModel, setPipeli
   };
 
   const getDynamicTab = () => {
-    if (featureFlagHideItemInProd() || pipelineWizardModel?.getData("run_count") === 1 || pipelineWizardModel?.getData("fromGitTasks") === true) {
+    if (pipelineWizardModel?.getData("run_count") === 1 || pipelineWizardModel?.getData("fromGitTasks") === true) {
       return null;
     }
 
