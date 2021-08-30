bcfdd46f43dcd1fe256d9f753432d43a9d199cc0
“Divyesha <divyesha@opsera.io>
2021-08-30 06:32:55 -0700

    changes

--- src/components/insights/charts/detail_overlay/ChartSummaryPanelWrapper.jsx
+++ src/components/insights/charts/detail_overlay/ChartSummaryPanelWrapper.jsx
@@ -130,9 +130,17 @@ function ChartSummaryPanelWrapper({
           />
         );
       case "opsera-deployment-frequency-stats-successful":
-        return <OpseraDeploymentFreqStatsSuccessSummaryPanel chartModel={chartModel} setActiveTab={setActiveTab} />;
+        return ( <OpseraDeploymentFreqStatsSuccessSummaryPanel
+            dashboardData={dashboardData}
+            kpiConfiguration={kpiConfiguration}
+            setActiveTab={setActiveTab} />
+      );
       case "opsera-deployment-frequency-stats-failed":
-        return <OpseraDeploymentFreqStatsFailureSummaryPanel chartModel={chartModel} setActiveTab={setActiveTab} />;
+        return ( <OpseraDeploymentFreqStatsFailureSummaryPanel
+            dashboardData={dashboardData}
+            kpiConfiguration={kpiConfiguration}
+            setActiveTab={setActiveTab} />
+        );
       case "status-by-pipeline-successful":
         return (
           <PipelineByStatusSuccessfulTable
