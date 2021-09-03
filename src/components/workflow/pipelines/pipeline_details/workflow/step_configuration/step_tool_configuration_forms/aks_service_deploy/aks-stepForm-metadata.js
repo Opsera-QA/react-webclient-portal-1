40d46098d4aebee3054d7221e22e374d3921d5e9
Shrey Malhotra <shrey@opsera.io>
2021-09-03 11:30:50 -0700

    updating the regex

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/aks_service_deploy/aks-stepForm-metadata.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/aks_service_deploy/aks-stepForm-metadata.js
@@ -9,6 +9,7 @@ const aksStepFormMetadata = {
     {
       label: "Service Name",
       id: "aksServiceName",
+      regexValidator: /[a-z]([-a-z0-9]*[a-z0-9])?/
     },
     {
       label: "Image Hub",
@@ -49,7 +50,7 @@ const aksStepFormMetadata = {
       id: "namePretext",
       formText: "Enter a prefix to be prepended to the uniquely generated name",
       maxLength: 25,
-      regexValidator: /^[A-Za-z0-9-.:]*$/
+      regexValidator: /[a-z]([-a-z0-9]*[a-z0-9])?/
     },
     {
       label: "Generate Dynamic Service Name?",
