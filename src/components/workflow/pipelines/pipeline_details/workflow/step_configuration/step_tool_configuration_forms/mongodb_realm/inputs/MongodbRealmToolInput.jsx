f97da89413b358988fcd4a1fba9f827fee355027
Vignesh Subramanian <vignesh@opsera.io>
2021-09-03 17:48:27 +0530

    review feedback and bug fix

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/mongodb_realm/inputs/MongodbRealmToolInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/mongodb_realm/inputs/MongodbRealmToolInput.jsx
@@ -1,137 +1,41 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { DialogToastContext } from "contexts/DialogToastContext";
-import SelectInputBase from "components/common/inputs/select/SelectInputBase";
-import { AuthContext } from "contexts/AuthContext";
-import PipelineActions from "components/workflow/pipeline-actions";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner, faTools } from "@fortawesome/pro-light-svg-icons";
-import axios from "axios";
+import RoleRestrictedToolByIdentifierInputBase from "components/common/list_of_values_input/tools/RoleRestrictedToolByIdentifierInputBase";
 
-function MongodbRealmToolInput({ fieldName, dataObject, setDataObject, disabled, textField, valueField, tool_prop }) {
-  const toastContext = useContext(DialogToastContext);
-  const { getAccessToken } = useContext(AuthContext);
-  const [mongodbRealmList, setMongodbRealmList] = useState([]);
-  const [isSearching, setIsSearching] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [placeholder, setPlaceholder] = useState("Select a MongoDB Realm Instance");
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
-
-  useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-
-    if (!disabled) {
-      loadData(source).catch((error) => {
-        if (isMounted?.current === true) {
-          throw error;
-        }
-      });
-    }
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
-  }, []);
-
-  const loadData = async (cancelSource = cancelTokenSource) => {
-    try {
-      setIsLoading(true);
-      await fetchMongodbRealmDetails(cancelSource);
-    } catch (error) {
-      if (isMounted?.current === true) {
-        console.error(error);
-        toastContext.showLoadingErrorDialog(error);
-      }
-    } finally {
-      if (isMounted?.current === true) {
-        setIsLoading(false);
-      }
-    }
-  };
-
-  const getInfoText = () => {
-    if (dataObject.getData(fieldName) !== "") {
-      return (
-        <Link to={`/inventory/tools/details/${dataObject.getData(fieldName)}`}>
-          <span>
-            <FontAwesomeIcon icon={faTools} className="pr-1" />
-            View Or Edit this Tool&apos;s Registry settings
-          </span>
-        </Link>
-      );
-    }
-    return "Select a tool to continue";
-  };
-
-  const fetchMongodbRealmDetails = async (cancelSource) => {
-    setIsSearching(true);
-    try {
-      let results = await PipelineActions.getToolsListV2(getAccessToken, cancelSource, "mongodb_realm");
-      if (results?.data) {
-        let respObj = [];
-        let arrOfObj = results.data;
-        arrOfObj.map((item) => {
-          respObj.push({
-            name: item.name,
-            id: item._id,
-            configuration: item.configuration,
-          });
-        });
-        results = respObj;
-      }
-      const filteredList = results ? results.filter((el) => el.configuration !== undefined) : [];
-      if (filteredList) {
-        setMongodbRealmList(filteredList);
-      }
-    } catch (error) {
-      setPlaceholder("No Config Tool Found");
-      console.error(error);
-      toastContext.showServiceUnavailableDialog();
-    } finally {
-      setIsSearching(false);
-    }
+function MongodbRealmToolInput({className, fieldName, model, setModel, disabled}) {
+  
+  const setDataFunction = (fieldName, selectedOption) => {
+    let newModel = {...model};
+    newModel.setData("mongoToolId", selectedOption?._id);    
+    setModel({...newModel});
   };
 
   return (
-    <div>
-      <SelectInputBase
-        fieldName={fieldName}
-        dataObject={dataObject}
-        setDataObject={setDataObject}
-        selectOptions={mongodbRealmList ? mongodbRealmList : []}
-        busy={isSearching}
-        valueField={valueField}
-        textField={textField}
-        placeholderText={placeholder}
-        disabled={disabled || isSearching || (mongodbRealmList == null || mongodbRealmList.length === 0)}
-      />
-      <small className="text-muted ml-3">{getInfoText()}</small>
-    </div>
+     <RoleRestrictedToolByIdentifierInputBase
+       toolIdentifier={"mongodb_realm"}
+       toolFriendlyName={"MongoDB Realm"}
+       fieldName={fieldName}
+       placeholderText={"Select MongoDB Realm Tool"}
+       configurationRequired={true}
+       model={model}
+       setModel={setModel}
+       setDataFunction={setDataFunction}
+       disabled={disabled}
+       className={className}
+       fields={["id", "configuration", "name"]}
+     />
   );
 }
 
 MongodbRealmToolInput.propTypes = {
-  fieldName: PropTypes.string,
-  dataObject: PropTypes.object,
-  setDataObject: PropTypes.func,
+  model: PropTypes.object,
+  setModel: PropTypes.func,
   disabled: PropTypes.bool,
-  textField: PropTypes.string,
-  valueField: PropTypes.string,
-  tool_prop: PropTypes.string,
+  className: PropTypes.string,
+  fieldName: PropTypes.string
 };
 
 MongodbRealmToolInput.defaultProps = {
-  valueField: "id",
-  textField: "name",
   fieldName: "mongoToolId",
 };
 
