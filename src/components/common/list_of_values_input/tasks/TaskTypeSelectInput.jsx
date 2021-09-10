230f2bf4e6f849ccd889e30adc6fd921bd4b5d3f
Noah Champoux <noah@opsera.io>
2021-09-10 11:24:15 -0500

    Wording enhancements for tasks

--- src/components/common/list_of_values_input/tasks/TaskTypeSelectInput.jsx
+++ src/components/common/list_of_values_input/tasks/TaskTypeSelectInput.jsx
@@ -5,9 +5,9 @@ import {AuthContext} from "contexts/AuthContext";
 import {TASK_TYPES} from "components/tasks/task-types";
 
 export const productionTaskTypes = [
-  {text: "Salesforce Organization Sync", value: TASK_TYPES.SYNC_SALESFORCE_REPO},
-  {text: "Salesforce Branch Structuring", value: TASK_TYPES.SYNC_SALESFORCE_BRANCH_STRUCTURE},
-  {text: "Git to Git Sync", value: TASK_TYPES.SYNC_GIT_BRANCHES},
+  {text: "Salesforce: Organization Sync", value: TASK_TYPES.SYNC_SALESFORCE_REPO},
+  {text: "Salesforce: Branch Structuring", value: TASK_TYPES.SYNC_SALESFORCE_BRANCH_STRUCTURE},
+  {text: "Git: Git to Git Sync", value: TASK_TYPES.SYNC_GIT_BRANCHES},
   {text: "AWS: Create ECS Cluster", value: TASK_TYPES.AWS_CREATE_ECS_CLUSTER},
   {text: "AWS: Create ECS Service", value: TASK_TYPES.AWS_CREATE_ECS_SERVICE},
   {text: "AWS: Create Lambda Function", value: TASK_TYPES.AWS_CREATE_LAMBDA_FUNCTION},
@@ -15,13 +15,13 @@ export const productionTaskTypes = [
 
 export const nonProductionTaskTypes = [
   {text: "Salesforce Organization Sync", value: TASK_TYPES.SYNC_SALESFORCE_REPO},
-  {text: "Generate Certificate for SFDX", value: TASK_TYPES.SALESFORCE_CERTIFICATE_GENERATION},
-  {text: "Salesforce Branch Structuring", value: TASK_TYPES.SYNC_SALESFORCE_BRANCH_STRUCTURE},
-  {text: "Git to Git Sync", value: TASK_TYPES.SYNC_GIT_BRANCHES},
+  {text: "Salesforce: Generate Certificate for SFDX", value: TASK_TYPES.SALESFORCE_CERTIFICATE_GENERATION},
+  {text: "Salesforce: Branch Structuring", value: TASK_TYPES.SYNC_SALESFORCE_BRANCH_STRUCTURE},
+  {text: "Git: Git to Git Sync", value: TASK_TYPES.SYNC_GIT_BRANCHES},
   {text: "AWS: Create ECS Cluster", value: TASK_TYPES.AWS_CREATE_ECS_CLUSTER},
   {text: "AWS: Create ECS Service", value: TASK_TYPES.AWS_CREATE_ECS_SERVICE},
   {text: "AWS: Create Lambda Function", value: TASK_TYPES.AWS_CREATE_LAMBDA_FUNCTION},
-  {name: "Create Azure AKS Cluster", value: TASK_TYPES.AZURE_CLUSTER_CREATION},
+  {name: "Azure: Create AKS Cluster", value: TASK_TYPES.AZURE_CLUSTER_CREATION},
 ];
 
 function TaskTypeSelectInput({ fieldName, model, setModel, setDataFunction, disabled }) {
