5d62ac5270b998074f74a9ea62053ad1a1c15319
“Divyesha <divyesha@opsera.io>
2021-09-10 07:37:28 -0700

    changes

--- src/components/insights/charts/opsera/bar_chart/mean_time_to_restore/opseraMeanTimeToRestoreConfigs.js
+++ src/components/insights/charts/opsera/bar_chart/mean_time_to_restore/opseraMeanTimeToRestoreConfigs.js
@@ -3,9 +3,12 @@ export default (getColor, maxValue, MeanLineLayer) => ({
   indexBy: "_id",
   colorBy: "id",
   minValue: 0,
-  layers: ["grid", "axes", "bars", "markers", "mesh", MeanLineLayer, "legends"],
+  layers: ["grid", "axes", "bars", "markers", "mesh", MeanLineLayer],
   colors: getColor,
   enableLabel: true,
   labelTextColor: "white",
-  label: d => `#${d.data.count}`
+  label: d => `#${d.data.count}`,
+  xScale: {
+    useUTC: false,
+  },
 });
