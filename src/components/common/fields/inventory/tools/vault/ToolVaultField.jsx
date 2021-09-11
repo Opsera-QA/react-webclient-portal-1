6a8be6c770fadb6f46472465f9ff2aeea8082b1c
Noah Champoux <noah@opsera.io>
2021-09-11 16:02:12 -0500

    Added ToolVaultField

---
+++ src/components/common/fields/inventory/tools/vault/ToolVaultField.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StandaloneTextFieldBase from "components/common/fields/text/standalone/StandaloneTextFieldBase";
+import ToolNameField from "components/common/fields/inventory/ToolNameField";
+
+function ToolVaultField({ model, fieldName }) {
+  if (model?.getData(fieldName) === "") {
+    return (<StandaloneTextFieldBase label={model?.getLabel(fieldName)} text={"Opsera Default Hashicorp Vault"} />);
+  }
+
+  return (<ToolNameField dataObject={model} fieldName={fieldName} />);
+}
+
+ToolVaultField.propTypes = {
+  model: PropTypes.object,
+  fieldName: PropTypes.string,
+};
+
+ToolVaultField.defaultProps = {
+  fieldName: "vault",
+};
+
+export default ToolVaultField;
