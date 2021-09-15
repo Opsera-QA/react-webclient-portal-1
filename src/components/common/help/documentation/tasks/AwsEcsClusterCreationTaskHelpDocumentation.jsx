77874dcf9a644026797d45df2c281495f88340b1
Christina Bowers <christina@opsera.io>
2021-09-15 13:47:53 -0400

    Changed task names

---
+++ src/components/common/help/documentation/tasks/AwsEcsClusterCreationTaskHelpDocumentation.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import HelpDocumentationContainer from "components/common/help/HelpDocumentationContainer";
+import PropTypes from "prop-types";
+
+function AwsEcsClusterCreationTaskHelpDocumentation({closeHelpPanel}) {
+  return (
+    <HelpDocumentationContainer helpTopic={"AWS: Create ECS Cluster"} closeHelpPanel={closeHelpPanel}>
+      <div className={"mb-1"}>When creating an ECS Cluster, it is important to know the correct values to enter. It is a nuanced operation and prior knowledge is essential. There are 2 steps to cluster creation including template creation. Once the template is created, you must select <b>Run Task</b>. If the Activity Logs indicate that cluster creation is successful, the cluster has been created in AWS and the task can be linked to a pipeline step.</div>
+      <div className={"mt-2"}><h5>Task Setup Instructions:</h5></div>
+      <ol>
+        <li>Select <b>AWS: Create ECS Cluster</b> from the Type drop down. Once this has been selected, the following values will be fetched for selection:
+          <ul>
+            <li><b>AWS Tool</b> - Select an established AWS tool from the Tool Registry.</li>
+            <li><b>Cluster Template</b> - Select Networking/Fargate or EC2.</li>
+            <li><b>Cluster Name</b> - Create a unique name for your cluster. </li>
+            <li><b>Image Type</b> - Select Windows or Linux/Unix </li>
+            <li><b>Create VPC</b> - Either use an existing VPC from the AWS VPC console or create a new one by enabling the toggle. </li>
+            <li><b>EC2 Instance Type</b> - Select a value from the list fetched from the AWS portal. </li>
+            <li><b>Key Pair</b> - Find this value in the AWS portal in EC2.</li>
+            <li><b>Existing VPC</b> - Select an existing VPC from the list of values fetched from the AWS portal. </li>
+            <li><b>Private Subnets</b> - Both subnets should be from different availability zones. Maximum 2 subnets preferred.</li>
+            <li><b>Security Group</b> - Select a value from the list fetched from the AWS portal. </li>
+          </ul></li>
+        <li>Select <b>Create</b> to save. A task template has now been created.</li>
+      </ol>
+      <div className={"mt-1"}><h5>Cluster Creation Instructions:</h5></div>
+      <ol>
+        <li>Select <b>Run Task</b> to trigger the cluster creation.</li>
+        <li>View the Activity Logs in Task Details to view status and confirm that creation was successful.</li>
+      </ol>
+      <div>If the Activity Logs indicate that cluster creation is complete, cluster has been created and can be accessed in AWS.</div>
+      <div>To create a new cluster from an existing template, edit the Cluster Name, as AWS does not support creating 2 clusters with the same name.</div>
+      <div>To edit an existing task template, click on the cog wheel icon.</div>
+      <div>Once the Task and cluster have been created, the task can be linked to a Docker step in the pipeline.</div>
+    </HelpDocumentationContainer>
+  );
+}
+
+AwsEcsClusterCreationTaskHelpDocumentation.propTypes = {
+  closeHelpPanel: PropTypes.func,
+};
+
+export default React.memo(AwsEcsClusterCreationTaskHelpDocumentation);
---
+++ src/components/common/help/documentation/tasks/AwsLambdaFunctionCreationTaskHelpDocumentation.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import HelpDocumentationContainer from "components/common/help/HelpDocumentationContainer";
+import PropTypes from "prop-types";
+
+function AwsLambdaFunctionCreationTaskHelpDocumentation({closeHelpPanel}) {
+  return (
+    <HelpDocumentationContainer helpTopic={"AWS: Create Lambda Function"} closeHelpPanel={closeHelpPanel}>
+      <div>This workflow requires setup in both Tasks and Opsera Pipelines. The workflow is separated into two parts where the user enters certain static information in the Tasks page and then links the task to the respective step in the pipeline. For more detailed information on the AWS Create Lambda Function workflow including pipeline setup, view the <a href="https://opsera.atlassian.net/l/c/Pcv3z811" target="_blank" rel="noreferrer"><b>AWS Lambda Functions Documentation</b>.</a></div>
+      <div className={"mt-2"}><h5>Task Setup Instructions:</h5></div>
+      <ol>
+        <li>Select <b>AWS: Create Lambda Function</b> from the Type drop down. Once this has been selected, the following values will be fetched for selection:
+          <ul style={{listStyleType: "none"}}>
+            <li><b>AWS Tool</b> - Select an established AWS tool from the Tool Registry.</li>
+            <li><b>Function Name</b> - Create a unique name for the function. To confirm that the name is unique and does not exist in AWS yet, click the Validate button. If it already exists user will receive an error stating that the function with that name already exists.</li>
+            <li><b>Handler</b> - Use this syntax for Java 8:   <em>example.Hello::handleRequest</em> </li>
+            <li><b>IAM Role</b> - Select from the drop down fetched from AWS. </li>
+            <li><b>Runtime</b> - Select the language to write the function. Currently supported: Java 8 </li>
+          </ul></li>
+        <li> Select <b>Save</b> to create a template.</li>
+      </ol>
+      <div className={"mt-2"}>Once the templates are created the user can create and deploy the functions via the Opsera pipeline.  </div>
+    </HelpDocumentationContainer>
+  );
+}
+
+AwsLambdaFunctionCreationTaskHelpDocumentation.propTypes = {
+  closeHelpPanel: PropTypes.func,
+};
+
+export default React.memo(AwsLambdaFunctionCreationTaskHelpDocumentation);
