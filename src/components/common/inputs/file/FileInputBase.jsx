2375d002d7b4a3e8c0e9bde1585eef260c8c3c8b
Faseeh <faseeh@opsera.io>
2021-09-17 20:52:49 +0530

    covert tool reg change WIP

--- src/components/common/inputs/file/FileInputBase.jsx
+++ src/components/common/inputs/file/FileInputBase.jsx
@@ -4,15 +4,22 @@ import InputContainer from "components/common/inputs/InputContainer";
 import InputLabel from "components/common/inputs/info_text/InputLabel";
 import InfoText from "components/common/inputs/info_text/InfoText";
 
-function FileInputBase({ fieldName, dataObject, setDataObject, disabled }) {
+function FileInputBase({ fieldName, dataObject, setDataObject, setDataFunction, disabled }) {
   const [field] = useState(dataObject.getFieldById(fieldName));
   const [errorMessage, setErrorMessage] = useState("");
 
   const validateAndSetData = (value) => {
     let newDataObject = dataObject;
-    newDataObject.setTextData(fieldName, value);
+    console.log(value);
+    if (setDataFunction) {
+      newDataObject = setDataFunction(value);
+    }
+    else {
+      newDataObject = {...dataObject};
+      newDataObject.setTextData(fieldName, value);
+      setDataObject({...newDataObject});
+    }
     setErrorMessage(newDataObject.getFieldError(fieldName));
-    setDataObject({...newDataObject});
   };
 
   return (
@@ -23,6 +30,8 @@ function FileInputBase({ fieldName, dataObject, setDataObject, disabled }) {
         disabled={disabled}
         value={dataObject.getData(fieldName)}
         onChange={(event) => validateAndSetData(event.target.value)}
+        accept=".dat"
+        onClick={ e => e.target.value = null}
       />
       <InfoText field={field} errorMessage={errorMessage}/>
     </InputContainer>
@@ -33,6 +42,7 @@ FileInputBase.propTypes = {
   fieldName: PropTypes.string,
   dataObject: PropTypes.object,
   setDataObject: PropTypes.func,
+  setDataFunction: PropTypes.func,
   inputPopover: PropTypes.object,
   disabled: PropTypes.bool
 };
