4e14c6f670b1070c059e3fd7e37775bbc72e22d2
Noah Champoux <noah@opsera.io>
2021-09-21 15:16:20 -0500

    Created AzureToolApplicationSelectInput component

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/azure_functions/inputs/AzureFunctionsStepApplicationSelectInput.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AzureToolApplicationSelectInput
+  from "components/common/list_of_values_input/tools/azure/credentials/AzureToolApplicationSelectInput";
+
+function AzureFunctionsStepApplicationSelectInput({fieldName, model, setModel, azureToolId, setApplicationData}) {
+  // TODO: We should be pulling the applications based on ID in next input
+  const setDataFunction = (fieldName, selectedOption) => {
+    let newDataObject = {...model};
+    newDataObject.setData(fieldName, selectedOption._id);
+    newDataObject.setData("region", "");
+    newDataObject.setData("machine_type", "");
+    setApplicationData(selectedOption?.configuration);
+    setModel({...newDataObject});
+  };
+
+  const clearDataFunction = () => {
+    let newDataObject = {...model};
+    newDataObject.setData(fieldName, "");
+    newDataObject.setData("region", "");
+    newDataObject.setData("applicationType", "");
+    newDataObject.setData("artifactStepId", "");
+    setApplicationData(null);
+    setModel({...newDataObject});
+  };
+
+  return (
+    <AzureToolApplicationSelectInput
+      model={model}
+      setModel={setModel}
+      azureToolId={azureToolId}
+      fieldName={fieldName}
+      setDataFunction={setDataFunction}
+      clearDataFunction={clearDataFunction}
+    />
+  );
+}
+
+AzureFunctionsStepApplicationSelectInput.propTypes = {
+  fieldName: PropTypes.string,
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  azureToolId: PropTypes.string.isRequired,
+  setApplicationData: PropTypes.func,
+};
+
+AzureFunctionsStepApplicationSelectInput.defaultProps = {
+  fieldName: "azureCredentialId",
+};
+
+export default AzureFunctionsStepApplicationSelectInput;
