eb73633a52fe381416ffaca7e7c5f3dfa842f54c
Noah Champoux <noah@opsera.io>
2021-09-22 12:51:38 -0500

    Pipeline Wizard tweaks

--- src/components/common/help/documentation/pipelines/wizard/SfdcWizardComponentTypeSelectionHelpDocumentation.jsx
+++ src/components/common/help/documentation/pipelines/wizard/SfdcWizardComponentTypeSelectionHelpDocumentation.jsx
@@ -37,7 +37,7 @@ function SfdcWizardComponentTypeSelectionHelpDocumentation({closeHelpPanel}) {
             </li>
           </ul>
         </li>
-        <li><b>Include Dependencies</b> - By default, all CustomObject dependencies are included.
+        <li><b>Include Dependencies</b> - By default, all CustomObject dependencies are included while selecting components from the Git side.
           By disabling this toggle, dependencies will be excluded unless explicitly selected.
         </li>
       </ul>
