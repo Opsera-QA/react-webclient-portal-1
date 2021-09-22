ecbed68e65f81474366bcd52d3a67fc7c954186e
Noah Champoux <noah@opsera.io>
2021-09-22 08:31:54 -0500

    Passing reverse value to ensure it does not get lost

--- src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/SfdcPipelineWizardInitializationScreen.jsx
+++ src/components/workflow/wizards/sfdc_pipeline_wizard/initialization_screen/SfdcPipelineWizardInitializationScreen.jsx
@@ -242,7 +242,7 @@ const SfdcPipelineWizardInitializationScreen = ({ pipelineWizardModel, setPipeli
       newPipelineWizardModel.setData("toDate", parsedToDate);
     }
 
-    newPipelineWizardModel.setData("includeDependencies", existingRecord?.includeDependencies !== false);
+    newPipelineWizardModel.setData("includeDependencies", existingRecord?.excludeDependencies !== false);
     setPipelineWizardModel({...newPipelineWizardModel});
     setPipelineWizardScreen(PIPELINE_WIZARD_SCREENS.COMPONENT_SELECTOR);
   };
