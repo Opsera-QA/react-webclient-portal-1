b07ea145706580e6566e301b79f5f147b8b6499a
Noah Champoux <noah@opsera.io>
2021-09-23 18:52:01 -0500

    Copied over new inputs from test-master

---
+++ src/components/common/list_of_values_input/tools/jenkins/JenkinsDestinationSalesForceCredentialsInput.jsx
@@ -0,0 +1,97 @@
+
+import React from "react";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+import PropTypes from "prop-types";
+import { RegistryPopover } from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/jenkins/utility";
+import { OverlayTrigger, Button } from "react-bootstrap";
+import { faExclamationCircle, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+function JenkinsDestinationSalesForceCredentialsInput({ dataObject, setDataObject, sfdcList, disabled, busy }) {
+
+  const handleDestinationSFDCChange = (fieldname, selectedOption) => {
+		if (selectedOption.id && selectedOption.configuration) {
+			let newDataObject = { ...dataObject };
+			newDataObject.setData('sfdcDestToolId', selectedOption.id);
+			newDataObject.setData('destAccountUsername', selectedOption.configuration ? selectedOption.configuration.destAccountUsername : "");
+			setDataObject({ ...newDataObject });
+		}
+	};
+
+  const displayOverlayTrigger = (field) => {
+    if (field, dataObject.data[field]) {
+      return (<OverlayTrigger
+        trigger="click"
+        rootClose
+        placement="left"
+        overlay={RegistryPopover(sfdcList[sfdcList.findIndex((x) => x.id === dataObject.data[field])])}
+      >
+        <FontAwesomeIcon
+          icon={faEllipsisH}
+          className="fa-pull-right pointer pr-1"
+          onClick={() => document.body.click()}
+        />
+      </OverlayTrigger>);
+    }
+    return null;
+  };
+
+  const footer = () => {
+    return (<div className="form-text text-muted p-2">
+      <FontAwesomeIcon icon={faExclamationCircle} className="text-muted mr-1" fixedWidth />
+		  No accounts have been registered for SalesForce. Please go to
+      <Link to="/inventory/tools">Tool Registry</Link> and add a SalesForce (SFDC) Account entry
+		  in order to proceed.
+    </div>);
+  };
+
+  if (busy ||  (sfdcList && sfdcList.length > 0)) {
+    return (<>
+      {displayOverlayTrigger('sfdcDestToolId')}
+      <SelectInputBase
+        fieldName={'sfdcDestToolId'}
+        dataObject={dataObject}
+        setDataFunction={handleDestinationSFDCChange}
+        setDataObject={setDataObject}
+        placeholderText={""}
+        selectOptions={sfdcList}
+        valueField="id"
+        textField="name"
+        disabled={disabled || busy}
+        busy={busy}
+			/>
+      {dataObject.data.destAccountUsername && dataObject.data.destAccountUsername.length > 0 && (
+						<div className="text-right pt-2">
+							<OverlayTrigger
+								trigger="click"
+								rootClose
+								placement="left"
+								overlay={RegistryPopover(
+									sfdcList[sfdcList.findIndex((x) => x.id === dataObject.data.sfdcDestToolId)]
+								)}
+							>
+								<Button variant="outline-dark" size="sm">Info</Button>
+							</OverlayTrigger>
+						</div>
+					)}
+    </>);
+    } else {
+      return footer();
+    }
+}
+
+JenkinsDestinationSalesForceCredentialsInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+  sfdcList: PropTypes.any,
+  busy: PropTypes.bool,
+};
+
+JenkinsDestinationSalesForceCredentialsInput.defaultProps = {
+  disabled: false,
+  busy: false
+};
+
+export default JenkinsDestinationSalesForceCredentialsInput;
---
+++ src/components/common/list_of_values_input/tools/jenkins/JenkinsSfdcUnitTestTypeSelectInput.jsx
@@ -0,0 +1,47 @@
+
+import React from "react";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+import PropTypes from "prop-types";
+
+const UNIT_TEST_OPTIONS = [
+	{ value: "", label: "Select One", isDisabled: "yes" },
+	{ value: "RunLocalTests", label: "Run Local Tests" },
+	{ value: "RunAllTestsInOrg", label: "Run All Tests In Org" },
+	{ value: "RunSpecifiedTests", label: "Run Specified Tests" },
+];
+const PROD_UNIT_TEST_OPTIONS = [
+	{ value: "", label: "Select One", isDisabled: "yes" },
+	{ value: "NoTestRun", label: "No Test Run" },
+	{ value: "RunLocalTests", label: "Run Local Tests" },
+	{ value: "RunAllTestsInOrg", label: "Run All Tests In Org" },
+	{ value: "RunSpecifiedTests", label: "Run Specified Tests" },
+];
+function JenkinsSfdcUnitTestTypeSelectInput({ dataObject, setDataObject,  disabled }) {
+    const options = dataObject.data.jobType === "SFDC UNIT TESTING" ?  UNIT_TEST_OPTIONS : PROD_UNIT_TEST_OPTIONS;
+    return (<SelectInputBase
+      fieldName={'sfdcUnitTestType'}
+      dataObject={dataObject}
+      setDataObject={setDataObject} 
+      placeholderText={"Please select type of unit test"}
+      selectOptions={options}
+      valueField="value"
+      textField="label"
+      disabled={disabled}
+    />);
+  
+}
+
+JenkinsSfdcUnitTestTypeSelectInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+ 
+  
+};
+
+JenkinsSfdcUnitTestTypeSelectInput.defaultProps = {
+  disabled: false,
+ 
+};
+
+export default JenkinsSfdcUnitTestTypeSelectInput;
