4d7e6cc214fb02302e1b48faa9aad150e60bacbb
Faseeh <faseeh@opsera.io>
2021-09-24 13:00:39 +0530

    added support for java 7 on commandline step

--- src/components/common/list_of_values_input/workflow/pipelines/DependencyMultiSelectInput.jsx
+++ src/components/common/list_of_values_input/workflow/pipelines/DependencyMultiSelectInput.jsx
@@ -52,7 +52,11 @@ export const itemArray = [
     "dependencyType" : "terrascan",
     "version": "1.4.0",
   },
-
+  {
+    "name": "Java openJDK Version 7",
+    "dependencyType" : "java",
+    "version": "7",
+  },
   {
     "name": "Java openJDK Version 8",
     "dependencyType" : "java",
