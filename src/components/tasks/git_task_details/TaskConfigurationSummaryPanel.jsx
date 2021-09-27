63856d2230845fcf85a72a7aaee01d92d2d1a0ac
Noah Champoux <noah@opsera.io>
2021-09-27 15:18:11 -0500

    Moved Task Configuration Summary Cards into their own summary panel

---
+++ src/components/tasks/git_task_details/TaskConfigurationSummaryPanel.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LoadingDialog from "components/common/status_notifications/loading";
+import sfdcGitTaskConfigurationMetadata
+  from "components/tasks/git_task_details/configuration_forms/sfdc-org-sync/sfdc-git-task-configuration-metadata";
+import SFDCGitTaskTypeSummaryCard
+  from "components/tasks/git_task_details/configuration_forms/sfdc-org-sync/SFDCGitTaskTypeSummaryCard";
+import SFDCBranchStructuringTaskTypeSummaryCard from "./configuration_forms/sfdc-branch-structure/SFDCBranchStructuringTaskTypeSummaryCard";
+import sfdcGitBranchTaskConfigurationMetadata
+  from "components/tasks/git_task_details/configuration_forms/sfdc-branch-structure/sfdc-git-branch-structuring-task-configuration-metadata";
+import branchToBranchGitTaskConfigurationMetadata from "components/tasks/git_task_details/configuration_forms/branch-to-branch/branch-to-branch-git-task-configuration";
+import BranchToBranchGitTaskTypeSummaryCard from "components/tasks/git_task_details/configuration_forms/branch-to-branch/BranchToBranchGitTaskTypeSummaryCard";
+import sfdxCertGenTaskConfigurationMetadata from "components/tasks/git_task_details/configuration_forms/sfdx-cert-gen/sfdx-cert-gen-task-configuration-metadata";
+import SFDXCertGenTaskTypeSummaryCard from "./configuration_forms/sfdx-cert-gen/SFDXCertGenTaskTypeSummaryCard";
+import ECSCreationTaskTypeSummaryCard from "./configuration_forms/ecs-cluster-creation/ECSCreationTaskTypeSummaryCard";
+import ec2ClusterCreationTaskConfigurationMetadata
+  from "./configuration_forms/ecs-cluster-creation/ecs-creation-git-task-configuration";
+import ECSServiceCreationTaskTypeSummaryCard
+  from "./configuration_forms/ecs-service-creation/ECSServiceCreationTaskTypeSummaryCard";
+import ec2ServiceCreationTaskConfigurationMetadata
+  from "./configuration_forms/ecs-service-creation/ecs-service-creation-git-task-configuration";
+import AwsLambdaTaskTypeSummaryCard from "./configuration_forms/aws-lambda-creation/AwsLambdaSummaryPanel";
+import awsLambdaFunctionTaskConfigurationMetadata
+  from "./configuration_forms/aws-lambda-creation/aws-lambda-metadata";
+import AzureClusterSummaryPanel from "./configuration_forms/azure-cluster-creation/AzureClusterSummaryPanel";
+import azureAksClusterTaskConfigurationMetadata
+  from "./configuration_forms/azure-cluster-creation/azure-cluster-metadata";
+import {TASK_TYPES} from "components/tasks/task.types";
+import modelHelpers from "components/common/model/modelHelpers";
+
+function TaskConfigurationSummaryPanel({ taskModel }) {
+  const getTaskTypeSummaryPanel = () => {
+    switch (taskModel?.getData("type")) {
+      case TASK_TYPES.SYNC_SALESFORCE_REPO:
+        return (
+          <SFDCGitTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), sfdcGitTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.SYNC_SALESFORCE_BRANCH_STRUCTURE:
+        return (
+          <SFDCBranchStructuringTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), sfdcGitBranchTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.SYNC_GIT_BRANCHES:
+        return (
+          <BranchToBranchGitTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), branchToBranchGitTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.SALESFORCE_CERTIFICATE_GENERATION:
+        return (
+          <SFDXCertGenTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), sfdxCertGenTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.AWS_CREATE_ECS_CLUSTER:
+        return (
+          <ECSCreationTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), ec2ClusterCreationTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.AWS_CREATE_ECS_SERVICE:
+        return (
+          <ECSServiceCreationTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), ec2ServiceCreationTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.AWS_CREATE_LAMBDA_FUNCTION:
+        return (
+          <AwsLambdaTaskTypeSummaryCard
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), awsLambdaFunctionTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      case TASK_TYPES.AZURE_CLUSTER_CREATION:
+        return (
+          <AzureClusterSummaryPanel
+            gitTaskConfigurationData={
+              modelHelpers.parseObjectIntoModel(taskModel?.getData("configuration"), azureAksClusterTaskConfigurationMetadata)
+            }
+            gitTasksData={taskModel}
+          />
+        );
+      default:
+        return (<div>No type associated with this Task</div>);
+    }
+  };
+
+  if (taskModel == null) {
+    return (<LoadingDialog size="sm"/>);
+  }
+
+  return (
+    <div>
+      {getTaskTypeSummaryPanel()}
+    </div>
+  );
+}
+
+TaskConfigurationSummaryPanel.propTypes = {
+  taskModel: PropTypes.object,
+};
+
+export default TaskConfigurationSummaryPanel;
