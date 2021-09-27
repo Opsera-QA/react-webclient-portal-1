14751fa5552d75365c0bad420b2301d3d3034fda
Faseeh <faseeh@opsera.io>
2021-09-27 19:44:55 +0530

    added support for argo projects WIP

---
+++ src/components/inventory/tools/tool_details/tool_jobs/argo/argo-project-metadata.js
@@ -0,0 +1,67 @@
+const argoProjectMetadata = {
+  type: "Argo Project",
+  fields: [
+    {
+      label: "Argo Tool",
+      id: "toolId",
+      isRequired: true,
+      maxLength: 24,
+      regexDefinitionName: "mongoId",
+    },
+    {
+      label: "Name",
+      id: "name",
+      isRequired: true,
+      lowercase: true,
+      spacesAllowed: false,
+      formText: "Name cannot contain spaces.",
+      maxLength: 63
+    },
+    {
+      label: "SCM type",
+      id: "service",
+      isRequired: true
+    },
+    {
+      label: "SCM Tool",
+      id: "gitToolId",
+      isRequired: true,
+      maxLength: 24,
+      regexDefinitionName: "mongoId",
+    },   
+    {
+      id: "gitUrl",
+    },
+    {
+      id: "sshUrl",
+    },
+    {
+      label: "Workspace/Project",
+      id: "workspace",
+      maxLength: 255,
+      regexDefinitionName: "generalTextWithSpacesSlash",
+    },
+    {
+      label: "Repository Name",
+      id: "repositoryName",
+      isRequired: true
+    },
+    {
+      label: "Repository Type",
+      id: "repositoryType",
+      isRequired: true
+    },
+  ],
+  newObjectFields: {
+    name: "",
+    gitToolId: "",
+    service: "",
+    gitUrl: "",
+    sshUrl: "",
+    workspace: "",
+    repositoryName: "",
+    repositoryType: "git",
+  }
+};
+
+export default argoProjectMetadata;
