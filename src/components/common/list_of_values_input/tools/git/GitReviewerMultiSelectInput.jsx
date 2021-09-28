dc0f44a4af18aa6d73dc4a58f95bc537f4ee1840
Noah Champoux <noah@opsera.io>
2021-09-28 10:28:12 -0500

    Cleaned up GitReviewerMultiSelectInput

--- src/components/common/list_of_values_input/tools/git/GitReviewerMultiSelectInput.jsx
+++ src/components/common/list_of_values_input/tools/git/GitReviewerMultiSelectInput.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, {useEffect, useContext, useState, useRef} from 'react';
 import PropTypes from 'prop-types';
 import MultiSelectInputBase from "components/common/inputs/select/MultiSelectInputBase";
 import {DialogToastContext} from "contexts/DialogToastContext";
 import {AuthContext} from "contexts/AuthContext";
 import toolsActions from "components/inventory/tools/tools-actions";
+import axios from "axios";
 
 function GitReviewerMultiSelectInput({
   gitToolId, 
@@ -23,39 +24,70 @@ function GitReviewerMultiSelectInput({
   const [reviewers, setReviewers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [allReviewers, setAllReviewers] = useState([]);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
 
   useEffect(() => {
-    loadData();
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
   }, [gitToolId]);
 
-  useEffect(() => {    
-    setReviewers(allReviewers.filter(acc => ((service === 'bitbucket' && acc.workspace === workspace && acc.repository === repository) || (service !== 'bitbucket' && acc.repository === repository))));
+  // TODO: Rewrite
+  useEffect(() => {
+    setReviewers(
+      allReviewers.filter(acc =>
+        ((service === 'bitbucket' && acc.workspace === workspace && acc.repository === repository)
+          || (service !== 'bitbucket' && acc.repository === repository)))
+    );
   }, [workspace, repository]);
 
-  const loadData = async () => {
+  const loadData = async (cancelSource = cancelTokenSource) => {
     try {
-        setIsLoading(true);
-        await getReviewers();
-    }
-    catch (error) {
-        console.error(error);
-        toastContext.showServiceUnavailableDialog();
-    }
-    finally {
+      setIsLoading(true);
+      await getReviewers(cancelSource);
+    } catch (error) {
+      console.error(error);
+      toastContext.showServiceUnavailableDialog();
+    } finally {
+      if (isMounted?.current === true) {
         setIsLoading(false);
+      }
     }
   };
 
-  const getReviewers = async () => {
-    const response = await toolsActions.getRoleLimitedToolById(gitToolId, getAccessToken);
-    if(response.data.data[0].accounts){
-      setAllReviewers(response.data.data[0].accounts);      
-      setReviewers(response.data.data[0].accounts
+  const getReviewers = async (cancelSource = cancelTokenSource) => {
+    const response = await toolsActions.getRoleLimitedToolByIdV2(getAccessToken, cancelSource, gitToolId);
+    const tools = response?.data?.data;
+    const accounts = Array.isArray(tools) && tools.length > 0 ? tools[0]?.accounts : undefined;
+
+    if(Array.isArray(accounts) && accounts.length > 0){
+      setAllReviewers(accounts);
+      setReviewers(accounts
         .filter(acc => ((service === 'bitbucket' && acc.workspace === workspace && acc.repository === repository) || (service !== 'bitbucket' && acc.repository === repository))));
     }    
   };
 
   const getNoReviewersMessage = () => {
+    if (!isLoading && gitToolId !== "" && (!Array.isArray(allReviewers) || allReviewers.length === 0)) {
+      return ("No Reviewers Assigned in Accounts for the selected tool.");
+    }
+
     if (!isLoading && (!Array.isArray(reviewers) || reviewers.length === 0) && gitToolId !== "") {
         return ("No Reviewers found for the details provided");
     }
@@ -82,22 +114,18 @@ function GitReviewerMultiSelectInput({
   );
 }
 
-GitReviewerMultiSelectInput.propTypes = {    
-    gitToolId: PropTypes.string,
-    fieldName: PropTypes.string,
-    dataObject: PropTypes.object,
-    setDataObject: PropTypes.func,
-    setDataFunction: PropTypes.func,
-    disabled: PropTypes.bool,
-    visible: PropTypes.bool,
-    clearDataFunction: PropTypes.func,
-    service: PropTypes.string,
-    workspace: PropTypes.string,
-    repository: PropTypes.string
-};
-
-GitReviewerMultiSelectInput.defaultProps = {
-    visible: true,
+GitReviewerMultiSelectInput.propTypes = {
+  gitToolId: PropTypes.string,
+  fieldName: PropTypes.string,
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  setDataFunction: PropTypes.func,
+  disabled: PropTypes.bool,
+  visible: PropTypes.bool,
+  clearDataFunction: PropTypes.func,
+  service: PropTypes.string,
+  workspace: PropTypes.string,
+  repository: PropTypes.string
 };
 
 export default GitReviewerMultiSelectInput;
