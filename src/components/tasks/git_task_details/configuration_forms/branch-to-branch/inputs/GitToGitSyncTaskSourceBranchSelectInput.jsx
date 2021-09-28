92e2aae528ad7620a2165d7b0dcf64a75684ce6f
Noah Champoux <noah@opsera.io>
2021-09-28 12:30:13 -0500

    OPL-1518  Option to copy Package Xml (Manifest file) while executing Salesforce Git Sync Task

--- src/components/tasks/git_task_details/configuration_forms/branch-to-branch/inputs/GitToGitSyncTaskSourceBranchSelectInput.jsx
+++ src/components/tasks/git_task_details/configuration_forms/branch-to-branch/inputs/GitToGitSyncTaskSourceBranchSelectInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import GitBranchInput from "components/common/list_of_values_input/tools/git/GitBranchInput";
 
-function GitToGitSyncTaskSourceBranchInput({model, setModel, disabled, targetBranch}) {
+function GitToGitSyncTaskSourceBranchSelectInput({model, setModel, disabled, targetBranch}) {
   const setSourceBranch = (fieldName, selectedOption) => {
     let newModel = {...model};
     newModel.setData("sourceBranch", selectedOption);
@@ -34,11 +34,11 @@ function GitToGitSyncTaskSourceBranchInput({model, setModel, disabled, targetBra
   );
 }
 
-GitToGitSyncTaskSourceBranchInput.propTypes = {
+GitToGitSyncTaskSourceBranchSelectInput.propTypes = {
   model: PropTypes.object,
   setModel: PropTypes.func,
   disabled: PropTypes.bool,
   targetBranch: PropTypes.string,
 };
 
-export default GitToGitSyncTaskSourceBranchInput;
+export default GitToGitSyncTaskSourceBranchSelectInput;
