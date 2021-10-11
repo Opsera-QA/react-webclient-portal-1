23a2b23a0da001e944c398a3788dea6bb6383017
Noah Champoux <noah@opsera.io>
2021-10-11 12:07:24 -0500

    Wired up Help Toggle on Pipeline Summary action bar (Feature flagged)

--- src/components/common/actions/pipeline/PipelineSummaryActionBar.jsx
+++ src/components/common/actions/pipeline/PipelineSummaryActionBar.jsx
@@ -1,22 +1,30 @@
-import React  from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
 import ActionBarDeleteButton from "../buttons/ActionBarDeleteButton";
 import ActionBarDuplicateButton from "../buttons/ActionBarDuplicateButton";
 import ActionBarTransferPipelineButton from "./buttons/ActionBarTransferPipelineButton";
 import ActionBarViewButton from "../buttons/ActionBarViewButton";
 import ActionBarPublishPipelineButton from "./buttons/ActionBarPublishPipelineButton";
-import ActionBarEditAccessRolesButton from "components/common/actions/buttons/ActionBarEditAccessRolesButton";
 import PipelineSubscriptionIcon from "components/common/icons/subscription/PipelineSubscriptionIcon";
+import ActionBarToggleHelpButton from "components/common/actions/buttons/ActionBarToggleHelpButton";
+import {DialogToastContext} from "contexts/DialogToastContext";
+import PipelinesHelpDocumentation from "components/common/help/documentation/pipelines/PipelinesHelpDocumentation";
 
-function PipelineSummaryActionBar({pipelineModel, canTransferPipeline, handleDuplicateClick, handleDeleteClick, handleEditAccessRolesClick, handleViewClick, handlePublishClick, pipeline, loadPipeline}) {
+function PipelineSummaryActionBar({pipelineModel, canTransferPipeline, handleDuplicateClick, handleDeleteClick, handleViewClick, handlePublishClick, pipeline, loadPipeline}) {
+  const toastContext = useContext(DialogToastContext);
+
+  const toggleHelp = () => {
+    toastContext.showOverlayPanel(<PipelinesHelpDocumentation />);
+  };
+
   return (
     <div className="text-muted action-bar justify-content-end d-flex pb-2 pt-2">
       <div className="ml-3"><PipelineSubscriptionIcon pipelineModel={pipelineModel} /></div>
       {handleViewClick && <div className="ml-3"><ActionBarViewButton handleViewClick={handleViewClick} itemName={"Pipeline"} data={pipeline} /></div>}
       {handlePublishClick && <div className="ml-3"><ActionBarPublishPipelineButton handlePublishClick={handlePublishClick} itemId={pipeline._id} /></div>}
       {handleDuplicateClick && <div className="ml-3"><ActionBarDuplicateButton handleDuplicateClick={handleDuplicateClick} itemName={"Pipeline"} itemId={pipeline._id} /></div>}
-      {/*{handleEditAccessRolesClick && <div className="ml-3"><ActionBarEditAccessRolesButton handleEditAccessRolesClick={handleEditAccessRolesClick} itemName={"Pipeline"} /></div>}*/}
       {canTransferPipeline && pipeline.account != null && <div className="ml-3"><ActionBarTransferPipelineButton loadPipeline={loadPipeline} pipeline={pipeline} itemId={pipeline._id} /></div>}
+      {/*<ActionBarToggleHelpButton className={"ml-3 action-bar-icon pointer"} toggleHelp={toggleHelp} />*/}
       {handleDeleteClick && <div className="ml-3"><ActionBarDeleteButton handleDeleteClick={handleDeleteClick} itemId={pipeline._id} itemName={"Pipeline"} /></div>}
     </div>
   );
