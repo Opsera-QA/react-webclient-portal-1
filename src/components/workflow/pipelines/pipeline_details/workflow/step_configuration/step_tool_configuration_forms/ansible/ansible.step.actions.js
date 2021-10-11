45471df5de2ed334658207aec425f563392ee800
Murali Kurma <murali@opsera.io>
2021-10-11 23:25:10 +0530

    Ansible pipeline changes for master branch

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ansible/ansible.step.actions.js
@@ -0,0 +1,24 @@
+import baseActions from "../../../../../../../../utils/actionsBase";
+
+const AnsibleStepActions = {};
+
+AnsibleStepActions.getScmRepositoryFiles = async (getAccessToken, cancelTokenSource, playbookFilePath, defaultBranch, gitToolId, projectId, workspace, service) => {
+  const apiUrl = `/tools/scm/getFilesUnderPath`;
+  const postBody = {
+    branch: defaultBranch,
+    filePath: playbookFilePath,
+    gitToolId: gitToolId,
+    projectId: projectId,
+    workspace: workspace,
+    service: service,
+  };
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+
+};
+
+AnsibleStepActions.getConfigurations = async (getAccessToken, cancelTokenSource, id) => {
+  const apiUrl = `/tools/ansible/configurations/${id}`;
+  return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl);
+};
+
+export default AnsibleStepActions;
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/ansible/inputs/AnsibleStepGitRepositorySelectInput.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import RepositorySelectInput from "../../../../../../../../common/list_of_values_input/tools/git/RepositorySelectInput";
+
+function AnsibleStepGitRepositorySelectInput({model, setModel, disabled}) {
+  const setDataFunction = (fieldName, selectedOption) => {
+    let newModelObject = {...model};
+    newModelObject.setData("repository", selectedOption.name);
+    newModelObject.setData("repoId", selectedOption.id);
+    newModelObject.setData("projectId", selectedOption.id);
+    newModelObject.setData("sshUrl", selectedOption.sshUrl || "");
+    newModelObject.setData("gitUrl", selectedOption.httpUrl || "");
+    setModel({...newModelObject});
+  };
+  const clearDataFunction=(fieldName)=>{
+    let newModelObject = {...model};
+    newModelObject.setData("repository", '');
+    newModelObject.setData("repoId", '');
+    newModelObject.setData("projectId", '');
+    newModelObject.setData("sshUrl", '');
+    newModelObject.setData("gitUrl", '');
+    setModel({...newModelObject});
+  };
+
+  if(!model) {
+    return null;
+  }
+
+  return (
+     <RepositorySelectInput
+       fieldName={"repository"}
+       service={model?.getData("service")}
+       gitToolId={model?.getData("gitToolId")}
+       workspace={model?.getData("workspace")}
+       dataObject={model}
+       setDataObject={setModel}
+       clearDataFunction={clearDataFunction}
+       setDataFunction={setDataFunction}
+       disabled={disabled}
+     />
+  );
+}
+
+AnsibleStepGitRepositorySelectInput.propTypes = {
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default AnsibleStepGitRepositorySelectInput;
