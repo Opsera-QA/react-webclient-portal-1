afd05dbdb91d0a8f0587471a3952a86a4f2e7e82
Noah Champoux <noah@opsera.io>
2021-10-15 13:13:05 -0500

    Cleaned up SalesforceOrganizationSyncTaskNewBranchToggleInput

--- src/components/tasks/details/tasks/sfdc-org-sync/salesforceOrganizationSyncTaskConfigurationMetadata.js
+++ src/components/tasks/details/tasks/sfdc-org-sync/salesforceOrganizationSyncTaskConfigurationMetadata.js
@@ -154,6 +154,7 @@ const salesforceOrganizationSyncTaskConfigurationMetadata = {
     workspace: "",
     workspaceName: "",
     repository: "",
+    branch: "",
     gitBranch: "",
     sourceBranch: "",
     autoApprove: false,
