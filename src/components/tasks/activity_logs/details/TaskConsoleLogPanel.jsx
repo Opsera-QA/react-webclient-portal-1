5b1998d863f24947cf9e0be1ab6faf47fbefea1f
Noah Champoux <noah@opsera.io>
2021-10-15 10:13:46 -0500

    Added containers to single and multiple console log tabs

--- src/components/tasks/activity_logs/details/TaskConsoleLogPanel.jsx
+++ src/components/tasks/activity_logs/details/TaskConsoleLogPanel.jsx
@@ -3,14 +3,17 @@ import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
-import PipelineTaskConsoleLogField from "components/common/fields/pipelines/activity/PipelineTaskConsoleLogField";
+import StandaloneConsoleLogField from "components/common/fields/pipelines/activity/StandaloneConsoleLogField";
 
 function TaskActivityConsoleLogPanel({ gitTaskActivityData }) {
   return (
     <SummaryPanelContainer>
       <Row>
         <Col md={12}>
-          <PipelineTaskConsoleLogField dataObject={gitTaskActivityData} />
+          <StandaloneConsoleLogField
+            apiResponse={gitTaskActivityData["api_response"]}
+            dataObject={gitTaskActivityData}
+          />
         </Col>
       </Row>
     </SummaryPanelContainer>
