23123c0c78d6cadce9ec3a034a8753782070f06f
Noah Champoux <noah@opsera.io>
2021-10-19 08:01:24 -0500

    Push temp fix

--- src/components/common/list_of_values_input/tools/git/RepositorySelectInput.jsx
+++ src/components/common/list_of_values_input/tools/git/RepositorySelectInput.jsx
@@ -97,10 +97,11 @@ function RepositorySelectInput(
               "Previously saved repository is no longer available. It may have been deleted. Please select another repository from the list."
             );
           }
-        } else {
-          toastContext.showSystemErrorBanner(repositoriesResponse);
         }
       }
+    }
+    else {
+      toastContext.showSystemErrorBanner(response);
     }
   };
 
