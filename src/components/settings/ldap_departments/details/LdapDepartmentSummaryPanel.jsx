73fd3849aa751b06a38e3a02b0b7fae0d519ad76
Noah Champoux <noah@opsera.io>
2021-10-19 08:29:47 -0500

    Departments tweaks

--- src/components/settings/ldap_departments/details/LdapDepartmentSummaryPanel.jsx
+++ src/components/settings/ldap_departments/details/LdapDepartmentSummaryPanel.jsx
@@ -3,8 +3,9 @@ import { Row, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 import TextFieldBase from "components/common/fields/text/TextFieldBase";
 import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
+import LdapUsersTable from "components/settings/ldap_users/LdapUsersTable";
 
-function LdapDepartmentSummaryPanel({ ldapDepartmentData, setActiveTab }) {
+function LdapDepartmentSummaryPanel({ ldapDepartmentData, orgDomain, setActiveTab }) {
   return (
     <SummaryPanelContainer setActiveTab={setActiveTab}>
       <Row>
@@ -18,13 +19,15 @@ function LdapDepartmentSummaryPanel({ ldapDepartmentData, setActiveTab }) {
           <TextFieldBase dataObject={ldapDepartmentData} fieldName={"departmentGroupName"}/>
         </Col>
       </Row>
+      <div className={"mt-3"}><LdapUsersTable orgDomain={orgDomain} userData={ldapDepartmentData.members} /></div>
     </SummaryPanelContainer>
   );
 }
 
 LdapDepartmentSummaryPanel.propTypes = {
   ldapDepartmentData: PropTypes.object,
-  setActiveTab: PropTypes.func
+  setActiveTab: PropTypes.func,
+  orgDomain: PropTypes.string,
 };
 
 
