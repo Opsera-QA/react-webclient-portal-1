0581f65e7692da62f4acf8a1449cce9fc9fd431e
“Divyesha <divyesha@opsera.io>
2021-10-19 09:22:55 -0700

    change

---
+++ src/components/insights/release_360/views/traceability/TotalBuilds.jsx
@@ -0,0 +1,134 @@
+import React, {useState, useEffect, useContext, useRef} from "react";
+import PropTypes from "prop-types";
+import { ResponsiveBar } from "@nivo/bar";
+import config from "./opseraBuildDurationBarChartConfigs";
+import ModalLogs from "components/common/modal/modalLogs";
+import {AuthContext} from "contexts/AuthContext";
+import axios from "axios";
+import chartsActions from "components/insights/charts/charts-actions";
+import ChartContainer from "components/common/panels/insights/charts/ChartContainer";
+import { defaultConfig, getColorByData, assignStandardColors, capitalizeLegend,
+  adjustBarWidth } from '../../../charts-views';
+import ChartTooltip from '../../../ChartTooltip';
+import { useHistory } from "react-router-dom";
+
+function OpseraBuildDurationBarChart({ kpiConfiguration, setKpiConfiguration, dashboardData, index, setKpis }) {
+  const history = useHistory();
+  const {getAccessToken} = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metrics, setMetrics] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [JSON.stringify(dashboardData)]);
+
+  const loadData = async (cancelSource = cancelTokenSource) => {
+    try {
+      setIsLoading(true);
+      let dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(getAccessToken, cancelSource, "totalBuilds", kpiConfiguration, dashboardTags);
+      let dataObject = response?.data ? response?.data?.data[0]?.totalBuilds?.data : [];
+      assignStandardColors(dataObject, true);
+      capitalizeLegend(dataObject, ["time"]);
+
+      if (isMounted?.current === true && dataObject) {
+        setMetrics(dataObject);
+      }
+    }
+    catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    }
+    finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const onRowSelect = (rowData) => {
+    history.push(`/blueprint/${rowData.data._id.pipelineId}/${rowData.data._id.run}`);
+  };
+
+  const getChartBody = () => {
+    if (!Array.isArray(metrics) || metrics.length === 0) {
+      return null;
+    }
+
+    return (
+      <div className="new-chart mb-3" style={{height: "300px"}}>
+        <ResponsiveBar
+          data={metrics}
+          {...defaultConfig("Duration (Minutes)", "Pipeline Run",
+            false, false, "wholeNumbers", "cutoffString")}
+          {...config(getColorByData)}
+          {...adjustBarWidth(metrics)}
+          onClick={(data) => onRowSelect(data)}
+          tooltip={({ data, value, color }) => <ChartTooltip
+            titles = {["Pipeline ID", "Duration"]}
+            values = {[data.pipelineId, `${value} minutes`]}
+            style = {false}
+            color = {color} />}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <ChartContainer
+        title={kpiConfiguration?.kpi_name}
+        kpiConfiguration={kpiConfiguration}
+        setKpiConfiguration={setKpiConfiguration}
+        chart={getChartBody()}
+        loadChart={loadData}
+        dashboardData={dashboardData}
+        index={index}
+        error={error}
+        setKpis={setKpis}
+        isLoading={isLoading}
+      />
+      <ModalLogs
+        header="Build Duration"
+        size="lg"
+        jsonMessage={metrics}
+        dataType="bar"
+        show={showModal}
+        setParentVisibility={setShowModal}
+      />
+    </div>
+  );
+}
+
+OpseraBuildDurationBarChart.propTypes = {
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
+  index: PropTypes.number,
+  setKpiConfiguration: PropTypes.func,
+  setKpis: PropTypes.func
+};
+
+export default OpseraBuildDurationBarChart;
