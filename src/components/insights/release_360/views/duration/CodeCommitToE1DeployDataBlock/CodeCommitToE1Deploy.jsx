5a2c792d8b3c20740c768ddb6131349e356c7077
harsha-opsera <harsha@opsera.io>
2021-10-19 22:40:37 -0700

    OPL-1717 Actionable insights for Code commit to E1->E2->E3

--- src/components/insights/release_360/views/duration/CodeCommitToE1DeployDataBlock/CodeCommitToE1Deploy.jsx
+++ src/components/insights/release_360/views/duration/CodeCommitToE1DeployDataBlock/CodeCommitToE1Deploy.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useRef, useContext } from "react";
+import { React, useEffect, useState, useRef, useContext, useMemo } from "react";
 // import PropTypes from "prop-types";
 import axios from "axios";
 import ThreeLineDataBlockBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockBase";
@@ -6,6 +6,17 @@ import MetricScoreText from "components/common/metrics/score/MetricScoreText";
 import { METRIC_QUALITY_LEVELS } from "components/common/metrics/text/MetricTextBase";
 import { AuthContext } from "contexts/AuthContext";
 import chartsActions from "components/insights/charts/charts-actions";
+import Model from "../../../../../../core/data_model/model";
+import genericChartFilterMetadata from "../../../../charts/generic_filters/genericChartFilterMetadata";
+import CodeCommitToE1DeployMetaData from "./code-commit-to-E1-deploy";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
+import { getTableTextColumn } from "components/common/table/table-column-helpers-v2";
+import { faDraftingCompass, faTable } from "@fortawesome/pro-light-svg-icons";
+import { getField } from "components/common/metadata/metadata-helpers";
+import { getTableDateTimeColumn } from "components/common/table/column_definitions/model-table-column-definitions";
+import FilterContainer from "components/common/table/FilterContainer";
+import VanityTable from "components/common/table/VanityTable";
 
 function CodeCommitToE1Deploy() {
   const { getAccessToken } = useContext(AuthContext);
@@ -13,6 +24,10 @@ function CodeCommitToE1Deploy() {
   const isMounted = useRef(false);
   const [isLoading, setIsLoading] = useState(false);
   const [metrics, setMetrics] = useState([]);
+  const [tableFilterDto, setTableFilterDto] = useState(
+    new Model({ ...genericChartFilterMetadata.newObjectFields }, CodeCommitToE1DeployMetaData, false)
+  );
+  const toastContext = useContext(DialogToastContext);
 
   useEffect(() => {
     if (cancelTokenSource) {
@@ -53,8 +68,82 @@ function CodeCommitToE1Deploy() {
     }
   };
 
+  const closePanel = () => {
+    toastContext.removeInlineMessage();
+    toastContext.clearOverlayPanel();
+  };
+
+  const getTable = () => {
+    return (
+      <VanityTable
+        // isLoading={isLoading}
+        columns={columns}
+        data={metrics?.codeCommitToEnvDeploy}
+        noDataMessage={"No Insights are available for this chart at this time"}
+        paginationModel={tableFilterDto}
+        setPaginationModel={setTableFilterDto}
+        loadData={loadData}
+        scrollOnLoad={false}
+        onRowSelect={onRowSelect}
+      />
+    );
+  };
+
+  const getBody = () => {
+    let newFilterDto = tableFilterDto;
+    newFilterDto.setData("totalCount", metrics?.codeCommitToEnvDeploy?.length);
+    setTableFilterDto({ ...newFilterDto });
+
+    return (
+      <FilterContainer
+        // isLoading={isLoading}
+        showBorder={false}
+        title={`Code Commit to E1 Deploy`}
+        titleIcon={faDraftingCompass}
+        body={getTable()}
+        className={"px-2 pb-2"}
+      />
+    );
+  };
+
+  const onRowSelect = () => {
+    toastContext.showOverlayPanel(
+      <FullScreenCenterOverlayContainer
+        closePanel={closePanel}
+        showPanel={true}
+        titleText={`Code Commit to E1 Deploy`}
+        showToasts={true}
+        titleIcon={faTable}
+        isLoading={false}
+        linkTooltipText={"View Full Blueprint"}
+      >
+        <div className={"p-3"}>{getBody()}</div>
+      </FullScreenCenterOverlayContainer>
+    );
+  };
+
+  const fields = [
+    { id: "jiraIssueKey", label: "Jira Issue Key" },
+    // { id: "gitlabBranch", label: "Gitlab Branch" },
+    { id: "unitTestingStageIssueUpdated", label: "Code Commit Time" },
+    { id: "peerReviewStageIssueUpdated", label: "E1 Deploy Time" },
+    { id: "codeCommitToEnvDeploy", label: "Days Difference" },
+  ];
+
+  const columns = useMemo(
+    () => [
+      getTableTextColumn(getField(fields, "jiraIssueKey")),
+      // getTableTextColumn(getField(fields, "gitlabBranch")),
+      getTableDateTimeColumn(getField(fields, "unitTestingStageIssueUpdated")),
+      getTableDateTimeColumn(getField(fields, "peerReviewStageIssueUpdated")),
+      getTableTextColumn(getField(fields, "codeCommitToEnvDeploy")),
+    ],
+    []
+  );
+
   return (
     <ThreeLineDataBlockBase
+      onClickFunction={(thisData) => onRowSelect(thisData)}
       className={"p-2"}
       topText={"Code Commit to E1 Deploy"}
       middleText={
--- src/components/insights/release_360/views/duration/CodeCommitToE3DeployDataBlock/CodeCommitToE3Deploy.jsx
+++ src/components/insights/release_360/views/duration/CodeCommitToE3DeployDataBlock/CodeCommitToE3Deploy.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useRef, useContext } from "react";
+import { React, useEffect, useState, useRef, useContext, useMemo } from "react";
 // import PropTypes from "prop-types";
 import axios from "axios";
 import ThreeLineDataBlockBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockBase";
@@ -6,6 +6,17 @@ import MetricScoreText from "components/common/metrics/score/MetricScoreText";
 import { METRIC_QUALITY_LEVELS } from "components/common/metrics/text/MetricTextBase";
 import { AuthContext } from "contexts/AuthContext";
 import chartsActions from "components/insights/charts/charts-actions";
+import Model from "../../../../../../core/data_model/model";
+import genericChartFilterMetadata from "../../../../charts/generic_filters/genericChartFilterMetadata";
+import CodeCommitToE3DeployMetaData from "./code-commit-to-E3-deploy";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
+import { getTableTextColumn } from "components/common/table/table-column-helpers-v2";
+import { faDraftingCompass, faTable } from "@fortawesome/pro-light-svg-icons";
+import { getField } from "components/common/metadata/metadata-helpers";
+import { getTableDateTimeColumn } from "components/common/table/column_definitions/model-table-column-definitions";
+import FilterContainer from "components/common/table/FilterContainer";
+import VanityTable from "components/common/table/VanityTable";
 
 function CodeCommitToE3Deploy() {
   const { getAccessToken } = useContext(AuthContext);
@@ -13,6 +24,10 @@ function CodeCommitToE3Deploy() {
   const isMounted = useRef(false);
   const [isLoading, setIsLoading] = useState(false);
   const [metrics, setMetrics] = useState([]);
+  const [tableFilterDto, setTableFilterDto] = useState(
+    new Model({ ...genericChartFilterMetadata.newObjectFields }, CodeCommitToE3DeployMetaData, false)
+  );
+  const toastContext = useContext(DialogToastContext);
 
   useEffect(() => {
     if (cancelTokenSource) {
@@ -53,8 +68,82 @@ function CodeCommitToE3Deploy() {
     }
   };
 
+  const closePanel = () => {
+    toastContext.removeInlineMessage();
+    toastContext.clearOverlayPanel();
+  };
+
+  const getTable = () => {
+    return (
+      <VanityTable
+        // isLoading={isLoading}
+        columns={columns}
+        data={metrics?.codeCommitToEnvDeploy}
+        noDataMessage={"No Insights are available for this chart at this time"}
+        paginationModel={tableFilterDto}
+        setPaginationModel={setTableFilterDto}
+        loadData={loadData}
+        scrollOnLoad={false}
+        onRowSelect={onRowSelect}
+      />
+    );
+  };
+
+  const getBody = () => {
+    let newFilterDto = tableFilterDto;
+    newFilterDto.setData("totalCount", metrics?.codeCommitToEnvDeploy?.length);
+    setTableFilterDto({ ...newFilterDto });
+
+    return (
+      <FilterContainer
+        // isLoading={isLoading}
+        showBorder={false}
+        title={`Code Commit to E3 Deploy`}
+        titleIcon={faDraftingCompass}
+        body={getTable()}
+        className={"px-2 pb-2"}
+      />
+    );
+  };
+
+  const onRowSelect = () => {
+    toastContext.showOverlayPanel(
+      <FullScreenCenterOverlayContainer
+        closePanel={closePanel}
+        showPanel={true}
+        titleText={`Code Commit to E3 Deploy`}
+        showToasts={true}
+        titleIcon={faTable}
+        isLoading={false}
+        linkTooltipText={"View Full Blueprint"}
+      >
+        <div className={"p-3"}>{getBody()}</div>
+      </FullScreenCenterOverlayContainer>
+    );
+  };
+
+  const fields = [
+    { id: "jiraIssueKey", label: "Jira Issue Key" },
+    // { id: "gitlabBranch", label: "Gitlab Branch" },
+    { id: "unitTestingStageIssueUpdated", label: "Code Commit Time" },
+    { id: "productDeploymentStageIssueUpdated", label: "E3 Deploy Time" },
+    { id: "codeCommitToEnvDeploy", label: "Days Difference" },
+  ];
+
+  const columns = useMemo(
+    () => [
+      getTableTextColumn(getField(fields, "jiraIssueKey")),
+      // getTableTextColumn(getField(fields, "gitlabBranch")),
+      getTableDateTimeColumn(getField(fields, "unitTestingStageIssueUpdated")),
+      getTableDateTimeColumn(getField(fields, "productDeploymentStageIssueUpdated")),
+      getTableTextColumn(getField(fields, "codeCommitToEnvDeploy")),
+    ],
+    []
+  );
+
   return (
     <ThreeLineDataBlockBase
+      onClickFunction={(thisData) => onRowSelect(thisData)}
       className={"p-2"}
       topText={"Code Commit to E3 Deploy"}
       middleText={
