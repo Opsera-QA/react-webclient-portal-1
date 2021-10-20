67565b2b363d523c7e01946ca8be0d346e02c835
harsha-opsera <harsha@opsera.io>
2021-10-20 11:54:07 -0700

    Removing KPI call in chartViews file

--- src/components/insights/charts/ChartView.jsx
+++ src/components/insights/charts/ChartView.jsx
@@ -138,9 +138,6 @@ import ServiceNowMeanTimeBetweenFailuresBarChart from "./servicenow/bar_chart/me
 // Coverity KPIs
 import CoverityIssuesByCategory from "./coverity/CoverityIssuesByCategory/CoverityIssuesByCategory";
 
-// Jira-Github KPIs
-import ReqCreateToBranchCreation from "./github/req_create_to_branch_creation/ReqCreateToBranchCreation";
-
 import {
   getDateObjectFromKpiConfiguration,
   getTagsFromKpiConfiguration,
@@ -1094,19 +1091,6 @@ function ChartView({ kpiConfiguration, dashboardData, index, loadChart, setKpis 
         return (
           <Col xl={6} md={12} className="p-2">
             <GithubPendingMergeRequests
-              kpiConfiguration={kpiConfig}
-              setKpiConfiguration={setKpiConfig}
-              dashboardData={dashboardData}
-              setKpis={setKpis}
-              index={index}
-            />
-          </Col>
-        );
-      // Overall release duration KPIs
-      case "overall-release-duration":
-        return (
-          <Col xl={6} md={12} className="p-2">
-            <ReqCreateToBranchCreation
               kpiConfiguration={kpiConfig}
               setKpiConfiguration={setKpiConfig}
               dashboardData={dashboardData}
