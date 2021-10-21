cd46bdf1115e16774d608ddff9d3bd7b5c569466
Noah Champoux <noah@opsera.io>
2021-10-21 16:09:24 -0500

    Page Link Card Tweaks

--- src/components/common/card/link/PageLinkCard.jsx
+++ src/components/common/card/link/PageLinkCard.jsx
@@ -1,20 +1,27 @@
 import React  from "react";
 import PropTypes from "prop-types";
 import IconBase from "components/common/icons/IconBase";
+import {useHistory} from "react-router-dom";
 
 function PageLinkCard({link, linkText, icon, visible, className}) {
+  let history = useHistory();
+
+  const handlePageLink = () => {
+    history.push(link);
+  };
+
   if (!visible) {
     return null;
   }
 
   return (
     <div className={className}>
-      <div className={'my-3 page-link-card'}>
+      <div className={'my-3 page-link-card'} onClick={handlePageLink}>
         <div className={"d-flex"}>
-          <IconBase iconSize={"2x"} icon={icon} fixedWidth className="mr-3"/>
+          <IconBase iconSize={"2x"} icon={icon} fixedWidth className="ml-1 mr-3 my-auto"/>
           <div>
-            <div className={"mr-3"}>{linkText}</div>
-            <div className={"text-muted ml-2"}>This would be a description of the screen</div>
+            <div className={"mr-3 page-link-card-title-text"}>{linkText}</div>
+            <div className={"text-muted mt-1"}>This would be a description of the screen</div>
           </div>
         </div>
       </div>
