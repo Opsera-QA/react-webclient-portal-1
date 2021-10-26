4ea358c32a5b74c19e1fffe570ff3b102823f95f
Noah Champoux <noah@opsera.io>
2021-10-26 14:55:52 -0500

    Wired up Branch selection when running Bulk Migration tasks

--- src/components/tasks/details/tasks/sfdc-bulk-migration/salesforceBulkMigrationTaskConfigurationMetadata.js
+++ src/components/tasks/details/tasks/sfdc-bulk-migration/salesforceBulkMigrationTaskConfigurationMetadata.js
@@ -111,7 +111,7 @@ export const salesforceBulkMigrationTaskConfigurationMetadata = {
     {
       label: "Create a new branch?",
       id: "isNewBranch",
-      formText: "Creates a new branch and synch the changes.",
+      formText: "Creates a new branch and sync the changes.",
     },
     {
       label: "Upstream Branch",
