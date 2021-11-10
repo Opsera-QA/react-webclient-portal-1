08821eaa465054443de07fe2e8f63720974cfa17
Vignesh Subramanian <vignesh@opsera.io>
2021-11-10 18:47:00 +0530

    Bug fix

--- src/components/insights/marketplace/charts/goals/build_and_deploy_statistics/build-and-deploy-goals-metadata.js
+++ src/components/insights/marketplace/charts/goals/build_and_deploy_statistics/build-and-deploy-goals-metadata.js
@@ -3,19 +3,27 @@ const buildAndDeployGoalsMetadata = {
     fields: [
       {
         label: "Build Success Rate",
-        id: "build_success_rate"
+        id: "build_success_rate",
+        minNumber: 0,
+        maxNumber: 100
       },
       {
         label: "Average Builds per Day",
-        id: "average_builds"
+        id: "average_builds",
+        minNumber: 0,
+        maxNumber: 200
       },
       {
         label: "Deployment Success Rate",
-        id: "deployment_success_rate"
+        id: "deployment_success_rate",
+        minNumber: 0,
+        maxNumber: 100
       },
       {
         label: "Average Deployments per Day",
-        id: "average_deployments"
+        id: "average_deployments",
+        minNumber: 0,
+        maxNumber: 200
       },
     ],
     newObjectFields: {
