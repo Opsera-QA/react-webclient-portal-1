d3df659fc9b5e920bdfb5c8dd13cef592190b139
Murali Kurma <murali@opsera.io>
2021-11-12 14:49:46 -0500

    merge conflict resolved

--- src/components/insights/marketplace/charts/goals/GoalsInputBase.jsx
+++ src/components/insights/marketplace/charts/goals/GoalsInputBase.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import SalesforceDurationByStageGoals from "components/insights/marketplace/charts/goals/salesforce_duration_by_stage/SalesforceDurationByStageGoals";
 import SdlcDurationStatisticsGoals from "components/insights/marketplace/charts/goals/sdlc_duration_statistics/SdlcDurationStatisticsGoals";
+import BuildAndDeployGoals from "components/insights/marketplace/charts/goals/build_and_deploy_statistics/BuildAndDeployGoals";
 
 function GoalsInputBase({ dataObject, setDataObject, kpiName }) {
   switch (kpiName) {
