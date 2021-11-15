8ccb85ffbb3f8b39cc2d5961bfcb714082a82ee8
Noah Champoux <noah@opsera.io>
2021-11-15 16:17:20 -0600

    Minor Data Block tweaks

--- src/components/common/metrics/grade/MetricLetterGradeText.jsx
+++ src/components/common/metrics/grade/MetricLetterGradeText.jsx
@@ -13,17 +13,17 @@ function MetricLetterGradeText({ letterGrade }) {
   const getLetterGrade = () => {
     switch (letterGrade) {
       case LETTER_GRADES.A:
-        return <div className="green">A</div>;
+        return <div className="metric-block-content-text green">A</div>;
       case LETTER_GRADES.B:
-        return <div className="yellow">B</div>;
+        return <div className="metric-block-content-text yellow">B</div>;
       case LETTER_GRADES.C:
-        return <div className="yellow">C</div>;
+        return <div className="metric-block-content-text yellow">C</div>;
       case LETTER_GRADES.D:
-        return <div className="danger-red">D</div>;
+        return <div className="metric-block-content-textdanger-red">D</div>;
       case LETTER_GRADES.E:
-        return <div className="danger-red">E</div>;
+        return <div className="metric-block-content-text danger-red">E</div>;
       default:
-        return <div className="red">ERROR</div>;
+        return <div className="metric-block-content-text red">ERROR</div>;
     }
   };
 
--- src/components/common/metrics/score/MetricScoreText.jsx
+++ src/components/common/metrics/score/MetricScoreText.jsx
@@ -3,14 +3,11 @@ import PropTypes from "prop-types";
 import MetricTextBase from "components/common/metrics/text/MetricTextBase";
 
 function MetricScoreText({score, qualityLevel}) {
-  if (score == null) {
-    return null;
-  }
-
   return (
     <MetricTextBase
       formattedText={score}
       qualityLevel={qualityLevel}
+      className={"metric-block-content-text"}
     />
   );
 }
