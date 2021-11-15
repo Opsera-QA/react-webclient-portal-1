2dd2f5c918f8569a8bfdf9a7b4c11c43d7c48238
Tejas Bharadwaj <tejas@opsera.io>
2021-11-15 21:21:29 +0000

    OPL-1711: Modified Salesforce and SDLC Duration By Stage KPIs

--- src/components/insights/charts/sdlc/bar_chart/duration_by_stage/actionable_insights/SdlcDurationByStageOverviewDataBlockContainer.jsx
+++ src/components/insights/charts/sdlc/bar_chart/duration_by_stage/actionable_insights/SdlcDurationByStageOverviewDataBlockContainer.jsx
@@ -7,46 +7,56 @@ import { getTimeDisplay } from "components/insights/charts/sonar/sonar_ratings/d
 function SdlcDurationByStageOverviewDataBlockContainer({ data }) {
   let className = `p-2 dark-gray-text-primary`;
   return (
-    <Row className="px-2">
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock className={className} score={data.total_success} subtitle={"Successful Builds"} />
-        </DataBlockBoxContainer>
-      </Col>
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock className={className} score={data.total_failed} subtitle={"Failed Builds"} />
-        </DataBlockBoxContainer>
-      </Col>
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock className={className} score={data.mean_duration} subtitle={"Average Duration (hrs)"} />
-        </DataBlockBoxContainer>{" "}
-      </Col>
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock className={className} score={data.total_duration} subtitle={"Total Duration (hrs)"} />
-        </DataBlockBoxContainer>{" "}
-      </Col>
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock
-            className={className}
-            score={data.mean_time_to_resolve}
-            subtitle={"Average Time to Resolve (hrs)"}
-          />
-        </DataBlockBoxContainer>{" "}
-      </Col>
-      <Col xl={2} lg={2} sm={4} className={"my-3"}>
-        <DataBlockBoxContainer showBorder={true}>
-          <TwoLineScoreDataBlock
-            className={className}
-            score={data.total_time_to_resolve}
-            subtitle={"Total Time to Resolve (hrs)"}
-          />
-        </DataBlockBoxContainer>{" "}
-      </Col>
-    </Row>
+    <div>
+      <Row className="px-2">
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock className={className} score={data.total_success} subtitle={"Successful"} />
+          </DataBlockBoxContainer>
+        </Col>
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock
+              className={className}
+              score={data.total_duration}
+              subtitle={"Total Duration (hrs)"}
+            />
+          </DataBlockBoxContainer>{" "}
+        </Col>
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock
+              className={className}
+              score={data.total_time_to_resolve}
+              subtitle={"Total Time to Resolve (hrs)"}
+            />
+          </DataBlockBoxContainer>{" "}
+        </Col>
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock className={className} score={data.total_failed} subtitle={"Failed"} />
+          </DataBlockBoxContainer>
+        </Col>
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock
+              className={className}
+              score={data.mean_duration}
+              subtitle={"Average Duration (hrs)"}
+            />
+          </DataBlockBoxContainer>{" "}
+        </Col>
+        <Col lg={4} md={6} className={"my-3"}>
+          <DataBlockBoxContainer showBorder={true}>
+            <TwoLineScoreDataBlock
+              className={className}
+              score={data.mean_time_to_resolve}
+              subtitle={"Average Time to Resolve (hrs)"}
+            />
+          </DataBlockBoxContainer>{" "}
+        </Col>
+      </Row>
+    </div>
   );
 }
 
--- src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/validation/SalesforcePackageValidationDurationDataBlock.jsx
+++ src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/validation/SalesforcePackageValidationDurationDataBlock.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ThreeLineDataBlockNoFocusBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockNoFocusBase";
+import { getMiddleText, getMiddleStyle } from "../../salesforce-duration-by-stage-utility";
 
 function SalesforcePackageValidationDurationDataBlock({ meanData, countData }) {
-  const getMiddleText = (meanData, countData) => {
-    if (meanData && countData) {
-      return meanData + " min | " + countData + " runs";
-    }
-    if (meanData) {
-      return meanData + " min | 0";
-    }
-    return "No runs";
-  };
-
   return (
     <ThreeLineDataBlockNoFocusBase
       className="salesforce-duration-by-stage-kpi"
