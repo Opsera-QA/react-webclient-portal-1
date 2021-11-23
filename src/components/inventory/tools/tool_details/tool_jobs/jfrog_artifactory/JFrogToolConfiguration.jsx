333fd91de43f6dc534544cb826c5e669df55c511
Noah Champoux <noah@opsera.io>
2021-11-23 07:51:33 -0600

    Fixed issue with incorrect prop passed.

--- src/components/inventory/tools/tool_details/tool_jobs/jfrog_artifactory/JFrogToolConfiguration.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/jfrog_artifactory/JFrogToolConfiguration.jsx
@@ -13,36 +13,48 @@ import modelHelpers from "components/common/model/modelHelpers";
 
 function JFrogToolConfiguration({ toolData }) {
   const { getAccessToken } = useContext(AuthContext);
-  const [JFrogConfigurationDto, setJFrogConfigurationDto] = useState(undefined);
+  const [jFrogToolConfigurationModel, setJFrogToolConfigurationModel] = useState(undefined);
 
   useEffect(() => {
     loadData();
   }, []);
 
   const loadData = async () => {
-    setJFrogConfigurationDto(modelHelpers.getToolConfigurationModel(toolData.getData("configuration"), jfrogConnectionMetadata));
+    setJFrogToolConfigurationModel(modelHelpers.getToolConfigurationModel(toolData.getData("configuration"), jfrogConnectionMetadata));
   };
 
   const saveJFrogToolConfiguration = async () => {
-    let newConfiguration = JFrogConfigurationDto.getPersistData();
-    newConfiguration.accountPassword = await toolsActions.savePasswordToVault(toolData, JFrogConfigurationDto,"accountPassword", newConfiguration.accountPassword, getAccessToken);
+    let newConfiguration = jFrogToolConfigurationModel.getPersistData();
+    newConfiguration.accountPassword = await toolsActions.savePasswordToVault(toolData, jFrogToolConfigurationModel,"accountPassword", newConfiguration.accountPassword, getAccessToken);
     const item = { configuration: newConfiguration };
     return await toolsActions.saveToolConfiguration(toolData, item, getAccessToken);
   };
 
   return (
     <ToolConfigurationEditorPanelContainer
-      model={JFrogConfigurationDto}
-      setModel={JFrogConfigurationDto}
+      model={jFrogToolConfigurationModel}
+      setModel={setJFrogToolConfigurationModel}
       persistRecord={saveJFrogToolConfiguration}
       toolData={toolData}
       toolConnectionCheckName={"jfrog"}
     >
       <Row>
         <Col sm={12}>
-          <TextInputBase dataObject={JFrogConfigurationDto} setDataObject={setJFrogConfigurationDto} fieldName={"toolURL"} />
-          <TextInputBase dataObject={JFrogConfigurationDto} setDataObject={setJFrogConfigurationDto} fieldName={"accountUsername"} />
-          <VaultTextInput dataObject={JFrogConfigurationDto} setDataObject={setJFrogConfigurationDto} fieldName={"accountPassword"} />
+          <TextInputBase
+            dataObject={jFrogToolConfigurationModel}
+            setDataObject={setJFrogToolConfigurationModel}
+            fieldName={"toolURL"}
+          />
+          <TextInputBase
+            dataObject={jFrogToolConfigurationModel}
+            setDataObject={setJFrogToolConfigurationModel}
+            fieldName={"accountUsername"}
+          />
+          <VaultTextInput
+            dataObject={jFrogToolConfigurationModel}
+            setDataObject={setJFrogToolConfigurationModel}
+            fieldName={"accountPassword"}
+          />
         </Col>
       </Row>
     </ToolConfigurationEditorPanelContainer>
