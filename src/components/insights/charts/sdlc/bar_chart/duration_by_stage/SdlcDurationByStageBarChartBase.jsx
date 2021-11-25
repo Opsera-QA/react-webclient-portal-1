46c99451a7383713053058c601bcee26c0af27f5
tbharadwaj <tejas@opsera.io>
2021-11-24 16:32:05 -0800

    Added actionable insights for SDLC Duration By Stage

--- src/components/insights/charts/sdlc/bar_chart/duration_by_stage/SdlcDurationByStageBarChartBase.jsx
+++ src/components/insights/charts/sdlc/bar_chart/duration_by_stage/SdlcDurationByStageBarChartBase.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { ResponsiveLine } from "@nivo/line";
 import config from "./sdlcDurationByStageBarChartConfigs";
 import { defaultConfig } from "components/insights/charts/charts-views";
 import ChartTooltip from "components/insights/charts/ChartTooltip";
-
-function SdlcDurationByStageBarChartBase({ metric }) {
+import { DialogToastContext } from "contexts/DialogToastContext";
+import SdlcDurationByStageActionableInsightOverlay from "./actionable_insights/SdlcDurationByStageActionableInsightOverlay";
+function SdlcDurationByStageBarChartBase({ metric, kpiConfiguration, dashboardData }) {
+  const toastContext = useContext(DialogToastContext);
+  const onNodeSelect = (node) => {
+    let title = node.serieId;
+    toastContext.showOverlayPanel(
+      <SdlcDurationByStageActionableInsightOverlay
+        title={title}
+        actionableInsightsQueryData={node}
+        kpiConfiguration={kpiConfiguration}
+        dashboardData={dashboardData}
+      />
+    );
+  };
   return (
     <ResponsiveLine
       data={metric}
@@ -19,12 +32,15 @@ function SdlcDurationByStageBarChartBase({ metric }) {
           values={[node.point.data.range, String(node.point.data.yFormatted) + " minutes", node.point.data.count]}
         />
       )}
+      onClick={(node) => onNodeSelect(node)}
     />
   );
 }
 
 SdlcDurationByStageBarChartBase.propTypes = {
   metric: PropTypes.array,
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
 };
 
 export default SdlcDurationByStageBarChartBase;
