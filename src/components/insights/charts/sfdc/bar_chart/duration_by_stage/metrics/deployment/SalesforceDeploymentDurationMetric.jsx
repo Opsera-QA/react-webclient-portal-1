1296f198e20051132d0eb574c85fed875cd27711
tbharadwaj <tejas@opsera.io>
2021-11-24 16:43:18 -0800

    Added actionable insights for Salesforce Duration By Stage

--- src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/deployment/SalesforceDeploymentDurationMetric.jsx
+++ src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/deployment/SalesforceDeploymentDurationMetric.jsx
@@ -3,19 +3,34 @@ import PropTypes from "prop-types";
 import DataBlockAndChartContainer from "components/common/metrics/container/DataBlockAndChartContainer";
 import SalesforceDurationByStageBarChartBase from "components/insights/charts/sfdc/bar_chart/duration_by_stage/SalesforceDurationByStageBarChartBase";
 import SalesforceDeploymentDurationDataBlock from "components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/deployment/SalesforceDeploymentDurationDataBlock";
-function SalesforceDeploymentDurationMetric({ meanData, countData, goalsData, metric }) {
+function SalesforceDeploymentDurationMetric({
+  kpiConfiguration,
+  dashboardData,
+  meanData,
+  countData,
+  goalsData,
+  metric,
+}) {
   const getDataBlock = () => {
     return <SalesforceDeploymentDurationDataBlock meanData={meanData} countData={countData} goalsData={goalsData} />;
   };
 
   const getChart = () => {
-    return <SalesforceDurationByStageBarChartBase metric={metric} />;
+    return (
+      <SalesforceDurationByStageBarChartBase
+        kpiConfiguration={kpiConfiguration}
+        dashboardData={dashboardData}
+        metric={metric}
+      />
+    );
   };
 
   return <DataBlockAndChartContainer dataBlock={getDataBlock()} chart={getChart()} />;
 }
 
 SalesforceDeploymentDurationMetric.propTypes = {
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
   meanData: PropTypes.number,
   countData: PropTypes.number,
   goalsData: PropTypes.number,
--- src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/unit_testing/SalesforceUnitTestingDurationMetric.jsx
+++ src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/unit_testing/SalesforceUnitTestingDurationMetric.jsx
@@ -4,19 +4,34 @@ import DataBlockAndChartContainer from "components/common/metrics/container/Data
 import SalesforceDurationByStageBarChartBase from "components/insights/charts/sfdc/bar_chart/duration_by_stage/SalesforceDurationByStageBarChartBase";
 import SalesforceUnitTestingDurationDataBlock from "components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/unit_testing/SalesforceUnitTestingDurationDataBlock";
 
-function SalesforceProfileMigrationDurationMetric({ meanData, countData, goalsData, metric }) {
+function SalesforceProfileMigrationDurationMetric({
+  kpiConfiguration,
+  dashboardData,
+  meanData,
+  countData,
+  goalsData,
+  metric,
+}) {
   const getDataBlock = () => {
     return <SalesforceUnitTestingDurationDataBlock meanData={meanData} countData={countData} goalsData={goalsData} />;
   };
 
   const getChart = () => {
-    return <SalesforceDurationByStageBarChartBase metric={metric} />;
+    return (
+      <SalesforceDurationByStageBarChartBase
+        kpiConfiguration={kpiConfiguration}
+        dashboardData={dashboardData}
+        metric={metric}
+      />
+    );
   };
 
   return <DataBlockAndChartContainer dataBlock={getDataBlock()} chart={getChart()} />;
 }
 
 SalesforceProfileMigrationDurationMetric.propTypes = {
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
   meanData: PropTypes.number,
   countData: PropTypes.number,
   goalsData: PropTypes.object,
