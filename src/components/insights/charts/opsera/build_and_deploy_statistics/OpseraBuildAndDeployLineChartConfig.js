e7ab7805f3dff5147c2678cc5c29fcb94097d8d4
Vignesh Subramanian <vignesh@opsera.io>
2021-11-29 20:33:43 +0530

    build & deploy KPI enhancements

---
+++ src/components/insights/charts/opsera/build_and_deploy_statistics/OpseraBuildAndDeployLineChartConfig.js
@@ -0,0 +1,27 @@
+export default () => ({
+  enableGridX: false,
+  enableGridY: false,
+  yScale: {
+    type: 'linear',
+    min: '0',
+    max: '100',
+    stacked: false,
+    reverse: false
+  },
+  xFormat: "time:%Y-%m-%d",
+  colors: "#494173",
+  axisLeft: {
+    tickValues: [0, 50, 100],
+    legend: 'Success Rate %',
+    legendOffset: -40,
+    legendPosition: 'middle'
+  },
+  axisBottom: {
+    format: d => { var date = new Date(d).toDateString(); date = date.split(" "); return date[1] + " " + date[2]; },
+    tickRotation: -45,
+    legend: 'Date',
+    legendOffset: 55,
+    legendPosition: 'middle'
+  },
+  pointSize: 6
+});
--- src/components/insights/charts/opsera/build_and_deploy_statistics/deployment_frequency_statistics/DeploymentFrequencyStatisticsDataBlockContainer.jsx
+++ src/components/insights/charts/opsera/build_and_deploy_statistics/deployment_frequency_statistics/DeploymentFrequencyStatisticsDataBlockContainer.jsx
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import HorizontalDataBlocksContainer from "components/common/metrics/data_blocks/horizontal/HorizontalDataBlocksContainer";
 import {METRIC_QUALITY_LEVELS} from "components/common/metrics/text/MetricTextBase";
 import Col from "react-bootstrap/Col";
-import AverageDailyDeploymentsDataBlock
-  from "components/common/metrics/data_blocks/deployment/average_daily/AverageDailyDeploymentsDataBlock";
 import { ResponsiveLine } from '@nivo/line';
 import { defaultConfig, getColor, assignStandardColors } from 'components/insights/charts/charts-views';
 import _ from "lodash";
+import ChartTooltip from "components/insights/charts/ChartTooltip";
+import config from "../OpseraBuildAndDeployLineChartConfig";
+import MetricScoreText from "components/common/metrics/score/MetricScoreText";
+import ThreeLineDataBlockNoFocusBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockNoFocusBase";
 
 // TODO: Pass in relevant data and don't use hardcoded data
 function DeploymentFrequencyStatisticsDataBlockContainer({ metricData, chartData, goalsData }) {
@@ -24,17 +26,15 @@ function DeploymentFrequencyStatisticsDataBlockContainer({ metricData, chartData
   let dailyDeploymentsChartData = [
     {
       "id": "average daily deployments",
-      "color": "#ABA4CC",
       "data": chartData?.avgDeployments
     }  
   ];
 
   const getLeftDataBlock = () => {
     return (
-      <AverageDailyDeploymentsDataBlock
-        className={'build-deploy-kpi'}
-        qualityLevel={metricData?.deploy?.perDayAverage < goalsData ? METRIC_QUALITY_LEVELS.DANGER : METRIC_QUALITY_LEVELS.SUCCESS}
-        averageDailyCount={metricData?.deploy?.perDayAverage || 0}
+      <ThreeLineDataBlockNoFocusBase        
+        topText={"Average Daily Deployments"}
+        middleText={<MetricScoreText score={metricData?.deploy?.perDayAverage || 0} qualityLevel={metricData?.deploy?.perDayAverage < goalsData ? METRIC_QUALITY_LEVELS.DANGER : METRIC_QUALITY_LEVELS.SUCCESS} />}
         bottomText={`Goal: ${goalsData}`}
       />
     );
@@ -45,19 +45,22 @@ function DeploymentFrequencyStatisticsDataBlockContainer({ metricData, chartData
       <div className="new-chart p-0" style={{height: "150px"}}>
         <ResponsiveLine
           data={dailyDeploymentsChartData}
-          {...defaultConfig("", "Month", 
-                false, false, "numbers", "month")}
-          yScale={{ type: 'linear', min: '0', max: maxVal, stacked: false, reverse: false }}          
-          enableGridX={false}
-          enableGridY={false}
+          {...defaultConfig("", "Date", 
+                false, false, "numbers", "monthDate2")}
+          {...config()}
+          yScale={{ type: 'linear', min: '0', max: maxVal, stacked: false, reverse: false }}
           axisLeft={{            
             tickValues: [0, maxVal],
             legend: 'Avg Daily Deployments',
             legendOffset: -38,
             legendPosition: 'middle'
-          }}         
-          colors={getColor}
-          pointSize={6}
+          }}
+          tooltip={(node) => (            
+            <ChartTooltip
+              titles={["Date Range", "Number of Builds", "Avg Daily Builds"]}
+              values={[node.point.data.range, node.point.data.total, node.point.data.y]}
+            />
+          )}          
           markers={[
             {
                 axis: 'y',
@@ -76,10 +79,10 @@ function DeploymentFrequencyStatisticsDataBlockContainer({ metricData, chartData
       title={"Deployment Frequency Statistics"}
       // onClick={() => onRowSelect()}
     >      
-        <Col sm={4} className={"p-2"}>
+        <Col sm={3} className={"p-2"}>
           {getLeftDataBlock()}          
         </Col>        
-        <Col sm={8} className={"p-2"}>
+        <Col sm={9} className={"p-2"}>
           {getTrendChart()}
         </Col>              
     </HorizontalDataBlocksContainer>
