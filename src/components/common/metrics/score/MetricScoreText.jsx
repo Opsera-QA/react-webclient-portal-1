3872511b9b57acdd3cdcb82ffc0b4b763305a062
Noah Champoux <noah@opsera.io>
2021-12-01 10:13:38 -0600

    Added Data Point Evaluation to MetricScoreText

--- src/components/common/metrics/score/MetricScoreText.jsx
+++ src/components/common/metrics/score/MetricScoreText.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MetricTextBase from "components/common/metrics/text/MetricTextBase";
+import {strategicCriteriaHelpers} from "components/common/helpers/strategicCriteria.helpers";
 
+// TODO: Rename MetricNumberText
 function MetricScoreText(
   {
     score,
     qualityLevel, // TODO: Remove after strategic criteria is wired up everywhere
     dataPoint,
   }) {
+  const getQualityLevel = () => {
+    let evaluation = qualityLevel;
+
+    // TODO: allow existing data points to use their quality level but eventually require the use of strategic criteria
+    if (dataPoint) {
+      const evaluatedDataPoint = strategicCriteriaHelpers.evaluateDataPointQualityLevel(dataPoint, score);
+
+      if (typeof evaluatedDataPoint === "string") {
+        evaluation = evaluatedDataPoint;
+      }
+    }
+
+    return evaluation;
+  };
+
   return (
     <MetricTextBase
       formattedText={score}
-      qualityLevel={qualityLevel}
+      qualityLevel={getQualityLevel()}
       dataPoint={dataPoint}
       className={"metric-block-content-text"}
     />
