43e43031b86c67b82fae6c87463093f3222a0a32
Noah Champoux <noah@opsera.io>
2021-12-10 09:24:15 -0600

    Fixed imports v2

--- src/components/inventory/tools/tool_details/tool_jobs/azureV2/storage_accounts/AzureToolStorageAccountTable.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/azureV2/storage_accounts/AzureToolStorageAccountTable.jsx
@@ -1,6 +1,5 @@
 import React, {useContext, useMemo } from "react";
 import PropTypes from "prop-types";
-import azureStorageAccountMetadata from "components/inventory/tools/tool_details/tool_jobs/azureV2/storage_accounts/azureStorageAccount.metadata";
 import {getField} from "components/common/metadata/metadata-helpers";
 import FilterContainer from "components/common/table/FilterContainer";
 import {faBrowser} from "@fortawesome/pro-light-svg-icons";
@@ -9,6 +8,7 @@ import CreateAzureStorageOverlay from "components/inventory/tools/tool_details/t
 import {getTableTextColumn} from "components/common/table/table-column-helpers";
 import modelHelpers from "components/common/model/modelHelpers";
 import CustomTable from "components/common/table/CustomTable";
+import {azureStorageAccountMetadata} from "components/inventory/tools/tool_details/tool_jobs/azureV2/storage_accounts/azureStorageAccount.metadata";
 
 function AzureStorageTable(
   { 
