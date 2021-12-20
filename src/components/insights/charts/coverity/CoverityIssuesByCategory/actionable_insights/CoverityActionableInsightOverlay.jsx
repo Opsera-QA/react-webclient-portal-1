a8a8793c17459163ee706bfbe5f445dbef1b2a66
Divyesha Malhotra <divyeshamalhotra@Divyeshas-MacBook-Pro.local>
2021-12-20 14:29:35 -0800

    changes

---
+++ src/components/insights/charts/coverity/CoverityIssuesByCategory/actionable_insights/CoverityActionableInsightOverlay.jsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import { Col, Row } from "react-bootstrap";
+import Model from "core/data_model/model";
+import { AuthContext } from "contexts/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLink, faTable } from "@fortawesome/pro-light-svg-icons";
+import axios from "axios";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import chartsActions from "components/insights/charts/charts-actions";
+import { useHistory } from "react-router-dom";
+import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
+import CoverityActionableInsightTable from "./CoverityActionableInsightTable";
+import CoverityActionableDataBlockContainers from "./CoverityActionableDataBlockContainers";
+import { getTimeDisplay } from "components/insights/charts/sonar/sonar_ratings/data_blocks/sonar-ratings-pipeline-utility";
+import genericChartFilterMetadata from "components/insights/charts/generic_filters/genericChartFilterMetadata";
+function CoverityActionableInsightOverlay({
+                                            title,
+                                            coveritySeverity,
+                                            kpiConfiguration,
+                                            dashboardData,
+                                          }) {
+  const toastContext = useContext(DialogToastContext);
+  const history = useHistory();
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metrics, setMetrics] = useState([]);
+  const [dataBlockValues, setDataBlockValues] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [filterModel, setFilterModel] = useState(
+    new Model({ ...genericChartFilterMetadata.newObjectFields }, genericChartFilterMetadata, false)
+  );
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [JSON.stringify(dashboardData)]);
+  const loadData = async (cancelSource = cancelTokenSource, filterDto = filterModel) => {
+    try {
+      setIsLoading(true);
+      let dashboardTags =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+
+      let request = "coverityInsightsDatablocks";
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        request,
+        kpiConfiguration,
+        dashboardTags,
+        filterDto,
+        null,
+        null,
+        null,
+        null,
+        null,
+        null,
+        coveritySeverity
+      );
+      let dataObject = response?.data ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.data : [];
+      let dataCount = response?.data
+        ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.count[0]?.count
+        : [];
+      let DataBlocks = response?.data ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.DataBlocks[0] : [];
+      dataObject = dataObject.map((bd, index) => ({
+        ...bd,
+        _blueprint: <FontAwesomeIcon icon={faExternalLink} fixedWidth className="mr-2" />,
+      }));
+
+
+      let newFilterDto = filterDto;
+      newFilterDto.setData("totalCount", dataCount);
+      setFilterModel({ ...newFilterDto });
+      if (isMounted?.current === true && dataObject) {
+        setMetrics(dataObject);
+        setDataBlockValues(DataBlocks);
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const closePanel = () => {
+    toastContext.removeInlineMessage();
+    toastContext.clearOverlayPanel();
+  };
+
+  return (
+    <FullScreenCenterOverlayContainer
+      closePanel={closePanel}
+      showPanel={true}
+      titleText={title}
+      showToasts={true}
+      titleIcon={faTable}
+      isLoading={isLoading}
+      linkTooltipText={"View Full Blueprint"}
+    >
+      <div className={"p-3"}>
+        <CoverityActionableDataBlockContainers data={dataBlockValues} />
+        <CoverityActionableInsightTable
+          data={metrics}
+          isLoading={isLoading}
+          loadData={loadData}
+          filterModel={filterModel}
+          setFilterModel={setFilterModel}
+          title={title}
+        />
+        {/* {getFooterDetails()} */}
+      </div>
+    </FullScreenCenterOverlayContainer>
+  );
+}
+
+CoverityActionableInsightOverlay.propTypes = {
+  title: PropTypes.string,
+  coveritySeverity: PropTypes.string,
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
+};
+
+export default CoverityActionableInsightOverlay;
---
+++ src/components/insights/charts/coverity/CoverityIssuesByCategory/actionable_insights/CoverityActionableInsightTable.jsx
@@ -0,0 +1,87 @@
+import React, { useContext, useMemo } from "react";
+import PropTypes from "prop-types";
+import FilterContainer from "components/common/table/FilterContainer";
+import CoverityActionableMetadata from "./coverity-actionable-insight-metadata";
+import {
+  getChartTrendStatusColumn, getTableDateTimeColumn,
+  getTableTextColumn,
+  getTableTextColumnWithoutField,
+} from "components/common/table/table-column-helpers";
+import { getField } from "components/common/metadata/metadata-helpers";
+import CustomTable from "components/common/table/CustomTable";
+import { faDraftingCompass } from "@fortawesome/pro-light-svg-icons";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import BlueprintLogOverlay from "components/blueprint/BlueprintLogOverlay";
+import { getFormattedTimestamp } from "../../../../../common/fields/date/DateFieldBase";
+
+// TODO: Convert to cards
+function CoverityActionableInsightTable({ data, isLoading, loadData, filterModel, setFilterModel, title }) {
+  const toastContext = useContext(DialogToastContext);
+  const fields = CoverityActionableMetadata.fields;
+  const tableTitle = "Coverity " + title + " Report";
+  const noDataMessage = "Coverity " + title + " report is currently unavailable at this time";
+
+  const columns = useMemo(
+    () => [
+      getTableTextColumn(getField(fields, "project"), "project"),
+      getTableTextColumn(getField(fields, "pipelineName"), "pipelineName"),
+      getTableTextColumn(getField(fields, "run"), "run"),
+      getTableDateTimeColumn(getField(fields, "timestamp"), "timestamp"),
+      getChartTrendStatusColumn(getField(fields, "trend"), "trend"),
+      getTableTextColumn(getField(fields, "total_issues"), "total_issues"),
+      getTableTextColumn(getField(fields, "quality_issues"), "quality_issues"),
+      getTableTextColumn(getField(fields, "security_issues"), "security_issues"),
+      getTableTextColumnWithoutField("Actions", "_blueprint", "text-center"),
+    ],
+    []
+  );
+
+  const onRowSelect = (rowData) => {
+    toastContext.showOverlayPanel(
+      <BlueprintLogOverlay
+        pipelineId={rowData?.original?.pipeline}
+        runCount={rowData?.original?.run}
+      />
+    );
+  };
+
+  const getTable = () => {
+    return (
+      <CustomTable
+        isLoading={isLoading}
+        loadData={loadData}
+        columns={columns}
+        data={data}
+        noDataMessage={noDataMessage}
+        paginationDto={filterModel}
+        setPaginationDto={setFilterModel}
+        onRowSelect={onRowSelect}
+      />
+    );
+  };
+
+  return (
+    <FilterContainer
+      isLoading={isLoading}
+      showBorder={false}
+      title={tableTitle}
+      titleIcon={faDraftingCompass}
+      body={getTable()}
+      className={"px-2 pb-2"}
+      loadData={loadData}
+      setFilterDto={setFilterModel}
+      filterDto={filterModel}
+    />
+  );
+}
+
+CoverityActionableInsightTable.propTypes = {
+  data: PropTypes.array,
+  isLoading: PropTypes.bool,
+  loadData: PropTypes.func,
+  filterModel: PropTypes.object,
+  setFilterModel: PropTypes.func,
+  title: PropTypes.string,
+};
+
+export default CoverityActionableInsightTable;
