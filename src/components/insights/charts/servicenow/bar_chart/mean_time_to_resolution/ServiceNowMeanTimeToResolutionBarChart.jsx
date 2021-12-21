ad53bf7840da03d217803b92f318b604cf570c2b
harsha-opsera <harsha@opsera.io>
2021-12-21 15:28:37 -0800

    Font changes

--- src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/ServiceNowMeanTimeToResolutionBarChart.jsx
+++ src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/ServiceNowMeanTimeToResolutionBarChart.jsx
@@ -108,7 +108,7 @@ function ServiceNowMeanTimeToResolutionBarChart({
     }
 
     return (
-      <div className="new-chart mb-3 pointer" style={{ height: "300px" }}>
+      <div className="new-chart mb-3 pointer font-inter-light-300 dark-gray-text-primary" style={{ height: "300px" }}>
         <div style={{ float: "right", fontSize: "10px" }}>
           Total Number of Incidents - #<br></br>
           <FontAwesomeIcon icon={faMinus} color={neutralColor} size="lg" /> Average MTTR <b>({overallMean} Minutes)</b>
