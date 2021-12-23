e7789ca152ba9a04dd594d3700e9109840638c9c
Murali Kurma <murali@opsera.io>
2021-12-24 01:06:51 +0530

    FirstPass and Defect Removal kpi code merge for master

---
+++ src/components/insights/charts/first_pass/data_blocks/FirstPassYieldMetricDataBlockBase.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
+import TwoLineDataBlockBase from "components/common/metrics/data_blocks/base/TwoLineDataBlockBase";
+
+function FirstPassYieldMetricDataBlockBase({ score, subtitle, onClickFunction }) {
+  return (
+    <DataBlockBoxContainer showBorder={true} onClickFunction={onClickFunction}>
+      <div className={"p-2"} style={{minHeight: '100px'}}>
+        <TwoLineDataBlockBase className={"first-pass-yield"} title={score} subtitle={subtitle} />
+      </div>
+    </DataBlockBoxContainer>
+  );
+}
+FirstPassYieldMetricDataBlockBase.propTypes = {
+  score: PropTypes.string,
+  subtitle: PropTypes.string,
+  onClickFunction: PropTypes.func,
+};
+
+export default FirstPassYieldMetricDataBlockBase;
