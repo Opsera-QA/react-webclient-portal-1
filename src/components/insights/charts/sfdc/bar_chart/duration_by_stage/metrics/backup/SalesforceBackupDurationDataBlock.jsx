6528d9140fa9afda172a4df60f52ce1a142fb11d
Tejas Bharadwaj <tejas@opsera.io>
2022-01-03 13:25:58 +0000

    OPl-2160 [TEST]: Renamed variable names to be more meaningful, implemented new color palette, switched font to MetricTextBase for Salesforce and SDLC Duration By Stage

--- src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/backup/SalesforceBackupDurationDataBlock.jsx
+++ src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/backup/SalesforceBackupDurationDataBlock.jsx
@@ -2,20 +2,45 @@ import React from "react";
 import PropTypes from "prop-types";
 import ThreeLineDataBlockNoFocusBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockNoFocusBase";
 import "../../salesforce-duration-by-stage-kpi.css";
-import { getMiddleText, getMiddleStyle } from "../../salesforce-duration-by-stage-utility";
-function SalesforceBackupDurationDataBlock({ meanData, countData }) {
+import MetricTextBase from "components/common/metrics/text/MetricTextBase";
+function SalesforceBackupDurationDataBlock({ backupDurationMeanInMinutes, backupTotalRunCount }) {
+  const hasNumberValue = (potentialNumber) => {
+    return potentialNumber == undefined || potentialNumber == null || typeof potentialNumber !== "number"
+      ? false
+      : true;
+  };
+
+  const getBackupMeanData = () => {
+    if (hasNumberValue(backupDurationMeanInMinutes) && hasNumberValue(backupTotalRunCount)) {
+      return (
+        <>
+          <div>
+            <MetricTextBase formattedText={`${backupDurationMeanInMinutes} min`} />
+          </div>
+          <div>
+            <MetricTextBase formattedText={`${backupTotalRunCount} runs`} />
+          </div>
+        </>
+      );
+    }
+    if (hasNumberValue(backupDurationMeanInMinutes)) {
+      return <MetricTextBase formattedText={`${backupDurationMeanInMinutes} min`} />;
+    }
+    return "No runs";
+  };
+
   return (
     <ThreeLineDataBlockNoFocusBase
       className="salesforce-duration-by-stage-kpi"
       topText={"Backups"}
-      middleText={getMiddleText(meanData, countData)}
+      middleText={getBackupMeanData()}
     />
   );
 }
 
 SalesforceBackupDurationDataBlock.propTypes = {
-  meanData: PropTypes.number,
-  countData: PropTypes.number,
+  backupDurationMeanInMinutes: PropTypes.number,
+  backupTotalRunCount: PropTypes.number,
 };
 
 export default SalesforceBackupDurationDataBlock;
--- src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/create_package/SalesforceCreatePackageDurationDataBlock.jsx
+++ src/components/insights/charts/sfdc/bar_chart/duration_by_stage/metrics/create_package/SalesforceCreatePackageDurationDataBlock.jsx
@@ -1,24 +1,65 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ThreeLineDataBlockNoFocusBase from "components/common/metrics/data_blocks/base/ThreeLineDataBlockNoFocusBase";
-import { statusColors } from "components/insights/charts/charts-views";
+import MetricTextBase, { METRIC_QUALITY_LEVELS } from "components/common/metrics/text/MetricTextBase";
 import "../../salesforce-duration-by-stage-kpi.css";
-import { getMiddleText, getMiddleStyle } from "../../salesforce-duration-by-stage-utility";
+function SalesforceCreatePackageDurationDataBlock({
+  createPackageDurationMeanInMinutes,
+  createPackageTotalRunCount,
+  goalsData,
+}) {
+  const hasNumberValue = (potentialNumber) => {
+    return potentialNumber == undefined || potentialNumber == null || typeof potentialNumber !== "number"
+      ? false
+      : true;
+  };
 
-function SalesforceCreatePackageDurationDataBlock({ meanData, countData, goalsData }) {
+  const getMetricQualityLevel = () => {
+    if (!hasNumberValue(createPackageDurationMeanInMinutes) || !hasNumberValue(goalsData)) {
+      return;
+    }
+    if (goalsData > createPackageDurationMeanInMinutes) {
+      return METRIC_QUALITY_LEVELS.GREEN;
+    } else if (goalsData < createPackageDurationMeanInMinutes) {
+      return METRIC_QUALITY_LEVELS.DANGER;
+    } else if (goalsData == createPackageDurationMeanInMinutes) {
+      return METRIC_QUALITY_LEVELS.WARNING;
+    }
+  };
+
+  const getCreatePackageMeanData = () => {
+    if (hasNumberValue(createPackageDurationMeanInMinutes) && hasNumberValue(createPackageTotalRunCount)) {
+      const qualityLevel = getMetricQualityLevel();
+      return (
+        <>
+          <div>
+            <MetricTextBase formattedText={`${createPackageDurationMeanInMinutes} min`} qualityLevel={qualityLevel} />
+          </div>
+          <div>
+            <MetricTextBase formattedText={`${createPackageTotalRunCount} runs`} qualityLevel={qualityLevel} />
+          </div>
+        </>
+      );
+    }
+    if (hasNumberValue(createPackageDurationMeanInMinutes)) {
+      return <MetricTextBase formattedText={`${createPackageDurationMeanInMinutes} min`} />;
+    }
+    return "No runs";
+  };
+
   return (
     <ThreeLineDataBlockNoFocusBase
       className="salesforce-duration-by-stage-kpi"
       topText={"Package Creation"}
-      middleText={getMiddleText(meanData, countData, goalsData)}
-      bottomText={goalsData ? "Goal: " + goalsData + " min" : "No Goal"}
+      middleText={getCreatePackageMeanData()}
+      bottomText={hasNumberValue(goalsData) ? `Goal: ${goalsData} min` : "No Goal"}
     />
   );
 }
 
 SalesforceCreatePackageDurationDataBlock.propTypes = {
-  meanData: PropTypes.number,
-  countData: PropTypes.number,
+  createPackageDurationMeanInMinutes: PropTypes.number,
+  createPackageTotalRunCount: PropTypes.number,
   goalsData: PropTypes.number,
 };
 
