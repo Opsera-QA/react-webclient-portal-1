0aba7eb75ec883d2470cd5005e6d62583b22bc71
Christina Bowers <christina@opsera.io>
2022-01-05 16:29:17 -0500

    OPL2157 sdlc help update

--- src/components/common/help/documentation/insights/charts/SdlcDurationStatisticsHelpDocumentation.jsx
+++ src/components/common/help/documentation/insights/charts/SdlcDurationStatisticsHelpDocumentation.jsx
@@ -12,14 +12,14 @@ function SdlcDurationStatisticsHelpDocumentation({closeHelpPanel}) {
         <div className={"mb-2"}>This KPI represents different stages of an SDLC pipeline. It measures the average duration of each stage, along with the trend for the selected time period. Each stage has 1 data block which represents the average time taken to execute that stage in a pipeline, along with the total number of step executions. The chart associated with it shows the trend over the selected time period. </div>
         <div>
           <ul style={{listStyleType: "none"}}>
-            <li><b>Build</b> - Includes pipelines with a build stage<b>*</b>.</li>
-            <li><b>Deployments</b> - Includes pipelines with a deployment step<b>*</b>.</li>
-            <li><b>Security Scan</b> - Includes pipelines with Anchore or Twistlock steps.</li>
-            <li><b>Quality Scan</b> - Includes pipelines with Coverity or Sonar steps.</li>
-            <li><b>Testing</b> - Includes pipelines with test steps involved (Selenium, Jmeter, etc).</li>
-            <li><b>Scripts</b> - Includes pipelines with shell script steps.</li>
+            <li><b>Build</b> - Includes pipelines with a build<b>*</b> step using Jenkins tool.</li>
+            <li><b>Quality Scan</b> - Includes pipelines with quality step including Coverity or Sonar tool.</li>
+            <li><b>Test</b> - Includes pipelines with test steps using JUnit, NUnit, JMeter or Selenium tool.</li>
+            <li><b>Deploy</b> - Includes pipelines with a deployment<b>*</b> step using Argo, Octopus, Azure AKS Deploy, Azure Functions, AWS Lambda, AWS ECS Deploy, or SSH Deploy tool.</li>
+            <li><b>Security Scan</b> - Includes pipelines with a security step using Anchore, Twistlock, Sonar, Coverity or Terrascan tool.</li>
+            <li><b>Command Line</b> - Includes pipelines with shell script steps using PowerShell, Command Line, NPM, or .NET tool.</li>
           </ul>
-          <div className={"ml-2"}><b>*</b>Salesforce builds are not accounted for.</div>
+          <div className={"ml-2"}><b>*</b>Salesforce builds and deployments are not accounted for.</div>
         </div>
       </div>
     </HelpDocumentationContainer>
