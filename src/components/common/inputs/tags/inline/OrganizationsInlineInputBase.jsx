7f0c9ac08c6051194265a0ad097950e64eabe71d
Tejas Bharadwaj <tejas@opsera.io>
2022-01-06 19:38:48 +0000

    OPL-1335 [TEST]: Added support for Dashboard Orgs filter

---
+++ src/components/common/inputs/tags/inline/OrganizationsInlineInputBase.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import EditIcon from "components/common/icons/field/EditIcon";
+import TagMultiSelectOverlay from "components/common/inputs/tags/inline/modal/TagMultiSelectOverlay";
+import AppliedTagBadge from "components/common/badges/tag/AppliedTagBadge";
+import OrganizationMultiSelectOverlay from "components/common/inputs/tags/inline/modal/OrganizationMultiSelectOverlay";
+
+function OrganizationsInlineInputBase({ model, fieldName, disabled, saveDataFunction, visible, type, tagLocation }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const showEditor = () => {
+    setShowModal(true);
+  };
+
+  if (visible === false || model == null) {
+    return null;
+  }
+
+  return (
+    <div className="role-access">
+      <div className="d-flex">
+        <div>
+          <AppliedTagBadge tags={model?.getData(fieldName)} tagLocation={tagLocation} showNoTagsAppliedBadge={true} />
+        </div>
+        <div>
+          <EditIcon
+            className={"ml-2 text-muted"}
+            handleEditFunction={showEditor}
+            disabled={disabled || saveDataFunction == null}
+            tooltipBody={`Select ${tagLocation ? `${tagLocation} ` : ""}Organizations`}
+          />
+        </div>
+      </div>
+      <OrganizationMultiSelectOverlay
+        type={type}
+        dataObject={model}
+        fieldName={fieldName}
+        saveDataFunction={saveDataFunction}
+        showModal={showModal}
+        handleClose={closeModal}
+      />
+    </div>
+  );
+}
+
+OrganizationsInlineInputBase.propTypes = {
+  fieldName: PropTypes.string,
+  model: PropTypes.object,
+  disabled: PropTypes.bool,
+  visible: PropTypes.bool,
+  tagLocation: PropTypes.string,
+  saveDataFunction: PropTypes.func,
+  type: PropTypes.string,
+};
+
+OrganizationsInlineInputBase.defaultProps = {
+  fieldName: "organizations",
+};
+
+export default OrganizationsInlineInputBase;
---
+++ src/components/insights/dashboards/DashboardOrganizationsInlineInput.jsx
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import dashboardsActions from "components/insights/dashboards/dashboards-actions";
+import axios from "axios";
+import { AuthContext } from "contexts/AuthContext";
+import modelHelpers from "components/common/model/modelHelpers";
+import { getMetricFilterValue } from "components/common/helpers/metrics/metricFilter.helpers";
+import Model from "core/data_model/model";
+import OrganizationsInlineInputBase from "components/common/inputs/tags/inline/OrganizationsInlineInputBase";
+
+function DashboardOrganizationsInlineInput({ model, loadData, disabled, visible }) {
+  const { getAccessToken } = useContext(AuthContext);
+  const [temporaryModel, setTemporaryModel] = useState(undefined);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    if (model) {
+      const newModel = { ...new Model({ ...model.getPersistData() }, model?.getMetaData(), false) };
+      newModel.setData("organizations", getMetricFilterValue(model?.getData("filters"), "organizations"));
+      setTemporaryModel({ ...newModel });
+    }
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [model]);
+
+  const updateDashboardOrganizations = async (newDataModel) => {
+    const newModel = modelHelpers.setDashboardFilterModelField(
+      model,
+      "organizations",
+      newDataModel?.getData("organizations")
+    );
+    const response = await dashboardsActions.updateDashboardV2(getAccessToken, cancelTokenSource, newModel);
+    loadData();
+    return response;
+  };
+
+  if (model == null) {
+    return null;
+  }
+
+  return (
+    <OrganizationsInlineInputBase
+      tagLocation={"Dashboard"}
+      disabled={disabled}
+      visible={visible}
+      model={temporaryModel}
+      fieldName={"organizations"}
+      saveDataFunction={updateDashboardOrganizations}
+    />
+  );
+}
+
+DashboardOrganizationsInlineInput.propTypes = {
+  model: PropTypes.object,
+  disabled: PropTypes.bool,
+  visible: PropTypes.bool,
+  loadData: PropTypes.func,
+};
+
+export default DashboardOrganizationsInlineInput;
