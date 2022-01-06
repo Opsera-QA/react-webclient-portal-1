b30c4e78415817a39f9bca2b689564fc810c0ec8
tbharadwaj <tejas@opsera.io>
2022-01-06 02:50:56 -0800

    Added dashboard orgs support for manual qa metrics

--- src/components/insights/charts/qa_metrics/AdoptionPercentagePieChart.jsx
+++ src/components/insights/charts/qa_metrics/AdoptionPercentagePieChart.jsx
@@ -1,15 +1,19 @@
-import React, {useState, useEffect, useContext, useRef} from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import PropTypes from "prop-types";
 import { ResponsivePie } from "@nivo/pie";
 import config from "components/insights/charts/qa_metrics/manualQaTestPieChartConfig";
 import ModalLogs from "components/common/modal/modalLogs";
-import {AuthContext} from "contexts/AuthContext";
+import { AuthContext } from "contexts/AuthContext";
 import axios from "axios";
 import chartsActions from "components/insights/charts/charts-actions";
 import ChartContainer from "components/common/panels/insights/charts/ChartContainer";
 import {
-  defaultConfig, getColorByData,getColor, assignStandardColors,
-  shortenPieChartLegend, mainColor,
+  defaultConfig,
+  getColorByData,
+  getColor,
+  assignStandardColors,
+  shortenPieChartLegend,
+  mainColor,
 } from "../charts-views";
 import ChartTooltip from "../ChartTooltip";
 import { Col, Container, Row } from "react-bootstrap";
@@ -55,24 +59,28 @@ function AdoptionPercentagePieChart({ kpiConfiguration, setKpiConfiguration, das
       let notes = kpiConfiguration?.filters[kpiConfiguration?.filters.findIndex((obj) => obj.type === "notes")]?.value;
       setNotesData(notes);
       setIsLoading(true);
-      let dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
-      const response = await chartsActions.parseConfigurationAndGetChartMetrics(getAccessToken, cancelSource, "adoptionPercentage", kpiConfiguration, dashboardTags);
+      let dashboardTags =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "adoptionPercentage",
+        kpiConfiguration,
+        dashboardTags
+      );
       let dataObject = response?.data ? response?.data?.data[0]?.adoptionPercentage?.data : [];
       assignStandardColors(dataObject[0]?.pairs);
       shortenPieChartLegend(dataObject[0]?.pairs);
 
-
       if (isMounted?.current === true && dataObject) {
         setMetrics(dataObject);
       }
-    }
-    catch (error) {
+    } catch (error) {
       if (isMounted?.current === true) {
         console.error(error);
         setError(error);
       }
-    }
-    finally {
+    } finally {
       if (isMounted?.current === true) {
         setIsLoading(false);
       }
@@ -85,31 +93,35 @@ function AdoptionPercentagePieChart({ kpiConfiguration, setKpiConfiguration, das
     }
 
     return (
-
-      <div className="new-chart mb-3" style={{height: "300px", display: "flex"}}>
+      <div className="new-chart mb-3" style={{ height: "300px", display: "flex" }}>
         <Container>
           <Row className="p-1">
             <Col lg={6} md={8}>
-              <TwoLineScoreDataBlock className={"test-percentage"}
-                 score={metrics[0]?.executedTests}
-                 subtitle={"No of Automated Test Cases Executed"}
+              <TwoLineScoreDataBlock
+                className={"test-percentage"}
+                score={metrics[0]?.executedTests}
+                subtitle={"No of Automated Test Cases Executed"}
               />
             </Col>
             <Col lg={6} md={8}>
-              <TwoLineScoreDataBlock className={"test-percentage"}
-                 score={metrics[0]?.manualTests}
-                 subtitle={"No of Automated Test Cases Executed Manually"}
+              <TwoLineScoreDataBlock
+                className={"test-percentage"}
+                score={metrics[0]?.manualTests}
+                subtitle={"No of Automated Test Cases Executed Manually"}
               />
             </Col>
           </Row>
           <Row className="p-1 text-center">
             <Col lg={6} md={8}>
-            <NewPercentageDataBlock className={"test-percentage"}
-               percentage={metrics[0]?.adoptionRate}
-               subtitle={"Adoption Percentage"}
-               qualityLevel={metrics[0]?.adoptionRate < 98 ? METRIC_QUALITY_LEVELS.DANGER : METRIC_QUALITY_LEVELS.SUCCESS  }
+              <NewPercentageDataBlock
+                className={"test-percentage"}
+                percentage={metrics[0]?.adoptionRate}
+                subtitle={"Adoption Percentage"}
+                qualityLevel={
+                  metrics[0]?.adoptionRate < 98 ? METRIC_QUALITY_LEVELS.DANGER : METRIC_QUALITY_LEVELS.SUCCESS
+                }
                 goal={"Goal: Adoption Percentage > 98%"}
-            />
+              />
             </Col>
           </Row>
           <Row className="p-3">
@@ -159,6 +171,7 @@ AdoptionPercentagePieChart.propTypes = {
   dashboardData: PropTypes.object,
   index: PropTypes.number,
   setKpiConfiguration: PropTypes.func,
-  setKpis: PropTypes.func};
+  setKpis: PropTypes.func,
+};
 
 export default AdoptionPercentagePieChart;
