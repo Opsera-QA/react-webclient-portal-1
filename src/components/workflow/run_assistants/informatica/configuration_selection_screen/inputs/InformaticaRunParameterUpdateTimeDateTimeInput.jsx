866a427746877e70c30e55cee118f0b1614e1228
Noah Champoux <noah@opsera.io>
2022-01-10 08:14:55 -0600

    [Dormant Code, but tested and approved] OPL-1970  Informatica: Run Parameters Wizard

---
+++ src/components/workflow/run_assistants/informatica/configuration_selection_screen/inputs/InformaticaRunParameterUpdateTimeDateTimeInput.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DateTimeInput from "components/common/inputs/date/DateTimeInput";
+
+const InformaticaRunParameterUpdateTimeDateTimeInput = (
+  {
+    informaticaRunParameterConfigurationModel,
+    setInformaticaRunParameterConfigurationModel,
+  }) => {
+  const setDataFunction = (fieldName, newDate) => {
+    const newModel = {...informaticaRunParameterConfigurationModel};
+    const parsedDate = newDate.toISOString().split('.')[0] + "Z";
+    newModel.setData(fieldName, parsedDate);
+    setInformaticaRunParameterConfigurationModel({...newModel});
+    return newModel;
+  };
+
+  return (
+    <DateTimeInput
+      fieldName={"updateTime"}
+      setDataFunction={setDataFunction}
+      dataObject={informaticaRunParameterConfigurationModel}
+      setDataObject={setInformaticaRunParameterConfigurationModel}
+      defaultToNull={true}
+      maxDate={new Date()}
+    />
+  );
+};
+
+InformaticaRunParameterUpdateTimeDateTimeInput.propTypes = {
+  informaticaRunParameterConfigurationModel: PropTypes.object,
+  setInformaticaRunParameterConfigurationModel: PropTypes.func,
+};
+
+export default InformaticaRunParameterUpdateTimeDateTimeInput;
