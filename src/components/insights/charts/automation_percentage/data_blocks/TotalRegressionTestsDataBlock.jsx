440fa0d43d6df21cfe9aa513fa4862cb11c8c399
Divyesha Malhotra <divyeshamalhotra@Divyeshas-MacBook-Pro.local>
2022-01-10 11:09:36 -0800

    changes

--- src/components/insights/charts/automation_percentage/data_blocks/TotalRegressionTestsDataBlock.jsx
+++ src/components/insights/charts/automation_percentage/data_blocks/TotalRegressionTestsDataBlock.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 import TwoLineDataBlockBase from "components/common/metrics/data_blocks/base/TwoLineDataBlockBase";
 import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
+import TwoLineScoreDataBlock from "../../../../common/metrics/score/TwoLineScoreDataBlock";
 
 function TotalRegressionTestsDataBlock({ defects }) {
   return (
     <DataBlockBoxContainer showBorder={true}>
-      <div className={"p-3"}>
-        <TwoLineDataBlockBase title={defects} subtitle={"Total Number of Regression Test Cases"} />
-      </div>
+        <TwoLineScoreDataBlock
+          className={"p-3"}
+          score={defects}
+          subtitle={"Total Number of Regression Test Cases"} />
     </DataBlockBoxContainer>
   );
 }
