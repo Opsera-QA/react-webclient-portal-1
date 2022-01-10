d92991eb7d70f33fde2fd64a6ee32f9e5188f089
Noah Champoux <noah@opsera.io>
2022-01-10 07:57:50 -0600

    Minor enhancements to InformaticaRunAssistantConfigurationEditorPanel

---
+++ src/components/workflow/run_assistants/informatica/configuration_selection_screen/inputs/InformaticaRunParameterUpdateTimeDateTimeInput.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DateTimeInput from "components/common/inputs/date/DateTimeInput";
+
+const InformaticaRunParameterUpdateTimeDateTimeInput = (
+  {
+    informaticaRunParameterConfigurationModel,
+    setInformaticaRunParameterConfigurationModel,
+  }) => {
+  const setDataFunction = (fieldName, newDate) => {
+    const newModel = {...informaticaRunParameterConfigurationModel};
+    const parsedDate = newDate.toISOString().split('.')[0] + "Z";
+    console.log("parsedDate: " + JSON.stringify(parsedDate));
+    newModel.setData(fieldName, parsedDate);
+    setInformaticaRunParameterConfigurationModel({...newModel});
+  };
+
+  return (
+    <DateTimeInput
+      fieldName={"updateTime"}
+      setDataFunction={setDataFunction}
+      dataObject={informaticaRunParameterConfigurationModel}
+      setDataObject={setInformaticaRunParameterConfigurationModel}
+      defaultToNull={true}
+      maxDate={new Date()}
+    />
+  );
+};
+
+InformaticaRunParameterUpdateTimeDateTimeInput.propTypes = {
+  informaticaRunParameterConfigurationModel: PropTypes.object,
+  setInformaticaRunParameterConfigurationModel: PropTypes.func,
+};
+
+export default InformaticaRunParameterUpdateTimeDateTimeInput;
