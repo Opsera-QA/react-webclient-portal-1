c393321c95b1c4ec0d0fb28a086572f4bef630b8
Faseeh <faseeh@opsera.io>
2022-01-17 09:50:13 +0530

    added new informatica changes

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/informatica/informatica-stepForm-metadata.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/informatica/informatica-stepForm-metadata.js
@@ -17,10 +17,10 @@ const InformaticaStepFormMetadata = {
       lowercase: true,
     },  
     {
-      label: "Informatica Build Step",
+      label: "Informatica Export Object Step",
       id: "buildStepId",
       isRequiredFunction: (model) => {
-        return model?.getData("type") === "deploy";
+        return model?.getData("type") === "import";
       },
       maxLength: 24,
       regexDefinitionName: "mongoId",
@@ -30,7 +30,7 @@ const InformaticaStepFormMetadata = {
       label: "SCM Type",
       id: "service",
       isRequiredFunction: (model) => {
-        return model?.getData("type") === "build";
+        return model?.getData("type") === "export";
       },
       maxLength: 10,
       lowercase: true,
@@ -39,7 +39,7 @@ const InformaticaStepFormMetadata = {
       label: "SCM Account",
       id: "gitToolId",
       isRequiredFunction: (model) => {
-        return model?.getData("type") === "build";
+        return model?.getData("type") === "export";
       },
       maxLength: 24,
       regexDefinitionName: "mongoId",
@@ -49,7 +49,7 @@ const InformaticaStepFormMetadata = {
       label: "Repository",
       id: "repository",
       isRequiredFunction: (model) => {
-        return model?.getData("type") === "build";
+        return model?.getData("type") === "export";
       },
       maxLength: 255,
       regexDefinitionName: "generalTextWithSpacesSlash",
@@ -66,7 +66,7 @@ const InformaticaStepFormMetadata = {
       label: "Branch",
       id: "gitBranch",
       isRequiredFunction: (model) => {
-        return model?.getData("type") === "build";
+        return model?.getData("type") === "export";
       },
       maxLength: 255,
       regexDefinitionName: "generalTextWithSpacesSlash",
