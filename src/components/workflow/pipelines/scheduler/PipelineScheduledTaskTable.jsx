7b535c09b3557078f284db1e7a2521bef4d94fe2
Jim Provenza <Jim@Opsera.io>
2022-01-19 13:44:14 -0800

    update schedule icon. update pipeline scheduled task table

--- src/components/workflow/pipelines/scheduler/PipelineScheduledTaskTable.jsx
+++ src/components/workflow/pipelines/scheduler/PipelineScheduledTaskTable.jsx
@@ -1,11 +1,12 @@
 import React, {useMemo} from "react";
 import PropTypes from "prop-types";
 import pipelineSchedulerMetadata from "components/workflow/pipelines/scheduler/pipeline-scheduler-metadata";
-import {getTableBooleanIconColumn, getTableTextColumn, getTableDateTimeColumn} from "components/common/table/table-column-helpers-v2";
+import {getTableActiveBooleanIconColumn, getTableTextColumn, getTableDateTimeColumn} from "components/common/table/table-column-helpers-v2";
 import {faCalendarAlt} from "@fortawesome/pro-light-svg-icons";
 import FilterContainer from "components/common/table/FilterContainer";
 import Model from "core/data_model/model";
 import VanityTable from "components/common/table/VanityTable";
+import { getField } from "components/common/metadata/metadata-helpers";
 
 function PipelineScheduledTaskTable({ data, isLoading, paginationModel, setPaginationModel, pipeline, loadData, isMounted, setScheduledTaskData }) {
   const fields = [...pipelineSchedulerMetadata.fields, {label: "Next Run", id: "executionDate"}, {label: "Interval", id: "interval"} ];
@@ -33,13 +34,17 @@ function PipelineScheduledTaskTable({ data, isLoading, paginationModel, setPagin
     }
   };
 
+  const getTooltipTemplate = () => {
+    return "testing the tool tip to see if ohhhhhhjjjjjjjjjjjjjjjjjjjj it's showing the tooltip behind the table";
+  };
+
   const columns = useMemo(
     () => [
-      getTableTextColumn(fields.find(field => { return field.id === "name";})),
-      getTableDateTimeColumn(fields.find(field => { return field.id === "executionDate";})),
-      getTableTextColumn(fields.find(field => { return field.id === "interval";})),
-      getTableDateTimeColumn(fields.find(field => { return field.id === "lastRun";})),
-      getTableBooleanIconColumn(fields.find(field => { return field.id === "active";})),
+      getTableTextColumn(getField(fields, "name"), "no-wrap-inline", undefined, undefined, getTooltipTemplate),
+      getTableDateTimeColumn(getField(fields, "executionDate"), "no-wrap-inline", 175, undefined, getTooltipTemplate),
+      getTableTextColumn(getField(fields, "interval"), "no-wrap-inline", undefined, undefined, getTooltipTemplate),
+      getTableDateTimeColumn(getField(fields, "lastRun"), "no-wrap-inline", 175, undefined, getTooltipTemplate),
+      getTableActiveBooleanIconColumn(getField(fields, "active"), "no-wrap-inline", 60, getTooltipTemplate)
     ],
     [],
   );
