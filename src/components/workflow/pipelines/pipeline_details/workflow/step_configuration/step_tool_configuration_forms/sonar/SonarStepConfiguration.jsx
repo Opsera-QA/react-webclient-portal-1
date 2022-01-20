35f25b5662f3c56103d6c12b35b62fb974e60e4c
Noah Champoux <noah@opsera.io>
2022-01-20 11:12:01 -0600

    Update SonarStepConfiguration.jsx

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/sonar/SonarStepConfiguration.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/sonar/SonarStepConfiguration.jsx
@@ -29,15 +29,16 @@ import PipelineStepSelectInput from "components/common/list_of_values_input/work
 import sonarPipelineStepMetadata
   from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/sonar/sonarPipelineStep.metadata";
 
-function SonarStepConfiguration({
-  stepTool,
-  pipelineId,
-  plan,
-  stepId,
-  parentCallback,
-  handleCloseFunction,
-  createJob,
-}) {
+function SonarStepConfiguration(
+  {
+    stepTool,
+    pipelineId,
+    plan,
+    stepId,
+    parentCallback,
+    handleCloseFunction,
+    createJob,
+  }) {
   const [thresholdVal, setThresholdValue] = useState("");
   const [thresholdType, setThresholdType] = useState("");
   const [sonarStepModel, setSonarStepModel] = useState(undefined);
