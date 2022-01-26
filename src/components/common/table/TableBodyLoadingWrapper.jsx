b4a037a6b40b105b3bd663f5b75fe8688c3aa16c
Noah Champoux <noah@opsera.io>
2022-01-26 07:45:39 -0600

    [Stability] Better defensive programming checks on table data

--- src/components/common/table/TableBodyLoadingWrapper.jsx
+++ src/components/common/table/TableBodyLoadingWrapper.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import {faExclamationCircle} from "@fortawesome/pro-light-svg-icons";
 import IconBase from "components/common/icons/IconBase";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 // TODO: Fix so when passing in height the height doesn't change after the table loads
 function TableBodyLoadingWrapper({ isLoading, data, tableComponent, noDataMessage, tableHeight }) {
@@ -15,18 +17,14 @@ function TableBodyLoadingWrapper({ isLoading, data, tableComponent, noDataMessag
 
   if (!Array.isArray(data) || data.length === 0) {
     return (
-      <div className={"h-100 w-100 table-border"}>
-        <div className="w-100 info-text text-center p-3">
-          <div className="row" style={{height: tableHeight, width: "100%"}}>
-            <div className="col-sm-12 my-auto text-center">
-              <span>
-                <IconBase icon={faExclamationCircle} isLoading={isLoading} className="mr-2 mt-1"/>
-                {getNoDataMessage()}
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Row className={"mx-0 w-100 table-border"} style={{height: tableHeight}}>
+        <Col xs={12} className={"my-auto text-center px-0"}>
+          <span className={"info-text"}>
+            <IconBase icon={faExclamationCircle} isLoading={isLoading} className={"mr-2 mt-1"}/>
+            {getNoDataMessage()}
+          </span>
+        </Col>
+      </Row>
     );
   }
 
