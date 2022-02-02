f6afe22dcd800860bbef95dbe36f594e52483823
Shrey Malhotra <shrey@opsera.io>
2022-02-02 15:30:01 -0800

    adding the buildkite tool

---
+++ src/components/inventory/tools/tool_details/tool_jobs/buildkite/BuildkiteToolConfiguration.jsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import Col from "react-bootstrap/Col";
+import ToolConfigurationEditorPanelContainer from "components/common/panels/detail_panel_container/tools/ToolConfigurationEditorPanelContainer";
+import Row from "react-bootstrap/Row";
+import BuildkiteMetadata from "./buildkite-tool-metadata";
+import toolsActions from "components/inventory/tools/tools-actions";
+import { AuthContext } from "contexts/AuthContext";
+import VaultTextAreaInput from "components/common/inputs/text/VaultTextAreaInput";
+import modelHelpers from "components/common/model/modelHelpers";
+import TextInputBase from "../../../../../common/inputs/text/TextInputBase";
+import axios from "axios";
+
+function BuildkiteToolConfiguration({ toolData }) {
+  const { getAccessToken } = useContext(AuthContext);
+  const [buildkiteConfigurationDto, setBuildkiteConfigurationDto] = useState(undefined);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData().catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async () => {
+    setBuildkiteConfigurationDto(
+      modelHelpers.getToolConfigurationModel(toolData.getData("configuration"), BuildkiteMetadata)
+    );
+  };
+
+  const saveBuildkiteToolConfiguration = async () => {
+    const newConfiguration = buildkiteConfigurationDto.getPersistData();
+    newConfiguration.accessToken = await toolsActions.saveThreePartToolPasswordToVaultV2(
+      getAccessToken,
+      cancelTokenSource,
+      toolData,
+      buildkiteConfigurationDto,
+      "accessToken",
+      newConfiguration.accessToken
+    );
+    return await toolsActions.saveToolConfigurationV2(getAccessToken, cancelTokenSource, toolData, newConfiguration);
+  };
+
+  return (
+    <ToolConfigurationEditorPanelContainer
+      model={buildkiteConfigurationDto}
+      setModel={setBuildkiteConfigurationDto}
+      persistRecord={saveBuildkiteToolConfiguration}
+      // toolConnectionCheckName={"buildkite"}
+      toolData={toolData}
+    >
+      <Row>
+        <Col sm={12}>
+          <TextInputBase
+            dataObject={buildkiteConfigurationDto}
+            setDataObject={setBuildkiteConfigurationDto}
+            fieldName={"organizationName"}
+          />
+          <VaultTextAreaInput
+            dataObject={buildkiteConfigurationDto}
+            setDataObject={setBuildkiteConfigurationDto}
+            fieldName={"accessToken"}
+          />
+        </Col>
+      </Row>
+    </ToolConfigurationEditorPanelContainer>
+  );
+}
+
+BuildkiteToolConfiguration.propTypes = {
+  toolData: PropTypes.object,
+  toolId: PropTypes.string,
+  saveToolConfiguration: PropTypes.func,
+  fnSaveToVault: PropTypes.func,
+};
+
+export default BuildkiteToolConfiguration;
