ef9c866d7b9b2ab92f659c10ab036a0b573f9ef4
Noah Champoux <noah@opsera.io>
2022-02-09 12:57:00 -0600

    OPL-2603  [Prod Bug] Notifications Allow Site Role Users to Create But Not View Notifications

---
+++ src/components/notifications/NotificationPolicyManagement.jsx
@@ -0,0 +1,88 @@
+import React, {useContext, useEffect, useRef, useState} from "react";
+import ScreenContainer from "components/common/panels/general/ScreenContainer";
+import {AuthContext} from "contexts/AuthContext";
+import {DialogToastContext} from "contexts/DialogToastContext";
+import Model from "core/data_model/model";
+import notificationsFilterMetadata from "components/notifications/notifications-filter-metadata";
+import notificationsActions from "components/notifications/notifications-actions";
+import NotificationSubNavigationBar from "components/notifications/NotificationSubNavigationBar";
+import NotificationsTable from "components/notifications/NotificationsTable";
+import axios from "axios";
+
+function NotificationPolicyManagement() {
+  const { getAccessToken } = useContext(AuthContext);
+  const toastContext = useContext(DialogToastContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [notificationsList, setNotificationsList] = useState([]);
+  const [notificationFilterDto, setNotificationFilterDto] = useState(new Model({...notificationsFilterMetadata.newObjectFields}, notificationsFilterMetadata, false));
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    loadData().catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async (newFilterDto = notificationFilterDto) => {
+    try {
+      setIsLoading(true);
+      await getNotificationsList(newFilterDto);
+    } catch (error) {
+      console.error(error);
+      toastContext.showLoadingErrorDialog(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const getNotificationsList = async (filterDto = notificationFilterDto) => {
+    // TODO: Update to use cancel token
+    const response = await notificationsActions.getNotificationsList(filterDto, getAccessToken);
+    const notificationsList = response?.data?.data;
+
+    if (Array.isArray(notificationsList)) {
+      setNotificationsList(response?.data?.data);
+      let newFilterDto = filterDto;
+      newFilterDto.setData("totalCount", response.data.count);
+      newFilterDto.setData("activeFilters", newFilterDto.getActiveFilters());
+      setNotificationFilterDto({...newFilterDto});
+    }
+  };
+
+  return (
+    <ScreenContainer
+      breadcrumbDestination={"notificationManagement"}
+      pageDescription={`
+        Create Notification Policies to tailor activity logging to your needs.
+      `}
+      navigationTabContainer={<NotificationSubNavigationBar activeTab={"notifications"} />}
+      isLoading={isLoading}
+    >
+      <NotificationsTable
+        isLoading={isLoading}
+        loadData={loadData}
+        data={notificationsList}
+        notificationFilterDto={notificationFilterDto}
+        setNotificationFilterDto={setNotificationFilterDto}
+        isMounted={isMounted}
+      />
+    </ScreenContainer>
+  );
+}
+
+export default NotificationPolicyManagement;
--- src/components/notifications/NotificationsTable.jsx
+++ src/components/notifications/NotificationsTable.jsx
@@ -10,8 +10,26 @@ import {
 import notificationsMetadata from "./notifications-metadata";
 import {useHistory} from "react-router-dom";
 import { getField } from "components/common/metadata/metadata-helpers";
+import {faFlag} from "@fortawesome/pro-light-svg-icons";
+import FilterContainer from "components/common/table/FilterContainer";
+import ActiveFilter from "components/common/filters/status/ActiveFilter";
+import NotificationTypeFilter from "components/common/filters/notifications/notification_type/NotificationTypeFilter";
+import TagFilter from "components/common/filters/tags/tag/TagFilter";
+import InlineNotificationTypeFilter
+  from "components/common/filters/notifications/notification_type/InlineNotificationTypeFilter";
+import NewNotificationOverlay from "components/notifications/NewNotificationOverlay";
+import {DialogToastContext} from "contexts/DialogToastContext";
 
-function NotificationsTable({ data, notificationFilterDto, setNotificationFilterDto, loadData, isLoading }) {
+function NotificationsTable(
+  {
+    data,
+    notificationFilterDto,
+    setNotificationFilterDto,
+    loadData,
+    isLoading,
+    isMounted,
+  }) {
+  const toastContext = useContext(DialogToastContext);
   let history = useHistory();
   const fields = notificationsMetadata.fields;
 
@@ -34,17 +52,61 @@ function NotificationsTable({ data, notificationFilterDto, setNotificationFilter
     history.push(`/notifications/details/${rowData.original._id}`);
   };
 
+  const getDropdownFilters = () => {
+    return (
+      <>
+        <ActiveFilter filterDto={notificationFilterDto} setFilterDto={setNotificationFilterDto} className="mb-2" />
+        <NotificationTypeFilter filterDto={notificationFilterDto} setFilterDto={setNotificationFilterDto} className="mb-2" />
+        <TagFilter filterDto={notificationFilterDto} setFilterDto={setNotificationFilterDto} />
+      </>
+    );
+  };
+
+  const getInlineFilters = () => {
+    return (
+      <InlineNotificationTypeFilter
+        filterModel={notificationFilterDto}
+        setFilterModel={setNotificationFilterDto}
+        loadData={loadData}
+        className={"mr-2"}
+      />
+    );
+  };
+
+  const createNewNotification = () => {
+    toastContext.showOverlayPanel(<NewNotificationOverlay loadData={loadData} isMounted={isMounted} />);
+  };
+
+  const getNotificationTable = () => {
+    return (
+      <CustomTable
+        className={"no-table-border"}
+        columns={columns}
+        data={data}
+        isLoading={isLoading}
+        onRowSelect={onRowSelect}
+        paginationDto={notificationFilterDto}
+        setPaginationDto={setNotificationFilterDto}
+        rowStyling={rowStyling}
+        loadData={loadData}
+      />
+    );
+  }
+
   return (
-    <CustomTable
-      className={"no-table-border"}
-      columns={columns}
-      data={data}
-      isLoading={isLoading}
-      onRowSelect={onRowSelect}
-      paginationDto={notificationFilterDto}
-      setPaginationDto={setNotificationFilterDto}
-      rowStyling={rowStyling}
+    <FilterContainer
+      className="px-2 pb-2"
       loadData={loadData}
+      filterDto={notificationFilterDto}
+      setFilterDto={setNotificationFilterDto}
+      addRecordFunction={createNewNotification}
+      inlineFilters={getInlineFilters()}
+      supportSearch={true}
+      isLoading={isLoading}
+      body={getNotificationTable()}
+      dropdownFilters={getDropdownFilters()}
+      titleIcon={faFlag}
+      title={"Notification Policies"}
     />
   );
 }
@@ -55,6 +117,7 @@ NotificationsTable.propTypes = {
   isLoading: PropTypes.bool,
   notificationFilterDto: PropTypes.object,
   setNotificationFilterDto: PropTypes.func,
+  isMounted: PropTypes.object,
 };
 
 export default NotificationsTable;
