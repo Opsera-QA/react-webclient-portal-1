2db5583cf6899e3f7b54dddae9640ad64713a5c8
Noah Champoux <noah@opsera.io>
2022-02-11 14:51:06 -0600

    [Dormant Code] Added new field base component ConstantFieldBase

--- src/components/common/fields/tasks/TaskTypeField.jsx
+++ src/components/common/fields/tasks/TaskTypeField.jsx
@@ -1,23 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import FieldContainer from "components/common/fields/FieldContainer";
-import FieldLabel from "components/common/fields/FieldLabel";
 import {getTaskTypeLabel} from "components/tasks/task.types";
+import ConstantFieldBase from "components/common/fields/constant/ConstantFieldBase";
 
 function TaskTypeField({model, fieldName, className, showLabel }) {
-  const [field] = useState(model?.getFieldById(fieldName));
-
-  if (field == null) {
-    return null;
-  }
-
   return (
-    <FieldContainer className={className}>
-      <div className="w-100 d-flex">
-        <FieldLabel field={field} showLabel={showLabel}/>
-        <span>{getTaskTypeLabel(model?.getData(fieldName))}</span>
-      </div>
-    </FieldContainer>
+    <ConstantFieldBase
+      getLabelFunction={getTaskTypeLabel}
+      model={model}
+      fieldName={fieldName}
+      className={className}
+      showLabel={showLabel}
+    />
   );
 }
 
