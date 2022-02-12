dca92f33835fbf0192c4efd88d2d33d41eb45d0a
Noah Champoux <noah@opsera.io>
2022-02-12 15:59:42 -0600

    [Dormant Code] Minor cleanup on new components

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/helpers/git-actions-helper.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/helpers/git-actions-helper.js
@@ -2,19 +2,6 @@ import baseActions from "utils/actionsBase";
 
 const GitActionsHelper = {};
 
-GitActionsHelper.getBranchesV2 = async (getAccessToken, cancelTokenSource, service, gitAccountId, repoId, workspaces) => {
-  const apiUrl = "/tools/properties";
-  const postBody = {
-    tool: service,
-    metric: "getBranches",
-    gitAccountId: gitAccountId,
-    repoId: repoId,
-    workspaces: workspaces
-  };
-
-  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
-};
-
 GitActionsHelper.getReviewersV2 = async (getAccessToken, cancelTokenSource, service, gitAccountId, repoId, workspaces) => {
   const apiUrl = "/tools/properties";
   const postBody = {
