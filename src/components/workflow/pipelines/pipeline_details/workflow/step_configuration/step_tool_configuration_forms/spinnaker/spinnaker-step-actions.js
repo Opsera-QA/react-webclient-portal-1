742f9e68c269185de45f70a36dc966d696e24bbc
Noah Champoux <noah@opsera.io>
2022-02-13 08:15:51 -0600

    [Dormant Code] Added action referencing new controller route

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/spinnaker/spinnaker-step-actions.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/spinnaker/spinnaker-step-actions.js
@@ -2,15 +2,9 @@ import baseActions from "utils/actionsBase";
 
 const SpinnakerStepActions = {};
 
-SpinnakerStepActions.getSpinnakerApplicationsV2 = async (getAccessToken, cancelTokenSource, spinnakerToolId) => {
-  const apiUrl = "/tools/properties";
-  const postBody = {
-    tool: "spinnaker",
-    metric: "applications",
-    id: spinnakerToolId,
-  };
-
-  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+SpinnakerStepActions.getSpinnakerApplicationsV2 = async (getAccessToken, cancelTokenSource, toolId) => {
+  const apiUrl = `/tools/${toolId}/spinnaker/applications`;
+  return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl);
 };
 
 SpinnakerStepActions.getSpinnakerToolsV2 = async (getAccessToken, cancelTokenSource, spinnakerToolId, applicationName) => {
