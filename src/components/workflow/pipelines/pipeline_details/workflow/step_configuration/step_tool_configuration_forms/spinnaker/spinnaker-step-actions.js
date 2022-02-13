34cd5a98e765ee571669314aae5e3d922c2dcada
Noah Champoux <noah@opsera.io>
2022-02-13 08:55:19 -0600

    [Dormant Code] added note to test new route

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/spinnaker/spinnaker-step-actions.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/spinnaker/spinnaker-step-actions.js
@@ -2,9 +2,21 @@ import baseActions from "utils/actionsBase";
 
 const SpinnakerStepActions = {};
 
-SpinnakerStepActions.getSpinnakerApplicationsV2 = async (getAccessToken, cancelTokenSource, toolId) => {
+// TODO: Replace v2 once we can confirm it works
+SpinnakerStepActions.getSpinnakerApplicationsV3 = async (getAccessToken, cancelTokenSource, toolId) => {
   const apiUrl = `/tools/${toolId}/spinnaker/applications`;
   return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl);
+};
+
+SpinnakerStepActions.getSpinnakerApplicationsV2 = async (getAccessToken, cancelTokenSource, spinnakerToolId) => {
+  const apiUrl = `/tools/properties`;
+  const postBody = {
+    tool: "spinnaker",
+    metric: "applications",
+    id: spinnakerToolId,
+  };
+
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
 };
 
 SpinnakerStepActions.getSpinnakerToolsV2 = async (getAccessToken, cancelTokenSource, spinnakerToolId, applicationName) => {
