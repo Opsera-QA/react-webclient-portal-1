84f196e2ea7861fd9a41af3b423b1b05b3acf314
Shrey Malhotra <shrey@opsera.io>
2022-02-15 14:29:45 -0800

    adding some buildkite step config data

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/buildkite/buildkite-metadata.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/buildkite/buildkite-metadata.js
@@ -1,14 +1,80 @@
 const buildkiteMetadata = {
-  type: "AWS ECS Deploy Tool Configuration",
+  type: "Buildkite Tool Configuration",
   fields: [
     {
       label: "Buildkite Tool",
       id: "toolConfigId",
       isRequired: true
-    }
+    },
+    {
+      label: "Buildkite Pipeline",
+      id: "pipeline",
+      isRequired: true
+    },
+    {
+      label: "SCM Tool",
+      id: "gitToolId",
+    },
+    {
+      label: "Repository ID",
+      id: "repoId",
+    },
+    {
+      label: "Project ID",
+      id: "projectId",
+    },
+    {
+      label: "SCM Service Type",
+      id: "service",
+    },
+    {
+      label: "GIT URL",
+      id: "gitUrl"
+    },
+    {
+      label: "SSH URL",
+      id: "sshUrl",
+    },
+    {
+      label: "Repository",
+      id: "repository",
+    },
+    {
+      label: "Workspace",
+      id: "workspace"
+    },
+    {
+      label: "Workspace/Project",
+      id: "workspaceName"
+    },
+    {
+      label: "Branch",
+      id: "defaultBranch",
+    },
+    {
+      label: "Commit",
+      id: "commit",
+    },
+    {
+      label: "Commit Message",
+      id: "message",
+    },
   ],
   newObjectFields: {
-    toolConfigId: ""
+    toolConfigId: "",
+    pipeline: "",
+    gitToolId : "",
+    repoId : "",
+    projectId : "",
+    service : "",
+    gitUrl : "",
+    sshUrl : "",
+    repository : "",
+    workspace : "",
+    workspaceName : "",
+    defaultBranch : "",
+    commit : "",
+    message: ""
   }
 };
 
---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/buildkite/inputs/KafkConnectSCMToolTypeSelectInput.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+
+function BuildkiteSCMToolTypeSelectInput({dataObject, setDataObject, isLoading, disabled}) {
+  
+  const handleDTOChange = async (fieldName, selectedOption) => {
+    let newDataObject = {...dataObject};
+    await newDataObject.setData(fieldName, selectedOption.value);
+    newDataObject.setData("gitToolId", "");
+    newDataObject.setData("repoId", "");
+    newDataObject.setData("gitUrl", "");
+    newDataObject.setData("sshUrl", "");
+    newDataObject.setData("repository", "");
+    newDataObject.setData("workspace", "");
+    newDataObject.setData("workspaceName", "");
+    newDataObject.setData("gitBranch", "");
+    setDataObject({...newDataObject});
+    return;
+  };
+
+  const SCM_TOOL_LIST = [
+    {
+      name: "Gitlab",
+      value: "gitlab",
+    },
+    {
+      name: "Github",
+      value: "github",
+    },
+    {
+      name: "Bitbucket",
+      value: "bitbucket",
+    },
+  ];
+
+  return (
+    
+     <SelectInputBase
+       fieldName={"service"}
+       dataObject={dataObject}
+       setDataObject={setDataObject}
+       selectOptions={SCM_TOOL_LIST}
+       valueField={"value"}
+       textField={"name"}
+       placeholderText={"Select a SCM Tool Type"}
+       setDataFunction={handleDTOChange}
+       disabled={disabled}
+       busy={isLoading}
+     />
+  );
+}
+
+BuildkiteSCMToolTypeSelectInput.propTypes = {
+  dataObject: PropTypes.object,
+  setDataObject: PropTypes.func,
+  disabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  className: PropTypes.string
+};
+
+export default BuildkiteSCMToolTypeSelectInput;
