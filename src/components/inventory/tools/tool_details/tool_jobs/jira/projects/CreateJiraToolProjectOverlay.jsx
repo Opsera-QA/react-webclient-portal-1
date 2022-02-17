72a06736b1484204d3566ff9130f21edb3d8b8d4
Noah Champoux <noah@opsera.io>
2022-02-17 06:50:37 -0600

    [Dormant Code] OPL-2646 Tool Registry_JIRA_No char length restrictions available in description

--- src/components/inventory/tools/tool_details/tool_jobs/jira/projects/CreateJiraToolProjectOverlay.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/jira/projects/CreateJiraToolProjectOverlay.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import JiraToolProjectEditorPanel
   from "components/inventory/tools/tool_details/tool_jobs/jira/projects/details/JiraToolProjectEditorPanel";
 import Model from "core/data_model/model";
-import {jiraProjectMetadata} from "components/inventory/tools/tool_details/tool_jobs/jira/projects/jiraProject.metadata";
+import {jiraToolProjectMetadata} from "components/inventory/tools/tool_details/tool_jobs/jira/projects/jiraToolProject.metadata";
 import CreateCenterPanel from "components/common/overlays/center/CreateCenterPanel";
 import {DialogToastContext} from "contexts/DialogToastContext";
 
@@ -12,7 +12,7 @@ function CreateJiraToolProjectOverlay({ toolData, loadData } ) {
   const [jiraProjectData, setJiraProjectData] = useState(undefined);
 
   useEffect(() => {
-    setJiraProjectData(new Model({...jiraProjectMetadata.newObjectFields}, jiraProjectMetadata, true));
+    setJiraProjectData(new Model({...jiraToolProjectMetadata.newObjectFields}, jiraToolProjectMetadata, true));
   }, []);
 
   const closePanel = () => {
