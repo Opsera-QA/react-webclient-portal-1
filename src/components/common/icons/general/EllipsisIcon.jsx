ed013f5ee87531f0cb070623a29c384e4c064e5b
Noah Champoux <noah@opsera.io>
2022-02-20 12:31:56 -0600

    [Dormant Code] Added further enhancements to DialogToastContext for future enhancements

--- src/components/common/icons/general/EllipsisIcon.jsx
+++ src/components/common/icons/general/EllipsisIcon.jsx
@@ -1,30 +1,23 @@
-import React, {useRef, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import PropTypes from "prop-types";
-import {Overlay} from "react-bootstrap";
 import IconBase from "components/common/icons/IconBase";
-import Popover from "react-bootstrap/Popover";
 import {faEllipsisHAlt} from "@fortawesome/pro-light-svg-icons";
 import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
+import {DialogToastContext} from "contexts/DialogToastContext";
 
-function EllipsisIcon({ overlay, className, size, placement, maxWidth, tooltipText }) {
-  const [showOverlay, setShowOverlay] = useState(false);
+function EllipsisIcon(
+  {
+    overlay,
+    className,
+    size,
+    tooltipText,
+  }) {
+  const toastContext = useContext(DialogToastContext);
   const [target, setTarget] = useState(null);
   const ref = useRef(null);
 
-  const getPopover = () => {
-    return (
-      <Popover
-        id={`input-popover`}
-        style={{ width: "1000px", maxWidth: maxWidth }}
-        className={"info-overlay"}
-      >
-        {overlay}
-      </Popover>
-    );
-  };
-
   const handleClick = (event) => {
-    setShowOverlay(!showOverlay);
+    toastContext.showInfoOverlayPanel(overlay);
     setTarget(event.target);
   };
 
@@ -33,29 +26,16 @@ function EllipsisIcon({ overlay, className, size, placement, maxWidth, tooltipTe
   }
 
   return (
-    <>
-      <TooltipWrapper innerText={tooltipText} >
+    <TooltipWrapper innerText={tooltipText}>
       <span className={className} ref={ref} onClick={handleClick}>
-          <IconBase
-            icon={faEllipsisHAlt}
-            className={"pointer"}
-            iconSize={size}
-            onClickFunction={handleClick}
-          />
+        <IconBase
+          icon={faEllipsisHAlt}
+          className={"pointer"}
+          iconSize={size}
+          onClickFunction={handleClick}
+        />
       </span>
-      </TooltipWrapper>
-      <Overlay
-        onHide={() => setShowOverlay(false)}
-        rootClose
-        show={showOverlay}
-        target={target}
-        placement={placement}
-        container={ref.current}
-        containerPadding={20}
-      >
-        {getPopover()}
-      </Overlay>
-    </>
+    </TooltipWrapper>
   );
 }
 
@@ -63,14 +43,10 @@ EllipsisIcon.propTypes = {
   className: PropTypes.string,
   overlay: PropTypes.any,
   size: PropTypes.string,
-  placement: PropTypes.string,
-  maxWidth: PropTypes.string,
   tooltipText: PropTypes.string,
 };
 
 EllipsisIcon.defaultProps = {
-  placement: "left",
-  maxWidth: "1500px",
   size: "lg"
 };
 
