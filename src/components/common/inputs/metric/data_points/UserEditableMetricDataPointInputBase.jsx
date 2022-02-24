3c69b9b74da232b09ed9170e83fa1420691a73e5
Noah Champoux <noah@opsera.io>
2022-02-24 17:30:14 -0600

    [Dormant Code] UserEditableMetricDataPointInputBase

--- src/components/common/inputs/metric/data_points/UserEditableMetricDataPointInputBase.jsx
+++ src/components/common/inputs/metric/data_points/UserEditableMetricDataPointInputBase.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import UserEditableMetricStrategicCriteriaPanel
   from "components/common/inputs/metric/data_points/strategic_criteria/UserEditableMetricStrategicCriteriaPanel";
-import InfoContainer from "components/common/containers/InfoContainer";
 
 function UserEditableMetricDataPointInputBase(
   {
@@ -11,14 +10,16 @@ function UserEditableMetricDataPointInputBase(
     dataPoint,
   }) {
   return (
-    <InfoContainer titleText={`${dataPoint?.name} Data Point`}>
-      <span className={"mb-2"}>{dataPoint?.description}</span>
-      <UserEditableMetricStrategicCriteriaPanel
-        model={model}
-        setModel={setModel}
-        strategicCriteria={dataPoint?.strategic_criteria}
-      />
-    </InfoContainer>
+    <div className={"m-1"}>
+      <span className={"mb-2 mx-2"}>{dataPoint?.description}</span>
+      <div className={"m-3"}>
+        <UserEditableMetricStrategicCriteriaPanel
+          model={model}
+          setModel={setModel}
+          strategicCriteria={dataPoint?.strategic_criteria}
+        />
+      </div>
+    </div>
   );
 }
 
