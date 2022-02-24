ff3ab1e3aba024088ae359133c53f82d20a2294d
Vignesh Subramanian <vignesh@opsera.io>
2022-02-24 23:43:43 +0530

    terraform step config changes

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/terraform/sub_forms/BackendStateTerraformCloud.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/terraform/sub_forms/BackendStateTerraformCloud.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TerraformCloudToolSelectInput from "../inputs/terraform_cloud/TerraformCloudToolSelectInput";
 import TerraformCloudOrganizationsSelectInput from "../inputs/terraform_cloud/TerraformCloudOrganizationsSelectInput";
+import TerraformCloudWorkspaceSelectInput from "../inputs/terraform_cloud/TerraformCloudWorkspaceSelectInput";
 
 function BackendStateAzure({ model, setModel }) {
   if (!model?.getData("backendState") || model?.getData("backendState") !== "TERRAFORM_CLOUD") {
@@ -12,6 +13,7 @@ function BackendStateAzure({ model, setModel }) {
     <>
       <TerraformCloudToolSelectInput model={model} setModel={setModel} />
       <TerraformCloudOrganizationsSelectInput dataObject={model} setDataObject={setModel} disabled={model?.getData("terraformCloudId")?.length === 0} toolId={model?.getData("terraformCloudId")} />
+      <TerraformCloudWorkspaceSelectInput dataObject={model} setDataObject={setModel} disabled={model?.getData("organizationName")?.length === 0} toolId={model?.getData("terraformCloudId")} />
     </>
   );
 }
