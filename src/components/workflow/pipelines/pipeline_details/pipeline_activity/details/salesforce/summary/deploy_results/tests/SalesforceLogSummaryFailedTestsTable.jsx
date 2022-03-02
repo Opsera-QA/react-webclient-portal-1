977563df890c7d53d143e65f84b4a2d6382ad7c8
Noah Champoux <noah@opsera.io>
2022-03-02 10:56:10 -0600

    Fixed broken imports

--- src/components/workflow/pipelines/pipeline_details/pipeline_activity/details/salesforce/summary/deploy_results/tests/SalesforceLogSummaryFailedTestsTable.jsx
+++ src/components/workflow/pipelines/pipeline_details/pipeline_activity/details/salesforce/summary/deploy_results/tests/SalesforceLogSummaryFailedTestsTable.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import {faCheckCircle, faExclamationCircle} from "@fortawesome/pro-light-svg-icons";
 import IconBase from "components/common/icons/IconBase";
 import SalesforceLogSummaryFailedTestResultsTable
-  from "components/workflow/pipelines/pipeline_details/pipeline_activity/details/salesforce/summary/deploy_results/tests/SalesforceLogSummaryFailedTestResultsTable";
+  from "components/workflow/pipelines/pipeline_details/pipeline_activity/details/salesforce/summary/deploy_results/SalesforceLogSummaryFailedTestResultsTable";
 
 function SalesforceLogSummaryFailedTestsTable({ hasSuccessfulTests, unsuccessfulTests }) {
   if (!Array.isArray(unsuccessfulTests) || unsuccessfulTests.length === 0) {
