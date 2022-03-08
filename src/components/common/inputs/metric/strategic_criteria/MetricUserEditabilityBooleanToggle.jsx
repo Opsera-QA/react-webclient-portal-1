451726e78c5e0205170fea69dfd10c9882e8bfc7
Noah Champoux <noah@opsera.io>
2022-03-08 15:13:44 -0600

    [Dormant Code] Extended visibility and editability support for data points

---
+++ src/components/common/inputs/metric/strategic_criteria/MetricUserEditabilityBooleanToggle.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StandaloneBooleanToggleInput from "components/common/inputs/boolean/StandaloneBooleanToggleInput";
+
+function MetricUserEditabilityBooleanToggle({model, setModel}) {
+  const setDataFunction = (fieldName, newValue) => {
+    const newModel = {...model};
+    const strategicCriteria = model?.getData("strategic_criteria");
+    strategicCriteria.isUserEditable = newValue;
+    newModel.setData("strategic_criteria", strategicCriteria);
+    setModel({...newModel});
+  };
+
+  if (model == null) {
+    return null;
+  }
+
+  return (
+    <StandaloneBooleanToggleInput
+      fieldLabel={"Allow Users to Edit Strategic Criteria"}
+      setDataFunction={setDataFunction}
+      fieldId={"isUserEditable"}
+      checkedValue={model?.getData("strategic_criteria")?.isUserEditable === true}
+    />
+  );
+}
+
+MetricUserEditabilityBooleanToggle.propTypes = {
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+};
+
+export default MetricUserEditabilityBooleanToggle;
