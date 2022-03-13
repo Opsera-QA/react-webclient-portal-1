178b686506468c64dfea424f26c0d7fdf427fa24
Noah Champoux <noah@opsera.io>
2022-03-13 10:24:46 -0500

    [Dormant Code] Added ability to create an endpoint on a tool (feature flagged

--- src/components/inventory/tools/details/identifiers/external_api_integrator/endpoints/NewExternalApiIntegratorEndpointOverlay.jsx
+++ src/components/inventory/tools/details/identifiers/external_api_integrator/endpoints/NewExternalApiIntegratorEndpointOverlay.jsx
@@ -1,16 +1,22 @@
 import React, {useContext, useState} from "react";
 import PropTypes from "prop-types";
-import JenkinsJobEditorPanel from "components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/JenkinsJobEditorPanel";
 import CreateCenterPanel from "components/common/overlays/center/CreateCenterPanel";
 import { DialogToastContext } from "contexts/DialogToastContext";
-import JenkinsJobMetadata from "components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/jenkins-job-metadata";
-import Model from "core/data_model/model";
+import toolEndpointsMetadata from "components/inventory/tools/details/endpoints/toolEndpoints.metadata";
+import ExternalApiIntegratorEndpointEditorPanel
+  from "components/inventory/tools/details/identifiers/external_api_integrator/endpoints/ExternalApiIntegratorEndpointEditorPanel";
+import modelHelpers from "components/common/model/modelHelpers";
 
-function NewExternalApiIntegratorEndpointOverlay({ toolId, loadData, isMounted }) {
+function NewExternalApiIntegratorEndpointOverlay(
+  {
+    toolId,
+    loadData,
+    isMounted,
+  }) {
   const toastContext = useContext(DialogToastContext);
-  const [jenkinsJobModel, setJenkinsJobModel] = useState(new Model({...JenkinsJobMetadata.newObjectFields}, JenkinsJobMetadata, true));
+  const [externalApiIntegratorModel, setExternalApiIntegratorModel] = useState(modelHelpers.parseObjectIntoModel(undefined, toolEndpointsMetadata));
 
-  const closePanel = () => {
+  const closePanelFunction = () => {
     if (isMounted?.current === true) {
       loadData();
     }
@@ -19,27 +25,28 @@ function NewExternalApiIntegratorEndpointOverlay({ toolId, loadData, isMounted }
     toastContext.clearOverlayPanel();
   };
 
-  if (toolData == null) {
+  if (externalApiIntegratorModel == null) {
     return null;
   }
 
   return (
     <CreateCenterPanel
-      objectType={JenkinsJobMetadata.type}
+      objectType={toolEndpointsMetadata?.type}
       loadData={loadData}
-      closePanel={closePanel}
+      closePanel={closePanelFunction}
     >
-      <JenkinsJobEditorPanel
-        jenkinsJobModel={jenkinsJobModel}
-        setJenkinsJobModel={setJenkinsJobModel}
-        handleClose={closePanel}
+      <ExternalApiIntegratorEndpointEditorPanel
+        externalApiIntegratorModel={externalApiIntegratorModel}
+        setExternalApiIntegratorModel={setExternalApiIntegratorModel}
+        closePanelFunction={closePanelFunction}
+        toolId={toolId}
       />
     </CreateCenterPanel>
   );
 }
 
 NewExternalApiIntegratorEndpointOverlay.propTypes = {
-  toolData: PropTypes.object,
+  toolId: PropTypes.string,
   loadData: PropTypes.func,
   isMounted: PropTypes.object,
 };
--- src/components/inventory/tools/details/identifiers/external_api_integrator/endpoints/externalApiIntegratorEndpoints.actions.js
+++ src/components/inventory/tools/details/identifiers/external_api_integrator/endpoints/externalApiIntegratorEndpoints.actions.js
@@ -7,4 +7,13 @@ externalApiIntegratorEndpointsActions.getExternalApiIntegratorEndpointsV2 = asyn
   return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl);
 };
 
+externalApiIntegratorEndpointsActions.createExternalApiIntegratorEndpointV2 = async (getAccessToken, cancelTokenSource, toolId, endpointModel) => {
+  const apiUrl = `/tool/external-api-integrator/${toolId}/endpoints/create`;
+  const postBody = {
+    ...endpointModel?.getPersistData(),
+  };
+
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, postBody);
+};
+
 export default externalApiIntegratorEndpointsActions;
