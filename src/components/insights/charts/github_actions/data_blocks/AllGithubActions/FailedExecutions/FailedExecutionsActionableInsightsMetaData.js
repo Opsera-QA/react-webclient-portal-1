3149ee5589dd0f29225d2a5f30994d445766d254
Phanisri Adusumilli <phanisriadusumilli@Phanisris-MacBook-Pro.local>
2022-03-20 20:09:56 -0400

    OPL-2678-made changes to get response from api and show data according to the response

---
+++ src/components/insights/charts/github_actions/data_blocks/AllGithubActions/FailedExecutions/FailedExecutionsActionableInsightsMetaData.js
@@ -0,0 +1,21 @@
+const FailedExecutionsActionableInsightsMetaData = {
+  idProperty: "_id",
+  type: "Failed Executions Actionable Insights",
+  fields: [
+    {
+      label: "Average duration to complete",
+      id: "actionDurationInMins",
+    },
+    {
+      label: "Run Number/Run Count",
+      id: "actionRunNumber",
+    },
+    {
+      label: "Trend",
+      id: "runTrend",
+    }
+  ],
+  newObjectFields: {},
+};
+
+export default FailedExecutionsActionableInsightsMetaData;
--- src/components/insights/charts/github_actions/data_blocks/AllGithubActions/SuccessPercent/SuccessPercentActionableInsights.jsx
+++ src/components/insights/charts/github_actions/data_blocks/AllGithubActions/SuccessPercent/SuccessPercentActionableInsights.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, {useEffect, useState, useRef, useContext} from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import { Row, Col } from "react-bootstrap";
@@ -15,10 +15,18 @@ import {ResponsivePie} from "@nivo/pie";
 import DataBlockBoxContainer from "../../../../../../common/metrics/data_blocks/DataBlockBoxContainer";
 import MetricContentDataBlockBase from "../../../../../../common/metrics/data_blocks/MetricContentDataBlockBase";
 import {ResponsiveBar} from "@nivo/bar";
+import {AuthContext} from "../../../../../../../contexts/AuthContext";
+import chartsActions from "../../../../charts-actions";
 
 function SuccessPercentActionableInsights({ kpiConfiguration, dashboardData }) {
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const { getAccessToken } = useContext(AuthContext);
   const isMounted = useRef(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(undefined);
+  const [topSuccessfulActions, setTopSuccessfulActions] = useState([]);
+  const [topSuccessfulApplications, setTopSuccessfulApplications] = useState([]);
+  const [topSuccessfulJobs, setTopSuccessfulJobs] = useState([]);
 
   useEffect(() => {
     if (cancelTokenSource) {
@@ -29,35 +37,17 @@ function SuccessPercentActionableInsights({ kpiConfiguration, dashboardData }) {
     setCancelTokenSource(source);
 
     isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
 
     return () => {
       source.cancel();
       isMounted.current = false;
     };
   }, []);
-
-  const getDateRange = () => {
-    const date = getMetricFilterValue(kpiConfiguration?.filters, "date");
-    return <MetricDateRangeBadge startDate={date?.startDate} endDate={date?.endDate} />;
-  };
-
-  const data = [
-    {
-      color: "#5B5851",
-      id: "Test1",
-      value: 60,
-    },
-    {
-      color: "#7A756C",
-      id: "Test2",
-      value: 30,
-    },
-    {
-      color: "#7A756C",
-      id: "Test3",
-      value: 10,
-    }
-  ];
 
   const barData = [
     {
@@ -82,6 +72,57 @@ function SuccessPercentActionableInsights({ kpiConfiguration, dashboardData }) {
     }
   ];
 
+  const getDateRange = () => {
+    const date = getMetricFilterValue(kpiConfiguration?.filters, "date");
+    return <MetricDateRangeBadge startDate={date?.startDate} endDate={date?.endDate} />;
+  };
+
+  const loadData = async (cancelSource = cancelTokenSource) => {
+    try {
+      setIsLoading(true);
+      let dashboardOrgs =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "organizations")]
+          ?.value;
+      const dashboardTags =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        "releaseTraceabilitySuccess",
+        kpiConfiguration,
+        dashboardTags,
+        null,
+        null,
+        dashboardOrgs
+      );
+      const topActions = response?.data?.data[0]?.topSuccessfulActions;
+      const topApplications = response?.data?.data[0]?.topSuccessfulApplications;
+      const topJobs = response?.data?.data[0]?.topSuccessfulJobs;
+      //To remove following for loops once api is chnaged to send value param as expected.
+      for(let i =0; i<=topActions.length - 1; i++){
+        topActions[i].value = topActions[i].success_percentage;
+      }
+      for(let j =0; j<=topApplications.length - 1; j++){
+        topApplications[j].value = topApplications[j].success_percentage;
+      }
+      for(let k =0; k<=topJobs.length - 1; k++){
+        topJobs[k].value = topJobs[k].success_percentage;
+      }
+      setTopSuccessfulActions(topActions);
+      setTopSuccessfulApplications(topApplications);
+      setTopSuccessfulJobs(topJobs);
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
   const getBody = () => {
     return (
       <>
@@ -96,138 +137,109 @@ function SuccessPercentActionableInsights({ kpiConfiguration, dashboardData }) {
     return (
       <Row className="pb-3 px-2 my-5">
         <Col xl={4} lg={4} md={4} className={"my-1"}>
-          <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-            <ResponsivePie
-              data={data}
-              {...defaultConfig()}
-              {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-            />
-            <div style={{textAlign: 'center', marginLeft: '2rem'}}>
-              Top 5 Applications
-            </div>
-          </div>
+          {getTopSuccessfulApplicationsPieChart()}
         </Col>
         <Col xl={4} lg={4} md={4} className={"my-1"}>
-          <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-            <ResponsivePie
-              data={data}
-              {...defaultConfig()}
-              {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-            />
-            <div style={{textAlign: 'center', marginLeft: '2rem'}}>
-              Top Five Actions
-            </div>
-          </div>
+          {getTopSuccessfulActionsPieChart()}
         </Col>
         <Col xl={4} lg={4} md={4} className={"my-1"}>
-          <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-            <ResponsivePie
-              data={data}
-              {...defaultConfig()}
-              {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-            />
-            <div style={{textAlign: 'center', marginLeft: '2rem'}}>
-              Top Five Jobs
-            </div>
-          </div>
+          {getTopSuccessfulJobsPieChart()}
         </Col>
       </Row>
     );
   };
 
-  const getSuccessPercentSummaryTextBoxes = () => {
+  const getTopSuccessfulApplicationsPieChart = () => {
+    if(!topSuccessfulApplications || topSuccessfulApplications.length === 0) {
+      return null;
+    }
     return (
-      <Row className="pb-3 px-2 my-5">
-        <Col md={6}>
-          <DataBlockBoxContainer showBorder={true} className={'github-actions-border-left'}>
-            <MetricContentDataBlockBase
-              title={"Action Name"}
-              content={
-                <div style={{display: "grid"}}>
-                  <span>Application Name</span>
-                  <Row className="pb-3 px-2 my-5">
-                    <Col xl={12} lg={12} md={12} className={"my-1"} style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-                      <ResponsiveBar
-                        data={barData}
-                        {...defaultConfig("Percentage", "", false, false, "", "")}
-                        {...barConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-                        {...adjustBarWidth(barData)}
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              }
-            />
-          </DataBlockBoxContainer>
-        </Col>
-        <Col md={6}>
-          <DataBlockBoxContainer showBorder={true} className={'github-actions-border-left'}>
-            <MetricContentDataBlockBase
-              title={"Action Name"}
-              content={
-                <div style={{display: "grid"}}>
-                  <span>Application Name</span>
-                  <Row className="pb-3 px-2 my-5">
-                    <Col xl={12} lg={12} md={12} className={"my-1"} style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-                      <ResponsiveBar
-                        data={barData}
-                        {...defaultConfig("Percentage", "", false, false, "", "")}
-                        {...barConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-                        {...adjustBarWidth(barData)}
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              }
-            />
-          </DataBlockBoxContainer>
-        </Col>
-        <Col md={6} className={"my-4"}>
-          <DataBlockBoxContainer showBorder={true} className={'github-actions-border-left'}>
-            <MetricContentDataBlockBase
-              title={"Action Name"}
-              content={
-                <div style={{display: "grid"}}>
-                  <span>Application Name</span>
-                  <Row className="pb-3 px-2 my-5">
-                    <Col xl={12} lg={12} md={12} className={"my-1"} style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-                      <ResponsiveBar
-                        data={barData}
-                        {...defaultConfig("Percentage", "", false, false, "", "")}
-                        {...barConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-                        {...adjustBarWidth(barData)}
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              }
-            />
-          </DataBlockBoxContainer>
-        </Col>
-        <Col md={6} className={"my-4"}>
-          <DataBlockBoxContainer showBorder={true} className={'github-actions-border-left'}>
-            <MetricContentDataBlockBase
-              title={"Action Name"}
-              content={
-                <div style={{display: "grid"}}>
-                  <span>Application Name</span>
-                  <Row className="pb-3 px-2 my-5">
-                    <Col xl={12} lg={12} md={12} className={"my-1"} style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
-                      <ResponsiveBar
-                        data={barData}
-                        {...defaultConfig("Percentage", "", false, false, "", "")}
-                        {...barConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
-                        {...adjustBarWidth(barData)}
-                      />
-                    </Col>
-                  </Row>
-                </div>
-              }
-            />
-          </DataBlockBoxContainer>
-        </Col>
-      </Row>
+      <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
+        <ResponsivePie
+          data={topSuccessfulApplications}
+          {...defaultConfig()}
+          {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
+        />
+        <div style={{textAlign: 'center', marginLeft: '3rem'}}>
+          Top Five Applications
+        </div>
+      </div>
+    );
+  };
 
+  const getTopSuccessfulActionsPieChart = () => {
+    if(!topSuccessfulActions || topSuccessfulActions.length === 0) {
+      return null;
+    }
+    return (
+      <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
+        <ResponsivePie
+          data={topSuccessfulActions}
+          {...defaultConfig()}
+          {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
+        />
+        <div style={{textAlign: 'center', marginLeft: '3rem'}}>
+          Top Five Actions
+        </div>
+      </div>
+    );
+  };
+
+  const getTopSuccessfulJobsPieChart = () => {
+    if(!topSuccessfulJobs || topSuccessfulJobs.length === 0) {
+      return null;
+    }
+    return (
+      <div style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
+        <ResponsivePie
+          data={topSuccessfulJobs}
+          {...defaultConfig()}
+          {...chartConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
+        />
+        <div style={{textAlign: 'center', marginLeft: '3rem'}}>
+          Top Five Jobs
+        </div>
+      </div>
+    );
+  };
+
+  const getSuccessPercentSummaryTextBoxes = () => {
+    if(!topSuccessfulActions || topSuccessfulActions.length === 0) {
+      return null;
+    }
+    var successPercentSummary = [];
+    for (var i = 0; i <= topSuccessfulActions.length - 1; i++) {
+      successPercentSummary.push(
+        <Col md={6} className={'mb-2'}>
+          <DataBlockBoxContainer showBorder={true} className={'github-actions-border-left'}>
+            <MetricContentDataBlockBase
+              title={topSuccessfulActions[i].actionName}
+              content={
+                <div style={{display: "grid"}}>
+                  <span>Application Name: {topSuccessfulActions[i].applicationName}</span>
+                  <Row className="pb-3 px-2 my-5">
+                    <Col xl={12} lg={12} md={12} className={"my-1"} style={{ height: METRIC_CHART_STANDARD_HEIGHT }}>
+                      <ResponsiveBar
+                        data={barData}
+                        {...defaultConfig("Percentage", "", false, false, "", "")}
+                        {...barConfig(METRIC_THEME_CHART_PALETTE_COLORS)}
+                        {...adjustBarWidth(barData)}
+                      />
+                    </Col>
+                  </Row>
+                </div>
+              }
+            />
+          </DataBlockBoxContainer>
+        </Col>
+      );
+    }
+    return (
+      <div>
+        <Row className="pb-3 px-2 my-5">
+          {successPercentSummary}
+        </Row>
+      </div>
     );
   };
 
