f3580b4bf69e84073271a7f84c2dc7ec38e32a33
Divyesha Malhotra <divyeshamalhotra@Divyeshas-MacBook-Pro.local>
2022-03-23 00:11:42 -0700

    changes

--- src/components/insights/charts/jira/line_chart/lead_time/JiraLeadTimeLineChart.jsx
+++ src/components/insights/charts/jira/line_chart/lead_time/JiraLeadTimeLineChart.jsx
@@ -116,19 +116,19 @@ function JiraLeadTimeLineChart({ kpiConfiguration, setKpiConfiguration, dashboar
     };
 
     //TODO: Do these need to be passed in via object props?
-    const RollingMeanLineLayer = ({ nodes, xScale, yScale }) => {
-      const lineGenerator = line()
-        .x((d) => xScale(d.data.x))
-        .y((d) => yScale(d.data.rolling_mean));
-      return (
-        <path
-          d={lineGenerator(nodes)}
-          fill="none"
-          stroke={METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_3}
-          strokeWidth="2"
-        />
-      );
-    };
+    // const RollingMeanLineLayer = ({ nodes, xScale, yScale }) => {
+    //   const lineGenerator = line()
+    //     .x((d) => xScale(d.data.x))
+    //     .y((d) => yScale(d.data.rolling_mean));
+    //   return (
+    //     <path
+    //       d={lineGenerator(nodes)}
+    //       fill="none"
+    //       stroke={METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_3}
+    //       strokeWidth="2"
+    //     />
+    //   );
+    // };
     const onNodeSelect = (node) => {
       setModalData(
         issueData.filter(function (item) {
@@ -200,7 +200,7 @@ function JiraLeadTimeLineChart({ kpiConfiguration, setKpiConfiguration, dashboar
                   false,
                   "circle"
                 )}
-                {...config(getColor, MeanLineLayer, RollingMeanLineLayer)}
+                {...config(getColor, MeanLineLayer)}
                 onClick={(node) => onNodeSelect(node)}
                 tooltip={({ node, color }) => (
                   <ChartTooltip
