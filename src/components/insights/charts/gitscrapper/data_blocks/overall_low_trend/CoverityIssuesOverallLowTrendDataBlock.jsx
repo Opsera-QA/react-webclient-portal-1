3127d4a4279f18092ef0096e98f6b502259824ce
ldap <ldap@opsera.io>
2022-04-01 18:46:09 -0700

    OPL-2888/git-scrapper-metrics-kpi

---
+++ src/components/insights/charts/gitscrapper/data_blocks/overall_low_trend/CoverityIssuesOverallLowTrendDataBlock.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
+import ThreeLineScoreDataBlock from "components/common/metrics/score/ThreeLineScoreDataBlock";
+
+function CoverityIssuesOverallLowTrendDataBlock({ score, icon, className, onSelect, lastScore, iconOverlayBody }) {
+  return (
+    <DataBlockBoxContainer showBorder={true} onClickFunction={onSelect}>
+      <ThreeLineScoreDataBlock
+        className={`${className} p-2`}
+        score={score}
+        topText={"Total Repositories Scanned"}
+        bottomText={"Last Scan: " + lastScore}
+        icon={icon}
+        iconOverlayBody={iconOverlayBody}
+      />
+    </DataBlockBoxContainer>
+  );
+}
+
+CoverityIssuesOverallLowTrendDataBlock.propTypes = {
+  score: PropTypes.number,
+  icon: PropTypes.object,
+  className: PropTypes.string,
+  onSelect: PropTypes.func,
+  lastScore: PropTypes.number,
+  iconOverlayBody: PropTypes.any,
+};
+
+export default CoverityIssuesOverallLowTrendDataBlock;
---
+++ src/components/insights/charts/gitscrapper/data_blocks/overall_medium_trend/CoverityIssuesOverallMediumTrendDataBlock.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
+import ThreeLineScoreDataBlock from "components/common/metrics/score/ThreeLineScoreDataBlock";
+
+function CoverityIssuesOverallMediumTrendDataBlock({ score, icon, className, onSelect, lastScore, iconOverlayBody }) {
+  return (
+    <DataBlockBoxContainer showBorder={true} onClickFunction={onSelect}>
+      <ThreeLineScoreDataBlock
+        className={`${className} p-2`}
+        score={score}
+        topText={"Total Clean Repositories"}
+        bottomText={"Last Scan: " + lastScore}
+        icon={icon}
+        iconOverlayBody = {iconOverlayBody}
+      />
+    </DataBlockBoxContainer>
+  );
+}
+
+CoverityIssuesOverallMediumTrendDataBlock.propTypes = {
+  score: PropTypes.number,
+  icon: PropTypes.object,
+  className: PropTypes.string,
+  onSelect: PropTypes.func,
+  lastScore: PropTypes.number,
+  iconOverlayBody: PropTypes.any,
+};
+
+export default CoverityIssuesOverallMediumTrendDataBlock;
