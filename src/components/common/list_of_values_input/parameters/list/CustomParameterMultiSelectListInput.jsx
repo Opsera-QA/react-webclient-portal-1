fc51a9eb053c023d5df66ee3eec18ff4782623df
Noah Champoux <noah@opsera.io>
2022-04-03 14:37:24 -0500

    OPL-3003 Create New Custom Parameter Pipeline Input Component
    Added new custom parameter entry input that matches new standards

--- src/components/common/list_of_values_input/parameters/list/CustomParameterMultiSelectListInput.jsx
+++ src/components/common/list_of_values_input/parameters/list/CustomParameterMultiSelectListInput.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
-import { faHandshake, faMinusCircle } from "@fortawesome/pro-light-svg-icons";
+import {faHandshake, faMinusCircle} from "@fortawesome/pro-light-svg-icons";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { AuthContext } from "contexts/AuthContext";
@@ -8,15 +8,17 @@ import parametersActions from "components/inventory/parameters/parameters-action
 import axios from "axios";
 import InfoText from "components/common/inputs/info_text/InfoText";
 import StandaloneSelectInput from "components/common/inputs/select/StandaloneSelectInput";
-import { isMongoDbId } from "components/common/helpers/mongo/mongoDb.helpers";
+import {isMongoDbId} from "components/common/helpers/mongo/mongoDb.helpers";
 import InfoContainer from "components/common/containers/InfoContainer";
 import H5FieldSubHeader from "components/common/fields/subheader/H5FieldSubHeader";
 import NewRecordButton from "components/common/buttons/data/NewRecordButton";
-import { hasStringValue } from "components/common/helpers/string-helpers";
-import { parseError } from "components/common/helpers/error-helpers";
+import {hasStringValue} from "components/common/helpers/string-helpers";
+import {parseError} from "components/common/helpers/error-helpers";
 import InputContainer from "components/common/inputs/InputContainer";
+import {Button} from "react-bootstrap";
 import CustomParameterInputRow from "components/common/list_of_values_input/parameters/list/CustomParameterInputRow";
-import ButtonBase from "components/common/buttons/ButtonBase";
+import SyncTerraformOutputParametersButton
+  from "components/common/list_of_values_input/parameters/list/SyncTerraformOutputParametersButton";
 
 function CustomParameterMultiSelectListInput({
   model,
@@ -27,6 +29,7 @@ function CustomParameterMultiSelectListInput({
   titleText,
   disabled,
   terraformStepId,
+  pipelineId,
   className,
   height,
 }) {
@@ -103,7 +106,8 @@ function CustomParameterMultiSelectListInput({
   const updateValue = (newParameterList) => {
     if (setDataFunction) {
       setDataFunction(fieldName, newParameterList);
-    } else {
+    }
+    else {
       validateAndSetData(newParameterList);
     }
   };
@@ -111,7 +115,7 @@ function CustomParameterMultiSelectListInput({
   const addParameter = () => {
     setError("");
     const currentParameters = model?.getArrayData(fieldName);
-    const matchingParameter = currentParameters.find((parameter) => parameter?._id === selectedParameter?._id);
+    const matchingParameter = currentParameters.find((parameter) => parameter?.parameterId === selectedParameter?._id);
 
     if (matchingParameter) {
       setError("Existing parameters can not be added again");
@@ -122,6 +126,7 @@ function CustomParameterMultiSelectListInput({
       parameterName: selectedParameter?.name,
       parameterId: selectedParameter?._id,
     });
+
     updateValue(currentParameters);
     setSelectedParameter(undefined);
   };
@@ -176,18 +181,18 @@ function CustomParameterMultiSelectListInput({
         <Col xs={5}>
           <span>Parameter Origin</span>
         </Col>
-        <Col xs={2} />
+        <Col xs={2}/>
       </div>
     );
   };
 
   const syncTerraformStepParameters = (terraformStepParameters) => {
     const currentParameters = model?.getArrayData(fieldName);
-    const newArray = currentParameters?.filter((parameter) => parameter?.outputKey == null);
+    const newArray = currentParameters?.filter((parameter) => hasStringValue(parameter?.outputKey) !== true);
 
-    if (Array.isArray(terraformStepParameters) && terraformStepParameters > 0) {
+    if (Array.isArray(terraformStepParameters) && terraformStepParameters.length > 0) {
       terraformStepParameters.forEach((terraformParameter) => {
-        if (terraformParameter?.outputKey) {
+        if (hasStringValue(terraformParameter?.outputKey) === true) {
           newArray.push(terraformParameter);
         }
       });
@@ -199,42 +204,17 @@ function CustomParameterMultiSelectListInput({
   const getTerraformButton = () => {
     if (isMongoDbId(terraformStepId) === true) {
       return (
-        <div className={"mr-1"}>
-          <div style={{ minWidth: "275px" }}>
-            <ButtonBase
-              variant={"primary"}
-              disabled={disabled || hasMaximumItems() || isPotentialValueADuplicate()}
-              onClickFunction={syncTerraformStepParameters}
-              buttonText={"Sync Terraform Output Parameters"}
-            />
-          </div>
+        <div className={"my-2"}>
+          <SyncTerraformOutputParametersButton
+            syncTerraformStepParameters={syncTerraformStepParameters}
+            terraformStepId={terraformStepId}
+            pipelineId={pipelineId}
+            disabled={disabled || hasMaximumItems() || isPotentialValueADuplicate()}
+          />
         </div>
       );
     }
   };
-
-  // const getRefreshButton = () => {
-  //   if (isMongoDbId(terraformStepId) === true) {
-  //     return (
-  //       <OverlayTrigger
-  //         trigger="hover"
-  //         rootClose
-  //         placement="top"
-  //         overlay={
-  //           <Popover id="popover-basic" style={{ maxWidth: "500px" }}>
-  //             <Popover.Content>
-  //               <div className="text-muted mb-2">
-  //                 Refresh Terraform Output Parameters
-  //               </div>
-  //             </Popover.Content>
-  //           </Popover>
-  //         }
-  //       >
-  //         <IconBase icon={faSync} className={"fa-pull-right pointer pr-2 mt-1 pl-0"} onClickFunction={() => refreshParameters()}/>
-  //       </OverlayTrigger>
-  //     );
-  //   }
-  // };
 
   const getTerraformStepText = () => {
     if (isMongoDbId(terraformStepId) === true) {
@@ -242,8 +222,9 @@ function CustomParameterMultiSelectListInput({
         <div>
           <H5FieldSubHeader subheaderText={"Pipelines with Terraform Steps"} />
           <div>
-            If the <strong>Use Terraform Output</strong> checkbox has been selected, the available parameters will
-            appear in the Parameter selection option with <strong>Terraform Output</strong> as the Parameter Origin.
+            If the <strong>Use Terraform Output</strong> checkbox has been selected, the available parameters
+            can be synced using the Sync Terraform Output Parameters button.
+            Terraform Output Parameters will appear in the Parameter selection option with <strong>Terraform Output</strong> as the Parameter Origin.
           </div>
           <div>They must use the same syntax mentioned above in order to be used in the commands.</div>
         </div>
@@ -262,13 +243,13 @@ function CustomParameterMultiSelectListInput({
     }
 
     const currentParameters = model?.getArrayData(fieldName);
-    const foundParameter = currentParameters.find((parameter) => parameter.name === selectedParameter?.parameterName);
+    const foundParameter = currentParameters.find((parameter) => parameter?.parameterName === selectedParameter?.name);
     return foundParameter != null;
   };
 
   const getErrorMessage = () => {
     if (isPotentialValueADuplicate() === true) {
-      return `The entered Parameter is a duplicate. Duplicate Parameters are not allowed.`;
+      return (`The entered Parameter is a duplicate. Duplicate Parameters are not allowed.`);
     }
 
     if (error) {
@@ -282,7 +263,7 @@ function CustomParameterMultiSelectListInput({
 
   const getInfoText = () => {
     if (hasMaximumItems() === true) {
-      return "You have reached the maximum allowed number of parameters. Please remove one to add another.";
+      return ("You have reached the maximum allowed number of Parameters. Please remove one to add another.");
     }
   };
 
@@ -292,12 +273,12 @@ function CustomParameterMultiSelectListInput({
 
   return (
     <InputContainer className={className}>
-      <InfoContainer titleText={titleText} titleIcon={titleIcon}>
+      <InfoContainer
+        titleText={titleText}
+        titleIcon={titleIcon}
+      >
         <div
-          style={{
-            height: height,
-            maxHeight: height,
-          }}
+          style={{height: height, maxHeight: height}}
           className={"scroll-y"}
         >
           {getFieldBody()}
@@ -305,7 +286,6 @@ function CustomParameterMultiSelectListInput({
       </InfoContainer>
       <div className={"mt-3"}>
         <div className={"d-flex"}>
-          {getTerraformButton()}
           <StandaloneSelectInput
             selectOptions={parametersList}
             valueField={"_id"}
@@ -316,7 +296,7 @@ function CustomParameterMultiSelectListInput({
             disabled={disabled}
             dropUp={true}
           />
-          <div className={"ml-2"} style={{ minWidth: "156px" }}>
+          <div className={"ml-2"} style={{minWidth: "156px"}}>
             <NewRecordButton
               size={"md"}
               variant={"primary"}
@@ -334,6 +314,7 @@ function CustomParameterMultiSelectListInput({
         errorMessage={getErrorMessage()}
         customMessage={getInfoText()}
       />
+      {getTerraformButton()}
       <div className={"text-muted my-2"}>
         <div className={"mb-2"}>
           In order to use Opsera Parameters during the step execution, please reference them in the scripts or commands
@@ -341,10 +322,12 @@ function CustomParameterMultiSelectListInput({
           matches an Opsera Parameter name.
         </div>
         <div className={"mb-2"}>
-          All Parameters passed in the commands or scripts must be selected in order for the details to be fetched
-          during runtime.
+          All Parameters passed in the commands or scripts must be selected in
+          order for the details to be fetched during runtime.
         </div>
-        <div>Opsera Parameters are defined under the Parameters tab in Tool Registry.</div>
+        <div>
+          Opsera Parameters are defined under the Parameters tab in Tool Registry.
+        </div>
         {getTerraformStepText()}
       </div>
     </InputContainer>
@@ -360,6 +343,7 @@ CustomParameterMultiSelectListInput.propTypes = {
   titleText: PropTypes.string,
   disabled: PropTypes.bool,
   terraformStepId: PropTypes.string,
+  pipelineId: PropTypes.string,
   className: PropTypes.string,
   height: PropTypes.string,
 };
