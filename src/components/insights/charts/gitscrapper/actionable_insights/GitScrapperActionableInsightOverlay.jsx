9a20c2027ce0899b37bd523f4d8925d1019a7f36
ldap <ldap@opsera.io>
2022-04-04 00:16:48 -0700

    OPL-2888/git-scrapper-metrics-kpi - actionable insights

---
+++ src/components/insights/charts/gitscrapper/actionable_insights/GitScrapperActionableInsightOverlay.jsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+// import { Col, Row } from "react-bootstrap";
+import Model from "core/data_model/model";
+import { AuthContext } from "contexts/AuthContext";
+import { faExternalLink, faTable } from "@fortawesome/pro-light-svg-icons";
+import axios from "axios";
+import { DialogToastContext } from "contexts/DialogToastContext";
+import chartsActions from "components/insights/charts/charts-actions";
+import { useHistory } from "react-router-dom";
+import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
+import GitScrapperActionableInsightTable from "./GitScrapperActionableInsightTable";
+import GitScrapperActionableDataBlockContainers from "./GitScrapperActionableDataBlockContainers";
+// import { getTimeDisplay } from "components/insights/charts/sonar/sonar_ratings/data_blocks/sonar-ratings-pipeline-utility";
+import actionableInsightsGenericChartFilterMetadata from "components/insights/charts/generic_filters/actionableInsightsGenericChartFilterMetadata";
+import { getMetricFilterValue } from "components/common/helpers/metrics/metricFilter.helpers";
+import MetricDateRangeBadge from "components/common/badges/date/metrics/MetricDateRangeBadge";
+import IconBase from "components/common/icons/IconBase";
+
+function GitScrapperActionableInsightOverlay({ title, gitScrapperSeverity, kpiConfiguration, dashboardData }) {
+  const toastContext = useContext(DialogToastContext);
+  const history = useHistory();
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metrics, setMetrics] = useState([]);
+  const [dataBlockValues, setDataBlockValues] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const [filterModel, setFilterModel] = useState(
+    new Model(
+      { ...actionableInsightsGenericChartFilterMetadata.newObjectFields },
+      actionableInsightsGenericChartFilterMetadata,
+      false
+    )
+  );
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, [JSON.stringify(dashboardData)]);
+  const loadData = async (cancelSource = cancelTokenSource, filterDto = filterModel) => {
+    try {
+      setIsLoading(true);
+      let dashboardTags =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      let dashboardOrgs =
+        dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "organizations")]
+          ?.value;
+      let request = "coverityInsightsDatablocks";
+      let response = await chartsActions.parseConfigurationAndGetChartMetrics(
+        getAccessToken,
+        cancelSource,
+        request,
+        kpiConfiguration,
+        dashboardTags,
+        filterDto,
+        null,
+        dashboardOrgs,
+        null,
+        null,
+        null,
+        null,
+        gitScrapperSeverity
+      );
+
+      // TODO
+
+      console.log('gitScrapperSeverity', gitScrapperSeverity);
+
+      if (gitScrapperSeverity === 'Low') {
+        response['data'] = {"status":200,"status_text":"ES Pipeline Summary Query Results","message":"ES Query Response from Living Connection","data":[{"coverityInsightsDatablocks":{"tool":"Coverity","data":[{"data":[{"length":46,"total_issues":0,"project":"github-integrator","pipeline":"623a5aa91fdb2f0012a3d84a","pipelineName":"Coverity Test Automation DO NOT DELETE Copy","run":2,"timestamp":"2022-03-22T23:52:22.224Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov-ant","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:58:07.433Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov-node1","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:56:10.079Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:53:22.787Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0}],"DataBlocks":[{"_id":1,"totalIssues":0,"totalSecurity":0,"totalQuality":0,"totalVarious":0,"totalProjects":4,"totalScans":52}],"count":[{"count":4}]}],"length":1,"status":200,"status_text":"OK"}}]};
+      } else if (gitScrapperSeverity === 'Medium') {
+        response['data'] = {"status":200,"status_text":"ES Pipeline Summary Query Results","message":"ES Query Response from Living Connection","data":[{"coverityInsightsDatablocks":{"tool":"Coverity","data":[{"data":[{"length":46,"total_issues":0,"project":"github-integrator","pipeline":"623a5aa91fdb2f0012a3d84a","pipelineName":"Coverity Test Automation DO NOT DELETE Copy","run":2,"timestamp":"2022-03-22T23:52:22.224Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov-ant","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:58:07.433Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":2,"project":"cov-node1","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:56:10.079Z","totalIssues":2,"trend":"Neutral","trendNumber":0,"quality_issues":2,"security_issues":0,"various_issues":0},{"length":2,"total_issues":1,"project":"cov","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:53:22.787Z","totalIssues":1,"trend":"Neutral","trendNumber":0,"quality_issues":1,"security_issues":0,"various_issues":0}],"DataBlocks":[{"_id":1,"totalIssues":3,"totalSecurity":0,"totalQuality":3,"totalVarious":0,"totalProjects":4,"totalScans":52}],"count":[{"count":4}]}],"length":1,"status":200,"status_text":"OK"}}]};
+      } else {
+        response['data'] = {"status":200,"status_text":"ES Pipeline Summary Query Results","message":"ES Query Response from Living Connection","data":[{"coverityInsightsDatablocks":{"tool":"Coverity","data":[{"data":[{"length":46,"total_issues":0,"project":"github-integrator","pipeline":"623a5aa91fdb2f0012a3d84a","pipelineName":"Coverity Test Automation DO NOT DELETE Copy","run":2,"timestamp":"2022-03-22T23:52:22.224Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov-ant","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:58:07.433Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov-node1","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:56:10.079Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0},{"length":2,"total_issues":0,"project":"cov","pipeline":"6104118328b45b573c285eba","pipelineName":"Coverity_test","run":72,"timestamp":"2022-01-13T15:53:22.787Z","totalIssues":0,"trend":"Neutral","trendNumber":0,"quality_issues":0,"security_issues":0,"various_issues":0}],"DataBlocks":[{"_id":1,"totalIssues":0,"totalSecurity":0,"totalQuality":0,"totalVarious":0,"totalProjects":4,"totalScans":52}],"count":[{"count":4}]}],"length":1,"status":200,"status_text":"OK"}}]};
+      }
+
+      
+
+      let dataObject = response?.data ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.data : [];
+      let dataCount = response?.data
+        ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.count[0]?.count
+        : [];
+      let DataBlocks = response?.data
+        ? response?.data?.data[0]?.coverityInsightsDatablocks?.data[0]?.DataBlocks[0]
+        : [];
+      dataObject = dataObject.map((bd, index) => ({
+        ...bd,
+        _blueprint: <IconBase icon={faExternalLink} className={"mr-2"} />,
+      }));
+
+      console.log('response', response);
+      console.log('dataObject', dataObject);
+    
+
+      let newFilterDto = filterDto;
+      newFilterDto.setData("totalCount", dataCount);
+      setFilterModel({ ...newFilterDto });
+      if (isMounted?.current === true && dataObject) {
+        setMetrics(dataObject);
+        setDataBlockValues(DataBlocks);
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const closePanel = () => {
+    toastContext.removeInlineMessage();
+    toastContext.clearOverlayPanel();
+  };
+
+  const getDateRange = () => {
+    const date = getMetricFilterValue(kpiConfiguration?.filters, "date");
+    return <MetricDateRangeBadge startDate={date?.startDate} endDate={date?.endDate} />;
+  };
+
+  return (
+    <FullScreenCenterOverlayContainer
+      closePanel={closePanel}
+      showPanel={true}
+      titleText={title}
+      showToasts={true}
+      titleIcon={faTable}
+      isLoading={isLoading}
+      linkTooltipText={"View Full Blueprint"}
+    >
+      <div className={"p-3"}>
+        {getDateRange()}
+        <GitScrapperActionableDataBlockContainers data={dataBlockValues} level={gitScrapperSeverity} />
+        <GitScrapperActionableInsightTable
+          data={metrics}
+          isLoading={isLoading}
+          loadData={loadData}
+          filterModel={filterModel}
+          setFilterModel={setFilterModel}
+          title={title}
+        />
+        {/* {getFooterDetails()} */}
+      </div>
+    </FullScreenCenterOverlayContainer>
+  );
+}
+
+GitScrapperActionableInsightOverlay.propTypes = {
+  title: PropTypes.string,
+  gitScrapperSeverity: PropTypes.string,
+  kpiConfiguration: PropTypes.object,
+  dashboardData: PropTypes.object,
+};
+
+export default GitScrapperActionableInsightOverlay;
---
+++ src/components/insights/charts/gitscrapper/git-scrapper-actionable-metadata.js
@@ -0,0 +1,33 @@
+const GitScrapperActionableMetadata = {
+  idProperty: "_id",
+  type: "Coverity Issues By Category Table Details",
+  fields: [
+    {
+      label: "Run",
+      id: "runCount",
+    },
+    {
+      label: "Pipeline Id",
+      id: "pipelineId",
+    },
+    {
+      label: "Pipeline Name",
+      id: "pipelineName",
+    },
+    {
+      label: "Stream Name",
+      id: "coverityStreamName",
+    },
+    {
+      label: "Date",
+      id: "timestamp",
+    },
+    {
+      label: "Issues",
+      id: "High",
+    },
+  ],
+  newObjectFields: {},
+};
+
+export default GitScrapperActionableMetadata;
