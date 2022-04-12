e4702cf689a57581a936089c3482316150ba6ed1
Noah Champoux <noah@opsera.io>
2022-04-12 09:34:59 -0500

    OPL-2680 Build Extensible API Integrator Step

--- src/components/common/inputs/endpoints/endpoint/request/headers/endpointRequestHeaderConfiguration.metadata.js
+++ src/components/common/inputs/endpoints/endpoint/request/headers/endpointRequestHeaderConfiguration.metadata.js
@@ -9,7 +9,7 @@ export const endpointRequestHeaderConfigurationMetadata = {
     {
       label: "Authorization Token Custom Parameter",
       id: "authorizationTokenCustomParameterId",
-      formText: `Do not include "Bearer" in the Custom Parameter`,
+      formText: `Custom Parameters are saved in the Tool Registry. Authorization Tokens must be encrypted Custom Parameters. Do not include "Bearer" in the Custom Parameter`,
       isRequiredFunction: (model) => {
         return model?.getData("useAuthorizationToken") === true;
       },
