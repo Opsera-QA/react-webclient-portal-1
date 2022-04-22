7ccd6bcb2cc70b73cbfd3548beea02f9fbd8c9ee
Phanisri Adusumilli <phanisriadusumilli@Phanisris-MacBook-Pro.local>
2022-04-21 23:43:08 -0400

    OPL-3125-made changes to Service Now MTTR to add the fucnitonality to let user handle which chart needs to be shown

---
+++ src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/serviceNowMeanTimeToResolution.metadata.js
@@ -0,0 +1,17 @@
+export const serviceNowMeanTimeToResolutionMetadata = {
+  type: "Service Now Mean Time To Resolution",
+  fields: [
+    {
+      label: "Tags",
+      id: "tags",
+    },
+    {
+      label: "Date Range",
+      id: "date",
+    },
+  ],
+  newObjectFields: {
+    tags: [],
+    date: undefined,
+  }
+};
--- src/components/insights/dashboards/metrics/DashboardMetricOverlayContainer.jsx
+++ src/components/insights/dashboards/metrics/DashboardMetricOverlayContainer.jsx
@@ -23,6 +23,10 @@ import AutomationPercentageMetricEditorPanel
   from "../../charts/automation_percentage/AutomationPercentageMetricEditorPanel";
 import SonarRatingMetricsEditorPanel from "../../charts/sonar/sonar_ratings/SonarRatingMetricsEditorPanel";
 import {kpiIdentifierConstants} from "components/admin/kpi_identifiers/kpiIdentifier.constants";
+import LeadTimeAndReleaseTracebilityEditorPanel
+  from "../../charts/github_actions/data_blocks/LeadTimeAndReleaseTracebilityEditorPanel";
+import ServiceNowMeanTimeToResolutionEditorPanel
+  from "../../charts/servicenow/bar_chart/mean_time_to_resolution/ServiceNowMeanTimeToResolutionEditorPanel";
 
 // TODO: combine with chart settings overlay?
 function DashboardMetricOverlayContainer(
@@ -146,6 +150,33 @@ function DashboardMetricOverlayContainer(
       case kpiIdentifierConstants.KPI_IDENTIFIERS.SONAR_RATINGS:
         return (
           <SonarRatingMetricsEditorPanel
+            metricModel={metricModel}
+            metricFilterModel={metricFilterModel}
+            setMetricFilterModel={setMetricFilterModel}
+            unpackedFilterData={unpackedFilterData}
+          />
+        );
+      case kpiIdentifierConstants.KPI_IDENTIFIERS.ALL_GITHUB_ACTIONS_DATA_BLOCK:
+        return (
+          <AllGithubActionsDataBlockEditorPanel
+            metricModel={metricModel}
+            metricFilterModel={metricFilterModel}
+            setMetricFilterModel={setMetricFilterModel}
+            unpackedFilterData={unpackedFilterData}
+          />
+        );
+      case kpiIdentifierConstants.KPI_IDENTIFIERS.LEAD_TIME_AND_RELEASE_TRACEABILITY:
+        return (
+          <LeadTimeAndReleaseTracebilityEditorPanel
+            metricModel={metricModel}
+            metricFilterModel={metricFilterModel}
+            setMetricFilterModel={setMetricFilterModel}
+            unpackedFilterData={unpackedFilterData}
+          />
+        );
+      case kpiIdentifierConstants.KPI_IDENTIFIERS.SERVICE_NOW_MEAN_TIME_TO_RESOLUTION:
+        return (
+          <ServiceNowMeanTimeToResolutionEditorPanel
             metricModel={metricModel}
             metricFilterModel={metricFilterModel}
             setMetricFilterModel={setMetricFilterModel}
