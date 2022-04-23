dd7bc27975a02bb410887a31c3df8c58cb4f4445
Vignesh Subramanian <vignesh@opsera.io>
2022-04-23 15:29:20 +0530

    apigee tool registry form

---
+++ src/components/inventory/tools/details/identifiers/apigee/ApigeeToolConnectionEditorPanel.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import Col from "react-bootstrap/Col";
+import ToolConfigurationEditorPanelContainer
+  from "components/common/panels/detail_panel_container/tools/ToolConfigurationEditorPanelContainer";
+import Row from "react-bootstrap/Row";
+import toolsActions from "components/inventory/tools/tools-actions";
+import { AuthContext } from "contexts/AuthContext";
+import VaultTextInput from "components/common/inputs/text/VaultTextInput";
+import modelHelpers from "components/common/model/modelHelpers";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
+import axios from "axios";
+import {
+  apigeeToolConnectionMetadata
+} from "components/inventory/tools/details/identifiers/apigee/apigeeToolConnection.metadata";
+import { toolIdentifierConstants } from "components/admin/tools/identifiers/toolIdentifier.constants";
+import ApigeeVersionSelectInput from "components/common/list_of_values_input/tools/apigee/ApigeeVersionSelectInput";
+
+function ApigeeToolConnectionEditorPanel({ toolData }) {
+  const { getAccessToken } = useContext(AuthContext);
+  const [apigeeConfigurationModel, setApigeeConfigurationModel] = useState(undefined);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+    isMounted.current = true;
+
+    loadData().catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async () => {
+    setApigeeConfigurationModel(modelHelpers.parseObjectIntoModel(toolData?.getData("configuration"), apigeeToolConnectionMetadata));
+  };
+
+  const saveApigeeToolConfiguration = async () => {
+    const newConfiguration = apigeeConfigurationModel.getPersistData();
+    newConfiguration.password = await toolsActions.saveThreePartToolPasswordToVaultV3(
+      getAccessToken,
+      cancelTokenSource,
+      toolData?.getMongoDbId(),
+      toolData.getData("tool_identifier"),
+      "password",
+      newConfiguration?.password
+    );
+
+    return await toolsActions.saveToolConfigurationV2(getAccessToken, cancelTokenSource, toolData, newConfiguration);
+  };
+
+  if (apigeeConfigurationModel == null) {
+    return null;
+  }
+
+  return (
+    <ToolConfigurationEditorPanelContainer
+      model={apigeeConfigurationModel}
+      setModel={setApigeeConfigurationModel}
+      persistRecord={saveApigeeToolConfiguration}
+      toolData={toolData}
+      toolConnectionCheckName={toolIdentifierConstants.TOOL_IDENTIFIERS.APIGEE}
+    >
+      <Row>
+        <Col sm={12}>
+          <ApigeeVersionSelectInput
+            model={apigeeConfigurationModel}
+            setModel={setApigeeConfigurationModel}
+            fieldName={"version"}
+          />
+          <TextInputBase
+            dataObject={apigeeConfigurationModel}
+            setDataObject={setApigeeConfigurationModel}
+            fieldName={"zone"}
+          />
+          <TextInputBase
+            dataObject={apigeeConfigurationModel}
+            setDataObject={setApigeeConfigurationModel}
+            fieldName={"userName"}
+          />
+          <VaultTextInput
+            dataObject={apigeeConfigurationModel}
+            setDataObject={setApigeeConfigurationModel}
+            fieldName={"password"}
+          />
+        </Col>
+      </Row>
+    </ToolConfigurationEditorPanelContainer>
+  );
+}
+
+ApigeeToolConnectionEditorPanel.propTypes = {
+  toolData: PropTypes.object,
+};
+
+export default ApigeeToolConnectionEditorPanel;
