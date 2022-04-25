6f70946aad4f47a49991cda1c6f357ccaaa90684
Phanisri Adusumilli <phanisriadusumilli@Phanisris-MacBook-Pro.local>
2022-04-25 12:24:53 -0400

    OPL-3125-created separate file for data point identifiers

---
+++ src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/ServiceNowMeanTimeToResolution_datapoint_identifiers.js
@@ -0,0 +1,7 @@
+export const SERVICE_NOW_MEAN_TIME_TO_RESOLUTION_CONSTANTS = {};
+
+SERVICE_NOW_MEAN_TIME_TO_RESOLUTION_CONSTANTS.SUPPORTED_DATA_POINT_IDENTIFIERS = {
+  MTTR_DATA_POINT: "mttr-in-hours-chart-data-point",
+  NUMBER_OF_INCIDENTS_DATA_POINT: "number-of-incidents-charts-data-point",
+  AVERAGE_MTTR_DATA_BLOCK_DATA_POINT: "average-mttr-data-block-data-point"
+};
