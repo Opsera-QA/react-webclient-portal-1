87d06f6620ce814883b7edbb0ce05b002cad1307
Noah Champoux <noah@opsera.io>
2022-05-10 15:39:16 -0500

    Update serviceNowMeanTimeToResolution.metadata.js

--- src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/serviceNowMeanTimeToResolution.metadata.js
+++ src/components/insights/charts/servicenow/bar_chart/mean_time_to_resolution/serviceNowMeanTimeToResolution.metadata.js
@@ -17,6 +17,6 @@ export const serviceNowMeanTimeToResolutionMetadata = {
   newObjectFields: {
     tags: [],
     date: undefined,
-    "servicenow-priorities": "",
+    "servicenow-priorities": [],
   }
 };
