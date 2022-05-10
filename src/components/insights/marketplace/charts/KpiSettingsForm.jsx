afc9fbe6cf27471d3c737bafc73eb0ebcc7b9a6f
Noah Champoux <noah@opsera.io>
2022-05-10 15:46:38 -0500

    Minor tweaks

--- src/components/insights/marketplace/charts/KpiSettingsForm.jsx
+++ src/components/insights/marketplace/charts/KpiSettingsForm.jsx
@@ -612,8 +612,8 @@ function KpiSettingsForm({
               fieldName={"value"}
               valueField={"value"}
               textField={"text"}
-              setDataObject={setKpiServiceNowPrioritiesFilter}
-              dataObject={kpiServiceNowPrioritiesFilter}
+              setModel={setKpiServiceNowPrioritiesFilter}
+              model={kpiServiceNowPrioritiesFilter}
             />
           </div>
         );
