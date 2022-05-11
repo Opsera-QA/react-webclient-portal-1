8f467379ac049dc94e781481d04ba88aa80f7532
Phanisri Adusumilli <phanisriadusumilli@Phanisris-MacBook-Pro.local>
2022-05-11 11:50:01 -0400

    OPL-3299-adding description for connected Assets tab and adding the date range

--- src/components/insights/connectedAssets/ConnectedAssets.jsx
+++ src/components/insights/connectedAssets/ConnectedAssets.jsx
@@ -236,10 +236,10 @@ function ConnectedAssets() {
   return (
     <ScreenContainer
       navigationTabContainer={<InsightsSubNavigationBar currentTab={"connectedAssets"}/>}
-      pageDescription={``}
+      pageDescription={`An 'Admin Only' page that provides categorized number of assets that are connected and associated to Opsera through different sources.`}
       breadcrumbDestination={"insightsConnectedAssets"}
     >
-      {/*{getConnectedAssetsActionBar()}*/}
+      {getConnectedAssetsActionBar()}
       <ConnectedAssetsDetails dashboardData={dashboardData} setDashboardData={setDashboardData}/>
     </ScreenContainer>
   );
