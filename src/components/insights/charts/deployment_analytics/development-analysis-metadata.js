7b7018aec3099a184a38b47b5d740b270e5bf0c9
Murali Kurma <murali@opsera.io>
2022-05-13 13:48:10 -0700

    removed unnessasary fileds removed

--- src/components/insights/charts/deployment_analytics/development-analysis-metadata.js
+++ src/components/insights/charts/deployment_analytics/development-analysis-metadata.js
@@ -5,14 +5,9 @@ const DeploymentAnalyticsMetadata = {
       {label: "Artifactory Name", id: "artifactoryName"},
       {label: "Pipeline Name", id: "pipelineName"},
       {label: "Run Count", id: "runCount"},
-      //{label: "Time Stamp", id: "timeStamp"},
       {label: "Deployment Status", id: "status"},
       {label: "Version", id: "version"},
-      
       {label: "Environment", id: "destination"},
-      // {label: "Namespace", id: "namespace"},
-      // {label: "Pipeline Id", id: "pipelineId"},
-      // {label: "Metadata Name", id: "metadataName"},
     ],
     newObjectFields: {
     }
