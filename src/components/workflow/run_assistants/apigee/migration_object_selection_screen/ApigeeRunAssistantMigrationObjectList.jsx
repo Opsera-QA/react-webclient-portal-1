246a094bc89e8213aaaa5452c55ba96fa925ecd3
Vignesh Subramanian <vignesh@opsera.io>
2022-05-19 22:59:35 +0530

    apigee bug fix

--- src/components/workflow/run_assistants/apigee/migration_object_selection_screen/ApigeeRunAssistantMigrationObjectList.jsx
+++ src/components/workflow/run_assistants/apigee/migration_object_selection_screen/ApigeeRunAssistantMigrationObjectList.jsx
@@ -47,6 +47,15 @@ const ApigeeRunAssistantMigrationObjectList = (
   };
 
   const processSelectedData = (fieldName, selectedOption) => {
+    if (selectedOption.type === "keyValueMaps") {
+      const dataObj = {...apigeeRunParametersModel};
+      const selectedMigrationObjects = dataObj.getData("selectedMigrationObjects");
+      selectedMigrationObjects.push(selectedOption);
+      dataObj.setData("selectedMigrationObjects", selectedMigrationObjects);
+      setApigeeRunParametersModel({...dataObj});
+      return;
+    }
+
     const newRunParametersModel = new Model({ ...apigeeMigrationObjectMetadata.newObjectFields, ...selectedOption }, apigeeMigrationObjectMetadata, false);
     setMigrationObject({...newRunParametersModel});
     setUpdateVersionMode(true);
