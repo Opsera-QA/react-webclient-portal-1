07e217bf16cb49db2fc96a79b37d44745084b458
Noah Champoux <noah@opsera.io>
2022-06-01 13:00:17 -0500

    Removed Faq screen feature flag

--- src/components/about/faq/Faq.jsx
+++ src/components/about/faq/Faq.jsx
@@ -1,17 +1,10 @@
-import React, {useContext} from "react";
+import React from "react";
 import ScreenContainer from "components/common/panels/general/ScreenContainer";
-import {AuthContext} from "contexts/AuthContext";
 import FrequentlyAskedQuestionsVerticalTabContainer
   from "components/about/faq/FrequentlyAskedQuestionsVerticalTabContainer";
 import FaqSubNavigationBar from "components/about/faq/FaqSubNavigationBar";
 
 const Faq = () => {
-  const { featureFlagHideItemInProd } = useContext(AuthContext);
-
-  if (featureFlagHideItemInProd() !== false) {
-    return null;
-  }
-
   return (
     <ScreenContainer
       navigationTabContainer={<FaqSubNavigationBar currentTab={"all"} />}
