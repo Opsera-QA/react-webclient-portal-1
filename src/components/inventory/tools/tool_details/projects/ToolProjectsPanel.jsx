7032289ece89bba09871c2283fafac969d94d5d0
Christina Bowers <christina@opsera.io>
2022-06-08 09:50:53 -0400

    OPL3215 argo repository and projects summary panel are inline

--- src/components/inventory/tools/tool_details/projects/ToolProjectsPanel.jsx
+++ src/components/inventory/tools/tool_details/projects/ToolProjectsPanel.jsx
@@ -4,9 +4,9 @@ import LoadingDialog from "components/common/status_notifications/loading";
 import MessageField from "components/common/fields/text/MessageField";
 import DetailPanelContainer from "components/common/panels/detail_panel_container/DetailPanelContainer";
 import JiraToolProjectsPanel from "components/inventory/tools/tool_details/tool_jobs/jira/projects/JiraToolProjectsPanel";
-import ArgoProject from "../tool_jobs/argo/projects/ArgoProject";
+import ArgoToolProjectsPanel from "../tool_jobs/argo/projects/ArgoToolProjectsPanel";
 import {toolIdentifierConstants} from "components/admin/tools/identifiers/toolIdentifier.constants";
-
+import SnaplogicProjectsPanel from "../tool_jobs/snaplogic/projects/SnaplogicProjectsPanel";
 function ToolProjectsPanel({ toolData, loadData, isLoading }) {
   const getToolProjectsPanel = () => {
     switch (toolData?.getData("tool_identifier")) {
@@ -19,7 +19,9 @@ function ToolProjectsPanel({ toolData, loadData, isLoading }) {
         />
       );
     case toolIdentifierConstants.TOOL_IDENTIFIERS.ARGO:
-      return <ArgoProject isLoading={isLoading} toolData={toolData} toolActions={toolData?.getData("projects")} loadData={loadData}/>;
+      return <ArgoToolProjectsPanel isLoading={isLoading} toolData={toolData} toolActions={toolData?.getData("projects")} loadData={loadData}/>;
+    case toolIdentifierConstants.TOOL_IDENTIFIERS.SNAPLOGIC:
+      return <SnaplogicProjectsPanel isLoading={isLoading} toolData={toolData} toolActions={toolData?.getData("projects")} loadData={loadData}/>;
     default:
       return <LoadingDialog message={"Loading Tool Projects"} size={"sm"} />;
     }
