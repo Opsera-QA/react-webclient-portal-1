25e2f5923e3cabed470dcb91ec88254a6e883f99
Noah Champoux <noah@opsera.io>
2022-06-10 08:05:24 -0500

    OPL-3816 Pipeline_Step type is editable

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/StepConfigurationTypeSelectInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/StepConfigurationTypeSelectInput.jsx
@@ -1,85 +1,32 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import SelectInputBase from "../../../../../common/inputs/select/SelectInputBase";
-import { AuthContext } from "../../../../../../contexts/AuthContext";
-import axios from "axios";
-import { toolCategoryActions } from "../../../../../admin/tools/categories/toolCategory.actions";
+import ToolTypeSelectInput from "components/common/list_of_values_input/admin/tools/ToolTypeSelectInput";
+import { hasStringValue } from "components/common/helpers/string-helpers";
 
-function StepConfigurationTypeSelectInput({ dataObject, setDataObject, disabled, fieldName, textField, valueField }) {
-  const { getAccessToken } = useContext(AuthContext);
-  const [toolTypes, setToolTypes] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
-
-  useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-    setError(undefined);
-
-    loadData(source).catch((error) => {
-      if (isMounted?.current === true) {
-        setError(error);
-      }
-    });
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
-  }, []);
-
-  const loadData = async (cancelSource = cancelTokenSource) => {
-    try {
-      setIsLoading(true);
-      await loadToolTypes(cancelSource);
-    } catch (error) {
-      if (isMounted?.current === true) {
-        setError(error);
-      }
-    } finally {
-      if (isMounted?.current === true) {
-        setIsLoading(false);
-      }
-    }
-  };
-
-  const loadToolTypes = async (cancelSource = cancelTokenSource) => {
-    const response = await toolCategoryActions.getToolTypesV2(getAccessToken, cancelSource, false);
-
-    const toolTypes = response?.data?.data;
-
-    if (isMounted.current === true && Array.isArray(toolTypes)) {
-      setToolTypes(toolTypes);
-    }
-  };
-
+function StepConfigurationTypeSelectInput(
+  {
+    model,
+    setModel,
+    fieldName,
+    textField,
+    valueField,
+  }) {
   return (
-    <SelectInputBase
+    <ToolTypeSelectInput
       fieldName={fieldName}
-      dataObject={dataObject}
-      setDataObject={setDataObject}
-      selectOptions={toolTypes}
-      placeholderText={"Select a Step Type"}
-      disabled={disabled}
-      busy={isLoading}
+      model={model}
+      setModel={setModel}
+      disabled={hasStringValue(model?.getData(fieldName)) && model?.isChanged(fieldName) !== true}
       valueField={valueField}
       textField={textField}
-      error={error}
+      includeInactive={false}
     />
   );
 }
 
 StepConfigurationTypeSelectInput.propTypes = {
-  dataObject: PropTypes.object,
-  setDataObject: PropTypes.func,
-  disabled: PropTypes.bool,
+  model: PropTypes.object,
+  setModel: PropTypes.func,
   className: PropTypes.string,
   fieldName: PropTypes.string,
   textField: PropTypes.string,
