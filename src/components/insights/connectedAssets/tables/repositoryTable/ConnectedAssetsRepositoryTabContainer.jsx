89432fc9784ad6b64a63a06cc7f3da1e70f3cc00
Phanisri Adusumilli <phanisriadusumilli@Phanisris-MacBook-Pro.local>
2022-06-19 16:00:36 -0400

    OPL-3354-adding table details for Connected Assets Repositories, altering pagination container to support pagination for VanityTab

---
+++ src/components/insights/connectedAssets/tables/repositoryTable/ConnectedAssetsRepositoryTabContainer.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
+import { faDiagramSuccessor, faListCheck, faAnalytics} from "@fortawesome/pro-light-svg-icons";
+import TabPanelContainer from "components/common/panels/general/TabPanelContainer";
+import CustomTabContainer from "components/common/tabs/CustomTabContainer";
+import CustomTab from "components/common/tabs/CustomTab";
+import ConnectedAssetsRepositoriesPipelinesTab from "./pipelines/ConnectedAssetsRepositoriesPipelinesTab";
+import ConnectedAssetsRepositoriesTasksTab from "./tasks/ConnectedAssetsRepositoriesTasksTab";
+import ConnectedAssetsRepositoriesAnalyticsTab from "./analytics/ConnectedAssetsRepositoriesAnalyticsTab";
+
+function ConnectedAssetsRepositoryTabContainer({ dashboardData }) {
+  const [activeTab, setActiveTab] = useState("pipelines");
+
+  const getBody = () => {
+    if (activeTab == "pipelines") {
+      return (
+        <ConnectedAssetsRepositoriesPipelinesTab
+          dashboardData={dashboardData}
+          icon={faDiagramSuccessor}
+        />
+      );
+    } else if (activeTab == "tasks") {
+      return (
+        <ConnectedAssetsRepositoriesTasksTab
+          dashboardData={dashboardData}
+          icon={faListCheck}
+        />
+      );
+    } else if (activeTab == "analytics") {
+      return (
+        <ConnectedAssetsRepositoriesAnalyticsTab
+          dashboardData={dashboardData}
+          icon={faAnalytics}
+        />
+      );
+    }
+  };
+
+  const handleTabClick = (tabSelection) => (e) => {
+    e.preventDefault();
+    setActiveTab(tabSelection);
+  };
+
+  const getTabContainer = () => {
+    return (
+      <CustomTabContainer>
+        <CustomTab
+          activeTab={activeTab}
+          tabText={"Pipelines"}
+          handleTabClick={handleTabClick}
+          tabName={"pipelines"}
+          icon={faDiagramSuccessor}
+        />
+        <CustomTab
+          activeTab={activeTab}
+          tabText={"Tasks"}
+          handleTabClick={handleTabClick}
+          tabName={"tasks"}
+          icon={faListCheck}
+        />
+        <CustomTab
+          activeTab={activeTab}
+          tabText={"Analytics"}
+          handleTabClick={handleTabClick}
+          tabName={"analytics"}
+          icon={faAnalytics}
+        />
+      </CustomTabContainer>
+    );
+  };
+
+  return (
+      <div className={"p-3"}>
+        <TabPanelContainer currentView={getBody()} tabContainer={getTabContainer()} />
+      </div>
+  );
+}
+
+ConnectedAssetsRepositoryTabContainer.propTypes = {
+  dashboardData: PropTypes.object,
+};
+
+export default ConnectedAssetsRepositoryTabContainer;
