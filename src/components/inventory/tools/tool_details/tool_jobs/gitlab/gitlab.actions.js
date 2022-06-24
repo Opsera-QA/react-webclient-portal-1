a4a230ae607460ee8ec05761c4799f84398ad679
Syed Faseeh Uddin <faseeh@opsera.io>
2022-06-24 12:19:40 +0000

    MASTER PR : OPL-4382 : Added support for search gitlab repos and branch

--- src/components/inventory/tools/tool_details/tool_jobs/gitlab/gitlab.actions.js
+++ src/components/inventory/tools/tool_details/tool_jobs/gitlab/gitlab.actions.js
@@ -7,11 +7,34 @@ gitlabActions.getRepositoriesFromGitlabInstanceV2 = async (getAccessToken, cance
   return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl);
 };
 
+gitlabActions.getRepositoriesFromGitlabInstanceV3 = async (getAccessToken, cancelTokenSource, searchTerm, toolId) => {
+  const apiUrl = `/tools/${toolId}/gitlab/v2/repositories`;
+
+  const urlParams = {
+    params: {
+      searchTerm: searchTerm ? searchTerm : "",
+    }
+  };
+
+  return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl, urlParams);
+};
+
 gitlabActions.getBranchesFromGitlabInstanceV2 = async (getAccessToken, cancelTokenSource, toolId, repositoryId) => {
   const apiUrl = `/tools/${toolId}/gitlab/branches`;
   const queryParams = {
     params: {
       repositoryId: repositoryId,
+    },
+  };
+  return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl, queryParams);
+};
+
+gitlabActions.getBranchesFromGitlabInstanceV3 = async (getAccessToken, cancelTokenSource, toolId, repositoryId, searchTerm) => {
+  const apiUrl = `/tools/${toolId}/gitlab/v2/branches`;
+  const queryParams = {
+    params: {
+      repositoryId: repositoryId,
+      searchTerm: searchTerm ? searchTerm : "",
     },
   };
   return await baseActions.apiGetCallV2(getAccessToken, cancelTokenSource, apiUrl, queryParams);
