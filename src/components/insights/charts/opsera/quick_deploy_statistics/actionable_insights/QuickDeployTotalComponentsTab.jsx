4dd70e17d052aea12e4c750132d5ac462b35cfd7
Noah Champoux <noah@opsera.io>
2022-06-30 09:05:23 -0500

    Tweaks for Divyesha

--- src/components/insights/charts/opsera/quick_deploy_statistics/actionable_insights/QuickDeployTotalComponentsTab.jsx
+++ src/components/insights/charts/opsera/quick_deploy_statistics/actionable_insights/QuickDeployTotalComponentsTab.jsx
@@ -1,49 +1,59 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import VanitySetTabAndViewContainer from "components/common/tabs/vertical_tabs/VanitySetTabAndViewContainer";
-import VanitySetTabViewContainer from "components/common/tabs/vertical_tabs/VanitySetTabViewContainer";
-import VanitySetTabView from "components/common/tabs/vertical_tabs/VanitySetTabView";
-import GithubCommitsVerticalTabContainer from "components/insights/charts/github/pie_chart/commits_statistics/actionable_insights/GithubCommitsVerticalTabContainer";
 import QuickDeployTotalComponentsActionableTable from "./QuickDeployTotalComponentsActionableTable";
 import QuickDeployVerticalTabContainer from "../QuickDeployVerticalTabContainer";
+import { hasStringValue } from "components/common/helpers/string-helpers";
 
-function QuickDeployTotalComponentsTab({data, components, dashboardData, kpiConfiguration,icon}) {
+function QuickDeployTotalComponentsTab({ data, components, dashboardData, kpiConfiguration, icon }) {
+  const [activeTab, setActiveTab] = useState(undefined);
 
-    const getTabContentContainer = () => {
+  useEffect(() => {
+    if (Array.isArray(components) && components.length > 0) {
+      setActiveTab(components[0]);
+    }
+  }, [components]);
+
+  const getCurrentView = () => {
+    if (hasStringValue(activeTab) === true) {
+      const component = components[activeTab];
+
+      if (component) {
         return (
-            <VanitySetTabViewContainer className={"mb-3"}>
-                {components.map((item,index)=>(
-                    <VanitySetTabView key={index} tabKey={item} >
-                        <QuickDeployTotalComponentsActionableTable
-                            data={data}
-                            component={item}
-                            dashboardData={dashboardData}
-                            kpiConfiguration={kpiConfiguration}
-                            icon={icon}
-                        />
-                    </VanitySetTabView>
-                ))}
-
-            </VanitySetTabViewContainer>
+          <QuickDeployTotalComponentsActionableTable
+            data={data}
+            component={component}
+            dashboardData={dashboardData}
+            kpiConfiguration={kpiConfiguration}
+            icon={icon}
+          />
         );
-    };
+      }
+    }
+  };
 
-
-    return (
-        <VanitySetTabAndViewContainer
-            title={`Quick Deploy Total Components Report`}
-            defaultActiveKey={components && Array.isArray(components)}
-            verticalTabContainer={<QuickDeployVerticalTabContainer highestMergesMetric={components} />}
-            currentView={getTabContentContainer()}
+  return (
+    <VanitySetTabAndViewContainer
+      title={`Quick Deploy Total Components Report`}
+      defaultActiveKey={Array.isArray(components) && components.length > 0 ? components[0] : undefined}
+      verticalTabContainer={
+        <QuickDeployVerticalTabContainer
+          highestMergesMetrics={components}
+          activeTab={activeTab}
+          handleTabClick={setActiveTab}
         />
-    );
-
+      }
+      currentView={getCurrentView()}
+    />
+  );
 }
+
 QuickDeployTotalComponentsTab.propTypes = {
-    data: PropTypes.array,
-    components: PropTypes.array,
-    dashboardData: PropTypes.object,
-    kpiConfiguration: PropTypes.object,
-    icon: PropTypes.object
+  data: PropTypes.array,
+  components: PropTypes.array,
+  dashboardData: PropTypes.object,
+  kpiConfiguration: PropTypes.object,
+  icon: PropTypes.object,
 };
+
 export default QuickDeployTotalComponentsTab;
