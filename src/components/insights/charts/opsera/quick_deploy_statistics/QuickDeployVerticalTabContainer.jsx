96c24f75f30a772945afcd0a9b6463b482bbfbae
Divyesha Malhotra <divyeshamalhotra@Divyeshas-MacBook-Pro.local>
2022-06-30 12:23:59 -0400

    changes

--- src/components/insights/charts/opsera/quick_deploy_statistics/QuickDeployVerticalTabContainer.jsx
+++ src/components/insights/charts/opsera/quick_deploy_statistics/QuickDeployVerticalTabContainer.jsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import VanitySetVerticalTab from "components/common/tabs/vertical_tabs/VanitySetVerticalTab";
 import VanitySetVerticalTabContainer from "components/common/tabs/vertical_tabs/VanitySetVerticalTabContainer";
 import { faFileCode } from "@fortawesome/pro-light-svg-icons";
 
-function QuickDeployVerticalTabContainer({ highestMergesMetric }) {
-    const [activeTab, setActiveTab] = useState();
-    useEffect(() => {
-        if (highestMergesMetric && Array.isArray(highestMergesMetric) && highestMergesMetric[0]) {
-            setActiveTab(highestMergesMetric[0]);
-        }
-    }, [highestMergesMetric]);
-
+function QuickDeployVerticalTabContainer({ activeTab, handleTabClick, highestMergesMetrics }) {
     return (
         <VanitySetVerticalTabContainer className={"h-100"}>
-            {highestMergesMetric.map((item, index) => {
-                return <VanitySetVerticalTab key={index} icon={faFileCode} tabText={item} tabName={item} />;
+            {highestMergesMetrics.map((item, index) => {
+                return (
+                    <VanitySetVerticalTab
+                        key={index}
+                        activeTab={activeTab}
+                        handleTabClick={handleTabClick}
+                        icon={faFileCode}
+                        tabText={item}
+                        tabName={`${index}`}
+                    />
+                );
             })}
         </VanitySetVerticalTabContainer>
     );
 }
+
 QuickDeployVerticalTabContainer.propTypes = {
-    highestMergesMetric: PropTypes.array,
+    highestMergesMetrics: PropTypes.array,
+    handleTabClick: PropTypes.func,
+    activeTab: PropTypes.string,
 };
 export default QuickDeployVerticalTabContainer;
