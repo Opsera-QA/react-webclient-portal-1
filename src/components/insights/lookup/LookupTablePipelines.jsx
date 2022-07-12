c51a8f6c0ae9db692ec6b69e3515b46f83a4091e
Matthew Greci <mgreci@opsera.io>
2022-07-11 22:59:45 -0600

    move set data outside component

--- src/components/insights/lookup/LookupTablePipelines.jsx
+++ src/components/insights/lookup/LookupTablePipelines.jsx
@@ -5,63 +5,62 @@ import {getField} from "components/common/metadata/metadata-helpers";
 import {getTableTextColumn} from "components/common/table/table-column-helpers";
 import MakeupTableBase from 'components/common/table/makeup/MakeupTableBase';
 
+const fields = [
+  {
+    label: "Pipeline",
+    id: "pipeline"
+  },
+  {
+    label: "Deploy Count",
+    id: "deploy_count",
+  },
+  {
+    label: "Validations Passed",
+    id: "validations_passed",
+  },
+  {
+    label: "Failed",
+    id: "validations_failed",
+  },
+  {
+    label: "Unit Tests Passed",
+    id: "unit_tests_passed",
+  },
+  {
+    label: "Failed",
+    id: "unit_tests_failed",
+  },
+  {
+    label: "Deployed",
+    id: "last_deploy"
+  }
+];
+
+const initialState = {
+  pageIndex: 0,
+  sortBy: [
+    {
+      id: "deploy_count",
+      desc: true,
+    },
+    {
+      id: "validations_passed",
+      desc: true,
+    },
+    {
+      id: "unit_tests_passed",
+      desc: true,
+    },
+    {
+      id: "last_deploy",
+      desc: true,
+    }
+  ],
+};
+
 const LookupTablePipelines = ({
   data
 }) => {
-
-  const fields = [
-    {
-      label: "Pipeline",
-      id: "pipeline"
-    },
-    {
-      label: "Deploy Count",
-      id: "deploy_count",
-    },
-    {
-      label: "Validations Passed",
-      id: "validations_passed",
-    },
-    {
-      label: "Failed",
-      id: "validations_failed",
-    },
-    {
-      label: "Unit Tests Passed",
-      id: "unit_tests_passed",
-    },
-    {
-      label: "Failed",
-      id: "unit_tests_failed",
-    },
-    {
-      label: "Deployed",
-      id: "last_deploy"
-    }
-  ];
-
-  const initialState = {
-    pageIndex: 0,
-    sortBy: [
-      {
-        id: "deploy_count",
-        desc: true,
-      },
-      {
-        id: "validations_passed",
-        desc: true,
-      },
-      {
-        id: "unit_tests_passed",
-        desc: true,
-      },
-      {
-        id: "last_deploy",
-        desc: true,
-      }
-    ],
-  };
-  
   const columns = useMemo(() => [
     getTableTextColumn(getField(fields, "pipeline"), "no-wrap-inline"),
     getTableTextColumn(getField(fields, "deploy_count")),
