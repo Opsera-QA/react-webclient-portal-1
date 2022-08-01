11c5a2c33a1b67dfe91c2d4824726d7d41f96c58
Suriya <suriya@Suriyas-MacBook-Pro.local>
2022-08-01 12:39:07 +0530

    unit test jest in progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, shallow,render } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallowToJson  } from 'enzyme-to-json';
+configure({adapter: new Adapter()});
+
+jest.mock("@okta/okta-signin-widget", () => {
+  return {
+    OktaSignIn: () => {
+      return '';
+    }
+  };
+});
+
+jest.mock("react-syntax-highlighter/dist/esm/styles/hljs/docco", () => {
+  return {};
+});
+
+jest.mock("react-syntax-highlighter/dist/esm/languages/hljs/xml", () => {
+  return '';
+});
+
+jest.mock("react-syntax-highlighter", () => {
+  return {
+    Light: {
+      registerLanguage : () => ''
+    }
+
+  };
+});
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = render(<App/>);
 });
