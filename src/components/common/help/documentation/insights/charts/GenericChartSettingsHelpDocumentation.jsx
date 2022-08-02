f750ea47a0a1b916f253efbf0963505da924d9fa
Christina Bowers <christina@opsera.io>
2022-08-02 10:39:04 -0400

    OPL2132 update kpi settings help panel master

--- src/components/common/help/documentation/insights/charts/GenericChartSettingsHelpDocumentation.jsx
+++ src/components/common/help/documentation/insights/charts/GenericChartSettingsHelpDocumentation.jsx
@@ -9,12 +9,16 @@ function GenericChartSettingsHelpDocumentation({closeHelpPanel}) {
       closeHelpPanel={closeHelpPanel}
     >
       <div>
-      <div className={"mb-1 ml-2"}>Change KPI display name and set filters and date range for the selected KPI. To apply the settings and view the KPI with the reflected data, click the <b>Save</b> button. To remove any selected settings, select <b>Clear Value</b> in respective field.</div>
+      <div className={"mb-1 ml-2"}>Set custom filters and date range for the selected KPI. To apply the settings and view the KPI with the reflected data, click <b>Save</b>. To remove any selected settings, select <b>Clear Value</b> in respective field.</div>
         <ul style={{listStyleType: "none"}}>
-          <li><b>Delete KPI</b> - To remove the KPI from this dashboard, click the <b>Delete KPI</b> button. Data cannot be recovered once this KPI is deleted.</li>
-          <li><b>Reset KPI</b> - Reset the KPI name and/or internal properties. When new features are added, sometimes the internal data needs to be reset to enable those features. Data cannot be recovered once this KPI is reset. </li>
-          <li><b>KPI Tags</b> - Enable toggle to apply tags to selected KPI.</li>
-          <li><b>Dashboard Tags</b> - Enable toggle to apply dashboard tags to this KPI. Dashboard tags are applied in Dashboard Settings.</li>
+          <li><b>Name</b> - Customize KPI display name. </li>
+          <li><b>Use KPI Tags</b> - Enable toggle to apply tags to selected KPI.</li>
+          <li><b>Use Dashboard Tags</b> - Enable toggle to apply dashboard tags to this KPI. Dashboard tags are applied in Dashboard filter.</li>
+          <li><b>Tags</b> - Any associated tags can be applied to the KPI to allow for more targeted analytics.</li>
+          <li><b>Date Range</b> - Provide a Start Date and End Date to apply to KPI.</li>
+          <li><b>Delete KPI</b> - To remove the KPI from this dashboard, click the <b>Delete KPI</b> button. Once KPI is deleted the data cannot be recovered.</li>
+          <li><b>Reset KPI</b> - When new features are added, the internal data may need to be reset in order for changes to be enabled. Data cannot be recovered once this KPI is reset. </li>
+          <li><b>Data Point Settings</b> - Provide Strategic Criteria to be triggered upon metric data success, warning and failure.</li>
         </ul>
       </div>
     </HelpDocumentationContainer>
