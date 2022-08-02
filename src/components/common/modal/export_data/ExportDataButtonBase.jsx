72b5cd1a0e156046f352a6f4ccde32784f58d4d8
Christina Bowers <christina@opsera.us>
2022-08-02 21:26:20 +0000

    OPL5198 export task log modal to master

---
+++ src/components/common/modal/export_data/ExportDataButtonBase.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
+import Button from "react-bootstrap/Button";
+import IconBase from "components/common/icons/IconBase";
+import { faFileDownload } from "@fortawesome/pro-light-svg-icons";
+
+function ExportDataButtonBase(
+  {
+    className,
+    isLoading,
+    launchOverlayFunction,
+  }) {
+  return (
+    <TooltipWrapper innerText={"Export"}>
+      <div className={className}>
+        <Button
+          variant={"outline-primary"}
+          size={"sm"}
+          disabled={isLoading}
+          onClick={launchOverlayFunction}
+        >
+          <span><IconBase icon={faFileDownload} /></span>
+        </Button>
+      </div>
+    </TooltipWrapper>
+  );
+}
+
+ExportDataButtonBase.propTypes = {
+  className: PropTypes.string,
+  launchOverlayFunction: PropTypes.func,
+  isLoading: PropTypes.bool,
+};
+
+export default ExportDataButtonBase;
