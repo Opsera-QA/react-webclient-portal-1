04060d077075f92c97b2f6d22cd4ad54d50d721f
Christina Bowers <christina@opsera.us>
2022-08-03 20:26:28 +0000

    OPL5203 tools used in pipelines report export button

--- src/components/common/buttons/export/reports/ExportToolsUsedInPipelineReportButton.jsx
+++ src/components/common/buttons/export/reports/ExportToolsUsedInPipelineReportButton.jsx
@@ -1,24 +1,37 @@
-import React, {useState} from "react";
+import React, { useContext, useState} from "react";
 import PropTypes from "prop-types";
 import "jspdf-autotable";
 import Button from "react-bootstrap/Button";
 import {faFileDownload} from "@fortawesome/pro-light-svg-icons";
 import TooltipWrapper from "components/common/tooltip/TooltipWrapper";
-import ExportReportsDataModal from "components/common/modal/export_data/ExportReportsDataModal";
 import IconBase from "components/common/icons/IconBase";
+import { DialogToastContext} from "../../../../../contexts/DialogToastContext";
+import ExportReportsDataOverlay from "../../../modal/export_data/ExportReportsDataOverlay";
+import ExportDataButtonBase from "../../../modal/export_data/ExportDataButtonBase";
 
-function ExportToolsUsedInPipelineReportButton({isLoading, pipelineData, className}) {
-  const [showExportModal, setShowExportModal] = useState(false);
+function ExportToolsUsedInPipelineReportButton(
+  {
+    isLoading,
+    pipelineData,
+    className,
+  }) {
+  const toastContext = useContext(DialogToastContext);
 
-  const closeModal = () => {
-    setShowExportModal(false);
+  const launchOverlayFunction = () => {
+    toastContext.showOverlayPanel(
+      <ExportReportsDataOverlay
+        isLoading={isLoading}
+        formattedData={formattedData()}
+        rawData={rawDataResults()}
+      />
+    );
   };
 
   const rawDataResults = () =>{
     return pipelineData ? pipelineData.map(item => JSON.stringify(item)) : "export failure";
-   };
+  };
 
-  const formatPipelineData = () => {
+  const formattedData = () => {
     let formattedData = pipelineData;
 
     //any data formatting goes here
@@ -28,27 +41,11 @@ function ExportToolsUsedInPipelineReportButton({isLoading, pipelineData, classNa
 
   // TODO: Refine when more is complete
   return (
-    <>
-      <TooltipWrapper innerText={"Export"}>
-        <div className={className}>
-          <Button
-            variant={"outline-primary"}
-            size={"sm"}
-            disabled={isLoading}
-            onClick={() => setShowExportModal(true)}>
-            <span><IconBase icon={faFileDownload}/></span>
-          </Button>
-        </div>
-      </TooltipWrapper>
-      <ExportReportsDataModal
-        showModal={showExportModal}
-        closeModal={closeModal}
-        setParentVisibility={setShowExportModal}
-        isLoading={isLoading}
-        formattedData={formatPipelineData()}
-        rawData={rawDataResults()}
-       />
-    </>
+    <ExportDataButtonBase
+      isLoading={isLoading}
+      className={className}
+      launchOverlayFunction={launchOverlayFunction}
+    />
   );
 }
 
