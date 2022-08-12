5c2386b975d3d9af93f2887f01bcda74d4bde53d
Divyesha Malhotra <divyesha@opsera.us>
2022-08-12 17:54:37 +0000

    Opl 5494 master :Boomi Actionable Insights

---
+++ src/components/insights/charts/boomi/actionable_insights/boomi-actionable-insights-metadata.js
@@ -0,0 +1,45 @@
+const BoomiActionableMetadata = {
+    idProperty: "_id",
+    type: "Opsera Tasks Deployments Report",
+    fields: [
+        {
+            label: "Pipeline Name",
+            id: "pipeline",
+        },
+        {
+            label: "Total Components",
+            id: "totalComponents",
+        },
+        {
+            label: "Successful Components",
+            id: "successfulComponents",
+        },
+        {
+            label: "Failed Components",
+            id: "failedComponents",
+        },
+        {
+            label: "Total Packages",
+            id: "totalPackages",
+        },
+        {
+            label: "Successful Packages",
+            id: "successfulPackages",
+        },
+        {
+            label: "Failed Packages",
+            id: "failedPackages",
+        },
+        {
+            label: "Run Count",
+            id: "runCount",
+        },
+        {
+            label: "Timestamp",
+            id: "timestamp",
+        }
+    ],
+    newObjectFields: {},
+};
+
+export default BoomiActionableMetadata;
