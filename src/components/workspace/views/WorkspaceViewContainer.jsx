fe58375b6b0eabb1f19c4a9518f8103485b37c05
Noah Champoux <noah@opsera.io>
2022-08-12 15:54:07 -0500

    Update WorkspaceViewContainer.jsx

--- src/components/workspace/views/WorkspaceViewContainer.jsx
+++ src/components/workspace/views/WorkspaceViewContainer.jsx
@@ -1,39 +1,57 @@
+@@ -0,0 +1,57 @@
 import React, { useState } from "react";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import TableCardView from "components/common/table/TableCardView";
 import WorkspaceVerticalTabContainer from "components/workspace/views/WorkspaceVerticalTabContainer";
+import TabAndViewContainer from "components/common/tabs/tree/TabTreeAndViewContainer";
+import { faNetworkWired } from "@fortawesome/pro-light-svg-icons";
+import FilterContainer from "components/common/table/FilterContainer";
 
 export default function WorkspaceViewContainer() {
   const [activeView, setActiveView] = useState("pipelines");
 
-  const getViews = () => {
+  const getCurrentView = () => {
+    switch (activeView) {
+      case "pipelines":
+        return activeView;
+      case "tasks":
+        return activeView;
+      case "registry":
+        return activeView;
+    }
+  };
+
+  const getVerticalTabContainer = () => {
     return (
-      <Row className={"mx-0"}>
-        {/*<Col sm={2} className={"px-0 makeup-tree-container"}>*/}
-        {/*  <WorkspaceVerticalTabContainer*/}
-        {/*    isLoading={isLoading}*/}
-        {/*    loadData={loadData}*/}
-        {/*    taskFilterModel={taskFilterModel}*/}
-        {/*  />*/}
-        {/*</Col>*/}
-        {/*<Col sm={10} className={"px-0"}>*/}
-        {/*  <TableCardView*/}
-        {/*    filterModel={taskFilterModel}*/}
-        {/*    data={taskData}*/}
-        {/*    isLoading={isLoading}*/}
-        {/*    cardView={getCardView()}*/}
-        {/*    tableView={getTableView()}*/}
-        {/*  />*/}
-        {/*</Col>*/}
-      </Row>
+      <WorkspaceVerticalTabContainer
+        activeView={activeView}
+        setActiveView={setActiveView}
+      />
+    );
+  };
+
+  const getTabAndViewContainer = () => {
+    return (
+      <TabAndViewContainer
+        verticalTabContainer={getVerticalTabContainer()}
+        currentView={getCurrentView()}
+        tabColumnSize={2}
+        // bodyClassName={bodyClassName}
+        // minimumHeight={minimumHeight}
+        // maximumHeight={maximumHeight}
+        // overflowXBodyStyle={overflowXBodyStyle}
+        // overflowYContainerStyle={overflowYContainerStyle}
+        // overflowYBodyStyle={overflowYBodyStyle}
+      />
     );
   };
 
   return (
-    <div>
-      test
-    </div>
+    <FilterContainer
+      // addRecordFunction={createNewTask}
+      body={getTabAndViewContainer()}
+      titleIcon={faNetworkWired}
+      title={"Workspace"}
+      className={"px-2 pb-2"}
+    />
   );
 }
 
