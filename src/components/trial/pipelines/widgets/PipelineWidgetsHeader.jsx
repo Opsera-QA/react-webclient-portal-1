35dafa4ba3b4836947ba6f6600957972b9e50d02
Noah Champoux <noah@opsera.io>
2022-08-18 14:07:08 -0500

    Update PipelineWidgetsHeader.jsx

--- src/components/trial/pipelines/widgets/PipelineWidgetsHeader.jsx
+++ src/components/trial/pipelines/widgets/PipelineWidgetsHeader.jsx
@@ -3,42 +3,53 @@ import PropTypes from "prop-types";
 import PipelineWidgetsPipelineSelectInput
   from "components/trial/pipelines/widgets/PipelineWidgetsPipelineSelectInput";
 import { Link } from "react-router-dom";
+import { isMongoDbId } from "components/common/helpers/mongo/mongoDb.helpers";
 
 function PipelineWidgetsHeader(
   {
     selectedPipelineId,
     setSelectedPipelineId,
   }) {
+  const getLinkBar = () => {
+    if (isMongoDbId(selectedPipelineId) === true) {
+      return (
+        <div className={"d-flex"}>
+          <div className={"ml-5 my-auto"}>
+            <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
+              {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
+              Pipeline
+            </Link>
+          </div>
+          <div className={"ml-5 my-auto"}>
+            <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
+              {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
+              Logs
+            </Link>
+          </div>
+          <div className={"ml-5 my-auto"}>
+            <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
+              {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
+              Metrics
+            </Link>
+          </div>
+          <div className={"ml-5 my-auto"}>
+            <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
+              {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
+              More...
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className={"d-flex"}>
       <PipelineWidgetsPipelineSelectInput
         selectedPipelineId={selectedPipelineId}
         setSelectedPipelineId={setSelectedPipelineId}
       />
-      <div className={"ml-5 my-auto"}>
-        <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
-          {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
-          Pipeline
-        </Link>
-      </div>
-      <div className={"ml-5 my-auto"}>
-        <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
-          {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
-          Logs
-        </Link>
-      </div>
-      <div className={"ml-5 my-auto"}>
-        <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
-          {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
-          Metrics
-        </Link>
-      </div>
-      <div className={"ml-5 my-auto"}>
-        <Link to={`/workflow/details/${selectedPipelineId}/summary`}>
-          {/*<IconBase icon={faCompassDrafting} className={"mr-2"}/>*/}
-          More...
-        </Link>
-      </div>
+      {getLinkBar()}
     </div>
   );
 }
