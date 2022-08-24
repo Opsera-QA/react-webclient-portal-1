374ba3602e16878467425f939239c5ca280b9481
Noah Champoux <noah@opsera.io>
2022-08-24 16:11:25 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,10 +8,10 @@ export default function Home() {
 
   if (authState?.isAuthenticated) {
     return (
-      // <FreeTrialLanding
-      // />
-    <OverviewLanding
-    />
+      <FreeTrialLanding
+      />
+    // <OverviewLanding
+    // />
     );
   }
 
