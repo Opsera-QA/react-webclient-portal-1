6de9202f56bc7817c1c0932b96d0d52eb7a542d7
Noah Champoux <noah@opsera.io>
2022-08-24 19:40:41 -0500

    Removed unnecessary item

--- src/components/wizard/free_trial/workflows/flows/salesforce/flows/organization_sync/task/CreateSalesforceOrganizationSyncTaskWizard.jsx
+++ src/components/wizard/free_trial/workflows/flows/salesforce/flows/organization_sync/task/CreateSalesforceOrganizationSyncTaskWizard.jsx
@@ -14,8 +14,6 @@ import CreateSalesforceOrganizationSyncTaskTestGitToolConnectionScreen
   from "components/wizard/free_trial/workflows/flows/salesforce/flows/organization_sync/task/tools/git/connection/CreateSalesforceOrganizationSyncTaskTestGitToolConnectionScreen";
 import CreateSalesforceOrganizationSyncTaskWizardTestSalesforceSourceToolConnectionScreen
   from "components/wizard/free_trial/workflows/flows/salesforce/flows/organization_sync/task/tools/salesforce/connection/CreateSalesforceOrganizationSyncTaskWizardTestSalesforceSourceToolConnectionScreen";
-import CreateSalesforceOrganizationSyncTaskWizardTestSalesforceDestinationToolConnectionScreen
-  from "components/wizard/free_trial/workflows/flows/salesforce/flows/organization_sync/task/tools/salesforce/connection/CreateSalesforceOrganizationSyncTaskWizardTestSalesforceDestinationToolConnectionScreen";
 
 export const CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS = {
   INITIALIZATION_SCREEN: "initialization_screen",
@@ -23,8 +21,6 @@ export const CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS = {
   TEST_GIT_TOOL_CONNECTION_SCREEN: "test_git_tool_connection_screen",
   CREATE_SOURCE_SALESFORCE_TOOL_SCREEN: "create_source_salesforce_tool_screen",
   TEST_SOURCE_SALESFORCE_TOOL_CONNECTION_SCREEN: "test_source_salesforce_tool_connection_screen",
-  CREATE_DESTINATION_SALESFORCE_TOOL_SCREEN: "create_salesforce_destination_tool_screen",
-  TEST_DESTINATION_SALESFORCE_TOOL_CONNECTION_SCREEN: "test_salesforce_source_tool_connection_screen",
   WORKFLOW_COMPLETION_SCREEN: "workflow_completion_screen",
 };
 
@@ -95,27 +91,6 @@ export default function CreateSalesforceOrganizationSyncTaskWizard(
             task={task}
             setTask={setTask}
             salesforceToolId={salesforceSourceToolId}
-          />
-        );
-      case CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS.CREATE_DESTINATION_SALESFORCE_TOOL_SCREEN:
-        return (
-          <CreateWorkflowWizardCreateSalesforceToolEditorPanel
-            salesforceToolModel={deploymentSalesforceToolModel}
-            setSalesforceToolModel={setDeploymentSalesforceToolModel}
-            salesforceToolId={salesforceDestinationToolId}
-            setSalesforceToolId={setSalesforceDestinationToolId}
-            onSuccessFunction={() => setCurrentScreen(CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS.TEST_DESTINATION_SALESFORCE_TOOL_CONNECTION_SCREEN)}
-            type={"destination"}
-            className={"m-3"}
-          />
-        );
-      case CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS.TEST_DESTINATION_SALESFORCE_TOOL_CONNECTION_SCREEN:
-        return (
-          <CreateSalesforceOrganizationSyncTaskWizardTestSalesforceDestinationToolConnectionScreen
-            setCurrentScreen={setCurrentScreen}
-            task={task}
-            setTask={setTask}
-            salesforceToolId={salesforceDestinationToolId}
           />
         );
       case CREATE_SALESFORCE_ORGANIZATION_SYNC_TASK_WIZARD_SCREENS.WORKFLOW_COMPLETION_SCREEN:
