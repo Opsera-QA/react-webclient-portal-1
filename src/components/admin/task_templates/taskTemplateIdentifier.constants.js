55dbc779dd3e3f3965c58c671f86031d65c3458e
Faseeh <faseeh@opsera.io>
2022-08-26 21:40:04 +0530

    task identifiers for freetrial added

--- src/components/admin/task_templates/taskTemplateIdentifier.constants.js
+++ src/components/admin/task_templates/taskTemplateIdentifier.constants.js
@@ -2,4 +2,6 @@ export const taskTemplateIdentifierConstants = {};
 
 taskTemplateIdentifierConstants.TASK_TEMPLATE_IDENTIFIERS = {
   FREE_TRIAL_ORGANIZATION_SYNC_TASK: "free-trial-organization-sync-task",
+  FREE_TRIAL_GIT_CUSTODIAN: "free-trial-git-custodian-task",
+  FREE_TRIAL_SALESFORCE_TO_GIT_MERGE_SYNC_PIPELINE: "free-trial-salesforce-to-git-merge-sync-task",
 };
