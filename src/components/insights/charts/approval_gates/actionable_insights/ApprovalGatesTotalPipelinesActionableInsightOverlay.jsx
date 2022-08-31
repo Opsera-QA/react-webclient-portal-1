09d773fc2682df90ce48ecfc06cad3a4b19f7fd7
Murali Kurma <murali@opsera.io>
2022-08-31 07:29:03 -0700

    title changed

--- src/components/insights/charts/approval_gates/actionable_insights/ApprovalGatesTotalPipelinesActionableInsightOverlay.jsx
+++ src/components/insights/charts/approval_gates/actionable_insights/ApprovalGatesTotalPipelinesActionableInsightOverlay.jsx
@@ -108,7 +108,7 @@ function ApprovalGatesTotalPipelinesActionableInsightOverlay({
     <FullScreenCenterOverlayContainer
       closePanel={closePanel}
       showPanel={true}
-      titleText={`Approval Gates - Total pipelines executed`}
+      titleText={`Approval Gates - Total Pipelines Executed`}
       showToasts={true}
       titleIcon={faTable}
       isLoading={false}
