f174f6c8aabe733dda2fb0213c77a7149e669a3b
Noah Champoux <noah@opsera.io>
2022-08-31 14:16:55 -0500

    Don't show shared Jenkins tool field on Task Summary Panel

--- src/components/tasks/details/tasks/sfdc-org-sync/SalesforceOrganizationSyncTaskTypeSummaryCard.jsx
+++ src/components/tasks/details/tasks/sfdc-org-sync/SalesforceOrganizationSyncTaskTypeSummaryCard.jsx
@@ -4,18 +4,35 @@ import TextFieldBase from "components/common/fields/text/TextFieldBase";
 import TaskSummaryCardContainer from "components/tasks/details/tasks/TaskSummaryCardContainer";
 import {Row, Col} from "react-bootstrap";
 import ToolNameField from "components/common/fields/inventory/ToolNameField";
+import useComponentStateReference from "hooks/useComponentStateReference";
 
 function SalesforceOrganizationSyncTaskTypeSummaryCard({ gitTasksData, gitTaskConfigurationData, isLoading }) {
+  const {
+    isFreeTrial,
+  } = useComponentStateReference();
+
   if (isLoading) {
     return <TaskSummaryCardContainer isLoading={isLoading} />;
   }
 
+  // For now, we have a shared Jenkins for free trial. The user doesn't need to know this, so it shouldn't be shown
+  const getDynamicFields = () => {
+    if (isFreeTrial !== true) {
+      return (
+        <Col xs={12} sm={6} md={4}>
+          <ToolNameField
+            model={gitTaskConfigurationData}
+            fieldName={"toolConfigId"}
+          />
+        </Col>
+      );
+    }
+  };
+
   return (
     <TaskSummaryCardContainer gitTasksDataDto={gitTasksData} isLoading={isLoading}>
       <Row className="mx-0 mb-2">
-        <Col xs={12} sm={6} md={4}>
-          <ToolNameField model={gitTaskConfigurationData} fieldName={"toolConfigId"} />
-        </Col>
+        {getDynamicFields()}
         <Col xs={12} sm={6} md={4}>
           <ToolNameField model={gitTaskConfigurationData} fieldName={"sfdcToolId"} />
         </Col>
