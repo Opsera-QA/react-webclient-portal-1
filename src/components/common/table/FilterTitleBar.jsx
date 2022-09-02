1cf7d968ab7ad4b93c99d2d5727ca23f26e01fc1
Noah Champoux <noah@opsera.io>
2022-09-02 11:39:41 -0500

    Minor tweak

--- src/components/common/table/FilterTitleBar.jsx
+++ src/components/common/table/FilterTitleBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import IconBase from "components/common/icons/IconBase";
+import { hasStringValue } from "components/common/helpers/string-helpers";
 
 function FilterTitleBar(
   {
@@ -8,14 +9,23 @@ function FilterTitleBar(
     inlineFilters,
     titleIcon,
     isLoading,
+    className,
   }) {
+  const getClassNames = () => {
+    if (hasStringValue(className) === true) {
+      return `${className} d-flex w-100 justify-content-between`;
+    }
+
+    return `d-flex w-100 justify-content-between py-2 pl-3 pr-2`;
+  };
+
   return (
-    <div className={"d-flex w-100 justify-content-between py-2 pl-3 pr-2"}>
+    <div className={getClassNames()}>
       <div className={"my-auto filter-title-text text-nowrap w-100"}>
-        <span className={"d-flex"}>
-          <IconBase icon={titleIcon} isLoading={isLoading} className={"mr-2 my-auto d-none d-lg-block"} />
-          {title}
-        </span>
+          <span className={"d-flex"}>
+            <IconBase icon={titleIcon} isLoading={isLoading} className={"mr-2 my-auto d-none d-lg-block"} />
+            <span className={"my-auto"}>{title}</span>
+          </span>
       </div>
       <div className={"d-flex"}>
         {inlineFilters}
@@ -29,6 +39,7 @@ FilterTitleBar.propTypes = {
   titleIcon: PropTypes.object,
   inlineFilters: PropTypes.any,
   isLoading: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 export default FilterTitleBar;
