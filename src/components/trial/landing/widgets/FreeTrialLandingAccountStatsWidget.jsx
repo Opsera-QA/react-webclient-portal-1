77d09c5e701ece379871b6b0d2ed52c2c4f2fb69
Noah Champoux <noah@opsera.io>
2022-09-15 14:42:28 -0500

    Update FreeTrialLandingAccountStatsWidget.jsx

--- src/components/trial/landing/widgets/FreeTrialLandingAccountStatsWidget.jsx
+++ src/components/trial/landing/widgets/FreeTrialLandingAccountStatsWidget.jsx
@@ -182,7 +182,7 @@ export default function FreeTrialLandingAccountStatsWidget({ className }) {
               className={"marketingModulesTextLink ml-1"}
               rel={"noreferrer"}
             >
-              click here
+             click here
             </a>.
           </div>
         </div>
