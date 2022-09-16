670554804067e7a8fa5d1ebbf8ad15e293f3db70
Noah Champoux <noah@opsera.io>
2022-09-16 08:31:58 -0500

    OPL5704 non admin access to connected assets and git custodian tabs

--- src/components/insights/gitCustodian/GitCustodian.jsx
+++ src/components/insights/gitCustodian/GitCustodian.jsx
@@ -1,32 +1,36 @@
-import React, {useEffect, useState, useContext, useRef} from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import { AuthContext } from "contexts/AuthContext";
 import LoadingDialog from "components/common/status_notifications/loading";
 import Model from "core/data_model/model";
 import axios from "axios";
-import {DialogToastContext} from "contexts/DialogToastContext";
+import { DialogToastContext } from "contexts/DialogToastContext";
 import ScreenContainer from "components/common/panels/general/ScreenContainer";
 import InsightsSubNavigationBar from "components/insights/InsightsSubNavigationBar";
 import GitCustodianDetails from "./GitCustodianDetails";
-import GitCustodianLookUpHelpDocumentation from "../../common/help/documentation/insights/GitCustodianLookUpHelpDocumentation";
+import GitCustodianLookUpHelpDocumentation
+  from "../../common/help/documentation/insights/GitCustodianLookUpHelpDocumentation";
 import EditGitCustodianFiltersIcon from "./filters/EditGitCustodianFiltersIcon";
 import { GitCustodianFilterMetadata } from "components/insights/gitCustodian/table/gitCustodianFilter.metadata";
+import useComponentStateReference from "../../../hooks/useComponentStateReference";
+import AccessDeniedContainer from "../../common/panels/detail_view_container/AccessDeniedContainer";
 
 function GitCustodian() {
-  const {getUserRecord, setAccessRoles} = useContext(AuthContext);
+  const { getUserRecord, setAccessRoles } = useContext(AuthContext);
   const [accessRoleData, setAccessRoleData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(true);
   const toastContext = useContext(DialogToastContext);
   const isMounted = useRef(false);
   const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
   const [gitCustodianData, setGitCustodianData] = useState(undefined);
-  const [gitCustodianFilterModel, setGitCustodianFilterModel] = useState(new Model({...GitCustodianFilterMetadata.newObjectFields}, GitCustodianFilterMetadata, false));
+  const [gitCustodianFilterModel, setGitCustodianFilterModel] = useState(new Model({ ...GitCustodianFilterMetadata.newObjectFields }, GitCustodianFilterMetadata, false));
+  const { isSiteAdministrator } = useComponentStateReference();
 
   useEffect(() => {
     if (cancelTokenSource) {
       cancelTokenSource.cancel();
     }
     // eslint-disable-next-line no-undef
-    let newDataObject = new Model({...GitCustodianFilterMetadata.newObjectFields}, GitCustodianFilterMetadata, true);
+    let newDataObject = new Model({ ...GitCustodianFilterMetadata.newObjectFields }, GitCustodianFilterMetadata, true);
     newDataObject.setData("filters", []);
     const source = axios.CancelToken.source();
     setCancelTokenSource(source);
@@ -50,7 +54,7 @@ function GitCustodian() {
       await getRoles(cancelSource);
       let newFilterDto = newDataObject;
       newFilterDto.setData("activeFilters", newFilterDto.getActiveFilters());
-      newFilterDto.setData("totalCount", newFilterDto.getData('totalCount'));
+      newFilterDto.setData("totalCount", newFilterDto.getData("totalCount"));
       let pageSize = newFilterDto.getData("pageSize");
       newFilterDto.setData("pageSize", pageSize);
       let sortOption = newFilterDto.getData("sortOption");
@@ -78,11 +82,11 @@ function GitCustodian() {
   };
 
   if (!accessRoleData) {
-    return (<LoadingDialog size="sm" message="Loading Git Custodian"/>);
+    return (<LoadingDialog size="sm" message="Loading Git Custodian" />);
   }
 
   const getTitleActionBar = () => {
-    return(
+    return (
       <EditGitCustodianFiltersIcon
         gitCustodianFilterModel={gitCustodianFilterModel}
         setGitCustodianFilterModel={setGitCustodianFilterModel}
@@ -92,15 +96,23 @@ function GitCustodian() {
     );
   };
 
+  if (isSiteAdministrator !== true) {
+    return (
+      <AccessDeniedContainer
+        navigationTabContainer={<InsightsSubNavigationBar currentTab={"gitCustodian"} />}
+      />
+    );
+  }
+
   return (
     <ScreenContainer
-      navigationTabContainer={<InsightsSubNavigationBar currentTab={"gitCustodian"}/>}
-      pageDescription={'Custodian for repositories. This Dashboard provides visibility to exposed secrets, tokens, passwords, key files, and sensitive data.'}
+      navigationTabContainer={<InsightsSubNavigationBar currentTab={"gitCustodian"} />}
+      pageDescription={"Custodian for repositories. This Dashboard provides visibility to exposed secrets, tokens, passwords, key files, and sensitive data."}
       breadcrumbDestination={"insightsGitCustodian"}
-      helpComponent={<GitCustodianLookUpHelpDocumentation/>}
+      helpComponent={<GitCustodianLookUpHelpDocumentation />}
       titleActionBar={getTitleActionBar()}
     >
-      {isLoading ? <LoadingDialog size="sm" message="Loading Git Custodian Report"/> :
+      {isLoading ? <LoadingDialog size="sm" message="Loading Git Custodian Report" /> :
         <GitCustodianDetails
           gitCustodianData={gitCustodianData}
           gitCustodianFilterModel={gitCustodianFilterModel}
