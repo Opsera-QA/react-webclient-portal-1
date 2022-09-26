9cdcdd77b6d2f521686189396a7630d78a57d4d5
Noah Champoux <noah@opsera.io>
2022-09-26 13:10:40 -0500

    [Tech Debt] Minor Scripts cleanup (no functional changes)

--- src/components/inventory/scripts/details/ScriptsEditorPanel.jsx
+++ src/components/inventory/scripts/details/ScriptsEditorPanel.jsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import TextInputBase from "components/common/inputs/text/TextInputBase";
-import axios from "axios";
 import RoleAccessInput from "components/common/inputs/roles/RoleAccessInput";
 import VanityEditorPanelContainer from "components/common/panels/detail_panel_container/VanityEditorPanelContainer";
 import ScriptLanguageSelectInput
   from "components/common/list_of_values_input/inventory/scripts/ScriptLanguageSelectInput";
 import ScriptValueInput from "components/inventory/scripts/details/ScriptValueInput";
 
-function ScriptsEditorPanel({ scriptModel, setScriptModel, scriptModelId, handleClose }) {
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
-
-  useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
-  }, [scriptModelId]);
-
+function ScriptsEditorPanel({ scriptModel, setScriptModel, handleClose }) {
   if (scriptModel == null) {
     return null;
   }
