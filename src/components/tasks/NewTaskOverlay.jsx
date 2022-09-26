8f71fc9abf838bb060189bdf1e364d12724a46ec
Noah Champoux <noah@opsera.io>
2022-09-26 13:13:12 -0500

    [Tech Debt] Minor Tasks cleanup (no functional changes)

--- src/components/tasks/NewTaskOverlay.jsx
+++ src/components/tasks/NewTaskOverlay.jsx
@@ -1,64 +1,18 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import CreateCenterPanel from "components/common/overlays/center/CreateCenterPanel";
 import {DialogToastContext} from "contexts/DialogToastContext";
 import TaskEditorPanel from "components/tasks/details/TaskEditorPanel";
-import axios from "axios";
-import { AuthContext } from "contexts/AuthContext";
-import TaskModel from "components/tasks/task.model";
+import useGetNewTaskModel from "components/tasks/hooks/useGetNewTaskModel";
 
-function NewTaskOverlay(
+export default function NewTaskOverlay(
   {
     loadData,
     isMounted,
     taskMetadata,
   }) {
-  const { getAccessToken } = useContext(AuthContext);
   const toastContext = useContext(DialogToastContext);
-  const [taskModel, setTaskModel] = useState(undefined);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
-
-  useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-
-    if (taskMetadata) {
-      createNewTaskModel(source).catch((error) => {
-        if (isMounted?.current === true) {
-          throw error;
-        }
-      });
-    }
-
-    return () => {
-      source.cancel();
-    };
-  }, [taskMetadata]);
-
-  const createNewTaskModel = async (cancelSource = cancelTokenSource) => {
-    try {
-      const newTaskModel = new TaskModel(
-        { ...taskMetadata.newObjectFields },
-        taskMetadata,
-        true,
-        getAccessToken,
-        cancelSource,
-        loadData,
-        true,
-        false,
-        setTaskModel
-      );
-      setTaskModel({...newTaskModel});
-    } catch (error) {
-      if (isMounted?.current === true) {
-        toastContext.showLoadingErrorDialog(error);
-      }
-    }
-  };
+  const { taskModel, setTaskModel } = useGetNewTaskModel();
 
   const closePanel = () => {
     if (isMounted?.current === true) {
@@ -92,5 +46,3 @@ NewTaskOverlay.propTypes = {
   isMounted: PropTypes.object,
   taskMetadata: PropTypes.object,
 };
-
-export default NewTaskOverlay;
