6e0eb3060b4ab9875ad5b936339376fc2efc3c67
Noah Champoux <noah@opsera.io>
2022-10-02 14:43:30 -0500

    minor cleanup

--- src/components/settings/unassigned_rules/UnassignedRulesItems.jsx
+++ src/components/settings/unassigned_rules/UnassignedRulesItems.jsx
@@ -1,48 +1,34 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
-import { AuthContext } from "contexts/AuthContext";
+import React, { useState, useEffect } from "react";
 import ScreenContainer from "components/common/panels/general/ScreenContainer";
 import { ROLE_LEVELS } from "components/common/helpers/role-helpers";
 import OrganizationsSubNavigationBar from "components/settings/organizations/OrganizationsSubNavigationBar";
-import axios from "axios";
-import { DialogToastContext } from "../../../contexts/DialogToastContext";
 import unassignedRulesActions from "./unassigned-rules-functions";
 import UnassignedRulesItemsTable from "./UnassignedRulesItemsTable";
 import Model from "core/data_model/model";
+import useComponentStateReference from "hooks/useComponentStateReference";
 
 function UnassignedRulesItems() {
-  const { userAccessRoles } = useContext(AuthContext);
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
-  const toastContext = useContext(DialogToastContext);
-  const { getAccessToken } = useContext(AuthContext);
   const [items, setItems] = useState([]);
   const [itemFilterModel, setItemFilterModel] = useState(undefined);
+  const {
+    getAccessToken,
+    isMounted,
+    cancelTokenSource,
+    accessRoleData,
+    toastContext,
+  } = useComponentStateReference();
 
   useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-
-    loadData(itemFilterModel, source).catch((error) => {
+    loadData(itemFilterModel).catch((error) => {
       if (isMounted?.current === true) {
         throw error;
       }
     });
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
   }, []);
 
   const loadData = async (
     itemFilterModel,
-    cancelSource = cancelTokenSource,
   ) => {
     try {
       setItems([]);
@@ -71,7 +57,7 @@ function UnassignedRulesItems() {
   return (
     <ScreenContainer
       breadcrumbDestination={"unassignedRulesItems"}
-      accessRoleData={userAccessRoles}
+      accessRoleData={accessRoleData}
       roleRequirement={ROLE_LEVELS.POWER_USERS_AND_SASS}
       navigationTabContainer={
         <OrganizationsSubNavigationBar activeTab={"organizations"} />
