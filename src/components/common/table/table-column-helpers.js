e5670f2687d44cf50f1ef0452db58a93bb302173
Noah Champoux <noah@opsera.io>
2022-10-07 17:31:32 -0500

    OPL-7326  Add New RBAC Access Column Definition (added new column definition, not wired up yet)

--- src/components/common/table/table-column-helpers.js
+++ src/components/common/table/table-column-helpers.js
@@ -5,7 +5,7 @@ import {
   faPauseCircle, faPlayCircle, faSearchPlus,
   faSpinner,
   faStopCircle,
-  faTimesCircle, faTrash, faPlay, faExclamationCircle
+  faTimesCircle, faTrash, faPlay, faExclamationCircle, faUnlock, faLock,
 } from "@fortawesome/pro-light-svg-icons";
 import {
   faGithub,
@@ -35,6 +35,9 @@ import { getTimeDisplay } from "components/insights/charts/sdlc/sdlc-duration-by
 import PipelineTypeIconBase from "components/common/fields/pipelines/types/PipelineTypeIconBase";
 import OrchestrationStateFieldBase
   from "temp-library-components/fields/orchestration/state/OrchestrationStateFieldBase";
+import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
+import { isMongoDbId } from "components/common/helpers/mongo/mongoDb.helpers";
+import AccessRoleDisplayer from "components/common/fields/multiple_items/roles/displayer/AccessRoleDisplayer";
 
 export const getCustomTableHeader = (field) => {
   return field ? field.label : "";
@@ -579,6 +582,7 @@ export const getCountColumnWithoutField = (header, accessor, className) => {
   };
 };
 
+// TODO: Combine with the below
 export const getRoleAccessLevelColumn = (field, className) => {
   return {
     Header: getCustomTableHeader(field),
@@ -604,6 +608,58 @@ export const getRoleAccessLevelColumn = (field, className) => {
       return "ROLE ACCESS LEVEL UNKNOWN";
     },
     class: className ? className :  "no-wrap-inline"
+  };
+};
+
+export const getRoleAccessColumn = (
+  type,
+  headerText = "Access",
+  rolesField = "roles",
+  className = "no-wrap-inline",
+) => {
+  return {
+    Header: headerText,
+    accessor: rolesField,
+    Cell: function getRoleAccessLevel(row) {
+      const roles = DataParsingHelper.parseArray(row?.value, []);
+      const owner = row?.data[row?.row?.index]?.owner;
+
+      if (!Array.isArray(roles) || roles.length === 0 || isMongoDbId(owner) === false) {
+        return (
+          <div>
+            <TooltipWrapper
+              innerText={`This ${type} is public and accessible to everyone.`}
+              wrapInDiv={true}
+            >
+              <IconBase icon={faUnlock} className={"danger-red ml-2"} />
+            </TooltipWrapper>
+          </div>
+        );
+      }
+
+      const rolesPopover = (
+        <div>
+          <div className={"mb-3"}>{`This ${type} is secured and only visible to Site Administrators, its owner, and those given access`}</div>
+          <AccessRoleDisplayer
+            roles={roles}
+          />
+        </div>
+      );
+
+      return (
+        <div>
+          <TooltipWrapper
+            title={`Secured ${type} Details`}
+            innerText={rolesPopover}
+            wrapInDiv={true}
+            placement={"bottom"}
+          >
+              <IconBase icon={faLock} className={"ml-2"} />
+          </TooltipWrapper>
+        </div>
+      );
+    },
+    class: className,
   };
 };
 
