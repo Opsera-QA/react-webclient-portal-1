e18ecddcc1fba94f9d714a53a6d872e9713079d5
Noah Champoux <noah@opsera.io>
2022-10-10 12:05:26 -0600

    OPL-7326  Add New RBAC Access Column Definition

--- src/components/insights/dashboards/views/DashboardsTable.jsx
+++ src/components/insights/dashboards/views/DashboardsTable.jsx
@@ -4,25 +4,38 @@ import CustomTable from "components/common/table/CustomTable";
 import {useHistory} from "react-router-dom";
 import dashboardMetadata from "components/insights/dashboards/dashboard-metadata";
 import {
-  getLimitedTableTextColumn,
+  getLimitedTableTextColumn, getRoleAccessColumn,
   getTableDateColumn,
   getTableTextColumn,
 } from "components/common/table/table-column-helpers";
 import {getField} from "components/common/metadata/metadata-helpers";
+import useComponentStateReference from "hooks/useComponentStateReference";
 
 function DashboardsTable({dashboards, dashboardFilterModel, setDashboardFilterModel, loadData, isLoading}) {
-  let history = useHistory();
+  const history = useHistory();
   const fields = dashboardMetadata.fields;
+  const {
+    isSaasUser,
+  } = useComponentStateReference();
 
   const columns = useMemo(
-    () => [
-      getTableTextColumn(getField(fields, "name")),
-      getTableTextColumn(getField(fields, "owner_name")),
-      getLimitedTableTextColumn(getField(fields, "description"), 100),
-      {...getTableTextColumn(getField(fields, "type")), class: "upper-case-first"},
-      getTableDateColumn(getField(fields, "createdAt")),
-    ],
-    []
+    () => {
+
+      const newColumns = [
+        getTableTextColumn(getField(fields, "name")),
+        getTableTextColumn(getField(fields, "owner_name")),
+        getLimitedTableTextColumn(getField(fields, "description"), 100),
+        { ...getTableTextColumn(getField(fields, "type")), class: "upper-case-first" },
+        getTableDateColumn(getField(fields, "createdAt")),
+      ];
+
+      if (isSaasUser === false) {
+        newColumns.push(getRoleAccessColumn("Dashboard"));
+      }
+
+      return newColumns;
+    },
+    [fields, isSaasUser]
   );
 
   const onRowSelect = (rowData) => {
