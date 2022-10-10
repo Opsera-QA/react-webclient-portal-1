290920000d15c7eeb18f9fffc770f58f902755c3
tbharadwaj <tejas@opsera.io>
2022-10-10 04:49:04 -0700

    Added changes for branch filter and workflow run summary

--- src/components/insights/charts/github_actions/workflows/actionable_insights/unique_run_summary/runs/githubActionsUniqueRunRunsSummary.metadata.js
+++ src/components/insights/charts/github_actions/workflows/actionable_insights/unique_run_summary/runs/githubActionsUniqueRunRunsSummary.metadata.js
@@ -34,7 +34,7 @@ export const githubActionsUniqueRunRunsSummaryMetadata = {
       id: "conclusion",
     },
     {
-      label: "Number of Runs",
+      label: "Run Attempt",
       id: "numberOfRuns",
     },
     {
