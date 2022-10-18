6b80f6936419eccb5afe387736df963821a1fbde
tbharadwaj <tejas@opsera.io>
2022-10-17 19:13:18 -0700

    Fixed data points editing

--- src/components/admin/kpi_identifiers/details/data_points/kpiDataPoint.model.js
+++ src/components/admin/kpi_identifiers/details/data_points/kpiDataPoint.model.js
@@ -4,26 +4,37 @@ import KpiDataPointRoleHelper from "@opsera/know-your-role/roles/analytics/data_
 import kpiDataPointMetadata from "components/common/inputs/metric/data_points/kpiDataPoint.metadata";
 
 export default class KpiDataPointModel extends ModelBase {
-  constructor(
-    userData,
-    data,
-    newModel,
-    parentId,
-  ) {
+  constructor(data, newModel, parentId) {
     super(data, kpiDataPointMetadata, newModel);
     this.kpiId = parentId;
   }
 
   createModel = async () => {
-    return await kpiDataPointActions.createKpiDataPointV2(this.getAccessToken, this.cancelTokenSource, this, this.kpiId);
+    console.log(this.kpiId);
+    return await kpiDataPointActions.createKpiDataPointV2(
+      this.getAccessToken,
+      this.cancelTokenSource,
+      this,
+      this.kpiId,
+    );
   };
 
   saveModel = async () => {
-    return await kpiDataPointActions.updateKpiDataPointV2(this.getAccessToken, this.cancelTokenSource, this, this.kpiId);
+    return await kpiDataPointActions.updateKpiDataPointV2(
+      this.getAccessToken,
+      this.cancelTokenSource,
+      this,
+      this.kpiId,
+    );
   };
 
   deleteModel = async () => {
-   return await kpiDataPointActions.deleteKpiDataPointV2(this.getAccessToken, this.cancelTokenSource, this, this.kpiId);
+    return await kpiDataPointActions.deleteKpiDataPointV2(
+      this.getAccessToken,
+      this.cancelTokenSource,
+      this,
+      this.kpiId,
+    );
   };
 
   canCreate = () => {
@@ -52,5 +63,3 @@ export default class KpiDataPointModel extends ModelBase {
     return `KPI Data Point`;
   };
 }
-
-
