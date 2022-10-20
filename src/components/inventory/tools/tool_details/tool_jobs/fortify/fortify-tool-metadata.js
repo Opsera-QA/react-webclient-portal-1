3d7f9e3e4ba98f6946e380cd5c6cede960af6e0b
Vignesh Subramanian <vignesh@opsera.io>
2022-10-20 18:35:17 +0530

    fortify on prem tool config changes

--- src/components/inventory/tools/tool_details/tool_jobs/fortify/fortify-tool-metadata.js
+++ src/components/inventory/tools/tool_details/tool_jobs/fortify/fortify-tool-metadata.js
@@ -10,26 +10,76 @@ const FortifyMetadata = {
     {
       label: "Portal URL",
       id: "url",
-      isRequired: true,
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On Demand";
+      },
     },
     {
       label: "Tenant Code",
       id: "tenantCode",
-      isRequired: true,
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On Demand";
+      },
       regexDefinitionName: "generalTextWithoutSpacesPeriod",
       maxLength: 100,
     },
     {
       label: "Access Key",
       id: "accessKey",
-      isRequired: true,
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On Demand";
+      },
       maxLength: 256
     },
     {
       label: "Secret Key",
       id: "secretKey",
-      isRequired: true,
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On Demand";
+      },
       maxLength: 256
+    },
+    {
+      label: "Scan Center Controller URL",
+      id: "fortifyScanCenterControllerUrl",
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On-Prem ScanCentral";
+      },
+      regexDefinitionName: "urlField",
+      maxLength: 1024
+    },    
+    {
+      label: "Scan Center Controller Client Token",
+      id: "token",
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On-Prem ScanCentral";
+      },
+      maxLength: 1024
+    },
+    {
+      label: "Software Security Center URL",
+      id: "fortifySscUrl",
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On-Prem ScanCentral";
+      },
+      regexDefinitionName: "urlField",
+      maxLength: 1024
+    },
+    {
+      label: "Software Security Center Username",
+      id: "userName",
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On-Prem ScanCentral";
+      },
+      maxLength: 2048,
+    },
+    {
+      label: "Software Security Center Password",
+      id: "password",
+      isRequiredFunction: (model) => {
+        return model?.getData("scanToolType") === "Fortify On-Prem ScanCentral";
+      },
+      maxLength: 2048,
     },
   ],
   newObjectFields:
@@ -39,6 +89,11 @@ const FortifyMetadata = {
     tenantCode: "",
     accessKey: "",
     secretKey: "",
+    fortifyScanCenterControllerUrl: "",
+    token: "",
+    fortifySscUrl: "",
+    userName: "",
+    password: "",
   }
 };
 
