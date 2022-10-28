bc7a141fecc8a13504397f7d821f6ec570159d1c
Christina Bowers <christina@opsera.io>
2022-10-28 13:00:55 -0400

    OPL7583 groups help edit

--- src/components/common/help/documentation/settings/GroupsHelpDocumentation.jsx
+++ src/components/common/help/documentation/settings/GroupsHelpDocumentation.jsx
@@ -12,22 +12,22 @@ function GroupsHelpDocumentation() {
 
   const getHelpDocumentation = () => {
     return (
-      <div>Group Management allows users to manage membership for existing groups and create new groups. These groups are the custom groups that users can create, manage and apply to individual resources. Once a group has been created, user can navigate to pipelines, tools, tasks, parameters or scripts and assign RBAC Access Rules and identify if group has Administrator, Manager, User or Guest access to that particular resource. All group members will have access if a group is assigned a role. Group Manager access is limited to Site Administrators or Power Users only. Users can either click on a group to manage membership OR create a new group. To view in depth documentation on group management, view the <b><a href="https://docs.opsera.io/role-based-access-pipelines-and-tool-registry#rolebasedaccess-pipelines-and-toolregistry-groupmanagement" target="_blank" rel="noreferrer">Group Management Help Documentation</a></b>.
+      <div>
+        <div>Group Management allows Site Administrators or Power Users to manage membership of existing groups and create new groups. These are custom groups that users can create, manage and apply to individual resources like pipelines, tools, tasks and parameters using RBAC Access Rules. All group members will have access if a group is assigned RBAC Access Rules. For more information on group management, view the <b><a href="https://docs.opsera.io/role-based-access-pipelines-and-tool-registry#rolebasedaccess-pipelines-and-toolregistry-groupmanagement" target="_blank" rel="noreferrer">Group Management Help Documentation</a></b>. Use the following instructions to manage group membership.</div>
         <div className={"mt-2 ml-2"}>
-          <h5>Group Management</h5>
           <ol>
-            <li>Click on the Group of which you’d like to add or remove a member.</li>
-            <li>Navigate to the ‘Manage Members‘ tab.</li>
-            <li>Locate a user by typing their name into the search box.</li>
-            <li>To <b>add</b> a member to the group, select user‘s name from the column on the left to highlight it, then click the ‘Add Selected’ button. You should see the user name move into the ‘Members’ box on the right.</li>
-            <li>To <b>remove</b> a member from the group, select user’s name from the column on the right to highlight it, then click the ‘Remove Selected‘ button. The user‘s name will move into the ‘Not Members’ column on the left.</li>
-            <li>Click the ‘Save’ button.</li>
+            <li>Select the Group where you will add or remove a member.</li>
+            <li>Navigate to the <b>Manage Members</b> tab.</li>
+            <li>Locate a user by typing their name in the search box.</li>
+            <li>To <b>add</b> a member to the group, select user from the <b>Not Members</b> table on left and select <b>Add Selected</b> button. User will move into the <b>Members</b> table on the right.</li>
+            <li>To <b>remove</b> a member from the group, select user from the <b>Not Members</b> on right and select <b>Remove Selected</b> button. User will move into the <b>Not Members</b> table on the left.</li>
+            <li>Select the <b>Save</b> button.</li>
           </ol>
-          <div><em>Note: For Group membership changes go into effect, user must log out then log back into the portal.</em></div>
-          <div className={"ml-3 mt-3"}>
+          <div className={"mb-2"}><b>* For Group membership changes to take effect, member must log out and log back into the portal.</b></div>
+          <div>
             <ul>
-              <li>If a Group is assigned specific Access Rules to a resource and a group member is already a member of the Site Administrators or Power User Site Role, that privilege will supersede these settings where applicable.</li>
-              <li>If a non-Site Administrator or non-Owner has RBAC Access to a resource and re-assigns RBAC access to a group they are NOT a member of, they will forfeit their access. </li>
+              <li>If a Group is assigned specific RBAC Access Rules to a resource and a group member is already a member of Site Administrators or Power User <b>Site Role</b>, the <b>Site Role</b> privileges supersede the RBAC Access Rules.</li>
+              <li>If a non-Site Administrator or non-Owner has RBAC Access to a resource and re-assigns RBAC Access to a group they are NOT a member of, they will forfeit their access. </li>
             </ul>
           </div>
         </div>
