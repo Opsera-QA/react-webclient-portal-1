fd331c68537d8e2944f920687e315a9d7a9bbce4
Noah Champoux <noah@opsera.io>
2022-11-03 09:18:50 -0500

    OPL-7653  Show Child Pipeline on Child Pipeline Step Summary Panel

--- src/components/common/fields/pipelines/PipelineSummaryField.jsx
+++ src/components/common/fields/pipelines/PipelineSummaryField.jsx
@@ -5,12 +5,17 @@ import pipelineActions from "components/workflow/pipeline-actions";
 import pipelineSummaryMetadata
   from "components/workflow/pipelines/pipeline_details/pipeline_activity/pipeline-summary-metadata";
 import PipelineSummaryCard from "components/workflow/pipelines/pipeline_details/pipeline_activity/PipelineSummaryCard";
-import FieldLabel from "components/common/fields/FieldLabel";
 import useComponentStateReference from "hooks/useComponentStateReference";
 import { isMongoDbId } from "components/common/helpers/mongo/mongoDb.helpers";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
 
-function PipelineSummaryField({ model, fieldName, pipelineId }) {
-  const field = model?.getFieldById(fieldName);
+function PipelineSummaryField(
+  {
+    model,
+    fieldName,
+    pipelineId,
+    className,
+  }) {
   const [isLoading, setIsLoading] = useState(false);
   const [pipeline, setPipeline] = useState(undefined);
   const {
@@ -58,38 +63,47 @@ function PipelineSummaryField({ model, fieldName, pipelineId }) {
     }
   };
 
-  const getPipelineCards = () => {
-    if (pipeline) {
+  const getSummaryCard = () => {
+    if (isMongoDbId(pipelineId) !== true) {
+      return null;
+    }
+
+    if (isLoading) {
       return (
         <PipelineSummaryCard
-          pipelineData={new Model(pipeline, pipelineSummaryMetadata, false)}
+          isLoading={isLoading}
         />
       );
     }
+
+    if (pipeline == null) {
+      return <span>No Pipeline found with ID ({pipelineId}). Access Rules may restrict you from seeing the Pipeline, or the Pipeline may have been deleted.</span>;
+    }
+
+    return (
+      <PipelineSummaryCard
+        pipelineData={new Model(pipeline, pipelineSummaryMetadata, false)}
+      />
+    );
   };
 
-  if (pipelineId == null || pipelineId === "") {
-    return null;
-  }
-
-  if (isLoading) {
-    return <PipelineSummaryCard isLoading={isLoading} />;
-  }
-
-  if (pipeline == null) {
-    return <span>No Pipeline found with ID ({pipelineId}). Access Rules may restrict you from seeing the Pipeline, or the Pipeline may have been deleted.</span>;
-  }
-
   return (
-    <div>
-      <FieldLabel field={field}/>
-      {getPipelineCards()}
+    <div className={className}>
+      <div className={"d-flex"}>
+        <TextFieldBase
+          fieldName={fieldName}
+          showClipboardButton={true}
+          dataObject={model}
+        />
+      </div>
+      {getSummaryCard()}
     </div>
   );
 }
 
 PipelineSummaryField.propTypes = {
   pipelineId: PropTypes.string,
+  className: PropTypes.string,
   model: PropTypes.object,
   fieldName: PropTypes.string,
 };
