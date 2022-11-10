8468284bd9de6010e141159da726d57a1b8179c4
Noah Champoux <noah@opsera.io>
2022-11-10 11:13:44 -0600

    [Styling] Fixed inconsistent padding of screen containers

--- src/components/common/panels/detail_view_container/VanitySetDetailScreenContainer.jsx
+++ src/components/common/panels/detail_view_container/VanitySetDetailScreenContainer.jsx
@@ -6,7 +6,6 @@ import {getBreadcrumb} from "components/common/navigation/trails";
 import RoleRequirementField from "components/common/fields/access/RoleRequirementField";
 import {meetsRequirements} from "components/common/helpers/role-helpers";
 import AccessRoleLevelField from "components/common/fields/access/AccessRoleLevelField";
-import InlineLoadingDialog from "components/common/status_notifications/loading/InlineLoadingDialog";
 import ScreenContainerBodyLoadingDialog
   from "components/common/status_notifications/loading/ScreenContainerBodyLoadingDialog";
 
@@ -132,7 +131,7 @@ function VanitySetDetailScreenContainer(
   }
 
   return (
-    <div className="max-content-width ml-2 max-content-height scroll-y hide-x-overflow">
+    <div className={"max-content-width max-content-height scroll-y hide-x-overflow"}>
       {getTopNavigation()}
       <div className="content-container content-card-1">
         <div className="px-2 content-block-header title-text-header-1">
