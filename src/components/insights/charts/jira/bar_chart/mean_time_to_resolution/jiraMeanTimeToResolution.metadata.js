cde685169f2bef4a3f868ce6c0691342428cdc29
Suriya Prakash <suriya.prakash@opsera.io>
2022-11-24 16:47:35 +0000

    Master opl 7488 maturity score jira

--- src/components/insights/charts/jira/bar_chart/mean_time_to_resolution/jiraMeanTimeToResolution.metadata.js
+++ src/components/insights/charts/jira/bar_chart/mean_time_to_resolution/jiraMeanTimeToResolution.metadata.js
@@ -27,6 +27,44 @@ export const jiraMeanTimeToResolutionMetadata = {
       id: "jira-team-names",
     },
   ],
+  ticketFields:[
+    {
+      label: "Jira Project",
+      id: "issueProjectName"
+    },
+    {
+      label: "ID",
+      id: "issueKey",
+    },
+    {
+      label: "Created Date",
+      id: "issueCreated"
+    },
+    {
+      label: "Priority",
+      id: "issuePriorityName"
+    },
+    {
+      label: "Resolution Date",
+      id: "issueResolutionDate"
+    },
+    {
+      label: "Resolution Time (Hours)",
+      id: "issueResolutionTime"
+    },
+    {
+      label: "Service Component Name",
+      id: "issueServiceComponent"
+    },
+    {
+      label: "Team Name",
+      id: "issueTeamName",
+    },
+    {
+      label: "Status",
+      id: "issueStatusName",
+    },
+  ],
   newObjectFields: {
     tags: [],
     date: undefined,
