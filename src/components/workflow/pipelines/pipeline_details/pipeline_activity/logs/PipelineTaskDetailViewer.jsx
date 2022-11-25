e95c7e8106f44a9ce56f65bfa65654e91753ee37
Noah Champoux <noah@opsera.io>
2022-11-25 13:24:13 -0600

    [Styling] Fixed issue with double scroll bar on Full screen panel

--- src/components/workflow/pipelines/pipeline_details/pipeline_activity/logs/PipelineTaskDetailViewer.jsx
+++ src/components/workflow/pipelines/pipeline_details/pipeline_activity/logs/PipelineTaskDetailViewer.jsx
@@ -76,12 +76,11 @@ function PipelineTaskDetailViewer({ pipelineActivityLogId, pipelineName }) {
   return (
     <FullScreenCenterOverlayContainer
       closePanel={closePanel}
-      showPanel={true}
       titleText={`${pipelineName} Pipeline Task Details`}
       titleIcon={faClipboardList}
       isLoading={isLoading}
     >
-      <div className="m-3">
+      <div className={"m-3"}>
         <PipelineTaskTabPanel pipelineTaskData={pipelineTaskData?.data} />
       </div>
     </FullScreenCenterOverlayContainer>
