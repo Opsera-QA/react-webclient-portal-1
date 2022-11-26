b6e0e600c7c61e9ab064128c493fe0704643ab03
Noah Champoux <noah@opsera.io>
2022-11-26 08:26:06 -0600

    Update PipelineActionControlsRefreshButton.jsx

--- src/components/workflow/pipelines/action_controls/PipelineActionControlsRefreshButton.jsx
+++ src/components/workflow/pipelines/action_controls/PipelineActionControlsRefreshButton.jsx
@@ -17,6 +17,7 @@ export default function PipelineActionControlsRefreshButton(
       onClickFunction={handleRefreshWorkflowClick}
       variant={"secondary"}
       className={""}
+      iconClassName={""}
     />
   );
 }
