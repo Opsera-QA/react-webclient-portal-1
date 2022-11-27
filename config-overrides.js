9af7836ee37ede432dd1c138fee62c2ea29ebf11
Noah Champoux <noah@opsera.io>
2022-11-27 14:40:03 -0600

    scripts upgrade

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const resolve = dir => path.resolve(__dirname, dir);
+
 const override = (config, env) => {
   config.resolve = {
     fallback: {
@@ -6,6 +9,17 @@ const override = (config, env) => {
       "stream": require.resolve("stream-browserify"),
       "util": require.resolve('util'),
     },
+    alias: Object.assign(config.resolve.alias, {
+      "components": resolve("src/components"),
+      "api": resolve("src/api"),
+      "core": resolve("src/core"),
+      "contexts": resolve("src/contexts"),
+      "hooks": resolve("src/hooks"),
+      "markdown": resolve("src/markdown"),
+      "temp-library-components": resolve("src/temp-library-components"),
+      "routes": resolve("src/routes"),
+      "utils": resolve("src/utils"),
+    }),
     extensions: ['.js', '.jsx'],
   };
 
