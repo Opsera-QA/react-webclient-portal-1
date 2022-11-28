9ff0be02eaa68afa3ad749fc8a78a5e6e83c1371
Christina Bowers <christina@opsera.us>
2022-11-28 18:03:10 +0000

    OPL5371 task detail help links

--- src/components/common/help/documentation/tasks/details/GitToGitMergeSyncTaskDetailsHelpDocumentation.jsx
+++ src/components/common/help/documentation/tasks/details/GitToGitMergeSyncTaskDetailsHelpDocumentation.jsx
@@ -14,7 +14,7 @@ function GitToGitMergeSyncTaskDetailsHelpDocumentation() {
   const getHelpDocumentation = () => {
     return (
       <div>
-        The <b>{getTaskTypeLabel(TASK_TYPES.GIT_TO_GIT_MERGE_SYNC)} Task</b> is used to selectively merge changes from a source branch to a target branch. Once the task has been created, you must select the <b>Run Task</b> button to initialize the <b>Git to Git Merge Sync Wizard</b>, verifying any file changes before triggering the merge. For more detailed information on this functionality, view the <a href="https://opsera.atlassian.net/l/c/7hqZQSKQ" target="_blank" rel="noreferrer"><b>Git to Git Sync Merge Task Documentation</b>.</a>
+        The <b>{getTaskTypeLabel(TASK_TYPES.GIT_TO_GIT_MERGE_SYNC)} Task</b> is used to selectively merge changes from a source branch to a target branch. Once the task has been created, you must select the <b>Run Task</b> button to initialize the <b>Git to Git Merge Sync Wizard</b>, verifying any file changes before triggering the merge. For more detailed information on this functionality, view the <a href="https://docs.opsera.io/salesforce/git-to-git-merge-sync-task" target="_blank" rel="noreferrer"><b>Git to Git Sync Merge Task Documentation</b>.</a>
       </div>
     );
   };
