a40b2edf14f478592373f2f51c3c18a21992513a
Noah Champoux <noah@opsera.io>
2022-11-30 15:13:36 -0600

    [Styling/Stability] Fixed height for pipeline activity log task panel. Fixed react crash state for invalid metadata fields

--- src/components/common/panels/general/OverlayTabPanelContainer.jsx
+++ src/components/common/panels/general/OverlayTabPanelContainer.jsx
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 
 function OverlayTabPanelContainer({ tabContainer, currentView }) {
   return (
-    <div className={"full-screen-overlay-tab-panel-body-with-buttons"}>
+    <>
       <div>
           {tabContainer}
       </div>
-      <div className="pt-1 full-screen-overlay-tab-panel-inner-body-with-buttons">
+      <div>
         {currentView}
       </div>
-    </div>
+    </>
   );
 }
 
