d5dafe0b7b7fde03ae0c31cc27cd94dd99fc18d9
Noah Champoux <noah@opsera.io>
2022-12-02 10:09:31 -0600

    OPL-8082   Create new Enable Branch Switching Setting in Pipeline Settings + Start Panel

--- src/components/common/fields/date/DateFieldBase.jsx
+++ src/components/common/fields/date/DateFieldBase.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { format } from "date-fns";
+import {format} from "date-fns";
 import FieldLabel from "components/common/fields/FieldLabel";
 import FieldContainer from "components/common/fields/FieldContainer";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
@@ -28,16 +28,27 @@ export const getFormattedTimestamp = (date) => {
   return getFormattedDate(date, DATE_FORMATS.TIMESTAMP);
 };
 
-function DateFieldBase({fieldName, dataObject, dateFormat, className}) {
+function DateFieldBase(
+  {
+    fieldName,
+    dataObject,
+    dateFormat,
+    className,
+    requireSavedValue,
+  }) {
   const field = dataObject?.getFieldById(fieldName);
 
-  if (dataObject == null || field == null) {
+  if (
+    dataObject == null
+    || field == null
+    || (requireSavedValue === true && DataParsingHelper.parseDate(dataObject?.getData(fieldName)) === false)
+  ) {
     return null;
   }
 
   return (
     <FieldContainer className={className}>
-      <FieldLabel field={field} fieldName={fieldName} /><span>
+      <FieldLabel field={field} fieldName={fieldName}/><span>
       {getFormattedDate(dataObject?.getData(field.id), dateFormat)}
     </span>
     </FieldContainer>
@@ -48,7 +59,8 @@ DateFieldBase.propTypes = {
   fieldName: PropTypes.string,
   dataObject: PropTypes.object,
   dateFormat: PropTypes.string,
-  className: PropTypes.string
+  className: PropTypes.string,
+  requireSavedValue: PropTypes.bool,
 };
 
 DateFieldBase.defaultProps = {
