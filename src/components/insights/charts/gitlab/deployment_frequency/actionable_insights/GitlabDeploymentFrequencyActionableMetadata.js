2569f5fd67149a7bc3230ba75001f872995c45de
DivyeshaMalhotra <divyesha@opsera.us>
2022-12-02 00:42:53 -0500

    changes

--- src/components/insights/charts/gitlab/deployment_frequency/actionable_insights/GitlabDeploymentFrequencyActionableMetadata.js
+++ src/components/insights/charts/gitlab/deployment_frequency/actionable_insights/GitlabDeploymentFrequencyActionableMetadata.js
@@ -1,45 +1,45 @@
 const GitlabDeploymentFrequencyActionableMetadata = {
-    idProperty: "_id",
-    type: "Gitlab Deployment Frequency Report",
-    fields: [
-        {
-            label: "Step Name",
-            id: "stepName",
-        },
-        {
-            label: "Status",
-            id: "stepStatus",
-        },
-        {
-            label: "Duration (seconds)",
-            id: "duration",
-        },
-        {
-            label: "Date Executed",
-            id: "activityDate",
-        },
-        {
-            label: "Commit Id",
-            id: "commitId",
-        },
-        {
-            label: "Pipeline Id",
-            id: "pipelineId",
-        },
-        {
-            label: "Repository Url",
-            id: "repoUrl",
-        },
-        {
-            label: "Repository Name",
-            id: "repoName",
-        },
-        {
-            label: "Branch Name",
-            id: "branch",
-        },
-    ],
-    newObjectFields: {},
+  idProperty: "_id",
+  type: "Gitlab Deployment Frequency Report",
+  fields: [
+    {
+      label: "Step Name",
+      id: "stepName",
+    },
+    {
+      label: "Status",
+      id: "stepStatus",
+    },
+    {
+      label: "Duration (seconds)",
+      id: "duration",
+    },
+    {
+      label: "Date Executed",
+      id: "activityDate",
+    },
+    {
+      label: "Commit Id",
+      id: "commitId",
+    },
+    {
+      label: "Pipeline Id",
+      id: "pipelineId",
+    },
+    {
+      label: "Repository Url",
+      id: "repoUrl",
+    },
+    {
+      label: "Repository Name",
+      id: "repoName",
+    },
+    {
+      label: "Branch Name",
+      id: "branch",
+    },
+  ],
+  newObjectFields: {},
 };
 
 export default GitlabDeploymentFrequencyActionableMetadata;
