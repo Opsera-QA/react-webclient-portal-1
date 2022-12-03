b20a6be6b634b24c11f178f69ae5603c231027f5
Noah Champoux <noah@opsera.io>
2022-12-03 14:04:43 -0600

    [Development Tools] Adjusted new hook

--- src/hooks/workflow/pipelines/orchestration/useGetPipelineOrchestrationStatusById.jsx
+++ src/hooks/workflow/pipelines/orchestration/useGetPipelineOrchestrationStatusById.jsx
@@ -43,8 +43,10 @@ export default function useGetPipelineOrchestrationStatusById(
   };
 
   return ({
-    status: orchestrationStatus.status,
-    isQueued: orchestrationStatus.isQueued,
+    status: DataParsingHelper.parseString(orchestrationStatus.status, "stopped"),
+    isQueued: orchestrationStatus.isQueued === true,
+    lastStep: DataParsingHelper.parseObject(orchestrationStatus.lastStep, {}),
+    runCount: DataParsingHelper.parseInteger(orchestrationStatus.runCount, 0),
     loadData: () => loadData(getOrchestrationStatus, handleErrorFunction),
     isLoading: isLoading,
     error: error,
