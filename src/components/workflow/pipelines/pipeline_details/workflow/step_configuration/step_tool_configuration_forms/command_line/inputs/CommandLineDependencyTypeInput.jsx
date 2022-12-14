9cef101b3936eabd381abaf95aa1454803a4dd58
Syed Faseeh Uddin <faseeh@opsera.io>
2022-12-14 12:45:16 +0000

    TEST PR : OPL-8277: Cleaned up dependency multiselect input and related components

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/command_line/inputs/CommandLineDependencyTypeInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/command_line/inputs/CommandLineDependencyTypeInput.jsx
@@ -6,15 +6,8 @@ import DependencyMultiSelectInput
 function CommandLineDependencyTypeInput({dataObject, setDataObject, disabled}) {
   const setDependencyTypes = (fieldName, selectedOption) => {
     let newDataObject = {...dataObject};
-
-    // Faseeh, this should be dealt with before sending it here. If this isn't good, change the way you store the item inside the base component
-    let dependenciesObj = {};
-    selectedOption.map((item) => {
-      dependenciesObj[item.dependencyType] = item.version;
-    });
-    // console.log(dependenciesObj);
-    newDataObject.setData("dependencyType", selectedOption);
-    newDataObject.setData("dependencies", dependenciesObj);
+    newDataObject.setData("dependencyType", selectedOption.dependencyType);
+    newDataObject.setData("dependencies", selectedOption.dependencies);
     setDataObject({...newDataObject});
   };
 
