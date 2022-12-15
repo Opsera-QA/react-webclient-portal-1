a29a96687a60b85a125515e9a6cf1656c03917d0
Noah Champoux <noah@opsera.io>
2022-12-15 15:46:44 -0600

    OPL-7264   [Roles+UI] Introduce new "Security Manager" & "Auditor" roles (Added temp page link cards)

--- src/components/settings/ldap_site_roles/cards/SiteRoleManagementPageLinkCards.jsx
+++ src/components/settings/ldap_site_roles/cards/SiteRoleManagementPageLinkCards.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import AdministratorsSiteRolePageLinkCard
   from "components/settings/ldap_site_roles/cards/AdministratorsSiteRolePageLinkCard";
+import PowerUsersSiteRolePageLinkCard from "components/settings/ldap_site_roles/cards/PowerUsersSiteRolePageLinkCard";
+import UsersSiteRolePageLinkCard from "components/settings/ldap_site_roles/cards/UsersSiteRolePageLinkCard";
+import AuditorsRolePageLinkCard from "components/settings/ldap_site_roles/cards/AuditorsRolePageLinkCard";
+import SecurityManagersRolePageLinkCard
+  from "components/settings/ldap_site_roles/cards/SecurityManagersRolePageLinkCard";
 
 export default function SiteRoleManagementPageLinkCards() {
   return (
     <div className={"mx-2"}>
       <AdministratorsSiteRolePageLinkCard />
+      <PowerUsersSiteRolePageLinkCard />
+      <UsersSiteRolePageLinkCard />
+      <SecurityManagersRolePageLinkCard />
+      <AuditorsRolePageLinkCard />
     </div>
   );
 }
