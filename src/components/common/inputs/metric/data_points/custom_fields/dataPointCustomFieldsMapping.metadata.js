a27cf3739722eac73ea1e0209242cb8608ff7ce8
Vignesh Subramanian <vignesh@opsera.io>
2022-12-16 13:05:37 +0000

    Test-Master (Jira Id: OPL-8307, OPL-8308) Custom MTTR bug fixes

--- src/components/common/inputs/metric/data_points/custom_fields/dataPointCustomFieldsMapping.metadata.js
+++ src/components/common/inputs/metric/data_points/custom_fields/dataPointCustomFieldsMapping.metadata.js
@@ -4,12 +4,17 @@ const dataPointCustomFieldsMappingMetadata = {
       label: "Allow Custom Fields Mapping",
       id: "enabled",
     },
+    {
+      label: "Use Custom Fields",
+      id: "useCustomFields",
+    },
     {      
       id: "mappedFields",
     },    
   ],
   newObjectFields: {
     enabled: false,
+    useCustomFields: false,
     mappedFields: undefined,
   }
 };
