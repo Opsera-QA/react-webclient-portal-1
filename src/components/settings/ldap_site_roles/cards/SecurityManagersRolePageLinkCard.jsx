4b69889c00f78b6f9847cd1ba126051f6012769e
Noah Champoux <noah@opsera.io>
2022-12-16 14:05:02 -0600

    [Development Tools] Site Role work for when we implement new roles (not in use yet)

--- src/components/settings/ldap_site_roles/cards/SecurityManagersRolePageLinkCard.jsx
+++ src/components/settings/ldap_site_roles/cards/SecurityManagersRolePageLinkCard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import SiteRoleManagementPageLinkCardBase
   from "components/settings/ldap_site_roles/cards/SiteRoleManagementPageLinkCardBase";
+import PropType from "prop-types";
 
-export default function SecurityManagersRolePageLinkCard() {
+export default function SecurityManagersRolePageLinkCard({ securityManagersSiteRole, }) {
+  if (securityManagersSiteRole == null) {
+    return "Security Managers must be activated before you can use it.";
+  }
+
   return (
     <SiteRoleManagementPageLinkCardBase
       breadcrumbDestination={"ldapSecurityManagersSiteRoleDetailView"}
     />
   );
 }
+
+SecurityManagersRolePageLinkCard.propTypes = {
+  securityManagersSiteRole: PropType.object,
+};
