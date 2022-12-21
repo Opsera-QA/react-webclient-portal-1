bc07fb6b5a5f3ebf146ed1fcf11cd207b5044d04
Faseeh <faseeh@opsera.io>
2022-12-22 00:57:47 +0530

    OPL-8329: Cleanedup approval step information on blueprints screen

--- src/components/blueprint/BlueprintSearchResult.jsx
+++ src/components/blueprint/BlueprintSearchResult.jsx
@@ -11,12 +11,13 @@ import {
 import Tab from "react-bootstrap/Tab";
 import ReactJson from "react-json-view";
 import ExportBlueprintLogButton from "components/common/buttons/export/blueprints/ExportBlueprintLogButton";
-import {faDraftingCompass} from "@fortawesome/pro-light-svg-icons";
+import { faDraftingCompass } from "@fortawesome/pro-light-svg-icons";
 import StandaloneTextFieldBase from "components/common/fields/text/standalone/StandaloneTextFieldBase";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import ShowSecurityReportButton from "components/blueprint/security_reports/ShowSecurityReportButton";
 import ShowPackageXmlButton from "components/blueprint/ShowPackageXmlButton";
 import IconBase from "components/common/icons/IconBase";
+import { format } from "date-fns";
 
 function BlueprintSearchResult({ logData, closeModal }) {
   const history = useHistory();
@@ -28,10 +29,17 @@ function BlueprintSearchResult({ logData, closeModal }) {
       if (logData.data.length > 0) {
         if (logData.data[item].api_response) {
           if (logData.data[item].api_response.jenkinsConsoleLog) {
-            completeInput.push(logData.data[item].api_response.jenkinsConsoleLog);
+            completeInput.push(
+              logData.data[item].api_response.jenkinsConsoleLog,
+            );
           } else if (logData.data[item].api_response.jenkins_console_log) {
-            completeInput.push(logData.data[item].api_response.jenkins_console_log);
-          } else if (logData.data[item].step_configuration?.topic === "opsera.pipeline.octopus.console.log") {
+            completeInput.push(
+              logData.data[item].api_response.jenkins_console_log,
+            );
+          } else if (
+            logData.data[item].step_configuration?.topic ===
+            "opsera.pipeline.octopus.console.log"
+          ) {
             completeInput.push(logData.data[item].api_response.message);
           } else if (logData.data[item].api_response.status) {
             completeInput.push(logData.data[item].api_response.status);
@@ -39,7 +47,9 @@ function BlueprintSearchResult({ logData, closeModal }) {
             completeInput.push(logData.data[item].api_response.buildLog);
           } else {
             if (logData.data[item].api_response) {
-              completeInput.push(JSON.stringify(logData.data[item].api_response, undefined, 4));
+              completeInput.push(
+                JSON.stringify(logData.data[item].api_response, undefined, 4),
+              );
             }
           }
         }
@@ -50,7 +60,8 @@ function BlueprintSearchResult({ logData, closeModal }) {
   function makeUpper(item) {
     let stringSplit = item.toLowerCase().split(" ");
     for (let i = 0; i < stringSplit.length; i++) {
-      stringSplit[i] = stringSplit[i].charAt(0).toUpperCase() + stringSplit[i].substring(1);
+      stringSplit[i] =
+        stringSplit[i].charAt(0).toUpperCase() + stringSplit[i].substring(1);
     }
     return stringSplit.join(" ");
   }
@@ -59,18 +70,36 @@ function BlueprintSearchResult({ logData, closeModal }) {
   const getStatusIcon = (item) => {
     switch (item?.current?.status) {
       case "pending":
-        return <IconBase className={"float-right mr-2 mt-1 yellow"} icon={faExclamationCircle} />;
+        return (
+          <IconBase
+            className={"float-right mr-2 mt-1 yellow"}
+            icon={faExclamationCircle}
+          />
+        );
       case "success":
-        return <IconBase className={"float-right mr-2 mt-1 green"} icon={faCheckCircle} />;
+        return (
+          <IconBase
+            className={"float-right mr-2 mt-1 green"}
+            icon={faCheckCircle}
+          />
+        );
       default:
-        return <IconBase className={"float-right mr-2 mt-1 cell-icon red"} icon={faTimesCircle} />;
+        return (
+          <IconBase
+            className={"float-right mr-2 mt-1 cell-icon red"}
+            icon={faTimesCircle}
+          />
+        );
     }
   };
 
   const getRow = (title, text) => {
     return (
       <Row>
-        <Col lg className="py-1">
+        <Col
+          lg
+          className="py-1"
+        >
           <span className="text-muted mr-1">{title}:</span>
           <span className="console-text-invert-modal w-100">{text}</span>
         </Col>
@@ -87,7 +116,9 @@ function BlueprintSearchResult({ logData, closeModal }) {
             <Row>
               <Col>
                 <strong>
-                  <span className="blueprint-step-title">{makeUpper(item.start.step_type)}</span>
+                  <span className="blueprint-step-title">
+                    {makeUpper(item.start.step_type)}
+                  </span>
                 </strong>
                 {getStatusIcon(item)}
               </Col>
@@ -95,25 +126,50 @@ function BlueprintSearchResult({ logData, closeModal }) {
             <hr style={{ margin: " 0.5em auto" }} />
             {getRow("Step Name", makeUpper(item?.start?.step_name))}
             {item?.start?.step_name === "approval" ? (
-              <>{getRow("Requested At", makeUpper(item?.start?.timestamp))}</>
+              <>
+                {getRow(
+                  "Requested At",
+                  format(
+                    new Date(item?.start?.timestamp),
+                    "yyyy-MM-dd', 'hh:mm:ss a",
+                  ),
+                )}
+              </>
             ) : (
               <>
-                {getRow("Requested At", makeUpper(item?.current?.timestamp))}
+                {getRow(
+                  "Requested At",
+                  format(
+                    new Date(item?.current?.timestamp),
+                    "yyyy-MM-dd', 'hh:mm:ss a",
+                  ),
+                )}
                 {getRow("Response Message", makeUpper(item?.current?.message))}
                 <div className={"py-1"}>
                   <div className="text-muted mb-2">API Response</div>
-                  <div><ReactJson src={item.current.data} displayDataTypes={false}  /></div>
+                  <div>
+                    <ReactJson
+                      src={item.current.data}
+                      displayDataTypes={false}
+                    />
+                  </div>
                 </div>
               </>
             )}
-            {item?.start?.step_name === "approval" && item?.current?.status === "success" && (
-              <>
-                {getRow("Approved At", item?.current?.timestamp)}
-                {getRow("Approver", item?.current?.approver)}
-                {getRow("Duration", `${item?.current?.duration} hours`)}
-                {getRow("Approval Message", item?.current?.message)}
-              </>
-            )}
+            {item?.start?.step_name === "approval" &&
+              item?.current?.status === "success" && (
+                <>
+                  {getRow(
+                    "Approved At",
+                    format(
+                      new Date(item?.current?.timestamp),
+                      "yyyy-MM-dd', 'hh:mm:ss a",
+                    ),
+                  )}
+                  {getRow("Approval Message", item?.current?.message)}
+                  {getRow("Duration", `${item?.current?.duration}`)}
+                </>
+              )}
             {getRow("Status", makeUpper(item.current.status))}
           </div>
         </>
@@ -121,7 +177,10 @@ function BlueprintSearchResult({ logData, closeModal }) {
     } else {
       return (
         <div className="mt-2">
-          <ReactJson src={item} displayDataTypes={false} />
+          <ReactJson
+            src={item}
+            displayDataTypes={false}
+          />
         </div>
       );
     }
@@ -142,28 +201,42 @@ function BlueprintSearchResult({ logData, closeModal }) {
         <Row>
           <Col>
             <strong>
-              <div className="blueprint-title">
-                {logData?.title}
-              </div>
+              <div className="blueprint-title">{logData?.title}</div>
             </strong>
           </Col>
-          {!closeModal &&
-            <Button variant="outline-dark mr-3" size="sm" onClick={() => { goToPipeline();}}>
+          {!closeModal && (
+            <Button
+              variant="outline-dark mr-3"
+              size="sm"
+              onClick={() => {
+                goToPipeline();
+              }}
+            >
               <IconBase icon={faDraftingCompass} />
               View Pipeline
             </Button>
-          }
+          )}
         </Row>
-        <hr/>
+        <hr />
         <Row className="mt-1">
           <Col lg>
-            <StandaloneTextFieldBase label={"Pipeline ID"} text={"" + logData?.pipelineId} />
+            <StandaloneTextFieldBase
+              label={"Pipeline ID"}
+              text={"" + logData?.pipelineId}
+            />
           </Col>
           <Col lg>
-            <StandaloneTextFieldBase label={"Run Number"} text={"" + logData?.runNumber} />
+            <StandaloneTextFieldBase
+              label={"Run Number"}
+              text={"" + logData?.runNumber}
+            />
           </Col>
-          <Col lg className="my-2">
-            <span className="text-muted mr-1">Number of Steps:</span> {logData?.data?.length ? logData?.data?.length : "N/A"}
+          <Col
+            lg
+            className="my-2"
+          >
+            <span className="text-muted mr-1">Number of Steps:</span>{" "}
+            {logData?.data?.length ? logData?.data?.length : "N/A"}
           </Col>
         </Row>
       </div>
@@ -175,17 +248,27 @@ function BlueprintSearchResult({ logData, closeModal }) {
     if (Array.isArray(logData?.data) && logData.data.length > 0) {
       return (
         <div className="mb-1 mt-3 bordered-content-block p-3 w-100">
-          <Tab.Container id="left-tabs-example" defaultActiveKey={0}>
+          <Tab.Container
+            id="left-tabs-example"
+            defaultActiveKey={0}
+          >
             <Row>
-              <Col sm={2} className={"pr-0"}>
+              <Col
+                sm={2}
+                className={"pr-0"}
+              >
                 <div className="blueprint-title mb-3">Steps</div>
-                <Nav variant="pills" className="flex-column">
+                <Nav
+                  variant="pills"
+                  className="flex-column"
+                >
                   {logData.data.map((item, idx) => (
                     <Nav.Item key={idx}>
-                      <Nav.Link key={idx} eventKey={idx}>
-                        {makeUpper(
-                          item?.step_name,
-                        )}
+                      <Nav.Link
+                        key={idx}
+                        eventKey={idx}
+                      >
+                        {makeUpper(item?.step_name)}
                       </Nav.Link>
                     </Nav.Item>
                   ))}
@@ -206,43 +289,78 @@ function BlueprintSearchResult({ logData, closeModal }) {
                     <div className="ml-1 blueprint-title">Blueprint</div>
                     <div className={"d-flex"}>
                       <ShowSecurityReportButton logData={logData} />
-                      {!closeModal && <ShowPackageXmlButton logData={logData} />}
+                      {!closeModal && (
+                        <ShowPackageXmlButton logData={logData} />
+                      )}
                       {/*// TODO: Just pass in logData to ExportBlueprintLogButton*/}
-                      {!closeModal && completeInput &&
-                      <ExportBlueprintLogButton
-                        className="mr-2"
-                        blueprintLog={completeInput}
-                        pipelineId={logData?.data[0]?.pipeline_id ? logData.data[0].pipeline_id : "N/A" }
-                        runCount={logData?.data[0]?.run_count ? logData.data[0].run_count : "N/A"}
-                        blueprintName={logData?.title ? logData?.title : "N/A"}
-                        numberOfSteps={logData?.data?.length ? logData?.data?.length : "N/A"}
-                        logData={logData.data}
-                      />
-                      }
+                      {!closeModal && completeInput && (
+                        <ExportBlueprintLogButton
+                          className="mr-2"
+                          blueprintLog={completeInput}
+                          pipelineId={
+                            logData?.data[0]?.pipeline_id
+                              ? logData.data[0].pipeline_id
+                              : "N/A"
+                          }
+                          runCount={
+                            logData?.data[0]?.run_count
+                              ? logData.data[0].run_count
+                              : "N/A"
+                          }
+                          blueprintName={
+                            logData?.title ? logData?.title : "N/A"
+                          }
+                          numberOfSteps={
+                            logData?.data?.length
+                              ? logData?.data?.length
+                              : "N/A"
+                          }
+                          logData={logData.data}
+                        />
+                      )}
                     </div>
                   </div>
                 </Row>
                 <Tab.Content>
                   {logData.data.map((item, idx) => (
-                    <Tab.Pane key={idx} eventKey={idx}>
+                    <Tab.Pane
+                      key={idx}
+                      eventKey={idx}
+                    >
                       {item.api_response.jenkinsConsoleLog ? (
-                        <div key={idx} className="console-text-invert">
+                        <div
+                          key={idx}
+                          className="console-text-invert"
+                        >
                           {item.api_response.jenkinsConsoleLog}
                         </div>
                       ) : item.api_response.jenkins_console_log ? (
-                        <div key={idx} className="console-text-invert">
+                        <div
+                          key={idx}
+                          className="console-text-invert"
+                        >
                           {item.api_response.jenkins_console_log}
                         </div>
-                      ) : item.step_configuration?.topic === "opsera.pipeline.octopus.console.log" ? (
-                        <div key={idx} className="console-text-invert">
+                      ) : item.step_configuration?.topic ===
+                        "opsera.pipeline.octopus.console.log" ? (
+                        <div
+                          key={idx}
+                          className="console-text-invert"
+                        >
                           {item.api_response.message}
                         </div>
                       ) : item.api_response.buildLog ? (
-                        <div key={idx} className="console-text-invert">
+                        <div
+                          key={idx}
+                          className="console-text-invert"
+                        >
                           {item.api_response.buildLog}
                         </div>
                       ) : typeof item.api_response === "string" ? (
-                        <div key={idx} className="console-text-invert">
+                        <div
+                          key={idx}
+                          className="console-text-invert"
+                        >
                           {item.api_response}
                         </div>
                       ) : (
