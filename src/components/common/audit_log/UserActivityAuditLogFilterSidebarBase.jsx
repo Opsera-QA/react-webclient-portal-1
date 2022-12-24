ce9cb45cdc51dc756d5dc9e80fc261a90079762e
Noah Champoux <noah@opsera.io>
2022-12-24 10:24:41 -0600

    [Development Tools] Added new Site Role filter components

--- src/components/common/audit_log/UserActivityAuditLogFilterSidebarBase.jsx
+++ src/components/common/audit_log/UserActivityAuditLogFilterSidebarBase.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import InlineUserFilterSelectInput from "components/common/filters/ldap/owner/InlineUserFilterSelectInput";
 import InlineAuditLogActionsMutiSelectCheckboxFilter
   from "components/common/audit_log/inputs/InlineAuditLogActionsMutiSelectCheckboxFilter";
-import NewDateRangeInput from "components/common/inputs/date/range/NewDateRangeInput";
 import NewInlineDateRangeFilter from "components/common/inputs/date/range/NewInlineDateRangeFilter";
+import InlineSiteRoleMultiSelectCheckboxFilterInput
+  from "components/common/list_of_values_input/ldap/site_roles/InlineSiteRoleMultiSelectCheckboxFilterInput";
 
 export default function UserActivityAuditLogFilterSidebarBase(
   {
@@ -18,6 +19,12 @@ export default function UserActivityAuditLogFilterSidebarBase(
       className={"h-100 w-100 p-2"}
       style={{overflowX: "hidden"}}
     >
+      <InlineSiteRoleMultiSelectCheckboxFilterInput
+        model={userActivityAuditLogFilterModel}
+        setModel={setUserActivityAuditLogFilterModel}
+        loadDataFunction={loadDataFunction}
+        fieldName={"siteRoles"}
+      />
       <InlineUserFilterSelectInput
         fieldName={"user"}
         loadDataFunction={loadDataFunction}
