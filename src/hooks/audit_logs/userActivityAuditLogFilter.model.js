3d410b1ff9de0c693eb486defdec2cae160d0a3f
Noah Champoux <noah@opsera.io>
2023-01-01 15:17:50 -0600

    [Tech Debt] Wired up helper function in Audit Log Filter Model

--- src/hooks/audit_logs/userActivityAuditLogFilter.model.js
+++ src/hooks/audit_logs/userActivityAuditLogFilter.model.js
@@ -112,7 +112,7 @@ export class UserActivityAuditLogFilterModel extends FilterModelBase {
   };
 
   getActionSelectOptionsForType = () => {
-    return auditLogActionConstants.getSelectOptionForUserActivityLogAction(this.type);
+    return auditLogActionConstants.getActionSelectOptionsForType(this.type);
   };
 }
 
