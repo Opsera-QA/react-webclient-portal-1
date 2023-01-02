b7213d1d5f38bdde5550c67f5ce9c53dd40b325b
Noah Champoux <noah@opsera.io>
2023-01-02 08:28:03 -0600

    [UX] Pull repository field from specific metadata stamped field.

--- src/components/workflow/pipelines/pipeline_details/workflow/item/PipelineStepWorkflowItemBody.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/item/PipelineStepWorkflowItemBody.jsx
@@ -11,6 +11,8 @@ import PipelineWorkflowItemActionField
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 import PipelineWorkflowItemFieldBase
   from "components/workflow/pipelines/pipeline_details/workflow/fields/PipelineWorkflowItemFieldBase";
+import metadataConstants from "@opsera/definitions/constants/metadata/metadata.constants";
+import {pipelineStepMetadataConstants} from "components/workflow/pipelines/pipelineStepMetadata.constants";
 
 // TODO: Use the workflow item step field instead of hardcoding these for consistency,
 //  wire up fields based on the tool identifier (pull through metadata based on dynamic field type set)
@@ -51,10 +53,30 @@ export default function PipelineStepWorkflowItemBody(
     }
   };
 
-  // TODO: In the long term we should stamp which field in the metadata should correspond to this,
+  // TODO: In the long term we should create separate workflow panels per step identifier
+  //  the short term solution is to stamp which field in the metadata should correspond to this,
   //  so we can capture cases where they're saved in different fields
+  //  For now, we will fall back to checking for the first relevant field which might be inaccurate.
   const getBranchField = () => {
     const stepConfiguration = DataParsingHelper.parseNestedObject(step, "tool.configuration", {});
+    const metadata = pipelineStepMetadataConstants.getMetadataForIdentifier(toolIdentifier?.identifier);
+    const dynamicSettingsBranchField = metadataConstants.getDynamicTypeSettingField(metadata, metadataConstants.SUPPORTED_DYNAMIC_SETTING_TYPES.PRIMARY_BRANCH);
+
+    if (dynamicSettingsBranchField) {
+      const branch = DataParsingHelper.parseNestedString(stepConfiguration, dynamicSettingsBranchField?.id);
+
+      if (hasStringValue(branch) === true) {
+        return (
+          <PipelineWorkflowItemFieldBase
+            className={"pl-1 pt-1"}
+            icon={faCodeBranch}
+            label={"Branch"}
+            value={branch}
+          />
+        );
+      }
+    }
+
     const branch = stepConfiguration.branch || stepConfiguration.gitBranch || stepConfiguration.defaultBranch;
 
     if (hasStringValue(branch) === true) {
