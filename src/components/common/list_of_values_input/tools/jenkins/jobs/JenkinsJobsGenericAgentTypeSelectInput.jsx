41f79e9245863d255dabcdb11f68c1c5676d6f60
Faseeh <faseeh@opsera.io>
2023-01-05 15:31:53 +0530

    OPL-8428: Moved centOS agent only for shellscript jobs

--- src/components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput.jsx
+++ src/components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput.jsx
@@ -12,22 +12,17 @@ export const JENKINS_JOBS_GENERIC_AGENT_TYPES = [
     "name": "Windows Agent",
     "env" : "windows",
     "value": "generic-windows",
-  },
-  {
-    "name": "CentOS Agent",
-    "env" : "linux",
-    "value": "generic-centos",
   }
 ];
 
-function JenkinsJobsGenericAgentTypeSelectInput({ fieldName, model, setModel, setDataFunction }) {
+function JenkinsJobsGenericAgentTypeSelectInput({ fieldName, model, setModel, setDataFunction, agentList }) {
   return (
     <SelectInputBase
       fieldName={fieldName}
       dataObject={model}
       setDataObject={setModel}
       setDataFunction={setDataFunction}
-      selectOptions={JENKINS_JOBS_GENERIC_AGENT_TYPES}
+      selectOptions={agentList ? agentList :JENKINS_JOBS_GENERIC_AGENT_TYPES}
       valueField="value"
       textField="name"
     />
@@ -39,6 +34,7 @@ JenkinsJobsGenericAgentTypeSelectInput.propTypes = {
   model: PropTypes.object,
   setModel: PropTypes.func,
   setDataFunction: PropTypes.func,
+  agentList: PropTypes.array,
 };
 
 JenkinsJobsGenericAgentTypeSelectInput.defaultProps = {
