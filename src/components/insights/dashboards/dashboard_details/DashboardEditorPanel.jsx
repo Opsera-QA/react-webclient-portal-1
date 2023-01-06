580d45fe8c7220fb76852a51335555abe7a71ac9
tbharadwaj <tejas@opsera.io>
2023-01-06 12:20:57 -0800

    Added changes for saving dashboard settings

--- src/components/insights/dashboards/dashboard_details/DashboardEditorPanel.jsx
+++ src/components/insights/dashboards/dashboard_details/DashboardEditorPanel.jsx
@@ -1,55 +1,81 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Model from "core/data_model/model";
 import { Col, Row } from "react-bootstrap";
 import dashboardsActions from "components/insights/dashboards/dashboards-actions";
 import EditorPanelContainer from "components/common/panels/detail_panel_container/EditorPanelContainer";
 import TextInputBase from "components/common/inputs/text/TextInputBase";
-import DashboardTypeSelectInput
-  from "components/common/list_of_values_input/insights/dashboards/types/DashboardTypeSelectInput";
-import DashboardPersonaSelectInput
-  from "components/common/list_of_values_input/insights/dashboards/DashboardPersonaSelectInput";
+import DashboardTypeSelectInput from "components/common/list_of_values_input/insights/dashboards/types/DashboardTypeSelectInput";
+import DashboardPersonaSelectInput from "components/common/list_of_values_input/insights/dashboards/DashboardPersonaSelectInput";
 import ActivityToggleInput from "components/common/inputs/boolean/ActivityToggleInput";
-import {dashboardAttributesMetadata} from "components/insights/dashboards/dashboard-metadata";
+import { dashboardAttributesMetadata } from "components/insights/dashboards/dashboard-metadata";
 import TagManager from "components/common/inputs/tags/TagManager";
 import RoleAccessInput from "components/common/inputs/roles/RoleAccessInput";
 import useComponentStateReference from "hooks/useComponentStateReference";
 
-function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) {
+function DashboardEditorPanel({
+  dashboardData,
+  setDashboardData,
+  handleClose,
+}) {
   const [dashboardDataDto, setDashboardDataDto] = useState(undefined);
-  const [dashboardAttributesDataDto, setDashboardAttributesDataDto] = useState(new Model({...dashboardAttributesMetadata.newObjectFields}, dashboardAttributesMetadata, false));
+  const [dashboardAttributesDataDto, setDashboardAttributesDataDto] = useState(
+    new Model(
+      { ...dashboardAttributesMetadata.newObjectFields },
+      dashboardAttributesMetadata,
+      false,
+    ),
+  );
   const [isLoading, setIsLoading] = useState(true);
-  const {
-    getAccessToken,
-    isSaasUser,
-    isMounted,
-    cancelTokenSource,
-  } = useComponentStateReference();
+  const { getAccessToken, isSaasUser, isMounted, cancelTokenSource } =
+    useComponentStateReference();
 
   useEffect(() => {
     setIsLoading(true);
     setDashboardDataDto(dashboardData?.clone());
-    setDashboardAttributesDataDto(new Model(dashboardData.getData("attributes"), dashboardAttributesMetadata, false));
+    setDashboardAttributesDataDto(
+      new Model(
+        dashboardData.getData("attributes"),
+        dashboardAttributesMetadata,
+        false,
+      ),
+    );
     setIsLoading(false);
   }, []);
 
   const createDashboard = async () => {
-    const attributes = dashboardAttributesDataDto ? dashboardAttributesDataDto.getPersistData() : {};
+    const attributes = dashboardAttributesDataDto
+      ? dashboardAttributesDataDto.getPersistData()
+      : {};
     dashboardDataDto.setData("attributes", attributes);
-    return await dashboardsActions.createDashboardV2(getAccessToken, cancelTokenSource, dashboardDataDto);
+    return await dashboardsActions.createDashboardV2(
+      getAccessToken,
+      cancelTokenSource,
+      dashboardDataDto,
+    );
   };
 
   const updateDashboard = async () => {
-    const attributes = dashboardAttributesDataDto ? dashboardAttributesDataDto.getPersistData() : {};
+    const attributes = dashboardAttributesDataDto
+      ? dashboardAttributesDataDto.getPersistData()
+      : {};
     dashboardDataDto.setData("attributes", attributes);
-    return await dashboardsActions.updateDashboardV2(getAccessToken, cancelTokenSource, dashboardDataDto);
+    return await dashboardsActions.updateDashboardV2(
+      getAccessToken,
+      cancelTokenSource,
+      dashboardDataDto,
+    );
   };
 
   const getActivityToggleInput = () => {
     if (dashboardDataDto?.isNew() === false) {
       return (
         <Col md={6}>
-          <ActivityToggleInput fieldName={"active"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+          <ActivityToggleInput
+            fieldName={"active"}
+            setDataObject={setDashboardDataDto}
+            dataObject={dashboardDataDto}
+          />
         </Col>
       );
     }
@@ -59,7 +85,10 @@ function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) 
     if (isSaasUser === false) {
       return (
         <Col md={12}>
-          <div className={"bg-white"} style={{borderRadius: "6px"}}>
+          <div
+            className={"bg-white"}
+            style={{ borderRadius: "6px" }}
+          >
             <div>
               <RoleAccessInput
                 setModel={setDashboardDataDto}
@@ -74,9 +103,9 @@ function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) 
   };
 
   if (
-    dashboardData == null
-    || dashboardDataDto == null
-    || (dashboardData?.isNew() !== true && dashboardData?.canUpdate() !== true)
+    dashboardData == null ||
+    dashboardDataDto == null ||
+    (dashboardData?.isNew() !== true && dashboardData?.canUpdate() !== true)
   ) {
     return null;
   }
@@ -94,24 +123,38 @@ function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) 
       <div className={"px-2"}>
         <Row>
           <Col md={6}>
-            <TextInputBase fieldName={"name"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+            <TextInputBase
+              fieldName={"name"}
+              setDataObject={setDashboardDataDto}
+              dataObject={dashboardDataDto}
+            />
           </Col>
           <Col md={6}>
-            <DashboardTypeSelectInput dataObject={dashboardDataDto} setDataObject={setDashboardDataDto}/>
+            <DashboardTypeSelectInput
+              dataObject={dashboardDataDto}
+              setDataObject={setDashboardDataDto}
+            />
           </Col>
           <Col md={6}>
-            <DashboardPersonaSelectInput dataObject={dashboardAttributesDataDto} setDataObject={setDashboardAttributesDataDto}/>
+            <DashboardPersonaSelectInput
+              dataObject={dashboardAttributesDataDto}
+              setDataObject={setDashboardAttributesDataDto}
+            />
           </Col>
           {/*<Col md={6}>*/}
           {/*  <DashboardAccessSelectInput dataObject={dashboardDataDto} setDataObject={setDashboardDataDto} disabled={["public"]}/>*/}
           {/*</Col>*/}
           {getRolesInput()}
           <Col md={12}>
-            <TextInputBase fieldName={"description"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
+            <TextInputBase
+              fieldName={"description"}
+              setDataObject={setDashboardDataDto}
+              dataObject={dashboardDataDto}
+            />
           </Col>
-          <Col md={6}>
+          {/* <Col md={6}>
             <TagManager type={"dashboard"} setDataObject={setDashboardDataDto} dataObject={dashboardDataDto}/>
-          </Col>
+          </Col> */}
           {getActivityToggleInput()}
         </Row>
       </div>
@@ -122,9 +165,7 @@ function DashboardEditorPanel({ dashboardData, setDashboardData, handleClose }) 
 DashboardEditorPanel.propTypes = {
   dashboardData: PropTypes.object,
   setDashboardData: PropTypes.func,
-  handleClose: PropTypes.func
+  handleClose: PropTypes.func,
 };
 
 export default DashboardEditorPanel;
-
-
