4950e1468e8caf4daa60ea57895ae2193dbee320
faseehOpsera <60170774+faseehOpsera@users.noreply.github.com>
2023-01-13 20:19:44 +0530

    [Cleanup] Azure webhook trigger fixes and clean up (#75)

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/source-repository-actions.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/source-repository-actions.js
@@ -5,10 +5,12 @@ const SourceRepositoryActions = {};
 SourceRepositoryActions.registerHook = async (getAccessToken, cancelTokenSource, pipelineOwner, pipelineId, sourceRepositoryModel) => {
   const nodeUrl = process.env.REACT_APP_OPSERA_API_SERVER_URL;
   const hookUrl = encodeURIComponent(`${nodeUrl}/hooks/${pipelineOwner}/${pipelineId}/source`);
+  console.log(sourceRepositoryModel.getPersistData());
   const apiUrl = `/connectors/${sourceRepositoryModel?.getData("service")}/${sourceRepositoryModel?.getData("accountId")}/hook/create`;
   const queryParams = {
     params: {
       repo: sourceRepositoryModel?.getData("repoId"),
+      projectId: sourceRepositoryModel?.getData("projectId"),
       hook: hookUrl,
       branch: sourceRepositoryModel?.getData("branch"),
       workspace: sourceRepositoryModel?.getData("workspace"),
