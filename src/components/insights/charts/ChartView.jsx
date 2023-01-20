f1b450e3eb7ba9c638dfd9e3fcdcb80c5334876b
Matthew Greci <mgreci@opsera.io>
2023-01-20 10:22:58 -0700

    junit is full screen kpi

--- src/components/insights/charts/ChartView.jsx
+++ src/components/insights/charts/ChartView.jsx
@@ -1141,7 +1141,7 @@ function ChartView({ kpiConfiguration, dashboardData, index, loadChart, setKpis 
       // Junit KPIs
       case "junit-test-results":
         return (
-          <Col xl={6} md={12} className="p-2">
+          <Col md={12} className="p-2">
             <JunitTestResultsTable
               kpiConfiguration={kpiConfig}
               setKpiConfiguration={setKpiConfig}
