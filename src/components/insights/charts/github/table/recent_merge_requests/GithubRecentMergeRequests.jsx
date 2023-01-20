a5caa290b573991623ca5694d3ba9194ef0facaf
Matthew Greci <mgreci@opsera.io>
2023-01-20 07:30:35 -0700

    use time display as in gitlab recent mr kpi

--- src/components/insights/charts/github/table/recent_merge_requests/GithubRecentMergeRequests.jsx
+++ src/components/insights/charts/github/table/recent_merge_requests/GithubRecentMergeRequests.jsx
@@ -15,7 +15,7 @@ import {
   getLimitedTableTextColumn,
   getTableDateTimeColumn,
   getTableTextColumn,
-  getTableSmartTimeDurationTextColumn,
+  getTableHourDurationTextColumn
 } from "components/common/table/table-column-helpers";
 import { getField } from "components/common/metadata/metadata-helpers";
 
@@ -42,7 +42,7 @@ function GithubRecentMergeRequests({ kpiConfiguration, setKpiConfiguration, dash
         getLimitedTableTextColumn(getField(fields, "ProjectName"), 20),
         getLimitedTableTextColumn(getField(fields, "BranchName"), 20),
         getTableDateTimeColumn(getField(fields, "mrCompletionTimeTimeStamp")),
-        getTableSmartTimeDurationTextColumn(getField(fields, "MergeRequestTimeTaken"))
+        getTableHourDurationTextColumn(getField(fields, "MergeRequestTimeTaken"))
       ],
       []
   );
