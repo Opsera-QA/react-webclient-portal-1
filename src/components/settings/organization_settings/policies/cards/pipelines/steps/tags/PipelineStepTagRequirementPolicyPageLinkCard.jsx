f25b3154bcec71403b05e1ff5ff7fd81c2ba793b
Noah Champoux <noah@opsera.io>
2023-01-21 13:24:14 -0600

    Update PipelineStepTagRequirementPolicyPageLinkCard.jsx

--- src/components/settings/organization_settings/policies/cards/pipelines/steps/tags/PipelineStepTagRequirementPolicyPageLinkCard.jsx
+++ src/components/settings/organization_settings/policies/cards/pipelines/steps/tags/PipelineStepTagRequirementPolicyPageLinkCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import PropType from "prop-types";
-import InactivePipelinePublishingPolicyPageLinkCard
-  from "components/settings/organization_settings/policies/cards/pipelines/publishing/InactivePipelinePublishingPolicyPageLinkCard";
 import PolicyManagementPageLinkCardBase
   from "components/settings/organization_settings/policies/cards/PolicyManagementPageLinkCardBase";
+import InactivePipelineStepTagRequirementPolicyPageLinkCard
+  from "components/settings/organization_settings/policies/cards/pipelines/steps/tags/InactivePipelineStepTagRequirementPolicyPageLinkCard";
 
 export default function PipelineStepTagRequirementPolicyPageLinkCard({ pipelineStepTagRequirementPolicy, }) {
   if (pipelineStepTagRequirementPolicy == null) {
     return (
-      <InactivePipelinePublishingPolicyPageLinkCard />
+      <InactivePipelineStepTagRequirementPolicyPageLinkCard />
     );
   }
 
