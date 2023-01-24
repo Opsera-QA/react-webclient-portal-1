d10915ddc3da8a70947cb124d5d532d35c4251c9
Matthew Greci <mgreci@opsera.io>
2023-01-23 21:58:15 -0700

    display Time Taken in github pending mr kpi

--- src/components/insights/charts/github/table/pending_merge_requests/github-pending-merge-requests-metadata.js
+++ src/components/insights/charts/github/table/pending_merge_requests/github-pending-merge-requests-metadata.js
@@ -42,6 +42,10 @@ const githubPendingMergeRequestsMetadata = {
         label: "Time",
         id: "mrCompletionTimeTimeStamp"
       },
+      {
+        label: "Time Taken",
+        id: "MergeRequestTimeTaken"
+      },
     ],
     newObjectFields: {
       pageSize: 5,
