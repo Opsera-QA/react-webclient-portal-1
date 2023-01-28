181261b7bd316c1c764d8059eb9c46db10a1feff
Noah Champoux <noah@opsera.io>
2023-01-28 10:22:07 -0600

    Minor tweaks

--- src/components/settings/data_mapping/projects/ProjectDataMappingManagement.jsx
+++ src/components/settings/data_mapping/projects/ProjectDataMappingManagement.jsx
@@ -1,46 +1,38 @@
 import React, {useState, useEffect, useContext, useRef} from "react";
-import {AuthContext} from "contexts/AuthContext";
 import {DialogToastContext} from "contexts/DialogToastContext";
-import axios from "axios";
 import ProjectDataMappingsTable from "components/settings/data_mapping/projects/ProjectDataMappingsTable";
-import {projectDataMappingActions} from "components/settings/data_mapping/projects/projectDataMapping.actions";
 import Model from "../../../../core/data_model/model";
 import projectMappingMetadata from "components/settings/data_mapping/projects/projectDataMappingFilter.metadata.js";
+import useAnalyticsDataProjectMappingActions
+  from "hooks/settings/insights/analytics_data_mappings/projects/useAnalyticsDataProjectMappingActions";
+import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 
 function ProjectDataMappingManagement() {
   const toastContext = useContext(DialogToastContext);
-  const {getAccessToken} = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
   const [projectDataMappings, setProjectDataMappings] = useState([]);
   const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
-  const [toolFilterDto, setToolFilterDto] = useState(new Model({...projectMappingMetadata.newObjectFields}, projectMappingMetadata, false));
+  const [projectDataMappingFilterModel, setProjectDataMappingFilterModel] = useState(new Model({...projectMappingMetadata.newObjectFields}, projectMappingMetadata, false));
+  const analyticsDataProjectMappingActions = useAnalyticsDataProjectMappingActions();
 
   useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
     isMounted.current = true;
 
-    loadData(source).catch((error) => {
+    loadData().catch((error) => {
       if (isMounted?.current === true) {
         throw error;
       }
     });
 
     return () => {
-      source.cancel();
       isMounted.current = false;
     };
   }, []);
 
-  const loadData = async (cancelSource = cancelTokenSource) => {
+  const loadData = async () => {
     try {
       setIsLoading(true);
-      await getProjectDataMappings(cancelSource);
+      await getProjectDataMappings();
     } catch (error) {
       if (isMounted?.current === true) {
         toastContext.showLoadingErrorDialog(error);
@@ -52,18 +44,14 @@ function ProjectDataMappingManagement() {
     }
   };
 
-  const getProjectDataMappings = async (cancelSource = cancelTokenSource, filterDto = toolFilterDto) => {
-    try {
-      const response = await projectDataMappingActions.getProjectDataMappingsV2(getAccessToken, cancelSource, filterDto);
-      const mappings = response?.data?.data;
+  const getProjectDataMappings = async (filterDto = projectDataMappingFilterModel) => {
+    const response = await analyticsDataProjectMappingActions.getProjectDataMappings(filterDto);
+    const mappings = DataParsingHelper.parseNestedArray(response, "data.data");
 
-      if (isMounted?.current === true && Array.isArray(mappings)) {
-        setProjectDataMappings(mappings);
-        filterDto.setData("activeFilters", filterDto?.getActiveFilters());
-        setToolFilterDto({...filterDto});
-      }
-    } catch (error) {
-      toastContext.showLoadingErrorDialog(error);
+    if (isMounted?.current === true && Array.isArray(mappings)) {
+      setProjectDataMappings(mappings);
+      filterDto.setData("activeFilters", filterDto?.getActiveFilters());
+      setProjectDataMappingFilterModel({...filterDto});
     }
   };
 
@@ -75,8 +63,8 @@ function ProjectDataMappingManagement() {
         isLoading={isLoading}
         projectDataMappings={projectDataMappings}
         isMounted={isMounted}
-        toolFilterDto={toolFilterDto}
-        setToolFilterDto={setToolFilterDto}
+        toolFilterDto={projectDataMappingFilterModel}
+        setToolFilterDto={setProjectDataMappingFilterModel}
       />
     </div>
   );
