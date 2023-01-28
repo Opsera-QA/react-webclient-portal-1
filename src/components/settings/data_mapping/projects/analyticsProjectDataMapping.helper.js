7d6182a69b3f2de95e2dc6e462734e2dc0308db8
Noah Champoux <noah@opsera.io>
2023-01-28 11:08:43 -0600

    Update analyticsProjectDataMapping.helper.js

--- src/components/settings/data_mapping/projects/analyticsProjectDataMapping.helper.js
+++ src/components/settings/data_mapping/projects/analyticsProjectDataMapping.helper.js
@@ -3,7 +3,7 @@ import {isMongoDbId} from "components/common/helpers/mongo/mongoDb.helpers";
 export const analyticsProjectDataMappingHelper = {};
 
 analyticsProjectDataMappingHelper.getManagementScreenLink = () => {
-  return `/settings/data_mapping/projects`;
+  return `/settings/data_mapping`;
 };
 
 analyticsProjectDataMappingHelper.getDetailViewLink = (analyticsDataEntryId) => {
