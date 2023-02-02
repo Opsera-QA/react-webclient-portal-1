3553c5104d3a928267c7deb2cd7cabbdd8867e39
Noah Champoux <noah@opsera.io>
2023-02-02 11:41:00 -0600

    OPL-8700  Add Audit Log Support to Analytics Data Mappings: Organizations

--- src/components/settings/organizations/NewOrganizationOverlay.jsx
+++ src/components/settings/organizations/NewOrganizationOverlay.jsx
@@ -2,14 +2,15 @@ import React, {useContext, useState} from "react";
 import PropTypes from "prop-types";
 import {DialogToastContext} from "contexts/DialogToastContext";
 import CreateCenterPanel from "components/common/overlays/center/CreateCenterPanel";
-import Model from "core/data_model/model";
 import {organizationMetadata} from "components/settings/organizations/organization.metadata";
 import OrganizationEditorPanel
   from "components/settings/organizations/organization_detail_view/OrganizationEditorPanel";
+import useGetOrganizationModel from "hooks/settings/insights/organizations/useGetOrganizationModel";
 
 function NewOrganizationOverlay({ isMounted, loadData }) {
   const toastContext = useContext(DialogToastContext);
-  const [organizationData, setOrganizationData] = useState(new Model({...organizationMetadata.newObjectFields}, organizationMetadata, true));
+  const getOrganizationModel = useGetOrganizationModel();
+  const [organizationModel, setOrganizationModel] = useState(getOrganizationModel(undefined, true));
 
   const closePanel = () => {
     if (isMounted?.current === true) {
@@ -21,8 +22,18 @@ function NewOrganizationOverlay({ isMounted, loadData }) {
   };
 
   return (
-    <CreateCenterPanel closePanel={closePanel} objectType={organizationMetadata.type} loadData={loadData}>
-      <OrganizationEditorPanel setOrganizationData={setOrganizationData} handleClose={closePanel} organizationData={organizationData}/>
+    <CreateCenterPanel
+      closePanel={closePanel}
+      objectType={organizationMetadata.type}
+      loadData={loadData}
+    >
+      <div className={"px-3 pb-3 pt-1"}>
+        <OrganizationEditorPanel
+          setOrganizationData={setOrganizationModel}
+          handleClose={closePanel}
+          organizationData={organizationModel}
+        />
+      </div>
     </CreateCenterPanel>
   );
 }
