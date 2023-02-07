4d859a7d4bd45886f240d81b26ef0936fb60bd66
Matthew Greci <mgreci@opsera.io>
2023-02-07 12:23:27 -0700

    query projects by tag with new api

--- src/components/settings/tags/admin-tags-actions.js
+++ src/components/settings/tags/admin-tags-actions.js
@@ -125,6 +125,11 @@ adminTagsActions.getRelevantDashboardsV2 = async (getAccessToken, cancelTokenSou
   return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, tags);
 };
 
+adminTagsActions.getAllProjectsWithTags = async (getAccessToken, cancelTokenSource, tags) => {
+  const apiUrl = `/reports/projects/all-tags`;
+  return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl, tags);
+};
+
 adminTagsActions.subscribeToTag = async (getAccessToken, cancelTokenSource, tagId) => {
   const apiUrl = `/tags/${tagId}/subscribe`;
   return await baseActions.apiPostCallV2(getAccessToken, cancelTokenSource, apiUrl);
