ae48246387189a769895beed6c9b93aacb6b10fb
Noah Champoux <noah@opsera.io>
2023-02-14 10:51:05 -0600

    Update notificationTypes.constants.js

--- src/components/common/list_of_values_input/notifications/type/notificationTypes.constants.js
+++ src/components/common/list_of_values_input/notifications/type/notificationTypes.constants.js
@@ -1,9 +1,11 @@
 export const NOTIFICATION_TYPES = {
+  AUDIT_LOG: "audit_log",
   PIPELINE: "pipeline",
   METRIC: "metric",
 };
 
 export const NOTIFICATION_TYPE_LABELS = {
+  AUDIT_LOG: "Audit Log",
   PIPELINE: "Pipeline",
   METRIC: "Metric",
 };
@@ -13,6 +15,10 @@ export const NOTIFICATION_TYPE_SELECT_OPTIONS = [
   //   value: NOTIFICATION_TYPES.METRIC,
   //   text: NOTIFICATION_TYPES_LABELS.METRIC
   // },
+  {
+    value: NOTIFICATION_TYPES.AUDIT_LOG,
+    text: NOTIFICATION_TYPE_LABELS.AUDIT_LOG
+  },
   {
     value: NOTIFICATION_TYPES.PIPELINE,
     text: NOTIFICATION_TYPE_LABELS.PIPELINE
@@ -25,6 +31,8 @@ export const getNotificationTypeLabel = (notificationType) => {
       return NOTIFICATION_TYPE_LABELS.PIPELINE;
     case NOTIFICATION_TYPES.METRIC:
       return NOTIFICATION_TYPE_LABELS.METRIC;
+    case NOTIFICATION_TYPES.AUDIT_LOG:
+      return NOTIFICATION_TYPE_LABELS.AUDIT_LOG;
     default:
       return notificationType;
   }
