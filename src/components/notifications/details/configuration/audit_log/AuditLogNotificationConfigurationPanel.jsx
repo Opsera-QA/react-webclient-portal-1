b0856a7e22a00abe6907344d560f97b099ae87ea
Noah Champoux <noah@opsera.io>
2023-02-16 10:32:54 -0600

    OPL-8995  Pipieline_UI : Azure: The Azure registry and Repository name are disabled once the Azure credential is clicked on clear

--- src/components/notifications/details/configuration/audit_log/AuditLogNotificationConfigurationPanel.jsx
+++ src/components/notifications/details/configuration/audit_log/AuditLogNotificationConfigurationPanel.jsx
@@ -13,6 +13,7 @@ import AuditLogActionsMultiSelectCheckboxInput
 import AuditLogNotificationTargetMultiSelectInput
   from "components/notifications/details/configuration/audit_log/inputs/AuditLogNotificationTargetMultiSelectInput";
 import WarningMessageFieldBase from "components/common/fields/text/message/WarningMessageFieldBase";
+import {hasStringValue} from "components/common/helpers/string-helpers";
 
 // TODO: If this is used in multiple spots, we should rename it. If it just has trigger, I would suggest TriggerNotificationEditorPanel
 export default function AuditLogNotificationConfigurationPanel(
@@ -29,6 +30,19 @@ export default function AuditLogNotificationConfigurationPanel(
     setNotificationConfigurationModel({...configurationData});
   };
 
+  const getWarningMessage = () => {
+    if (hasStringValue(notificationModel?.getData("method")) === true) {
+      return (
+        <Col lg={12}>
+          <WarningMessageFieldBase
+            className={"mt-2 mb-1"}
+            message={"Please Note: You can only subscribe to receive Notifications for items you have access to view"}
+          />
+        </Col>
+      );
+    }
+  };
+
   if (notificationModel == null || notificationConfigurationModel == null) {
     return (<LoadingDialog size="sm"/>);
   }
@@ -43,12 +57,7 @@ export default function AuditLogNotificationConfigurationPanel(
           setNotificationConfigurationModel={setNotificationConfigurationModel}
         />
       </Col>
-      <Col lg={12}>
-        <WarningMessageFieldBase
-          className={"mt-2 mb-1"}
-          message={"Please Note: You can only subscribe to receive Notifications for items you have access to view"}
-        />
-      </Col>
+      {getWarningMessage()}
       <Col lg={12}>
         <AuditLogNotificationTargetMultiSelectInput
           model={notificationModel}
