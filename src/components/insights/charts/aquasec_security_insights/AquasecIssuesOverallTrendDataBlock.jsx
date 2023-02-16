94cfb4ef93c24e6b9155a5001fd817b309421c27
Matthew Greci <mgreci@opsera.io>
2023-02-15 17:46:11 -0700

    use getTrend functions

--- src/components/insights/charts/aquasec_security_insights/AquasecIssuesOverallTrendDataBlock.jsx
+++ src/components/insights/charts/aquasec_security_insights/AquasecIssuesOverallTrendDataBlock.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
 import ThreeLineScoreDataBlock from "components/common/metrics/score/ThreeLineScoreDataBlock";
-import { ICON_FROM_TREND } from "./constants";
+import { getTrend, getTrendDescription, getTrendIcon } from "../charts-helpers";
 
 function AquasecIssuesOverallTrendDataBlock({ score, severity, trend, onSelect, lastScore }) {
   const displayScore = score || 0;
-  const { icon, color, description } = ICON_FROM_TREND[trend];
+  const color = getTrend(score, lastScore);
+  const icon = getTrendIcon(color);
+  const description = getTrendDescription(color);
   
   return (
     <DataBlockBoxContainer showBorder={true} onClickFunction={() => onSelect(severity)}>
