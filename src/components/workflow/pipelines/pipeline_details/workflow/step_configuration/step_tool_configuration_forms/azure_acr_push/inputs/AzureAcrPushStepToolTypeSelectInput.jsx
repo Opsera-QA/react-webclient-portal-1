d35a9e524bdf1a6f576f805443953242cc9764fe
Noah Champoux <noah@opsera.io>
2023-02-17 06:58:17 -0600

    OPL-8995   Pipieline_UI : Azure: The Azure registry and Repository name are disabled once the Azure credential is clicked on clear

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/azure_acr_push/inputs/AzureAcrPushStepToolTypeSelectInput.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+
+const ACTION_LIST = [
+  {
+    name: "Azure Tool",
+    value: "azure",
+  },
+  {
+    name: "Legacy Azure Connector",
+    value: "azure_account",
+  },
+];
+
+function AzureAcrPushStepToolTypeSelectInput(
+  {
+    model,
+    setModel,
+    disabled,
+  }) {
+  const setDataFunction = async (fieldName, value) => {
+    model.setData("toolType", value?.value);
+    model.setDefaultValue("azureRegistryName");
+    model.setDefaultValue("azureRepoName");
+    model.setDefaultValue("acrLoginUrl");
+    model.setDefaultValue("azureCredentialId");
+    model.setDefaultValue("azureToolConfigId");
+    model.setDefaultValue("newRepo");
+    setModel({ ...model });
+  };
+
+  const clearDataFunction = () => {
+    model.setDefaultValue("toolType");
+    model.setDefaultValue("azureRegistryName");
+    model.setDefaultValue("azureRepoName");
+    model.setDefaultValue("acrLoginUrl");
+    model.setDefaultValue("azureCredentialId");
+    model.setDefaultValue("azureToolConfigId");
+    model.setDefaultValue("newRepo");
+    setModel({ ...model });
+  };
+
+  return (
+    <SelectInputBase
+      fieldName={"toolType"}
+      model={model}
+      setModel={setModel}
+      setDataFunction={setDataFunction}
+      clearDataFunction={clearDataFunction}
+      selectOptions={ACTION_LIST}
+      valueField={"value"}
+      textField={"name"}
+      singularTopic={"Tool Type"}
+      pluralTopic={"Tool Types"}
+      disabled={disabled}
+    />
+  );
+}
+
+AzureAcrPushStepToolTypeSelectInput.propTypes = {
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  disabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+export default AzureAcrPushStepToolTypeSelectInput;
