ed3be22e9daf0f9e348b13750a8dfd5ce9f0c9df
Matthew Greci <mgreci@opsera.io>
2023-02-17 14:52:32 -0700

    use reverse trend

--- src/components/insights/charts/aquasec_security_insights/AquasecIssuesOverallTrendDataBlock.jsx
+++ src/components/insights/charts/aquasec_security_insights/AquasecIssuesOverallTrendDataBlock.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
 import ThreeLineScoreDataBlock from "components/common/metrics/score/ThreeLineScoreDataBlock";
-import { getTrend, getTrendDescription, getTrendIcon } from "../charts-helpers";
+import { getReverseTrend, getTrendDescription, getTrendIcon } from "../charts-helpers";
 
 function AquasecIssuesOverallTrendDataBlock({ score, severity, onSelect, lastScore }) {
   const displayScore = score || 0;
-  const color = getTrend(score, lastScore);
+  const color = getReverseTrend(score, lastScore);
   const icon = getTrendIcon(color);
   const description = getTrendDescription(color);
   
