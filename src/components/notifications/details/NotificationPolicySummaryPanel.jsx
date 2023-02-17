287e41caaf1b6824bba4279c1fe2d02f60dc8970
Noah Champoux <noah@opsera.io>
2023-02-17 16:09:08 -0600

    Removed duplicate file

--- src/components/notifications/details/NotificationPolicySummaryPanel.jsx
+++ src/components/notifications/details/NotificationPolicySummaryPanel.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import NotificationPolicySummaryPanelBase from "components/notifications/details/NotificationPolicySummaryPanelBase";
 import pipelineNotificationConfigurationMetadata
   from "components/notifications/details/configuration/pipeline/pipeline-notification-configuration-metadata";
 import metricNotificationConfigurationMetadata
@@ -15,6 +14,15 @@ import AuditLogNotificationTypeSummaryCard
 import {
   auditLogNotificationConfigurationMetadata
 } from "components/notifications/details/configuration/audit_log/auditLogNotificationConfigurationMetadata";
+import SummaryPanelContainer from "components/common/panels/detail_view/SummaryPanelContainer";
+import {Col, Row} from "react-bootstrap";
+import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import NotificationTypeField from "components/common/list_of_values_input/notifications/type/NotificationTypeField";
+import SsoUserField from "components/common/list_of_values_input/users/sso/user/SsoUserField";
+import SmartIdField from "components/common/fields/text/id/SmartIdField";
+import DateFieldBase from "components/common/fields/date/DateFieldBase";
+import TagField from "components/common/fields/multiple_items/tags/TagField";
+import NotificationsField from "components/common/fields/notifications/NotificationsField";
 
 function NotificationPolicySummaryPanel({ notificationData, setActiveTab }) {
   const getNotificationTypeSummaryPanel = () => {
@@ -46,11 +54,60 @@ function NotificationPolicySummaryPanel({ notificationData, setActiveTab }) {
   };
 
   return (
-    <NotificationPolicySummaryPanelBase
-      notificationData={notificationData}
-      setActiveTab={setActiveTab}
-      notificationTypeSummaryCard={getNotificationTypeSummaryPanel()}
-    />
+    <SummaryPanelContainer setActiveTab={setActiveTab}>
+      <Row>
+        <Col lg={6}>
+          <TextFieldBase
+            dataObject={notificationData}
+            fieldName={"name"}
+          />
+        </Col>
+        <Col lg={6}>
+          <NotificationTypeField
+            model={notificationData}
+            fieldName={"type"}
+          />
+        </Col>
+        <Col lg={6}>
+          <SsoUserField
+            model={notificationData}
+            fieldName={"owner"}
+          />
+        </Col>
+        <Col lg={6}>
+          <SmartIdField
+            model={notificationData}
+          />
+        </Col>
+        <Col lg={12}>
+          <TextFieldBase
+            dataObject={notificationData}
+            fieldName={"description"}
+          />
+        </Col>
+        <Col lg={6}>
+          <DateFieldBase
+            dataObject={notificationData}
+            fieldName={"createdAt"}
+          />
+        </Col>
+        <Col lg={12}>
+          <TagField
+            dataObject={notificationData}
+            fieldName={"tags"}
+          />
+        </Col>
+        <Col lg={12}>
+          {getNotificationTypeSummaryPanel()}
+        </Col>
+        <Col lg={12}>
+          <NotificationsField
+            fieldName={"notifications"}
+            model={notificationData}
+          />
+        </Col>
+      </Row>
+    </SummaryPanelContainer>
   );
 }
 
