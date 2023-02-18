99745c8d41f6f9943e0b46e3b45121f808481832
Noah Champoux <noah@opsera.io>
2023-02-18 10:09:21 -0600

    New components

---
+++ src/temp-library-components/button/remove/RemoveShownButtonBase.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {faMinusCircle} from "@fortawesome/pro-light-svg-icons";
+import VanityButtonBase from "temp-library-components/button/VanityButtonBase";
+
+export default function RemoveShownButtonBase(
+  {
+    onClickFunction,
+    buttonState,
+    disabled,
+    icon,
+    className,
+    normalText,
+    successText,
+    errorText,
+    busyText,
+    itemCount,
+    buttonSize,
+    buttonClassName,
+  }) {
+  return (
+    <VanityButtonBase
+      className={className}
+      icon={icon}
+      disabled={itemCount === 0 || disabled}
+      buttonState={buttonState}
+      onClickFunction={onClickFunction}
+      variant={"outline-danger"}
+      successText={successText}
+      errorText={errorText}
+      busyText={busyText}
+      normalText={normalText}
+      buttonSize={buttonSize}
+      badgeText={itemCount}
+      buttonClassName={buttonClassName}
+    />
+  );
+}
+
+RemoveShownButtonBase.propTypes = {
+  buttonState: PropTypes.string,
+  onClickFunction: PropTypes.func,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  icon: PropTypes.object,
+  normalText: PropTypes.string,
+  successText: PropTypes.string,
+  errorText: PropTypes.string,
+  busyText: PropTypes.string,
+  itemCount: PropTypes.number,
+  buttonSize: PropTypes.string,
+  buttonClassName: PropTypes.string,
+};
+
+RemoveShownButtonBase.defaultProps = {
+  icon: faMinusCircle,
+  normalText: "Remove Shown",
+};
