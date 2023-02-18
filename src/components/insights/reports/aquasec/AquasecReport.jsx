b333a1f1c901249ebf446cdc3b825017b4c2ecfc
DivyeshaMalhotra <divyesha@opsera.us>
2023-02-18 14:25:59 -0500

    changes

---
+++ src/components/insights/reports/aquasec/AquasecReport.jsx
@@ -0,0 +1,123 @@
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import axios from "axios";
+import AquasecScanReportTable from "./AquasecScanReportTable";
+import { AuthContext } from "contexts/AuthContext";
+import actionableInsightsGenericChartFilterMetadata
+    from "components/insights/charts/generic_filters/actionableInsightsGenericChartFilterMetadata";
+import Model from "core/data_model/model";
+import ScreenContainer from "components/common/panels/general/ScreenContainer";
+import InsightsSubNavigationBar from "components/insights/InsightsSubNavigationBar";
+import PropTypes from "prop-types";
+import aquasecActions from "../../charts/aquasec_security_insights/aquasec.action";
+
+function AquasecReport({ kpiConfiguration, dashboardData }) {
+    const { pipelineId, imageName, severity } = useParams();
+    const history = useHistory();
+    const { getAccessToken } = useContext(AuthContext);
+    const [error, setError] = useState(undefined);
+    const [metrics, setMetrics] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const isMounted = useRef(false);
+    const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+    const [filterModel, setFilterModel] = useState(
+        new Model(
+            { ...actionableInsightsGenericChartFilterMetadata.newObjectFields },
+            actionableInsightsGenericChartFilterMetadata,
+            false
+        )
+    );
+
+    useEffect(() => {
+        if (cancelTokenSource) {
+            cancelTokenSource.cancel();
+        }
+
+        const source = axios.CancelToken.source();
+        setCancelTokenSource(source);
+
+        isMounted.current = true;
+        loadData(source).catch((error) => {
+            if (isMounted?.current === true) {
+                throw error;
+            }
+        });
+
+        return () => {
+            source.cancel();
+            isMounted.current = false;
+        };
+    }, [JSON.stringify(dashboardData)]);
+
+    const loadData = async (cancelSource = cancelTokenSource, filterDto = filterModel) => {
+        try {
+            setIsLoading(true);
+            let dashboardTags =
+                dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+            let dashboardOrgs =
+                dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "organizations")]
+                    ?.value;
+
+            const response = await aquasecActions.aquasecSecurityInsightsActionableTwo(
+                getAccessToken,
+                cancelSource,
+                kpiConfiguration,
+                dashboardTags,
+                dashboardOrgs,
+                filterDto,
+                severity.toLowerCase(),
+                imageName,
+                pipelineId
+            );
+
+            const data = response?.data && response?.status === 200 ?
+                response?.data?.data[0]?.data : [];
+            if (isMounted?.current === true && data) {
+                setMetrics(data?.data);
+                let newFilterDto = filterDto;
+                newFilterDto.setData(
+                    "totalCount",
+                    data?.count[0]?.count
+                );
+                setFilterModel({ ...newFilterDto });
+            }
+        } catch (error) {
+            if (isMounted?.current === true) {
+                console.error(error);
+                setError(error);
+            }
+        } finally {
+            if (isMounted?.current === true) {
+                setIsLoading(false);
+            }
+        }
+    };
+
+    return (
+        <ScreenContainer
+            navigationTabContainer={<InsightsSubNavigationBar currentTab={"reportsViewer"} />}
+            pageDescription={`Downloadable Report for Aquasec`}
+            breadcrumbDestination={"aquasecReports"}
+        >
+            <AquasecScanReportTable
+                data={metrics}
+                isLoading={isLoading}
+                loadData={loadData}
+                filterModel={filterModel}
+                setFilterModel={setFilterModel}
+                severity={severity}
+                pipelineId={pipelineId}
+                imageName={imageName}
+            />
+        </ScreenContainer>
+    );
+}
+
+AquasecReport.propTypes = {
+    title: PropTypes.string,
+    severity: PropTypes.string,
+    kpiConfiguration: PropTypes.object,
+    dashboardData: PropTypes.object,
+};
+
+export default AquasecReport;
---
+++ src/components/insights/reports/aquasec/AquasecScanReportTable.jsx
@@ -0,0 +1,84 @@
+import React, {useMemo} from "react";
+import PropTypes from "prop-types";
+import CustomTable from "components/common/table/CustomTable";
+import {getTableTextColumn} from "components/common/table/table-column-helpers";
+import FilterContainer from "components/common/table/FilterContainer";
+import {faTally} from "@fortawesome/pro-light-svg-icons";
+import aquasecScanReportMetadata from "./aquasecScanReportTable.metadata";
+import ExportAquasecScanDetailsButton from "./export/ExportAquasecScanDetailsButton";
+
+function AquasecScanReportTable(
+    {
+        data,
+        allIssues,
+        isLoading,
+        loadData,
+        filterModel,
+        setFilterModel,
+        severity,
+        imageName,
+        pipelineId
+    }) {
+    const fields = aquasecScanReportMetadata.fields;
+
+    const columns = useMemo(
+        () => [
+            getTableTextColumn(fields.find(field => field.id === "componentName")),
+            getTableTextColumn(fields.find(field => field.id === "componentVersion")),
+            getTableTextColumn(fields.find(field => field.id === "severity")),
+            getTableTextColumn(fields.find(field => field.id === "identifier")),
+            getTableTextColumn(fields.find(field => field.id === "score")),
+            getTableTextColumn(fields.find(field => field.id === "description")),
+        ],
+        []
+    );
+
+    const getAquasecScanReportTable = () => {
+        return (
+            <CustomTable
+                columns={columns}
+                data={data}
+                isLoading={isLoading}
+                paginationDto={filterModel}
+                setPaginationDto={setFilterModel}
+                loadData={loadData}
+            />
+        );
+    };
+
+    return (
+        <div>
+            <div className={"d-flex details-title-text"}>
+                <div className={'mr-4'}>
+                    <b>PipelineId:</b> {pipelineId}
+                </div>
+                <div className={'mr-4'}>
+                    <b>ImageName:</b> {imageName}
+                </div>
+            </div>
+            <FilterContainer
+                loadData={loadData}
+                isLoading={isLoading}
+                body={getAquasecScanReportTable()}
+                titleIcon={faTally}
+                title={"Aquasec " + severity+" Issues Scan"}
+                className={"px-2 pb-2"}
+                exportButton={<ExportAquasecScanDetailsButton className={"ml-2"} isLoading={isLoading} scanData={data} />}
+            />
+        </div>
+    );
+}
+
+AquasecScanReportTable.propTypes = {
+    data: PropTypes.array,
+    allIssues: PropTypes.array,
+    isLoading: PropTypes.bool,
+    loadData: PropTypes.func,
+    filterModel: PropTypes.object,
+    setFilterModel: PropTypes.func,
+    severity:PropTypes.string,
+    imageName:PropTypes.string,
+    pipelineId:PropTypes.string,
+};
+
+export default AquasecScanReportTable;
