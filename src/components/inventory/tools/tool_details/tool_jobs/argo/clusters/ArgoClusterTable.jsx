31d32dd6896c280fcfa9db4f9fd84a003693b18a
Noah Champoux <noah@opsera.io>
2023-02-18 10:22:00 -0600

    Fixed broken imports

--- src/components/inventory/tools/tool_details/tool_jobs/argo/clusters/ArgoClusterTable.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/argo/clusters/ArgoClusterTable.jsx
@@ -7,7 +7,7 @@ import {faBrowser} from "@fortawesome/pro-light-svg-icons";
 import {DialogToastContext} from "contexts/DialogToastContext";
 import CreateArgoClusterOverlay
   from "components/inventory/tools/tool_details/tool_jobs/argo/clusters/CreateArgoClusterOverlay";
-import {getTableTextColumn, getArgoConnectionStatus} from "components/common/table/table-column-helpers-v2";
+import {getTableTextColumn} from "components/common/table/table-column-helpers-v2";
 import VanityTable from "components/common/table/VanityTable";
 
 function ArgoClusterTable(
@@ -35,7 +35,7 @@ function ArgoClusterTable(
     () => [
       getTableTextColumn(getField(fields, "name")),
       getTableTextColumn(getField(fields, "server")),
-      getArgoConnectionStatus(getField(fields, "connectionState")),
+      // getArgoConnectionStatus(getField(fields, "connectionState")),
     ],
     []
   );
