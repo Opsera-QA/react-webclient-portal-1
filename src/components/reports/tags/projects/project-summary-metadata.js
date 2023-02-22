789c46f9bd74bad3490e15b68d48d99e2206cae2
Matthew Greci <mgreci@opsera.io>
2023-02-22 14:21:12 -0700

    update column names

---
+++ src/components/reports/tags/projects/project-summary-metadata.js
@@ -0,0 +1,37 @@
+// TODO: Refine when all necessary fields are known
+const projectSummaryMetadata = {
+  idProperty: "_id",
+  fields: [
+    {
+      label: "ID",
+      id: "_id",
+    },
+    {
+      label: "Name",
+      id: "key",
+      isRequired: true
+    },
+    {
+      label: "Owner",
+      id: "owner",
+    },
+    {
+      label: "Tool Type",
+      id: "tool_identifier",
+    },
+    {
+      label: "Tags",
+      id: "value",
+    },
+    {
+      label: "Created On",
+      id: "createdAt",
+    },
+    {
+      label: "Updated On",
+      id: "updatedAt",
+    },
+  ]
+};
+
+export default projectSummaryMetadata;
