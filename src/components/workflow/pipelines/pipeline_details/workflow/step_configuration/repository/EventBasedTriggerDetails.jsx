f93347a5de0c142a9bf07a8ca3f9f37a1c2c57f2
Faseeh <faseeh@opsera.io>
2023-02-23 14:20:33 +0530

    OPL-7252: Fixed webhook copy button

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/EventBasedTriggerDetails.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/EventBasedTriggerDetails.jsx
@@ -3,10 +3,11 @@ import PropTypes from "prop-types";
 import RegisterSourceRepositoryHookButton from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/RegisterSourceRepositoryHookButton";
 import FieldContainer from "components/common/fields/FieldContainer";
 import InfoText from "components/common/inputs/info_text/InfoText";
-import CopyToClipboardButton from "components/common/buttons/clipboard/CopyToClipboardButton";
 import CheckboxInputBase from "../../../../../../common/inputs/boolean/CheckboxInputBase";
 import {toolIdentifierConstants} from "components/admin/tools/identifiers/toolIdentifier.constants";
 import {NODE_API_ORCHESTRATOR_SERVER_URL} from "config";
+import { faClipboardList } from "@fortawesome/pro-light-svg-icons";
+import CopyToClipboardIconBase from "components/common/icons/link/CopyToClipboardIconBase";
 
 // TODO: Refactor
 function EventBasedTriggerDetails({
@@ -90,14 +91,17 @@ function EventBasedTriggerDetails({
           <h6 className={"mb-0"}>Webhook URL:</h6>
         </div>
       </div>
-      <InfoText customMessage={triggerUrl} />
-      <div className={"d-flex justify-content-end mt-2"}>
-        <CopyToClipboardButton
-          size={"sm"}
+      <div className="ml-auto d-flex">
+        <InfoText customMessage={triggerUrl} />
+        <CopyToClipboardIconBase
+          className={"ml-2"}
           copyString={triggerUrl}
-          className={"mr-2"}
-          showLabel={true}
+          copyIcon={faClipboardList}
+          copyText={"Copy to Clipboard."}
+          copiedText={"Copied to Clipboard."}
         />
+      </div>
+      <div className={"d-flex justify-content-end mt-2"}>
         <RegisterSourceRepositoryHookButton
           model={model}
           savePipelineFunction={savePipelineFunction}
