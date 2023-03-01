59c9fac561665c65d6efc20a51f0a1d2aadf550a
Noah Champoux <noah@opsera.io>
2023-03-01 11:23:04 -0500

    Manually enforce form text

--- src/utils/regexDefinitions.js
+++ src/utils/regexDefinitions.js
@@ -190,6 +190,9 @@ regexDefinitions.customParameterValueRegex = {
   regex: /^[a-zA-Z0-9-+|!.*$@&:_,; [\]\\/]*$/,
   formText: "Values are visible to all users if no Access Roles are assigned to this record. Value can be up to 16,000 characters and contain alphanumeric characters, spaces, and these symbols: @ ! & + * - _ / \\ . $ [ ] : , ; |",
   errorFormText: "Values are visible to all users if no Access Roles are assigned to this record. Value can be up to 16,000 characters and contain alphanumeric characters, spaces, and these symbols: @ ! & + * - _ / \\ . $ [ ] : , ; |",
+  isRequiredFunction: (model) => {
+    return (model != null && model.getData("vaultEnabled") !== true);
+  },
 };
 
 regexDefinitions.customParameterNameRegex = {
