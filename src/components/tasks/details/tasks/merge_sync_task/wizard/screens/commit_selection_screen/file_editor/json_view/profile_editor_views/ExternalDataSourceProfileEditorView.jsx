efcfa60f0a3e2a377d5e51ae645a71fe64249356
Faseeh <faseeh@opsera.io>
2023-03-06 16:09:17 +0530

    OPL-8539: Added support for external data source access

---
+++ src/components/tasks/details/tasks/merge_sync_task/wizard/screens/commit_selection_screen/file_editor/json_view/profile_editor_views/ExternalDataSourceProfileEditorView.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import LoadingDialog from "components/common/status_notifications/loading";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CheckboxInputBase from "../../../../../../../../../../common/inputs/boolean/CheckboxInputBase";
+import modelHelpers from "../../../../../../../../../../common/model/modelHelpers";
+import { jsonViewFileMetadata } from "../../../jsonViewFile.metadata";
+import TextFieldBase from "../../../../../../../../../../common/fields/text/TextFieldBase";
+import { externalDataSourceJsonMetadata } from "../jsonFileEdit.metadata";
+
+const ExternalDataSourceProfileEditorView = ({
+                                               externalDataSourceData,
+                                               setExternalSourceDataJson,
+  isLoading,
+}) => {
+  const [customMetaJsonMetadata, setCustomMetaJsonMetadata] =
+    useState(undefined);
+
+  useEffect(() => {
+    const newCustomMetadata = modelHelpers.parseObjectIntoModel(
+      externalDataSourceData,
+      externalDataSourceJsonMetadata,
+    );
+    setCustomMetaJsonMetadata(newCustomMetadata);
+  }, [externalDataSourceData]);
+
+  if (isLoading) {
+    return (
+      <LoadingDialog
+        size={"sm"}
+        message={"Loading"}
+      />
+    );
+  }
+
+  const setDataFunction = (fieldName, newValue) => {
+    const newModel = { ...customMetaJsonMetadata };
+    newModel?.setData("enabled", newValue);
+    setCustomMetaJsonMetadata({ ...newModel });
+    setExternalSourceDataJson(newModel.getPersistData());
+  };
+
+  return (
+    <div>
+      <Col>
+        <TextFieldBase
+          dataObject={customMetaJsonMetadata}
+          fieldName={"name"}
+        />
+      </Col>
+      <Col>
+        <CheckboxInputBase
+          fieldName={"enabled"}
+          model={customMetaJsonMetadata}
+          setModel={setCustomMetaJsonMetadata}
+          setDataFunction={setDataFunction}
+        />
+      </Col>
+    </div>
+  );
+};
+
+ExternalDataSourceProfileEditorView.propTypes = {
+  setExternalSourceDataJson: PropTypes.func,
+  isLoading: PropTypes.bool,
+  externalDataSourceData: PropTypes.object,
+};
+
+export default ExternalDataSourceProfileEditorView;
