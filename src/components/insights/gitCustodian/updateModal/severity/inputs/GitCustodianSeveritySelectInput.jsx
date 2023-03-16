9f754a07bea073b05629b801a110fb76459e500a
Vignesh Subramanian <vignesh@opsera.io>
2023-03-16 21:56:45 +0530

    update gc severity changes

---
+++ src/components/insights/gitCustodian/updateModal/severity/inputs/GitCustodianSeveritySelectInput.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+
+const SEVERITY = [
+  "Low",
+  "Medium",
+  "High",
+];
+
+function GitCustodianSeveritySelectInput({ model, setModel, disabled }) {
+
+  return (
+    <SelectInputBase
+      dataObject={model}  
+      setDataObject={setModel}           
+      selectOptions={SEVERITY}
+      fieldName={"severity"}
+      disabled={disabled}
+    />
+  );
+}
+
+GitCustodianSeveritySelectInput.propTypes = {
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+export default GitCustodianSeveritySelectInput;
