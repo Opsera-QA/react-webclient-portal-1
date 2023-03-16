a65bd5ee3e3415ebb639d22d79ac29e645e20a61
Vignesh Subramanian <vignesh@opsera.io>
2023-03-16 16:40:46 +0530

    OPL-9421

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/service_now/ServiceNowPipelineStepConfigurationSummaryPanel.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/service_now/ServiceNowPipelineStepConfigurationSummaryPanel.jsx
@@ -5,6 +5,8 @@ import PipelineStepSummaryPanelContainer
   from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/PipelineStepSummaryPanelContainer";
 import LoadingDialog from "components/common/status_notifications/loading";
 import TextFieldBase from "components/common/fields/text/TextFieldBase";
+import ToolNameField from "components/common/fields/inventory/ToolNameField";
+import BooleanField from "components/common/fields/boolean/BooleanField";
 
 function ServiceNowPipelineStepConfigurationSummaryPanel({ serviceNowPipelineDataObject, pipelineData, setActiveTab }) {
 
@@ -16,10 +18,37 @@ function ServiceNowPipelineStepConfigurationSummaryPanel({ serviceNowPipelineDat
     <PipelineStepSummaryPanelContainer setActiveTab={setActiveTab} pipelineData={pipelineData}>
       <Row>
         <Col lg={6}>
-          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"message"}/>
+          <ToolNameField model={serviceNowPipelineDataObject} fieldName={"serviceNowToolId"}/>
         </Col>
         <Col lg={6}>
-          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"contact"}/>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"type"}/>
+        </Col>
+        <Col lg={6}>
+          <BooleanField dataObject={serviceNowPipelineDataObject} fieldName={"existingChangeRequest"} />
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestNumber"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestShortDescription"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestApproval"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestStartDate"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestEndDate"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestState"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"assignmentGroupId"}/>
+        </Col>
+        <Col lg={6}>
+          <TextFieldBase dataObject={serviceNowPipelineDataObject} fieldName={"changeRequestDescription"}/>
         </Col>
       </Row>
     </PipelineStepSummaryPanelContainer>
