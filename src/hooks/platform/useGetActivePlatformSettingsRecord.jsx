5b5dad438125d9e4942584051d0eab00a3f8abc6
Noah Champoux <noah@opsera.io>
2023-03-16 11:37:06 -0500

    Minor cleanup to new hook

---
+++ src/hooks/platform/useGetActivePlatformSettingsRecord.jsx
@@ -0,0 +1,43 @@
+import {useEffect, useState} from "react";
+import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
+import useLoadData from "temp-library-components/useLoadData/useLoadData";
+import usePlatformSettingsActions from "hooks/platform/settings/usePlatformSettingsActions";
+
+export default function useGetActivePlatformSettingsRecord(
+  userData,
+  handleErrorFunction,
+) {
+  const [platformSettingsRecord, setPlatformSettingsRecord] = useState(undefined);
+  const platformSettingsActions = usePlatformSettingsActions();
+  const {
+    isLoading,
+    error,
+    setError,
+    loadData,
+  } = useLoadData();
+
+  useEffect(() => {
+    setPlatformSettingsRecord(undefined);
+    loadData(getActivePlatformSettingsRecord, handleErrorFunction).catch(() => {
+    });
+  }, []);
+
+  const getActivePlatformSettingsRecord = async () => {
+    setPlatformSettingsRecord(undefined);
+    const response = await platformSettingsActions.getPlatformSettings();
+    const platformSettings = DataParsingHelper.parseNestedObject(response, "data.data", []);
+
+    if (platformSettings) {
+      setPlatformSettingsRecord({...platformSettings});
+    }
+  };
+
+  return ({
+    platformSettingsRecord: platformSettingsRecord,
+    setPlatformSettingsRecord: setPlatformSettingsRecord,
+    loadData: () => loadData(getActivePlatformSettingsRecord, handleErrorFunction),
+    isLoading: isLoading,
+    error: error,
+    setError: setError,
+  });
+}
