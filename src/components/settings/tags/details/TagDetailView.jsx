943b001123069a060e0a0c79f45fcac2764fd611
Noah Champoux <noah@opsera.io>
2023-03-17 15:05:04 -0500

    Update TagDetailView.jsx

--- src/components/settings/tags/details/TagDetailView.jsx
+++ src/components/settings/tags/details/TagDetailView.jsx
@@ -5,14 +5,13 @@ import ActionBarContainer from "components/common/actions/ActionBarContainer";
 import ActionBarBackButton from "components/common/actions/buttons/ActionBarBackButton";
 import DetailScreenContainer from "components/common/panels/detail_view_container/DetailScreenContainer";
 import TagDetailPanel from "components/settings/tags/details/TagDetailPanel";
-import {ROLE_LEVELS} from "components/common/helpers/role-helpers";
 import TagSubscriptionIcon from "components/common/icons/subscription/TagSubscriptionIcon";
 import TagManagementSubNavigationBar from "components/settings/tags/TagManagementSubNavigationBar";
 import useGetTagModelById from "hooks/settings/tags/id/useGetTagModelById";
 import useComponentStateReference from "hooks/useComponentStateReference";
 import DeleteTagActionBarButton from "components/settings/tags/actions/DeleteTagActionBarButton";
 
-function TagDetailView() {
+export default function TagDetailView() {
   const { id } = useParams();
   const {
     accessRoleData,
@@ -49,7 +48,6 @@ function TagDetailView() {
     <DetailScreenContainer
       breadcrumbDestination={"tagDetailView"}
       metadata={tagMetadata}
-      roleRequirement={ROLE_LEVELS.POWER_USERS_AND_SASS}
       accessRoleData={accessRoleData}
       navigationTabContainer={<TagManagementSubNavigationBar activeTab={"tagViewer"} />}
       dataObject={tagModel}
@@ -59,5 +57,3 @@ function TagDetailView() {
     />
   );
 }
-
-export default TagDetailView;
