bba811a24d8766cdfe83c475fd62c30a4fe06be2
Noah Champoux <noah@opsera.io>
2023-03-22 14:42:10 -0500

    Further cleanup

--- src/components/common/list_of_values_input/settings/groups/LdapGroupForDomainMultiSelectInput.jsx
+++ src/components/common/list_of_values_input/settings/groups/LdapGroupForDomainMultiSelectInput.jsx
@@ -1,10 +1,8 @@
-import React, {useContext, useEffect, useRef, useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import {AuthContext} from "contexts/AuthContext";
-import {DialogToastContext} from "contexts/DialogToastContext";
-import accountsActions from "components/admin/accounts/accounts-actions";
-import axios from "axios";
 import MultiSelectInputBase from "components/common/inputs/multi_select/MultiSelectInputBase";
+import useComponentStateReference from "hooks/useComponentStateReference";
+import useGetLdapGroupsForDomain from "hooks/ldap/groups/useGetLdapGroupsForDomain";
 
 export default function LdapGroupForDomainMultiSelectInput(
   {
@@ -15,74 +13,17 @@ export default function LdapGroupForDomainMultiSelectInput(
     disabled,
     organizationDomain,
   }) {
-  const {getUserRecord, getAccessToken, setAccessRoles, isSassUser} = useContext(AuthContext);
-  const toastContext = useContext(DialogToastContext);
-  const [field] = useState(model.getFieldById(fieldName));
-  const [isLoading, setIsLoading] = useState(false);
-  const [groupList, setGroupList] = useState([]);
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const {
+    isSaasUser
+  } = useComponentStateReference();
+  const {
+    loadData,
+    groups,
+    isLoading,
+  } = useGetLdapGroupsForDomain(organizationDomain);
 
-  useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-
-    loadData().catch((error) => {
-      if (isMounted?.current === true) {
-        throw error;
-      }
-    });
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
-  }, []);
-
-  const loadData = async (cancelSource = cancelTokenSource) => {
-    const user = await getUserRecord();
-    const {ldap} = user;
-    const userRoleAccess = await setAccessRoles(user);
-
-    if (userRoleAccess && ldap?.domain != null) {
-      await getGroupsByDomain(cancelSource, ldap?.domain);
-    }
-  };
-
-  const getGroupsByDomain = async (cancelSource = cancelTokenSource, ldapDomain) => {
-    try {
-      setIsLoading(true);
-      let response = await accountsActions.getLdapGroupsWithDomainV2(getAccessToken, cancelSource, organizationDomain);
-
-      const groupResponse = response?.data;
-
-      if (Array.isArray(groupResponse) && groupResponse.length > 0) {
-        let filteredGroups = [];
-
-        groupResponse.map((group) => {
-          let groupType = group.groupType.toLowerCase();
-          if (groupType !== "role" && groupType !== "dept") {
-            filteredGroups.push(group);
-          }
-        });
-
-        setGroupList(filteredGroups);
-      }
-    } catch (error) {
-      toastContext.showLoadingErrorDialog(error);
-      console.error(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  if (field == null || isSassUser() === true) {
-    return <></>;
+  if (isSaasUser !== false) {
+    return null;
   }
 
   return (
@@ -92,10 +33,11 @@ export default function LdapGroupForDomainMultiSelectInput(
       fieldName={fieldName}
       dataObject={model}
       setDataObject={setModel}
-      selectOptions={groupList}
+      selectOptions={groups}
       valueField={"name"}
       textField={"name"}
       busy={isLoading}
+      loadDataFunction={loadData}
     />
   );
 }
