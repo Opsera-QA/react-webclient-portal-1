071a5e9580bc24248f2f79c547c99274ccbe6de9
Noah Champoux <noah@opsera.io>
2023-03-22 14:51:42 -0500

    Update useGetLdapGroups.js

--- src/hooks/ldap/groups/useGetLdapGroups.js
+++ src/hooks/ldap/groups/useGetLdapGroups.js
@@ -1,17 +1,14 @@
 import { useEffect, useState } from "react";
 import useComponentStateReference from "hooks/useComponentStateReference";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
-import {hasStringValue} from "components/common/helpers/string-helpers";
 import useLdapGroupActions from "hooks/ldap/groups/useLdapGroupActions";
 import useLoadData from "temp-library-components/useLoadData/useLoadData";
 
-export default function useGetLdapGroups(domain, handleErrorFunction) {
+export default function useGetLdapGroups(handleErrorFunction) {
   const [groups, setGroups] = useState([]);
   const ldapGroupActions = useLdapGroupActions();
   const {
-    userData,
     isSaasUser,
-    isOpseraAdministrator,
   } = useComponentStateReference();
   const {
     isLoading,
@@ -22,23 +19,14 @@ export default function useGetLdapGroups(domain, handleErrorFunction) {
   useEffect(() => {
     setGroups([]);
 
-    const ldapDomain = DataParsingHelper.parseNestedString(userData, "ldap.domain");
-
-    if (
-      isSaasUser === false
-      && hasStringValue(domain) === true
-      && (ldapDomain === domain || isOpseraAdministrator === true)
-      && loadData
-    ) {
+    if (isSaasUser === false && loadData) {
       loadData(getLdapGroupsForDomain, handleErrorFunction).catch(() => {
       });
     }
   }, []);
 
   const getLdapGroupsForDomain = async () => {
-    const response = await ldapGroupActions.getLdapUserGroupsWithDomain(
-      domain,
-    );
+    const response = await ldapGroupActions.getLdapUserGroups();
 
     setGroups([...DataParsingHelper.parseArray(response?.data?.data, [])]);
   };
