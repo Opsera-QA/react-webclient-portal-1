d8a88a71b709e7afe3e765e823bf759c3ca9cf96
Noah Champoux <noah@opsera.io>
2023-03-27 06:49:44 -0500

    OPL-8947   Tool Registry - Scripts - Observations

--- src/components/inventory/scripts/details/PullScriptValueButton.jsx
+++ src/components/inventory/scripts/details/PullScriptValueButton.jsx
@@ -47,6 +47,7 @@ export default function PullScriptValueButton(
       className={className}
       buttonSize={"sm"}
       variant={"outline-primary"}
+      tooltip={"Pull Script from Database"}
     />
   );
 }
