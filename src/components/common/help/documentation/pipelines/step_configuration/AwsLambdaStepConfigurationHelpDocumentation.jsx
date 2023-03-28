a3bdc6ef81ab3daaeef4b2994bd7b85a23225403
Christina Bowers <92869267+christinabowers@users.noreply.github.com>
2023-03-28 09:44:09 -0400

    Opl9524pipelinestephelp t (#384)
    * OPL9524 added help panels to pipeline steps
    
    * OPL9524 pipeline step help panels
    
    * OPL9524 pipeline step help aws lambda and synk edits

--- src/components/common/help/documentation/pipelines/step_configuration/AwsLambdaStepConfigurationHelpDocumentation.jsx
+++ src/components/common/help/documentation/pipelines/step_configuration/AwsLambdaStepConfigurationHelpDocumentation.jsx
@@ -24,7 +24,8 @@ function AwsLambdaStepConfigurationHelpDocumentation() {
         </div>
         <div className={"mb-1 ml-4"}>
           The prerequisites for AWS Lambda service pipeline also include task creation in <b>Tasks</b>.
-          To view in depth documentation on task and pipeline setup, view the <a href="https://docs.opsera.io/aws-native-support/aws-ecs" target="_blank" rel="noreferrer">AWS Lambda Functions Help Documentation</a>.
+          To view in depth documentation on task and pipeline setup, view the <a href="https://docs.opsera.io/aws-native-support/aws-ecs" target="_blank" rel="noreferrer"><b>AWS Lambda Functions Help Documentation</b>. </a>
+
           Once step is configured with AWS Lambda Deploy set as tool, set the following values:
         </div>
         <ul style={{listStyleType: "none"}}>
