70395cbca8945f33715ec971a53aacf88e6be771
Noah Champoux <noah@opsera.io>
2023-03-30 06:45:33 -0500

    OPL-8927   Add Ability to Change Auto-Generated Tag On Pipeline Step Save

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/tag_warning/PipelineStepTagWarningOverlay.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/tag_warning/PipelineStepTagWarningOverlay.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import OverlayPanelBodyContainer from "components/common/panels/detail_panel_container/OverlayPanelBodyContainer";
 import {faTriangleExclamation} from "@fortawesome/pro-light-svg-icons";
@@ -11,6 +11,13 @@ import PipelineStepTagWarningSkipButton
 import PipelineStepTagWarningApplyTagButton
   from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/tag_warning/PipelineStepTagWarningApplyTagButton";
 import WarningMessageFieldBase from "components/common/fields/text/message/WarningMessageFieldBase";
+import {Col, Row} from "react-bootstrap";
+import TextInputBase from "components/common/inputs/text/TextInputBase";
+import modelHelpers from "components/common/model/modelHelpers";
+import {
+  tagRequirementMetadata
+} from "components/workflow/pipelines/pipeline_details/workflow/step_configuration/tag_warning/tagRequirement.metadata";
+import {pipelineHelper} from "components/workflow/pipeline.helper";
 
 export default function PipelineStepTagWarningOverlay(
   {
@@ -20,9 +27,18 @@ export default function PipelineStepTagWarningOverlay(
   }) {
   const field = stepConfigurationModel?.getFieldById("tags");
   const [busy, setBusy] = useState(false);
+  const [tagRequirementModel, setTagRequirementModel] = useState(undefined);
   const {
     toastContext,
   } = useComponentStateReference();
+
+  useEffect(() => {
+    const object = {
+      tag: pipelineHelper.getTagValueForStep(stepConfigurationModel?.getCurrentData())
+    };
+    const newModel = modelHelpers.parseObjectIntoModel(object, tagRequirementMetadata);
+    setTagRequirementModel({...newModel});
+  }, []);
 
   const closeOverlayFunction = () => {
     toastContext.clearOverlayPanel();
@@ -52,10 +68,15 @@ export default function PipelineStepTagWarningOverlay(
           savePipelineStepConfiguration={savePipelineStepConfiguration}
           disabled={busy === true}
           setBusy={setBusy}
+          tagRequirementModel={tagRequirementModel}
         />
       </ButtonContainerBase>
     );
   };
+
+  if (tagRequirementModel == null) {
+    return null;
+  }
 
   return (
     <ConfirmationOverlay
@@ -74,7 +95,16 @@ export default function PipelineStepTagWarningOverlay(
           <WarningMessageFieldBase
             message={field?.formText}
           />
-          <div className={"mx-2"}>{`If you aren't sure what Tag value to use and no Tags are applied, Opsera will apply a unique tag derived from the step upon hitting Proceed.`}</div>
+          <div className={"mx-2"}>{`If you aren't sure what Tag value to use and no Tags are applied, Opsera will apply this unique tag derived from the step upon hitting Proceed.`}</div>
+          <Row>
+            <Col xs={12}>
+              <TextInputBase
+                dataObject={tagRequirementModel}
+                setDataObject={setTagRequirementModel}
+                fieldName={"tag"}
+              />
+            </Col>
+          </Row>
         </div>
       </OverlayPanelBodyContainer>
     </ConfirmationOverlay>
