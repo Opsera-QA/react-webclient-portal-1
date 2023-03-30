59c5e92961d848a97e2a36a51e5e17338efccd65
Noah Champoux <noah@opsera.io>
2023-03-30 14:35:01 -0500

    Pipeline Source Repository UI Tweaks

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/PipelineSourceRepositorySecretInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/repository/PipelineSourceRepositorySecretInput.jsx
@@ -1,29 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 import TextInputBase from "components/common/inputs/text/TextInputBase";
+import FieldLabelBase from "components/common/fields/FieldLabelBase";
 
 function PipelineSourceRepositorySecretInput({className, fieldName, model, setModel, disabled, visible}) {
-  if (visible === false) {
-    return null;
-  }
-
   return (
-    <div>
-      <h6>Settings:</h6>
-      <div className="text-muted pl-1 mb-3">
-        <b>Content Type:</b> application/json<br/>
-        <b>SSL verification:</b> enabled<br/>
-        <b>Selected events:</b> <i>just the push event</i><br/>
-      </div>
-      <TextInputBase
-        fieldName={fieldName}
-        dataObject={model}
-        setDataObject={setModel}
-        disabled={disabled}
-        className={className}
-        style={{WebkitTextSecurity: 'disc'}}
-      />
-    </div>
+    <TextInputBase
+      fieldName={fieldName}
+      dataObject={model}
+      setDataObject={setModel}
+      disabled={disabled}
+      className={className}
+      style={{WebkitTextSecurity: 'disc'}}
+      visible={visible}
+    />
   );
 }
 
