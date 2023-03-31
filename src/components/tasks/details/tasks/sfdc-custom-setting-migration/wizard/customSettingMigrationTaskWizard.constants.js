165f4079b07496fa2be924b92f59dd758d262c5f
Faseeh <faseeh@opsera.io>
2023-03-31 17:27:23 +0530

    OPL-5933: minor comment

--- src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/customSettingMigrationTaskWizard.constants.js
+++ src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/customSettingMigrationTaskWizard.constants.js
@@ -3,6 +3,6 @@ export const CUSTOM_SETTING_MIGRATION_WIZARD_SCREENS = {
   CONFIGURATION_SCREEN: "CONFIGURATION_SCREEN", // create template/use template or upload file for csv to org (yet to design)
   MAPPING_SCREEN: "MAPPING_SCREEN", // mapping headers screen (this could be complex based on data)
   QUERY_BUILDER_SCREEN: "QUERY_BUILDER_SCREEN", // Query builder and validation screen (similar to our filter)
-  CONFIRMATION_SCREEN: "CONFIRMATION_SCREEN", // confirmation screen needed?
+  CONFIRMATION_SCREEN: "CONFIRMATION_SCREEN", // summary of run - an option to create a template from right here
 };
 
