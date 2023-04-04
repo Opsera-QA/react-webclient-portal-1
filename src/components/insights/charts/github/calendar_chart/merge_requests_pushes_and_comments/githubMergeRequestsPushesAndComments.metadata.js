29edf6b3b6fe1a0b1186369610d923f996bc6ae3
tbharadwaj <tejas@opsera.io>
2023-04-04 09:48:49 -0700

    Added changes needed for repository and branch filters

---
+++ src/components/insights/charts/github/calendar_chart/merge_requests_pushes_and_comments/githubMergeRequestsPushesAndComments.metadata.js
@@ -0,0 +1,27 @@
+export const githubMergeRequestsPushesAndCommentsMetadata = {
+  type: "Github Merge Requests Pushes and Comments",
+  fields: [
+    {
+      label: "Tags",
+      id: "tags",
+    },
+    {
+      label: "Date Range",
+      id: "date",
+    },
+    {
+      label: "Repositories",
+      id: "github-repository",
+    },
+    {
+      label: "Branches",
+      id: "github-branch",
+    },
+  ],
+  newObjectFields: {
+    tags: [],
+    date: undefined,
+    repositories: [],
+    branches: [],
+  },
+};
