980c053697cda89ebfbb1193ba2b9144d6c254d5
Matthew Greci <mgreci@opsera.io>
2023-04-07 15:04:34 -0600

    factor out chart

---
+++ src/components/insights/charts/system_driven_maturity/SystemDrivenMaturityTimelineChart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SystemDrivenMaturityTimelineChart () {
+  return (
+    <h4>TODO: Timeline Chart...</h4>
+  );
+}
+
+SystemDrivenMaturityTimelineChart.propTypes = {
+  
+};
+
+export default SystemDrivenMaturityTimelineChart;
