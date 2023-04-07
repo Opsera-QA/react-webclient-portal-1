963596202178b2c3b4dab6c05d1518292704787a
Matthew Greci <mgreci@opsera.io>
2023-04-07 16:40:08 -0600

    pull projects from endpoint

--- src/components/insights/charts/system_driven_maturity/SystemDrivenMaturityProjectsTab.jsx
+++ src/components/insights/charts/system_driven_maturity/SystemDrivenMaturityProjectsTab.jsx
@@ -1,11 +1,100 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import axios from "axios";
 import PropTypes from 'prop-types';
 import Container from "react-bootstrap/Container";
+import { AuthContext } from "contexts/AuthContext";
+import {
+  getResultFromKpiConfiguration,
+  MATURITY_SCORE_TEXT
+} from "../charts-helpers";
+import doraActions from "../dora/dora.action";
 import { MaturityScoreItemType } from './maturityScoreItemType';
 import SystemDrivenMaturityTimelineChart from './SystemDrivenMaturityTimelineChart';
 
 function SystemDrivenMaturityProjectsTab ({ kpiConfiguration, dashboardData, group }) {
-  // TODO: obtain projects from group
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metricData, setMetricData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async (cancelSource = cancelTokenSource) => {
+    try {
+      setIsLoading(true);
+      const dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const dashboardOrgs = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "organizations")]?.value;
+      const jiraResolutionNames = getResultFromKpiConfiguration(kpiConfiguration, 'jira-resolution-names');
+
+      const response = await doraActions.systemDrivenMaturityProjects({
+        getAccessToken,
+        cancelSource,
+        kpiConfiguration,
+        dashboardTags,
+        dashboardOrgs,
+        jiraResolutionNames,
+        group: group?.name
+      });
+
+      const projects = response?.data?.projects;
+
+      if (isMounted?.current === true && projects?.length) {
+        setMetricData(
+          projects.map(({ name, overallMaturityScoreText }) => ({
+            name,
+            score: overallMaturityScoreText,
+            previousScore: MATURITY_SCORE_TEXT.LOW // TODO: set from api
+          }))
+        );
+      } else {
+        setMetricData([]);
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <h3 className='text-center'>Loading...</h3>
+    );
+  }
+
+  if (error) {
+    return (
+      <h3 className='text-center'>Error occured: {error?.message}</h3>
+    );
+  }
+
+  console.log({ metricData });
 
   return (
     <Container>
