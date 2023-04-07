b52603d3ccd97c54bba121bceb7060697bce84a6
Matthew Greci <mgreci@opsera.io>
2023-04-07 14:39:38 -0600

    setup for real connection

--- src/components/insights/charts/system_driven_maturity/SystemDrivenMaturity.jsx
+++ src/components/insights/charts/system_driven_maturity/SystemDrivenMaturity.jsx
@@ -1,13 +1,21 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import axios from "axios";
 import PropTypes from 'prop-types';
 import Container from "react-bootstrap/Container";
 import FullScreenCenterOverlayContainer from "components/common/overlays/center/FullScreenCenterOverlayContainer";
 import TabPanelContainer from "components/common/panels/general/TabPanelContainer";
 import VanityMetricContainer from "components/common/panels/insights/charts/VanityMetricContainer";
+import { AuthContext } from "contexts/AuthContext";
 import { DialogToastContext } from "contexts/DialogToastContext";
-import { MATURITY_SCORE_TEXT, MATURITY_SCORE_VALUE } from "../charts-helpers";
 import SystemDrivenMaturityHelpDocumentation from "components/common/help/documentation/insights/charts/SystemDrivenMaturityHelpDocumentation";
-
+import {
+  getDeploymentStageFromKpiConfiguration,
+  getResultFromKpiConfiguration,
+  getUseDashboardTagsFromKpiConfiguration,
+  MATURITY_SCORE_TEXT,
+  MATURITY_SCORE_VALUE
+} from "../charts-helpers";
+import doraActions from "../dora/dora.action";
 
 const MaturityScoreItemType = PropTypes.shape({
   name: PropTypes.string,
@@ -317,28 +325,101 @@ SystemDrivenMaturityChart.propTypes = {
 function SystemDrivenMaturity ({ kpiConfiguration, dashboardData, index, setKpiConfiguration, setKpis }) {
   const toastContext = useContext(DialogToastContext);
 
-  const orgTags = [
-    {
-      name: "Org Tag One",
-      score: MATURITY_SCORE_TEXT.HIGH,
-      previousScore: MATURITY_SCORE_TEXT.MEDIUM
-    },
-    {
-      name: "Org Tag Two",
-      score: MATURITY_SCORE_TEXT.MEDIUM,
-      previousScore: MATURITY_SCORE_TEXT.MEDIUM
-    },
-    {
-      name: "Org Tag Three",
-      score: MATURITY_SCORE_TEXT.LOW,
-      previousScore: MATURITY_SCORE_TEXT.MEDIUM
-    },
-    {
-      name: "Org Tag Four",
-      score: MATURITY_SCORE_TEXT.ELITE,
-      previousScore: MATURITY_SCORE_TEXT.LOW
+  const { getAccessToken } = useContext(AuthContext);
+  const [error, setError] = useState(undefined);
+  const [metricData, setMetricData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const isMounted = useRef(false);
+  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+
+  useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
     }
-  ];
+
+    const source = axios.CancelToken.source();
+    setCancelTokenSource(source);
+
+    isMounted.current = true;
+    loadData(source).catch((error) => {
+      if (isMounted?.current === true) {
+        throw error;
+      }
+    });
+
+    return () => {
+      source.cancel();
+      isMounted.current = false;
+    };
+  }, []);
+
+  const loadData = async (cancelSource = cancelTokenSource) => {
+    try {
+      setIsLoading(true);
+      const dashboardTags = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "tags")]?.value;
+      const dashboardOrgs = dashboardData?.data?.filters[dashboardData?.data?.filters.findIndex((obj) => obj.type === "organizations")]?.value;
+      const selectedDeploymentStages = getDeploymentStageFromKpiConfiguration(kpiConfiguration)?.length || 0;
+      const jiraResolutionNames = getResultFromKpiConfiguration(kpiConfiguration, 'jira-resolution-names');
+      const useDashboardTags = getUseDashboardTagsFromKpiConfiguration(kpiConfiguration);
+
+      console.log({ selectedDeploymentStages, jiraResolutionNames, useDashboardTags, dashboardOrgs, dashboardTags });
+
+      if (selectedDeploymentStages && jiraResolutionNames?.length && useDashboardTags && dashboardOrgs?.length) {
+        // const response = await doraActions.jiraGitlabRolledUp(
+        //   getAccessToken,
+        //   cancelSource,
+        //   kpiConfiguration,
+        //   dashboardTags,
+        //   dashboardOrgs,
+        //   jiraResolutionNames
+        // );
+
+        const response = {
+          data: {
+            data: [
+              {
+                name: "Org Tag One",
+                score: MATURITY_SCORE_TEXT.HIGH,
+                previousScore: MATURITY_SCORE_TEXT.MEDIUM
+              },
+              {
+                name: "Org Tag Two",
+                score: MATURITY_SCORE_TEXT.MEDIUM,
+                previousScore: MATURITY_SCORE_TEXT.MEDIUM
+              },
+              {
+                name: "Org Tag Three",
+                score: MATURITY_SCORE_TEXT.LOW,
+                previousScore: MATURITY_SCORE_TEXT.MEDIUM
+              },
+              {
+                name: "Org Tag Four",
+                score: MATURITY_SCORE_TEXT.ELITE,
+                previousScore: MATURITY_SCORE_TEXT.LOW
+              }
+            ]
+          }
+        };
+
+        const metrics = response?.data?.data;
+
+        if (isMounted?.current === true && metrics?.length) {
+          setMetricData(metrics);
+        } else {
+          setMetricData([]);
+        }
+      }
+    } catch (error) {
+      if (isMounted?.current === true) {
+        console.error(error);
+        setError(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
+      }
+    }
+  };
 
   const onRowSelect = orgTag => {
     toastContext.showOverlayPanel(
@@ -353,7 +434,7 @@ function SystemDrivenMaturity ({ kpiConfiguration, dashboardData, index, setKpiC
   const getChartBody = () => {
     return (
       <Container className="p-3" style={{fontSize: '2rem'}}>
-        <SystemDrivenMaturityChart items={orgTags} onRowSelect={onRowSelect} />
+        <SystemDrivenMaturityChart items={metricData} onRowSelect={onRowSelect} />
       </Container>
     );
   };
@@ -366,9 +447,9 @@ function SystemDrivenMaturity ({ kpiConfiguration, dashboardData, index, setKpiC
       chart={getChartBody()}
       dashboardData={dashboardData}
       index={index}
-      // error={error}
+      error={error}
       setKpis={setKpis}
-      // isLoading={isLoading}
+      isLoading={isLoading}
       chartHelpComponent={(closeHelpPanel) => (
         <SystemDrivenMaturityHelpDocumentation onClose={closeHelpPanel} />
       )}
