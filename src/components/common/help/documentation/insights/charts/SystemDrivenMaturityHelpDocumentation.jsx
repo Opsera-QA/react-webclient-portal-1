7329c2ae78a4765f47f7a995a649ef4c652930f1
Matthew Greci <mgreci@opsera.io>
2023-04-07 14:27:08 -0600

    use new sdm help component

--- src/components/common/help/documentation/insights/charts/SystemDrivenMaturityHelpDocumentation.jsx
+++ src/components/common/help/documentation/insights/charts/SystemDrivenMaturityHelpDocumentation.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import HelpDocumentationContainer from "components/common/help/HelpDocumentationContainer";
 import PropTypes from "prop-types";
 
-function SystemDrivenMaturityHelpDocumentation({ closeHelpPanel }) {
+function SystemDrivenMaturityHelpDocumentation({ onClose }) {
   return (
-    <HelpDocumentationContainer closeHelpPanel={closeHelpPanel} helpTopic={"System Driven Maturity"}>
+    <HelpDocumentationContainer closeHelpPanel={onClose} helpTopic={"System Driven Maturity"}>
       <div>The System Driven Maturity KPI calculates the maturity levels for each organization tag in a unified way. For more information view the <a href="https://docs.opsera.io/insights/kpi/dora-metrics/system-driven-maturity-kpi" target="_blank" rel="noreferrer"><b>System Driven Maturity KPI Help Documentation</b>.</a>
           <ul style={{listStyleType: "none"}}>
             <li><b>Deployment Stage</b> - Choose a deployment stage (Example: Prod).</li>
@@ -21,7 +21,7 @@ function SystemDrivenMaturityHelpDocumentation({ closeHelpPanel }) {
 }
 
 SystemDrivenMaturityHelpDocumentation.propTypes = {
-  closeHelpPanel: PropTypes.func,
+  onClose: PropTypes.func,
 };
 
 export default React.memo(SystemDrivenMaturityHelpDocumentation);
