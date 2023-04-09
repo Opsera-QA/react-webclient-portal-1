1afb9f637dbae14416dc46d6a044e3f54d36e5cb
Noah Champoux <noah@opsera.io>
2023-04-09 10:30:32 -0500

    Minor cleanup

---
+++ src/components/reports/reports.paths.js
@@ -0,0 +1,20 @@
+export const reportsPaths = {};
+
+//Reports
+reportsPaths.reports = "reports";
+reportsPaths.toolReports = reportsPaths.reports + "/tools";
+reportsPaths.toolsUsedInPipelineReport = reportsPaths.toolReports + "/tools-used-in-pipeline";
+reportsPaths.toolCountsReport = reportsPaths.toolReports + "/tool-counts";
+reportsPaths.detailedToolReport = reportsPaths.toolReports + "/detailed-tool-report";
+reportsPaths.tagReports = reportsPaths.reports + "/tags";
+reportsPaths.tagsUsedInPipelineReport = reportsPaths.tagReports + "/tags-used-in-pipeline";
+reportsPaths.tagsUsedInToolsReport = reportsPaths.tagReports + "/tags-used-in-tools";
+reportsPaths.tagsUsedInDashboardsReport = reportsPaths.tagReports + "/tags-used-in-dashboards";
+reportsPaths.tagsUsedInProjectsReport = reportsPaths.tagReports + "/tags-used-in-projects";
+reportsPaths.userReports = reportsPaths.reports + "/users";
+reportsPaths.groupMembershipReport = reportsPaths.userReports + "/group-membership";
+reportsPaths.pipelineOwnershipReport = reportsPaths.userReports + "/pipeline-ownership";
+reportsPaths.toolOwnershipReport = reportsPaths.userReports + "/tool-ownership";
+reportsPaths.taskOwnershipReport = reportsPaths.userReports + "/task-ownership";
+reportsPaths.consolidatedUserReport = reportsPaths.userReports + "/user-report";
+reportsPaths.pipelineReports = reportsPaths.reports + "/pipelines";
