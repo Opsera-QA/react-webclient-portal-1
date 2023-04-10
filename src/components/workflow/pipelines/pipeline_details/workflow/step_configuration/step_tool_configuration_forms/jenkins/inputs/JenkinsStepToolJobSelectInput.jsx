27809cb1c7c2fa6f6a473df7380fe113f021ccbd
Vignesh Subramanian <117057918+Vigneshsub30@users.noreply.github.com>
2023-04-10 19:07:21 +0530

    Test-Master (Jira Id: OPL-9746) Custom maven setting xml ui changes (#420)
    * maven changes
    
    * fix

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/jenkins/inputs/JenkinsStepToolJobSelectInput.jsx
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/jenkins/inputs/JenkinsStepToolJobSelectInput.jsx
@@ -34,12 +34,16 @@ function JenkinsStepToolJobSelectInput({ fieldName, model, setModel, disabled, j
     const configuration = selectedOption?.configuration;
     const gradleTask = configuration?.gradleTask || "";
     const mavenTask = configuration?.mavenTask || "";
+    const customMavenSettings = configuration?.customMavenSettings || false;
+    const scriptId = customMavenSettings === true ? (configuration?.scriptId || "") : "";
     const buildTool = configuration?.buildTool || "";
     const agentLabels = configuration?.agentLabels || "";
     const buildType = configuration?.buildType || "";
 
     newDataObject.setData("gradleTask", gradleTask);
     newDataObject.setData("mavenTask", mavenTask);
+    newDataObject.setData("customMavenSettings", customMavenSettings);
+    newDataObject.setData("scriptId", scriptId);
     newDataObject.setData("agentLabels", agentLabels);
     newDataObject.setData("buildType", buildType);
     newDataObject.setData("buildTool", buildTool);
