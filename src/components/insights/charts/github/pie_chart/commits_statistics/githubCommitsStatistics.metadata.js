769296436c6dc3b566bb30515ab028d880ba4a1c
tbharadwaj <tejas@opsera.io>
2023-04-11 06:59:45 -0700

    Added repo and branch filters to Github metrics in addition to Salesforce Component changes

---
+++ src/components/insights/charts/github/pie_chart/commits_statistics/githubCommitsStatistics.metadata.js
@@ -0,0 +1,27 @@
+export const githubCommitsStatisticsMetadata = {
+  type: "Github Commits Statistics",
+  fields: [
+    {
+      label: "Tags",
+      id: "tags",
+    },
+    {
+      label: "Date Range",
+      id: "date",
+    },
+    {
+      label: "Repositories",
+      id: "github-repository",
+    },
+    {
+      label: "Branches",
+      id: "github-branch",
+    },
+  ],
+  newObjectFields: {
+    tags: [],
+    date: undefined,
+    repositories: [],
+    branches: [],
+  },
+};
---
+++ src/components/insights/charts/github/table/pending_merge_requests/GithubPendingMergeRequestsEditorPanel.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import MetricTagFilterInput from "components/common/inputs/metric/filters/tags/MetricTagFilterInput";
+import MetricDateRangeFilterInput from "components/common/inputs/metric/filters/date/MetricDateRangeFilterInput";
+import MetricGithubRepositoryFilterInput from "components/common/inputs/metric/filters/MetricGithubRepositoryFilterInput";
+import MetricGithubBranchFilterInput from "components/common/inputs/metric/filters/MetricGithubBranchFilterInput";
+import modelHelpers from "components/common/model/modelHelpers";
+import githubPendingMergeRequestsMetadata from "./github-pending-merge-requests-metadata";
+
+function GithubPendingMergeRequestsEditorPanel({
+  metricModel,
+  unpackedFilterData,
+  metricFilterModel,
+  setMetricFilterModel,
+}) {
+  useEffect(() => {
+    setMetricFilterModel(undefined);
+
+    if (unpackedFilterData) {
+      setMetricFilterModel(
+        modelHelpers.parseObjectIntoModel(
+          unpackedFilterData,
+          githubPendingMergeRequestsMetadata,
+        ),
+      );
+    }
+  }, [unpackedFilterData]);
+
+  if (metricFilterModel == null) {
+    return null;
+  }
+
+  return (
+    <div>
+      <MetricTagFilterInput
+        metricModel={metricModel}
+        metricFilterModel={metricFilterModel}
+        setMetricFilterModel={setMetricFilterModel}
+      />
+      <MetricGithubRepositoryFilterInput
+        metricModel={metricModel}
+        metricFilterModel={metricFilterModel}
+        setMetricFilterModel={setMetricFilterModel}
+      />
+      <MetricGithubBranchFilterInput
+        metricModel={metricModel}
+        metricFilterModel={metricFilterModel}
+        setMetricFilterModel={setMetricFilterModel}
+      />
+      <MetricDateRangeFilterInput
+        metricModel={metricModel}
+        metricFilterModel={metricFilterModel}
+        setMetricFilterModel={setMetricFilterModel}
+      />
+    </div>
+  );
+}
+
+GithubPendingMergeRequestsEditorPanel.propTypes = {
+  metricModel: PropTypes.object,
+  unpackedFilterData: PropTypes.object,
+  metricFilterModel: PropTypes.object,
+  setMetricFilterModel: PropTypes.func,
+};
+
+export default GithubPendingMergeRequestsEditorPanel;
