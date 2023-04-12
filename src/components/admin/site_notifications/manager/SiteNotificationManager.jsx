5f7c9d4c5faa6d941cbf929d34dcedd6e843b80d
Noah Champoux <noah@opsera.io>
2023-04-12 09:52:58 -0500

    Cleanup

--- src/components/admin/site_notifications/manager/SiteNotificationManager.jsx
+++ src/components/admin/site_notifications/manager/SiteNotificationManager.jsx
@@ -1,54 +1,41 @@
-import React, {useState, useEffect, useContext, useRef} from "react";
-import {AuthContext} from "contexts/AuthContext";
-import {DialogToastContext} from "contexts/DialogToastContext";
+import React, {useState, useEffect} from "react";
 import siteNotificationActions from "components/admin/site_notifications/site-notification-actions";
 import DetailScreenContainer from "components/common/panels/detail_view_container/DetailScreenContainer";
 import siteNotificationHelpers from "components/admin/site_notifications/site-notification-helpers";
 import SiteNotificationManagerDetailPanel
   from "components/admin/site_notifications/manager/SiteNotificationManagerDetailPanel";
 import siteNotificationMetadata from "components/admin/site_notifications/siteNotification.metadata";
-import axios from "axios";
 import SiteNotificationManagementSubNavigationBar
   from "components/admin/site_notifications/SiteNotificationManagementSubNavigationBar";
+import useComponentStateReference from "hooks/useComponentStateReference";
 
 function SiteNotificationManager() {
-  const {getUserRecord, getAccessToken, setAccessRoles} = useContext(AuthContext);
-  const toastContext = useContext(DialogToastContext);
-  const [accessRoleData, setAccessRoleData] = useState(undefined);
   const [isLoading, setIsLoading] = useState(false);
   const [siteWideNotificationData, setSiteWideNotificationData] = useState(undefined);
-  const isMounted = useRef(false);
-  const [cancelTokenSource, setCancelTokenSource] = useState(undefined);
+  const {
+    getAccessToken,
+    isOpseraAdministrator,
+    toastContext,
+    isMounted,
+    cancelTokenSource,
+    accessRoleData,
+  } = useComponentStateReference();
 
   useEffect(() => {
-    if (cancelTokenSource) {
-      cancelTokenSource.cancel();
-    }
-
-    const source = axios.CancelToken.source();
-    setCancelTokenSource(source);
-    isMounted.current = true;
-
-    loadData(source).catch((error) => {
+    loadData().catch((error) => {
       if (isMounted?.current === true) {
         throw error;
       }
     });
-
-    return () => {
-      source.cancel();
-      isMounted.current = false;
-    };
   }, []);
 
-  const loadData = async (cancelSource) => {
+  const loadData = async () => {
     try {
       setIsLoading(true);
-      await getRoles(cancelSource);
+      await getSiteNotifications();
     } catch (error) {
       if (isMounted?.current === true && !error?.error?.message?.includes(404)) {
         toastContext.showLoadingErrorDialog(error);
-        console.error(error);
       }
     } finally {
       if (isMounted?.current === true) {
@@ -57,26 +44,10 @@ function SiteNotificationManager() {
     }
   };
 
-  const getRoles = async (cancelSource) => {
-    const user = await getUserRecord();
-    const userRoleAccess = await setAccessRoles(user);
-    if (isMounted?.current === true && userRoleAccess) {
-      setAccessRoleData(userRoleAccess);
+  const getSiteNotifications = async () => {
+    const response = await siteNotificationActions.getSiteNotifications(getAccessToken, cancelTokenSource);
 
-      if (userRoleAccess?.OpseraAdministrator) {
-        await getSiteNotifications(cancelSource);
-      }
-    }
-  };
-
-  const getSiteNotifications = async (cancelSource) => {
-    const response = await siteNotificationActions.getSiteNotifications(getAccessToken, cancelSource);
-
-    await unpackNotifications(response?.data);
-  };
-
-  const unpackNotifications = (response) => {
-    let siteWideNotification = siteNotificationHelpers.parseSiteNotification(response, "site");
+    const siteWideNotification = siteNotificationHelpers.parseSiteNotification(response?.data, "site");
 
     if (isMounted?.current === true) {
       setSiteWideNotificationData(siteWideNotification);
@@ -88,7 +59,6 @@ function SiteNotificationManager() {
       breadcrumbDestination={"siteNotificationManager"}
       metadata={siteNotificationMetadata}
       dataObject={siteWideNotificationData}
-      accessDenied={!accessRoleData?.OpseraAdministrator}
       isLoading={isLoading}
       navigationTabContainer={
         <SiteNotificationManagementSubNavigationBar
