23b595f3e0e4f7d39eba6530df7c93f67cb036ae
Matthew Greci <mgreci@opsera.io>
2023-04-17 15:08:31 -0600

    indent

--- src/components/insights/charts/system_driven_maturity/SystemDrivenMaturityTimelineChart.jsx
+++ src/components/insights/charts/system_driven_maturity/SystemDrivenMaturityTimelineChart.jsx
@@ -9,60 +9,60 @@ function SystemDrivenMaturityTimelineChart ({ data }) {
   console.log('SDM Timeline Chart', { data });
   return (
     <ResponsiveLine
-    data={data}
-    {...defaultConfig("", "Date", false, true, "numbers", "monthDate2")}
-    colors={[
-      METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_1,
-      METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_2,
-      METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_3,
-      METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_4
-    ]}
-    enableGridX={false}
-    xFormat="time:%Y-%m-%d"
-    axisBottom={{
-      format: d => { var date = new Date(d).toUTCString(); date = date.split(" "); return date[2]+" "+date[1]; },
-      tickRotation: -45,
-      legend: 'Date',
-      legendOffset: 55,
-      legendPosition: 'middle'
-    }}
-    enableGridY={false}
-    axisLeft={{
-      tickValues: [1, 2, 3, 4],
-      legend: "Maturity Score",
-      legendOffset: -35,
-      legendPosition: "middle",
-    }}
-    yScale={{
-      type: "linear",
-      min: 0,
-      max: 4,
-      stacked: false,
-      reverse: false,
-    }}
-    pointSize={10}
-    margin={{
-      top: 40,
-      bottom: 80,
-      left: 50,
-      right: 80
-    }}
-    legends={[
-      {
-        anchor: "right",
-        direction: "column",
-        itemWidth: 10,
-        itemHeight: 10,
-        itemsSpacing: 15,
-        translateX: 30
-      }
-    ]}
-    tooltip={(node) => (
-      <ChartTooltip
-        titles={["Date Range", "Maturity Score"]}
-        values={[node.point.data.range, node.point.data.sdmScoreText]}
-      />
-    )}
+      data={data}
+      {...defaultConfig("", "Date", false, true, "numbers", "monthDate2")}
+      colors={[
+        METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_1,
+        METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_2,
+        METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_3,
+        METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_4
+      ]}
+      enableGridX={false}
+      xFormat="time:%Y-%m-%d"
+      axisBottom={{
+        format: d => { var date = new Date(d).toUTCString(); date = date.split(" "); return date[2]+" "+date[1]; },
+        tickRotation: -45,
+        legend: 'Date',
+        legendOffset: 55,
+        legendPosition: 'middle'
+      }}
+      enableGridY={false}
+      axisLeft={{
+        tickValues: [1, 2, 3, 4],
+        legend: "Maturity Score",
+        legendOffset: -35,
+        legendPosition: "middle",
+      }}
+      yScale={{
+        type: "linear",
+        min: 0,
+        max: 4,
+        stacked: false,
+        reverse: false,
+      }}
+      pointSize={10}
+      margin={{
+        top: 40,
+        bottom: 80,
+        left: 50,
+        right: 80
+      }}
+      legends={[
+        {
+          anchor: "right",
+          direction: "column",
+          itemWidth: 10,
+          itemHeight: 10,
+          itemsSpacing: 15,
+          translateX: 30
+        }
+      ]}
+      tooltip={(node) => (
+        <ChartTooltip
+          titles={["Date Range", "Maturity Score"]}
+          values={[node.point.data.range, node.point.data.sdmScoreText]}
+        />
+      )}
     />
   );
 }
