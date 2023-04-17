e094a5fe4e12fc3a9b350bc0d5f87a9b58e5ec7f
Matthew Greci <mgreci@opsera.io>
2023-04-17 13:17:18 -0600

    timeline chart is displayed

--- src/components/insights/charts/system_driven_maturity/systemDrivenMaturityTimelineChartConfig.js
+++ src/components/insights/charts/system_driven_maturity/systemDrivenMaturityTimelineChartConfig.js
@@ -1,29 +1,24 @@
 import { METRIC_THEME_CHART_PALETTE_COLORS } from "components/common/helpers/metrics/metricTheme.helpers";
 
 export default () => ({
-  indexBy: "date",
-  yScale: {
-    type: "linear",
-    min: "auto",
-    max: "auto",
-    stacked: false
-  },
-  axisLeft: {
-    tickSize: 5,
-    tickValues: 5,
-    tickPadding: 5,
-    tickRotation: 0,
-    legend: "Issues",
-    legendOffset: -45,
-    legendPosition: "middle",
-  },
+  enableGridX: false,
+  enableGridY: false,
   xFormat: "time:%Y-%m-%d",
   colors: [
     METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_1,
     METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_2,
     METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_3,
-    METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_4,
-    METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_5,
-    ],
+    METRIC_THEME_CHART_PALETTE_COLORS.CHART_PALETTE_COLOR_4
+  ],
+  axisBottom: {
+    format: d => { var date = new Date(d).toUTCString(); date = date.split(" "); return date[2]+" "+date[1]; },
+    tickRotation: -45,
+    legend: 'Date',
+    legendOffset: 55,
+    legendPosition: 'middle'
+  },
+  layers: ['grid', 'markers', 'axes', 'areas', 'crosshair', 'lines', 'points', 'slices', 'mesh'],
   pointSize: 6,
+  //enableSlices: "x",
+  enableArea: true
 });
