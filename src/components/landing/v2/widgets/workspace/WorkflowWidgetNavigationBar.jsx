5578c42f861a7062bb138d7fb818f334c9ceb268
Noah Champoux <noah@opsera.io>
2023-04-19 12:28:31 -0500

    Update WorkflowWidgetNavigationBar.jsx

--- src/components/landing/v2/widgets/workspace/WorkflowWidgetNavigationBar.jsx
+++ src/components/landing/v2/widgets/workspace/WorkflowWidgetNavigationBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {SubMenuContainer, SubMenuItem} from "@opsera/react-vanity-set";
 import PropTypes from "prop-types";
+import sessionHelper from "utils/session.helper";
 
 export const WORKFLOW_WIDGET_VIEWS = {
   MY_WORKFLOWS: "owned",
@@ -13,6 +14,10 @@ export default function WorkflowWidgetNavigationBar(
     currentView,
     setCurrentView,
   }) {
+  const handleNavigationItemClick = (itemKey) => {
+    sessionHelper.setCookie(sessionHelper.SUPPORTED_COOKIE_STORAGE_KEYS.LANDING_SCREEN_WORKFLOW_WIDGET_CURRENT_VIEW, itemKey);
+    setCurrentView(itemKey);
+  };
 
   if (setCurrentView == null) {
     return null;
@@ -24,20 +29,20 @@ export default function WorkflowWidgetNavigationBar(
         className={"px-3"}
         itemKey={WORKFLOW_WIDGET_VIEWS.MY_WORKFLOWS}
         activeKey={currentView}
-        setActiveKey={setCurrentView}
+        setActiveKey={handleNavigationItemClick}
         label={"My Workflows"}
       />
       <SubMenuItem
         className={"px-3"}
         activeKey={currentView}
-        setActiveKey={setCurrentView}
+        setActiveKey={handleNavigationItemClick}
         label={"Following"}
         itemKey={WORKFLOW_WIDGET_VIEWS.FOLLOWING}
       />
       <SubMenuItem
         className={"px-3"}
         activeKey={currentView}
-        setActiveKey={setCurrentView}
+        setActiveKey={handleNavigationItemClick}
         label={"Recent"}
         itemKey={WORKFLOW_WIDGET_VIEWS.RECENT_ACTIVITY}
       />
