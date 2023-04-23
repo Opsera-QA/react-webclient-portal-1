28b20b0db71e36567938ff7b0ee9d8592c8135b9
Noah Champoux <noah@opsera.io>
2023-04-23 11:09:54 -0500

    Added missing dependency and ignoring warning to config override

--- config-overrides.js
+++ config-overrides.js
@@ -3,25 +3,21 @@ const webpack = require("webpack");
 module.exports = function override(config) {
   const fallback = config.resolve.fallback || {};
   Object.assign(fallback, {
-    crypto: require.resolve("crypto-browserify"),
-    stream: require.resolve("stream-browserify"),
-    assert: require.resolve("assert"),
-    http: require.resolve("stream-http"),
-    https: require.resolve("https-browserify"),
-    os: require.resolve("os-browserify"),
-    url: require.resolve("url"),
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "process/browser": require.resolve('process/browser'),
+    "url": require.resolve("url"),
   });
   config.resolve.fallback = fallback;
-  config.module.rules.push({
-    test: /\.m?js/,
-    resolve: {
-      fullySpecified: false,
-    },
-  });
+  config.ignoreWarnings = [/Failed to parse source map/];
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      process: "process/browser",
-      Buffer: ["buffer", "Buffer"],
+      "process": "process/browser",
+      "Buffer": ["buffer", "Buffer"],
     }),
   ]);
   return config;
