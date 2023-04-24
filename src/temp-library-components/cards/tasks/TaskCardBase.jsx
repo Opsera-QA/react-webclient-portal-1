e8ad61e221ce296af43dd46870456fcc3c6fef29
Noah Champoux <noah@opsera.io>
2023-04-24 10:19:15 -0500

    Minor cleanup

--- src/temp-library-components/cards/tasks/TaskCardBase.jsx
+++ src/temp-library-components/cards/tasks/TaskCardBase.jsx
@@ -1,70 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
 import useComponentStateReference from "hooks/useComponentStateReference";
-import { Col, Row } from "react-bootstrap";
-import { TASK_TYPE_CATEGORIES, taskTypeConstants } from "components/tasks/task.types";
 import TaskCardFooter from "temp-library-components/cards/tasks/TaskCardFooter";
-import OrchestrationStateFieldBase
-  from "temp-library-components/fields/orchestration/state/OrchestrationStateFieldBase";
 import TaskCardHeader from "temp-library-components/cards/tasks/TaskCardHeader";
-import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 import CardIconTitleBar from "components/common/fields/title/CardIconTitleBar";
 import SelectionIconCard from "components/common/card_containers/SelectionIconCard";
-import DateFormatHelper from "@opsera/persephone/helpers/date/dateFormat.helper";
 import TaskCardBody from "temp-library-components/cards/tasks/TaskCardBody";
 import {getLargeVendorIconComponentFromTaskType} from "components/common/helpers/icon-helpers";
-
-const getLastRunDetails = (taskModel) => {
-  const runCount = DataParsingHelper.parseInteger(taskModel?.getData("run_count"), 0);
-  const lastRun = taskModel?.getData("last_run");
-  const lastRunCompletionDate = taskModel?.getData("last_run.completed");
-
-  if (runCount === 0) {
-    return (
-      <div className={"d-flex"}>
-        <div className={"text-muted m-auto"}>
-          {`This pipeline has not been run yet`}
-        </div>
-      </div>
-    );
-  }
-
-  if (lastRunCompletionDate != null) {
-    return (
-      <div className={"d-flex justify-content-between"}>
-        {DateFormatHelper.formatDateAsTimestampWithoutSeconds(lastRunCompletionDate)}
-        <div>
-          <OrchestrationStateFieldBase
-            orchestrationState={lastRun?.status}
-            type={"Pipeline"}
-          />
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className={"d-flex"}>
-      <div className={"text-muted m-auto"}>
-        {`No metrics captured for last run`}
-      </div>
-    </div>
-  );
-};
-
-// TODO: Add when wired up in orchestrator
-const getLastRunEntry = (taskModel) => {
-  return (
-    <Col xs={12}>
-      <div className={"mt-2"}>
-        <div className={"d-flex"}>
-          <div className={"mx-auto"}>Last Run</div>
-        </div>
-        {getLastRunDetails(taskModel)}
-      </div>
-    </Col>
-  );
-};
 
 export default function TaskCardBase(
   {
--- src/temp-library-components/cards/tasks/TaskCardHeader.jsx
+++ src/temp-library-components/cards/tasks/TaskCardHeader.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import OrchestrationStateFieldBase
   from "temp-library-components/fields/orchestration/state/OrchestrationStateFieldBase";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
+import DateFormatHelper from "@opsera/persephone/helpers/date/dateFormat.helper";
 
 export default function TaskCardHeader(
   {
@@ -11,15 +12,21 @@ export default function TaskCardHeader(
   }) {
   const state = taskModel?.getData("status");
   const runCount = DataParsingHelper.parseInteger(taskModel?.getData("run_count"), 0);
+  const lastRunCompletionDate = taskModel?.getData("completion");
 
   const getOrchestrationStateFieldBase = () => {
     if (runCount > 0) {
       return (
-        <OrchestrationStateFieldBase
-          orchestrationState={state}
-          type={"Task"}
-          showStoppedState={false}
-        />
+        <div className={"d-flex justify-content-between"}>
+          {DateFormatHelper.formatDateAsTimestampWithoutSeconds(lastRunCompletionDate)}
+          <div>
+            <OrchestrationStateFieldBase
+              orchestrationState={state}
+              type={"Task"}
+              showStoppedState={false}
+            />
+          </div>
+        </div>
       );
     }
   };
@@ -27,7 +34,7 @@ export default function TaskCardHeader(
 
   return (
     <CardHeaderBase>
-      <div className={"w-100 d-flex justify-content-between pl-3 pr-2 pt-1 small"}>
+      <div className={"w-100 d-flex justify-content-between px-2 pt-1 small"}>
         <div>
           <span>{runCount} Runs</span>
         </div>
