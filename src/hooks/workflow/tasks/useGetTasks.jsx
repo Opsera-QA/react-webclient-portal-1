ba7afa10b2fa45220109c5eb21ac7269892fc139
Noah Champoux <noah@opsera.io>
2023-04-26 06:51:21 -0500

    Update useGetTasks.jsx

--- src/hooks/workflow/tasks/useGetTasks.jsx
+++ src/hooks/workflow/tasks/useGetTasks.jsx
@@ -38,6 +38,8 @@ export default function useGetTasks(
   const getTasks = async (
     newFilterModel = taskFilterModel,
   ) => {
+    setTasks([]);
+
     if (TaskRoleHelper.canGetTasks(userData) !== true) {
       return;
     }
