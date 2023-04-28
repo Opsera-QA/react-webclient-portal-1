063d2e515f351801a4822e9139f75fb21bc9c91f
faseehOpsera <60170774+faseehOpsera@users.noreply.github.com>
2023-04-28 18:56:30 +0530

    OPL-10064 : Add support for custom script on maven unit test jobs (#501)

--- src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata.js
+++ src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata.js
@@ -1,38 +1,50 @@
 const JenkinsUnitTestJobMetadata = {
-    type: "Jenkins Unit Test Job",
-    fields: [
-      {
-        label: "Build Type",
-        id: "buildType",
-        isRequired: true,
-      },
-      {
-        label: "Build Tool",
-        id: "buildTool",
-        isRequired: true,
-      },
-      {
-        label: "Gradle Task",
-        id: "gradleTask",
-      },
-      {
-        label: "Maven Task",
-        id: "mavenTask",
-      },
-      {
-        label: "Agent Label",
-        id: "agentLabels",
-        formText: "Currently, the only supported Agent Label is Ubuntu Agent",
-      },
-    ],
-    newObjectFields: {
-      buildType: "",
-      buildTool: "",
-      gradleTask:"",
-      mavenTask:"",
-      agentLabels: "",
+  type: "Jenkins Unit Test Job",
+  fields: [
+    {
+      label: "Build Type",
+      id: "buildType",
+      isRequired: true,
     },
-  };
-  
-  export default JenkinsUnitTestJobMetadata;
-  
+    {
+      label: "Build Tool",
+      id: "buildTool",
+      isRequired: true,
+    },
+    {
+      label: "Gradle Task",
+      id: "gradleTask",
+    },
+    {
+      label: "Maven Task",
+      id: "mavenTask",
+    },
+    {
+      label: "Agent Label",
+      id: "agentLabels",
+      formText: "Currently, the only supported Agent Label is Ubuntu Agent",
+    },
+    {
+      label: "Use Custom Maven Settings",
+      id: "customMavenSettings"
+    },
+    {
+      label: "Custom Script",
+      id: "scriptId",
+      isRequiredFunction: (model) => {
+        return model?.getData("customMavenSettings") === true;
+      },
+    },
+  ],
+  newObjectFields: {
+    buildType: "",
+    buildTool: "",
+    gradleTask:"",
+    mavenTask:"",
+    agentLabels: "",
+    customMavenSettings: false,
+    scriptId: "",
+  },
+};
+
+export default JenkinsUnitTestJobMetadata;
