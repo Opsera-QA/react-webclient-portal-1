04b8e1cfdbf56dda5c0aba150cd5797897a02777
Faseeh <faseeh@opsera.io>
2023-04-28 18:12:54 +0530

    OPL-10064 : Add support for custom script on maven unit test jobs

--- src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata.js
+++ src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata.js
@@ -24,6 +24,17 @@ const JenkinsUnitTestJobMetadata = {
         id: "agentLabels",
         formText: "Currently, the only supported Agent Label is Ubuntu Agent",
       },
+      {
+        label: "Use Custom Maven Settings",
+        id: "customMavenSettings"
+      },
+      {
+        label: "Custom Script",
+        id: "scriptId",
+        isRequiredFunction: (model) => {
+          return model?.getData("customMavenSettings") === true;
+        },
+      },
     ],
     newObjectFields: {
       buildType: "",
@@ -31,6 +42,8 @@ const JenkinsUnitTestJobMetadata = {
       gradleTask:"",
       mavenTask:"",
       agentLabels: "",
+      customMavenSettings: false,
+      scriptId: "",
     },
   };
   
