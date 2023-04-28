dc580af0756d5bb7e0b8a2284acd311d47086f4d
Faseeh <faseeh@opsera.io>
2023-04-28 18:12:36 +0530

    OPL-10064 : Add support for custom script on maven unit test jobs

--- src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/JenkinsUnitTestingEditorPanel.jsx
+++ src/components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/JenkinsUnitTestingEditorPanel.jsx
@@ -1,24 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import TextInputBase from "components/common/inputs/text/TextInputBase";
 import modelHelpers from "components/common/model/modelHelpers";
-import JenkinsUnitTestJobMetadata
-  from "components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata";
-import JenkinsJobsGenericAgentTypeSelectInput
-  from "components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput";
-import JenkinsJobsUnitTestingTypeSelectInput
-  from "components/common/list_of_values_input/tools/jenkins/jobs/unit_testing/JenkinsJobsUnitTestingTypeSelectInput";
+import JenkinsUnitTestJobMetadata from "components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/details/inputs/unit_testing/jenkins-unit-test-metadata";
+import JenkinsJobsGenericAgentTypeSelectInput from "components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput";
+import JenkinsJobsUnitTestingTypeSelectInput from "components/common/list_of_values_input/tools/jenkins/jobs/unit_testing/JenkinsJobsUnitTestingTypeSelectInput";
+import ScriptLibrarySelectInput from "components/common/list_of_values_input/inventory/scripts/ScriptLibrarySelectInput";
+import BooleanToggleInput from "components/common/inputs/boolean/BooleanToggleInput";
 
-function JenkinsUnitTestingEditorPanel({ jenkinsJobConfiguration, model, setModel, autoScalingEnabled }) {
+function JenkinsUnitTestingEditorPanel({
+  jenkinsJobConfiguration,
+  model,
+  setModel,
+  autoScalingEnabled,
+}) {
   useEffect(() => {
     unpackJobConfiguration();
   }, [jenkinsJobConfiguration]);
 
   const unpackJobConfiguration = () => {
-    const parsedModel = modelHelpers.parseObjectIntoModel(jenkinsJobConfiguration, JenkinsUnitTestJobMetadata);
-    setModel({...parsedModel});
+    const parsedModel = modelHelpers.parseObjectIntoModel(
+      jenkinsJobConfiguration,
+      JenkinsUnitTestJobMetadata,
+    );
+    setModel({ ...parsedModel });
+  };
+
+  const setCustomMavenFlag = (fieldName, newValue) => {
+    let newModel = {...model};
+    newModel.setData(fieldName, newValue);
+    newModel.setData("scriptId", "");
+    setModel({...newModel});
   };
 
   const getDynamicBuildTypeFields = () => {
@@ -26,14 +40,41 @@ function JenkinsUnitTestingEditorPanel({ jenkinsJobConfiguration, model, setMode
       case "gradle":
         return (
           <Col lg={6}>
-            <TextInputBase dataObject={model} setDataObject={setModel} fieldName={"gradleTask"}/>
+            <TextInputBase
+              dataObject={model}
+              setDataObject={setModel}
+              fieldName={"gradleTask"}
+            />
           </Col>
         );
       case "maven":
         return (
-          <Col lg={6}>
-            <TextInputBase dataObject={model} setDataObject={setModel} fieldName={"mavenTask"}/>
-          </Col>
+          <>
+            <Col lg={6}>
+              <TextInputBase
+                dataObject={model}
+                setDataObject={setModel}
+                fieldName={"mavenTask"}
+              />
+            </Col>
+            <Col lg={12}>
+              <BooleanToggleInput
+                dataObject={model}
+                setDataObject={setModel}
+                fieldName={"customMavenSettings"}
+                setDataFunction={setCustomMavenFlag}
+              />
+            </Col>
+            {model?.getData("customMavenSettings") === true && (
+              <Col lg={12}>
+                <ScriptLibrarySelectInput
+                  fieldName={"scriptId"}
+                  model={model}
+                  setModel={setModel}
+                />
+              </Col>
+            )}
+          </>
         );
     }
   };
@@ -50,7 +91,6 @@ function JenkinsUnitTestingEditorPanel({ jenkinsJobConfiguration, model, setMode
       );
     }
   };
-
 
   if (!model) {
     return <></>;
@@ -75,7 +115,7 @@ JenkinsUnitTestingEditorPanel.propTypes = {
   jenkinsJobConfiguration: PropTypes.object,
   model: PropTypes.object,
   setModel: PropTypes.func,
-  autoScalingEnabled: PropTypes.bool
+  autoScalingEnabled: PropTypes.bool,
 };
 
 export default JenkinsUnitTestingEditorPanel;
