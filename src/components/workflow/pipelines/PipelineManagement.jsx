66b37357d35f53ef22e0304af06cc1ce1f16540f
Noah Champoux <noah@opsera.io>
2023-05-02 06:32:36 -0500

    OPL-9775   Standardize ALL Cards in main views: Pipelines

--- src/components/workflow/pipelines/PipelineManagement.jsx
+++ src/components/workflow/pipelines/PipelineManagement.jsx
@@ -1,76 +1,171 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import ScreenContainer from "components/common/panels/general/ScreenContainer";
 import WorkflowSubNavigationBar from "components/workflow/WorkflowSubNavigationBar";
-import pipelineActions from "components/workflow/pipeline-actions";
-import PipelineTableCardView from "components/workflow/pipelines/PipelineTableCardView";
 import PipelinesHelpDocumentation from "../../common/help/documentation/pipelines/PipelinesHelpDocumentation";
-import PipelineFilterModel from "components/workflow/pipelines/pipeline.filter.model";
-import useComponentStateReference from "hooks/useComponentStateReference";
 import auditLogTypeConstants from "@opsera/definitions/constants/audit-logs/types/auditLogType.constants";
+import useGetPipelines from "hooks/workflow/pipelines/useGetPipelines";
+import PipelineFilterOverlay from "components/workflow/PipelineFilterOverlay";
+import PipelineWelcomeView from "components/workflow/pipelines/PipelineWelcomeView";
+import PaginationContainer from "components/common/pagination/PaginationContainer";
+import SideBySideViewBase from "components/common/tabs/SideBySideViewBase";
+import InformationDialog from "components/common/status_notifications/info";
+import PipelineRoleHelper from "@opsera/know-your-role/roles/pipelines/pipelineRole.helper";
+import PipelineCardView from "components/workflow/pipelines/PipelineCardView";
+import PipelinesTableBase from "components/workflow/pipelines/pipeline_details/PipelinesTableBase";
+import PipelineVerticalTabContainer from "components/workflow/pipelines/PipelineVerticalTabContainer";
+import TableCardView from "components/common/table/TableCardView";
+import useComponentStateReference from "hooks/useComponentStateReference";
+import CenteredContentWrapper from "components/common/wrapper/CenteredContentWrapper";
+import {
+  FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION
+} from "components/common/table/FilterContainer";
+import {useHistory} from "react-router-dom";
+
+const pipelineFields = [
+  "type",
+  "_id",
+  "name",
+  "owner",
+  "workflow.last_step",
+  "workflow.run_count",
+  "workflow.last_run",
+  "createdAt",
+  "updatedAt",
+];
 
 function PipelineManagement() {
-  const [pipelines, setPipelines] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [pipelineFilterModel, setPipelineFilterModel] = useState(new PipelineFilterModel());
-  const [subscribedPipelineIds, setSubscribedPipelineIds] = useState([]);
   const {
-    cancelTokenSource,
-    getAccessToken,
-    isMounted,
-    toastContext,
-  } = useComponentStateReference();
+    pipelines,
+    isLoading,
+    error,
+    pipelineFilterModel,
+    setPipelineFilterModel,
+    loadData,
+    subscribedPipelineIds
+  } = useGetPipelines(
+    pipelineFields,
+    true,
+    true,
+  );
+  const { userData, } = useComponentStateReference();
+  const history = useHistory();
 
-  useEffect(() => {
-    loadData().catch((error) => {
-      if (isMounted?.current === true) {
-        throw error;
-      }
-    });
-  }, []);
+  useEffect(() => {}, []);
 
-  const loadData = async (newPipelineFilterModel = pipelineFilterModel) => {
-    try {
-      if (isMounted?.current === true) {
-        setIsLoading(true);
-        setPipelines([]);
-      }
-
-      const pipelineFields = [
-        "type",
-        "_id",
-        "name",
-        "owner",
-        "workflow.last_step",
-        "workflow.run_count",
-        "workflow.last_run",
-        "createdAt",
-        "updatedAt",
-      ];
-      const response = await pipelineActions.getPipelinesV2(
-        getAccessToken,
-        cancelTokenSource,
-        newPipelineFilterModel,
-        newPipelineFilterModel?.getData("type"),
-        pipelineFields,
+  const getBody = () => {
+    if (!Array.isArray(pipelines) && !isLoading) {
+      return (
+        <CenteredContentWrapper minHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}>
+          <div className="my-5">
+            <InformationDialog message="Could not load pipelines."/>
+          </div>
+        </CenteredContentWrapper>
       );
-      const pipelines = response?.data?.data;
-
-      if (isMounted?.current === true && Array.isArray(pipelines)) {
-        setPipelines([...pipelines]);
-        newPipelineFilterModel.updateTotalCount(response?.data?.count);
-        newPipelineFilterModel.updateActiveFilters();
-        setSubscribedPipelineIds(response?.data?.subscriptions);
-        setPipelineFilterModel({...newPipelineFilterModel});
-      }
-    } catch (error) {
-      if (isMounted?.current === true) {
-        toastContext.showLoadingErrorDialog(error);
-      }
-    } finally {
-      if (isMounted?.current === true) {
-        setIsLoading(false);
-      }
     }
+
+    if (Array.isArray(pipelines) && pipelines.count === 0 && pipelineFilterModel?.getData("type") === "owner" && (pipelineFilterModel?.getActiveFilters() == null || pipelineFilterModel?.getActiveFilters()?.length === 0)) {
+      return (
+        <CenteredContentWrapper minHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}>
+          <PipelineWelcomeView />
+        </CenteredContentWrapper>
+      );
+    }
+
+    return (
+      <PaginationContainer
+        loadData={loadData}
+        isLoading={isLoading}
+        filterDto={pipelineFilterModel}
+        setFilterDto={setPipelineFilterModel}
+        data={pipelines}
+        nextGeneration={true}
+      >
+        <SideBySideViewBase
+          leftSideView={getVerticalTabContainer()}
+          rightSideView={getCurrentView()}
+          leftSideMinimumWidth={"245px"}
+          leftSideMaximumWidth={"245px"}
+          minimumHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}
+        />
+      </PaginationContainer>
+    );
+  };
+
+  const getPipelinesBody = () => {
+    if (!Array.isArray(pipelines) && pipelines.length === 0) {
+      const activeFilters = pipelineFilterModel?.getActiveFilters();
+      if (activeFilters && activeFilters.length > 0) {
+        return (
+          <CenteredContentWrapper minHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}>
+            <div className="px-2 max-content-width mx-auto" style={{ minWidth: "505px" }}>
+              <div className="my-5"><InformationDialog message="No pipelines meeting the filter requirements were found."/></div>
+            </div>
+          </CenteredContentWrapper>
+        );
+      }
+
+      return (
+        <CenteredContentWrapper minHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}>
+          <div className="px-2 max-content-width" style={{ minWidth: "505px" }}>
+            <div className="my-5"><InformationDialog message="No pipelines are available for this view at this time."/></div>
+          </div>
+        </CenteredContentWrapper>
+      );
+    }
+
+    return (getBody());
+  };
+
+  const addPipeline = () => {
+    history.push(`/workflow/catalog/library`);
+  };
+
+  const onRowSelect = (rowData) => {
+    history.push(`/workflow/details/${rowData.original._id}/summary`);
+  };
+
+  const getCardView = () => {
+    return (
+      <PipelineCardView
+        isLoading={isLoading}
+        loadData={loadData}
+        pipelines={pipelines}
+        subscribedPipelineIds={subscribedPipelineIds}
+      />
+    );
+  };
+
+  const getTableView = () => {
+    return (
+      <PipelinesTableBase
+        isLoading={isLoading}
+        pipelines={pipelines}
+        loadData={loadData}
+        onRowClickFunction={onRowSelect}
+      />
+    );
+  };
+
+  const getVerticalTabContainer = () => {
+    return (
+      <PipelineVerticalTabContainer
+        pipelineFilterModel={pipelineFilterModel}
+        isLoading={isLoading}
+        loadData={loadData}
+      />
+    );
+  };
+
+  const getCurrentView = () => {
+    return (
+      <TableCardView
+        filterModel={pipelineFilterModel}
+        data={pipelines}
+        isLoading={isLoading}
+        cardView={getCardView()}
+        tableView={getTableView()}
+      />
+    );
   };
 
   return (
@@ -79,18 +174,17 @@ function PipelineManagement() {
       navigationTabContainer={<WorkflowSubNavigationBar currentTab={"pipelines"}/>}
       pageDescription={"Select a Pipeline to view details."}
       auditLogType={auditLogTypeConstants.USER_ACTIVITY_LOG_TYPES.PIPELINE}
+      filterOverlay={<PipelineFilterOverlay pipelineFilterModel={pipelineFilterModel} loadDataFunction={loadData} />}
+      addRecordFunction={PipelineRoleHelper.canCreatePipeline(userData) === true ? addPipeline : undefined}
+      loadDataFunction={loadData}
+      isSoftLoading={isLoading}
+      filterModel={pipelineFilterModel}
+      setFilterModel={setPipelineFilterModel}
       helpComponent={
         <PipelinesHelpDocumentation/>
       }
     >
-      <PipelineTableCardView
-        pipelines={pipelines}
-        isLoading={isLoading}
-        pipelineFilterModel={pipelineFilterModel}
-        setPipelineFilterModel={setPipelineFilterModel}
-        loadData={loadData}
-        subscribedPipelineIds={subscribedPipelineIds}
-      />
+      {getPipelinesBody()}
     </ScreenContainer>
   );
 
