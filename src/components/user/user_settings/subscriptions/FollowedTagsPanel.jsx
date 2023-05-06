92787eaf81962ff49c0eb8e447ca8f1cee798207
Noah Champoux <noah@opsera.io>
2023-05-06 09:16:02 -0500

    [Development Tools] New components and helper function work

---
+++ src/components/user/user_settings/subscriptions/FollowedTagsPanel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MyTagCloud from "components/common/fields/tags/cloud/MyTagCloud";
+
+export default function FollowedTagsPanel() {
+  return (
+    <div className={"p-3"}>
+      <MyTagCloud />
+    </div>
+  );
+}
