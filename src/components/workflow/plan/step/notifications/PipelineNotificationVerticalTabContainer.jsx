4389c2d24e64a63b943374af4ac1f3ff387aad28
Jim Provenza <Jim@Opsera.io>
2023-05-08 12:18:31 -0600

    update setep tool configuration to include pipeline ID, pipeline step, and pipeline for approval gate step configuration. Update approval gate step configuration to include pipeline step notification configuration overlay. update pipeline step editor, pipeline notification tab view, and pipeline notification vertical tab container

--- src/components/workflow/plan/step/notifications/PipelineNotificationVerticalTabContainer.jsx
+++ src/components/workflow/plan/step/notifications/PipelineNotificationVerticalTabContainer.jsx
@@ -3,53 +3,60 @@ import React from "react";
 import VanitySetVerticalTab from "components/common/tabs/vertical_tabs/VanitySetVerticalTab";
 import {faMicrosoft, faSlack, faJira, faGoogle} from "@fortawesome/free-brands-svg-icons";
 import VanitySetVerticalTabContainer from "components/common/tabs/vertical_tabs/VanitySetVerticalTabContainer";
-import {faEnvelope} from "@fortawesome/pro-light-svg-icons";
+import {faEnvelope, faLightbulb} from "@fortawesome/pro-light-svg-icons";
+import IconBase from "components/common/icons/IconBase";
 
 function PipelineNotificationVerticalTabContainer(
   {
     handleTabClickFunction,
     activeTab,
+    slackEnabled, 
+    teamsEnabled,
+    jiraEnabled,
+    serviceNowEnabled,
+    emailEnabled,
+    gChatEnabled,
   }) {
   return (
     <VanitySetVerticalTabContainer>
       <VanitySetVerticalTab
         icon={faEnvelope}
-        tabText={"Email"}
         tabName={"email"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
+        tabText = {emailEnabled ? <div className={"d-flex"}>Email<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Email"}
       />
       <VanitySetVerticalTab
         icon={faJira}
-        tabText={"Jira"}
+        tabText = {jiraEnabled ? <div className={"d-flex"}>Jira<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Jira"}
         tabName={"jira"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       <VanitySetVerticalTab
         icon={faMicrosoft}
-        tabText={"Microsoft Teams"}
+        tabText = {teamsEnabled ? <div className={"d-flex"}>Microsoft Teams<IconBase icon={faLightbulb} className={"ml-1"}/> </div> : "Microsoft Teams"}
         tabName={"teams"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       <VanitySetVerticalTab
         icon={faEnvelope}
-        tabText={"Service Now"}
+        tabText = {serviceNowEnabled ? <div className={"d-flex"}>Service Now<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Service Now"}
         tabName={"service-now"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       <VanitySetVerticalTab
         icon={faSlack}
-        tabText={"Slack"}
+        tabText = {slackEnabled ? <div className={"d-flex"}>Slack<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Slack"}
         tabName={"slack"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       <VanitySetVerticalTab
         icon={faGoogle}
-        tabText={"Google Chat"}
+        tabText = {gChatEnabled ? <div className={"d-flex"}>Google Chat<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Google Chat"}
         tabName={"gchat"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
@@ -61,6 +68,12 @@ function PipelineNotificationVerticalTabContainer(
 PipelineNotificationVerticalTabContainer.propTypes = {
   activeTab: PropTypes.string,
   handleTabClickFunction: PropTypes.func,
+  slackEnabled: PropTypes.bool,
+  jiraEnabled: PropTypes.bool, 
+  teamsEnabled: PropTypes.bool, 
+  serviceNowEnabled: PropTypes.bool,
+  emailEnabled: PropTypes.bool,
+  gChatEnabled: PropTypes.bool,
 };
 
 export default PipelineNotificationVerticalTabContainer;
