9b8b610fbfee8230efeed515791845a2ea036be2
Vignesh Subramanian <vignesh@opsera.io>
2023-05-08 19:30:11 +0530

    OPL-10111 pull and push rpts fields

--- src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/oracle_fusion_report_migration/oracleFusion-reportMigration-stepForm-metadata.js
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/oracle_fusion_report_migration/oracleFusion-reportMigration-stepForm-metadata.js
@@ -1,3 +1,5 @@
+import metadataConstants from "@opsera/definitions/constants/metadata/metadata.constants";
+
 const OracleFusionReportMigrationStepFormMetadata = {
   type: "Oracle Fusion Report Migration Step Configuration",
   fields: [
@@ -47,6 +49,80 @@ const OracleFusionReportMigrationStepFormMetadata = {
         return model?.getData("migrationType") === "artifactory_to_instance";
       },
     },
+    {
+      label: "Source Code Management Type",
+      id: "service",
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "pull_reports";
+      },
+    },
+    {
+      label: "Source Code Management Tool",
+      id: "gitToolId",
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "pull_reports";
+      },
+    },
+    {
+      label: "Repository",
+      id: "repository",
+      dynamicSettingType: metadataConstants.SUPPORTED_DYNAMIC_SETTING_TYPES.REPOSITORY_NAME,
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "pull_reports";
+      },
+    },    
+    {
+      label: "Repository",
+      id: "repoId",
+      dynamicSettingType: metadataConstants.SUPPORTED_DYNAMIC_SETTING_TYPES.REPOSITORY_ID,
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "pull_reports";
+      },
+    },
+    {
+      label: "Branch",
+      id: "gitBranch",
+      dynamicSettingType: metadataConstants.SUPPORTED_DYNAMIC_SETTING_TYPES.PRIMARY_BRANCH,
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "pull_reports";
+      },
+    },
+    {
+      id: "sshUrl"
+    },
+    {
+      id: "gitUrl"
+    },
+    {
+      label: "GIT Commit Id",
+      id: "gitCommitId",      
+    },
+    {
+      label: "Nexus Tool",
+      id: "nexusToolConfigId",
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "push_reports";
+      },
+    },
+    {
+      label: "Group Name",
+      id: "groupName",
+      regexDefinitionName: "generalText",
+      maxLength: 50,
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "push_reports";
+      },
+    },
+    {
+      id: "repositoryGroup",
+    },
+    {
+      label: "Repository",
+      id: "repositoryName",
+      isRequiredFunction: (model) => {
+        return model?.getData("migrationType") === "push_reports";
+      },
+    },
   ],
   newObjectFields: {
     migrationType: "",
@@ -55,7 +131,18 @@ const OracleFusionReportMigrationStepFormMetadata = {
     sourceInstanceReports: [],
     targetInstanceToolId: "",
     targetInstancePath: "",
-    artifactStepId: "",    
+    artifactStepId: "",
+    service: "github",
+    repoId: "",
+    repository: "",
+    gitBranch: "",
+    sshUrl: "",    
+    gitUrl: "",
+    gitCommitId: "",
+    nexusToolConfigId: "",
+    groupName: "",
+    repositoryGroup : "maven2",
+    repositoryName: "",
   }
 };
 
