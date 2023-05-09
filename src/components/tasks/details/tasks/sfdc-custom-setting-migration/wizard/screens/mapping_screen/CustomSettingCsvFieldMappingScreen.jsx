2855d369007da310ce954acfd1dd1eee213a7150
Faseeh <faseeh@opsera.io>
2023-05-10 00:30:35 +0530

    OPL-5933: added field fetch on mapping screen

--- src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/screens/mapping_screen/CustomSettingCsvFieldMappingScreen.jsx
+++ src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/screens/mapping_screen/CustomSettingCsvFieldMappingScreen.jsx
@@ -18,6 +18,8 @@ import customSettingMigrationTaskWizardActions from "../../customSettingMigratio
 import { parseError } from "../../../../../../../common/helpers/error-helpers";
 import useAxiosCancelToken from "../../../../../../../../hooks/useAxiosCancelToken";
 import axios from "axios";
+import Col from "react-bootstrap/Col";
+import SelectInputBase from "../../../../../../../common/inputs/select/SelectInputBase";
 
 const CustomSettingCsvFieldMappingScreen = ({
   wizardModel,
@@ -30,59 +32,104 @@ const CustomSettingCsvFieldMappingScreen = ({
   const toastContext = useContext(DialogToastContext);
   const [isLoading, setIsLoading] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
+  const [filePullCompleted, setFilePullCompleted] = useState(false);
   const [fieldsPropertiesList, setFieldsPropertiesList] = useState([]);
   const [mappedData, setMappedData] = useState([]);
 
   const { cancelTokenSource } = useAxiosCancelToken();
   const isMounted = useRef(false);
+  let timerIds = [];
 
   useEffect(() => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel();
+    }
+
+    const source = axios.CancelToken.source();
     isMounted.current = true;
-    loadData().catch((error) => {
+    setFieldsPropertiesList([]);
+
+    loadData(source).catch((error) => {
       if (isMounted?.current === true) {
         throw error;
       }
     });
 
     return () => {
+      source.cancel();
       isMounted.current = false;
+      stopPolling();
     };
   }, []);
 
   const loadData = async (cancelSource = cancelTokenSource) => {
     try {
       setIsLoading(true);
-      console.log("load data");
-      const response =
-        await customSettingMigrationTaskWizardActions.pullFieldList(
-          getAccessToken,
-          cancelSource,
-          wizardModel,
-        );
-      const errorMessage = response?.data?.data?.errorMessage;
-      const fieldList = response?.data?.data?.fieldList;
-
-      console.log(fieldList);
+      await handleFieldPropertiesListPolling(cancelSource);
+    } catch (error) {
       if (isMounted?.current === true) {
-        if (errorMessage) {
-          const parsedError = parseError(errorMessage);
-          toastContext.showInlineErrorMessage(
-            `Service Error Fetching Fields: ${parsedError}`,
-          );
-        }
-
-        if (Array.isArray(fieldList)) {
-          setFieldsPropertiesList(fieldList);
-        }
+        toastContext.showInlineErrorMessage(error);
+      }
+    } finally {
+      if (isMounted?.current === true) {
+        setIsLoading(false);
       }
     }
-    catch (error) {
-      console.error(error);
-      toastContext.showInlineErrorMessage(error);
+  };
+
+  const handleFieldPropertiesListPolling = async (
+    cancelSource = cancelTokenSource,
+    count = 1,
+  ) => {
+    if (isMounted?.current !== true) {
+      return;
     }
-    finally {
-      setIsLoading(false);
+
+    const newFileList = await getFieldPropertiesList(cancelSource);
+
+    if (
+      !Array.isArray(newFileList) &&
+      count <= 5 &&
+      filePullCompleted === false
+    ) {
+      await new Promise((resolve) => timerIds.push(setTimeout(resolve, 15000)));
+      return await handleFieldPropertiesListPolling(cancelSource, count + 1);
     }
+  };
+
+  const stopPolling = () => {
+    if (Array.isArray(timerIds) && timerIds.length > 0) {
+      timerIds?.forEach((timerId) => clearTimeout(timerId));
+    }
+  };
+
+  const getFieldPropertiesList = async (cancelSource = cancelTokenSource) => {
+    setIsLoading(true);
+    const response =
+      await customSettingMigrationTaskWizardActions.pullFieldList(
+        getAccessToken,
+        cancelSource,
+        wizardModel,
+      );
+    const errorMessage = response?.data?.data?.errorMessage;
+    const fieldList = response?.data?.data?.fieldList;
+
+    if (isMounted?.current === true) {
+      if (errorMessage) {
+        const parsedError = parseError(errorMessage);
+        toastContext.showInlineErrorMessage(
+          `Service Error Fetching Fields: ${parsedError}`,
+        );
+      }
+
+      if (Array.isArray(fieldList)) {
+        setFieldsPropertiesList(fieldList);
+        setIsLoading(false);
+        setFilePullCompleted(true);
+      }
+    }
+
+    return fieldList;
   };
 
   const handleBackButton = () => {
@@ -114,7 +161,7 @@ const CustomSettingCsvFieldMappingScreen = ({
     }
   };
   const getBody = () => {
-    if (wizardModel == null) {
+    if (wizardModel == null || isLoading) {
       return (
         <CenterLoadingIndicator
           minHeight={"500px"}
@@ -133,8 +180,23 @@ const CustomSettingCsvFieldMappingScreen = ({
           />
         </Row>
         {fieldsPropertiesList && fieldsPropertiesList.length > 1 &&
-          <Row>
-
+          <Row className="d-flex mx-1 justify-content-between">
+            <Col sm={12} className={"px-0"}>
+              <Row className={"mx-0"}>
+                <Col
+                  xs={6}
+                  className={"pr-1 pl-0"}
+                >
+                  Selection for fields
+                </Col>
+                <Col
+                  xs={6}
+                  className={"pr-1 pl-0"}
+                >
+                  Selection for fields
+                </Col>
+              </Row>
+            </Col>
           </Row>
         }
       </div>
