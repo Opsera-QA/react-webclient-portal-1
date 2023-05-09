75d3c31118cdfc9af3e9c3ad97711b31fbe56070
Matthew Greci <mgreci@opsera.io>
2023-05-09 15:58:06 -0600

    correct location of legend

--- src/components/insights/charts/gitlab/calendar_chart/commits_by_author/gitlabCommitsByAuthorConfig.js
+++ src/components/insights/charts/gitlab/calendar_chart/commits_by_author/gitlabCommitsByAuthorConfig.js
@@ -10,9 +10,26 @@ export default (users) => ({
   cellComponent: "circle",
   hoverTarget: "cell",
   cellHoverOtherOpacity: .25,
+  legends: [
+    {
+      anchor: "top-right",
+      direction: "column",
+      justify: false,
+      translateX: 40,
+      translateY: -60,
+      itemsSpacing: 0,
+      itemDirection: "right-to-left",
+      itemWidth: 80,
+      itemHeight: 50,
+      itemOpacity: 1,
+      symbolSize: 10,
+      symbolShape: "square",
+      symbolBorderColor: "rgba(0, 0, 0, .5)",
+    },
+  ],
   margin: {
     top: 10,
-    right: 40,
+    right: 80,
     bottom: 60,
     left: 40
   },
