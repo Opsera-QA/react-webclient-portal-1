a423a6c6db86882d0401ad8603e691373656b969
Noah Champoux <noah@opsera.io>
2023-05-11 09:30:35 -0500

    New card wip

--- src/temp-library-components/cards/pipelines/PipelineCardBody.jsx
+++ src/temp-library-components/cards/pipelines/PipelineCardBody.jsx
@@ -1,70 +1,14 @@
 import PropTypes from "prop-types";
 import React from "react";
 import Row from "react-bootstrap/Row";
-import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
-import DateFormatHelper from "@opsera/persephone/helpers/date/dateFormat.helper";
 import {Col} from "react-bootstrap";
-
-const getLastRunDetails = (pipelineModel) => {
-  const runCount = DataParsingHelper.parseInteger(pipelineModel?.getData("workflow.run_count"), 0);
-  const lastRunCompletionDate = pipelineModel?.getData("workflow.last_run.completed");
-
-  if (runCount === 0) {
-    return (
-      <div className={"d-flex"}>
-        <div className={"text-muted m-auto"}>
-          {`This Pipeline has not been run yet`}
-        </div>
-      </div>
-    );
-  }
-
-  if (lastRunCompletionDate != null) {
-    return (
-      <div className={"d-flex text-muted"}>
-        <div className={"mx-auto"}>
-          {DateFormatHelper.formatDateAsTimestampWithoutSeconds(lastRunCompletionDate)}
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className={"d-flex"}>
-      <div className={"text-muted m-auto"}>
-        {`No metrics captured for last run`}
-      </div>
-    </div>
-  );
-};
-
-const getLastRunEntry = (pipelineModel) => {
-  const runCount = DataParsingHelper.parseInteger(pipelineModel?.getData("workflow.run_count"), 0);
-  const lastRunColor =
-    runCount > 0
-      ? ""
-      : "white";
-
-  return (
-    <Col xs={12}>
-      <div className={"mt-2"}>
-        <div className={"d-flex"}>
-          <div className={"mx-auto"}>
-            <span style={{color: lastRunColor}}>Last Run</span>
-          </div>
-        </div>
-        {getLastRunDetails(pipelineModel)}
-      </div>
-    </Col>
-  );
-};
+import {orchestrationHelper} from "temp-library-components/helpers/orchestration/orchestration.helper";
+import {pipelineHelper} from "components/workflow/pipeline.helper";
 
 export default function PipelineCardBody(
   {
     pipelineModel,
   }) {
-  const formattedLastRun = getLastRunEntry(pipelineModel);
-
   if (pipelineModel == null) {
     return undefined;
   }
@@ -72,7 +16,19 @@ export default function PipelineCardBody(
   return (
     <div className={"mb-1"}>
       <Row className={"small"}>
-        {formattedLastRun}
+        <Col xs={12}>
+          <div
+            className={"w-100 d-flex mt-2"}
+            style={{
+              minHeight: "39px",
+              maxHeight: "39px",
+            }}
+          >
+            <div className={"mt-auto mx-auto"}>
+              {orchestrationHelper.getLastRunCardSummary(pipelineHelper.getLastRunCompletionTime(pipelineModel?.getOriginalData()), pipelineHelper.getPipelineModelOrchestrationState(pipelineModel))}
+            </div>
+          </div>
+        </Col>
       </Row>
     </div>
   );
