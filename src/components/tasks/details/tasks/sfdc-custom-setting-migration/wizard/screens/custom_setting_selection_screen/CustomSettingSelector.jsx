9ef371ee066007c78a0d0dff0c9a5d1c3ea27678
Faseeh <faseeh@opsera.io>
2023-05-11 22:59:28 +0530

    OPL-5933: OPL-10136: fixed custom obj selected value font

--- src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/screens/custom_setting_selection_screen/CustomSettingSelector.jsx
+++ src/components/tasks/details/tasks/sfdc-custom-setting-migration/wizard/screens/custom_setting_selection_screen/CustomSettingSelector.jsx
@@ -190,13 +190,13 @@ const CustomSettingSelector = ({ wizardModel, setWizardModel, handleClose, setCu
     if (!enableEdit && wizardModel?.getData("selectedCustomSetting")) {
       return (
         <div className={"d-flex mx-1 my-1 w-100"}>
-          Selected Custom Object :{" "}
-          {wizardModel?.getData("selectedCustomSetting")?.componentName}
+          <h5>Selected Custom Object :</h5>{" "}
+          <span className="ml-2" style={{ fontSize: "1rem", fontWeight: 500}}>{wizardModel?.getData("selectedCustomSetting")?.componentName}</span>
           <EnableEditingIcon
             enableEditingFunction={() => {
               setEnableEdit(true);
             }}
-            className={"ml-2 my-auto"}
+            className={"ml-2 mb-1 my-auto"}
           />
         </div>
       );
