3d9fbd96d6f19df5b484ab761f055da5ed80838d
Noah Champoux <noah@opsera.io>
2023-05-12 13:04:21 -0500

    Update CreateWorkspaceResourceWizard.jsx

--- src/components/wizard/workspace/CreateWorkspaceResourceWizard.jsx
+++ src/components/wizard/workspace/CreateWorkspaceResourceWizard.jsx
@@ -41,13 +41,13 @@ export default function CreateWorkspaceResourceWizard({ loadDataFunction }) {
           />
         );
       case CREATE_WORkSPACE_RESOURCE_WIZARD_SCREENS.CREATE_TASK_SCREEN:
-        if (isActive === true) {
-          return (
+        // if (isActive === true) {
+        //   return (
             // <CreateTasksWizard
             //   loadData={loadDataFunction}
             // />
-          );
-        }
+          // );
+        // }
 
         return (
           <NewTaskOverlay
