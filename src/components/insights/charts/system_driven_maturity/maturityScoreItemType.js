c19128afe3904ba2e296e47e278cf41e216a3ab0
mgreci-opsera <113466817+mgreci-opsera@users.noreply.github.com>
2023-05-12 06:36:45 -0600

    OPL-9933 MASTER: SDM 2.0 - Please show maturity of Individual KPIs within the org / group tags (#511)
    * center missing filter text
    
    * add more prop to MaturityScoreItemType
    
    * capture all properties from group
    
    * move functions to util file
    
    * display each kpi for each org tag

--- src/components/insights/charts/system_driven_maturity/maturityScoreItemType.js
+++ src/components/insights/charts/system_driven_maturity/maturityScoreItemType.js
@@ -3,5 +3,9 @@ import PropTypes from 'prop-types';
 export const MaturityScoreItemType = PropTypes.shape({
   name: PropTypes.string,
   score: PropTypes.string,
-  previousScore: PropTypes.string
+  previousScore: PropTypes.string,
+  deploymentFrequencyMaturityScoreText: PropTypes.string,
+  leadTimeForChangesMaturityScoreText: PropTypes.string,
+  changeFailureRateMaturityScoreText: PropTypes.string,
+  meanTimeToResolutionMaturityScoreText: PropTypes.string,
 });
