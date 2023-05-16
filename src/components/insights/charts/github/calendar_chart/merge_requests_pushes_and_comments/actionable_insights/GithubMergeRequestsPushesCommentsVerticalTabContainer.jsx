bb38d1f6ebe6592a817a26ab08f51f0b29455c16
DivyeshaMalhotra <divyesha@opsera.us>
2023-05-15 20:47:22 -0400

    OPL-10026-Master github merges comments actionable insights

---
+++ src/components/insights/charts/github/calendar_chart/merge_requests_pushes_and_comments/actionable_insights/GithubMergeRequestsPushesCommentsVerticalTabContainer.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import PropTypes from "prop-types";
+import VanitySetTabAndViewContainer from "components/common/tabs/vertical_tabs/VanitySetTabAndViewContainer";
+import GithubMergeRequestsPushesCommentsActionableTableOverlay
+    from "./GithubMergeRequestsPushesCommentsActionableTableOverlay";
+import VanitySetTabViewContainer from "../../../../../../common/tabs/vertical_tabs/VanitySetTabViewContainer";
+import VanitySetTabView from "../../../../../../common/tabs/vertical_tabs/VanitySetTabView";
+import GithubMergesPushesVerticalTabContainer from "./GithubMergesPushesVerticalTabContainer";
+
+function GithubMergeRequestsPushesCommentsVerticalTabContainer({
+                                                                   highestMergesMetric,
+                                                                   dashboardData,
+                                                                   kpiConfiguration,
+                                                                   icon,
+                                                                   date,
+                                                               }) {
+    const getTabContentContainer = () => {
+        return (
+            <VanitySetTabViewContainer>
+                {highestMergesMetric.map((item, index) => (
+                    <VanitySetTabView
+                        key={index}
+                        tabKey={item}
+                    >
+                        <GithubMergeRequestsPushesCommentsActionableTableOverlay
+                            projectName={item}
+                            dashboardData={dashboardData}
+                            kpiConfiguration={kpiConfiguration}
+                            icon={icon}
+                            date={date}
+                        />
+                    </VanitySetTabView>
+                ))}
+            </VanitySetTabViewContainer>
+        );
+    };
+
+    if(highestMergesMetric.length > 0) {
+        return (
+            <VanitySetTabAndViewContainer
+                title={`Github Event Name`}
+                currentView={getTabContentContainer()}
+                defaultActiveKey={
+                    highestMergesMetric &&
+                    Array.isArray(highestMergesMetric) &&
+                    highestMergesMetric[0] &&
+                    highestMergesMetric[0]
+                }
+                verticalTabContainer={
+                    <GithubMergesPushesVerticalTabContainer
+                        highestMergesMetrics={highestMergesMetric}
+                    />
+                }
+                maximumHeight={"h-200"}
+            />
+        );
+    }
+}
+GithubMergeRequestsPushesCommentsVerticalTabContainer.propTypes = {
+    highestMergesMetric: PropTypes.array,
+    dashboardData: PropTypes.object,
+    kpiConfiguration: PropTypes.object,
+    icon: PropTypes.object,
+    date : PropTypes.string,
+
+};
+export default GithubMergeRequestsPushesCommentsVerticalTabContainer;
