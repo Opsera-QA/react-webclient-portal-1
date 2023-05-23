387bde3b152713be7fe491f84d7b3fdc5956e882
Noah Champoux <noah@opsera.io>
2023-05-23 07:00:35 -0600

    Websocket test

--- src/hooks/settings/tags/id/useGetTagById.jsx
+++ src/hooks/settings/tags/id/useGetTagById.jsx
@@ -3,8 +3,16 @@ import useLoadData from "temp-library-components/useLoadData/useLoadData";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 import useTagActions from "hooks/settings/tags/useTagActions";
 import {isMongoDbId} from "components/common/helpers/mongo/mongoDb.helpers";
+import useComponentStateReference from "hooks/useComponentStateReference";
+import {websocketLiveUpdateHelper} from "core/websocket/websocket.helper";
+import useItemSubscription from "core/websocket/hooks/useItemSubscription";
+import {useHistory} from "react-router-dom";
+import {accountSettingsTrails} from "components/settings/accountSettings.trails";
+import liveMessageTopicConstants from "@opsera/definitions/constants/websocket/constants/liveMessageTopic.constants";
 
 export default function useGetTagById(tagId, handleErrorFunction) {
+  const {toastContext} = useComponentStateReference();
+  const history = useHistory();
   const tagActions = useTagActions();
   const [tag, setTag] = useState(undefined);
   const {
@@ -13,6 +21,22 @@ export default function useGetTagById(tagId, handleErrorFunction) {
     setError,
     loadData,
   } = useLoadData();
+
+  const onUpdateFunction = (newTag) => {
+    websocketLiveUpdateHelper.handleSingleObjectLiveUpdate(tag, newTag, setTag);
+  };
+
+  const onDeleteFunction = (tagId) => {
+    toastContext.showSystemInformationToast("The Tag has been deleted.");
+    history.push(`/${accountSettingsTrails.tagManagement.path}`);
+  };
+
+  useItemSubscription(
+    liveMessageTopicConstants.LIVE_MESSAGE_TOPICS.TAGS,
+    tagId,
+    onUpdateFunction,
+    onDeleteFunction,
+  );
 
   useEffect(() => {
     setTag(undefined);
@@ -24,6 +48,11 @@ export default function useGetTagById(tagId, handleErrorFunction) {
 
   const getCustomerTags = async () => {
     setTag(undefined);
+
+    if (isMongoDbId(tagId) !== true) {
+      return;
+    }
+
     const response = await tagActions.getTagById(tagId);
     const tag = DataParsingHelper.parseNestedObject(response, "data.data");
 
