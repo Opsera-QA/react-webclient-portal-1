45166d4f40bf4e841fc7bf8e044e6d6b238d173c
Christina Bowers <christina@opsera.io>
2023-05-25 15:14:53 -0400

    OPL10488 kpi help links

--- src/components/common/help/documentation/insights/charts/AquasecIssuesBySeverityHelpDocumentation.jsx
+++ src/components/common/help/documentation/insights/charts/AquasecIssuesBySeverityHelpDocumentation.jsx
@@ -9,7 +9,7 @@ function AquasecIssuesBySeverityHelpDocumentation({ closeHelpPanel }) {
                 The purpose of this KPI is to show Issues by severity. Each data block defines the total number of issues in all
                 projects by severity. Each datablock will also display the trend of each severity, based on the previous
                 scan result of each project. The KPI also displays the top 3 projects that have the overall highest number of
-                issues.
+                issues. For more information view the <a href="https://docs.opsera.io/insights/kpi/aquasec-security-insights" target="_blank" rel="noreferrer"><b>Aquasec Security Insights Help Documentation</b>.</a>
             </div>
         </HelpDocumentationContainer>
     );
