2318f1bc04f6ddbbaaae9c8a8b6096d64ea16d23
Faseeh <faseeh@opsera.io>
2023-05-25 15:54:09 +0530

    OPL-9274: fixed report error

--- src/config.js
+++ src/config.js
@@ -13,4 +13,5 @@ export const okta_config = {
 
 export const NODE_API_ORCHESTRATOR_SERVER_URL = process.env.REACT_APP_OPSERA_API_SERVER_URL;
 export const NODE_ANALYTICS_API_SERVER_URL = process.env.REACT_APP_ANALYTICS_API_SERVER_URL;
-export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = process.env.REACT_APP_OPSERA_API_SERVER_URL+"/data-migration-file";
+// export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = process.env.REACT_APP_OPSERA_API_SERVER_URL+"/data-migration-file";
+export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = "https://api.opsera-dev.opsera.io/data-migration-file";
