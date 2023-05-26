8a6051e0898a459a18873c7c2ff9b79fdfdd4bbf
Faseeh <faseeh@opsera.io>
2023-05-26 10:19:17 +0530

    OPL-9274: minor fixes on csv flow

--- src/config.js
+++ src/config.js
@@ -8,10 +8,13 @@ export const okta_config = {
   redirect_uri: process.env.REACT_APP_OPSERA_OKTA_REDIRECTURI,
   pkce: true,
   disableHttpsCheck: false,
-  onAuthRequired: onAuthRequired
+  onAuthRequired: onAuthRequired,
 };
 
-export const NODE_API_ORCHESTRATOR_SERVER_URL = process.env.REACT_APP_OPSERA_API_SERVER_URL;
-export const NODE_ANALYTICS_API_SERVER_URL = process.env.REACT_APP_ANALYTICS_API_SERVER_URL;
-// export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = process.env.REACT_APP_OPSERA_API_SERVER_URL+"/data-migration-file";
-export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = "https://api.opsera-dev.opsera.io/data-migration-file";
+export const NODE_API_ORCHESTRATOR_SERVER_URL =
+  process.env.REACT_APP_OPSERA_API_SERVER_URL;
+export const NODE_ANALYTICS_API_SERVER_URL =
+  process.env.REACT_APP_ANALYTICS_API_SERVER_URL;
+export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL =
+  process.env.REACT_APP_OPSERA_API_SERVER_URL + "/data-migration-file";
+// export const NODE_DATA_MIGRATION_FILE_API_SERVER_URL = "https://api.opsera-dev.opsera.io/data-migration-file";
