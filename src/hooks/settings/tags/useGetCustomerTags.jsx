6a18b95074921cbc361299dd62ecf04ae369dd01
Noah Champoux <noah@opsera.io>
2023-05-30 08:02:28 -0600

    WIP

--- src/hooks/settings/tags/useGetCustomerTags.jsx
+++ src/hooks/settings/tags/useGetCustomerTags.jsx
@@ -2,8 +2,8 @@ import {useEffect, useState} from "react";
 import useLoadData from "temp-library-components/useLoadData/useLoadData";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 import useTagActions from "hooks/settings/tags/useTagActions";
-import useComponentStateReference from "hooks/useComponentStateReference";
 import liveMessageTopicConstants from "@opsera/definitions/constants/websocket/constants/liveMessageTopic.constants";
+import useCollectionSubscriptionHelper from "core/websocket/hooks/useCollectionSubscriptionHelper";
 
 export default function useGetCustomerTags(handleErrorFunction) {
   const tagActions = useTagActions();
@@ -14,7 +14,48 @@ export default function useGetCustomerTags(handleErrorFunction) {
     setError,
     loadData,
   } = useLoadData();
-  const {websocketClient} = useComponentStateReference();
+  const onCreateFunction = (newTag) => {
+    const parsedTag = DataParsingHelper.parseObject(newTag);
+
+    if (parsedTag) {
+      const tagIndex = customerTags.findIndex((tag) => tag._id === parsedTag._id);
+
+      if (tagIndex === -1) {
+        customerTags.push(parsedTag);
+        setCustomerTags([...customerTags]);
+      }
+    }
+  };
+
+  const onUpdateFunction = (updatedTag) => {
+    const parsedTag = DataParsingHelper.parseObject(updatedTag);
+
+    if (parsedTag) {
+      const tagIndex = customerTags.findIndex((tag) => tag._id === parsedTag._id);
+
+      if (tagIndex !== -1) {
+        customerTags[tagIndex] = parsedTag;
+        setCustomerTags([...customerTags]);
+      }
+    }
+  };
+
+  const onDeleteFunction = (deletedTag) => {
+    const parsedTag = DataParsingHelper.parseObject(deletedTag);
+
+    if (parsedTag) {
+      const parsedTagMongoDbId = DataParsingHelper.parseMongoDbId(parsedTag, "_id");
+      const updatedTags = customerTags.filter((tag) => tag._id !== parsedTagMongoDbId);
+      setCustomerTags([...updatedTags]);
+    }
+  };
+
+  useCollectionSubscriptionHelper(
+    liveMessageTopicConstants.LIVE_MESSAGE_TOPICS.TAGS,
+    onCreateFunction,
+    onUpdateFunction,
+    onDeleteFunction,
+  );
 
   useEffect(() => {
     setCustomerTags([]);
@@ -28,7 +69,6 @@ export default function useGetCustomerTags(handleErrorFunction) {
     setCustomerTags([]);
     const response = await tagActions.getAllTagsV2(false);
     setCustomerTags(DataParsingHelper.parseArray(response?.data?.data, []));
-    websocketClient.subscribeToTopic(liveMessageTopicConstants.LIVE_MESSAGE_TOPICS.TAGS);
   };
 
   return ({
