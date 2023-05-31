960fb838865894cf0d8c768d65477ddc5b724e82
Noah Champoux <noah@opsera.io>
2023-05-31 08:34:57 -0600

    Update CustomerPipelineTemplateCardBody.jsx

--- src/temp-library-components/cards/templates/pipelines/customer/CustomerPipelineTemplateCardBody.jsx
+++ src/temp-library-components/cards/templates/pipelines/customer/CustomerPipelineTemplateCardBody.jsx
@@ -1,17 +1,7 @@
 import PropTypes from "prop-types";
 import React from "react";
 import Row from "react-bootstrap/Row";
-import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
 import {Col} from "react-bootstrap";
-import {orchestrationHelper} from "temp-library-components/helpers/orchestration/orchestration.helper";
-import {pipelineHelper} from "components/workflow/pipeline.helper";
-import SelectButtonBase from "components/common/buttons/select/base/SelectButtonBase";
-import modelHelpers from "components/common/model/modelHelpers";
-import {createPipelineFromTemplateMetadata} from "components/workflow/catalog/createPipelineFromTemplate.metadata";
-import {faDraftingCompass} from "@fortawesome/pro-light-svg-icons";
-import CreateCustomerPipelineButton from "temp-library-components/cards/templates/pipelines/customer/deploy/CreateCustomerPipelineButton";
-import ViewCustomerPipelineTemplateDetailsButton
-  from "components/workflow/catalog/private/ViewCustomerPipelineTemplateDetailsButton";
 import {truncateString} from "components/common/helpers/string-helpers";
 
 export default function CustomerPipelineTemplateCardBody(
