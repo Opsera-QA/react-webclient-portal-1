04db9b010b9b388dd8009839af3893a2add808e2
Noah Champoux <noah@opsera.io>
2023-05-31 09:10:51 -0600

    Update PipelineTemplateSelectionScreen.jsx

--- src/components/workflow/create/PipelineTemplateSelectionScreen.jsx
+++ src/components/workflow/create/PipelineTemplateSelectionScreen.jsx
@@ -69,6 +69,7 @@ export default function PipelineTemplateSelectionScreen(
           activeTemplates={activeTemplates}
           selectTemplateFunction={setSelectedPlatformTemplate}
           setupMode={setupMode}
+          cardTooltip={"Click to select Pipeline"}
         />
       );
     }
@@ -81,6 +82,7 @@ export default function PipelineTemplateSelectionScreen(
             activeTemplates={activeTemplates}
             selectTemplateFunction={setSelectedCustomerTemplate}
             setupMode={setupMode}
+            cardTooltip={"Click to select Pipeline"}
           />
         );
     }
