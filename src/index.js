b2ae7f78a3ff3ad88b94c083ec25cb1fb5a2995b
Noah Champoux <noah@opsera.io>
2023-06-05 07:53:40 -0600

    [Stability] Better handling of startup with proper monitoring file for React 17+

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import "core-js";
 import React from "react";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 import ReactDOM from "react-dom";
+import reportWebVitals from "reportWebVitals";
 // import { createRoot } from 'react-dom/client';
 
 if (typeof window["TextEncoder"] !== "function") {
@@ -14,14 +13,14 @@ if (typeof window["TextEncoder"] !== "function") {
 
 const browserNotSupported = (function (agent) {
   switch (true) {
-  case agent.indexOf("edge") > -1: return false; // "edge";
-  case agent.indexOf("edg") > -1: return false; //"chromium based edge (dev or canary)";
-  case agent.indexOf("opr") > -1 && !!window.opr: return false; // "opera";
-  case agent.indexOf("chrome") > -1 && !!window.chrome: return false; // "chrome";
-  case agent.indexOf("trident") > -1: return "ie";
-  case agent.indexOf("firefox") > -1: return false; // "firefox";
-  case agent.indexOf("safari") > -1: return false; // "safari";
-  default: return false;
+    case agent.indexOf("edge") > -1: return false; // "edge";
+    case agent.indexOf("edg") > -1: return false; //"chromium based edge (dev or canary)";
+    case agent.indexOf("opr") > -1 && !!window.opr: return false; // "opera";
+    case agent.indexOf("chrome") > -1 && !!window.chrome: return false; // "chrome";
+    case agent.indexOf("trident") > -1: return "ie";
+    case agent.indexOf("firefox") > -1: return false; // "firefox";
+    case agent.indexOf("safari") > -1: return false; // "safari";
+    default: return false;
   }
 })(window.navigator.userAgent.toLowerCase());
 
@@ -42,7 +41,7 @@ if (browserNotSupported) {
   // root.render(<App />);
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
