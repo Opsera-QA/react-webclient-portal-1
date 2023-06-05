1ae9576663b4ae8505677da084205067edb876b1
Vignesh Subramanian <vignesh@opsera.io>
2023-06-05 11:00:26 +0530

    OPL-10552 AWS custom image UI changes

---
+++ src/components/workflow/pipelines/pipeline_details/workflow/step_configuration/step_tool_configuration_forms/argo_cd/inputs/ArgoCdStepAwsRepoSelectInput.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AWSRepositoryInput from "components/common/list_of_values_input/tools/aws/AWSRepositoryInput";
+import { isMongoDbId } from "components/common/helpers/mongo/mongoDb.helpers";
+
+function ArgoCdStepAwsRepoSelectInput({model, setModel, disabled, awsToolConfigId}) {
+  const setRepo = (fieldName, selectedOption) => {
+    let newModel = {...model};
+    newModel.setData("ecrRepoName", selectedOption);
+    setModel({...newModel});
+  };
+  const clearDataFunction = () => {
+    let newModel = {...model};
+    newModel.setData("ecrRepoName", "");
+    setModel({...newModel});
+  };
+  return (
+     <AWSRepositoryInput
+       fieldName={"ecrRepoName"}
+       awsToolId={awsToolConfigId}
+       dataObject={model}
+       setDataFunction={setRepo}
+       setDataObject={setModel}
+       clearDataFunction={clearDataFunction}
+       disabled={disabled || isMongoDbId(awsToolConfigId)}
+     />
+  );
+}
+
+ArgoCdStepAwsRepoSelectInput.propTypes = {
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  disabled: PropTypes.bool,
+  awsToolConfigId: PropTypes.string,
+};
+
+export default ArgoCdStepAwsRepoSelectInput;
