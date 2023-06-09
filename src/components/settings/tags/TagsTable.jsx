07b09b6fff4fcbe86cea49d8b40c5ab579ebea16
Noah Champoux <noah@opsera.io>
2023-06-09 08:56:46 -0600

    OPL-10362   [POC] Websocket Support for Tags

--- src/components/settings/tags/TagsTable.jsx
+++ src/components/settings/tags/TagsTable.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useMemo} from "react";
+import React, {useMemo} from "react";
 import PropTypes from "prop-types";
 import CustomTable from "components/common/table/CustomTable";
 import { useHistory } from "react-router-dom";
@@ -10,23 +10,21 @@ import {
   getTableTextColumn
 } from "components/common/table/table-column-helpers";
 import {getField} from "components/common/metadata/metadata-helpers";
-import NewTagOverlay from "components/settings/tags/NewTagOverlay";
-import ActiveFilter from "components/common/filters/status/ActiveFilter";
-import FilterContainer from "components/common/table/FilterContainer";
-import {faTags} from "@fortawesome/pro-light-svg-icons";
-import InlineTagTypeFilter from "components/common/filters/tags/tag_type/InlineTagTypeFilter";
-import {DialogToastContext} from "contexts/DialogToastContext";
-import TagTypeFilter from "components/common/filters/tags/tag_type/TagTypeFilter";
-import useComponentStateReference from "hooks/useComponentStateReference";
-import SiteRoleHelper from "@opsera/know-your-role/roles/helper/site/siteRole.helper";
 import tagTypeConstants from "@opsera/definitions/constants/settings/tags/tagType.constants";
+import {tagHelper} from "components/settings/tags/tag.helper";
+import {
+  FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION
+} from "components/common/table/FilterContainer";
 
-function TagsTable({ data, loadData, isLoading, tagFilterDto, setTagFilterDto, isMounted }) {
-  const toastContext = useContext(DialogToastContext);
+export default function TagsTable(
+  {
+    data,
+    loadData,
+    isLoading,
+    error,
+  }) {
   const history = useHistory();
   const fields = tagMetadata.fields;
-  const { userData } = useComponentStateReference();
-  const siteRole = SiteRoleHelper.getSiteRoleLevel(userData);
 
   const columns = useMemo(
     () => [
@@ -38,63 +36,24 @@ function TagsTable({ data, loadData, isLoading, tagFilterDto, setTagFilterDto, i
     []
   );
 
-  const onRowSelect = (rowData, type) => {
-    history.push("/settings/tags/" + rowData.original._id);
+  const onRowSelect = (rowData) => {
+    history.push(tagHelper.getDetailViewLink(rowData.original._id));
   };
 
   const rowStyling = (row) => {
     return !row["values"].active ? " inactive-row" : "";
   };
 
-  const createTag = () => {
-    toastContext.showOverlayPanel(<NewTagOverlay loadData={loadData} isMounted={isMounted} />);
-  };
-
-  const getDropdownFilters = () => {
-    return (
-      <>
-        <TagTypeFilter filterModel={tagFilterDto} setFilterModel={setTagFilterDto} className={"mb-2"} />
-        <ActiveFilter filterDto={tagFilterDto} setFilterDto={setTagFilterDto} />
-      </>
-    );
-  };
-
-  const getInlineFilters = () => {
-    return (
-      <InlineTagTypeFilter filterModel={tagFilterDto} setFilterModel={setTagFilterDto} loadData={loadData} className={"mr-2"} />
-    );
-  };
-
-  const getTagsTable = () => {
-    return (
-      <CustomTable
-        onRowSelect={onRowSelect}
-        data={data}
-        rowStyling={rowStyling}
-        columns={columns}
-        isLoading={isLoading}
-        loadData={loadData}
-        paginationDto={tagFilterDto}
-        setPaginationDto={setTagFilterDto}
-      />
-    );
-  };
-
   return (
-    <FilterContainer
-      loadData={loadData}
-      filterDto={tagFilterDto}
-      setFilterDto={setTagFilterDto}
-      addRecordFunction={siteRole !== SiteRoleHelper.SITE_ROLES.AUDITOR && siteRole !== SiteRoleHelper.SITE_ROLES.SECURITY_MANAGER ? createTag : undefined}
-      supportSearch={true}
+    <CustomTable
+      onRowSelect={onRowSelect}
+      data={data}
+      rowStyling={rowStyling}
+      columns={columns}
       isLoading={isLoading}
-      body={getTagsTable()}
-      inlineFilters={getInlineFilters()}
-      dropdownFilters={getDropdownFilters()}
-      metadata={tagMetadata}
-      titleIcon={faTags}
-      title={"Tags"}
-      className={"px-2 pb-2"}
+      loadData={loadData}
+      error={error}
+      tableHeight={FILTER_CONTAINER_FULL_HEIGHT_IN_SCREEN_CONTAINER_MINUS_DESCRIPTION}
     />
   );
 }
@@ -103,10 +62,5 @@ TagsTable.propTypes = {
   data: PropTypes.array,
   loadData: PropTypes.func,
   isLoading: PropTypes.bool,
-  tagFilterDto: PropTypes.object,
-  activeTagFilterDto: PropTypes.object,
-  setTagFilterDto: PropTypes.func,
-  isMounted: PropTypes.object
+  error: PropTypes.any,
 };
-
-export default TagsTable;
