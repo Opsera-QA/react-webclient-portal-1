8472bee4ffa082f277b2ef8877e816a4e799d9a6
Jim Provenza <Jim@Opsera.io>
2023-06-12 11:32:27 -0600

    add lightbulb when notification is enabled

--- src/components/tasks/details/tasks/notifications/TaskNotificationVerticalTabContainer.jsx
+++ src/components/tasks/details/tasks/notifications/TaskNotificationVerticalTabContainer.jsx
@@ -3,53 +3,60 @@ import React from "react";
 import VanitySetVerticalTab from "components/common/tabs/vertical_tabs/VanitySetVerticalTab";
 import {faMicrosoft, faSlack, faGoogle} from "@fortawesome/free-brands-svg-icons";
 import VanitySetVerticalTabContainer from "components/common/tabs/vertical_tabs/VanitySetVerticalTabContainer";
-import {faEnvelope} from "@fortawesome/pro-light-svg-icons";
+import {faEnvelope, faLightbulb} from "@fortawesome/pro-light-svg-icons";
+import IconBase from "components/common/icons/IconBase";
 
 function TaskNotificationVerticalTabContainer(
   {
     handleTabClickFunction,
     activeTab,
+    slackEnabled, 
+    teamsEnabled,
+    jiraEnabled,
+    serviceNowEnabled,
+    emailEnabled,
+    gChatEnabled,
   }) {
   return (
     <VanitySetVerticalTabContainer>
       <VanitySetVerticalTab
         icon={faEnvelope}
-        tabText={"Email"}
+        tabText={emailEnabled ? <div className={"d-flex"}>Email<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Email"}
         tabName={"email"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       {/*<VanitySetVerticalTab*/}
       {/*  icon={faJira}*/}
-      {/*  tabText={"Jira"}*/}
+      {/*  tabText={jiraEnabled ? <div className={"d-flex"}>Jira<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Jira"} */}
       {/*  tabName={"jira"}*/}
       {/*  handleTabClick={handleTabClickFunction}*/}
       {/*  activeTab={activeTab}*/}
       {/*/>*/}
       <VanitySetVerticalTab
         icon={faMicrosoft}
-        tabText={"Microsoft Teams"}
+        tabText={teamsEnabled ? <div className={"d-flex"}>Microsoft Teams<IconBase icon={faLightbulb} className={"ml-1"}/> </div> : "Microsoft Teams"}
         tabName={"teams"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       {/*<VanitySetVerticalTab*/}
       {/*  icon={faEnvelope}*/}
-      {/*  tabText={"ServiceNow"}*/}
+      {/*  tabText={serviceNowEnabled ? <div className={"d-flex"}>ServiceNow<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "ServiceNow"} */}
       {/*  tabName={"service-now"}*/}
       {/*  handleTabClick={handleTabClickFunction}*/}
       {/*  activeTab={activeTab}*/}
       {/*/>*/}
       <VanitySetVerticalTab
         icon={faSlack}
-        tabText={"Slack"}
+        tabText={slackEnabled ? <div className={"d-flex"}>Slack<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Slack"}
         tabName={"slack"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
       />
       <VanitySetVerticalTab
         icon={faGoogle}
-        tabText={"Google Chat"}
+        tabText={gChatEnabled ? <div className={"d-flex"}>Google Chat<IconBase icon={faLightbulb} className={"ml-3"}/> </div> : "Google Chat"}
         tabName={"gchat"}
         handleTabClick={handleTabClickFunction}
         activeTab={activeTab}
