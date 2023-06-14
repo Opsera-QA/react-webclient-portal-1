cfaeac5ca245d999dddbbc41da74d7579cd84925
Noah Champoux <noah@opsera.io>
2023-06-14 13:41:14 -0600

    Cleanup

---
+++ src/components/common/inputs/endpoints/endpoint/request/parameters/parameter/EndpointRequestParameterManualValueEntryInput.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CustomParameterSelectInput from "components/common/list_of_values_input/parameters/CustomParameterSelectInput";
+import CustomParameterComboBoxInput
+  from "components/common/list_of_values_input/parameters/CustomParameterComboBoxInput";
+import MultiTextListInputBase from "components/common/inputs/list/text/MultiTextListInputBase";
+import DateTimeInput from "components/common/inputs/date/DateTimeInput";
+import CenteredContentWrapper from "components/common/wrapper/CenteredContentWrapper";
+import BooleanSelectInput from "components/common/list_of_values_input/boolean/BooleanSelectInput";
+import {hasStringValue} from "components/common/helpers/string-helpers";
+import InfoText from "components/common/inputs/info_text/InfoText";
+
+export default function EndpointRequestParameterManualValueEntryInput(
+  {
+    disabled,
+    setDataFunction,
+    model,
+    setModel,
+    endpointParameterArrayInputHeight,
+    endpointParameterInputHeight,
+  }) {
+  const type = model?.getData("type");
+  const isSensitiveData = model?.getData("isSensitiveData");
+
+  const getInfoText = () => {
+    let infoText = "";
+
+    if (model?.getData("isRequired") === true) {
+      infoText += `${model?.getData("fieldName")} is required. If this is not included, the run will fail.`;
+    }
+
+    if (model?.getData("isSensitiveData") === true) {
+      infoText += `${model?.getData("fieldName")} is listed as sensitive data. You must use an encrypted custom parameter saved in the Tool Registry`;
+    }
+
+    return infoText;
+  };
+
+  const getInfoTextField = () => {
+    const infoText = getInfoText();
+
+    if (hasStringValue(infoText) === true) {
+      return (
+        <div className={"mx-2 mb-2"}>
+          <InfoText
+            customMessage={getInfoText()}
+          />
+        </div>
+      );
+    }
+  };
+
+  if (type == null || model == null || setDataFunction == null) {
+    return null;
+  }
+
+  const getInput = () => {
+    switch (type) {
+      case "string":
+        if (isSensitiveData === true) {
+          return (
+            <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
+              <CustomParameterSelectInput
+                model={model}
+                fieldName={"value"}
+                className={"value-parameter"}
+                requireVaultSavedParameters={true}
+                setDataFunction={(fieldName, customParameter) => setDataFunction(fieldName, customParameter?._id)}
+                disabled={disabled}
+              />
+              <div className={"d-flex justify-content-end"}>
+                {getInfoTextField()}
+              </div>
+            </div>
+          );
+        }
+
+        return (
+          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
+            <CustomParameterComboBoxInput
+              model={model}
+              fieldName={"value"}
+              className={"value-parameter"}
+              requireInsensitiveParameters={true}
+              setDataFunction={setDataFunction}
+              disabled={disabled}
+            />
+            <div className={"d-flex justify-content-end"}>
+              {getInfoTextField()}
+            </div>
+          </div>
+        );
+      case "array":
+        return (
+          <div className={"m-3"}>
+            <MultiTextListInputBase
+              model={model}
+              setModel={setModel}
+              fieldName={"value"}
+              setDataFunction={setDataFunction}
+              disabled={disabled}
+              singularTopic={"Value"}
+              pluralTopic={"Values"}
+              allowDuplicates={true}
+              minimumHeight={endpointParameterArrayInputHeight}
+              maximumHeight={endpointParameterArrayInputHeight}
+            />
+          </div>
+        );
+      case "date":
+        return (
+          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
+            <DateTimeInput
+              dataObject={model}
+              setDataObject={setModel}
+              setDataFunction={setDataFunction}
+              fieldName={"value"}
+              defaultToNull={true}
+              disabled={disabled}
+              clearDataFunction={() => setDataFunction("value", undefined)}
+            />
+          </div>
+        );
+      case "boolean":
+        return (
+          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
+            <BooleanSelectInput
+              model={model}
+              setModel={setModel}
+              fieldName={"value"}
+              setDataFunction={(fieldName, selectedOption) => setDataFunction(fieldName, selectedOption?.value)}
+              disabled={disabled}
+            />
+          </div>
+        );
+      case "object":
+      default:
+        return (
+          <CenteredContentWrapper>
+            <div>{`Entering this parameter type's value is not currently supported.`}</div>
+          </CenteredContentWrapper>
+        );
+    }
+  };
+
+  return (
+    <>
+      {getInput()}
+    </>
+  );
+}
+
+EndpointRequestParameterManualValueEntryInput.propTypes = {
+  setDataFunction: PropTypes.func,
+  disabled: PropTypes.bool,
+  model: PropTypes.object,
+  setModel: PropTypes.func,
+  endpointParameterArrayInputHeight: PropTypes.string,
+  endpointParameterInputHeight: PropTypes.string,
+};
--- src/components/workflow/plan/step/external_rest_api_integration/inputs/request/status/parameters/parameter/ExternalApiIntegratorStatusEndpointRequestParameterInputRow.jsx
+++ src/components/workflow/plan/step/external_rest_api_integration/inputs/request/status/parameters/parameter/ExternalApiIntegratorStatusEndpointRequestParameterInputRow.jsx
@@ -1,21 +1,14 @@
 import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import modelHelpers from "components/common/model/modelHelpers";
-import InfoText from "components/common/inputs/info_text/InfoText";
-import {hasStringValue} from "components/common/helpers/string-helpers";
-import CustomParameterSelectInput from "components/common/list_of_values_input/parameters/CustomParameterSelectInput";
-import CustomParameterComboBoxInput
-  from "components/common/list_of_values_input/parameters/CustomParameterComboBoxInput";
 import {faCode} from "@fortawesome/pro-light-svg-icons";
-import MultiTextListInputBase from "components/common/inputs/list/text/MultiTextListInputBase";
-import DateTimeInput from "components/common/inputs/date/DateTimeInput";
 import VanitySetTabContentContainer from "components/common/tabs/vertical_tabs/VanitySetTabContentContainer";
-import CenteredContentWrapper from "components/common/wrapper/CenteredContentWrapper";
-import BooleanSelectInput from "components/common/list_of_values_input/boolean/BooleanSelectInput";
 import BooleanToggleInput from "components/common/inputs/boolean/BooleanToggleInput";
 import {
   externalApiIntegratorStatusEndpointRequestParameterMetadata
 } from "components/workflow/plan/step/external_rest_api_integration/inputs/request/status/parameters/parameter/externalApiIntegratorStatusEndpointRequestParameter.metadata";
+import EndpointRequestParameterManualValueEntryInput
+  from "components/common/inputs/endpoints/endpoint/request/parameters/parameter/EndpointRequestParameterManualValueEntryInput";
 
 export default function ExternalApiIntegratorStatusEndpointRequestParameterInputRow(
   {
@@ -37,129 +30,27 @@ export default function ExternalApiIntegratorStatusEndpointRequestParameterInput
     updateParameterFunction({...newModel?.getCurrentData()});
   };
 
-  const getInfoText = () => {
-    let infoText = "";
-
-    if (endpointFieldModel?.getData("isRequired") === true) {
-      infoText += `${endpointFieldModel?.getData("fieldName")} is required. If this is not included, the run will fail.`;
-    }
-
-    if (endpointFieldModel?.getData("isSensitiveData") === true) {
-      infoText += `${endpointFieldModel?.getData("fieldName")} is listed as sensitive data. You must use an encrypted custom parameter saved in the Tool Registry`;
-    }
-
-    return infoText;
-  };
-
-  const getInfoTextField = () => {
-    const infoText = getInfoText();
-
-    if (hasStringValue(infoText) === true) {
-      return (
-        <div className={"mx-2 mb-2"}>
-          <InfoText
-            customMessage={getInfoText()}
-          />
-        </div>
-      );
-    }
-  };
-
-  const updateSensitiveCustomParameter = (fieldName, newValue) => {
-    updateMainModelFunction(fieldName, newValue?._id);
-  };
-
   const getValueInput = () => {
-    const type = endpointFieldModel?.getData("type");
-    const isSensitiveData = endpointFieldModel?.getData("isSensitiveData");
     const useRunApiResponseParameter = endpointFieldModel?.getData("useRunApiResponseParameter");
 
-    switch (type) {
-      case "string":
-        if (isSensitiveData === true) {
-          return (
-            <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
-              <CustomParameterSelectInput
-                model={endpointFieldModel}
-                fieldName={"value"}
-                className={"value-parameter"}
-                requireVaultSavedParameters={true}
-                setDataFunction={updateSensitiveCustomParameter}
-                disabled={disabled}
-              />
-              <div className={"d-flex justify-content-end"}>
-                {getInfoTextField()}
-              </div>
-            </div>
-          );
-        }
-
-        return (
-          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
-            <CustomParameterComboBoxInput
-              model={endpointFieldModel}
-              fieldName={"value"}
-              className={"value-parameter"}
-              requireInsensitiveParameters={true}
-              setDataFunction={updateMainModelFunction}
-              disabled={disabled}
-            />
-            <div className={"d-flex justify-content-end"}>
-              {getInfoTextField()}
-            </div>
-          </div>
-        );
-      case "array":
-        return (
-          <div className={"m-3"}>
-            <MultiTextListInputBase
-              model={endpointFieldModel}
-              setModel={setEndpointFieldModel}
-              fieldName={"value"}
-              setDataFunction={updateMainModelFunction}
-              disabled={disabled}
-              singularTopic={"Value"}
-              pluralTopic={"Values"}
-              allowDuplicates={true}
-              minimumHeight={endpointParameterArrayInputHeight}
-              maximumHeight={endpointParameterArrayInputHeight}
-            />
-          </div>
-        );
-      case "date":
-        return (
-          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
-            <DateTimeInput
-              dataObject={endpointFieldModel}
-              setDataObject={setEndpointFieldModel}
-              setDataFunction={updateMainModelFunction}
-              fieldName={"value"}
-              defaultToNull={true}
-              disabled={disabled}
-              clearDataFunction={() => updateMainModelFunction("value", undefined)}
-            />
-          </div>
-        );
-      case "boolean":
-        return (
-          <div className={"mx-3 mt-2"} style={{minHeight: endpointParameterInputHeight}}>
-            <BooleanSelectInput
-              model={endpointFieldModel}
-              setModel={setEndpointFieldModel}
-              fieldName={"value"}
-              setDataFunction={(fieldName, selectedOption) => updateMainModelFunction(fieldName, selectedOption?.value)}
-              disabled={disabled}
-            />
-          </div>
-        );
-      case "object":
-      default:
-        return (
-          <CenteredContentWrapper>
-            <div>{`Entering this parameter type's value is not currently supported.`}</div>
-          </CenteredContentWrapper>
-        );
+    if (useRunApiResponseParameter === true) {
+      return (
+        <>
+          Select run endpoint field
+        </>
+      );
     }
+
+    return (
+      <EndpointRequestParameterManualValueEntryInput
+        endpointParameterInputHeight={endpointParameterInputHeight}
+        endpointParameterArrayInputHeight={endpointParameterArrayInputHeight}
+        disabled={disabled}
+        model={endpointFieldModel}
+        setModel={setEndpointFieldModel}
+        setDataFunction={updateMainModelFunction}
+      />
+    );
   };
 
   if (endpointFieldModel == null) {
@@ -173,6 +64,7 @@ export default function ExternalApiIntegratorStatusEndpointRequestParameterInput
     >
       <div className={"h-100"}>
         <div className={"mx-3 mt-2"}>
+          {/*//TODO Ensure to clear out selected values when toggling this boolean*/}
           <BooleanToggleInput
             dataObject={endpointFieldModel}
             setDataFunction={(fieldName, newValue) => updateMainModelFunction(fieldName, newValue)}
