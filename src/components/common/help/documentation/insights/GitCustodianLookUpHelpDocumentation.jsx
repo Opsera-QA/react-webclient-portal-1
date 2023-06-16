c5fc7b848384c8feb1b9de48039fcfc88673722e
VishnuPriya-Opsera <vishnupriya@opsera.io>
2023-06-16 16:18:22 +0530

    OPL-10703 update in Gitcustodian tasks Dashboard help content

--- src/components/common/help/documentation/insights/GitCustodianLookUpHelpDocumentation.jsx
+++ src/components/common/help/documentation/insights/GitCustodianLookUpHelpDocumentation.jsx
@@ -1,6 +1,6 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 
-import {DialogToastContext} from "contexts/DialogToastContext";
+import { DialogToastContext } from "contexts/DialogToastContext";
 import HelpOverlayBase from 'components/common/overlays/center/help/HelpOverlayBase';
 
 const GitCustodianLookUpHelpDocumentation = () => {
@@ -20,25 +20,34 @@ const GitCustodianLookUpHelpDocumentation = () => {
                     <div className={"ml-2"}>
                         <div className={"mt-2"}><h5>Visuals</h5></div>
                         <div> There are 5 visuals placed as a carousel. The default page initially shows 3 visuals. Scroll towards right to see more.</div>
-                    <ul style={{listStyleType: "none"}}>
-                        <li><b>Total Repositories</b> - Provides the total number of repositories split by total clean and total unclean repositories. Supported filters are Date Range, Authors, Origin, Repositories, and Status.
-                        </li>
-                        <li><b>Timeline</b> - Represents the new secrets that have been exposed in a time series. The default time range is 3 months. Horizontal axis for Date, Vertical axis for Issues added. Users can change the time range using the Date Range filter along with Authors, Origin, Repositories, and Status.</li>
-                        <li><b>Top Secrets</b> - Limits to 5 commit hashes always. The top commit hashes that have the most number of secrets exposed. All filters are applicable. For a given date range, it represents the Top 5 commits that stands out with highest number of secrets.</li>
-                        <li><b>Top Repositories</b> - Limits to 5 repositories. The top repositories that have the highest number of secrets exposed. All filters are applicable. For a given date range, it represents the Top 5 Repositories that stand out with highest number of secrets.</li>
-                        <li><b>Top Users</b> - Limits to 5 users. The top 5 users who have contributed to exposing the most number of secrets. For a given date range, it represents the Top 5 Users whose commits have exposed highest number of secrets. All filters are applicable.</li>
-                    </ul>
-                    <div>
-                        <div className={"mt-2"}><h5>Report</h5></div>
-                        <div> The table view report provides in detail the list of secrets exposed. Users will be able to see when the issue was exposed, which repository, who is the Author, where is the secret located in the repository, how long it has been exposed for, what type of a secret is it, and what is the Jira Ticket number if created. All filters except the Date Range is applicable to this table.
-                            Apart from these, there are two other features.</div>
-                    </div>
-                    <ul style={{listStyleType: "none"}}>
-                        <li><b>Export Option</b> - Top right corner of the table view has a button to export the list of issues in  a PDF format.
-                        </li>
-                        <li><b>Create Jira Ticket</b> - This option allows users to create a Jira Ticket by selecting a set of issues, and also selecting the right Project Key and Issue Type to create the ticket. This ticket will contain details on the secrets, which includes Repository, Line number and File path.
-                        </li>
-                    </ul>
+                        <ul style={{ listStyleType: "none" }}>
+                            <li><b>Total Repositories</b> - Provides the total number of repositories split by total clean and total unclean repositories. Repositories with either No Vulnerable Commits or all Commits being marked as resolved/false positives/commit removed are also considered as clean repositories.
+                            </li>
+                            <li><b>Timeline</b> - Represents the new secrets that have been exposed in a time series. The default time range is 3 months. Horizontal axis for Date, Vertical axis for Issues added.</li>
+                            <li><b>Top Secrets</b> - Limits to 5 commit hashes always. The top commit hashes that have the most number of secrets exposed. For a given date range, it represents the Top 5 commits that stands out with highest number of secrets.</li>
+                            <li><b>Top Repositories</b> - Limits to 5 repositories. The top repositories that have the highest number of secrets exposed. For a given date range, it represents the Top 5 Repositories that stand out with highest number of secrets.</li>
+                            <li><b>Top Users</b> - Limits to 5 users. The top 5 users who have contributed to exposing the most number of secrets. For a given date range, it represents the Top 5 Users whose commits have exposed highest number of secrets.</li>
+                        </ul>
+                        <div>
+                            <div className={"mt-2"}><h5>Filters</h5></div>
+                            <div> Supported filters are Date Range, Authors, Origin, Repositories, and Status.</div>
+                            <div className={"mt-2"}><h5>Report</h5></div>
+                            <div> The table view report provides in detail the list of secrets exposed. Users will be able to see when the issue was exposed, which repository, who is the Author, where is the secret located in the repository, how long it has been exposed for, what type of a secret is it, and what is the Jira Ticket number if created. All filters except the Date Range is applicable to this table.
+                                Apart from these, there are two other features.</div>
+                        </div>
+                        <ul style={{ listStyleType: "none" }}>
+                            <li><b>Export Option</b> - Top right corner of the table view has a button to export the list of issues in  a PDF format.
+                            </li>
+                            <li><b>Create Jira Ticket</b> - This option allows users to create a Jira Ticket by selecting a set of issues, and also selecting the right Project Key and Issue Type to create the ticket. This ticket will contain details on the secrets, which includes Repository, Line number and File path.
+                            </li>
+                        </ul>
+                        <div className={"mt-2"}><h5>Entropy</h5></div>
+                        <ul style={{ listStyleType: "none" }}>
+                            <li>Plain Text passwords are identified using the entropy of the string identified. </li>
+                            <li>For instance, a string composed of only one character <b><i>aaaaa</i></b> has very low entropy. A longer string with a larger set of characters <b><i>wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY</i></b>, has higher entropy. </li>
+                            <li>Another example, the string “password” has an entropy of 2.75 </li>
+                            <li>For Git Custodian tasks, the default entropy is set to 3.5.  However, entropy can be set to different values in the range of 2.4 to 5.</li>
+                        </ul>
                     </div>
                 </div>
             </div>
