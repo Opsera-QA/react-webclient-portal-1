5fbcd756abcf45a7345cdff18ce3a1d33a0a6783
Noah Champoux <noah@opsera.io>
2023-06-18 09:41:01 -0600

    Added more functions

--- src/hooks/users/useUserActions.jsx
+++ src/hooks/users/useUserActions.jsx
@@ -1,6 +1,10 @@
 import useApiService from "hooks/api/service/useApiService";
 import routeTokenConstants from "@opsera/definitions/constants/routes/tokens/routeToken.constants";
 import useApiTokenHandlerActions from "hooks/token/useApiTokenHandlerActions";
+import baseActions from "utils/actionsBase";
+import {stringHelper} from "components/common/helpers/string/string.helper";
+import {generateUUID} from "components/common/helpers/string-helpers";
+import {apiTokenHelper} from "temp-library-components/helpers/api/token/apiToken.helper";
 
 export default function useUserActions() {
   const apiService = useApiService();
@@ -21,6 +25,48 @@ export default function useUserActions() {
       postBody,
     );
   };
+
+  userActions.isEmailAvailable = async (emailAddress) => {
+    const token = await apiTokenHandlerActions.generateApiCallToken(routeTokenConstants.ROUTE_MIDDLEWARE_TOKEN_KEYS.DOES_EMAIL_EXIST);
+    const apiUrl = "/users/check-email";
+    const postBody = {
+      email: emailAddress,
+      hostname: window.location.hostname,
+    };
+
+    return await apiService.handleCustomTokenApiPostRequest(
+      token,
+      apiUrl,
+      postBody,
+    );
+  };
+
+  userActions.createFreeTrialAccount = async (registrationModel) => {
+    const finalObject = registrationModel?.getPersistData();
+    const company = finalObject?.company;
+    const attributes = {
+      title: finalObject?.title,
+      company: company,
+    };
+    const configuration = {
+      cloudProvider: "GKE",
+      cloudProviderRegion: "",
+    };
+    finalObject.company = undefined;
+    finalObject.title = undefined;
+    finalObject.attributes = attributes;
+    finalObject.configuration = configuration;
+    finalObject.organizationName = `${stringHelper.replaceSpacesWithUnderscores(company)}-${finalObject?.email}-${generateUUID()}`;
+    const token = await apiTokenHandlerActions.generateApiCallToken(routeTokenConstants.ROUTE_MIDDLEWARE_TOKEN_KEYS.CREATE_OPSERA_ACCOUNT);
+    const apiUrl = "/users/create";
+
+    return await apiService.handleCustomTokenApiPostRequest(
+      token,
+      apiUrl,
+      finalObject,
+    );
+  };
+
 
   return userActions;
 }
