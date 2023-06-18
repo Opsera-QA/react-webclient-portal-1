b075861c6af1c4b13d0dc1fda3d0309ab81d6ac1
Noah Champoux <noah@opsera.io>
2023-06-18 09:25:06 -0600

    WIP

--- src/hooks/token/useGetUnauthenticatedReactToken.jsx
+++ src/hooks/token/useGetUnauthenticatedReactToken.jsx
@@ -1,45 +1,23 @@
-import {useEffect, useState} from "react";
 import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
-import useLoadData from "temp-library-components/useLoadData/useLoadData";
 import useAuthenticationActions from "hooks/token/useAuthenticationActions";
-import routeTokenConstants from "@opsera/definitions/constants/routes/tokens/routeToken.constants";
+import {decodeToken, isExpired} from "react-jwt";
 
-export default function useGetUnauthenticatedReactToken(
-  id = routeTokenConstants.UNAUTHENTICATED_ROUTE_MIDDLEWARE_TOKEN_KEYS.GENERIC_KEY,
-  handleErrorFunction,
-) {
-  const [reactToken, setReactToken] = useState(undefined);
+export default function useApiTokenHandlerActions() {
   const authenticationActions = useAuthenticationActions();
-  const {
-    isLoading,
-    error,
-    setError,
-    loadData,
-  } = useLoadData();
+  const apiTokenHandlerActions = {};
 
-  useEffect(() => {
-    setReactToken(undefined);
-    loadData(getReactToken, handleErrorFunction).catch(() => {
-    });
-  }, [id]);
-
-  const getReactToken = async () => {
-    setReactToken(undefined);
-
-    const response = await authenticationActions.getReactAuthenticationToken();
-    const temporaryToken = DataParsingHelper.parseNestedString(response, "data.data");
-
-    if (temporaryToken) {
-      setReactToken({...temporaryToken});
-    }
+  apiTokenHandlerActions.generateApiCallToken = async (id) => {
+    const response = await authenticationActions.getReactAuthenticationToken(id);
+    return DataParsingHelper.parseNestedString(response, "data.data");
   };
 
-  return ({
-    reactToken: reactToken,
-    setReactToken: setReactToken,
-    loadData: () => loadData(getReactToken, handleErrorFunction),
-    isLoading: isLoading,
-    error: error,
-    setError: setError,
-  });
+  apiTokenHandlerActions.decodeToken = (token) => {
+    return decodeToken(token);
+  };
+
+  apiTokenHandlerActions.isTokenExpired = (token) => {
+    return isExpired(token);
+  };
+  
+  return apiTokenHandlerActions;
 }
