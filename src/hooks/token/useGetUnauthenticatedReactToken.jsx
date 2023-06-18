87ebe70c515e2bd043c7b5dea04e4853c67ad5f8
Noah Champoux <noah@opsera.io>
2023-06-18 08:52:07 -0600

    WIP

---
+++ src/hooks/token/useGetUnauthenticatedReactToken.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import DataParsingHelper from "@opsera/persephone/helpers/data/dataParsing.helper";
+import useLoadData from "temp-library-components/useLoadData/useLoadData";
+import useAuthenticationActions from "hooks/token/useAuthenticationActions";
+
+export default function useGetUnauthenticatedReactToken(
+  id = ,
+  handleErrorFunction,
+) {
+  const [reactToken, setReactToken] = useState(undefined);
+  const authenticationActions = useAuthenticationActions();
+  const {
+    isLoading,
+    error,
+    setError,
+    loadData,
+  } = useLoadData();
+
+  useEffect(() => {
+    setReactToken(undefined);
+    loadData(getReactToken, handleErrorFunction).catch(() => {
+    });
+  }, [id]);
+
+  const getReactToken = async () => {
+    setReactToken(undefined);
+
+    const response = await authenticationActions.getReactAuthenticationToken();
+    const temporaryToken = DataParsingHelper.parseNestedString(response, "data.data");
+
+    if (temporaryToken) {
+      setReactToken({...temporaryToken});
+    }
+  };
+
+  return ({
+    reactToken: reactToken,
+    setReactToken: setReactToken,
+    loadData: () => loadData(getReactToken, handleErrorFunction),
+    isLoading: isLoading,
+    error: error,
+    setError: setError,
+  });
+}
