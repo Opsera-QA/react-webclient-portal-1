9f427bd714ee3e357b4fae3482c62254be8c6217
Noah Champoux <noah@opsera.io>
2023-06-21 10:16:08 -0600

    Update EndpointRequestParameterInputRow.jsx

--- src/components/common/inputs/endpoints/endpoint/request/parameters/parameter/EndpointRequestParameterInputRow.jsx
+++ src/components/common/inputs/endpoints/endpoint/request/parameters/parameter/EndpointRequestParameterInputRow.jsx
@@ -10,6 +10,7 @@ import EndpointRequestParameterManualValueEntryInput
   from "components/common/inputs/endpoints/endpoint/request/parameters/parameter/EndpointRequestParameterManualValueEntryInput";
 import ExternalApiIntegrationStepRunResponseParameterSelectInput
   from "components/workflow/plan/step/external_rest_api_integration/inputs/request/ExternalApiIntegrationStepRunResponseParameterSelectInput";
+import H5FieldSubHeader from "components/common/fields/subheader/H5FieldSubHeader";
 
 export default function EndpointRequestParameterInputRow(
   {
@@ -33,24 +34,26 @@ export default function EndpointRequestParameterInputRow(
     updateParameterFunction({...newModel?.getCurrentData()});
   };
 
-  const setModelFunction = (newModel) => {
-    setEndpointFieldModel({...newModel});
-    updateParameterFunction({...newModel?.getCurrentData()});
-  };
-
   const getValueInput = () => {
     const useRunApiResponseParameter = endpointFieldModel?.getData("useRunApiResponseParameter");
 
     if (useRunApiResponseParameter === true) {
       return (
-        <ExternalApiIntegrationStepRunResponseParameterSelectInput
-          model={endpointFieldModel}
-          setModel={setModelFunction}
-          toolId={toolId}
-          runEndpointId={runEndpointId}
-          disabled={disabled}
-          setDataFunction={updateMainModelFunction}
-        />
+        <div
+          className={"mx-3 mt-2"}
+          style={{minHeight: endpointParameterInputHeight}}
+        >
+          <H5FieldSubHeader
+            subheaderText={"Select the Call Operation Response field to use for this parameter"}
+          />
+          <ExternalApiIntegrationStepRunResponseParameterSelectInput
+            model={endpointFieldModel}
+            toolId={toolId}
+            runEndpointId={runEndpointId}
+            disabled={disabled}
+            setDataFunction={updateMainModelFunction}
+          />
+        </div>
       );
     }
 
