e8b349fc0df80fb273c78932a098b2e93a700247
Noah Champoux <noah@opsera.io>
2023-06-22 14:20:57 -0600

    Update EndpointResponseEvaluationRuleInput.jsx

--- src/components/common/inputs/endpoints/endpoint/response/evaluation/rule/EndpointResponseEvaluationRuleInput.jsx
+++ src/components/common/inputs/endpoints/endpoint/response/evaluation/rule/EndpointResponseEvaluationRuleInput.jsx
@@ -108,6 +108,8 @@ function EndpointResponseEvaluationRuleInput(
             responseBodyType={endpoint?.responseBodyType}
           />
         );
+      default:
+        return null;
     }
   };
 
