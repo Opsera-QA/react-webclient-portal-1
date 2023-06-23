e2ff854f8ef45c4d9d280524191e0f3f70a7ad00
Kishlaya Kesarwani <133632490+KishlayaK-Opsera@users.noreply.github.com>
2023-06-23 18:08:53 +0530

    master opl-10243-m Agent Label clean up - Jenkins job template (#737)
    * opl-10243 Agent Label clean up - Jenkins job template
    
    * opl-10243 Agent Label clean up - Jenkins job template - minor fix
    
    * OPL-10243 Agent Label clean up-Jenkins job template

--- src/components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput.jsx
+++ src/components/common/list_of_values_input/tools/jenkins/jobs/JenkinsJobsGenericAgentTypeSelectInput.jsx
@@ -1,28 +1,84 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SelectInputBase from "components/common/inputs/select/SelectInputBase";
+import JenkinsJobTypes from "components/inventory/tools/tool_details/tool_jobs/jenkins/jobs/jenkinsJobTypes.constants";
 
 export const JENKINS_JOBS_GENERIC_AGENT_TYPES = [
   {
     "name": "Ubuntu Agent",
-    "env" : "linux",
+    "env": "linux",
     "value": "generic-linux",
   },
   {
     "name": "Windows Agent",
-    "env" : "windows",
+    "env": "windows",
     "value": "generic-windows",
   }
 ];
 
-function JenkinsJobsGenericAgentTypeSelectInput({ fieldName, model, setModel, setDataFunction, agentList }) {
+function JenkinsJobsGenericAgentTypeSelectInput({ fieldName, model, setModel, setDataFunction, agentList, jenkinsJobType }) {
+
+  const getJenkinsJobAgentTypes = () => {
+
+    const JENKINS_JOBS_WINDOWS_AGENT_TYPE = [
+      {
+        "name": "Windows Agent",
+        "env": "windows",
+        "value": "generic-windows",
+      },
+    ];
+    const JENKINS_JOBS_UBUNTU_AGENT_TYPE = [
+      {
+        "name": "Ubuntu Agent",
+        "env": "linux",
+        "value": "generic-linux",
+      },
+    ];
+
+    if (jenkinsJobType === JenkinsJobTypes.BUILD) {
+      switch (model?.getData("buildType")) {
+        case "dotnet":
+          return JENKINS_JOBS_WINDOWS_AGENT_TYPE;
+        case "gradle":
+        case "maven":
+        case "docker":
+        case "node":
+          return JENKINS_JOBS_UBUNTU_AGENT_TYPE;
+        case "msbuild":
+          return [];
+        default:
+          return JENKINS_JOBS_GENERIC_AGENT_TYPES;
+      }
+    }
+
+    switch (jenkinsJobType) {
+      case JenkinsJobTypes.SFDC:
+      case JenkinsJobTypes.PMD_SCAN:
+      case JenkinsJobTypes.SFDC_CODE_SCAN:
+      case JenkinsJobTypes.CODE_SCAN:
+      case JenkinsJobTypes.UNIT_TESTING:
+      case JenkinsJobTypes.FUNCTIONAL_TESTING:
+      case JenkinsJobTypes.PERFORMANCE_TESTING:
+      case JenkinsJobTypes.CYPRESS_UNIT_TESTING:
+      case JenkinsJobTypes.NUNIT_UNIT_TESTING:
+      case JenkinsJobTypes.DOCKER_PUSH:
+      case JenkinsJobTypes.ARTIFACTORY_DOCKER_PUSH:
+      case JenkinsJobTypes.SFDC_PUSH_ARTIFACTS:
+      case JenkinsJobTypes.POWERSHELL_SCRIPT:
+      case JenkinsJobTypes.AZURE_DOCKER_PUSH:
+        return JENKINS_JOBS_UBUNTU_AGENT_TYPE;
+      case JenkinsJobTypes.COVERITY:
+        return JENKINS_JOBS_GENERIC_AGENT_TYPES;
+    }
+  }
+
   return (
     <SelectInputBase
       fieldName={fieldName}
       dataObject={model}
       setDataObject={setModel}
       setDataFunction={setDataFunction}
-      selectOptions={agentList ? agentList :JENKINS_JOBS_GENERIC_AGENT_TYPES}
+      selectOptions={agentList ? agentList : getJenkinsJobAgentTypes()}
       valueField="value"
       textField="name"
     />
@@ -35,6 +91,7 @@ JenkinsJobsGenericAgentTypeSelectInput.propTypes = {
   setModel: PropTypes.func,
   setDataFunction: PropTypes.func,
   agentList: PropTypes.array,
+  jenkinsJobType: PropTypes.string
 };
 
 JenkinsJobsGenericAgentTypeSelectInput.defaultProps = {
