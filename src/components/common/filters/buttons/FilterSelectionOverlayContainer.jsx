461239082404a841b9635e5e406701cec1e1748e
Noah Champoux <noah@opsera.io>
2023-06-27 10:21:49 -0600

    Update FilterSelectionOverlayContainer.jsx

--- src/components/common/filters/buttons/FilterSelectionOverlayContainer.jsx
+++ src/components/common/filters/buttons/FilterSelectionOverlayContainer.jsx
@@ -40,9 +40,9 @@ export default function FilterSelectionOverlayContainer(
     if (filterModel?.getNewInstance) {
       newFilterModel = filterModel.getNewInstance();
 
-      // if (newFilterModel?.resetUrlParameters) {
-      //   newFilterModel?.resetUrlParameters();
-      // }
+      if (newFilterModel?.resetUrlParameters) {
+        newFilterModel?.resetUrlParameters();
+      }
     } else {
       newFilterModel = new Model({...filterModel.getNewObjectFields()}, filterModel.getMetaData(), false);
     }
