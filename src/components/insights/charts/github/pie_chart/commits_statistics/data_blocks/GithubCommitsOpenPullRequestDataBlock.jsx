277fcf630047324a3272946b71c1663b6426ba40
DivyeshaMalhotra <divyesha@opsera.us>
2023-06-27 12:14:22 -0400

    OPL-10596-Master

---
+++ src/components/insights/charts/github/pie_chart/commits_statistics/data_blocks/GithubCommitsOpenPullRequestDataBlock.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import DataBlockBoxContainer from "components/common/metrics/data_blocks/DataBlockBoxContainer";
+import ThreeLineScoreDataBlock from "../../../../../../common/metrics/score/ThreeLineScoreDataBlock";
+import TwoLineScoreDataBlock from "../../../../../../common/metrics/score/TwoLineScoreDataBlock";
+
+function GitHubCommitsOpenPullRequestDataBlock({ data, icon, className, dataPoint, onSelect, lastScore, iconOverlayBody}) {
+    return (
+        <DataBlockBoxContainer showBorder={true}  onClickFunction={onSelect}>
+            <TwoLineScoreDataBlock
+                className={"p-3 h-100"}
+                score={data}
+                subtitle={"Total Open Pull Requests"}
+            />
+        </DataBlockBoxContainer>
+    );
+}
+
+GitHubCommitsOpenPullRequestDataBlock.propTypes = {
+    data: PropTypes.number,
+    icon: PropTypes.object,
+    className: PropTypes.string,
+    onSelect: PropTypes.func,
+    lastScore: PropTypes.number,
+    iconOverlayBody: PropTypes.any,
+    dataPoint: PropTypes.object,
+};
+
+export default GitHubCommitsOpenPullRequestDataBlock;
