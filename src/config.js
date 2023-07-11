d7795e0b736b1847d847cff8003558f765143868
Noah Champoux <noah@opsera.io>
2023-07-11 11:08:00 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,3 @@
-function onAuthRequired({ history }) {
-  history.push("/login");
-}
-
-export const okta_config = {
-  issuer: process.env.REACT_APP_OKTA_ISSUER,
-  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
-  redirect_uri: process.env.REACT_APP_OPSERA_OKTA_REDIRECTURI,
-  pkce: true,
-  disableHttpsCheck: false,
-  onAuthRequired: onAuthRequired,
-};
-
 export const NODE_API_ORCHESTRATOR_SERVER_URL =
   process.env.REACT_APP_OPSERA_API_SERVER_URL;
 export const NODE_ANALYTICS_API_SERVER_URL =
